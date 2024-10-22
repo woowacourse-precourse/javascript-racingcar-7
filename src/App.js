0cfb4be073f772bef81f4d5f1145e4a13830bf0d
RohSungKyun <songkyun1108@naver.com>
2024-10-22 22:12:53 +0900

    feat : 자동차 이름, 반복횟수 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const carName = await this.carInput();
+    const time = await this.timeInput();
+  }
+  async carInput(){
+    const carNameArray = Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    return carNameArray;
+  }
+  async timeInput(){
+    const time = Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    return time;
+  }
 }
 
 export default App;
