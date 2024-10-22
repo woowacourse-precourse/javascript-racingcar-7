fc910b3e4f0a5dd912a219be699fa2b275236a3f
yheseul <kangyheseul@naver.com>
2024-10-22 17:54:11 +0900

    chore: 입력받는 메시지에 개행 추가

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 export const RUN_MESSAGE = {
   INPUT_CAR_NAMES:
-    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
-  INPUT_ATTEMPTS: "시도할 횟수는 몇 회인가요?",
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+  INPUT_ATTEMPTS_NUMBER: "시도할 횟수는 몇 회인가요?\n",
   FINAL_WINNER: "최종 우승자 : ",
   RUN_RESULT: "실행 결과",
   COLON: " : ",
