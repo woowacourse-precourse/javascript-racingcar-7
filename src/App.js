2c28af287208d0e9d9da6a4fac5c6e62d9ed896e
위영진 <qser155@naver.com>
2024-10-22 17:02:30 +0900

    feat: validate input values for car name and race count

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import { input, print } from '../utils/index.js';
+
 class App {
     constructor() {
         this.state = {
             cars: [],
             raceCount: 0,
         };
+        this.error = {
+            cars: (value) => {
+                if (value.length === 0) return 'error';
+                const parsedString = value.split(',');
+                for (let i = 0; i < parsedString.length; i++) {
+                    if (
+                        parsedString[i].length === 0 ||
+                        parsedString[i].length > 5
+                    ) {
+                        return 'error';
+                    }
+                }
+            },
+            raceCount: (value) => {
+                const num = Number(value);
+                if (isNaN(num)) return 'error';
+                if (num < 0) return 'error';
+                if (num !== Math.floor(num)) return 'error';
+            },
+        };
     }
+
     setState(key, value) {
         this.state = {
             ...this.state,
@@ -15,10 +37,14 @@ class App {
 
     async getValues() {
         const cars = await input(
-            '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
+            '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+            this.error.cars
         );
-        const raceCount = await input('시도할 횟수는 몇 회인가요?');
-        this.setState('cars', cars);
+        const raceCount = await input(
+            '시도할 횟수는 몇 회인가요?',
+            this.error.raceCount
+        );
+        this.setState('cars', cars.split(','));
         this.setState('raceCount', Number(raceCount));
     }
     async run() {
--- utils/input.js
+++ utils/input.js
@@ -1,11 +1,11 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-export default async function input(sentence) {
+export default async function input(sentence, checkError) {
     try {
         const inputValue = await MissionUtils.Console.readLineAsync(
             `${sentence}\n`
         );
-
+        if (checkError(inputValue) === 'error') throw new Error('[ERROR]');
         return inputValue;
     } catch (error) {
         throw new Error('[ERROR]');
