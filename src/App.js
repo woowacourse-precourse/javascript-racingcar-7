9ed4063927a81f291605fa8de4d5ef9b220780ab
spoyodevelop <kjy9505@gmail.com>
2024-10-22 18:04:20 +0900

    feat: add finishLine input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { Console, Random } from '@woowacourse/mission-utils';
 class App {
-  async run() {}
+  async run() {
+    const finishLine =
+      await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+  }
 }
 
 export default App;
