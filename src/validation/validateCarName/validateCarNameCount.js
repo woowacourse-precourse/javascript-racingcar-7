6d03c9587f7060d4a8d2dfd2f68f2a2038264e68
yheseul <kangyheseul@naver.com>
2024-10-22 18:11:18 +0900

    refactor: 입력받은 자동차 이름 수 검증 함수명 및 파일명 변경

--- src/validation/validateCarName/validateCarNameCount.js
+++ src/validation/validateCarName/validateCarNameCount.js
@@ -1,6 +1,6 @@
 import { ERROR_MESSAGES } from "../../constants/constants.js";
 
-export function validateCarNameMinimumNumber(inputCarNames) {
+export function validateCarNameCount(inputCarNames) {
   const isValid = inputCarNames.length >= 2;
   if (!isValid) throw new Error(ERROR_MESSAGES.MINIMUM_TWO_CARS_REQUIRED);
   return;
