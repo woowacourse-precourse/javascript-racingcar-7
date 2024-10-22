2e970979ec219d3213d889c4d22340dca6e3629a
dmson1218 <dmson1218@naver.com>
2024-10-23 00:36:36 +0900

    feat(App): 자동차 이름 입력 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+    async run() {}
+
+    async getCarNameString() {
+        try {
+            const message = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+            const input = await Console.readLineAsync(message);
+            return input;
+        } catch (error) {}
+    }
 }
 
 export default App;
