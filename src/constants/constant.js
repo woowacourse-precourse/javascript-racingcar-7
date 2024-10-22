30f7b3ed04adb72cacad17805858a693ba4fbc27
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-22 20:09:49 +0900

    feat: 자동차경주 실행을 위한 상수 설정

---
+++ src/constants/constant.js
@@ -0,0 +1,12 @@
+const CARRACE_START_CONDITION = Object.freeze({
+  carNumber: 2,
+  carNameMaxlength: 5,
+  attemptMaxnumber: 30,
+});
+
+const SEPARATOR = Object.freeze({
+  carSeparator: ",",
+  winnerSeparator: ",",
+});
+
+export { CARRACE_START_CONDITION, SEPARATOR };
