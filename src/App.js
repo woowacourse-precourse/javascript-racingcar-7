a1df2729b9f7a51f1c89c656a5aebd314804f6a9
yheseul <kangyheseul@naver.com>
2024-10-22 18:13:18 +0900

    feat: 자동차 이름 검증 로직 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Console } from "@woowacourse/mission-utils";
 import { RUN_MESSAGE } from "./constants/constants.js";
+import { validateCarName } from "./validation/validation.js";
 
 class App {
   async run() {
     const inputCarName = await Console.readLineAsync(
       RUN_MESSAGE.INPUT_CAR_NAMES
     );
+    validateCarName(inputCarName);
     const inputAttemptsNumber = await Console.readLineAsync(
       RUN_MESSAGE.INPUT_ATTEMPTS_NUMBER
     );
