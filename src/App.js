2dc68c0a4aaedae9dcf32846d56987c4062d7bcd
unknown <suyeonshim@gm.gist.ac.kr>
2024-10-22 18:17:26 +0900

    feat(1): 입력 예외 처리

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ class App {
     const cars = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
+    if (!cars) {
+      throw new Error("[ERROR] 자동차 이름을 입력해주세요.");
+    }
     const times = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    if (!times) {
+      throw new Error("[ERROR] 시도 횟수를 입력해주세요.");
+    }
     const cars_list = cars.split(",");
     // Console.print(cars_list);
     Console.print("");
