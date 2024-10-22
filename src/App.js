92f1bc12e539f7f6a08d2b8ca721df7d8638f377
이은호 <eunho@ieunhoui-MacBookAir.local>
2024-10-22 20:54:38 +0900

    feat : 자동차 이름 입력 유효성 검증 && 리스트<자동차>생성

--- src/App.js
+++ src/App.js
@@ -2,23 +2,40 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const cars = await inputCars()
+    const cars = await inputCars();
   }
 }
 
 class Car {
-  constructor(name){
+  constructor(name) {
     this.name = name;
     this.distance = "";
   }
-  moveForward(){
+  moveForward() {
     this.distance += "-";
   }
 }
 
 async function inputCars() {
-  let cars =  String(await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n")).split(",");
-  return cars;
+  const cars = String(
+    await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    )
+  ).split(",");
+  validateInput(cars);
+  return cars.map((car)=>new Car(car));
 }
+function validateInput(cars){
+  cars = cars.forEach((car) => {
+    if (car == "") {
+      throw Error(["[ERROR] : 잘못된 입력입니다."]);
+    }
+    if (car.length > 5) {
+      throw Error(["[ERROR] : 자동차 이름은 5자 이하만 가능합니다."]);
+    }
+  });
+}
+
+
 
 export default App;
