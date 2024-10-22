2ba0420358a8f5213e1fbb4d93b4361b3f7d05eb
murphybread <nargene@gmail.com>
2024-10-22 16:24:59 +0900

    feat(input): add getUserInputCount function to get count from user

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { Console, Random } from "@woowacourse/mission-utils";
 
 
 const RACING_VARIABLES = {
-  INPUT_PROMPT : "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+  INPUT_PROMPT : "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+  INPUT_COUNT_PROMPT : "시도할 횟수는 몇 회인가요?\n"
+
 }
 Object.freeze(RACING_VARIABLES);
 
@@ -14,12 +16,20 @@ class UserInput{
         const userInput = await Console.readLineAsync(RACING_VARIABLES.INPUT_PROMPT);
         return userInput;
     } catch (error) {
-        Console.print('[ERROR]: 입력을 받는 중 문제가 발생했습니다.');
+        Console.print('[ERROR]: 경주할 자동차를 입력을 받는 중 문제가 발생했습니다.');
         return null;
     }
+  }
 
-  
-}
+  async getUserInputCount(){
+    try {
+      const userInput = await Console.readLineAsync(RACING_VARIABLES.INPUT_COUNT_PROMPT);
+      return Number(userInput);
+    } catch (error) {
+      Console.print('[ERROR]: 횟수를 입력을 받는 중 문제가 발생했습니다.');
+      return null;
+    }
+  }
 }
 
 class Validator {
@@ -44,6 +54,8 @@ class App {
     const userInputArray = userInput.split(",");
 
     this.validator.validateNameLength(userInputArray);
+
+    const userInputCount = await this.userInput.getUserInputCount();
 
 
   }
