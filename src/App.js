a0d7d6616d196268ecbaab54840ac1abb91502c0
spoyodevelop <kjy9505@gmail.com>
2024-10-22 18:10:15 +0900

    feat: add input console for carNames

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
   async run() {
+    //  TODO : 자동차 이름 validation 하기
+
+    const carNames = await Console.readLineAsync('자동차 이름을 입력해 주세요');
+    console.log(carNames.split(','));
     const finishLine =
       await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+    console.log(finishLine);
   }
 }
 
