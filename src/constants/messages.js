7b1db7a5dadb84750b2c55200ea29d2d89ada698
장정안 <wjddks96@naver.com>
2024-10-22 18:49:04 +0900

    feat: 자동차 경주 게임에 필요한 메세지 추가

---
+++ src/constants/messages.js
@@ -0,0 +1,8 @@
+const GAME_MESSAGE = Object.freeze({
+  START: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  ATTEMPTS: '시도할 횟수는 몇 회인가요?\n',
+  RESULT: '실행 결과',
+  FINAL_WINNER: '최종 우승자 :',
+});
+
+export { GAME_MESSAGE };
