9298389abb203fb6215151b19a6f48e7265da3bd
oko_jin <dswvgw6412@naver.com>
2024-10-23 02:48:59 +0900

    feat(App): 자동차 이름 입력받기 구현
    입력받으면 쉼표로 구분하여 자동차 이름으로 Car 객체 생성 후 cars 배열에 저장

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from '@woowacourse/mission-utils';
+import Car from './Car.js';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const carInput = await Console.readLineAsync(
+        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+      );
+      const cars = carInput.split(',').map((name) => new Car(name));
+    } catch (e) {}
+  }
 }
 
 export default App;
