f15c7f24d1159a7a0cbe20534741ebc0ec2f5c47
murphybread <nargene@gmail.com>
2024-10-22 18:35:09 +0900

    feat(validator): add validateRound maximum round

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { Console, Random } from "@woowacourse/mission-utils";
 const RACING_VARIABLES = {
   INPUT_PROMPT : "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
   INPUT_COUNT_PROMPT : "시도할 횟수는 몇 회인가요?\n",
-  POSITION_MARKER : "-"
+  POSITION_MARKER : "-",
+  MAXIMUM_ROUND : 10
 
 }
 Object.freeze(RACING_VARIABLES);
@@ -41,6 +42,12 @@ class Validator {
         throw new Error(`[ERROR]: 이름 "${name}"이 5자를 초과했습니다.`);
       }
     });
+  }
+
+  validateRound(count){
+    if (count > RACING_VARIABLES.MAXIMUM_ROUND){
+      throw new Error(`[ERROR]: 최대 라운드 횟수 "${RACING_VARIABLES.MAXIMUM_ROUND}"회를 초과했습니다.`);
+    }
   }
 }
 
@@ -110,6 +117,8 @@ class App {
     let positions = this.raceController.createPlayersPositions(userInputArray.length);
 
     const userInputCount = await this.userInput.getUserInputCount();
+
+    this.validator.validateRound(userInputCount);
 
     
     // Game start
