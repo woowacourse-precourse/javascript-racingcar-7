b811ae2ca80d580f4d5d1e9360260a1d56b5e661
kimywann <Kimywan10@gmail.com>
2024-10-22 18:12:13 +0900

    feat(App.js): 자동차 이름 입력 및 검증 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+import { validateCarNames, playNumIsNaN } from "./Validator.js"
+
+
 class App {
-  async run() {}
+  async run() {
+    const carName = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+    const carNames = carName.split(',');
+    validateCarNames(carNames);
+  }
 }
 
 export default App;
