0a6f3e6d4874137baaae921038e32c686af45451
kimywann <Kimywan10@gmail.com>
2024-10-22 22:44:05 +0900

    feat(App.js): 자동차 전진 여부를 출력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import { Console, Random } from "@woowacourse/mission-utils";
-import { validateCarNames, playNumIsNaN } from "./Validator.js"
+import { Validator } from "./Validator.js"
 
 
 class App {
   async run() {
     const carName = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-    const carNames = carName.split(',');
-    validateCarNames(carNames);
+    const carNames = carName.split(",");
+    Validator.validateCarNames(carNames);
 
     const playNum = await Console.readLineAsync("시도할 횟수는 몇 회 인가요?\n");
-    playNumIsNaN(playNum)
+    Validator.validatePlayNum(playNum)
+
+    const results = {};
+
+    carNames.forEach(name => {
+      results[name] = "";
+    });
+    Console.print("\n실행 결과");
+
+    for (let i = 0; i < playNum; i++) {
+
+      carNames.forEach(name => {
+        const randomNum = Random.pickNumberInRange(0, 9);
+
+        if (randomNum >= 4) {
+          results[name] += "-";
+        }
+        Console.print(`${name} : ${results[name]}`);
+      });
+      Console.print("");
+    }
   }
 }
 
