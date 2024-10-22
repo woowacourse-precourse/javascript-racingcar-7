017d2c676745724da6d1d3f6059de2c129f340f9
donggyun <donggyunkim12@gmail.com>
2024-10-22 23:01:13 +0900

    feat(App): 자동차 입력 기능 구현
    - 사용자에게 경주할 자동차들을 입력받는다.
    - 예외처리를 통과한다면 입력받은 자동차를 쉼표(,) 로 구분하여 split된 배열을 반환한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console } from '@woowacourse/mission-utils';
+
+const MESSAGE = {
+  INTRO: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n',
+};
+
 class App {
-  async run() {}
+  async run() {
+    const cars = await this.setCars();
+  }
+
+  async setCars() {
+    const input = await Console.readLineAsync(MESSAGE.INTRO);
+
+    const carsArr = input.split(',');
+    if (carsArr.some((name) => name.length > 5)) {
+      throw new Error('[ERROR] 자동차의 이름은 5자 이하만 가능합니다.');
+    }
+
+    return carsArr;
+  }
 }
 
 export default App;
