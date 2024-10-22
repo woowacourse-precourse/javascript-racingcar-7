ded8af9c4a729910b87b206083df5bd443a82066
kimywann <Kimywan10@gmail.com>
2024-10-22 22:45:01 +0900

    refactor(Validator.js): Validator 클래스로 자동차 이름 및 시도 횟수 검증 기능 리팩토링

--- src/Validator.js
+++ src/Validator.js
@@ -1,16 +1,21 @@
-export function validateCarNames(names) {
-    for (let i = 0; i < names.length; i++) {
-    const name = names[i].trim();
+export class Validator {
+    static validateCarNames(names) {
+
+        for (let i = 0; i < names.length; i++) {
+            const name = names[i].trim();
     
-    if (name.length > 5) {
-            throw new Error('[ERROR] 이름은 5자 이하만 가능합니다.');
+            if (name.length > 5) {
+                throw new Error("[ERROR] 이름은 5자 이하만 가능합니다.");
+            }
         }
     }
-}
+    static validatePlayNum(num) {
 
-export function playNumIsNaN(num) {
-    
-    if (isNaN(num)) {
-        throw new Error('[ERROR] 시도 횟수는 숫자를 입력해야 합니다.');
+        if (isNaN(num)) {
+            throw new Error("[ERROR] 시도 횟수는 숫자를 입력해야 합니다.");
+        }
+        if (num < 0) {
+            throw new Error("[ERROR] 시도 횟수는 음수 값을 허용하지 않습니다.");
+        }
     }
 }
