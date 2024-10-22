a06b121d2071018b08aacf201067517679c16a43
Dongubak <91736442+Dongubak@users.noreply.github.com>
2024-10-22 16:46:48 +0900

    featL: input from console
    input from console: 콘솔로 부터 구분자로 구분된 이름들과 시도 횟수를 입력받는 함수 input 생성
    
    BREAKE input from console

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  constructor() {
+    this.stringFromConsole = '';
+    this.testCount = 0;
+  }
+
+  async input() {
+    this.stringFromConsole = await Console.readLineAsync();
+    this.testCount = await Console.readLineAsync();
+  }
+
+
+
+  async run() {
+    await this.input();
+    
+  }
 }
 
 export default App;
