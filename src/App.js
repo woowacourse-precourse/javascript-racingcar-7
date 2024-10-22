673b075c068d9da617901507ea90c5b8f31e4a3b
yheseul <kangyheseul@naver.com>
2024-10-22 17:54:37 +0900

    feat: 사용자로부터 경주할 자동차 이름을 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
+import { RUN_MESSAGE } from "./constants/constants.js";
+
 class App {
-  async run() {}
+  async run() {
+    const inputCarName = await Console.readLineAsync(
+      RUN_MESSAGE.INPUT_CAR_NAMES
+    );
+  }
 }
 
 export default App;
