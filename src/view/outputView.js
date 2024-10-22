07c5bf6cbd4cd01cba5ec51f7383f93b4c6ec095
장정안 <wjddks96@naver.com>
2024-10-22 18:53:18 +0900

    feat: 게임의 프린트를 담당하는 outputView 작성

---
+++ src/view/outputView.js
@@ -0,0 +1,10 @@
+//@ts-check
+import { Console } from '@woowacourse/mission-utils';
+
+const outputView = {
+  /**@param {string} message  */
+  printMessage(message) {
+    Console.print(message);
+  },
+};
+export default outputView;
