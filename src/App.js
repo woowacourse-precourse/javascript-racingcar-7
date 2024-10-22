302d6c2ed29a79c75b7cae6cba3b1df428d1a8aa
이은호 <eunho@ieunhoui-MacBookAir.local>
2024-10-22 20:45:49 +0900

    feat : 자동차 이름 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    
+    const cars = await inputCars()
   }
 }
 
@@ -14,6 +14,11 @@ class Car {
   moveForward(){
     this.distance += "-";
   }
+}
+
+async function inputCars() {
+  let cars =  String(await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n")).split(",");
+  return cars;
 }
 
 export default App;
