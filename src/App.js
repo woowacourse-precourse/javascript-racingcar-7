f96b1990ff5c54b775a7cc740e320c561c130561
Yubeenpark <yoobinpark@gmail.com>
2024-10-22 18:46:00 +0900

    feat(car): add function for car name input with length validation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const { Console } = MissionUtils;
+async function carNameInput() {
+  const carNames = await Console.readLineAsync(
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+  );
+  if (carNames.split(",").some((carName) => carName.trim().length > 5)) {
+    throw new Error("[ERROR]");
+  }
+  return carNames.split(",");
+}
 class App {
-  async run() {}
+  async run() {
+    const Cars = await carNameInput();
+  }
 }
 
 export default App;
