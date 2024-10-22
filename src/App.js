8c97aa4f6d1f3652a639dc3c8dd74fb51bb82255
kimywann <Kimywan10@gmail.com>
2024-10-22 18:16:25 +0900

    feat(App.js): 경주 시도 횟수를 입력받고 숫자를 검증하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ class App {
     const carName = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     const carNames = carName.split(',');
     validateCarNames(carNames);
+
+    const playNum = await Console.readLineAsync("시도할 횟수는 몇 회 인가요?\n");
+    playNumIsNaN(playNum)
   }
 }
 
