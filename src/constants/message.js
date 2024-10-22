5ce038a650e0d390a98380930218ccc55f61c155
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-22 20:06:34 +0900

    feat: 입,출력 메시지 상수화

---
+++ src/constants/message.js
@@ -0,0 +1,12 @@
+const INPUT_MESSAGE = Object.freeze({
+  CarNameMessage:
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n",
+  attemptNumberMessage: "시도할 횟수는 몇 회인가요?\n",
+});
+
+const OUTPUT_MESSAGE = Object.freeze({
+  winnerMessage: "최종 우승자 : ",
+  moveForward: "-",
+});
+
+export { INPUT_MESSAGE, OUTPUT_MESSAGE };
