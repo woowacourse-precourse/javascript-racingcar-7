bc3be3dbabc5154bb70ba33601d518f48041817d
deokbin <deokbin@naver.com>
2024-10-24 00:56:03 +0900

    test<Validation> : 불필요 테스트 케이스 제거 쉼표 이외의 다른 값의 경우- 쉼표로 구분시 하나의 문자열로 반환 추가 구분자 인식 불필요하여 테스트 케이스 제거

--- __tests__/Validation.test.js
+++ __tests__/Validation.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 import App from '../src/App.js';
-import isValidInput from '../src/NameValidation.js';
+import isValidName from '../src/NameValidation.js';
 import {
   EMPTY_CAR_NAME_ERROR,
   ONE_CAR_NAME_ERROR,
@@ -13,29 +13,25 @@ import {
 describe('유효성 검사 테스트', () => {
   test('문자열 여러개를 쉼표로 구분지어 입력하면 그대로 반환한다.', () => {
     const validInput = 'pobi,woni';
-
-    const app = new App();
-    expect(app.isValidInput(validInput)).toBe(true);
+    expect(isValidName(validInput)).toBe(true);
   });
 });
 
 // Error Case
 describe('유효하지 않은 값 테스트', () => {
   test('빈 값일 경우 에러를 반환한다.', () => {
-    expect(() => isValidInput('')).toThrow(EMPTY_CAR_NAME_ERROR);
+    expect(() => isValidName('')).toThrow(EMPTY_CAR_NAME_ERROR);
   });
   test('한 개의 값만 입력한 경우 에러를 반환한다.', () => {
-    expect(() => isValidInput('john')).toThrow(ONE_CAR_NAME_ERROR);
+    expect(() => isValidName('john')).toThrow(ONE_CAR_NAME_ERROR);
   });
-  test('쉼표 이외의 다른 값으로 구분했을 경우 에러를 반환한다.', () => {
-    expect(() => isValidInput('john;mary')).toThrow(INVALID_SEPERATOR);
-  });
+
   test('이름이 다섯글자 이상인 경우 에러를 반환한다.', () => {
-    expect(() => isValidInput('Catherine, John')).toThrow(
+    expect(() => isValidName('Catherine, John')).toThrow(
       MORE_THAN_FIVE_LETTERS,
     );
   });
   test('중복된 이름이 있을 경우 에러를 반환한다.', () => {
-    expect(() => isValidInput('John,John')).toThrow(SAME_CAR_NAME_ERROR);
+    expect(() => isValidName('John,John')).toThrow(SAME_CAR_NAME_ERROR);
   });
 });
