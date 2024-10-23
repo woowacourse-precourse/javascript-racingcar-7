8f9f8e481ff9e571bdd06eaff008d8d092c20473
guesung <gueit214@naver.com>
2024-10-23 13:41:44 +0900

    refactor: 사용자 입력 받는 과정을 `getUserInput`로 모듈화

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ class App {
   #TRACE_CHARACTER = "-";
   #MAX_CAR_NAME_LENGTH = 5;
   async run() {
-    const userInputCarNames = await readLineAsync(INPUT_MESSAGE_CAR_NAMES);
-    const userInputTryCount = await readLineAsync(INPUT_MESSAGE_TRY_COUNT);
+    const [userInputCarNames, userInputTryCount] = await this.getUserInput();
+
     const carNameArray = convertStringToArray(userInputCarNames, ",");
     this.validateCarNameLength(carNameArray, 5);
     const tryCount = +userInputTryCount;
@@ -50,6 +50,14 @@ class App {
     const winnerCarArray = getKeyArrayHasSameValueInMap(carTraceMap, maxTrace);
     print(`${OUTPUT_MESSAGE_WINNER}${winnerCarArray.join(", ")}`);
   }
+
+  async getUserInput() {
+    const carNames = await readLineAsync(INPUT_MESSAGE_CAR_NAMES);
+    const tryCount = await readLineAsync(INPUT_MESSAGE_TRY_COUNT);
+
+    return [carNames, tryCount];
+  }
+
   validateCarNameLength(carNameArray) {
     try {
       validateStringArrayLength(carNameArray, this.#MAX_CAR_NAME_LENGTH);
