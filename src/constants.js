a4ceee51c3b745a0b4001e4a96b9990e993faaa4
guesung <gueit214@naver.com>
2024-10-23 12:13:07 +0900

    feat: 입출력 메시지 상수화

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,9 @@ export const ERROR_MESSAGE_CAR_NAME_OVER_FIVE = `${ERROR_MESSAGE_DEFAULT} 길이
 export const ERROR_MESSAGE_NOT_INTEGER = `${ERROR_MESSAGE_DEFAULT} 정수를 입력해주세요.`;
 export const ERROR_MESSAGE_NOT_POSITIVE_POSITIVE = `${ERROR_MESSAGE_DEFAULT} 양수를 입력해주세요.`;
 export const ERROR_MESSAGE_NOT_NUMBER = `${ERROR_MESSAGE_DEFAULT} 숫자를 입력해주세요.`;
+
+export const INPUT_MESSAGE_CAR_NAMES =
+  "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+export const INPUT_MESSAGE_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
+
+export const OUTPUT_MESSAGE_WINNER = "최종 우승자 : ";
