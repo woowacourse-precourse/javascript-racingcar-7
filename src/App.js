9bc5b4c49291afc02fa121afbaeea69f881c258b
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-10-24 00:36:54 +0900

    refactor: 함수 이름 변경
    - 함수에서 검증까지 이뤄진다는 것을 표현하기 위해 이름 변경

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { createCars } from './utils/createCars';
-import { getNameInput, getRoundCountInput } from './utils/inputHandler';
+import {
+  getValidateNameInput,
+  getValidateRoundCountInput,
+} from './utils/inputHandler';
 import Race from './models/Race';
 import WinnerCalculator from './models/WinnerCalculator';
 
 class App {
   async run() {
     try {
-      const names = await getNameInput();
+      const names = await getValidateNameInput();
       const cars = createCars(names);
 
-      const roundCount = await getRoundCountInput();
+      const roundCount = await getValidateRoundCountInput();
 
       const race = new Race(cars, roundCount);
       race.start();
--- src/utils/inputHandler.js
+++ src/utils/inputHandler.js
@@ -1,7 +1,7 @@
 import { Console } from '@woowacourse/mission-utils';
 import Validator from '../validators/Validator';
 
-export const getNameInput = async () => {
+export const getValidateNameInput = async () => {
   const nameString = await Console.readLineAsync(
     '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
   );
@@ -13,7 +13,7 @@ export const getNameInput = async () => {
   return names;
 };
 
-export const getRoundCountInput = async () => {
+export const getValidateRoundCountInput = async () => {
   const roundCount = Number(
     await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n')
   );
