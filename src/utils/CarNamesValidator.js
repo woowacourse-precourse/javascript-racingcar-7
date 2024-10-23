faeb704f57770389fdf1482e5b2460d38901346b
yoo_min <vendetta12@naver.com>
2024-10-23 23:58:31 +0900

    feat: 입력한 자동차가 1개일 경우 예외처리

--- src/utils/CarNamesValidator.js
+++ src/utils/CarNamesValidator.js
@@ -7,7 +7,7 @@ class CarNamesValidator {
         });
     }
 
-    checkEmptyCarName(carNames) {
+    checkEmptyCarNames(carNames) {
         carNames.forEach((name) => {
             if (name === "" || name === null) {
                 throw new Error("[ERROR] 자동차 이름은 빈 문자열일 수 없습니다.");
@@ -15,17 +15,24 @@ class CarNamesValidator {
         });
     }
 
-    checkDuplicateCarName(carNames) {
+    checkDuplicateCarNames(carNames) {
         const uniqueCarNames = new Set(carNames);
         if (uniqueCarNames.size !== carNames.length) {
             throw new Error("[ERROR] 자동차 이름은 중복될 수 없습니다.");
         }
     }
 
+    checkCarNamesOnce(carNames) {
+        if (carNames.length < 2) {
+            throw new Error("[ERROR] 자동차는 최소 2대 이상이어야 합니다.");
+        }
+    }
+
     validateCarNames(carNames) {
         this.checkCarNamesLength(carNames);
-        this.checkEmptyCarName(carNames);
-        this.checkDuplicateCarName(carNames);
+        this.checkEmptyCarNames(carNames);
+        this.checkDuplicateCarNames(carNames);
+        this.checkCarNamesOnce(carNames);
     }
 }
 export default CarNamesValidator;
