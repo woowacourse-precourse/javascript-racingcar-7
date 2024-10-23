0f73acb99edd4498fb1f1cc4099417d602bd3b1e
oko_jin <dswvgw6412@naver.com>
2024-10-23 16:02:23 +0900

    refactor(constant): 에러메시지 상수로 정리
    - prefix: [ERROR]
    - unexpectedError: 예상치 못한 에러
    - invalidDelimiter: 구분자 에러
    - invalidCount: 시도 횟수 입력 에러

---
+++ src/constant.js
@@ -0,0 +1,6 @@
+export const errorMessages = {
+  prefix: '[ERROR]',
+  unexpectedError: '예상치 못한 에러가 발생하였습니다.',
+  invalidDelimiter: '쉼표(,)로 구분된 이름을 입력해주세요.',
+  invalidCount: '시도 횟수는 1 이상의 정수만 입력 가능합니다.',
+};
