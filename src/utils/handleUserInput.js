4dd1ab3c2160d8ea21fe52e6a2ccdc1a8bdb9c27
ssingfly32 <ssingfly3232@gmail.com>
2024-10-23 14:37:16 +0900

    feat: 시도 횟수 입력받는 기능 추가

--- src/utils/handleUserInput.js
+++ src/utils/handleUserInput.js
@@ -3,8 +3,10 @@ import { validateCars } from "./validate.js";
 
 export default async function handleUserInput() {
     const carNameInput = await getCarName();
-    const cars = validateCars(carNameInput);
-    
+    const carName = validateCars(carNameInput);
+    const executionCount = await getExecutionCount();
+    const cars = { carName, executionCount };
+
     return cars;
 }
 
@@ -12,4 +14,11 @@ async function getCarName() {
     const carNameInput = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력해주세요.(5자 이하, 여러 대일 경우 쉼표로 구분)\n');
 
     return carNameInput;
+}
+
+async function getExecutionCount() {
+    const userInput = await MissionUtils.Console.readLineAsync('시도할 횟수를 입력해주세요.\n');
+    const executionCount = parseInt(userInput);
+    
+    return executionCount;
 }
