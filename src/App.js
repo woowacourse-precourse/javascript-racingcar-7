9c90f8298aa517e005d3803d6cbb77d6c2281ded
guddnboy <hung3018@naver.com>
2024-10-23 16:08:56 +0900

    feat: 경주할 자동차 이름 입력 받기 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
+function getCarNames(userInput) {
+  const carNames = userInput.split(",");
+  carNames.map((carName) => {
+    return carName.trim();
+  });
+  return carNames;
+}
 class App {
-  async run() {}
+  async run() {
+    let userInput = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.\n"
+    );
+    getCarNames(userInput);
+  }
 }
 
 export default App;
