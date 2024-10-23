67fbdf379f759b939d7006534a985e13f1f5b1d3
Minhyuk Kim <labcs@naver.com>
2024-10-23 16:26:46 +0900

    docs : README 파일에 배운점 정리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Console } from '@woowacourse/mission-utils';
 
+import { makeError } from '../util/makeError.js';
+
 class App {
   async run() {
     const inputValue = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
@@ -7,6 +9,8 @@ class App {
     const attemptCount = Number(await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n')); // 시도할 횟수 입력
 
     const cars = inputValue.split(',').map((car) => car.trim()); // 자동차 이름을 배열에 저장
+
+    makeError(cars);
 
     const carMoveCount = {};
 
