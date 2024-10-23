43c2aa11eedb22e6b547c4adf92dc889a1e5df09
jicho <jIcho6412@gmail.com>
2024-10-24 01:30:04 +0900

    입력받은 사용자 이름을 쉼표(,)로 구분하여 변수에 담는 기능 구현

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ class App {
     const carNames = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
+    function separateCarName(carNames) {
+      const separateCarNames = carNames.split(",");
+      return separateCarNames;
+    }
+    Console.print(separateCarName(carNames));
   }
 }
 
