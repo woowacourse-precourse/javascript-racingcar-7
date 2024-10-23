2588c4b99b490a3f42ab48bd5da8a9fdd78b3606
heeyoung123 <heeyoung321@naver.com>
2024-10-24 05:11:08 +0900

    feat: 자동차 이름과 시도할 횟수 입출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { getInputCar } from './Input/carName.js';
+import { getInputCount } from './Input/gameCount.js';
+
 class App {
-  async run() {}
+  async run() {
+    const inputCarName = await getInputCar();
+    const inputGameCount = await getInputCount();
+  }
 }
 
 export default App;
