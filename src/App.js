2edcf27c49ec6e448223410ff060d2afb1e205c0
seongmin <seongm22n@gmail.com>
2024-10-23 23:12:13 +0900

    refactor: 변수명 수정
    - 의미를 더 명확히 하기 위해 변수명 변경

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import { Console } from '@woowacourse/mission-utils';
+import { checkCarInputLength } from './errors/carError.js';
+import { checkEmptyInput } from './errors/commonError.js';
+
 class App {
 	async run() {
 		const carNameInput = await Console.readLineAsync(
 			`경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n`
 		);
-		const raceCountInput = await Console.readLineAsync(
+		const tryNumber = await Console.readLineAsync(
 			`시도할 횟수는 몇 회인가요?\n`
 		);
 
 		const carList = carNameInput.split(',');
 		checkCarInputLength(carList);
 		checkEmptyInput(carNameInput);
-		checkEmptyInput(raceCountInput);
+		checkEmptyInput(tryNumber);
+
+		// 전진 : 무작위 값 0-9까지 값의 4 이상의 값
+
+		// 멈춤 : 3이하
+
 	}
 }
 
