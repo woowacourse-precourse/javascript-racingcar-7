a44b52d7d650ee5478d23b43a73ee8fd3b7aa0b8
장정안 <wjddks96@naver.com>
2024-10-23 16:36:21 +0900

    feat: 자동차 경주를 진행하는 로직 구현
    - 입력받은 자동차들을 배열로 저장한 다음, 무작위 숫자가 4이상일 경우 앞으로 전진

--- src/controller/Game.js
+++ src/controller/Game.js
@@ -1,15 +1,43 @@
-//@ts-check
-
+import { MissionUtils } from '@woowacourse/mission-utils';
 import { GAME_MESSAGE } from '../constants/messages.js';
 import User from '../user/User.js';
+import outputView from '../view/outputView.js';
+import Car from '../model/Car.js';
 
 class Game {
   constructor() {
     this.user = new User();
+    this.cars = [];
   }
+
   async process() {
     const carNameInput = await this.user.readCarNameInput();
+    this.initializeCars(carNameInput.split(','));
     const attempts = await this.user.readAttemptsInput();
+    outputView.printMessage(`${GAME_MESSAGE.RESULT}`);
+    this.race(Number(attempts));
+  }
+
+  initializeCars(carNames) {
+    this.cars = carNames.map((name) => new Car(name.trim()));
+  }
+
+  race(attempts) {
+    for (let i = 0; i < attempts; i++) {
+      this.moveForward();
+      this.printRaceStatus();
+    }
+  }
+
+  moveForward() {
+    this.cars.forEach((car) => car.move());
+  }
+
+  printRaceStatus() {
+    this.cars.forEach((car) => {
+      outputView.printMessage(`${car.name} : ${'-'.repeat(car.position)}`);
+    });
+    outputView.printMessage('\n');
   }
 }
 
