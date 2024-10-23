d8a56b5d0306227f0b1adcead627fc48594a58e3
jeongeunLee <leeju1009@gmail.com>
2024-10-24 00:03:26 +0900

    feat(racing): 자동차 경주를 정해진 횟수만큼 시도하는 함수 추가

--- src/Racing.js
+++ src/Racing.js
@@ -13,6 +13,16 @@ class Racing {
     const distances = cars.map((car) => car.driving());
     return distances;
   }
+
+  play() {
+    const tryCount = this.#tryCount;
+    const rounds = Array.from(
+      { length: tryCount },
+      (arrayLike, index) => index + 1,
+    );
+
+    rounds.forEach(() => this.try());
+  }
 }
 
 export default Racing;
