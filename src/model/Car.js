caf12e6b399b7c28a815cad8db621b6c6911418f
장정안 <wjddks96@naver.com>
2024-10-23 16:35:07 +0900

    feat: 자동차의 움직임을 관리하는 Car 클래스 생성

---
+++ src/model/Car.js
@@ -0,0 +1,16 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.position = 0;
+  }
+
+  move() {
+    if (MissionUtils.Random.pickNumberInRange(0, 9) > 4) {
+      this.position++;
+    }
+  }
+}
+
+export default Car;
