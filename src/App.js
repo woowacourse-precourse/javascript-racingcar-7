ab10cf287a14a3218e0197fd0d4b197b0d47877f
guddnboy <hung3018@naver.com>
2024-10-23 16:22:35 +0900

    feat: 이름 5글자 초과 시 에러 처리

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import { Console, Random } from "@woowacourse/mission-utils";
 function getCarNames(userInput) {
   const carNames = userInput.split(",");
   carNames.map((carName) => {
+    if (carName.length > 5) {
+      throw new Error("[ERROR] 이름은 5글자를 초과할 수 없습니다.");
+    }
     return carName.trim();
   });
   return carNames;
 }
+
 class App {
   async run() {
     let userInput = await Console.readLineAsync(
