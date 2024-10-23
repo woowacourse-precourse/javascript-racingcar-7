a66617e552979a6e63c90b244b796ee6af1b1556
youngju6143 <youngju6143@naver.com>
2024-10-23 23:29:43 +0900

    feat(Input): 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
+async function getNames() {
+  try {
+    let input = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    return input;
+  } catch (error) {
+    throw new Error('[ERROR]');
+  }
+}
+
+async function getCount() {
+  try {
+    let count = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    return count;
+  } catch (error) {
+    throw new Error('[ERROR]');
+  }
+}
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      let input = await getNames();
+      let count = await getCount();
+
+      let nameArray = input.split(',');
+      let moveArray = Array.from({length: nameArray.length}, () => '');
+
+    } catch (error) {
+      throw new Error('[ERROR]');
+    }
+  }
 }
 
 export default App;
