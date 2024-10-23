5b94dcf9bf8159cf0705a1adbc86d9a586bf9381
RAKRAK21 <94699316+RAKRAK21@users.noreply.github.com>
2024-10-23 18:46:25 +0900

    feat: implement car name input in App class
    - Add functionality to receive car names from user input
    - Complete feature #4 from README requirements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Console } from "@woowacourse/mission-utils";
+import Car from "./Car";
+
 class App {
-  async run() {}
+  cars = [];
+
+  async run() {
+    try {
+      await carNames();
+    }
+    catch {
+
+    }
+  }
+  
+  async carNames() {
+    const input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+    const names = input.split(',');
+    this.cars = names.map(name => new Car(name));
+  }
 }
 
 export default App;
