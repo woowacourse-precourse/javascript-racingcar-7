df7dcaddbc5bc157ab3e09340e22d2a062022b80
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-23 14:52:17 +0900

    feat: 랜덤 값 4 이상인 경우 전진

--- src/model/racingcar.js
+++ src/model/racingcar.js
@@ -1,6 +1,6 @@
 import { Random } from "@woowacourse/mission-utils";
 
-class racingcar {
+class Racingcar {
   constructor(name) {
     this.name = name;
     this.distance = 0;
@@ -9,4 +9,14 @@ class racingcar {
   randomNumberProduction = () => {
     return Random.pickNumberInRange(0, 9);
   };
+
+  stopAndGo = () => {
+    const STANDARD_NUMBER = this.randomNumberProduction();
+
+    if (STANDARD_NUMBER >= 4) {
+      this.distance += 1;
+    }
+  };
 }
+
+export default Racingcar;
