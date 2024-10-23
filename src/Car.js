798e9e56318d7b434b52d409bebca755244628c3
spoyodevelop <kjy9505@gmail.com>
2024-10-23 21:24:05 +0900

    refactor(car): refactor car for more reusability

--- src/Car.js
+++ src/Car.js
@@ -1,45 +1,42 @@
 import { Random } from '@woowacourse/mission-utils';
 
 export class Car {
-  #carName; // private 필드
-  #position; // private 필드
+  #carName;
+  #position;
+
+  static MIN_MOVE_THRESHOLD = 4;
+  static MAX_RANDOM_VALUE = 10;
 
   constructor(carName) {
+    if (typeof carName !== 'string') {
+      throw new Error('유효하지 않은 이름입니다.');
+    }
     this.#carName = carName;
     this.#position = 0;
   }
 
-  // carName에 대한 getter
   get carName() {
     return this.#carName;
   }
 
-  // carName에 대한 setter
-  set carName(newName) {
-    if (typeof newName === 'string' && newName.trim() !== '') {
-      this.#carName = newName;
-    } else {
-      throw new Error('유효하지 않은 이름입니다.');
-    }
-  }
-
-  // position에 대한 getter (position은 외부에서 직접 수정하지 않으므로 setter는 필요하지 않음)
   get position() {
     return this.#position;
   }
 
   moveForward() {
-    if (Random.pickNumberInRange(1, 10) >= 4) {
+    if (
+      Random.pickNumberInRange(1, Car.MAX_RANDOM_VALUE) >=
+      Car.MIN_MOVE_THRESHOLD
+    ) {
       this.#position += 1;
     }
   }
 
   getPositionString() {
-    if (this.#position === 0) return '';
-    return '-'.repeat(this.#position);
+    return '-'.repeat(this.#position); // 위치가 0일 경우에도 빈 문자열을 반환함
   }
 
   toString() {
-    return `${this.carName} : ${this.getPositionString()}`;
+    return `${this.#carName} : ${this.getPositionString()}`;
   }
 }
