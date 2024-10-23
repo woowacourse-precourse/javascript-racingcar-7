e2f2845b090814f3d2c636ad7ef75bd97ced4337
youdame <112458620+youdame@users.noreply.github.com>
2024-10-24 02:30:10 +0900

    feat : 랜덤 값 후 4이상이 나올 경우, 전진하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
   async run() {
     const carNamesInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
     const carNames = carNamesInput.split(',');
-    const attemptCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요\n');
 
-    // 5자가 넘는 이름이 나올 경우 에러
-    console.log(carNames, attemptCount);
+    const attemptCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    Console.print('\n');
+    Console.print('실행 결과');
+    // 5자가 넘는 이름이 나올 경우 에러 , 같은 이름일 경우 에러?
+
+    const record = {};
+
+    carNames.forEach((car) => {
+      record[car] = 0;
+    });
+
+    for (let count = 1; count <= attemptCount; count += 1) {
+      for (let order = 0; order < carNames.length; order += 1) {
+        const randomNumber = Random.pickNumberInRange(0, 9);
+        if (randomNumber > 4) {
+          record[carNames[order]] += 1;
+        }
+
+        Console.print(`${[carNames[order]]} : ${'-'.repeat(record[carNames[order]])}`);
+      }
+      Console.print('\n');
+    }
+    Console.print(`최종 우승자 : ${record}`);
   }
 }
 
