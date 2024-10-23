af9e7f18897a5f57e58d61bd4c382e5f7ba36273
murphybread <nargene@gmail.com>
2024-10-23 13:54:45 +0900

    refactor(RacingVariables.js): make class to file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import { Console, Random } from "@woowacourse/mission-utils";
+import RACING_VARIABLES from './constants/RacingVariables';
 
 
-const RACING_VARIABLES = {
-  INPUT_PROMPT : "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
-  INPUT_COUNT_PROMPT : "시도할 횟수는 몇 회인가요?\n",
-  POSITION_MARKER : "-",
-  MAXIMUM_ROUND : 10,
-  WINNER_PROMPT : "최종 우승자"
 
-}
-Object.freeze(RACING_VARIABLES);
 
 class UserInput{
 
