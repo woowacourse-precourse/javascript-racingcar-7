3a1635407c029a048da7a8a013ffd0432a9da0e9
deokbin <deokbin@naver.com>
2024-10-23 23:24:22 +0900

    feat: 에러 메시지 상수로 추가

---
+++ src/constants/Error.js
@@ -0,0 +1,7 @@
+export const EMPTY_CAR_NAME_ERROR = '[ERROR] 빈 문자열을 입력할 수 없습니다.';
+export const ONE_CAR_NAME_ERROR =
+  '[ERROR] 경주할 자동차 이름을 두 개 이상 입력해주세요.';
+export const INVALID_SEPERATOR = '[ERROR] 유효하지 않은 구분자입니다.';
+export const MORE_THAN_FIVE_LETTERS =
+  '[ERROR] 자동차 이름은 5글자를 넘어갈 수 없습니다.';
+export const SAME_CAR_NAME_ERROR = '[ERROR] 자동차 이름은 중복될 수 없습니다.';
