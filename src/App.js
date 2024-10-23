d1aa0a49db513229dae9365c0e92666d340253cb
Minhyuk Kim <labcs@naver.com>
2024-10-23 15:02:14 +0900

    feat : 경주할 자동차 이름 입력 및 쉼표 기준 분리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const INPUT = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const Cars = INPUT.split(',').map((car) => car.trim());
+
+    Console.print(Cars);
+  }
 }
 
 export default App;
