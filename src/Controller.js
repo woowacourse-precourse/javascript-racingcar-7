17789ec37c4d2961b7362370b9170bd3311c77cc
kimyou1102 <yoosk5485@naver.com>
2024-10-23 21:16:19 +0900

    feat: 자동차 이름을 입력받는 기능 구현

---
+++ src/Controller.js
@@ -0,0 +1,24 @@
+import View from './View.js';
+
+export default class Controller {
+  constructor() {
+    this.view = new View();
+    this.names = [];
+  }
+
+  async start() {
+    this.names = await this.getNames();
+  }
+
+  async getNames() {
+    const rawInput = this.view.getInput(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+    );
+
+    const input = await rawInput;
+
+    const names = input.split(',').map((name) => name.trim());
+
+    return names;
+  }
+}
