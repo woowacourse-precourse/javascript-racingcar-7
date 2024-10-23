c21f0f88572bfb61c0ee5ed78c4601a12a932483
dmson1218 <dmson1218@naver.com>
2024-10-23 23:28:25 +0900

    feat(InputValidator): 자동차 이름 입력 검증 메서드 구현

--- src/InputValidator.js
+++ src/InputValidator.js
@@ -1,4 +1,24 @@
 class InputValidator {
+    static ERROR_MESSAGES = Object.freeze({
+        EMPTY: "자동차 이름이 입력되지 않았습니다.",
+        INVALID: "자동차 이름은 1~5글자여야 하며, 공백이나 특수 문자는 포함할 수 없습니다.",
+        DUPLICATE: "중복되는 자동차 이름이 있습니다.",
+    });
+
+    static validateCarNameList(carNameList) {
+        const regex = /^[a-zA-Z가-힣0-9]{1,5}$/;
+
+        if (carNameList.length === 0) {
+            InputValidator.#throwError(InputValidator.ERROR_MESSAGES.EMPTY);
+        } else if (!carNameList.every((carName) => regex.test(carName))) {
+            InputValidator.#throwError(InputValidator.ERROR_MESSAGES.INVALID);
+        }
+        // 중복된 자동차 이름을 체크
+        else if (carNameList.length !== new Set(carNameList).size) {
+            InputValidator.#throwError(InputValidator.ERROR_MESSAGES.DUPLICATE);
+        }
+    }
+
     static #throwError(message) {
         throw new Error(`[ERROR] ${message}`);
     }
