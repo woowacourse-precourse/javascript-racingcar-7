9462558de1018dd4ecc554e7b7d3d6c0f83156a7
ockko <ocko119@gmail.com>
2024-10-23 23:14:42 +0900

    feat: 사용자 입력 받기
    * 경주할 자동차 이름
    * 시도할 횟수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const carname = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+    const tryCount = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n")
+  }  
 }
 
 export default App;
