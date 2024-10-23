8ed0e8caee1a7b3378fe1b3afa0547c833242539
oko_jin <dswvgw6412@naver.com>
2024-10-23 19:01:46 +0900

    feat(input): 자동차 입력, 횟수 입력 구현

--- src/utils/input.js
+++ src/utils/input.js
@@ -1,20 +1,20 @@
 import { Console } from '@woowacourse/mission-utils';
 import Car from '../Car.js';
 import { errorMessages } from '../constant.js';
+import Validator from '../Validator.js';
+
+const validator = new Validator();
 
 export async function carInput() {
   const input = await Console.readLineAsync(
     '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
   );
-  return input.split(',').map((name) => new Car(name));
+  const result = validator.validCarInput(input);
+  return result;
 }
 
 export async function countInput() {
   const input = await Console.readLineAsync('시도할 횟수는 몇번인가요?\n');
-
-  if (!input) {
-    throw new Error(`${errorMessages.prefix} ${errorMessages.invalidCount}`);
-  }
-
-  return parseInt(input);
+  const result = parseInt(validator.validCountInput(input));
+  return result;
 }
