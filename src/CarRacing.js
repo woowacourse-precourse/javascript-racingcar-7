0acdb124c180ea405af03c54cb1971a919660112
yeji0214 <1225yj50@naver.com>
2024-10-23 16:46:58 +0900

    feat: Limit the car's name to five characters or less

--- src/CarRacing.js
+++ src/CarRacing.js
@@ -7,6 +7,17 @@ export const startRace = async () => {
 
 const inputCarNames = async () => {
     const carNames = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const trimmedCarNames = carNames.split(',').map(car => car.trim());
 
-    return carNames.split(',')
+    validateCarNames(trimmedCarNames);
+
+    return trimmedCarNames;
+}
+
+const validateCarNames = (carNames) => {
+    carNames.forEach((car) => {
+        if (car.length > 5) {
+            throw new Error('[ERROR] 모든 자동차 이름은 5자 이하여야 합니다.');
+        }
+    });
 }
