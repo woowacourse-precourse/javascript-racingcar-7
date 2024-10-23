bd64e9d7f80151993d0cc21ea696d8673ae2f248
eungyo <eungyo0965@gmail.com>
2024-10-23 16:46:21 +0200

    feat(InputUI) : Used a Promise object to handle asynchronous input from the console effectively. - This ensures the program waits for user input before proceeding. - It prevents blocking other code execution while waiting for input.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    return new Promise((resolve, reject) => {
+      MissionUtils.Console.readLineAsync(
+        '자동차 이름을 입력하세요: ',
+        (input) => {
+          const names = input.split(',');
+        },
+      );
+    });
+  }
 }
 
 export default App;
