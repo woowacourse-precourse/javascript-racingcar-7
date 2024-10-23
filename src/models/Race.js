99164e166cd5f88314d8c1bc083ca4f52ce1cef6
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-10-24 00:15:56 +0900

    refactor: 라운드 출력 별로도 분리
    - 한 함수가 한 가지 일만 하게끔 분리

--- src/models/Race.js
+++ src/models/Race.js
@@ -11,14 +11,17 @@ class Race {
 
   start() {
     Console.print('\n실행 결과');
+    this.#runRounds();
+  }
 
+  #runRounds() {
     for (let round = 0; round < this.#roundCount; round++) {
       this.#cars.forEach((car) => {
         car.tryMove();
         car.printStatus();
       });
 
-      // 스텝 간 간격을 위한 빈 줄 출력
+      // 라운드 간 간격을 위한 빈 줄 출력
       Console.print('');
     }
   }
