2fc90189f33d1adb68ecf775935e8d560ba8b153
youdame <112458620+youdame@users.noreply.github.com>
2024-10-24 02:03:11 +0900

    feat : 자동차 이름과 시도 횟수에 대한 인풋 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from '@woowacourse/mission-utils';
 class App {
-  async run() {}
+  async run() {
+    const carNamesInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const carNames = carNamesInput.split(',');
+    const attemptCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요\n');
+
+    // 5자가 넘는 이름이 나올 경우 에러
+    console.log(carNames, attemptCount);
+  }
 }
 
 export default App;
