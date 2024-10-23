94beba567dbc49a5eb1f66ebe37306e4d0377a51
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-10-23 12:03:33 +0900

    feat: 시도할 횟수 입력 구현
    - readLineAsync를 이용해 입력 구현
    - await 붙여 수정
    - 테스트 코드 작성
    - 테스트 코드 mockResolvedValue -> mockResolvedValueOnce로 변경

--- __tests__/AppTest.js
+++ __tests__/AppTest.js
@@ -9,13 +9,24 @@ jest.mock('@woowacourse/mission-utils', () => ({
 
 describe('App 클래스 테스트', () => {
   test('자동차 이름 문자열 입력 받기', async () => {
-    Console.readLineAsync.mockResolvedValue('pobi,woni,jun');
+    Console.readLineAsync.mockResolvedValueOnce('pobi,woni,jun');
 
     const app = new App();
     await app.run();
 
     expect(Console.readLineAsync).toHaveBeenCalledWith(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+    );
+  });
+
+  test('시도할 횟수 입력 받기', async () => {
+    Console.readLineAsync.mockResolvedValueOnce('5');
+
+    const app = new App();
+    await app.run();
+
+    expect(Console.readLineAsync).toHaveBeenCalledWith(
+      '시도할 횟수는 몇 회인가요?\n'
     );
   });
 });
--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     try {
-      const inputString = Console.readLineAsync(
+      const inputString = await Console.readLineAsync(
         '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+      );
+      const repeatCount = await Console.readLineAsync(
+        '시도할 횟수는 몇 회인가요?\n'
       );
     } catch (error) {
       throw new Error(`[ERROR] ${error.message}`);
