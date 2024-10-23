f3be7db23ab170e83794047b2097cdbdab3b801f
dmson1218 <dmson1218@naver.com>
2024-10-23 23:58:46 +0900

    feat(App): 전진 시도 횟수 입력 메서드 구현

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ class App {
     async run() {
         const inputCarNames = await this.getInputCarNames();
         const carNameList = inputCarNames.split(",");
+
+        const inputTryCount = await this.getInputTryCount();
     }
 
     async getInputCarNames() {
@@ -11,6 +13,14 @@ class App {
             const message = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
             const inputCarNames = await Console.readLineAsync(message);
             return inputCarNames;
+        } catch (error) {}
+    }
+
+    async getInputTryCount() {
+        try {
+            const message = "시도할 횟수는 몇회인가요?\n";
+            const inputTryCount = await Console.readIntAsync(message);
+            return inputTryCount;
         } catch (error) {}
     }
 }
