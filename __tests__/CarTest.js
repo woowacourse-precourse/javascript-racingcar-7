296a8e9d33aace4dd95d68124eaf5d1580496273
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-10-23 16:29:58 +0900

    test: Car 클래스의 getName 메서드 테스트
    - 정상적으로 이름을 반환하는지 테스트

---
+++ __tests__/CarTest.js
@@ -0,0 +1,10 @@
+import Car from '../src/Car.js';
+
+describe('Car 클래스 테스트', () => {
+  test('getName 메서드가 자동차의 이름을 반환하는지 확인', () => {
+    const car = new Car('pobi');
+    const carName = car.getName();
+
+    expect(carName).toBe('pobi');
+  });
+});
