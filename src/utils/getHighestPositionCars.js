69fa4a506cdea5b64b3f89cf565669ef4fe808bc
oko_jin <dswvgw6412@naver.com>
2024-10-23 22:48:30 +0900

    feat(getWinner): position이 가장 높은 Car 객체를 추출하는 함수 구현
    기능 요구사항인 우승자를 추출하기 위해

---
+++ src/utils/getHighestPositionCars.js
@@ -0,0 +1,4 @@
+export function getHighestPositionCars(cars) {
+  const highestPosition = Math.max(...cars.map((car) => car.position));
+  return cars.filter((car) => car.position === highestPosition).map((car) => car.name);
+}
