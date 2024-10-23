b85d2b7b67d1a403fe458f31a7b18688f6d825bf
thumbthing <ilscc0902@gmail.com>
2024-10-24 02:44:02 +0900

    Feat: 자동차 이름 입력 기능 구현

--- src/UI/carNameInput.js
+++ src/UI/carNameInput.js
@@ -1,3 +1,5 @@
+import { Console } from "@woowacourse/mission-utils"
+
 async function carNameInput() {
   /**
    * TODO: 
@@ -8,4 +10,20 @@ async function carNameInput() {
    *  - 유효성을 통과하지 못했을 경우 에러를 반환한다
    * 4. 유효성을 통과한 값의 배열을 반환한다
    */
+  try {
+    const USER_INPUT = Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+    const CAR_NAME = USER_INPUT.split(',');
+  
+    CAR_NAME.forEach((name) => {
+      if(name.length > 6) {
+        const OVER_FIVE_CHARACTOR = '자동차 이름이 5글자를 초과 합니다.';
+        const ERROR = new Error(OVER_FIVE_CHARACTOR);
+        throw ERROR;
+      }
+    });
+  
+    return CAR_NAME;
+  } catch (error) {
+    throw error;
+  }
 }
