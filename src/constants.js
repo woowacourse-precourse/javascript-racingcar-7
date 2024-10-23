8bae94d6c6b13a1695507eb526201dbe86d563c9
guesung <gueit214@naver.com>
2024-10-23 12:11:29 +0900

    feat: 에러 메시지 상수화

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const ERROR_MESSAGE_DEFAULT = "[ERROR]";
+export const ERROR_MESSAGE_CAR_NAME_OVER_FIVE = `${ERROR_MESSAGE_DEFAULT} 길이가 5 미만의 자동차 이름을 입력해주세요.`;
+export const ERROR_MESSAGE_NOT_INTEGER = `${ERROR_MESSAGE_DEFAULT} 정수를 입력해주세요.`;
+export const ERROR_MESSAGE_NOT_POSITIVE_POSITIVE = `${ERROR_MESSAGE_DEFAULT} 양수를 입력해주세요.`;
+export const ERROR_MESSAGE_NOT_NUMBER = `${ERROR_MESSAGE_DEFAULT} 숫자를 입력해주세요.`;
