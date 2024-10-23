3f3ac8889305a8f23d8027eb5376d19b47d6c808
dyoon <dyoon@dyoonui-MacBookAir.local>
2024-10-23 13:34:00 +0900

    자동차 이름 입력 및 중복 처리 기능 구현하라
    - 사용자로부터 받은 값을 ,로 구분했고 공백은 받지 않을 거라 trim으로 제거함
    - 자동차 이름 유효성 검증이 끝나면 향후 자동차의 이동거리'-'를 저장하기 위해 객체 타입으로 저장함
    - 코드 흐름 헤치지 않으려고 중복된 자동차 이름 입력 시 오류 메시지 출력하게 함 (추후 함수로 업데이트 예정)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import { Random } from '@woowacourse/mission-utils';
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    function splitUserInput(userInput) {
+      return userInput.split(',').map(car => car.trim());
+    }
+
+    function addCar(car, raceCars) {
+      if (car.length > 5) {
+        console.log('error: 자동차 이름은 최대 5글자여야 합니다.');
+        return false;
+      }
+
+      if (car in raceCars) {
+        console.log(`중복된 참가자가 있습니다: "${car}"`);
+        return false;
+      }
+
+      raceCars[car] = '';
+      return true;
+    }
+
+    async function getRaceCar() {
+      const userInput = await Console.readLineAsync(
+        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분, 최대 5글자, 공백 제외)\n'
+      );
+
+      const cars = splitUserInput(userInput);
+      let raceCars = {};
+
+      cars.forEach(car => addCar(car, raceCars));
+    }
+
+    getRaceCar();
+  }
 }
 
 export default App;
