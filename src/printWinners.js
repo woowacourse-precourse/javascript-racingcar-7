304308a309f6392157f3e1230b7092bfbc4d67e3
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-10-23 19:37:41 +0900

    refactor: 매개 변수 이름, 변수 이름 변경
    - carArray -> cars
    - winners -> winnerCars
    - winnerString -> winnerNames

--- src/printWinners.js
+++ src/printWinners.js
@@ -1,14 +1,16 @@
 import { Console } from '@woowacourse/mission-utils';
 
-const printWinners = function printWinners(carArray) {
-  const maxDistance = carArray.reduce((maxDistance, car) => {
+const printWinners = function printWinners(cars) {
+  const maxDistance = cars.reduce((maxDistance, car) => {
     return Math.max(maxDistance, car.getDistance());
   }, 0);
 
-  const winners = carArray.filter((car) => car.getDistance() === maxDistance);
+  const winnerCars = carArray.filter(
+    (car) => car.getDistance() === maxDistance
+  );
 
-  const winnerString = winners.map((winner) => winner.getName()).join(',');
-  Console.print(`최종 우승자 : ${winnerString}`);
+  const winnerNames = winners.map((winner) => winner.getName()).join(',');
+  Console.print(`최종 우승자 : ${winnerNames}`);
 };
 
 export default printWinners;
