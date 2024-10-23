aceec12d5c44b5b458cd954db4f78c643b3bc6dc
jeongeunLee <leeju1009@gmail.com>
2024-10-23 22:02:28 +0900

    feat(racing): 자동차 경주를 실행하는 함수 추가

--- src/Racing.js
+++ src/Racing.js
@@ -7,6 +7,12 @@ class Racing {
     this.#tryCount = tryCount;
     this.#cars = cars;
   }
+
+  play() {
+    const cars = [...this.#cars];
+    const distances = cars.map((car) => car.driving());
+    return distances;
+  }
 }
 
 export default Racing;
