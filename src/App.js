58449bdb9815165bc1d3ad6e7af062eda9d342e9
yusuShin <tlsdbtn0507@naver.com>
2024-10-24 00:48:14 +0900

    feat(App) Add checkCarNameInput func - Check input that user typed includes ',' - Check user typed more than 2 cars

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import PrintFunc from "./print/printFunc.js";
+import { Console } from "@woowacourse/mission-utils";
 
 class App {
-  async run() {
+  async run() { }
+
+  async checkCarNameInput() {
+    const carsInput = await Console.readLineAsync(
+      `경주할 자동차 이름을 입력하세요.
+      (이름은 쉼표(,) 기준으로 구분 / 자동차는 최소 두대 이상이어야 합니다) : `
+    );
+
+    if (!carsInput.includes(",")) {
+      return PrintFunc.printError("쉼표(,)로 참가자들을 구분해주세요");
+    }
+
+    const carsInputArray = carsInput.split(",");
+
+    if (carsInputArray.length < 2) {
+      return PrintFunc.printError("최소 두대 이상의 자동차를 참가 시켜주세요");
+    }
+
+    return carsInputArray;
   }
 }
 
