79a8659488fc955b13af0a9ef005c686a6238830
yheseul <kangyheseul@naver.com>
2024-10-23 18:01:05 +0900

    chore: validation 폴더의 함수들을 묶는 파일 이름을 index로 변경

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import { Console } from "@woowacourse/mission-utils";
 import { RUN_MESSAGE } from "./constants/constants.js";
-import { validateCarName } from "./validation/validateCarName/validateCarName.js";
+import { validateCarName } from "./validation/validateCarName/index.js";
 import { validateAttemptsNumber } from "./validation/validateAttemptsNumber/validateAttemptsNumber.js";
 
 class App {
   async run() {
-    const inputCarName = await Console.readLineAsync(
-      RUN_MESSAGE.INPUT_CAR_NAMES
-    );
+    const inputCarName = await Console.readLineAsync(RUN_MESSAGE.INPUT_CAR_NAMES);
     validateCarName(inputCarName);
-    const inputAttemptsNumber = await Console.readLineAsync(
-      RUN_MESSAGE.INPUT_ATTEMPTS_NUMBER
-    );
+    const inputAttemptsNumber = await Console.readLineAsync(RUN_MESSAGE.INPUT_ATTEMPTS_NUMBER);
     validateAttemptsNumber(inputAttemptsNumber);
+    startCarRace(inputAttemptsNumber);
   }
 }
 
