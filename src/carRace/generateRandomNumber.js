62299ad806c03b7d8b055a127e93da235428e0ae
yheseul <kangyheseul@naver.com>
2024-10-24 08:46:49 +0900

    feat: 0에서 9까지 무작위 정수 생성 함수 구현

---
+++ src/carRace/generateRandomNumber.js
@@ -0,0 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import {
+  MAX_RANDOM_NUMBER,
+  MIN_RANDOM_NUMBER,
+} from "../constants/constants.js";
+
+export function generateRandomNumber() {
+  const randomNumber = MissionUtils.Random.pickNumberInRange(
+    MIN_RANDOM_NUMBER,
+    MAX_RANDOM_NUMBER
+  );
+  return randomNumber;
+}
