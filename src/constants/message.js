5a0048b47b86aeefb691784c8b2cee000cdc9818
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-23 14:28:51 +0900

    feat: 에러메시지 상수화

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,12 +1,28 @@
 const INPUT_MESSAGE = Object.freeze({
-  CarNameMessage:
-    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n",
-  attemptNumberMessage: "시도할 횟수는 몇 회인가요?\n",
+  carName: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n",
+  attemptNumber: "시도할 횟수는 몇 회인가요?\n",
 });
 
 const OUTPUT_MESSAGE = Object.freeze({
-  winnerMessage: "최종 우승자 : ",
-  moveForward: "-",
+  winner: "최종 우승자 : ",
+  racingResult: "실행 결과",
 });
 
-export { INPUT_MESSAGE, OUTPUT_MESSAGE };
+const PREFIX_ERROR = "[ERROR]";
+
+const ERROR_MESSAGE = Object.freeze({
+  blank: `${PREFIX_ERROR} 값을 입력해주십시오.`,
+
+  carName: {
+    invalidLength: `${PREFIX_ERROR} 자동차 이름은 5글자 이내입니다.`,
+    invalidOverlap: `${PREFIX_ERROR} 자동차 이름이 중복됩니다.`,
+    invalidDelimiter: `${PREFIX_ERROR} ,로만 구분해야 합니다.`,
+  },
+
+  attemptNumber: {
+    invalidType: `${PREFIX_ERROR} 숫자를 입력해주십시오.`,
+    invalidInteger: `${PREFIX_ERROR} 정수를 입력해주십시오.`,
+  },
+});
+
+export { INPUT_MESSAGE, OUTPUT_MESSAGE, ERROR_MESSAGE };
