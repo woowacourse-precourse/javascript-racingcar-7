21a2d866d318ee2b79c6047135b57b15fc8350d9
dmson1218 <dmson1218@naver.com>
2024-10-23 23:23:02 +0900

    style(App): 입력 메시지 개행 문자 추가

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App {
 
     async getInputCarNames() {
         try {
-            const message = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+            const message = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
             const inputCarNames = await Console.readLineAsync(message);
             return inputCarNames;
         } catch (error) {}
