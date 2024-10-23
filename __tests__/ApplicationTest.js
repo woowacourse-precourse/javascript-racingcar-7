3418f18e18de40ceabec9d6758f41a9b35127b45
위영진 <qser155@naver.com>
2024-10-23 16:30:24 +0900

    test: add a application test case

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,23 +1,58 @@
 import App from '../src/App.js';
 import { getLogSpy, mockQuestions, mockRandoms } from '../utils/testUtils.js';
 
+const MOVING_FORWARD = 4;
+const STOP = 3;
+
 describe('자동차 경주', () => {
-    test('기능 테스트', async () => {
-        // given
-        const MOVING_FORWARD = 4;
-        const STOP = 3;
-        const inputs = ['pobi,woni', '1'];
-        const logs = ['pobi : -', 'woni : ', '최종 우승자 : pobi'];
+    test.each([
+        [
+            '단독 우승 테스트',
+            ['pobi,woni', '1'],
+            ['pobi : -', 'woni : ', '최종 우승자 : pobi'],
+            [MOVING_FORWARD, STOP],
+        ],
+        [
+            '공동 우승 테스트',
+            ['pobi,woni', '2'],
+            [
+                'pobi : -',
+                'woni : -',
+                'pobi : --',
+                'woni : --',
+                '최종 우승자 : pobi,woni',
+            ],
+            [MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD],
+        ],
+        [
+            '3대 이상 자동차 테스트',
+            ['pobi,woni,jun', '2'],
+            [
+                'pobi : -',
+                'woni : -',
+                'jun : -',
+                'pobi : --',
+                'woni : --',
+                'jun : -',
+                '최종 우승자 : pobi,woni',
+            ],
+            [
+                MOVING_FORWARD,
+                MOVING_FORWARD,
+                MOVING_FORWARD,
+                MOVING_FORWARD,
+                MOVING_FORWARD,
+                STOP,
+            ],
+        ],
+    ])('%s', async (testName, inputs, logs, random) => {
         const logSpy = getLogSpy();
-
         mockQuestions(inputs);
-        mockRandoms([MOVING_FORWARD, STOP]);
+        mockRandoms(random);
 
-        // when
         const app = new App();
         await app.run();
 
-        // then
         logs.forEach((log) => {
             expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
         });
