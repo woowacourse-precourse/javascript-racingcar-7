c4a5a79e07d3c2a7b094913fbbc06cefb1890433
osw6858 <osw7875@naver.com>
2024-10-23 12:38:34 +0900

    feat: 랜덤값 추출과 랜덤값이 4 이상인지 판단하는 함수 추가

---
+++ src/random.js
@@ -0,0 +1,9 @@
+import {Random} from '@woowacourse/mission-utils'
+
+export function getRandomNumber() {
+    return Random.pickNumberInRange(0,9)
+}
+
+export function isMoveForward() {
+    return getRandomNumber() >= 4;
+}
