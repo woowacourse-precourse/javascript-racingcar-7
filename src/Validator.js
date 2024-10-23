7ad5332007ca3fb67bd30e2801401eab698dd5cd
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-10-23 14:12:47 +0900

    feat: 이름이 하나만 입력됐을 때 검증 구현
    - ,이 포함되어 있나 확인해 이름이 하나인지 검증

--- src/Validator.js
+++ src/Validator.js
@@ -3,6 +3,10 @@ class Validator {
     if (!nameString.trim()) {
       throw new Error('공백이 입력됐습니다');
     }
+
+    if (!nameString.includes(',')) {
+      throw new Error('플레이어는 1명일 수 없습니다');
+    }
   }
 }
 
