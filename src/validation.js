e9ff392c348ffa48c1acfd71824c9c1218b2560c
DongjaJ <jdh981118@naver.com>
2024-10-23 18:32:56 +0900

    feat: 중복된 자동차 이름이 있는지 검사

--- src/validation.js
+++ src/validation.js
@@ -3,6 +3,7 @@ class InputValidation {
 
   static validateCarNameList(input) {
     this.#validateMaxLength(input);
+    this.#validateDuplicatedCarName(input);
   }
 
   static #validateMaxLength(input) {
@@ -11,6 +12,12 @@ class InputValidation {
         throw new Error("[ERROR]: 자동차의 이름은 5이하여야 해요.");
       }
     });
+  }
+
+  static #validateDuplicatedCarName(input) {
+    if (new Set(input).length !== input.length) {
+      throw new Error("[ERROR]: 중복된 자동차의 이름은 사용할 수 없어요.");
+    }
   }
 }
 
