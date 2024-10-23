65205b98f7a9f5ffd5c07ebd94e3ea8e832167dc
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-10-23 19:43:02 +0900

    refactor: 우승자 찾기 별도 함수로 분리
    - printWinners는 출력에만 집중할 수 있도록 우승자 찾기는 findWinners로 분리

--- src/printWinners.js
+++ src/printWinners.js
@@ -1,15 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
 
-const printWinners = function printWinners(cars) {
+const findWinners = function findWinners(cars) {
   const maxDistance = cars.reduce((maxDistance, car) => {
     return Math.max(maxDistance, car.getDistance());
   }, 0);
 
-  const winnerCars = carArray.filter(
-    (car) => car.getDistance() === maxDistance
-  );
+  return cars.filter((car) => car.getDistance() === maxDistance);
+};
 
-  const winnerNames = winners.map((winner) => winner.getName()).join(',');
+const printWinners = function printWinners(cars) {
+  const winnerCars = findWinners(cars);
+
+  const winnerNames = winnerCars.map((winner) => winner.getName()).join(',');
   Console.print(`최종 우승자 : ${winnerNames}`);
 };
 
