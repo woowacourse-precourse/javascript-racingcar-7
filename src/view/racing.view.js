a0840b3c83670824d77bb1dc6470e8a871b05c38
L2HYUNN <donghyuni98@gmail.com>
2024-10-23 18:12:44 +0900

    feat(view): 사용자로부터 시도할 횟수를 입력 받는 기능 구현

--- src/view/racing.view.js
+++ src/view/racing.view.js
@@ -4,6 +4,7 @@ import { input } from '../lib/view.js';
 export class RacingView {
   static QUERY = Object.freeze({
     GET_CAR_NAMES: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+    GET_TRIAL_NUMBER: '시도할 횟수는 몇 회인가요?',
   });
 
   /**
@@ -12,6 +13,16 @@ export class RacingView {
    */
   static async getCarNames() {
     const result = await input(RacingView.QUERY.GET_CAR_NAMES);
+
+    return result;
+  }
+
+  /**
+   *
+   * @returns {Promise<string>}
+   */
+  static async getTrialNumber() {
+    const result = await input(RacingView.QUERY.GET_TRIAL_NUMBER);
 
     return result;
   }
