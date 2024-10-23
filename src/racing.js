f79c849f0e7d776f6dae947d0f937aaf407fce4d
osw6858 <osw7875@naver.com>
2024-10-23 13:08:43 +0900

    feat: 경기 결과를 객체로 반환하는 함수 구현

--- src/racing.js
+++ src/racing.js
@@ -1,4 +1,5 @@
 import {isMoveForward} from "./random.js";
+import {input} from "./input.js";
 
 function parseStringToObject(validCars) {
     const carsArray = validCars.split(',')
@@ -6,12 +7,23 @@ function parseStringToObject(validCars) {
 }
 
 
-export function moveCars(validCars) {
-    const carsObject = parseStringToObject(validCars)
+function moveCars(carsObject) {
 
     carsObject.forEach((car) => {
         if(isMoveForward()) {
             car.distance += 1
         }
     })
+}
+
+export async function racingCars() {
+    const {cars, count} = await input();
+
+    const carsObject = parseStringToObject(cars)
+
+    for(let i= 0; i < count; i++){
+        moveCars(carsObject)
+    }
+
+    return carsObject;
 }
