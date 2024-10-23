fab1ab8983ff71b48fba6020fcbf2d69c0c4c6a5
Teayeon-Lim <wingofyeon@naver.com>
2024-10-23 16:53:33 +0900

    feat(car racing): Add car racing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  goCar() {
+    return Random.pickNumberInRange(0, 9) >= 4;
+  }
+
+  async run() {
+    const inputCarNames = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    const names = inputCarNames.split(",");
+    const movements = names.reduce((obj, name) => {
+      obj[name] = "";
+
+      return obj;
+    }, {});
+
+    const count = await Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    ).then(Number);
+
+    for (let i = 0; i < count; i++) {
+      for (let j = 0; j < names.length; j++) {
+        if (this.goCar()) {
+          movements[names[j]] += "-";
+        }
+      }
+    }
+
+    Console.print(`자동차 경주 최종 결과 : ${JSON.stringify(movements)}`);
+  }
 }
 
 export default App;
