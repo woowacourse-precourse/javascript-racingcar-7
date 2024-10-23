92a5317d7ac6c5289436784c62eb5cd4231ef680
Teayeon-Lim <wingofyeon@naver.com>
2024-10-23 17:30:53 +0900

    feat(car racing): Display the forward distance of a car in each race
    경주를 진행 할 때마다 모든 자동차의 진행 상황을 표시한다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
-  goCar() {
+  go() {
     return Random.pickNumberInRange(0, 9) >= 4;
+  }
+
+  moveCar(movements, currentRace) {
+    if (this.go()) {
+      movements[currentRace][1] += "-";
+    }
   }
 
   async run() {
@@ -11,21 +17,27 @@ class App {
     );
     const names = inputCarNames.split(",");
     const movements = names.reduce((obj, name) => {
-      obj[name] = "";
+      obj.push([name, ""]);
 
       return obj;
-    }, {});
+    }, []);
 
     const count = await Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?\n"
     ).then(Number);
 
+    Console.print("\n실행 결과");
+
     for (let i = 0; i < count; i++) {
       for (let j = 0; j < names.length; j++) {
-        if (this.goCar()) {
-          movements[names[j]] += "-";
-        }
+        this.moveCar(movements, j);
       }
+
+      movements.forEach(car => {
+        Console.print(car.join(" : "));
+      });
+
+      Console.print("");
     }
 
     Console.print(`자동차 경주 최종 결과 : ${JSON.stringify(movements)}`);
