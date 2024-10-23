0a8f18c23ffd96c5e9c70276ddc221691f2e89dc
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-10-23 21:33:16 +0900

    refactor: 메서드, 매개 변수 이름 변경
    - nameString() -> checkNameString()
    - repeatCount() -> checkRoundCount()
    - nameArray() -> checkNames()
    - repeatCount -> roundCount
    - nameArray -> names

--- src/App.js
+++ src/App.js
@@ -10,17 +10,17 @@ class App {
       const inputString = await Console.readLineAsync(
         '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
       );
-      Validator.nameString(inputString);
+      Validator.checkNameString(inputString);
 
       const nameArray = inputString.split(',');
-      Validator.names(nameArray);
+      Validator.checkNames(nameArray);
 
       const carArray = nameArray.map((name) => new Car(name));
 
       const repeatCount = Number(
         await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n')
       );
-      Validator.repeatCount(repeatCount);
+      Validator.checkRoundCount(repeatCount);
 
       runRaceRounds(carArray, repeatCount);
 
--- src/Validator.js
+++ src/Validator.js
@@ -1,31 +1,31 @@
 class Validator {
-  static nameString(nameString) {
+  static checkNameString(nameString) {
     if (!nameString.trim()) {
       throw new Error('공백이 입력됐습니다');
     }
   }
 
-  static repeatCount(repeatCount) {
-    if (Number.isNaN(repeatCount)) {
+  static checkRoundCount(roundCount) {
+    if (Number.isNaN(roundCount)) {
       throw new Error('숫자를 입력해야합니다');
     }
 
-    if (repeatCount === 0) {
+    if (roundCount === 0) {
       throw new Error('빈 문자열 또는 0은 입력할 수 없습니다');
     }
 
-    if (repeatCount < 0) {
+    if (roundCount < 0) {
       throw new Error('음수는 입력할 수 없습니다');
     }
 
-    if (!Number.isSafeInteger(repeatCount)) {
+    if (!Number.isSafeInteger(roundCount)) {
       throw new Error('실수, 정상 범위를 벗어난 정수는 입력할 수 없습니다');
     }
   }
 
-  static nameArray(nameArray) {
-    nameArray.forEach((name) => {
-      if (name.length > 5) {
+  static checkNames(names) {
+    names.forEach((name) => {
+      if (names.length > 5) {
         throw new Error('이름은 5글자를 넘을 수 없습니다');
       }
 
@@ -34,11 +34,11 @@ class Validator {
       }
     });
 
-    if (nameArray.length === 1) {
+    if (names.length === 1) {
       throw new Error('플레이어는 1명일 수 없습니다');
     }
 
-    if (nameArray.every((name) => name === nameArray[0])) {
+    if (names.every((name) => name === names[0])) {
       throw new Error('모든 플레이어들의 이름이 같습니다');
     }
   }
