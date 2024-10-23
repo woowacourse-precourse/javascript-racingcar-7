952a3621eb5e2821b5d1bde8093c01044bb651ca
kim jiyeong <youngdev96@naver.com>
2024-10-24 01:29:39 +0900

    refactor: 자동차 전진하는 조건 수정

--- src/MoveCar.js
+++ src/MoveCar.js
@@ -16,11 +16,9 @@ class MoveCar {
     carObj.forEach((car) => {
       let number = MissionUtils.Random.pickNumberInRange(0, 9);
 
-      if (number > 4) {
+      if (number >= 4) {
         car.distance += '-';
       }
-
-      
     });
   }
 
