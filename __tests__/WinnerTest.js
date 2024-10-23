6f34f00fc58e08ec960f985b37472f46b8ed3fef
ChaDongmin <cdm1263@gmail.com>
2024-10-23 20:42:25 +0900

    feat(WinnerTest): 우승자 출력 테스트코드 추가
    - 단독 우승자 텍스트 출력 테스트
    
    - 공동 우승자 텍스트 출력 테스트

---
+++ __tests__/WinnerTest.js
@@ -0,0 +1,29 @@
+import { mockQuestions, mockRandoms } from "./testUtils.js";
+
+describe("우승자 안내 문구", () => {
+  test("단독 우승자", async () => {
+    mockQuestions(["car1,car2", "3"]);
+    mockRandoms([4, 3, 4, 3, 4, 3]);
+    const logSpy = getLogSpy();
+
+    const app = new App();
+    await app.run();
+
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining("최종 우승자 : car1")
+    );
+  });
+
+  test("공동 우승자", async () => {
+    mockQuestions(["car1,car2,car3", "3"]);
+    mockRandoms([4, 4, 3, 4, 4, 3, 4, 4, 3]);
+    const logSpy = getLogSpy();
+
+    const app = new App();
+    await app.run();
+
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining("최종 우승자 : car1, car2")
+    );
+  });
+});
