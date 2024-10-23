fb2ae3bd037195ca5bdb02546c55eb67671456e4
oko_jin <dswvgw6412@naver.com>
2024-10-23 15:56:45 +0900

    feat(App): 시도할 횟수 입력 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
 import Car from './Car.js';
+import { carInput, countInput } from './utils/input.js';
 
 class App {
   async run() {
     try {
-      const carInput = await Console.readLineAsync(
-        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
-      );
-      const cars = carInput.split(',').map((name) => new Car(name));
-    } catch (e) {}
+      const cars = await carInput();
+      const count = await countInput();
+
+      Console.print(cars, count);
+    } catch (e) {
+      throw new Error(e);
+    }
   }
 }
 
