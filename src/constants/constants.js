4bc1d4f21b427b25e9807edee0a745e414fdf064
yheseul <kangyheseul@naver.com>
2024-10-24 08:42:17 +0900

    chore: 필요한 값 및 메시지 상수화 추가

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,21 +1,25 @@
+export const MIN_RANDOM_NUMBER = 0;
+export const MAX_RANDOM_NUMBER = 9;
+
 export const RUN_MESSAGE = {
-  INPUT_CAR_NAMES:
-    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
   INPUT_ATTEMPTS_NUMBER: "시도할 횟수는 몇 회인가요?\n",
-  FINAL_WINNER: "최종 우승자 : ",
-  RUN_RESULT: "실행 결과",
+  INPUT_CAR_NAMES: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+};
+
+export const CAR_RACE = {
   COLON: " : ",
   CAR_MOVE: "-",
+  MOVE_THRESHOLD: 4,
+  RUN_RESULT: "\n실행 결과",
+  FINAL_WINNER: "최종 우승자 : ",
+  NEW_LINE: "\n",
 };
 
 export const ERROR_MESSAGES = {
-  CAR_NAME_TOO_LONG: "[ERROR] 경주할 자동차 이름은 5자 이하여야 합니다.",
-  INVALID_NAME_SEPARATOR:
-    "[ERROR] 자동차 이름은 쉼표(,)를 기준으로 구분해야합니다.",
-  NAME_MUST_BE_KOREAN_OR_ENGLISH:
-    "[ERROR] 자동차 이름은 한글 또는 영어로 작성해야합니다.",
-  MINIMUM_TWO_CARS_REQUIRED:
-    "[ERROR] 경주할 자동차는 2개 이상이여야 진행할 수 있습니다.",
-  ATTEMPTS_MUST_BE_NUMERIC: "[ERROR] 시도 횟수는 숫자로 작성해야합니다.",
   DUPLICATE_CAR_NAMES: "[ERROR] 자동차 이름은 중복될 수 없습니다.",
+  CAR_NAME_TOO_LONG: "[ERROR] 경주할 자동차 이름은 5자 이하여야 합니다.",
+  ATTEMPTS_MUST_BE_NUMERIC: "[ERROR] 시도 횟수는 숫자로 작성해야합니다.",
+  INVALID_NAME_SEPARATOR: "[ERROR] 자동차 이름은 쉼표(,)를 기준으로 구분해야합니다.",
+  MINIMUM_TWO_CARS_REQUIRED: "[ERROR] 경주할 자동차는 2개 이상이여야 진행할 수 있습니다.",
+  NAME_MUST_BE_KOREAN_OR_ENGLISH: "[ERROR] 자동차 이름은 한글 또는 영어로 작성해야합니다.",
 };
