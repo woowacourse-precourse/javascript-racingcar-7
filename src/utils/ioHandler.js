dc58e0a468df960076fb33d6207ed2f913d45236
jongbin26 <010jjj@naver.com>
2024-10-23 17:16:53 +0900

    refactor(ioHandlers): 기능요구사항에 맞게 변수명 수정
    - players 변수명을 cars로 변경했습니다.
    - printResult 함수를 더 명확하게 printGameResult로 변경했습니다.

--- src/utils/ioHandler.js
+++ src/utils/ioHandler.js
@@ -2,13 +2,13 @@ import { Console } from '@woowacourse/mission-utils';
 import { IO_MESSAGE } from '../constants';
 
 async function getGameInput() {
-  const players = await Console.readLineAsync(IO_MESSAGE.FIRST_INPUT_MESSAGE);
+  const cars = await Console.readLineAsync(IO_MESSAGE.FIRST_INPUT_MESSAGE);
   const count = await Console.readLineAsync(IO_MESSAGE.SECOND_INPUT_MESSAGE);
-  return { players, count };
+  return { cars, count };
 }
 
-function printResult(result) {
+function printGameResult(result) {
   Console.print(IO_MESSAGE.OUTPUT_MESSAGE + result.join(IO_MESSAGE.SEPERATOR));
 }
 
-export { getGameInput, printResult };
+export { getGameInput, printGameResult };
