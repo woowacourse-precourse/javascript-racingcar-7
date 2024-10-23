037c9c0912b6a595d3d2d49d229bbdf82719be71
juhui88 <sand8594@naver.com>
2024-10-23 14:17:21 +0900

    feat: Car class 정의
    - generateRandomNumber() 난수 생성 함수 구현
    - canMoveForward(randomValue) 난수 판단 함수 구현
    - move() 판단 후에 distance에 1을 더하는 함수 구현
    - getDistance() 현재 자동차의 거리를 반환하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+      const tries = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+
+
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+      throw error
+    }
+  }
 }
 
 export default App;
