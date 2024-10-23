4e7dcf5242cc636ec47a41a4506778606b808f09
guesung <gueit214@naver.com>
2024-10-23 12:16:46 +0900

    refactor: App을 상수로 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import {
+  ERROR_MESSAGE_CAR_NAME_OVER_FIVE,
+  ERROR_MESSAGE_NOT_INTEGER,
+  ERROR_MESSAGE_NOT_NUMBER,
+  ERROR_MESSAGE_NOT_POSITIVE_POSITIVE,
+  INPUT_MESSAGE_CAR_NAMES,
+  INPUT_MESSAGE_TRY_COUNT,
+  OUTPUT_MESSAGE_WINNER,
+} from "./constants";
 
 class App {
   async run() {
     const userInputCarNames = await MissionUtils.Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+      INPUT_MESSAGE_CAR_NAMES
     );
     const userInputTryCount = await MissionUtils.Console.readLineAsync(
-      "시도할 횟수는 몇 회인가요?"
+      INPUT_MESSAGE_TRY_COUNT
     );
     const carNameList = userInputCarNames.split(",");
     for (const carName of carNameList) {
       if (carName.length >= 5)
-        throw new Error("[ERROR] 자동차 이름의 길이가 5 이상입니다.");
+        throw new Error(ERROR_MESSAGE_CAR_NAME_OVER_FIVE);
     }
     const tryCount = +userInputTryCount;
-    if (isNaN(tryCount) || tryCount < 0 || !Number.isInteger(tryCount))
-      throw new Error("[ERROR]");
+    if (isNaN(tryCount)) throw new Error(ERROR_MESSAGE_NOT_NUMBER);
+    if (tryCount < 0) throw new Error(ERROR_MESSAGE_NOT_POSITIVE_POSITIVE);
+    if (!Number.isInteger(tryCount)) throw new Error(ERROR_MESSAGE_NOT_INTEGER);
     const carPositions = {};
     for (let i = 0; i < tryCount; i++) {
       for (const carName of carNameList) {
@@ -39,7 +49,9 @@ class App {
       .filter((carPosition) => carPosition[1] === winPosition)
       .map((it) => it[0]);
 
-    MissionUtils.Console.print(`최종 우승자 : ${winnerList.join(", ")}`);
+    MissionUtils.Console.print(
+      `${OUTPUT_MESSAGE_WINNER}${winnerList.join(", ")}`
+    );
   }
 }
 
