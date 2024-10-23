7d69d00b3ba3f0b8b37baa48ae57f46ddae1583a
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-10-23 14:55:55 +0900

    test: 이름 문자열 쉼표로 분리해 배열 전환 구현
    - split을 이용해 배열로 전환

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ class App {
       );
       Validator.nameString(inputString);
 
+      const nameArray = inputString.split(',');
+
       const repeatCount = Number(
         await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n')
       );
