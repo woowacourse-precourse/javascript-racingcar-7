5b796251ee6c9ad00f8f78a2e4a02e2c900ec544
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-10-23 19:14:35 +0900

    feat: 우승자 판별 구현
    - 최대 이동 거리로 filter해 우승자 배열 획득

--- src/printWinners.js
+++ src/printWinners.js
@@ -2,6 +2,8 @@ const printWinners = function printWinners(carArray) {
   const maxDistance = carArray.reduce((maxDistance, car) => {
     return Math.max(maxDistance, car.getDistance());
   }, 0);
+
+  const winners = carArray.filter((car) => car.getDistance() === maxDistance);
 };
 
 export default printWinners;
