3b65be507a65037d4c7bcdcd37e4393d179c5e59
yoo_min <vendetta12@naver.com>
2024-10-24 05:43:08 +0900

    feat: 0-9 사이 randomNumber 생성 함수 추가, 자동차 이동 기능 추가

--- src/Car.js
+++ src/Car.js
@@ -3,6 +3,16 @@ class Car {
         this.name = name;
         this.distance = 0;
     }
+    carMove() {
+        const ramdomNumber = this.drawRandomNumber()
+        if (ramdomNumber >= 4) {
+            this.distance += 1;
+        }
+    }
+    drawRandomNumber() {
+        const randomNumber = Random.pickNumberInRange(0, 9)
+        return randomNumber;
+    }
 }
 
 export default Car;
