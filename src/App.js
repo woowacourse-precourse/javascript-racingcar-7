e8c8f29c6c77c1511219fd9999fa9c9381cf87e6
guesung <gueit214@naver.com>
2024-10-23 11:36:04 +0900

    feat: 사용자의 입력 받기 - 시도할 횟수

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ class App {
     const userInputCarNames = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
+    const userInputTryCount = await MissionUtils.Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?"
+    );
   }
 }
 
