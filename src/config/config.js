f504634c7686c1baee5bc8772faed474760a6ef8
Jaeho Cha <ischar@naver.com>
2024-10-23 20:44:31 +0900

    feat(input): add inputHandler for managing input
    - inputHandler 기능 추가
    - Config.js 추가

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+export const MESSAGES = {
+  INPUT_CAR_NAME:
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+  INPUT_TRY_COUNT: "시도할 횟수는 몇 회인가요?",
+  ERROR_INVALID_NUMBER: "[ERROR]: 잘못된 값입니다.",
+  ERROR_INPUT_EMPTY: "[ERROR]: 입력값이 없습니다.",
+};
+
+export const DELIMITER = ",";
