daa138c6cdb874fa76c9aa1c36cf0e5b782f2ae8
jongbin26 <010jjj@naver.com>
2024-10-23 21:10:34 +0900

    test(createGameRecord.test): createGameRecord 함수 테스트 코드 작성

--- src/test/createGameRecord.test.js
+++ src/test/createGameRecord.test.js
@@ -1,4 +1,4 @@
-import { splitCarString } from '../utils/createGameRecord';
+import { splitCarString, createGameRecord } from '../utils/createGameRecord';
 
 test('split 함수 테스트', () => {
   const carString = 'pobi,woni,jun';
@@ -6,3 +6,8 @@ test('split 함수 테스트', () => {
   expect(splitedCarString).toEqual(['pobi', 'woni', 'jun']);
 });
 
+test('createGameRecord 함수 테스트', () => {
+  const cars = ['pobi', 'woni', 'jun'];
+  const gameRecord = createGameRecord(cars);
+  expect(gameRecord).toEqual({ pobi: 0, woni: 0, jun: 0 });
+});
