d5188d4f4ec4ee57d360e73839709acb47cb90ab
deokbin <deokbin@naver.com>
2024-10-24 01:14:14 +0900

    feat<App,NameValidation> : 자동차 이름 입력값 유효성 검사 함수 추가 5글자 초과인 경우, 빈 문자열인 경우, 값이 하나인 경우, 중복인 경우 에러 처리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Console } from '@woowacourse/mission-utils';
-import isValidInput from '../src/NameValidation.js';
+import isValidName from './NameValidation.js';
 
 class App {
   async run() {
     const userInput = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
     );
-    isValidInput(userInput.trim());
+    isValidName(userInput.trim());
     Console.print(userInput);
   }
 }
---
+++ src/NameValidation.js
@@ -0,0 +1,29 @@
+import {
+  EMPTY_CAR_NAME_ERROR,
+  MORE_THAN_FIVE_LETTERS,
+  SAME_CAR_NAME_ERROR,
+  ONE_CAR_NAME_ERROR,
+} from './constants/Error';
+
+export default function isValidName(input) {
+  if (input === '') {
+    throw new Error(EMPTY_CAR_NAME_ERROR);
+  }
+  const carList = input.split(',');
+  carList.forEach(car => {
+    if (car.length > 5) {
+      throw new Error(MORE_THAN_FIVE_LETTERS);
+    }
+  });
+  if (carList.length > 1) {
+    const carSet = new Set(carList);
+
+    if (carList.length !== carSet.size) {
+      throw new Error(SAME_CAR_NAME_ERROR);
+    }
+    return true;
+  } else if (carList.length === 1) {
+    throw new Error(ONE_CAR_NAME_ERROR);
+  }
+  return true;
+}
