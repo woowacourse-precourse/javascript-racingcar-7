6e73059e32527f5edbed73319da13799a8ecec3f
seongmin <seongm22n@gmail.com>
2024-10-23 19:38:56 +0900

    feat: 기능 1-(2)
    - 시도할 횟수 입력

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ class App {
 		const carNameInput = await Console.readLineAsync(
 			`경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n`
 		);
+		const raceCountInput = await Console.readLineAsync(
+			`시도할 횟수는 몇 회인가요?\n`
+		);
 	}
 }
 
