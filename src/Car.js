33a96ad11048c047aaafab92311d2484fdea038b
RAKRAK21 <94699316+RAKRAK21@users.noreply.github.com>
2024-10-23 18:20:35 +0900

    feat: implement car movement function and update README
    - Add goAhead() method to Car class using Random for movement
    - Movement occurs when random number (0-9) is greater than or equal to 4
    - Update README with sub-feature details for feature #3

---
+++ src/Car.js
@@ -0,0 +1,28 @@
+import { Random } from "@woowacourse/mission-utils";
+
+class Car{
+    position = 0;
+
+    constructor(name){
+        this.validateName(name);
+        this.name = name;
+    }
+
+    validateName(name){
+        if(length(name) > 5) {
+            throw new Error("[ERROR] 이름은 5자를 초과하면 안됩니다.");
+        }
+    }
+
+    goAhead(){
+        if(Random.pickNumberInRange(0, 9) >= 4) {
+            this.position += 1;
+        };
+    }
+
+    currentPosition(){
+        return '-'.repeat(this.position)
+    }
+}
+
+export default Car;
