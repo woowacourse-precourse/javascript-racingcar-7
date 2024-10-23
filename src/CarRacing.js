7d859b178a65ae34457a1830671e4155351950d8
yeji0214 <1225yj50@naver.com>
2024-10-23 16:26:10 +0900

    feat: Separate entered car names with comma

--- src/CarRacing.js
+++ src/CarRacing.js
@@ -5,6 +5,8 @@ export const startRace = async () => {
     Console.print(carNames);
 }
 
-const inputCarNames = () => {
-    return Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+const inputCarNames = async () => {
+    const carNames = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+
+    return carNames.split(',')
 }
