e3e826bc0e6d161f749b1254c323ff181d19cd29
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-10-24 02:00:00 +0900

    refactor: App에서 입력 값 검증
    - 함수에서 입력과 함께 검증 하는 것은 책임 2가지라고 생각함

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { createCars } from './models/createCars.js';
-import {
-  getValidateNameInput,
-  getValidateRoundCountInput,
-} from './utils/inputHandler.js';
+import { getNameInput, getRoundCountInput } from './utils/inputHandler.js';
 import Race from './models/Race.js';
+import Validator from './validators/Validator.js';
 import WinnerCalculator from './models/WinnerCalculator.js';
 
 class App {
   async run() {
     try {
-      const names = await getValidateNameInput();
+      const names = await getNameInput();
+      Validator.checkNames(names);
+
       const cars = createCars(names);
 
-      const roundCount = await getValidateRoundCountInput();
+      const roundCount = await getRoundCountInput();
+      Validator.checkRoundCount(roundCount);
 
       const race = new Race(cars, roundCount);
       race.start();
--- src/utils/inputHandler.js
+++ src/utils/inputHandler.js
@@ -1,22 +1,18 @@
 import { Console } from '@woowacourse/mission-utils';
 import Validator from '../validators/Validator.js';
 
-export const getValidateNameInput = async () => {
+export const getNameInput = async () => {
   const nameString = await Console.readLineAsync(
     '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
   );
 
-  const names = nameString.split(',');
-  Validator.checkNames(names);
-
-  return names;
+  return nameString.split(',');
 };
 
-export const getValidateRoundCountInput = async () => {
+export const getRoundCountInput = async () => {
   const roundCount = Number(
     await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n')
   );
-  Validator.checkRoundCount(roundCount);
 
   return roundCount;
 };
