f5dad3adf3cac4c7f0f8adb24da104bce2c63b14
eungyo <eungyo0965@gmail.com>
2024-10-23 16:53:42 +0200

    feat(Input) : Add validation for car name length

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ class App {
         '자동차 이름을 입력하세요: ',
         (input) => {
           const names = input.split(',');
+          if (names.some((name) => name.length > 5)) {
+            reject(new Error('[ERROR] 자동차 이름은 5자 이하만 가능합니다.'));
+          } else {
+            resolve(names);
+          }
         },
       );
     });
