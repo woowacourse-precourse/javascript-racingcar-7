c7c977756469d9bf40ad560d2a1e4e711428abe1
donggyun <donggyunkim12@gmail.com>
2024-10-23 23:00:02 +0900

    test(App): 우승자 출력 기능에 대한 테스트 구현

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -100,6 +100,7 @@ describe('게임 시작 테스트', () => {
 
   test('매 시행마다 각 자동차들의 상태 출력 테스트', () => {
     const logSpy = getLogSpy();
+
     mockRandoms(randomNumbers);
 
     const app = new App();
@@ -108,5 +109,40 @@ describe('게임 시작 테스트', () => {
     answer.forEach((log) => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
     });
+  });
+});
+
+describe('우승자 출력 테스트', () => {
+  const TEST_CASES = [
+    [[new Car('pobi')], [false, true, false], ['최종 우승자 : pobi']],
+    [
+      [new Car('pobi'), new Car('lisa')],
+      [false, false, false, false, false, false],
+      ['최종 우승자 : pobi,lisa'],
+    ],
+    [
+      [new Car('pobi'), new Car('lisa'), new Car('dany')],
+      [false, false, false, true, false, false, false, false, true],
+      ['최종 우승자 : pobi,dany'],
+    ],
+  ];
+
+  test.each(TEST_CASES)('여러명일 경우 쉼표(,)를 사용하여 출력한다', (cars, isMoved, answer) => {
+    const logSpy = getLogSpy();
+
+    cars.forEach((car, carIdx) => {
+      isMoved.forEach((bool, boolIdx) => {
+        if (boolIdx % 3 === carIdx && bool) {
+          car.move();
+        }
+      });
+    });
+
+    cars.forEach((car) => car.getCarInformation());
+
+    const app = new App();
+    app.result(cars);
+
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(answer.toString()));
   });
 });
