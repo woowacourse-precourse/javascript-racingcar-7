a691e0b414a0e8b5d9ed558353d5cdc1add93c09
minjeongss <dotoriido@gmail.com>
2024-10-23 22:20:41 +0900

    feat: 입력 부분(inputView) 구현
    - 경주할 자동차 입력 받는 함수 구현
    - 시도할 횟수 입력 받는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const CAR_NAME_LIST = await this.readCarName();
+    const GAME_COUNT = await this.readGameCount();
+  }
+
+  async readCarName() {
+    return await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+  }
+
+  async readGameCount() {
+    return await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+  }
 }
 
 export default App;
