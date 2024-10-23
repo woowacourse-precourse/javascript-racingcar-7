777a4b73dd0d3193b28dc84d856dd20ffce69976
조혜린 <hyer1205@gmail.com>
2024-10-23 19:14:34 +0900

    Feat(app): 자동차 경주에 참여할 자동차 이름 입력
    자동차 경주 게임 시작 전 설정 값으로 게임에 참여할 n대의 자동차 이름 입력
    자동차의 이름은 쉼표(,)로 구분
    잘 입력됐는지 확인하기 위해 Console.print() API 사용하여 확인

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const inputCarNames = await Console.readLineAsync(
+        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+      );
+
+      const carNames = inputCarNames.split(',');
+      Console.print(`자동차 이름: ${carNames}`);
+    } catch (error) {
+      throw new Error(`[ERROR] ${error.message}`);
+    }
+  }
 }
 
 export default App;
