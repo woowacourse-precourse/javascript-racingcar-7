d2f4ce5aafd1754d16a69d6fe47e5b2de3e30927
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-10-23 17:48:18 +0900

    feat: CarRacingInputReader 클래스 추가
    - getCarNames(): 입력받은 자동차 이름을 쉼표로 구분하여 처리
    - getTotalRounds(): 시도할 라운드 수 입력 처리
    - validateInput(): 입력값의 유효성 검증

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import CarRacingInputReader from './classes/CarRacingInputReader.js';
+
 class App {
-  async run() {}
+  async run() {
+    const inputReader = new CarRacingInputReader();
+    const carNamesInput = await inputReader.getCarNames();
+    const totalRounds = await inputReader.getTotalRounds();
+  }
 }
 
 export default App;
---
+++ src/classes/CarRacingInputReader.js
@@ -0,0 +1,28 @@
+import { Console } from '@woowacourse/mission-utils';
+
+class CarRacingInputReader {
+  async getCarNames() {
+    const input = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+    );
+
+    this.validateInput(input);
+
+    return input;
+  }
+
+  async getTotalRounds() {
+    const input = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+
+    this.validateInput(input);
+
+    return input;
+  }
+
+  validateInput(input) {
+    if (!input.trim()) {
+      throw new Error(`[ERROR] 값을 제대로 입력해주세요.`);
+    }
+  }
+}
+export default CarRacingInputReader;
