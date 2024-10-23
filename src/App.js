8a2939b43c1a36e8750c167b8073a96a3685a4c7
choeeun-in <cei8645@naver.com>
2024-10-23 16:14:25 +0900

    feat: 경주할 자동차 이름 부여하는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    //경주할 자동차 이름 부여하는 기능
+    const CAR_NAME = await Console.readLineAsync(
+      "자동차 이름은 무엇입니까?"
+    );
+    function Separator(CAR_NAME) {
+      const CAR = CAR_NAME.indexOf(',');
+
+      if (CAR.length < 6) {
+        throw new Error("[Error] 자동차 이름은 5자 이하만 가능합니다.")
+      }
+    }
+
+    const FIRST = Separator(CAR_NAME);
+
+    
+
+
+    //결과 출력
+    Console.print(FIRST);
+  }
 }
 
 export default App;
