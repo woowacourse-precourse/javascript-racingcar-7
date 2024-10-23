52efee7795a67d4d254a50db4c6be3536c14d0ec
guesung <gueit214@naver.com>
2024-10-23 11:29:34 +0900

    test: 각 테스트 케이스 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -25,36 +25,115 @@ const getLogSpy = () => {
 };
 
 describe("자동차 경주", () => {
-  test("기능 테스트", async () => {
-    // given
-    const MOVING_FORWARD = 4;
-    const STOP = 3;
-    const inputs = ["pobi,woni", "1"];
-    const logs = ["pobi : -", "woni : ", "최종 우승자 : pobi"];
-    const logSpy = getLogSpy();
+  describe("기능 테스트", () => {
+    test("최종 우승자가 1명인 경우", async () => {
+      const MOVING_FORWARD = 4;
+      const STOP = 3;
+      const inputs = ["pobi,woni", "1"];
+      const logs = [
+        "pobi : -",
+        "woni : ",
+        "pobi : --",
+        "woni : ",
+        "pobi : ---",
+        "woni : ",
+        "pobi : ----",
+        "woni : ",
+        "pobi : -----",
+        "woni : ",
+        "최종 우승자 : pobi",
+      ];
+      const logSpy = getLogSpy();
 
-    mockQuestions(inputs);
-    mockRandoms([MOVING_FORWARD, STOP]);
+      mockQuestions(inputs);
+      mockRandoms([MOVING_FORWARD, STOP]);
+      mockRandoms([MOVING_FORWARD, STOP]);
+      mockRandoms([MOVING_FORWARD, STOP]);
+      mockRandoms([MOVING_FORWARD, STOP]);
+      mockRandoms([MOVING_FORWARD, STOP]);
 
-    // when
-    const app = new App();
-    await app.run();
+      const app = new App();
+      await app.run();
 
-    // then
-    logs.forEach((log) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+      logs.forEach((log) => {
+        expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+      });
+    });
+
+    test("최종 우승자가 5명인 경우", async () => {
+      const MOVING_FORWARD = 4;
+      const STOP = 3;
+      const inputs = ["pobi,woni,guesung,hyunjin,guehyeok", "2"];
+      const logs = [
+        "pobi : -",
+        "woni : ",
+        "guesung : ",
+        "hyunjin : ",
+        "guehyeok : ",
+        "pobi : -",
+        "woni : -",
+        "guesung : ",
+        "hyunjin : ",
+        "guehyeok : ",
+        "pobi : -",
+        "woni : -",
+        "guesung : -",
+        "hyunjin : ",
+        "guehyeok : ",
+        "pobi : -",
+        "woni : -",
+        "guesung : -",
+        "hyunjin : -",
+        "guehyeok : ",
+        "pobi : -",
+        "woni : -",
+        "guesung : -",
+        "hyunjin : -",
+        "guehyeok : -",
+        "최종 우승자 : pobi,woni,guesung,hyunjin,guehyeok",
+      ];
+      const logSpy = getLogSpy();
+
+      mockQuestions(inputs);
+      mockRandoms([MOVING_FORWARD, STOP]);
+      mockRandoms([STOP, MOVING_FORWARD]);
+
+      const app = new App();
+      await app.run();
+
+      logs.forEach((log) => {
+        expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+      });
     });
   });
 
-  test("예외 테스트", async () => {
-    // given
-    const inputs = ["pobi,javaji"];
-    mockQuestions(inputs);
+  describe("입력 1번 예외 테스트", () => {
+    test("5글자 이상의 문자가 올 경우 에러를 출력한다.", async () => {
+      const inputs = ["pobi,javaji"];
+      mockQuestions(inputs);
 
-    // when
-    const app = new App();
+      const app = new App();
 
-    // then
-    await expect(app.run()).rejects.toThrow("[ERROR]");
+      await expect(app.run()).rejects.toThrow("[ERROR]");
+    });
+  });
+
+  describe("입력 2번 예외 테스트", () => {
+    test("양수가 아닌 경우 에러를 출력한다.", async () => {
+      const inputs = ["pobi,java", -5];
+      mockQuestions(inputs);
+
+      const app = new App();
+
+      await expect(app.run()).rejects.toThrow("[ERROR]");
+    });
+    test("정수가 아닌 경우 에러를 출력한다.", async () => {
+      const inputs = ["pobi,java", 5.5];
+      mockQuestions(inputs);
+
+      const app = new App();
+
+      await expect(app.run()).rejects.toThrow("[ERROR]");
+    });
   });
 });
