92771a732dccaef9cc5dc48aafe70220d2029efb
yusuShin <tlsdbtn0507@naver.com>
2024-10-23 22:16:13 +0900

    feat(app) Add print functions - functions that print their own parameters or specific string - printParams : function that prints its param ex) userInput or 실행결과 - printRacing : function that prints carname and distance (-) that how much it has gone - printResult : function that prints the result of game - printError : function that prints the Error and its message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+
+  }
+
+  printError(msg) {
+    Console.print('[ERROR]' + msg);
+    throw new Error("[ERROR]");
+  }
+
+  printParams(paramStr) {
+    Console.print(paramStr);
+  }
+
+  printRacing(carName, racingDistances) {
+    Console.print(`${carName} : ${racingDistances}`);
+  }
+
+  printResult(str) {
+    Console.print('최종 우승자 : ' + str);
+  }
 }
 
 export default App;
