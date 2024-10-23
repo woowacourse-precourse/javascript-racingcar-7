7ded72e447dfba4c9bce3672cc9d90a09b47018b
정해성 <gotjd0230@naver.com>
2024-10-23 21:54:44 +0900

    test: 전체 테스트 코드 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,7 +1,7 @@
 import App from '../src/App.js';
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-const mockQuestions = inputs => {
+const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
 
   MissionUtils.Console.readLineAsync.mockImplementation(() => {
@@ -10,7 +10,7 @@ const mockQuestions = inputs => {
   });
 };
 
-const mockRandoms = numbers => {
+const mockRandoms = (numbers) => {
   MissionUtils.Random.pickNumberInRange = jest.fn();
 
   numbers.reduce((acc, number) => {
@@ -25,11 +25,11 @@ const getLogSpy = () => {
 };
 
 describe('자동차 경주', () => {
-  test('최종 우승자 출력', async () => {
+  test('기능 테스트', async () => {
     const MOVING_FORWARD = 4;
     const STOP = 3;
     const inputs = ['pobi,woni', '1'];
-    const logs = ['최종 우승자 : pobi'];
+    const logs = ['pobi : -', 'woni : ', '최종 우승자 : pobi'];
     const logSpy = getLogSpy();
 
     mockQuestions(inputs);
@@ -38,7 +38,25 @@ describe('자동차 경주', () => {
     const app = new App();
     await app.run();
 
-    logs.forEach(log => {
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test('복수 우승자 테스트', async () => {
+    const MOVING_FORWARD = 4;
+    const STOP = 3;
+    const inputs = ['pobi,woni,jun', '1'];
+    const logs = ['pobi : -', 'woni : -', 'jun : -', '최종 우승자 : pobi, woni, jun'];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD]);
+
+    const app = new App();
+    await app.run();
+
+    logs.forEach((log) => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
     });
   });
