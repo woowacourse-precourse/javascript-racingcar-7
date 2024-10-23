29e7a8a560b8777ef86ae9ad406c9a03927dcdf6
yheseul <kangyheseul@naver.com>
2024-10-24 08:52:42 +0900

    feat: 랜덤 값에 따른 자동차 전진 및 정지 기능 구현

---
+++ src/carRace/movingForward.js
@@ -0,0 +1,12 @@
+import { CAR_RACE } from "../constants/constants.js";
+import { getExecutionProcess } from "./getExecutionProcess.js";
+import { generateRandomNumber } from "./generateRandomNumber.js";
+
+export function movingForward(playersObject) {
+  for (const player in playersObject) {
+    const randomNumber = generateRandomNumber();
+    if (randomNumber >= CAR_RACE.MOVE_THRESHOLD) playersObject[player] += CAR_RACE.CAR_MOVE;
+    if (randomNumber < CAR_RACE.MOVE_THRESHOLD) playersObject;
+  }
+  return getExecutionProcess(playersObject);
+}
