9791253dbd705bb5bb3cdb1a1dbd0d60b5281a3f
dmson1218 <dmson1218@naver.com>
2024-10-24 00:05:43 +0900

    refactor(InputValidator): 에러 메시지 수정

--- src/InputValidator.js
+++ src/InputValidator.js
@@ -1,30 +1,30 @@
 class InputValidator {
     static ERROR_MESSAGES = Object.freeze({
-        EMPTY: "자동차 이름이 입력되지 않았습니다.",
-        INVALID: "자동차 이름은 1~5글자여야 하며, 공백이나 특수 문자는 포함할 수 없습니다.",
-        DUPLICATE: "중복되는 자동차 이름이 있습니다.",
-        NOT_NUMBER: "시도 횟수는 숫자여야 합니다.",
-        LESS_THAN_ZERO: "시도 횟수는 0보다 커야합니다.",
+        CAR_NAME_EMPTY: "자동차 이름이 입력되지 않았습니다.",
+        INVALID_NAME: "자동차 이름은 1~5글자여야 하며, 공백이나 특수 문자는 포함할 수 없습니다.",
+        DUPLICATED_NAME: "중복되는 자동차 이름이 있습니다.",
+        TRY_COUNT_EMPTY: "시도 횟수가 입력되지 않았습니다.",
+        INVALID_TRY_COUNT: "시도 횟수는 1 이상의 정수여야 합니다.",
     });
 
     static validateCarNameList(carNameList) {
         const regex = /^[a-zA-Z가-힣0-9]{1,5}$/;
 
         if (carNameList.length === 0) {
-            InputValidator.#throwError(InputValidator.ERROR_MESSAGES.EMPTY);
+            InputValidator.#throwError(InputValidator.ERROR_MESSAGES.CAR_NAME_EMPTY);
         } else if (!carNameList.every((carName) => regex.test(carName))) {
-            InputValidator.#throwError(InputValidator.ERROR_MESSAGES.INVALID);
+            InputValidator.#throwError(InputValidator.ERROR_MESSAGES.INVALID_NAME);
         }
         // 중복된 자동차 이름을 체크
         else if (carNameList.length !== new Set(carNameList).size) {
-            InputValidator.#throwError(InputValidator.ERROR_MESSAGES.DUPLICATE);
+            InputValidator.#throwError(InputValidator.ERROR_MESSAGES.DUPLICATED_NAME);
         }
     }
 
     static validateInputTryCount(tryInputCount) {
-        if (isNaN(tryInputCount)) {
-            InputValidator.#throwError(InputValidator.ERROR_MESSAGES.NOT_NUMBER);
-        } else if (tryInputCount <= 0) {
+        if (tryInputCount === "") {
+            InputValidator.#throwError(InputValidator.ERROR_MESSAGES.TRY_COUNT_EMPTY);
+        } else if (isNaN(tryInputCount) || tryInputCount < 1) {
             InputValidator.#throwError(InputValidator.ERROR_MESSAGES.LESS_THAN_ZERO);
         }
     }
