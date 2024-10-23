5c3aba423681e55fa64cf4921a3916f101ae8ca8
yheseul <kangyheseul@naver.com>
2024-10-23 17:27:06 +0900

    test: 자동차 이름 구분자가 쉼표(,)가 아닌 경우 예외 처리 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,11 @@
 import App from "../src/App.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
+import { validateCarNameLength } from "../src/validation/validateCarName/validateCarNameLength.js";
+import { ERROR_MESSAGES } from "../src/constants/constants.js";
+import { validateCarNameSeparator } from "../src/validation/validateCarName/validateCarNameSeparator.js";
+import { validateCarNameOnlyLetters } from "../src/validation/validateCarName/validateCarNameOnlyLetters.js";
+import { validateCarNameCount } from "../src/validation/validateCarName/validateCarNameCount.js";
+import { validateDuplicateCarNames } from "../src/validation/validateCarName/validateDuplicateCarNames.js";
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -46,15 +52,9 @@ describe("자동차 경주", () => {
     });
   });
 
-  test("예외 테스트", async () => {
-    // given
-    const inputs = ["pobi,javaji"];
-    mockQuestions(inputs);
-
-    // when
-    const app = new App();
-
-    // then
-    await expect(app.run()).rejects.toThrow("[ERROR]");
+  test("입력된 자동차 이름이 쉼표(,)가 아닌 다른 구분자를 포함한 경우", () => {
+    expect(() => validateCarNameSeparator("emma/sophia/mia")).toThrow(
+      ERROR_MESSAGES.INVALID_NAME_SEPARATOR
+    );
   });
 });
