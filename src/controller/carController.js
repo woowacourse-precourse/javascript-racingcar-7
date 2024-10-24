3b454b85c2d3d0c519a669aef08a271131dc3c9f
RohSungKyun <songkyun1108@naver.com>
2024-10-24 11:18:00 +0900

    feat : 우승자 반환함수 구현

--- src/controller/carController.js
+++ src/controller/carController.js
@@ -14,6 +14,8 @@ class CarController{
     await this.inputCount();
 
     await this.moveforward();
+    const winner = await this.getWinner();
+    Console.print(winner);
   }
 
   async inputCarList() {
@@ -40,13 +42,33 @@ class CarController{
     this.carList.forEach((car) => {
       let speed = MissionUtils.Random.pickNumberInRange(0, 9);
       if(speed > 4){
-        if(!this.distance.get(car)){ //존재하지 않는다면
+        if(!this.distance.get(car)){
           this.distance.set(car, 1);
+          Console.print(`${car} 전진`);
+          // 결과 출력 View
         } else{
           this.distance.set(car, this.distance.get(car)+1);
+          Console.print(`${car} 전진`);
+          // 결과 출력 View
         }
       }
     })
+  }
+  async getWinner() {
+    // 해시맵에서 우승자를 추출 구현
+    const winner = [];
+    const allRecords = [];
+    let maxRecord = 0;
+    for (let [carName, record] of this.distance){
+      allRecords.push(record);
+    }
+    maxRecord = Math.max(...allRecords);
+    for (let [carName, record] of this.distance){
+      if(record === maxRecord){ // 문자열 비교 분석 필요
+        winner.push(carName);
+      }
+    }
+    return winner;
   }
 
 
