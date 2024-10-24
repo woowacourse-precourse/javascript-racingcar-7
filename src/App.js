125612c4cdc9becdbb9f93091b87f4dc4aa5c3fa
hjoo830 <hjoo830@naver.com>
2024-10-24 12:25:13 +0900

    feat: Console.readLineAsync()를 이용해 경주할 자동차 이름 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const inputCar = await this.getInputCarNames();
+  }
+
+  async getInputCarNames() {
+    return await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
+    );
+  }
 }
 
 export default App;
