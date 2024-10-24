6fb1d402026ee23fe97ab696e4cb483361485708
jongbin26 <010jjj@naver.com>
2024-10-24 10:45:17 +0900

    feat(Game): print 함수 구현
    - 게임 결과를 출력하는 print 함수를 구현했습니다.

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,4 @@
-import { Random } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 import { initRecord } from './utils/initRecord.js';
 
 export class Game {
@@ -12,16 +12,24 @@ export class Game {
     });
   }
   play() {
+    Console.print('\n실행 결과');
     for (let i = 0; i < this.count; i++) {
       this.move();
     }
   }
   move() {
-    let movingCars = [];
+    const movingCars = [];
     Object.keys(this.record).map((key) => {
       Random.pickNumberInRange(0, 9) >= 4 && movingCars.push(key);
     });
     this.update(movingCars);
+    this.print();
   }
-  print() {}
+
+  print() {
+    Object.entries(this.record).map(([car, movingLength]) => {
+      Console.print(car + ' : ' + '-'.repeat(movingLength));
+    });
+    Console.print('\n');
+  }
 }
