0e8d63f1aa6b96568ab018d1abfcae20d1a6975c
aydenote <dslgpgh@gmail.com>
2024-10-24 19:26:06 +0900

    test(TryCount): 시도 횟수 최대 입력 유닛 테스트 추가

--- __tests__/TryCount.test.js
+++ __tests__/TryCount.test.js
@@ -1,5 +1,5 @@
 import RacingCar from "../src/RacingCar.js";
-import { validateMinCount } from "../src/utils/validate.js";
+import { validateMaxCount, validateMinCount } from "../src/utils/validate.js";
 
 describe("시도 횟수 입력", () => {
 	test("시도 횟수 정상 입력", async () => {
@@ -20,6 +20,16 @@ describe("시도 횟수 입력", () => {
 		// when, then
 		await expect(validateMinCount(tryCount)).rejects.toThrow(
 			"[ERROR] 입력할 수 있는 시도 횟수는 최소 1회입니다."
+		);
+	});
+
+	test("시도 횟수 초과 입력", async () => {
+		// given
+		const tryCount = "11";
+
+		// when, then
+		await expect(validateMaxCount(tryCount)).rejects.toThrow(
+			"[ERROR] 입력할 수 있는 시도 횟수는 최대 10회입니다."
 		);
 	});
 });
