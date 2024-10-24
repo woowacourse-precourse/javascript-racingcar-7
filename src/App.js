9e32b9bfb9f18d283c11212e2c9e0e03c02b4501
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-10-24 22:47:35 +0900

    feat: 자동차 이름은 쉼표(,)를 기준으로 구분한다

--- src/App.js
+++ src/App.js
@@ -10,10 +10,15 @@ class App {
   async run() {
     this.carList = this.makeCars([]);
     const carNames = await this.getCarName();
+    const splittedCarName = this.splitCarName(carNames);
   }
 
   async getCarName() {
     return await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+  }
+
+  splitCarName(name) {
+    return name.split(',');
   }
 
   makeCars(names) {
