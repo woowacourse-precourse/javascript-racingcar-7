2f028fdb1bf55c2e6dbd02376bf83c2c3414e075
hjoo830 <hjoo830@naver.com>
2024-10-24 12:26:06 +0900

    feat: Console.readLineAsync()를 이용해 시도할 횟수 입력받기

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     const inputCar = await this.getInputCarNames();
+    const inputCount = await this.getInputCount();
   }
 
   async getInputCarNames() {
     return await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
     );
+  }
+
+  async getInputCount() {
+    return await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
   }
 }
 
