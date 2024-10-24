8dc3e215175f0a2928dc33a8fdce66feadbbff21
sanghyeonLee <hyeon012366@gmail.com>
2024-10-24 21:19:25 +0900

    feat: 문자열을 "," 기준으로 split 하는 메소드 추가

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -4,12 +4,16 @@ import {PROMPT_MESSAGE} from "./constants/message.js";
 class RacingGame {
     async start(){
         const input = await this.input()
+        this.sliceString(input)
     }
 
      async input() {
         return await Console.readLineAsync(PROMPT_MESSAGE.INPUT_CAR_NAME)
     }
 
+    sliceString(param){
+        return param.split(",")
+    }
 
 
 
