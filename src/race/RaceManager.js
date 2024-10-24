37bdbb59cc3d7d624a23e65c9b0dddcc83207b94
choiihwa <chldlghk@gmail.com>
2024-10-24 22:01:43 +0900

    feat(race): 자동차명 배열 생성
    - 입력받은 값을 쉼표(,)를 기준으로 분리하여 배열 생성
    - 자동차명 최대 길이 설정
    - 자동차명 최대 길이 초과 검증 및 초과 시 에러 발생

--- src/race/RaceManager.js
+++ src/race/RaceManager.js
@@ -1,14 +1,25 @@
 import IOHandler from "../utils/IOHandler.js";
+import { isValidNameLength } from "./Validator.js";
 
 class RaceManager {
+    #MAX_NAME_LENGTH = Object.freeze(5);
 
     #carList;
 
     #racingCount;
 
+    #getNamesFromStr(str) {
+        return str.replaceAll(' ', '').split(',');
+    }
+
     async #prepareRacing() {
         const inputStr = await IOHandler.input("경주할 자동차 이름을 입렿가세요.(이름은 쉼표(,) 기준으로 구분)\n");
-        IOHandler.output(inputStr);
+        const carNames = this.#getNamesFromStr(inputStr);
+        carNames.forEach(name => {
+            isValidNameLength(name, this.#MAX_NAME_LENGTH);
+        });
+
+        IOHandler.output(carNames);
     }
 
     racing() {
---
+++ src/race/Validator.js
@@ -0,0 +1,5 @@
+export function isValidNameLength(name, length) {
+    if (name.length > length) {
+        throw Error(`[ERROR] 이름은 최대 ${length}자까지 설정 가능합니다. ${name}`);
+    }
+}   
