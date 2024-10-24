5bfaff4f83aa0d31bfaa7e4bdd99447a14fa14f4
eunwoo-levi <eunwoo1341@gmail.com>
2024-10-24 09:21:20 +0900

    feat(car): implement Car class for racing
    - Create Car class with name and position management
    - Add movement logic with random number
    - Add state management methods

--- src/models/Car.js
+++ src/models/Car.js
@@ -1,5 +1,24 @@
 import { Console } from '@woowacourse/mission-utils';
 
-class Car {}
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.position = 0;
+  }
+
+  move(randonNumber) {
+    if (randonNumber >= 4) {
+      this.position += 1;
+    }
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getPosition() {
+    return this.position;
+  }
+}
 
 export default Car;
