a92e0de2dbe3dda7b5a0e15a8287eac3cd46b25a
rubberchin <88rubber@gmail.com>
2024-10-24 15:13:46 +0900

    feat($App): 경주 시행 횟수를 입력
    - 입력값이 비었는지 확인
    - 입력값이 1 이상의 정수인지 확인

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Console, Random } from '@woowacourse/mission-utils';
 
 
 class App {
-    async getCarNames(){
+    async getCarNames() {
         const input = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
         
         if (input === '') {
@@ -12,7 +12,7 @@ class App {
         }   
     }
 
-    splitCars(carNames){
+    splitCars(carNames) {
         const cars = carNames.split(',');
         const hasEmptyName = cars.some(car => car === '');
         const nameTooLong = cars.some(car => car.length > 5);
@@ -20,7 +20,7 @@ class App {
         const hasDuplicates = new Set(cars).size !== cars.length;
 
         if (hasEmptyName) {
-            throw new Error('[ERROR] 경주 하려면 자동차가 2대 이상 필요해요!');
+            throw new Error('[ERROR] 누군가 참가용지에 이름을 쓰지 않았습니다!');
         } else if (nameTooLong) {
             throw new Error('[ERROR] 자동차 이름은 5자 이하로 입력해주세요!');
         } else if (notEnoughCars) {
@@ -32,15 +32,32 @@ class App {
         }
     }
 
-    initializeRacers(cars) {
-        return Object.fromEntries(cars.map(car => [car, 'd']));
+    onStartLine(cars) {
+        return Object.fromEntries(cars.map(car => [car, '']));
+    }
+
+    async getLaps() {
+        const input = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+        
+        if (input === '') {
+            throw new Error('[ERROR] 모든 참가자가 경주를 포기했습니다!');
+        } else if (isNaN(Number(input))) {
+            throw new Error('[ERROR] 숫자를 입력해주세요!');
+        } else if (!Number.isInteger(Number(input))) {
+            throw new Error('[ERROR] 1 이상의 정수를 입력해주세요!');
+        } else if (Number(input) < 0) {
+            throw new Error('[ERROR] 경기 횟수는 0보다 작을 수 없어요!');
+        }else {
+            return input;
+        }   
     }
 
     async run() {
         try {
             const carNames = await this.getCarNames();
             const cars = this.splitCars(carNames);
-            const racers = this.initializeRacers(cars);
+            const racers = this.onStartLine(cars);
+            const laps = await this.getLaps();
         } catch (error) {
             Console.print(`${error.message}`);
         }
