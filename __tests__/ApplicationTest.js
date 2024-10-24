dbea916d3b92ae0fc244f6f671c5f374b9924f99
이은호 <eunho@ieunhoui-MacBookAir.local>
2024-10-24 21:16:18 +0900

    test : 기능 테스트 추가 및 인덱스 생성 && 예외테스트코드 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -25,7 +25,7 @@ const getLogSpy = () => {
 };
 
 describe("자동차 경주", () => {
-  test("기능 테스트", async () => {
+  test("기능 테스트 0", async () => {
     // given
     const MOVING_FORWARD = 4;
     const STOP = 3;
@@ -46,7 +46,7 @@ describe("자동차 경주", () => {
     });
   });
 
-  test("기능 테스트 2", async () => {
+  test("기능 테스트 1", async () => {
     // given
     const MOVING_FORWARD = 4;
     const STOP = 3;
@@ -82,7 +82,53 @@ describe("자동차 경주", () => {
     });
   });
 
-  test("예외 테스트", async () => {
+  test("기능 테스트 2", async () => {
+    // given
+    const MOVING_FORWARD = 4;
+    const STOP = 3;
+    const inputs = ["pobai,woni,dalsi", "1"];
+    const logs = [
+      "pobai : ",
+      "woni : ",
+      "dalsi : ",
+      "최종 우승자 : pobai, woni, dalsi",
+    ];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([STOP, STOP, STOP]);
+
+    // when
+    const app = new App();
+    await app.run();
+
+    // then
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test.each([
+    [["pobi, "]],
+    [[","]] 
+  ])("⚠️ 예외 : 자동차 이름 공백 입력 테스트 %#", async (inputs) => {
+    // given
+    mockQuestions(inputs);
+  
+    // when
+    const app = new App();
+  
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR] : 잘못된 입력입니다.");
+  });
+
+  test.each([
+    [["pobi,jamson"]],
+    [[" jonny, worker"]] ,
+    [["talisker, jack  "]], 
+    [["jack c, dani "]],
+    [["jack, daniels "]]
+  ])("⚠️ 예외 : 6자이상 자동차 이름 입력 테스트 %#", async (inputs) => {
     // given
     const inputs = ["pobi,javaji"];
     mockQuestions(inputs);
