031546367df1985b426f243ee7bfb4e63e4a2f73
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2024-10-24 23:26:11 +0900

    feat: 자동차 이름 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const carInput = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+    );
+
+    Console.print(carInput);
+  }
 }
 
 export default App;
