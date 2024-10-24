c3357a2dd673680d714a2ce775fb3ee7a9140750
choiihwa <chldlghk@gmail.com>
2024-10-24 23:04:18 +0900

    feat(car): 자동차명을 포함한 자동차 객체 생성

--- src/race/RaceManager.js
+++ src/race/RaceManager.js
@@ -1,5 +1,6 @@
 import IOHandler from "../utils/IOHandler.js";
 import { checkValidNameLength, checkLessThanOrEqualMaxCount } from "./Validator.js";
+import Car from "./Car.js";
 
 class RaceManager {
     #MAX_NAME_LENGTH = Object.freeze(5);
@@ -14,6 +15,10 @@ class RaceManager {
         return str.replaceAll(' ', '').split(',');
     }
 
+    #setCarListFromCarNames(carNames) {
+        this.#carList = carNames.map((name) => new Car(name));
+    }
+
     async #prepareRacing() {
         const inputStr = await IOHandler.input("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
         const carNames = this.#getNamesFromStr(inputStr);
@@ -24,6 +29,8 @@ class RaceManager {
         const inputCount = await IOHandler.input("시도할 횟수는 몇 회인가요?");
         checkLessThanOrEqualMaxCount(inputCount, this.#MAX_RACE_COUNT);
         IOHandler.output(`carNames: ${carNames}, inputCount: ${inputCount}`);
+
+        this.#setCarListFromCarNames(carNames);
     }
 
     racing() {
