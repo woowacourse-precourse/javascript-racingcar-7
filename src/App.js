4d68d23840335e15a6d402f61b072db582e65399
ohgus <123801385+ohgus@users.noreply.github.com>
2024-10-25 00:23:45 +0900

    [feat]: 시도할 횟수 입력 가져오기 기능 구현

--- src/App.js
+++ src/App.js
@@ -5,10 +5,19 @@ import { PRINT_MESSAGE } from './constants.js';
 class App {
   async run() {
     const carNamesInput = await this.getCarNamesInput();
+    const moveAttemptCountInput = await this.getMoveAttemptCount();
   }
 
   async getCarNamesInput() {
     const input = await Console.readLineAsync(PRINT_MESSAGE.INPUT_CAR_NAMES);
+
+    return input.replace(/ /g, '');
+  }
+
+  async getMoveAttemptCount() {
+    const input = await Console.readLineAsync(
+      PRINT_MESSAGE.INPUT_MOVE_ATTEMPT_COUNT,
+    );
 
     return input.replace(/ /g, '');
   }
--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 const PRINT_MESSAGE = {
   INPUT_CAR_NAMES:
     '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  INPUT_MOVE_ATTEMPT_COUNT: '시도할 횟수는 몇 회인가요?\n',
 };
 
 export { PRINT_MESSAGE };
