b8071e929147cdaf9ef766237ca9d48ad1ccc78f
yechan <kevin2695@naver.com>
2024-10-24 22:28:02 +0900

    [k1my3ch4n] 자동차 레이스를 진행하는 함수 생성
    - 랜덤값에 따라서 자동차 레이스를 진행하는 함수 생성

--- src/App.js
+++ src/App.js
@@ -4,15 +4,29 @@ const ERROR_MESSAGE = "[ERROR]";
 const NUMBER_REGEX = /^[0-9]*$/;
 
 class App {
-  #carNames = [];
+  #car = {};
   #round = 0;
 
-  #checkCarNamesError() {
-    return this.#carNames.some((carName) => carName.length > 5);
+  async #readCarNames() {
+    let carNamesInput = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+
+    let carNames = carNamesInput.split(",");
+
+    carNames.forEach((carName) => {
+      this.#car[carName] = 0;
+    });
+
+    if (this.#checkCarNamesError()) {
+      throw new Error(ERROR_MESSAGE);
+    }
   }
 
-  #checkRoundError(round) {
-    return !NUMBER_REGEX.test(round);
+  #checkCarNamesError() {
+    return Object.keys(this.#car).some(
+      (carName) => !carName.length || carName.length > 5
+    );
   }
 
   async #readRound() {
@@ -27,22 +41,27 @@ class App {
     this.#round = Number(roundInput);
   }
 
-  async #readCarNames() {
-    let carNamesInput = await MissionUtils.Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
-    );
+  #checkRoundError(round) {
+    return !round || !NUMBER_REGEX.test(round);
+  }
 
-    this.#carNames = carNamesInput.split(",");
-
-    if (this.#checkCarNamesError()) {
-      throw new Error(ERROR_MESSAGE);
-    }
-
-    this.#readRound();
+  #executeRace() {
+    Object.entries(this.#car).forEach(([carName, move]) => {
+      if (Math.random() >= 0.4) {
+        this.#car[carName] = move + 1;
+      }
+    });
   }
 
   async run() {
     await this.#readCarNames();
+    await this.#readRound();
+
+    while (this.#round > 0) {
+      this.#executeRace();
+
+      this.#round--;
+    }
   }
 }
 
