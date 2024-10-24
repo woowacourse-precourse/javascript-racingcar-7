e27bdcd359200f3cd9ea3d5817e42793b9bbce77
moonseonghui <msh9980@naver.com>
2024-10-25 02:44:44 +0900

    feat(RacingGamePlay): 자동차 경주 게임 실행 메서드 구현

--- src/racinggame/RacingGame.js
+++ src/racinggame/RacingGame.js
@@ -7,6 +7,13 @@ class RacingGame {
     this.tryCount = tryCount;
   }
 
+  play() {
+    for (let i = 0; i < this.tryCount; i += 1) {
+      this.cars.forEach((car) => car.move());
+      this.printCurrentState();
+    }
+  }
+
   printCurrentState() {
     this.cars.forEach((car) => {
       const movePosition = getMovePostion(car);
