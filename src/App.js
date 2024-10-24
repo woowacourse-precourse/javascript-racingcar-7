c08c8790b7de1029155e3eef46403024cfca58a1
ChaDongmin <cdm1263@gmail.com>
2024-10-24 15:24:09 +0900

    feat(App): 게임 연결
    - 유틸 함수 파일에서 read, print를 불러와 io 객체에 할당
    
    - 유틸 함수 파일에서 random을 불러와 random 변수에 할당
    
    - 게임 매니저 클래스 인스턴스 추가
    
    - 게임 실행 및 try-catch로 에러 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { GameManager } from "./GameManager";
+import { read, print, random } from "./lib/utils";
+
 class App {
-  async run() {}
+  constructor() {
+    this.io = {
+      read,
+      print,
+    };
+    this.random = random;
+    this.gameManager = new GameManager(this.io, this.random);
+  }
+
+  async run() {
+    try {
+      await this.gameManager.play();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
 }
 
 export default App;
