c51b50e697aa731012ee03062de7b5b58dc971f3
daeun <gbajs754@gmail.com>
2024-10-24 17:52:43 +0900

    feat : 차수별 경주를 진행하는 로직 구현

--- src/App.js
+++ src/App.js
@@ -9,6 +9,23 @@ class App {
       "시도할 횟수는 몇 회인가요? \n"
     );
 
+    // 차수별 실행결과를 저장할 변수를 선언합니다.
+    const racersProcess = new Map();
+    const racers = firstUserInput.split(",");
+
+    racers.map((racer) => {
+      racersProcess.set(racer, "");
+    });
+
+    // 경주를 진행합니다.
+    for (let i = 0; i < secondUserInput; i++) {
+      // racer별로 전진 또는 멈추는 조건을 확인합니다.
+      for (let j = 0; j < racersProcess.size; j++) {
+        if (Random.pickNumberInRange(0, 9) >= 4) {
+          racersProcess.set(racers[j], racersProcess.get(racers[j]) + "-");
+        }
+      }
+    }
     const winner = [];
     Console.print(`최종 우승자 : ${winner.join(", ")}`);
   }
