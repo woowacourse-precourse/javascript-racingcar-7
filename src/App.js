960402b13c62c926c3209de0f15da3619fcc60b7
deokbin <deokbin@naver.com>
2024-10-24 11:10:34 +0900

    feat<App> : 유효성 검사 클래스 메서드 호출 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
 import NameValidation from './utils/NameValidation.js';
+import NumValidation from './utils/NumValidation.js';
 
 class App {
   async run() {
@@ -7,7 +8,10 @@ class App {
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
     );
     NameValidation.isValid(userInput.trim());
+    const countInput = await Console.readLineAsync('시도횟수를 입력해주세요.');
+    NumValidation.isValidNum(countInput.trim());
     Console.print(userInput);
+    Console.print(countInput);
   }
 }
 
