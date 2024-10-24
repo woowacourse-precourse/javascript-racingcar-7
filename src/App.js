833a2256197ff6fbae4be84ff9a5262fcaa620c6
seongmin <seongm22n@gmail.com>
2024-10-24 23:38:50 +0900

    refactor: 기능1
    - useInput으로 입력 기능들을 이동

--- src/App.js
+++ src/App.js
@@ -1,44 +1,34 @@
 import { Console, Random } from '@woowacourse/mission-utils';
-import { checkCarInputLength } from './errors/carError.js';
-import { checkEmptyInput } from './errors/commonError.js';
+import { carNameInputHandler, tryNumberHandler } from './utils/userInput.js';
 
 class App {
 	async run() {
-		const carNameInput = await Console.readLineAsync(
-			`경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n`
-		);
-		const tryNumber = await Console.readLineAsync(
-			`시도할 횟수는 몇 회인가요?\n`
-		);
-
-		const carList = carNameInput.split(',');
-		checkCarInputLength(carList);
-		checkEmptyInput(carNameInput);
-		checkEmptyInput(tryNumber);
+		const carNameList = await carNameInputHandler();
+		const tryNumber = await tryNumberHandler();
 
 		// 각 자동차 위치 초기화
-		const positions = Array(carList.length).fill(0);
+		const positions = Array(carNameList.length).fill(0);
 
-		const startRacingGame = (carList, tryNumber) => {
+		const startRacingGame = (carNameList, tryNumber) => {
 			for (let i = 0; i < tryNumber; i++) {
 				// 전진 : 무작위 값 0-9까지 값의 4 이상의 값 -> 랜덤숫자
-				for (let j = 0; j < carList.length; j++) {
+				for (let j = 0; j < carNameList.length; j++) {
 					const randomNumber = Random.pickNumberInRange(0, 9);
 					if (randomNumber >= 4) {
 						positions[j] += 1;
 					}
-					Console.print(`${carList[j]} : ${'-'.repeat(positions[j])}`);
+					Console.print(`${carNameList[j]} : ${'-'.repeat(positions[j])}`);
 				}
 				Console.print('');
 			}
 		};
 
 		// 게임 시작
-		startRacingGame(carList, parseInt(tryNumber));
+		startRacingGame(carNameList, parseInt(tryNumber));
 
 		// 경주 결과 출력
 		const maxPosition = Math.max(...positions);
-		const winners = carList.filter(
+		const winners = carNameList.filter(
 			(_, index) => positions[index] === maxPosition
 		);
 		Console.print(`최종 우승자 : ${winners.join(', ')}`);
