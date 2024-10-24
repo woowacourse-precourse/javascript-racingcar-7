5c81fdb0b3b2c75bc0604b67f7804a493081f846
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-10-24 13:28:24 +0900

    feat(Car): 자동차 클래스 기본 구조 생성

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.moveCount = 0;
+  }
+}
+
 class App {
   async run() {}
 }
