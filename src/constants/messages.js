7ebae714652bc7991169f477e6639e47a5dfd413
장정안 <wjddks96@naver.com>
2024-10-24 21:09:24 +0900

    feat: 유효성 검증에 대한 에러 메세지 추가

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -5,4 +5,27 @@ const GAME_MESSAGE = Object.freeze({
   FINAL_WINNER: '최종 우승자 :',
 });
 
-export { GAME_MESSAGE };
+const ERROR_PREFIX = Object.freeze('[ERROR]');
+
+const CAR_NAME_ERROR_MESSAGE = Object.freeze({
+  NO_INPUT: '자동차 이름을 입력해주세요.',
+  OUT_OF_RANGE: '자동차 이름은 5자 이하로 입력해주세요.',
+  ONE_CAR_NAME: '자동차를 한 대만 입력했습니다.',
+  CONTAIN_COMMA: '자동차 이름은 쉼표(,)를 포함할 수 없습니다.',
+  SEPARATE_COMMA: '자동차 이름은 쉼표(,)로 구분되어야 합니다.',
+  INCLUDE_SPACE: '자동차 이름에 공백은 포함할 수 없습니다.',
+  DUPLICATE_CAR_NAME: '자동차 이름이 중복되었습니다.',
+});
+
+const ATTEMPT_ERROR_MESSAGE = Object.freeze({
+  NO_INPUT: '시도할 횟수를 입력해주세요.',
+  NOT_A_NUMBER: '시도할 횟수는 숫자로 입력해주세요.',
+  OUT_OF_RANGE: '시도할 횟수는 0보다 크고 9보다 작아야 합니다.',
+});
+
+export {
+  GAME_MESSAGE,
+  CAR_NAME_ERROR_MESSAGE,
+  ATTEMPT_ERROR_MESSAGE,
+  ERROR_PREFIX,
+};
