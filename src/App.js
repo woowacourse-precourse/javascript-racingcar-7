9260124398effe0de2f61a8b916f3266514a9eac
ockko <ocko119@gmail.com>
2024-10-24 22:17:53 +0900

    car 클래스 만들기

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const carname = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-    const tryCount = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n")
-  }  
+    const carname = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    const tryCount = await MissionUtils.Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    );
+
+    class Car {
+      constructor(carname) {
+        this.carname = carname;
+        this.result = "";
+      }
+      go = () => {
+        if (MissionUtils.Random.pickNumberInRange(0, 9) >= 4) {
+          this.result += "-";
+        }
+      };
+    }
+  }
 }
 
 export default App;
