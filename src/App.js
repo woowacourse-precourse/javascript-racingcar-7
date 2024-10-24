f5c804b888c2dd486db6790a1f3239d4e47ecec3
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-10-24 13:36:35 +0900

    feat(check_input): 입력에 대한 간단한 검사 기능

--- src/App.js
+++ src/App.js
@@ -12,12 +12,28 @@ function getNames(str) {
   return names;
 }
 
+function checkNames(arr) {
+  let isError = false;
+
+  arr.forEach((name) => {
+    if (name.length() > 5) isError = true;
+  });
+  if (arr.length < 2) {
+    isError = true;
+  }
+}
+
 class App {
   async run() {
     const input = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
     );
     const names = getNames(input);
+    if (checkNames(names)) {
+      throw new Error('[ERROR]');
+    } else {
+      // TODO: 정상적인 입력에 대한 수행
+    }
   }
 }
 
