e3619134649b0ae4d726d5862fd8776a2e492746
oko_jin <dswvgw6412@naver.com>
2024-10-24 17:15:02 +0900

    refactor(testCase): inputTestCase.js 파일 src/testCase 디렉토리로 이동

--- src/testCase/inputTestCase.js
+++ src/testCase/inputTestCase.js
@@ -1,10 +1,15 @@
-import { errorMessages } from '../src/constant.js';
-import Car from '../src/Car.js';
+import { errorMessages } from '../constant.js';
+import Car from '../Car.js';
 
 export const carNameInputCase = [
   {
     input: 'js,java',
     expected: [new Car('js'), new Car('java')],
+    throwError: false,
+  },
+  {
+    input: 'js,java,py,ts',
+    expected: [new Car('js'), new Car('java'), new Car('py'), new Car('ts')],
     throwError: false,
   },
   {
@@ -23,8 +28,23 @@ export const carNameInputCase = [
     throwError: true,
   },
   {
+    input: 'js ,java',
+    expected: `${errorMessages.prefix} ${errorMessages.invalidCarInputName}`,
+    throwError: true,
+  },
+  {
+    input: 'js,java, py',
+    expected: `${errorMessages.prefix} ${errorMessages.invalidCarInputName}`,
+    throwError: true,
+  },
+  {
     input: 'js.java',
     expected: `${errorMessages.prefix} ${errorMessages.invalidDelimiter}`,
+    throwError: true,
+  },
+  {
+    input: 'js,,java',
+    expected: `${errorMessages.prefix} ${errorMessages.invalidCarInputName}`,
     throwError: true,
   },
 ];
