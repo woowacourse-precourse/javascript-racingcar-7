f037db873b6445065f9e6d6b4bf1d32cf233100e
siggu <m0_w_0m@naver.com>
2024-10-24 19:48:08 +0900

    feat(Validator): 자동차 이름 유효성 검사 기능 추가

--- src/validator/Validator.js
+++ src/validator/Validator.js
@@ -1,9 +1,14 @@
 class Validator {
   static isValidCarName(cars) {
     const carNameArr = cars.split(',');
+    const carSet = [...new Set(carNameArr)];
 
     if (carNameArr.some((car) => car.length > 5)) {
       throw new Error('[ERROR] 자동차의 이름은 5자 이하로 부여해야 합니다.');
+    } else if (carNameArr.length !== carSet.length) {
+      throw new Error('[ERROR] 자동차의 이름은 중복해서 부여할 수 없습니다.');
+    } else if (carNameArr.some((car) => car === '')) {
+      throw new Error('[ERROR] 자동차에 빈 이름을 부여할 수 없습니다.');
     }
   }
 }
