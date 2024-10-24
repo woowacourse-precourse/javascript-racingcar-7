0bd44b033d36c1653877f191eb4b26d830ebd962
smimdla0205 <sangmin0205@gmail.com>
2024-10-24 17:09:07 +0900

    feat: 시도 횟수 입력 기능(2번째) 구현 및 시도 횟수 유효성 검사 추가
    - utils/errors/car-name-validator.js 파일 삭제 후 utils/validator.js에서 유효성 검사를 한번에 관리하도록 변경
    - 시도 횟수가 숫자인지, 정수인지, 양의 정수인지 확인하는 로직 추가
    
    예시:
    입력: abc
    에러: [ERROR] 입력값은 숫자여야 합니다.
    
    입력: 3.5
    에러: [ERROR] 정수를 입력해주세요.
    
    입력: -1
    에러: [ERROR] 시도 횟수는 양의 정수여야 합니다.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import { validateCarNames } from "./utils/errors/car-name-validator.js"; // 유효성 검사 함수 가져오기
+import { validateCarNames, validateAttemptCount } from "./utils/validator.js";
 
 class App {
   async run() {
     try {
-      const input = await MissionUtils.Console.readLineAsync(
+      const carInput = await MissionUtils.Console.readLineAsync(
         "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
       );
-      const carNames = this.parseCarNames(input);
-      validateCarNames(carNames); // 유효성 검사 함수 호출
+      const carNames = this.parseCarNames(carInput);
+      validateCarNames(carNames);
+
+      const tryCountInput = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+      validateAttemptCount(tryCountInput);
     } catch (error) {
       MissionUtils.Console.print(`[ERROR] ${error.message}`);
       throw error;
     }
   }
-
   parseCarNames(input) {
     return input.split(",").map((name) => name.trim());
   }
---
+++ src/utils/validator.js
@@ -0,0 +1,29 @@
+// 자동차 이름 유효성 검사
+export function validateCarNames(carNames) {
+  const uniqueNames = new Set(carNames);
+  if (uniqueNames.size !== carNames.length) {
+    throw new Error("[ERROR] 자동차 이름은 중복될 수 없습니다.");
+  }
+
+  carNames.forEach((name) => {
+    if (name.length < 1 || name.length > 5) {
+      throw new Error("[ERROR] 자동차 이름은 1자 이상 5자 이하여야 합니다.");
+    }
+  });
+}
+
+// 시도 횟수 유효성 검사
+export function validateAttemptCount(input) {
+  const tryCount = Number(input.trim());
+  if (isNaN(tryCount)) {
+    throw new Error("[ERROR] 입력값은 숫자여야 합니다.");
+  }
+  if (!Number.isInteger(tryCount)) {
+    throw new Error("[ERROR] 정수를 입력해주세요.");
+  }
+  if (tryCount <= 0) {
+    throw new Error("[ERROR] 시도 횟수는 양의 정수여야 합니다.");
+  }
+
+  return tryCount;
+}
