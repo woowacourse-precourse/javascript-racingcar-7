ac95663813c0f984a97ecf2ee82222b6cf22797d
bunju20 <ghkd2271@naver.com>
2024-10-24 17:13:15 +0900

    feat: 자동차 이름 입력 시 유효성 확인 로직 구현 - 공백만 입력한 경우 - 쉼표의 갯수가 적절하지 않은경우(a,b,c,,, 등) - 자동차의 이름이 5개 이상인 경우에 대해 유효하지 않으면 Error를 던지는 로직 추가. try catch문은 도입하지 않음.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,22 @@ class App {
     const cars = await MissionUtils.Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
     );
-    console.log(cars);
+
+    const carArray = cars.split(',').map(car => car.trim());
+
+    if (carArray.length === 1) {
+      throw new Error('[ERROR] 인식된 자동차가 없습니다.');
+    }
+
+    const hasEmptyName = carArray.some(car => !car || car.trim() === '');
+    if (hasEmptyName) {
+      throw new Error('[ERROR] 쉼표가 적절히 작성되지 않았습니다.');
+    }
+
+    const hasLongName = carArray.some(car => car.length > 5);
+    if (hasLongName) {
+      throw new Error('[ERROR] 자동차 이름은 5자 이하만 가능합니다.');
+    }
   }
 }
 
