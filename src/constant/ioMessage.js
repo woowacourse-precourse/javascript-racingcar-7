e9e962dc0442bef97a8698010bb7f95a3b4bca49
san1313 <skan134679@gmail.com>
2024-10-24 17:14:49 +0900

    feat(input): 사용자 입력 기능, 상수, 테스트 작성

---
+++ src/constant/ioMessage.js
@@ -0,0 +1,8 @@
+const IO_MESSAGE = Object.freeze({
+  INPUT_CAR_NAME: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  INPUT_TRY_COUNT: '시도할 횟수는 몇 회인가요?\n',
+  OUTPUT_ANNOUNCE_MESSAGE: '실행 결과',
+  OUTPUT_FINAL_RESULT: '최종 우승자 : ',
+});
+
+export default IO_MESSAGE;
