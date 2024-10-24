0c01ada2e2fc6f8075feb3ada3479da86d123d18
jungwho <jungwho0328@gmail.com>
2024-10-24 20:12:51 +0900

    feat(input) : Add reading round feature

--- src/gameSetting/features/handleInput.js
+++ src/gameSetting/features/handleInput.js
@@ -5,3 +5,7 @@ export const readUsers = () => {
     "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
   );
 };
+
+export const readRound = () => {
+  return Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+};
