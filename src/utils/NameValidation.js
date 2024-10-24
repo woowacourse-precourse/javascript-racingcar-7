070b8af28acbb9e36383a112e85e38ba805ab7b1
deokbin <deokbin@naver.com>
2024-10-24 09:48:55 +0900

    refactor<NameValidation>: 클래스 컴포넌트로 변경 함수형 -> 클래스 컴포넌트로 변경

--- src/utils/NameValidation.js
+++ src/utils/NameValidation.js
@@ -5,26 +5,33 @@ import {
   ONE_CAR_NAME_ERROR,
 } from '../constants/Error';
 
-export default function isValidName(input) {
-  if (input === '') {
-    throw new Error(EMPTY_CAR_NAME_ERROR);
-  }
-  const carList = input.split(',');
-  carList.forEach(car => {
-    if (car.length > 5) {
-      throw new Error(MORE_THAN_FIVE_LETTERS);
+class NameValidation {
+  isValid(carNames) {
+    if (carNames === '') {
+      throw new Error(EMPTY_CAR_NAME_ERROR);
     }
-  });
-  if (carList.length > 1) {
-    const carSet = new Set(carList);
 
-    if (carList.length !== carSet.size) {
-      throw new Error(SAME_CAR_NAME_ERROR);
+    const nameList = carNames.split(',');
+    nameList.forEach(name => {
+      if (name.length > 5) {
+        throw new Error(MORE_THAN_FIVE_LETTERS);
+      }
+    });
+
+    if (nameList.length > 1) {
+      const nameSet = new Set(nameList);
+
+      if (nameList.length !== nameSet.size) {
+        throw new Error(SAME_CAR_NAME_ERROR);
+      }
+
+      return true;
+    }
+    if (nameList.length === 1) {
+      throw new Error(ONE_CAR_NAME_ERROR);
     }
     return true;
   }
-  if (carList.length === 1) {
-    throw new Error(ONE_CAR_NAME_ERROR);
-  }
-  return true;
 }
+
+export default NameValidation;
