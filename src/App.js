a8868b4b18db3c5b65e5b3556a347cfb426c58e6
parkseyoung <hellosy76@naver.com>
2024-10-24 22:36:39 +0900

    feat: 사용자에게 입력받기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
 import { shouldMoveForward } from "./Random.js";
 
 class App {
-  async run() {}
+  async run() {
+    const carlist = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분).\n"
+    );
+    const repeat = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+
+    try {
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
 }
 
 export default App;
