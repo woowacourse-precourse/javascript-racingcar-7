93945ff38341284c396d4fde13461ae4b018e212
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-10-24 11:48:04 +0900

    refactor: 사용되지 않는 모듈 호출 삭제
    - 무의미한 호출이기에 삭제

--- src/utils/inputHandler.js
+++ src/utils/inputHandler.js
@@ -1,5 +1,4 @@
 import { Console } from '@woowacourse/mission-utils';
-import Validator from '../validators/Validator.js';
 
 export const getNameInput = async () => {
   const nameString = await Console.readLineAsync(
