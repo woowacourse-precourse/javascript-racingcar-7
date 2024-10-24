3296701e70060d3e6a37da742543836d431c8404
choeeun-in <cei8645@naver.com>
2024-10-25 02:56:25 +0900

    feat: 우승자 출력하는 기능

--- src/App.js
+++ src/App.js
@@ -4,49 +4,74 @@ import {MissionUtils} from "@woowacourse/mission-utils";
 class App {
   async run() {
     try {
-      //자동차 이름 부여 기능
-      const CAR_NAME = await Console.readLineAsync(
-      "자동차 이름은 무엇입니까?"
-    );
+      // 자동차 이름 부여 기능
+      const CAR_NAME = await Console.readLineAsync("자동차 이름은 무엇입니까?");
+      const CAR_NAMES = this.Separator(CAR_NAME);
 
-      //자동차 이름 검사
-      this.Separator(CAR_NAME);
+      // 이동 횟수 입력
+      const FORWARD_TIME = await Console.readLineAsync(
+        "이동 횟수를 입력하세요."
+      );
 
-      //이동 횟수 입력받기
-      const FORWARD_TIME = await Console.readLineAsync("이동 횟수를 입력하세요.");
+      // 이동 로직 실행
+      const CAR_POSITIONS = this.moveCar(CAR_NAMES, Number(FORWARD_TIME));
 
-      //이동 로직 실행
-      this.moveCar(Number(FORWARD_TIME));
+      // 우승자 로직 실행
+      this.victoryCar(CAR_NAMES, CAR_POSITIONS);
     } catch (error) {
       Console.print(error.message);
+      throw error;
+    }
+  }
+
+  // 자동차 이름 유효성 검사
+  Separator(CAR_NAME) {
+    const CAR_NAMES = CAR_NAME.split(",");
+
+    // 자동차 이름 글자수 제한
+    CAR_NAMES.forEach((name) => {
+      if (name.length > 5) {
+        throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
       }
-    }
+    });
 
-    //자동차 이름 유효성 검사
-    Separator(CAR_NAME) {
-      const CAR_NAMES = CAR_NAME.split(',');
+    return CAR_NAMES;
+  }
 
-      //자동차 이름 5자 이하 검사
-      CAR_NAMES.forEach(name => {
-        if (name.length >= 6) {
-          throw new Error("[Error] 자동차 이름은 5자 이하만 가능합니다.")
-        }
-      });
-    }
+  // 이동 로직
+  moveCar(CAR_NAMES, FORWARD_TIME) {
+    const CAR_POSITIONS = CAR_NAMES.reduce((acc, name) => {
+      acc[name] = 0;
+      return acc;
+    }, {});
 
-    //이동 로직
-    moveCar(FORWARD_TIME) {
+    CAR_NAMES.forEach((name) => {
       for (let i = 0; i < FORWARD_TIME; i++) {
         const RANDOM_NUM = MissionUtils.Random.pickNumberInRange(0, 9);
         if (RANDOM_NUM >= 4) {
-          Console.print(`이동 성공! 랜덤 숫자: ${RANDOM_NUM}`);
-        } else {
-          Console.print(`이동 실패. 랜덤 숫자: ${RANDOM_NUM}`);
+          CAR_POSITIONS[name] += 1; // 이동 성공 시 위치 증가
         }
-    }
+      }
+      // 각 자동차의 이동 결과 출력
+      Console.print(`${name} : ${"-".repeat(CAR_POSITIONS[name])}`);
+    });
+
+    return CAR_POSITIONS;
+  }
+
+  // 우승자 로직
+  victoryCar(CAR_NAMES, CAR_POSITIONS) {
+    // 자동차의 최대값을 찾기
+    const maxPosition = Math.max(...Object.values(CAR_POSITIONS));
+
+    // 우승자 찾기
+    const WINNER = CAR_NAMES.filter(
+      (name) => CAR_POSITIONS[name] === maxPosition
+    );
+
+    // 쉼표로 구분하여 우승자 출력
+    Console.print(`최종 우승자 : ${WINNER.join(", ")}`);
   }
 }
-
-
 
 export default App;
