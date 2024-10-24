286abd5477792e65025002e3da16789dd1d51607
deokbin <deokbin@naver.com>
2024-10-24 10:41:10 +0900

    feat<Error>: 에러 상수 추가

--- src/constants/Error.js
+++ src/constants/Error.js
@@ -4,3 +4,6 @@ export const ONE_CAR_NAME_ERROR =
 export const MORE_THAN_FIVE_LETTERS =
   '[ERROR] 자동차 이름은 5글자를 넘어갈 수 없습니다.';
 export const SAME_CAR_NAME_ERROR = '[ERROR] 자동차 이름은 중복될 수 없습니다.';
+export const INVALID_NUMBER = '[ERROR] 시도횟수는 0 보다 큰 숫자여야 합니다.';
+export const TOO_BIG_NUMBER =
+  '[ERROR] 너무 큰 숫자를 입력했습니다. 20 이하의 숫자로 입력해야 합니다.';
