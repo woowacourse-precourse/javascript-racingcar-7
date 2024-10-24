15f02e735bcc1dc18cb59105bfbbd9648ac1d683
Yurin Wang <king_1994@naver.com>
2024-10-24 15:33:43 +0900

    feat: 유효한 자동차 이름 검증 로직 추가 - 자동차 이름은 한글과 영어만 가능하다. - 자동차 이름에 숫자와 특수문자가 포함된 경우 에러를 반환한다.

--- src/validators/validator.js
+++ src/validators/validator.js
@@ -1,5 +1,5 @@
 export function checkCommaSaperatedNames(input) {
-  const pattern = /^[a-zA-Z0-9가-힇,\s]+$/;
+  const pattern = /^[a-zA-Z0-9가-힣,\s]+$/;
 
   if (!pattern.test(input)) {
     throw new Error('입력 형식이 올바르지 않습니다. 쉼표(,)로만 구분해주세요.');
@@ -12,9 +12,18 @@ function checkCarNameLength(name) {
   }
 }
 
+function checkAllowedCharacters(name) {
+  const pattern = /^[a-zA-Z가-힣\s]+$/;
+
+  if (!pattern.test(name)) {
+    throw new Error('자동차 이름은 한글과 영어만 가능합니다.');
+  }
+}
+
 export function checkCarNames(carNames) {
   const validateCarNames = carNames.map((name) => {
     checkCarNameLength(name);
+    checkAllowedCharacters(name);
     return name;
   });
   return validateCarNames;
