2082e4268daee1573fe5dbbc79262878e9a6a5c4
jongbin26 <010jjj@naver.com>
2024-10-24 16:11:40 +0900

    refactor(Game): Airbnb Style Guide 적용
    - Airbnb Style Guide에 맞게 코드를 수정했습니다.
    - map 함수는 반환값이 필요하므로 forEach 문으로 변경했습니다.

--- src/Game.js
+++ src/Game.js
@@ -1,16 +1,19 @@
 import { Console, Random } from '@woowacourse/mission-utils';
 import { initRecord } from './utils/initRecord.js';
 import { GAME, CRITERIA_NUMBER } from './constants/index.js';
+
 export class Game {
   constructor(cars, count) {
     this.record = initRecord(cars);
     this.count = count;
   }
+
   update(movingCars) {
-    movingCars.map((car) => {
+    movingCars.forEach((car) => {
       this.record[car] += 1;
     });
   }
+
   play() {
     Console.print(GAME.START_MESSAGE);
     for (let i = 0; i < this.count; i++) {
@@ -18,21 +21,25 @@ export class Game {
     }
     return this.getWinner();
   }
+
   move() {
     const movingCars = [];
-    Object.keys(this.record).map((key) => {
-      Random.pickNumberInRange(0, 9) >= CRITERIA_NUMBER && movingCars.push(key);
+    Object.keys(this.record).forEach((key) => {
+      if (Random.pickNumberInRange(0, 9) >= CRITERIA_NUMBER) {
+        movingCars.push(key);
+      }
     });
     this.update(movingCars);
     this.print();
   }
 
   print() {
-    Object.entries(this.record).map(([car, routeLength]) => {
+    Object.entries(this.record).forEach(([car, routeLength]) => {
       Console.print(car + GAME.SAPCE_COLON + GAME.ROUTE.repeat(routeLength));
     });
     Console.print(GAME.NEW_LINE);
   }
+
   getWinner() {
     const maxValue = Math.max(...Object.values(this.record));
     return Object.keys(this.record).filter(
