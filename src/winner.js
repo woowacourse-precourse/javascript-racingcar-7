b9ddf79c1849f4748a37411dd9f26189fd15eda0
yeabbbbb <b08009@naver.com>
2024-10-24 18:16:47 +0900

    refactor(winner): Use Array.filter for cleaner selection logic

--- src/winner.js
+++ src/winner.js
@@ -1,13 +1,4 @@
-import { Console } from '@woowacourse/mission-utils';
-
 export function getWinner(carList, racingStatus) {
   const maxDist = Math.max(...racingStatus);
-  const winnerList = []
-
-  for (const idx of Array(carList.length).keys()) {
-    if (racingStatus[idx] === maxDist) {
-      winnerList.push(carList[idx]);
-    }
-  }
-  return winnerList;
+  return carList.filter((_, idx) => racingStatus[idx] === maxDist);
 }
