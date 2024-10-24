d6c6ca92c490ec47fb68f616c6a36b4b6f54c954
eunoia-jaxson <eunoia@handong.ac.kr>
2024-10-24 11:56:18 +0900

    feat(input): 사용자 입력 받기
    사용자로부터 자동차의 부여할 이름과 시도할 횟수를 입력받습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from '@woowacourse/mission-utils';
+import { SYSTEM_MESSAGE } from './constants.js';
+
 class App {
-  async run() {}
+  async run() {
+    await this.enterInput();
+  }
+
+  async enterInput() {
+    const namesString = await Console.readLineAsync(SYSTEM_MESSAGE.ENTER_NAME);
+    this.names = namesString.split(',');
+    this.count = await Console.readLineAsync(SYSTEM_MESSAGE.ENTER_COUNT);
+  }
 }
 
 export default App;
