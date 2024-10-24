1bd1dd0928e85990b9bdab4693ba959ef9e960e9
jeongeunLee <leeju1009@gmail.com>
2024-10-25 03:56:23 +0900

    feat(app): 유저에게 자동차 이름을 질문하는 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
   async run() {}
+
+  static async askCarNames() {
+    const carNames = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+    );
+
+    return carNames;
+  }
 }
 
 export default App;
