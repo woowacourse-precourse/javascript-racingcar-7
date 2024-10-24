cf3b1a138fa08bf2a4b28446b595983b439026bc
hyunyeee <5004ohy@naver.com>
2024-10-24 22:33:31 +0900

    feat(output): 최종 우승자를 출력한다

---
+++ src/handler/PrintWinners.js
@@ -0,0 +1,17 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const PRINT_WINNER_MESSAGE = '최종 우승자 : ';
+
+export class PrintWinners {
+  printWinners(cars) {
+    const winnerNamesArr = this.#getWinners(cars);
+    const winnerNames = winnerNamesArr.join(', ');
+    MissionUtils.Console.print(PRINT_WINNER_MESSAGE + winnerNames);
+  }
+
+  #getWinners(cars) {
+    const maxLength = cars.reduce((max, car) => Math.max(max, car.getLength()), 0);
+    const winnerCars = cars.filter((car) => car.getLength() === maxLength);
+    return winnerCars.map((car) => car.getName());
+  }
+}
