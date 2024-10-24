4c63ec43bb5c7c2936246329a0730ec8bddd28d0
subsub-e <ab13696802@gmail.com>
2024-10-24 18:41:09 +0900

    feat(hasDuplicateName) : hasDuplicateName 함수 테스트 코드

---
+++ __tests__/hasDuplicateName.test.js
@@ -0,0 +1,13 @@
+import { hasDuplicateName } from '../src/utils/hasDuplicateName.js';
+
+describe('hasDuplicateName 함수 테스트 : ', () => {
+    
+    test('중복된 이름이 있는 경우 DUPLICATE_NAME 에러를 발생시킨다.', () => {
+        expect(hasDuplicateName(['subsub-e', 'subsub-e'])).toThrowError('[ERROR] 중복된 이름이 있어요. 각 자동차의 이름은 고유해야 합니다.');
+    });
+
+    test('중복된 이름이 없는 경우 에러를 발생시키지 않는다.', () => {
+        expect(hasDuplicateName(['subsub-e', 'hiiiiiie', '+21@)'])).not.toThrowError();
+    });
+
+})
