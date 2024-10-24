ae9bc56a0bab105adf0632f112a3f9b01729780f
TaewoooongHeo01 <chamjoeun0111@gmail.com>
2024-10-25 01:07:40 +0900

    test(input): 문자열 입력 파싱함수 테스트 수정
    추후 테스트케이스가 늘어남에 따라 같은 테스트코드가 반복되는 것을 방지하기 위해 test.each() 를 사용하여 파라미터화 테스팅

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -26,56 +26,60 @@ const getLogSpy = () => {
   return logSpy;
 };
 
-describe('자동차 경주', () => {
-  test.skip('기능 테스트', async () => {
-    // given
-    const MOVING_FORWARD = 4;
-    const STOP = 3;
-    const inputs = ['pobi,woni', '1'];
-    const logs = ['pobi : -', 'woni : ', '최종 우승자 : pobi'];
-    const logSpy = getLogSpy();
+describe(
+  '자동차 경주',
+  () => {
+    test.skip('기능 테스트', async () => {
+      // given
+      const MOVING_FORWARD = 4;
+      const STOP = 3;
+      const inputs = ['pobi,woni', '1'];
+      const logs = ['pobi : -', 'woni : ', '최종 우승자 : pobi'];
+      const logSpy = getLogSpy();
 
-    mockQuestions(inputs);
-    mockRandoms([MOVING_FORWARD, STOP]);
+      mockQuestions(inputs);
+      mockRandoms([MOVING_FORWARD, STOP]);
 
-    // when
-    const app = new App();
-    await app.run();
+      // when
+      const app = new App();
+      await app.run();
 
-    // then
-    logs.forEach((log) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+      // then
+      logs.forEach((log) => {
+        expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+      });
     });
-  });
 
-  test.skip('예외 테스트', async () => {
-    // given
-    const inputs = ['pobi,javaji'];
-    mockQuestions(inputs);
+    test.skip('예외 테스트', async () => {
+      // given
+      const inputs = ['pobi,javaji'];
+      mockQuestions(inputs);
 
-    // when
-    const app = new App();
+      // when
+      const app = new App();
 
-    // then
-    await expect(app.run()).rejects.toThrow('[ERROR]');
-  });
+      // then
+      await expect(app.run()).rejects.toThrow('[ERROR]');
+    });
+  },
 
-  test('문자열 올바른 경우 , 를 기준으로 배열을 반환', async () => {
-    //given
-    const inputs = 'pobi,woni,jun';
+  describe('입력모듈 테스트', () => {
+    test.each([
+      ['pobi,woni,jun', ['pobi', 'woni', 'jun']],
+      ['pobi, woni ,jun', ['pobi', ' woni ', 'jun']],
+      ['', new Error('[ERROR]')],
+    ])('parseCarString(%s)', (inputs, expected) => {
+      //given
+      const inputString = inputs;
 
-    //when
-    const cars = parseCarString(inputs);
-
-    //then
-    await expect(cars).toEqual(['pobi', 'woni', 'jun']);
-  });
-
-  test('문자열의 길이가 0 인 경우 예외를 발생', async () => {
-    //given
-    const inputs = '';
-
-    //when, then
-    await expect(() => parseCarString(inputs)).toThrow('[ERROR]');
-  });
-});
+      //when
+      if (expected instanceof Error) {
+        //then
+        expect(() => parseCarString(inputString)).toThrow(expected.message);
+      } else {
+        //then
+        expect(parseCarString(inputString)).toEqual(expected);
+      }
+    });
+  }),
+);
