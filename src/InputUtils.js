b5f7eabc2a6c4318514418d5c1911db13980c5d9
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-10-24 22:57:43 +0900

    style: 입력 문구의 끝에 줄 바꿈을 추가한다

--- src/InputUtils.js
+++ src/InputUtils.js
@@ -1,7 +1,7 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 export async function getCarName() {
-  return await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+  return await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
 }
 
 export function splitCarName(name) {
@@ -9,5 +9,5 @@ export function splitCarName(name) {
 }
 
 export async function getAttemptCount() {
-  return await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+  return await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n\n');
 }
