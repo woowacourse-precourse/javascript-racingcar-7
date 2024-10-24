133cc350d08b9cd451855660f71df7aa871e8bd4
eungyo <eungyo0965@gmail.com>
2024-10-24 03:41:12 +0200

    fix(input) : Fix the calling format of Input Function past : callback function / updated : return Promise and use then Edit the problem of sending wrong callback parameters To match the expected format of test code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,20 @@
 module.exports = {
-    extends: ['airbnb-base', 'prettier'],
-    plugins: ['prettier'],
-    env: {
-      browser: true,
-      es2021: true,
-      node: true,
-    },
-    rules: {
-      'prettier/prettier': 'error',
-      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-      'import/extensions': ['error', 'ignorePackages'],
-      'consistent-return': 'off',
-      'no-underscore-dangle': 'off',
-      'no-alert': 'warn',
-      // 상수명을 SNAKE_CASE로 강제
-      'camelcase': ['error', { properties: 'never', ignoreDestructuring: false }],
-    },
-  };
-  
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier', 'jest'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  rules: {
+    'class-methods-use-this': 'off',
+    'prettier/prettier': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/extensions': ['error', 'ignorePackages'],
+    'consistent-return': 'off',
+    'no-underscore-dangle': 'off',
+    'no-alert': 'warn',
+    // 상수명을 SNAKE_CASE로 강제
+    camelcase: ['error', { properties: 'never', ignoreDestructuring: false }],
+  },
+};
--- src/App.js
+++ src/App.js
@@ -2,18 +2,35 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
-    return new Promise((resolve, reject) => {
-      MissionUtils.Console.readLineAsync(
-        '자동차 이름을 입력하세요: ',
-        (input) => {
-          const names = input.split(',');
-          if (names.some((name) => name.length > 5)) {
-            reject(new Error('[ERROR] 자동차 이름은 5자 이하만 가능합니다.'));
-          } else {
-            resolve(names);
-          }
-        },
-      );
+    try {
+      const names = await App.getCarNames();
+      const trials = await App.getMoveCount();
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  static getCarNames() {
+    return MissionUtils.Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) ',
+    ).then((input) => {
+      const names = input.split(',');
+      if (names.some((name) => name.length > 5)) {
+        throw new Error('[ERROR] 자동차 이름은 5자 이하만 가능합니다.');
+      }
+      return names;
+    });
+  }
+
+  static getMoveCount() {
+    return MissionUtils.Console.readLineAsync(
+      '시도할 횟수는 몇 회인가요?',
+    ).then((input) => {
+      const trials = parseInt(input, 10);
+      if (Number.isNaN(trials) || trials <= 0) {
+        throw new Error('[ERROR] 이동 횟수는 1 이상의 정수여야 합니다.');
+      }
+      return trials;
     });
   }
 }
