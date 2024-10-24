12c075415765a604fb1a927d425342a79e4b4150
subsub-e <ab13696802@gmail.com>
2024-10-24 18:42:52 +0900

    feat(constant) : input, output, error 상수 정의

---
+++ src/constants/errorConstants.js
@@ -0,0 +1,7 @@
+export const ERROR_MESSAGES = {
+    CAR_NAME_TOO_LONG: '[ERROR] 이름이 너무 길어요. 자동차 이름은 5자 이하로 입력해주세요.',
+    DUPLICATE_NAME: '[ERROR] 중복된 이름이 있어요. 각 자동차의 이름은 고유해야 합니다.',
+    EMPTY_INPUT_NAME: '[ERROR] 빈 문자열은 유효하지 않습니다. 자동차 이름을 입력해주세요.',
+    EMPTY_INPUT_ROUND: '[ERROR] 숫자를 입력해 주세요: 빈 입력은 유효하지 않습니다.',
+    INVALID_ROUND_NUMBER: '[ERROR] 유효하지 않은 시도 횟수입니다: 숫자만 입력해 주세요.',
+};
---
+++ src/constants/outputConstants.js
@@ -0,0 +1,4 @@
+export const OUTPUT = {
+    RESULT: '실행 결과\n',
+    WINNER: '최종 우승자 : ',
+};
