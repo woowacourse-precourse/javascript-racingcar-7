6446a14705fb43ac3f468a40af79b4babf4c82f2
deokbin <deokbin@naver.com>
2024-10-24 09:49:31 +0900

    refactor<App,NameValidation.test>:  클래스 컴포넌트로 변경

--- __tests__/NameValidation.test.js
+++ __tests__/NameValidation.test.js
@@ -1,37 +1,48 @@
 /* eslint-disable */
-
-import App from '../src/App.js';
-import isValidName from '../src/utils/NameValidation.js';
+import NameValidation from '../src/utils/NameValidation.js';
 import {
   EMPTY_CAR_NAME_ERROR,
   ONE_CAR_NAME_ERROR,
-  INVALID_SEPERATOR,
   MORE_THAN_FIVE_LETTERS,
   SAME_CAR_NAME_ERROR,
 } from '../src/constants/Error.js';
 
 describe('유효성 검사 테스트', () => {
+  let nameValidation;
+
+  beforeEach(() => {
+    nameValidation = new NameValidation();
+  });
+
   test('문자열 여러개를 쉼표로 구분지어 입력하면 그대로 반환한다.', () => {
     const validInput = 'pobi,woni';
-    expect(isValidName(validInput)).toBe(true);
+    expect(nameValidation.isValid(validInput)).toBe(true);
   });
 });
 
 // Error Case
 describe('유효하지 않은 값 테스트', () => {
+  let nameValidation;
+
+  beforeEach(() => {
+    nameValidation = new NameValidation();
+  });
+
   test('빈 값일 경우 에러를 반환한다.', () => {
-    expect(() => isValidName('')).toThrow(EMPTY_CAR_NAME_ERROR);
+    expect(() => nameValidation.isValid('')).toThrow(EMPTY_CAR_NAME_ERROR);
   });
   test('한 개의 값만 입력한 경우 에러를 반환한다.', () => {
-    expect(() => isValidName('john')).toThrow(ONE_CAR_NAME_ERROR);
+    expect(() => nameValidation.isValid('john')).toThrow(ONE_CAR_NAME_ERROR);
   });
 
   test('이름이 다섯글자 이상인 경우 에러를 반환한다.', () => {
-    expect(() => isValidName('Catherine, John')).toThrow(
+    expect(() => nameValidation.isValid('Catherine, John')).toThrow(
       MORE_THAN_FIVE_LETTERS,
     );
   });
   test('중복된 이름이 있을 경우 에러를 반환한다.', () => {
-    expect(() => isValidName('John,John')).toThrow(SAME_CAR_NAME_ERROR);
+    expect(() => nameValidation.isValid('John,John')).toThrow(
+      SAME_CAR_NAME_ERROR,
+    );
   });
 });
--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Console } from '@woowacourse/mission-utils';
-import isValidName from './NameValidation.js';
+import NameValidation from './utils/NameValidation.js';
 
 class App {
   async run() {
     const userInput = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
     );
-    isValidName(userInput.trim());
+    NameValidation.isValid(userInput.trim());
     Console.print(userInput);
   }
 }
