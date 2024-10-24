eea5681627bffd10be7e69cba864e86539b74552
luii0415 <becky45210193@gmail.com>
2024-10-24 18:50:32 +0900

    feat : create
    자동차 이동 함수 생성
    loop 에서 실행되어야 할 기능 주석으로 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async run() {
+    const rolling_number = () => {
+      return MissionUtils.Random.pickNumberInRange(0, 9);
+    };
+    const plus_point = (cars) => {
+      const number_of_cars = cars.length();
+      let number = 0;
+      for (let i = 0; i < number_of_cars; i++) {
+        number = rolling_number();
+        if (number >= 4) {
+          cars_score[i] += 1;
+        }
+      }
+    };
+    //
+
     // 자동차 이름 입력 및 이동 횟수 입력
     Console.print(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
@@ -8,15 +23,21 @@ class App {
     let name_input = await Console.readLineAsync("");
     let car_names = name_input.split(",");
     car_names = car_names.map((name) => name.trim());
+    let cars_score = [0] * car_names.length();
     // 에러 조건 1 : 입력된 이름이 5자를 넘어가는가?
     for (let name of car_names) {
       if (name.length > 5) {
         throw new Error("[ERROR]");
       }
     }
-
     Console.print("시도할 횟수는 몇 회인가요?");
     const loop = await Console.readLineAsync("");
+
+    // 자동차 이동
+    for (i = 0; i < loop; i++) {
+      // plus point 호출
+      // print point 호출 (전진한 거리만큼 - 호출하는 함수)
+    }
   }
 }
 
