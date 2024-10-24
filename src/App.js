5734cfad5d969895c7bd40d986c460e3e7563572
jongbin26 <010jjj@naver.com>
2024-10-24 16:05:31 +0900

    feat(App): run 함수 구현
    - 만든 입출력 util 함수를 통해 게임 정보를 입력받고, 결과를 출력합니다.
    - Game 객체를 만들어 게임을 플레이합니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { getGameInput, printGameResult } from './utils/ioHandler.js';
+import { Game } from './Game.js';
+
 class App {
-  async run() {}
+  async run() {
+    const { carString, count } = await getGameInput();
+    const game = new Game(carString, count);
+    const gameResult = game.play();
+    printGameResult(gameResult);
+  }
 }
 
 export default App;
