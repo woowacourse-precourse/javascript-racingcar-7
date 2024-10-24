09bf1cfcdcb6677485fdbde8ef4ac5d652465581
hyunyeee <5004ohy@naver.com>
2024-10-24 20:11:16 +0900

    feat(validate): 공백을 입력하면 에러를 발생시킨다

--- src/handler/InputName.js
+++ src/handler/InputName.js
@@ -1,9 +1,20 @@
-import { ReadLine } from "./ReadLine.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
-const INPUT_NAME_MESSAGE = '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)';
+const INPUT_NAME_MESSAGE = '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) ';
+const LENGTH_ERROR_MESSAGE = '[ERROR] 이름은 1~5자로 입력해주세요.';
 
 export class InputName {
   async read() {
-    return await new ReadLine().readInput(INPUT_NAME_MESSAGE);
+    const input = await MissionUtils.Console.readLineAsync(INPUT_NAME_MESSAGE);
+    this.#validateInput(input);
+    return input;
+  }
+
+  #validateInput(input) {
+    this.#checkEmpty(input);
+  }
+
+  #checkEmpty(input) {
+    if (input.trim() === '') throw new Error(LENGTH_ERROR_MESSAGE);
   }
 }
--- src/handler/ReadLine.js
+++
@@ -1,7 +0,0 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
-
-export class ReadLine {
-  async readInput(inputMessage) {
-    return await MissionUtils.Console.readLineAsync(inputMessage);
-  }
-}
