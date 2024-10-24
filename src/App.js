28728695fca9e80d71074e5cb1e5f3d87cff827c
eunseopi <asxzqw12@naver.com>
2024-10-24 13:10:34 +0900

    Feat: Initialize basic class structure for racing car game
    Create core classes for racing car game:
    
    Car: Represents a car entity with name and position.
    RacingCar: Handles main game logic, including playing the game and determining winners.
    InputValidator: Utility class for input validation.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,55 @@
+import {MissionUtils} from "@woowacourse/mission-utils/src";
+
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.position = 0;
+  }
+
+  // 전진 메서드
+  move(randomNumber) {
+
+  }
+  // 현재 상태 반환 메서드
+  getCurrentPosition() {
+
+  }
+}
+
+class RacingCar {
+  constructor(cars, attempts) {
+
+  }
+  // 게임 실행 메서드
+
+  play () {
+
+  }
+
+  // 우승자 판별 메서드
+  getWinners() {
+
+  }
+}
+
+class InputValidator {
+  // 자동차 이름 유효성 검사
+  static validateCarNames(names){
+
+  }
+  // 시도 횟수 유효성 검사
+  static validateAttempts(attempts){
+
+  }
+}
+
+
+
 class App {
-  async run() {}
+  async run() {
+  }
+
+
 }
 
 export default App;
