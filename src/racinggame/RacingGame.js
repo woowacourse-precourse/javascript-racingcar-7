2c3adda0d84053ae96bc8288c1e6402ec485e9ca
moonseonghui <msh9980@naver.com>
2024-10-25 02:36:23 +0900

    feat(printCurrentState): 게임 중 자동차들의 현재 전진 생태를 출력하는 메서드 구현

--- src/racinggame/RacingGame.js
+++ src/racinggame/RacingGame.js
@@ -1,7 +1,18 @@
+import getMovePostion from '../utils/getMovePosition';
+import { printCarsMoving, printNewLine } from '../utils/ioModule';
+
 class RacingGame {
   constructor(cars, tryCount) {
     this.cars = cars;
     this.tryCount = tryCount;
+  }
+
+  printCurrentState() {
+    this.cars.forEach((car) => {
+      const movePosition = getMovePostion(car);
+      printCarsMoving(car.getName(), movePosition);
+    });
+    printNewLine();
   }
 }
 
