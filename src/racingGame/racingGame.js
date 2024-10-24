0552732a4b4e5ccc627134ca24541c2773534a8c
jungwho <jungwho0328@gmail.com>
2024-10-24 20:24:22 +0900

    feat(game) : Add playing racing game feature

---
+++ src/racingGame/racingGame.js
@@ -0,0 +1,22 @@
+import { playRoundGame, setRoundData, getWinner } from "./features/features.js";
+import {
+  printTitle,
+  printRound,
+  printWinner,
+} from "./features/handleOutput.js";
+
+export const playRacingGame = (userList, roundNum) => {
+  // 1. Set round data
+  let roundData = setRoundData(userList);
+
+  // 2. Play round game
+  printTitle();
+  for (let round = 0; round < roundNum; round++) {
+    roundData = playRoundGame(roundData);
+    printRound(roundData);
+  }
+
+  // 3. Get winner
+  let winnerList = getWinner(roundData);
+  printWinner(winnerList);
+};
