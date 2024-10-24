7535e18c373506cbad024d3e90038f8dcf4c4517
rubberchin <88rubber@gmail.com>
2024-10-24 17:59:05 +0900

    test($tests): 공동 우승자가 있을 때를 테스트하는 코드 추가
    - 공동 우승자가 2명 있을 때 결과에 2명 모두 출력되는지 테스트
    - 2명의 이름이 쉼표로 구분되는지 테스트

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -46,6 +46,27 @@ describe("자동차 경주", () => {
     });
   });
 
+  test("공동 우승자가 있을 때", async () => {
+    // given
+    const MOVING_FORWARD = 4;
+    const STOP = 3;
+    const inputs = ["quick,silvy,stone", "1"];
+    const logs = ["quick : -", "silvy : ", "stone : -", "최종 우승자 : quick, stone"];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([MOVING_FORWARD, STOP, MOVING_FORWARD]);
+
+    // when
+    const app = new App();
+    await app.run();
+
+    // then
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
   test("예외 테스트", async () => {
     // given
     const inputs = ["pobi,javaji"];
--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { Console, Random } from '@woowacourse/mission-utils';
 class App {
     async getCarNames() {
         const input = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-        
+
         if (input === '') {
             throw new Error('[ERROR] 경주 하려면 자동차가 2대 이상 필요해요!');
         } else {
             return input;
-        }   
+        }
     }
 
     splitCars(carNames) {
@@ -37,7 +37,7 @@ class App {
 
     async getLaps() {
         const input = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
-        
+
         if (input === '') {
             throw new Error('[ERROR] 모든 참가자가 경주를 포기했습니다!');
         } else if (isNaN(Number(input))) {
@@ -46,9 +46,9 @@ class App {
             throw new Error('[ERROR] 1 이상의 정수를 입력해주세요!');
         } else if (Number(input) < 0) {
             throw new Error('[ERROR] 경기 횟수는 0보다 작을 수 없어요!');
-        }else {
+        } else {
             return input;
-        }   
+        }
     }
 
     race(raceBoard, cars) {
@@ -56,10 +56,10 @@ class App {
             let hasMoved = Random.pickNumberInRange(0, 9);
             if (hasMoved >= 4) {
                 raceBoard[car] = raceBoard[car] + '-';
-            }    
+            }
         })
     }
-    
+
     printRaceBoard(raceBoard) {
         Object.entries(raceBoard).forEach(([car, moves]) => {
             Console.print(`${car} : ${moves}`);
@@ -91,7 +91,7 @@ class App {
         const laps = await this.getLaps();
 
         Console.print('\n실행 결과');
-        for(let move = 0; move < laps; move++) {
+        for (let move = 0; move < laps; move++) {
             this.race(raceBoard, cars);
             this.printRaceBoard(raceBoard);
             Console.print('\n');
