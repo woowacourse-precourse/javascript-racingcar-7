72d84e445e3f556d8e71b7ede155bc2e753d8933
H-sooyeon <wndqhr__@naver.com>
2024-10-24 17:45:17 +0900

    feat: 사용자 입력값 검증
    - 자동차 이름 유효성 검사
    - 시도 횟수 유효성 검사

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
+    validateCarName(carNameList) {
+        const carNames = carNameList.trim().split(",");
+
+        if (carNames.length < 2) {
+            throw new Error("[ERROR] 자동차는 2대 이상 입력해주세요.");
+        }
+
+        const regExp = /^[a-zA-Z가-힣]$/; // 한글, 영문만 입력 가능
+        const uniqueCarNames = new Set(); // 중복된 자동차 이름 체크
+
+        const validatedCarNames = carNames.map((carName) => {
+            const trimmedCarName = carName.trim();
+
+            if (trimmedCarName.length > 5) {
+                throw new Error("[ERROR] 자동차 이름은 5자 이하로 입력해주세요.");
+            } else if (!trimmedCarName.match(regExp)) {
+                throw new Error("[ERROR 자동차 이름은 한글, 영문만 입력 가능합니다.");
+            }
+
+            uniqueCarNames.add(trimmedCarName.toLowerCase());
+            return trimmedCarName;
+        });
+
+        if (uniqueCarNames.size !== carNames.length) {
+            throw new Error("[ERROR] 중복된 자동차 이름이 존재합니다.");
+        }
+
+        return validatedCarNames;
+    }
+
+    validateTryCount(tryCount) {
+        if (isNaN(tryCount)) {
+            throw new Error("[ERROR] 시도 횟수는 숫자로 입력해주세요.");
+        }
+
+        const parsedTryCount = Number(tryCount);
+        if (parsedTryCount < 1 || !Number.isInteger(parsedTryCount) || parsedTryCount > 10) {
+            throw new Error("[ERROR] 시도 횟수는 1 이상 10이하 정수값을 입력해주세요.");
+        }
+
+        return parsedTryCount;
+    }
+
+    solve(userInputCarName, userInputTryCount) {}
+
     async run() {
         const userInputCarName = await Console.readLineAsync(
             "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
         );
         const userInputTryCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+
+        const validatedCarNames = this.validateCarName(userInputCarName);
+        const validatedTryCount = this.validateTryCount(userInputTryCount);
+
+        this.solve(validatedCarNames, validatedTryCount);
     }
 }
 
