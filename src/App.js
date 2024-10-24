d8c1d213c2263d0d26cd4cd94669ebed11b4aafe
GSIII <kalseo901@gmail.com>
2024-10-24 15:41:38 +0900

    Revert "feat(view) :  입력기능 및 에러 검사 기능 구현"
    This reverts commit 070464eeb5131184c99a5ba3beceaa05dda1f610.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,5 @@
-import { Console } from "@woowacourse/mission-utils";
-import {
-  checkDuplicateNames,
-  checkNameLength,
-  checkRaceCount,
-} from "./errors/carsError.js";
-
 class App {
-  async run() {
-    let carName = await Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-    );
-    let carNameArr = carName.split(",");
-    checkNameLength(carNameArr);
-    checkDuplicateNames(carNameArr);
-
-    let raceCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
-    checkRaceCount(raceCount);
-  }
+  async run() {}
 }
 
 export default App;
