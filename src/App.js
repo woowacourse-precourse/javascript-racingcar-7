018d271209e2476af87b35cd45d52fd015a72628
jicho <jIcho6412@gmail.com>
2024-10-24 17:19:35 +0900

    입력받은 시행 횟수의 값 만큼 게임이 진행되도록 출력해보기.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
+    // 1, 사용자에게 값을 입력받고 처리하기
     const carNames = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
+
     const separate = () => {
       return carNames.split(",");
     };
@@ -19,7 +21,6 @@ class App {
           throw new Error("[ERROR] 자동차 이름을 공백으로 작성하였습니다.");
         }
       }
-
       const uniqueNames = [...new Set(separateNames)];
       if (separateNames.length !== uniqueNames.length) {
         throw new Error("[ERROR] 자동차의 이름을 중복으로 작성하였습니다.");
@@ -28,12 +29,21 @@ class App {
       return separateNames;
     };
 
-    Console.print(checkNames());
-
     const inputCount = await Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?\n"
     );
     Console.print(inputCount + "회");
+
+    // 2. 입력받은 값을 통해 게임 구현
+
+    const userNames = checkNames();
+    Console.print("실행 결과");
+    for (let cnt = 0; cnt < inputCount; cnt++) {
+      Console.print("\n");
+      for (let j = 0; j < userNames.length; j++) {
+        Console.print(userNames[j] + " : ");
+      }
+    }
   }
 }
 
