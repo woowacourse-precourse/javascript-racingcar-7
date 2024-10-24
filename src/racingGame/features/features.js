19c19ace1783db5700c87afa1d97a17c5cff7e5b
jungwho <jungwho0328@gmail.com>
2024-10-24 20:21:40 +0900

    feat(game) : Add getting winner feature

--- src/racingGame/features/features.js
+++ src/racingGame/features/features.js
@@ -18,3 +18,16 @@ export const playRoundGame = (roundData) => {
   }
   return roundData;
 };
+
+// 3. Get winner
+export const getWinner = (roundData) => {
+  let maxDistance = 0;
+  for (let userData of roundData) {
+    if (userData.distance > maxDistance) maxDistance = userData.distance;
+  }
+  let winnerList = [];
+  for (let userData of roundData) {
+    if (userData.distance === maxDistance) winnerList.push(userData.name);
+  }
+  return winnerList;
+};
