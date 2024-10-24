e8e6ae2a62a2dcfdbb7840b6881b6b28362f68e9
seongmin <seongm22n@gmail.com>
2024-10-24 23:36:41 +0900

    refactor: 기능 1
    - 입력기능끼리 하나의 파일로 모듈화

--- src/utils/userInput.js
+++ src/utils/userInput.js
@@ -10,3 +10,19 @@ const carListValidate = (carName) => {
 	checkCarInputLength(nonDuplicateCarList);
 	return nonDuplicateCarList;
 };
+
+export async function carNameInputHandler() {
+	const carName = await Console.readLineAsync(
+		`경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n`
+	);
+	checkEmptyInput(carName);
+
+	const carList = carListValidate(carName);
+	return carList;
+}
+
+export async function tryNumberHandler() {
+	const tryNumber = await Console.readLineAsync(`시도할 횟수는 몇 회인가요?\n`);
+	checkEmptyInput(tryNumber);
+	return tryNumber;
+}
