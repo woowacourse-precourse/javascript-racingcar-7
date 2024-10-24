630aeee47e2018423248e8c3387b757e389d360a
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2024-10-25 00:42:03 +0900

    feat: 자동차 이름이 6자 이상일 경우 에러 처리

--- src/Car.js
+++ src/Car.js
@@ -1,11 +1,22 @@
+import { CONSOLE_MESSAGE } from './constant.js';
+import { errorString } from './util.js';
+
 class Car {
   #name;
 
   #position;
 
   constructor(name) {
+    this.#validateName(name);
+
     this.#name = name;
     this.#position = 0;
+  }
+
+  #validateName(name) {
+    if (name.length > 5) {
+      throw new Error(errorString(CONSOLE_MESSAGE.MAX_CAR_NAME_LENGTH_ERROR));
+    }
   }
 }
 
--- src/constant.js
+++ src/constant.js
@@ -4,4 +4,5 @@ export const CONSOLE_MESSAGE = Object.freeze({
   TRY_COUNT_INPUT_MESSAGE: '시도할 횟수는 몇 회인가요?\n',
   MIN_CAR_COUNT_ERROR:
     '경주할 자동차는 2대 이상이어야 합니다. 쉼표(,)로 구분해서 2대 이상 입력해주세요.',
+  MAX_CAR_NAME_LENGTH_ERROR: '자동차 이름은 5자 이하만 가능합니다.',
 });
