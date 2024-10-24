a9d6cdc629d792566e55cbe56fbe1ed7c226c52c
Minhyuk Kim <labcs@naver.com>
2024-10-24 15:13:18 +0900

    refactor: extract function that push winner into array

--- util/print/printWinner.js
+++ util/print/printWinner.js
@@ -1,10 +1,12 @@
 import { Console } from "@woowacourse/mission-utils";
 
+import { setWinner } from "../setWinner";
+
 export function printWinner(cars, carMoveHashMap, ATTEMPT_COUNT) {
     const winners = [];
 
     for (let car of cars) {
-        if (carMoveHashMap[car].length === ATTEMPT_COUNT) winners.push(car);
+        setWinner(car, carMoveHashMap, winners, ATTEMPT_COUNT);
     }
 
     Console.print(`최종 우승자 : ${winners.join(', ')}`);
