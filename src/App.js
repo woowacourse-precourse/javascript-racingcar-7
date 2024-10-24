359fa2f0f4b670908e7910d3b6986ebf26a576c0
ohgus <123801385+ohgus@users.noreply.github.com>
2024-10-25 00:18:10 +0900

    [feat]: 자동차 이름 입력 가져오는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from '@woowacourse/mission-utils';
+
+import { PRINT_MESSAGE } from './constants.js';
+
 class App {
-  async run() {}
+  async run() {
+    const carNamesInput = await this.getCarNamesInput();
+  }
+
+  async getCarNamesInput() {
+    const input = await Console.readLineAsync(PRINT_MESSAGE.INPUT_CAR_NAMES);
+
+    return input.replace(/ /g, '');
+  }
 }
 
 export default App;
