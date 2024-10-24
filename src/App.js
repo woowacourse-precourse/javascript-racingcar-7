861120f21c9f101bf9fbbe21862502b530143642
H-sooyeon <wndqhr__@naver.com>
2024-10-24 15:44:04 +0900

    feat: 자동차 이름과 시도 횟수 입력받아 저장

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+    async run() {
+        const userInputCarName = await Console.readLineAsync(
+            "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+        );
+        const userInputTryCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    }
 }
 
 export default App;
