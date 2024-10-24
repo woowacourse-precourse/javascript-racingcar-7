5f1af50c1c9636486bf4518670db7e1a41e04323
hjoo830 <hjoo830@naver.com>
2024-10-24 12:44:36 +0900

    style: 입력 프롬프트에 줄바꿈 문자 추가

--- src/App.js
+++ src/App.js
@@ -19,12 +19,12 @@ class App {
 
   async getInputCarNames() {
     return await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
     );
   }
 
   async getInputCount() {
-    return await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+    return await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
   }
 
   initializeCars(inputCar) {
