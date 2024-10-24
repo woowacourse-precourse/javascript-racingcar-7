31e471306cd2cac482e56ba5fd6aa074021fb5b7
Kimyerim <yeye092302@gmail.com>
2024-10-25 00:54:01 +0900

    feat: 자동차 이름과 횟수 입력받는 메서드 추가
    - getCarNmae() 메서드에서 경주할 자동차 이름을 입력받고 ','를 기준으로 구분해 this.carNameArr에 저장
    - 시도할 횟수를 입력받아 this.count에 저장ur changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -7,7 +7,19 @@ class App {
   }
 
   async run() {
+    // 자동차 이름, 횟수 입력받기
+    await this.getCarName();
+  }
 
+  async getCarName(){
+    try{
+      const input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+      this.carNameArr = input.split(',').map(car => car.trim());
+      const num = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+      this.count = Number(num);
+    }catch(error){
+      throw new Error("[ERROR]");
+    }
   }
 }
 
