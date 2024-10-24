8d865844095cc2437099b897c77e5fe50ac871b5
bunju20 <ghkd2271@naver.com>
2024-10-24 17:37:49 +0900

    feat: 자동차 전진수 저장할 map 생성 - 함수makeCarsMap 생성 및 map 생성 로직 구현 - App.js 내에서 작동 확인 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
+function makeCarsMap(carArray) {
+  const carsMap = new Map();
+  carArray.forEach(car => {
+    carsMap.set(car, 0);
+  });
+  return carsMap;
+}
+
 class App {
   async run() {
-    const cars = await MissionUtils.Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
-    );
+    try {
+      const cars = await MissionUtils.Console.readLineAsync(
+        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+      );
 
-    const carArray = cars.split(',').map(car => car.trim());
+      const carArray = cars.split(',').map(car => car.trim());
 
-    if (carArray.length === 1) {
-      throw new Error('[ERROR] 인식된 자동차가 없습니다.');
-    }
+      if (carArray.length === 1) {
+        throw new Error('[ERROR] 인식된 자동차가 없습니다.');
+      }
 
-    const hasEmptyName = carArray.some(car => !car || car.trim() === '');
-    if (hasEmptyName) {
-      throw new Error('[ERROR] 쉼표가 적절히 작성되지 않았습니다.');
-    }
+      const hasEmptyName = carArray.some(car => !car || car.trim() === '');
+      if (hasEmptyName) {
+        throw new Error('[ERROR] 쉼표가 적절히 작성되지 않았습니다.');
+      }
 
-    const hasLongName = carArray.some(car => car.length > 5);
-    if (hasLongName) {
-      throw new Error('[ERROR] 자동차 이름은 5자 이하만 가능합니다.');
+      const hasLongName = carArray.some(car => car.length > 5);
+      if (hasLongName) {
+        throw new Error('[ERROR] 자동차 이름은 5자 이하만 가능합니다.');
+      }
+
+      const carsMap = makeCarsMap(carArray);
+    } catch (error) {
+      console.log(error.message);
     }
   }
 }
