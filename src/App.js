141df1e31d83c0650e14790d1ba7d4efe7c2d240
hjoo830 <hjoo830@naver.com>
2024-10-24 20:04:59 +0900

    chore: 불필요한 Console.print 제거

--- src/App.js
+++ src/App.js
@@ -44,7 +44,6 @@ class App {
     let cars = {};
     const carNames = inputCar.split(',').map((car) => car.trim());
     carNames.forEach((car) => {
-      Console.print(cars);
       if (car.length > 5) {
         throw new Error('[ERROR] 자동차 이름은 5자 이하만 가능합니다.');
       }
