fdc91cee1180a65a7e876f6dafe77bb14c27c5b6
8leamnk <silkviewrk@gmail.com>
2024-10-24 13:25:57 +0900

    test: 자동차 전진 기능 테스트
    테스트 내용:
    - 랜덤 숫자가 4 이상이 나오면 1이 더해지는지 테스트
    - 랜덤 숫자가 4 미만이 나오면 1이 더해지지 않는지 테스트
    - 입력된 자동차가 입력된 횟수만큼 경주를 할 때, 각 자동차의 총 전진 횟수가 올바르게 반환되는지 테스트

---
+++ __tests__/domain/MovingForwardTest.js
@@ -0,0 +1,82 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import MovingForward from '../../src/domain/MovingForward';
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    MissionUtils.Random.pickNumberInRange,
+  );
+};
+
+describe('자동차 전진 기능 테스트', () => {
+  const MOVING_FORWARD = 4;
+  const STOP = 3;
+
+  test('랜덤 숫자가 4 이상이 나오면 1이 더해진다.', () => {
+    // given
+    const CARS = new Map([['람보르기니', 0]]);
+    const NUMBER = 1;
+    const OUTPUT = new Map([['람보르기니', 1]]);
+
+    mockRandoms([MOVING_FORWARD]);
+
+    // when
+    const movingForword = new MovingForward(CARS, NUMBER);
+    const output = movingForword.moveForwardCars();
+
+    // then
+    expect(output).toEqual(OUTPUT);
+  });
+
+  test('랜덤 숫자가 4 미만이 나오면 1이 더해지지 않는다.', () => {
+    // given
+    const CARS = new Map([['포르쉐', 0]]);
+    const NUMBER = 1;
+    const OUTPUT = new Map([['포르쉐', 0]]);
+
+    mockRandoms([STOP]);
+
+    // when
+    const movingForword = new MovingForward(CARS, NUMBER);
+    const output = movingForword.moveForwardCars();
+
+    // then
+    expect(output).toEqual(OUTPUT);
+  });
+
+  test('입력된 자동차가 입력된 횟수만큼 경주를 할 때, 각 경주의 자동차마다 4 이상의 랜덤 숫자가 나오면 해당 자동차에 1씩 더해지고 그렇지 않으면 아무 것도 더해지지 않는다.', () => {
+    // given
+    const CARS = new Map([
+      ['소나타', 0],
+      ['그렌져', 0],
+      ['캠리', 0],
+    ]);
+    const NUMBER = 3;
+    const OUTPUT = new Map([
+      ['소나타', 1],
+      ['그렌져', 1],
+      ['캠리', 2],
+    ]);
+
+    mockRandoms([
+      MOVING_FORWARD,
+      STOP,
+      STOP,
+      STOP,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      STOP,
+      STOP,
+      MOVING_FORWARD,
+    ]);
+
+    // when
+    const movingForword = new MovingForward(CARS, NUMBER);
+    const output = movingForword.moveForwardCars();
+
+    // then
+    expect(output).toEqual(OUTPUT);
+  });
+});
