32f69459aefedbc65a0c7d70a22869be892ad7b9
jongbin26 <010jjj@naver.com>
2024-10-24 20:11:32 +0900

    feat(ValidationError): 오류 처리 함수 구현
    - ValidationError 클래스를 만들어서 명시적으로 검증 오류를 처리했습니다.
    - 자동차와 시도 횟수 각각 입력이 들어오지 않은 경우에 대해 에러를 처리했습니다.
    - 음수가 시도 횟수로 입력된 경우에 대해 에러를 처리했습니다.

--- src/utils/ioHandler.js
+++ src/utils/ioHandler.js
@@ -1,9 +1,23 @@
 import { Console } from '@woowacourse/mission-utils';
 import { IO_MESSAGE } from '../constants/index.js';
+import { ValidationError } from '../Error/ValidationError.js';
+
+function validateInput(carString, count) {
+  if (!carString) {
+    throw new ValidationError('[ERROR] 자동차가 입력되지 않았습니다.');
+  }
+  if (Number.isNaN(parseInt(count, 10))) {
+    throw new ValidationError('[ERROR] 숫자가 입력되지 않았습니다.');
+  }
+  if (count <= 0) {
+    throw new ValidationError('[ERROR] 양수를 입력해야 합니다.');
+  }
+}
 
 async function getGameInput() {
   const carString = await Console.readLineAsync(IO_MESSAGE.FIRST_INPUT_MESSAGE);
   const count = await Console.readLineAsync(IO_MESSAGE.SECOND_INPUT_MESSAGE);
+  validateInput(carString, count);
   return { carString, count };
 }
 
