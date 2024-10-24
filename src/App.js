56d8b528a3bab8f4aece0f9a105db4628c84c515
jicho <jIcho6412@gmail.com>
2024-10-24 16:15:16 +0900

    입력받은 자동차 이름들의 길이를 각각 확인하여 만약, 자동차 이름 하나라도 길이가 6이상일 경우 에러가 발생하도록 예외 처리

--- src/App.js
+++ src/App.js
@@ -5,10 +5,21 @@ class App {
     const carNames = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
-    const separateCarName = () => {
+    const separate = () => {
       return carNames.split(",");
     };
-    Console.print(separateCarName());
+
+    const checkWordLength = () => {
+      const separateNames = separate();
+      for (let i = 0; i < separateNames.length; i++) {
+        if (separateNames[i].length > 5) {
+          throw new Error("[ERROR] 자동차 이름을 5자 이하로 작성해주세요.");
+        }
+      }
+      return separateNames;
+    };
+
+    Console.print(checkWordLength());
 
     const inputCount = await Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?\n"
