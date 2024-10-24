f1cb947ad82850f3d15da02753fb05db651cca2b
salmonco <xkqjsslsek80@naver.com>
2024-10-25 00:44:38 +0900

    refactor(app): 자동차 경주 게임 로직을 함수로 분리

--- src/App.js
+++ src/App.js
@@ -5,6 +5,22 @@ class App {
     const racingCarNames = await this.readRacingCarNames();
     const attemptCount = await this.readAttemptCount();
 
+    this.startRacingGame(racingCarNames, attemptCount);
+  }
+
+  async readRacingCarNames() {
+    const input = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    return input;
+  }
+
+  async readAttemptCount() {
+    const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    return input;
+  }
+
+  startRacingGame(racingCarNames, attemptCount) {
     this.validateAttempCount(attemptCount);
 
     const racingCarNamesArray = this.parseRacingCarNames(racingCarNames);
@@ -34,18 +50,6 @@ class App {
     }
 
     this.printWinner(moveCntPerCar);
-  }
-
-  async readRacingCarNames() {
-    const input = await Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
-    );
-    return input;
-  }
-
-  async readAttemptCount() {
-    const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-    return input;
   }
 
   parseRacingCarNames(racingCarNames) {
