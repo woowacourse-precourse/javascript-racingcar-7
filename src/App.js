83855c89d4b6766f63765e204441398409f62246
Kimyerim <yeye092302@gmail.com>
2024-10-25 01:19:36 +0900

    fix: getCarNameCount() 메서드에서 checkNameLength() 메서드를 사용하도록 수정
    - checkNameLength()를 사용해 입력받은 자동차 이름의 길이를 확인하게 하였다.

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ class App {
     try{
       const input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
       this.carNameArr = input.split(',').map(car => car.trim());
+      // 이름 길이 확인
+      this.checkNameLength();
+      
       const num = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
       this.count = Number(num);
     }catch(error){
