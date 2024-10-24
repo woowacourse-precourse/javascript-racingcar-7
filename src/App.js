dc8774dd725e872105ce68662be4db7a941d3df5
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-24 19:44:31 +0900

    feat: 자동차 이름 입력 창

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+import { INPUT_MESSAGE } from "./constants/message.js";
+
 class App {
+  getRacingCarName = async () => {
+    await Console.readLineAsync(INPUT_MESSAGE.carName);
+  };
   async run() {}
 }
 
 export default App;
+
+//자동차 이름을 입력 받는다 → 시도할 횟수를 받는다 -> 횟수만큼 이동 → 차수별 실행 결과를 보여준다 → 우승자를 안내한다
