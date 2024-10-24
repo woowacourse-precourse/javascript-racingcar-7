81d71cadcab90167c100c1d49fe2865479e54890
GSIII <kalseo901@gmail.com>
2024-10-24 15:52:03 +0900

    feat(view) : 입력기능 및 에러처리 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console } from "@woowacourse/mission-utils";
+import {
+  checkDuplicateNames,
+  checkNameLength,
+  checkRaceCount,
+} from "./errors/carsErrors.js";
+
 class App {
-  async run() {}
+  async run() {
+    let carName = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+    let carNameArr = carName.split(",");
+    checkNameLength(carNameArr);
+    checkDuplicateNames(carNameArr);
+
+    let raceCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    checkRaceCount(raceCount);
+  }
 }
 
 export default App;
