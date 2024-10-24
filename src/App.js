dc593ceb8b6debfba8a2907d86c91ede2c27d5bc
salmonco <xkqjsslsek80@naver.com>
2024-10-24 23:21:29 +0900

    feat(app): 시도할 횟수를 입력받기

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const input = await this.readRacingCarNames();
+    const racingCarNames = await this.readRacingCarNames();
+    const attemptCount = await this.readAttemptCount();
   }
 
   async readRacingCarNames() {
     const input = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
+    return input;
+  }
+
+  async readAttemptCount() {
+    const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
     return input;
   }
 }
