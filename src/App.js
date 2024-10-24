33b02be658303a936e7463f1d4de365a72cec76d
deokbin <deokbin@naver.com>
2024-10-24 15:35:17 +0900

    feat<App> : 유효성 검사 및 자동차 전진 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import { Console } from '@woowacourse/mission-utils';
 import NameValidation from './utils/NameValidation.js';
 import NumValidation from './utils/NumValidation.js';
+import Car from './utils/Car.js';
 
 class App {
   async run() {
-    const userInput = await Console.readLineAsync(
+    const carInput = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
     );
-    NameValidation.isValid(userInput.trim());
+    const isNameValid = NameValidation.isValid(carInput.trim());
     const countInput = await Console.readLineAsync('시도횟수를 입력해주세요.');
-    NumValidation.isValidNum(countInput.trim());
-    Console.print(userInput);
-    Console.print(countInput);
+    const isNumValid = NumValidation.isValidNum(countInput.trim());
+    if (isNameValid && isNumValid) {
+      const carList = carInput.split(',').map(name => new Car(name));
+
+      for (let i = 0; i < countInput; i++) {
+        carList.forEach(car => {
+          car.move();
+          Console.print(car.getPosition());
+        });
+      }
+    }
   }
 }
 
