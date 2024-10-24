f8fabebd43fc8abc5451ed5f7e24ed1049191e7d
jungwho <jungwho0328@gmail.com>
2024-10-24 20:26:07 +0900

    feat(app) : Add setting game and playing game feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { readRound, readUsers } from "./gameSetting/features/handleInput.js";
+import { setRoundNum, setUserList } from "./gameSetting/gameSetting.js";
+import { playRacingGame } from "./racingGame/racingGame.js";
+
 class App {
-  async run() {}
+  async run() {
+    const userInput = await readUsers();
+    const userList = setUserList(userInput);
+
+    const roundInput = await readRound();
+    const roundNum = setRoundNum(roundInput);
+
+    playRacingGame(userList, roundNum);
+  }
 }
 
 export default App;
