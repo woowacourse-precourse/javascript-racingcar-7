02935851a2487fdfd29c1503f3d3cb27b6a42be8
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-10-24 13:29:33 +0900

    feat(Input): 기본적인 사용자 입력 처리

--- src/App.js
+++ src/App.js
@@ -7,8 +7,18 @@ class Car {
   }
 }
 
+function getNames(str) {
+  const names = str.split(',');
+  return names;
+}
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+    );
+    const names = getNames(input);
+  }
 }
 
 export default App;
