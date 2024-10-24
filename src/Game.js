a07989576568fc2e029858424ed1e2d0fd618ec0
jongbin26 <010jjj@naver.com>
2024-10-24 10:04:01 +0900

    feat(Game): play 함수 구현
    - count 횟수동안 랜덤 넘버로 움직이는 play 함수를 구현했습니다.
    - movingCar 배열에 이동하는 자동차를 넣고 한 번에 업데이트합니다.

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,25 @@
-import { initRecord } from "./utils/initRecord.js";
+import { Random } from '@woowacourse/mission-utils';
+import { initRecord } from './utils/initRecord.js';
 
 export class Game {
   constructor(cars, count) {
     this.record = initRecord(cars);
     this.count = count;
   }
-  update(movingCars){
+  update(movingCars) {
     movingCars.map((car) => {
       this.record[car] += 1;
-    })
+    });
+  }
+  play() {
+    for (let i = 0; i < this.count; i++) {
+      let movingCars = [];
+      Object.keys(this.record).map((key) => {
+        if (Random.pickNumberInRange(0, 9) >= 4) {
+          movingCars.push(key);
+        }
+      });
+      this.update(movingCars);
+    }
   }
 }
