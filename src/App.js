80e6530a39708d93cb393f7e86f12e57342e804b
luii0415 <becky45210193@gmail.com>
2024-10-24 18:10:09 +0900

    feat : input
    경주할 자동차 이름과 이동 횟수 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    // 자동차 이름 입력 및 이동 횟수 입력
+    Console.print(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+    let name_input = await Console.readLineAsync("");
+
+    Console.print("시도할 횟수는 몇 회인가요?");
+    const loop = await Console.readLineAsync("");
+    Console.print(`${name_input}, ${loop}`);
+  }
 }
 
 export default App;
