f8a1b5ad55967142b3952a2cb0fd2ecfb440b782
luii0415 <becky45210193@gmail.com>
2024-10-24 18:20:54 +0900

    feat : create
    에러 조건 1번 : 입력된 이름이 5자를 넘어갈 경우 에러 발생

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ class App {
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
     let name_input = await Console.readLineAsync("");
+    let car_names = name_input.split(",");
+    car_names = car_names.map((name) => name.trim());
+    // 에러 조건 1 : 입력된 이름이 5자를 넘어가는가?
+    for (let name of car_names) {
+      if (name.length > 5) {
+        throw new Error("[ERROR]");
+      }
+    }
 
     Console.print("시도할 횟수는 몇 회인가요?");
     const loop = await Console.readLineAsync("");
-    Console.print(`${name_input}, ${loop}`);
   }
 }
 
