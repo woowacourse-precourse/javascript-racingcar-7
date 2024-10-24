fcc988bff09907754bcd6d9e1f39b1aa64ae2b6f
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-10-24 22:37:54 +0900

    feat: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)' 문구와 함께 자동차의 이름을 입력을 받는다

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ class App {
 
   async run() {
     this.carList = this.makeCars([]);
+    const carNames = await this.getCarName();
+  }
+
+  async getCarName() {
+    return await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
   }
 
   makeCars(names) {
