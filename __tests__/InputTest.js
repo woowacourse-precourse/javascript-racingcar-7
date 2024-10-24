4bcb17bcd979b22f0d157f3ed5084472a3f88f61
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-10-24 23:53:38 +0900

    refactor: 입력과 관련된 테스트에서 입력 모듈을 불러온다

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,16 +1,10 @@
-import App from '../src/App.js';
+import { splitCarName } from '../src/InputUtils';
 
 describe('입력', () => {
-  let app;
-
-  beforeEach(() => {
-    app = new App();
-  });
-
   test('자동차 이름은 쉼표(,)를 기준으로 구분한다.', () => {
     const INPUT = 'pobi,woni,jun';
     const NAMES = ['pobi', 'woni', 'jun'];
 
-    expect(app.splitCarName(INPUT)).toEqual(NAMES);
+    expect(splitCarName(INPUT)).toEqual(NAMES);
   });
 });
