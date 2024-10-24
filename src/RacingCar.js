3a63dd8dfa2a05a3c34dfefe9703e93e54810225
aydenote <dslgpgh@gmail.com>
2024-10-24 16:02:12 +0900

    refactor(RacingCar): 변수명 변경

--- src/RacingCar.js
+++ src/RacingCar.js
@@ -2,15 +2,15 @@ import { userInput } from "./utils/MissionUtils.js";
 
 class RacingCar {
 	async runRacingCar() {
-		const { carNames, tryCount } = await this.getUserInput();
+		const { carNameValue, tryCountValue } = await this.getUserInput();
 	}
 
 	async getUserInput() {
-		const carNames = await userInput(
+		const carNameValue = await userInput(
 			"경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n"
 		);
-		const tryCount = await userInput("시도할 횟수는 몇 회인가요?\n");
-		return { carNames, tryCount };
+		const tryCountValue = await userInput("시도할 횟수는 몇 회인가요?\n");
+		return { carNameValue, tryCountValue };
 	}
 }
 
