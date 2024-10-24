a6055c91c0d575317c057837f9c18b5cddfb2eed
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2024-10-24 23:27:19 +0900

    feat: 시도 횟수 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
+import { CONSOLE_MESSAGE } from './constant.js';
+import { getUserInput } from './util.js';
 
 class App {
   async run() {
-    const carInput = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+    const carInput = await getUserInput(CONSOLE_MESSAGE.CAR_INPUT_MESSAGE);
+
+    const tryCountInput = await getUserInput(
+      CONSOLE_MESSAGE.TRY_COUNT_INPUT_MESSAGE,
     );
 
     Console.print(carInput);
+    Console.print(tryCountInput);
   }
 }
 
---
+++ src/util.js
@@ -0,0 +1,3 @@
+import { Console } from '@woowacourse/mission-utils';
+
+export const getUserInput = async (message) => Console.readLineAsync(message);
