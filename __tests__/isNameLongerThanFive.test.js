e001ac83f66c4b5dad39e4ee275c87eb1843dd60
subsub-e <ab13696802@gmail.com>
2024-10-25 03:01:20 +0900

    feat(isNameLongerThanFive) : isNameLongerThanFive 함수 구현 및 구현에 따른 테스트 코드 수정

--- __tests__/isNameLongerThanFive.test.js
+++ __tests__/isNameLongerThanFive.test.js
@@ -1,17 +1,25 @@
-import { isNameLongerThanFive } from '../src/utils/isNameLongerThanFive.js';
+import { isNameLongerThanFive } from "../src/utils/isNameLongerThanFive.js";
 
-describe('isNameLongerThanFive 함수 테스트 : ', () => {
-    
-    test('이름의 길이가 5를 초과하는 경우 CAR_NAME_TOO_LONG 에러를 발생시킨다.', () => {
-        expect(isNameLongerThanFive(['subsub-e', 'hiiiiiie', '+21@)'])).toThrowError('[ERROR] 이름이 너무 길어요. 자동차 이름은 5자 이하로 입력해주세요.');
-    });
+describe("isNameLongerThanFive 함수 테스트 : ", () => {
+  test("이름의 길이가 5를 초과하는 경우 CAR_NAME_TOO_LONG 에러를 발생시킨다.", () => {
+    expect(() =>
+      isNameLongerThanFive(["subsub-e", "hiiiiiie", "+21@)"])
+    ).toThrowError(
+      "[ERROR] 이름이 너무 길어요. 자동차 이름은 5자 이하로 입력해주세요."
+    );
+  });
 
-    test('이름의 길이가 5를 초과하는 경우 CAR_NAME_TOO_LONG 에러를 발생시킨다.', () => {
-        expect(isNameLongerThanFive(['이승섭입니다.', '하이요'])).toThrowError('[ERROR] 이름이 너무 길어요. 자동차 이름은 5자 이하로 입력해주세요.');
-    });
+  test("이름의 길이가 5를 초과하는 경우 CAR_NAME_TOO_LONG 에러를 발생시킨다.", () => {
+    expect(() =>
+      isNameLongerThanFive(["이승섭입니다.", "하이요"])
+    ).toThrowError(
+      "[ERROR] 이름이 너무 길어요. 자동차 이름은 5자 이하로 입력해주세요."
+    );
+  });
 
-    test('각 이름의 길이가 5자 이하이므로 에러를 발생시키지 않는다.', () => {
-        expect(isNameLongerThanFive(['이승섭이다', '하이요'])).not.toThrowError();
-    });
-
-})
+  test("각 이름의 길이가 5자 이하이므로 에러를 발생시키지 않는다.", () => {
+    expect(() =>
+      isNameLongerThanFive(["이승섭이다", "하이요"])
+    ).not.toThrowError();
+  });
+});
---
+++ src/utils/isNameLongerThanFive.js
@@ -0,0 +1,11 @@
+import { ERROR_MESSAGES } from "../constants/errorConstants.js";
+
+function checkLength(name) {
+  if (name.length > 5) {
+    throw new Error(ERROR_MESSAGES.CAR_NAME_TOO_LONG);
+  }
+}
+
+export function isNameLongerThanFive(extractCarNameArray) {
+  extractCarNameArray.forEach((name) => checkLength(name));
+}
