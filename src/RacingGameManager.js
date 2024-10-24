0f89a98dd47c9ddf1393ae3aeb2ee58e19ff3df3
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2024-10-25 00:38:39 +0900

    fix: 입력받은 값이 공백일 경우 에러 추가

--- src/RacingGameManager.js
+++ src/RacingGameManager.js
@@ -1,28 +1,30 @@
-import { Console } from '@woowacourse/mission-utils';
 import { errorString, getUserInput } from './util.js';
 import { CONSOLE_MESSAGE } from './constant.js';
 import Car from './Car.js';
 
 class RacingGameManager {
+  #cars = [];
+
+  #tryCount = 0;
+
   constructor() {}
 
   async playGame() {
     const carInput = await getUserInput(CONSOLE_MESSAGE.CAR_INPUT_MESSAGE);
 
+    this.#validateCarInput(carInput);
+
+    this.#cars = carInput.split(',').map((carName) => new Car(carName));
+
     const tryCountInput = await getUserInput(
       CONSOLE_MESSAGE.TRY_COUNT_INPUT_MESSAGE,
     );
 
-    this.#validateCarInput(carInput);
-
-    const cars = carInput.split(',').map((carName) => new Car(carName));
-
-    Console.print(carInput);
-    Console.print(tryCountInput);
+    this.#tryCount = Number(tryCountInput);
   }
 
   #validateCarInput(input) {
-    if (!input.includes(',')) {
+    if (!input || !input.includes(',')) {
       throw new Error(errorString(CONSOLE_MESSAGE.MIN_CAR_COUNT_ERROR));
     }
   }
