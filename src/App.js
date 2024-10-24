3281e6e5e29eadbef02853c381e69966a3c968a7
daeun <gbajs754@gmail.com>
2024-10-24 17:46:27 +0900

    feat : 입출력 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const firstUserInput = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n"
+    );
+    const secondUserInput = await Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요? \n"
+    );
+
+    const winner = [];
+    Console.print(`최종 우승자 : ${winner.join(", ")}`);
+  }
 }
 
 export default App;
