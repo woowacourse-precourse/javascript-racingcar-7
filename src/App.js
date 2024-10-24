cacf4bc82069993cde8e5368e396da5a98aad5b7
rwaeng <rwaeng0421@gmail.com>
2024-10-24 22:03:58 +0900

    feat: 자동차 이름 유효성 검사 기능 추가

--- src/App.js
+++ src/App.js
@@ -11,8 +11,22 @@ class App {
     );
   }
 
+  validateCarNames() {
+    if (this.names === '') {
+      throw new Error('[ERROR] 자동차 이름을 입력하세요.');
+    }
+
+    const splitedNames = this.names.split(',');
+    splitedNames.forEach(carName => {
+      if (carName.length > 5) {
+        throw new Error('[ERROR] 자동차 이름은 5글자 이하여야 합니다.');
+      }
+    });
+  }
+
   async run() {
-    this.getCarNames();
+    await this.getCarNames();
+    this.validateCarNames();
   }
 }
 
