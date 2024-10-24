3115acf8c9582b37599553d6dc0bc82782e88586
minji2219 <200146@jnu.ac.kr>
2024-10-25 00:12:05 +0900

    feat: 레이스 시작하는 함수 추가
    모든 입력을 받은 후 runRace()로 레이싱 경기 시작

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { scan } from './commonFuntion/scanner.js';
+import { runRace } from './race/runRace.js';
 
 class App {
   async run() {
     const carsName = await scan(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
     );
+
     const repeatNumber = await scan('시도할 횟수는 몇 회인가요?');
+
+    runRace(carsName, repeatNumber);
   }
 }
 
