a7b2d23c5fe0af0cd9c24f92dc354676bb56ad1c
aydenote <dslgpgh@gmail.com>
2024-10-24 16:05:34 +0900

    chore(RacingCar): 탈자 수정

--- src/RacingCar.js
+++ src/RacingCar.js
@@ -7,7 +7,7 @@ class RacingCar {
 
 	async getUserInput() {
 		const carNameValue = await userInput(
-			"경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n"
+			"경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
 		);
 		const tryCountValue = await userInput("시도할 횟수는 몇 회인가요?\n");
 		return { carNameValue, tryCountValue };
