ef127c4c60510232e6235af8bd3f892bae25cc6a
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-10-24 20:47:34 +0900

    refactor: 랜던 값을 모킹하는 부분을 함수로 만들어서 테스트의 중복을 제거한다

--- __tests__/AppTest.js
+++ __tests__/AppTest.js
@@ -10,21 +10,22 @@ describe('자동차 경주', () => {
     app = new App();
   });
 
-  test('무작위 값이 4 이상이면 참을 반환한다.', () => {
-    jest.spyOn(MissionUtils.Random, 'pickNumberInRange').mockReturnValue(4);
+  const mockRandomValue = (value) => {
+    jest.spyOn(MissionUtils.Random, 'pickNumberInRange').mockReturnValue(value);
+  };
 
+  test('무작위 값이 4 이상이면 참을 반환한다.', () => {
+    mockRandomValue(4);
     expect(app.isHighEnough()).toBe(true);
   });
 
   test('무작위 값이 4 미만이면 거짓을 반환한다.', () => {
-    jest.spyOn(MissionUtils.Random, 'pickNumberInRange').mockReturnValue(1);
-
+    mockRandomValue(1);
     expect(app.isHighEnough()).toBe(false);
   });
 
   test('자동차가 전진하면 자동차의 위치는 1 증가한다.', () => {
-    jest.spyOn(MissionUtils.Random, 'pickNumberInRange').mockReturnValue(5);
-
+    mockRandomValue(5);
     const car = new Car();
     const position = car.getPosition();
 
