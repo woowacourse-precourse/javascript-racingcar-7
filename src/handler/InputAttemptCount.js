f2660a7329bff4c59de46f02ee4bbfb7e5ba4139
hyunyeee <5004ohy@naver.com>
2024-10-24 20:15:57 +0900

    feat(validate): 시도 횟수가 숫자가 아닌 경우 에러를 발생시킨다

--- src/handler/InputAttemptCount.js
+++ src/handler/InputAttemptCount.js
@@ -1,9 +1,21 @@
-import { ReadLine } from "./ReadLine.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
-const INPUT_ATTEMPT_COUNT_MESSAGE = '시도할 횟수는 몇 회인가요?';
+const INPUT_ATTEMPT_COUNT_MESSAGE = '시도할 횟수는 몇 회인가요? ';
+const NAN_ERROR_MESSAGE = '[ERROR] 시도 횟수는 숫자입니다.';
 
 export class InputAttemptCount {
   async read() {
-    return await new ReadLine().readInput(INPUT_ATTEMPT_COUNT_MESSAGE);
+    const input = await MissionUtils.Console.readLineAsync(INPUT_ATTEMPT_COUNT_MESSAGE);
+    this.#validate(input);
+    return input;
+  }
+
+  #validate(input) {
+    const number = Number(input);
+    this.#isNumber(number);
+  }
+
+  #isNumber(input) {
+    if (isNaN(input)) throw new Error(NAN_ERROR_MESSAGE);
   }
 }
