bbee4dbc255c614e3f870a5f4dc90a271030bb8b
goodbyebada <goodbyebada@naver.com>
2024-10-25 17:46:31 +0900

    feat: 메인 함수에서 실행
    - controller 역할

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-class App {
-  async run() {}
-}
+import { Console } from '@woowacourse/mission-utils';
 
+import InputView from './views/InputView.js';
+import OutputView from './views/OutputView.js';
+
+import GameManager from './utils/GameManager.js';
+
+class App {
+
+  async run() {
+    const input = await InputView.getNames();
+    const gameManager = new GameManager(input);
+    const attempts = await InputView.getAttempts();
+
+    gameManager.repeatGameAttempts(attempts);
+
+    const winnerNameString = gameManager.getWinnerNameString();
+
+    if (winnerNameString.length > 0) {
+      OutputView.printWinner(winnerNameString);
+      return;
+    }
+    OutputView.printNoWinner();
+  }
+}
 export default App;
