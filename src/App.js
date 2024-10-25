fe0d5380c975b18846b9d1fcfd999d183375104d
Ryu1216 <r12dh16@gmail.com>
2024-10-26 03:25:18 +0900

    feat: Add recieving play times and error handling for nor-integer entries

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
+class CarNameParser {
+  constructor(names) {
+    this.names = this.splitCarName(names);
+    names.forEach(car => {
+      this.CheckCarName(car);
+    });
+  }
+
+  CheckCarName(name) {
+    if (name.length > 5) {
+      throw new Error("[ERROR]이름은 5자를 초과할 수 없습니다.");
+    }
+  }
+  splitCarName(input) {
+    const DELIMITER = ',';
+    return input.split(DELIMITER).map(part => part.trim());
+  }
+
+}
+
+class TimeStringToNumber{
+  constructor(times){
+    this.isPositiveIntegar(times);
+    return Number(times);
+  }
+  isPositiveIntegar(times){
+    const POSITIVE_INTEGER_REGEX = /^\d+$/;
+    if(POSITIVE_INTEGER_REGEX.test(times)==false){
+      throw new Error("[ERROR]횟수는 양의 정수이어야 합니다.");
+    }
+  }
+}
+
 class App {
   async run() {
     const cars = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)기준으로 구분)\n");
+    const players = new CarNameParser(cars);
+    const times = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    const time = TimeStringToNumber(times);
     
-}
+
+
+  }
+
 
 export default App;
