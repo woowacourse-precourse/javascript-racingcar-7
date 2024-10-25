bb34e660743d5d7f050877e2dafe77dcbe48dc5a
kch6396 <cjfghks6396@gmail.com>
2024-10-25 20:35:01 +0900

    feat(getInput): Implement cars name input and setting conditions
    Add functionality to input cars name, delimited by commas,
    checking that each car name does not exceed 5 characters in length.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import { Console } from "@woowacourse/mission-utils";
 
+const getInput = async (inputMessage) => {
+  try {
+    return await Console.readLineAsync(inputMessage);
+  } catch (error) {
+    throw error;
+  }
+};
+
 class App {
-  run() {
-    Console.print(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-    );
+  async run() {
+    try {
+      const carsName = await getInput(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+      );
+      const carNameLengthLimit = carsName
+        .split(",")
+        .every((carName) => carName.length <= 5);
+      if (!carNameLengthLimit)
+        throw new Error("자동차 이름은 5자 이하만 가능합니다.");
+    } catch (error) {
+      Console.print(`[ERROR] ${error.message}`);
+    }
   }
 }
 
