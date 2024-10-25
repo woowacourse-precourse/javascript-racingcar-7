173d61e372e23d8791e6de21fd0c1d4a838d3f28
Seio924 <seio9241@gmail.com>
2024-10-25 10:34:47 +0900

    test(InputViewTest): InputView 테스트 추가 및 수정
    자동차 이름 입력 테스트 코드 수정 : 쉼표를 기준으로 분리하여 배열로 반환받도록 고쳤다.
    자동차 이름 공백 제거 테스트 코드 추가

--- __tests__/InputViewTest.js
+++ __tests__/InputViewTest.js
@@ -2,10 +2,25 @@ import InputView from '../src/View/InputView';
 import { mockQuestions } from './ApplicationTest';
 
 describe('InputView', () => {
-  test('자동차 이름을 입력받는다', async () => {
+  test('자동차 이름을 입력받아 배열로 반환한다', async () => {
     // given
     const input = ['pobi,woni'];
-    const output = 'pobi,woni';
+    const output = ['pobi', 'woni'];
+
+    mockQuestions(input);
+
+    // when
+    const inputView = new InputView();
+    const carNames = await inputView.readCarNames();
+
+    // then
+    expect(carNames).toEqual(output);
+  });
+
+  test('자동차 이름 입력에서 공백 제거', async () => {
+    // given
+    const input = ['  pobi , woni  '];
+    const output = ['pobi', 'woni'];
 
     mockQuestions(input);
 
