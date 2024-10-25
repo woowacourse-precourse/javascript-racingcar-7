05fa30550e99016fb31601ba17e914318edc3137
Seio924 <seio9241@gmail.com>
2024-10-25 10:35:43 +0900

    feat(InputView): 쉼표를 기준으로 분리하고 공백 제거

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -14,9 +14,11 @@ class InputView {
   }
 
   async readCarNames() {
-    const carNames = await this.readInput(
+    const carNamesInput = await this.readInput(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
     );
+
+    const carNames = carNamesInput.split(',').map(name => name.trim());
 
     return carNames;
   }
