c48c6677da67ba1f34fb1468a472170ef46b6dc5
jeheecheon <jeheecheon@gmail.com>
2024-10-26 01:30:09 +0900

    feat: get car names from console input
    Get input from console.
    Input could be any value.
    Should validate the input to check if it's in the required format.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from '@woowacourse/mission-utils';
+import { QUERIES } from './constants.js';
+
 class App {
-  async run() {}
+  async run() {
+    const inputString = await Console.readLineAsync(QUERIES.CAR_NAMES);
+  }
 }
 
 export default App;
