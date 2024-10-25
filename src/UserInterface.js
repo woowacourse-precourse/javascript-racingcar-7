a48f9a4233c6101960206c91c50a30a80aa1c2d3
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-10-25 09:13:52 +0900

    feat: 사용자에게 몇번의 이동을 할것인지 입력받는 기능 추가

--- src/UserInterface.js
+++ src/UserInterface.js
@@ -3,3 +3,7 @@ import { Console } from '@woowacourse/mission-utils';
 export async function inputCarNames() {
   return await Console.readLineAsync('');
 }
+
+export async function inputNumberOfMove() {
+  return await Console.readLineAsync('');
+}
