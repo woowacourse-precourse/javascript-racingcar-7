211cbced374114ac96db1bf59f1d5affd79e7bf4
kch6396 <cjfghks6396@gmail.com>
2024-10-25 18:51:34 +0900

    feat(App): Implement start message prompt in App class
    Add functionality to display the start message
    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
    to prompt the user for input at the beginning of the application.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  run() {
+    Console.print(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+  }
 }
 
 export default App;
