870d82aa3e362107f9bd152f8fec762dc06b8de6
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-10-25 17:40:01 +0900

    feat: 사용자에게 자동차 입력받는 기능 구현

--- src/views/View.js
+++ src/views/View.js
@@ -1,2 +1,7 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import App from '../src/App.js';
+import App from '../App.js';
+
+export function getUserInput() {
+  // 입력을 비동기적으로 받아오는 함수
+  return MissionUtils.Console.readLineAsync(); // promist 반환
+}
