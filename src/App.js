ceb0340a1d1d528a88918024bb096fa1b50f566e
dragonite-lee <ross1222@naver.com>
2024-10-25 19:43:03 +0900

    feat(App): 시도할 횟수만큼 모든 차들이 움직이고 기록 - 경주할 자동차 딕셔너리로 분리 - 시도할 횟수 isNaN처리 - 수행하는 라운드 함수 처리 - 숫자를 기반으로 -로 보여주는 함수 처리 - 딕셔너리를 기반으로 우승자 출력 함수 처리

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
@@ -13,20 +13,61 @@ class App {
       // ERROR 경주할 자동차를 입력해주세요.
     }
 
-    if (try_input <= 0) {
+    if (isNaN(try_input) || try_input <= 0) {
       // ERROR 1 이상의 숫자부터 입력가능
+      throw error('111');
     }
-    
-    const participant_list = participant_input.split(',');
-    
+
+    const participant_dict = participant_input
+      .split(',')
+      .reduce((acc, name) => {
+        acc[name] = 0;
+        return acc;
+      }, {});
+
+    Console.print('\n실행 결과');
+    let count = 0;
+    while (count < try_input) {
+      this.isPerfomeRound(participant_dict);
+      count++;
+    }
+    this.isShowWinner(participant_dict);
+  }
+
+  isPerfomeRound(participants) {
+    Object.keys(participants).forEach((car) => {
+      if (this.isRamdomAbove(4)) {
+        participants[car] += 1;
+      }
+    });
+
+    this.isShowRound(participants);
   }
 
   isRamdomAbove(threshold) {
     const random_value = MissionUtils.Random.pickNumberInRange(0, 9);
     if (random_value >= threshold) {
-      return true
+      return true;
     }
-    return false
+    return false;
+  }
+
+  isShowRound(participants) {
+    Object.entries(participants).forEach(([car, progress]) => {
+      Console.print(`${car} : ${'-'.repeat(progress)}`);
+    });
+    Console.print('\n');
+  }
+
+  isShowWinner(participants) {
+    const max_progress = Math.max(...Object.values(participants));
+    let winner = [];
+    Object.entries(participants).forEach(([car, progress]) => {
+      if (progress === max_progress) {
+        winner.push(car);
+      }
+    });
+    Console.print(`최종 우승자 : ${winner.join(', ')}`);
   }
 }
 
