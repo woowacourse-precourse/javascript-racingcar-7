19eedcfddffe2eb54e643b594859aab46500205e
최영빈 <bin778@naver.com>
2024-10-25 15:38:14 +0900

    refactor(Error): 에러 메시지 따로 분리

--- src/Component/Check.js
+++ src/Component/Check.js
@@ -1,21 +1,20 @@
+import { ERR_LENGTH, ERR_POSITIVE, ERR_ISNUMBER } from "./Error.js";
+
 export const checkCarNames = (car_names_arr) => {
   const str_len_check = car_names_arr.some(
     (str) => str.length <= 0 || str.length >= 6
   );
 
   // 자동차 이름 길이가 1~5 사이인지 검사
-  if (str_len_check)
-    throw new Error("[ERROR] 자동차 이름을 1~5자 사이로 입력해야 합니다.");
+  if (str_len_check) ERR_LENGTH();
 };
 
 export const checkAttemptNumber = (attempt_number) => {
   const reg = /^[0-9\s]*$/;
 
   // 1 이상의 값을 입력했는지 검사
-  if (attempt_number <= 0)
-    throw new Error("[ERROR] 1 이상의 값을 입력해야 합니다.");
+  if (attempt_number <= 0) ERR_POSITIVE();
 
   // 입력한 데이터가 숫자인지 아닌지 검사
-  if (!reg.test(attempt_number))
-    throw new Error("[ERROR] 숫자만 입력이 가능합니다.");
+  if (!reg.test(attempt_number)) ERR_ISNUMBER();
 };
---
+++ src/Component/Error.js
@@ -0,0 +1,13 @@
+const ERR_MESSAGE = "[ERROR]";
+
+export const ERR_LENGTH = () => {
+  throw new Error(ERR_MESSAGE + " 자동차 이름은 1~5자 사이로 입력해야 합니다.");
+};
+
+export const ERR_POSITIVE = () => {
+  throw new Error(ERR_MESSAGE + " 1 이상의 값을 입력해야 합니다.");
+};
+
+export const ERR_ISNUMBER = () => {
+  throw new Error(ERR_MESSAGE + " 숫자만 입력이 가능합니다.");
+};
