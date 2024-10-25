52e4124dabf9cc38ce89c8c270c6b2077750a6ed
thumbthing <ilscc0902@gmail.com>
2024-10-26 02:45:13 +0900

    Feat: 레이스 진행 상황 출력 기능 구현

--- src/feature/raceProgression.js
+++ src/feature/raceProgression.js
@@ -1,13 +1,39 @@
-function raceProgression() {
+import { Console } from "@woowacourse/mission-utils";
+import goStopResult from "./goStopResult.js";
+
+function raceProgression(carList, initialRaceHistory, trialCount) {
   /** TODO: 
-   * 1. 매개 변수로 레이스 초기 데이터, 시행 횟수를 받는다
+   * 1. 매개 변수로 자동차 배열, 레이스 초기 데이터, 시행 횟수를 받는다
    * 2. 레이스 결과를 저장할 레이스 데이터를 선언
-   * 2. 레이스 초기 데이터의 키값을 배열로 선언 [...레이서.keys()]
    * 3. 
-   * . 반복문을 활용하여 시행 횟수와 레이서의 수만큼 레이스 결과를 실행한다
+   * 4. 반복문을 활용하여 시행 횟수와 레이서의 수만큼 레이스 결과를 실행한다
    *  - goStopResult의 결과를 변수에 저장
    *  - 해당 결과를 레이스 데이터에 set
    *  - 한 사이클을 돌은 결과를 출력
-   * 9. 레이스 결과를 반환
+   * 5. 레이스 결과를 반환
    */
+
+  const RACE_PROGRESS = initialRaceHistory;
+  
+  for(let count = 0; count < trialCount; count++) {
+    let singleRace = '';
+
+    if(count !== 0) {
+      singleRace = `\n`;
+    }
+
+    carList.forEach((car) => {
+      const GO_STOP = goStopResult();
+      const PROGRESS_RESULT = `${RACE_PROGRESS.get(car)}${GO_STOP}`;
+
+      RACE_PROGRESS.set(car, PROGRESS_RESULT);
+      singleRace = `${singleRace}
+      ${car} : ${PROGRESS_RESULT}`;
+    });
+    Console.print(singleRace);
+  };
+
+  return RACE_PROGRESS;
 };
+
+export default raceProgression;
