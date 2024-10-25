9a54d607a8af30dd2f19c8348b445de1a2326e2c
juhui88 <sand8594@naver.com>
2024-10-25 09:34:09 +0900

    refactor: Extract logRaceResult function
    - 각 시행횟수 별로 Race의 결과를 출력하는 함수 추출

--- src/Race.js
+++ src/Race.js
@@ -1,78 +1,85 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class Car {
-    constructor(name) {
-        this.name = name;
-        this.distance = 0;
-    }
+  constructor(name) {
+    this.name = name;
+    this.distance = 0;
+  }
 
-    generateRandomNumber() {
-        const randomValue = MissionUtils.Random.pickNumberInRange(0, 9);
-        return randomValue
-    }
+  generateRandomNumber() {
+    const randomValue = MissionUtils.Random.pickNumberInRange(0, 9);
+    return randomValue;
+  }
 
-    canMoveForward(randomValue) {
-        if (randomValue >= 4) return true
-        return false
-    }
+  canMoveForward(randomValue) {
+    if (randomValue >= 4) return true;
+    return false;
+  }
 
-    move() {
-        const randomValue = this.generateRandomNumber()
-        if (this.canMoveForward(randomValue)) this.distance += 1
-    }
+  move() {
+    const randomValue = this.generateRandomNumber();
+    if (this.canMoveForward(randomValue)) this.distance += 1;
+  }
 
-    getDistance() {
-        return this.distance
-    }
+  getDistance() {
+    return this.distance;
+  }
 }
 
 export default class Race {
-    constructor(input, tries) {
-        this.cars = this.getValidCarNames(input)
-        this.tries = this.convertToNumber(tries)
+  constructor(input, tries) {
+    this.cars = this.getValidCarNames(input);
+    this.tries = this.convertToNumber(tries);
+  }
+
+  splitInputByComma(input) {
+    return input.split(",").map((name) => name.trim());
+  }
+
+  inValidCarNameLength(carName) {
+    if (carName.length <= 5) return true;
+    throw new Error("[ERROR] 자동차의 이름은 5자 이하여야 합니다.");
+  }
+
+  getValidCarNames(input) {
+    const carNames = this.splitInputByComma(input);
+    carNames.forEach((carName) => {
+      this.inValidCarNameLength(carName);
+    });
+    return carNames.map((car) => new Car(car));
+  }
+
+  convertToNumber(tries) {
+    const tryNum = Number(tries);
+    if (isNaN(tryNum) || tryNum < 0) {
+      // 변환 값이 NaN이거나 음수인 경우 Error
+      throw new Error("[ERROR] 0 이상의 숫자를 입력해주세요");
     }
+    return tryNum;
+  }
 
-    splitInputByComma(input) {
-        return input.split(",").map(name => name.trim())
+  start() {
+    MissionUtils.Console.print("실행 결과");
+    for (let i = 0; i < this.tries; i++) {
+      this.logRaceResult();
+      MissionUtils.Console.print("");
     }
+  }
+  logRaceResult() {
+    this.cars.forEach((car) => {
+      car.move();
+      MissionUtils.Console.print(
+        `${car.name} : ${"-".repeat(car.getDistance())}`
+      );
+    });
+  }
 
-    inValidCarNameLength(carName) {
-        if (carName.length <= 5) return true
-        throw new Error("[ERROR] 자동차의 이름은 5자 이하여야 합니다.")
-    }
+  logWinners() {
+    const maxDistance = Math.max(...this.cars.map((car) => car.distance));
+    const winners = this.cars
+      .filter((car) => car.distance === maxDistance)
+      .map((car) => car.name);
 
-    getValidCarNames(input) {
-        const carNames = this.splitInputByComma(input)
-        carNames.forEach(carName => {
-            this.inValidCarNameLength(carName)
-        });
-        return carNames.map(car => new Car(car))
-    }
-
-    convertToNumber(tries) {
-        const tryNum = Number(tries)
-        if (isNaN(tryNum) || tryNum < 0) {  // 변환 값이 NaN이거나 음수인 경우 Error
-            throw new Error("[ERROR] 0 이상의 숫자를 입력해주세요")
-        }
-        return tryNum
-    }
-
-    start() {
-        MissionUtils.Console.print("실행 결과")
-        for (let i = 0; i < this.tries; i++) {
-            this.cars.forEach((car) => {
-                car.move()
-                MissionUtils.Console.print(`${car.name} : ${'-'.repeat(car.getDistance())}`)
-            })
-            MissionUtils.Console.print("")
-        }
-    }
-
-    logWinners() {
-        const maxDistance = Math.max(...this.cars.map((car) => car.distance))
-        const winners = this.cars.filter((car) => car.distance === maxDistance).map((car) => car.name)
-
-
-        MissionUtils.Console.print(`최종 우승자 : ${winners.join(", ")}`)
-    }
+    MissionUtils.Console.print(`최종 우승자 : ${winners.join(", ")}`);
+  }
 }
