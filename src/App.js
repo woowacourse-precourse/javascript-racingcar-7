c25247ec8284b790be9c28e003f35286e1b83dc4
Jiny <moc24@naver.com>
2024-10-25 23:49:19 +0900

    feat(App.js) : 자동차 이름 입력 및 쉼표(,) 기준으로 구분

--- src/App.js
+++ src/App.js
@@ -7,7 +7,23 @@ class App {
 
 
   async run() {
+    try{
+      const cars = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'); // pobi,woni,jun
+      // *자동차는 (,) 쉼표를 기준으로 배열로 만들어준다.
+      const carInputNameArray = cars.split(',');// [pobi,woni,jun]
+      console.log(carInputNameArray);
 
+      // * 자동차 객체 배열을 만든다. 
+      const carArray = carInputNameArray.map( 
+        (nameElement, index) => (new Car({name:nameElement}))
+      );
+
+
+      
+    
+    } catch(error) {
+      return Promise.reject(error); // 예외 던지기
+    }
   }
 }
 
