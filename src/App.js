aa94f62a1f8514024daea541ec1eed0ebd9f1e8e
hoyyChoi <hoyeon8165@gmail.com>
2024-10-25 12:22:29 +0900

    feat(App): 문자열 입력 및 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const carInput = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    const numberInput = await Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    );
+
+    Console.print(carInput);
+    Console.print(numberInput);
+  }
 }
 
 export default App;
