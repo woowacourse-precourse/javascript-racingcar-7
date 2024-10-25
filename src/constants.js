7d4f272ce7c16c2d5529eb3ab313f333df142dbe
jeheecheon <jeheecheon@gmail.com>
2024-10-26 05:57:58 +0900

    style: 객체의 속성을 따옴표로 불필요하게 감싼 것 제거
    ERROR_DETAILS와 QUERIES 객체들의 속성에 불필요하게 감싼 따옴표 모두 제거

--- src/constants.js
+++ src/constants.js
@@ -1,21 +1,21 @@
 const QUERIES = Object.freeze({
-  'CAR_NAMES': '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
-  'SECONDS': '시도할 횟수는 몇 회인가요?',
+  CAR_NAMES: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+  SECONDS: '시도할 횟수는 몇 회인가요?',
 });
 
 const ERROR_HEAD = '[ERROR]';
 
 const ERROR_DETAILS = Object.freeze({
-  'CARNAMES_NOT_ARRAY': `${ERROR_HEAD}: carNames must be an array.`,
-  'CARNAMES_EMPTY': `${ERROR_HEAD}: carNames must not be empty.`,
-  'CARNAMES_NOT_STRING': `${ERROR_HEAD}: carName must be a string.`,
-  'CARNAME_EMPTY': `${ERROR_HEAD}: carName must not be empty.`,
-  'CARNAME_LENGTH': `${ERROR_HEAD}: carName must be between 1 and 5 characters long.`,
-  'CARNAMES_DUPLICATE': `${ERROR_HEAD}: carNames must not have duplicates.`,
-  'SECONDS_NAN': `${ERROR_HEAD}: seconds must be a number.`,
-  'SECONDS_NOT_SAFE_INTEGER': `${ERROR_HEAD}: seconds must be a safe integer.`,
-  'SECONDS_NOT_POSITIVE': `${ERROR_HEAD}: seconds must be a positive number.`,
-  'INVALID_SECOND': `${ERROR_HEAD}: Second must be a non-negative number less than or equal to the total number of seconds.`,
+  CARNAMES_NOT_ARRAY: `${ERROR_HEAD}: carNames must be an array.`,
+  CARNAMES_EMPTY: `${ERROR_HEAD}: carNames must not be empty.`,
+  CARNAMES_NOT_STRING: `${ERROR_HEAD}: carName must be a string.`,
+  CARNAME_EMPTY: `${ERROR_HEAD}: carName must not be empty.`,
+  CARNAME_LENGTH: `${ERROR_HEAD}: carName must be between 1 and 5 characters long.`,
+  CARNAMES_DUPLICATE: `${ERROR_HEAD}: carNames must not have duplicates.`,
+  SECONDS_NAN: `${ERROR_HEAD}: seconds must be a number.`,
+  SECONDS_NOT_SAFE_INTEGER: `${ERROR_HEAD}: seconds must be a safe integer.`,
+  SECONDS_NOT_POSITIVE: `${ERROR_HEAD}: seconds must be a positive number.`,
+  INVALID_SECOND: `${ERROR_HEAD}: Second must be a non-negative number less than or equal to the total number of seconds.`,
 });
 
 export {
