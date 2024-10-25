c8689fcf506d6fbee63a1bcf02db996196285021
moonseonghui <msh9980@naver.com>
2024-10-25 22:05:05 +0900

    refactor(messages): ERROR_MESSAGE에 숫자 상수 적용
    자동차 이름 길이 제한과 시도 횟수 제한을 상수로 치환하여 ERROR_MESSAGE 개선

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,4 +1,10 @@
-import { MAX_CAR_COUNT } from './constants.js';
+import {
+  MAX_CAR_COUNT,
+  MIN_CAR_NAME_LEN,
+  MAX_CAR_NAME_LEN,
+  MIN_TRY_COUNT,
+  MAX_TRY_COUNT,
+} from './constants.js';
 
 const PRINT_MESSAGES = {
   INPUT: {
@@ -14,12 +20,10 @@ const PRINT_MESSAGES = {
 const ERROR_MESSAGE = {
   INVALID_CAR_INPUT:
     '[ERROR] 자동차 이름은 알파벳만 사용하고 이름들은 쉼표(,)로 구분되어야 합니다.',
-  INVALID_CAR_NAME_LENGTH:
-    '[ERROR] 자동차의 이름은 1자 이상 5자 이하로 구성되어야 합니다.',
+  INVALID_CAR_NAME_LENGTH: `[ERROR] 자동차의 이름은 ${MIN_CAR_NAME_LEN}자 이상 ${MAX_CAR_NAME_LEN}자 이하로 구성되어야 합니다.`,
   DUPLICATE_NAME: '[ERROR] 중복된 자동차 이름은 허용되지 않습니다.',
   INVALID_NUMBER_INPUT: '[ERROR] 시도 횟수는 숫자만 입력 가능합니다.',
-  INVALID_NUMBER_RANGE:
-    '[ERROR] 시도 횟수는 1이상 300이하의 숫자만 입력 가능합니다.',
+  INVALID_NUMBER_RANGE: `[ERROR] 시도 횟수는 ${MIN_TRY_COUNT}이상 ${MAX_TRY_COUNT}이하의 숫자만 입력 가능합니다.`,
   NULL_INPUT: '[ERROR] 입력값이 비어 있습니다. 값을 입력해 주세요.',
   LIMIT_CAR_COUNT: `[ERROR] 자동차 개수는 최대 ${MAX_CAR_COUNT}대를 초과할 수 없습니다.`,
 };
