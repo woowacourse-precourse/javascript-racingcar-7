6adb8f0bf822d31fc81be4b19c44c9b6bfa04c45
Yena Kim <120614713+ynkim-03@users.noreply.github.com>
2024-10-26 01:49:56 +0900

    feat: 자동차 이름, 경주 시도 횟수 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const carNames = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const raceCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+  };
 }
 
 export default App;
