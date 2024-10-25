42861c8af9e686c64381316c1786422deb801c4b
gunyongbok <qhrrjsdyd123@gmail.com>
2024-10-25 14:56:14 +0900

    feat: Add validation for car name length

--- src/utils/ValidateCarNamesInput.js
+++ src/utils/ValidateCarNamesInput.js
@@ -3,6 +3,9 @@ export default class ValidateCarNamesInput {
     const names = carNames.split(",");
 
     this.validateNumberOfCars(names);
+    names.forEach((name) => {
+      this.validateCarNameLength(name);
+    });
   }
 
   static validateNumberOfCars(names) {
@@ -10,5 +13,10 @@ export default class ValidateCarNamesInput {
       throw new Error(
         "[ERROR] 경주할 자동차는 최소 2대 이상, 최대 10대까지 가능합니다."
       );
+  }
+
+  static validateCarNameLength(name) {
+    if (name.length > 5)
+      throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
   }
 }
