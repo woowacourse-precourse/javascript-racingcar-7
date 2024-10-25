3b194c3d445dbed0381dc49728155d8d3b996087
deokbin <deokbin@naver.com>
2024-10-25 17:11:37 +0900

    feat<Car> : 클래스에 랜덤 숫자 메서드 추가

--- src/utils/Car.js
+++ src/utils/Car.js
@@ -1,10 +1,18 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class Car {
   constructor(name) {
     this.name = name;
     this.position = '';
   }
 
-  move(randomNumber) {
+  getRandomNum() {
+    const num = MissionUtils.Random.pickNumberInRange(0, 9);
+    return num;
+  }
+
+  move() {
+    const randomNumber = this.getRandomNum();
     if (randomNumber >= 4) {
       this.position += '-';
     }
