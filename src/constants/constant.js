faa5d7e89094599a50dff75e454a0225b1401518
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-25 22:21:05 +0900

    refactor: 에외조건 상수화 제거

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,11 +1,5 @@
-const CARRACE_START_CONDITION = Object.freeze({
-  carNumber: 2,
-  carNameMaxlength: 5,
-  attemptMaxnumber: 30,
-});
-
 const LETTER = Object.freeze({
   runResult: "-",
 });
 
-export { CARRACE_START_CONDITION, LETTER };
+export default LETTER;
