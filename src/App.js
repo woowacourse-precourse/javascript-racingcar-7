b181a4417d6264ce469f3fa3caa6ab5a6a77ffc9
sooinice <sooin9674@naver.com>
2024-10-25 15:07:18 +0900

    feat : 자동차 이름 및 경주횟수 검증

--- src/App.js
+++ src/App.js
@@ -5,9 +5,27 @@ class App {
     const carNamesInput = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
+    const carNames = this.validateCarNames(carNamesInput);
     const raceRoundsInput = await MissionUtils.Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?\n"
     );
+    const rounds = this.validateRaceRounds(raceRoundsInput);
+  }
+
+  validateCarNames(input) {
+    const carNames = input.split(",").map((name) => name.trim());
+    if (carNames.some((name) => name.length === 0 || name.length > 5)) {
+      throw new Error("[ERROR] 자동차 이름이 5자 이하가 아님");
+    }
+    return carNames;
+  }
+
+  validateRaceRounds(input) {
+    const rounds = parseInt(input, 10);
+    if (rounds <= 0) {
+      throw new Error("[ERROR] 시도횟수가 양의 정수가 아님");
+    }
+    return rounds;
   }
 }
 
