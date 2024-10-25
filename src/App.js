dc9658e5b62f93c5b2fef792a28c5ab14bcedda3
sooinice <sooin9674@naver.com>
2024-10-25 14:57:36 +0900

    refactor : 자동차 이름 및 경기 횟수 변수명 변경

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const carNames = await MissionUtils.Console.readLineAsync(
+    const carNamesInput = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
-    const raceRounds = await MissionUtils.Console.readLineAsync(
+    const raceRoundsInput = await MissionUtils.Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?\n"
     );
   }
