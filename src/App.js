c95705ad432677760f91511c9d90736ac94a8300
KimYoungWoo <rladuddn3924@gmail.com>
2024-10-25 12:56:10 +0900

    feat: App 클래스 업데이트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { Console } from '@woowacourse/mission-utils';
+
+import Input from './Input.js';
+import Racing from './Racing.js';
+
+import { validateCars, validateCount } from './validate.js';
+import { COMMON_MESSAGE } from './message.js';
+
 class App {
-  async run() {}
+  async run() {
+    const inputCars = new Input(validateCars.validation);
+    await inputCars.enterValue(COMMON_MESSAGE.INPUT_CARS);
+
+    const inputCount = new Input(validateCount.validation);
+    await inputCount.enterValue(COMMON_MESSAGE.INPUT_COUNT);
+
+    const racing = new Racing(inputCars, +inputCount);
+    const racingResult = racing.doRace();
+
+    this.printChampions(racingResult, +inputCount);
+  }
+
+  printChampions(carModels, count) {
+    const WinnerCars = carModels.filter((car) => car.movementCount === count);
+    const WinnerCarNames = WinnerCars.map((car) => car.name);
+
+    Console.print(`${COMMON_MESSAGE.OUTPUT} ${WinnerCarNames.join(", ")}`);
+  }
 }
 
 export default App;
