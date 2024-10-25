04062fedd40c96391b0a5ade1e58bed607ee2127
deokbin <deokbin@naver.com>
2024-10-25 17:12:51 +0900

    feat<App> : 요구사항에 맞게 출력 양식 수정

--- src/App.js
+++ src/App.js
@@ -6,19 +6,24 @@ import Car from './utils/Car.js';
 class App {
   async run() {
     const carInput = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n',
     );
     const isNameValid = NameValidation.isValid(carInput.trim());
-    const countInput = await Console.readLineAsync('시도횟수를 입력해주세요.');
+    const countInput =
+      await Console.readLineAsync('시도할 횟수는 몇 회인가요? \n');
     const isNumValid = NumValidation.isValidNum(countInput.trim());
     if (isNameValid && isNumValid) {
       const carList = carInput.split(',').map(name => new Car(name));
 
-      for (let i = 0; i < countInput; i++) {
+      Console.print('실행 결과 \n');
+
+      for (let i = 0; i < Number(countInput); i++) {
         carList.forEach(car => {
+          car.getRandomNum();
           car.move();
           Console.print(car.getPosition());
         });
+        Console.print('');
       }
     }
   }
