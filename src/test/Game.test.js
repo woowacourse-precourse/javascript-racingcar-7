b65118e55a618f1af0accc05a4c4b1328b7193f4
jongbin26 <010jjj@naver.com>
2024-10-26 01:07:51 +0900

    test(Game.test): Record 클래스 추출로 인한 테스트 코드 변경

--- src/test/Game.test.js
+++ src/test/Game.test.js
@@ -1,14 +1,14 @@
-import { Game } from "../Game.js";
+import { Game } from '../Game.js';
+import { Record } from '../Record.js';
 
 test('update 함수 테스트', () => {
-    const game = new Game('pobi,woni,jun', 3);
-    const movingCars = ['pobi', 'woni'];
-    const nextRecord = {
-        pobi: 1,
-        woni: 1,
-        jun: 0,
-    }
-    game.update(movingCars);
-    const result = game.record;
-    expect(result).toEqual(nextRecord);
-})
+  const movingCars = ['pobi', 'woni'];
+  const record = new Record('pobi,woni,jun');
+  const nextRecord = {
+    pobi: 1,
+    woni: 1,
+    jun: 0,
+  };
+  record.update(movingCars);
+  expect(record.table).toEqual(nextRecord);
+});
