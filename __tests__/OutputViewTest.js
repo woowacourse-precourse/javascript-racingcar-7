61d773c59eae9a50afa9b370a3690725eb839470
Seio924 <seio9241@gmail.com>
2024-10-25 17:28:08 +0900

    test(OutputViewTest): 전진 여부 출력 테스트 추가

--- __tests__/OutputViewTest.js
+++ __tests__/OutputViewTest.js
@@ -11,4 +11,30 @@ describe('OutputView', () => {
 
     expect(logSpy).toHaveBeenCalledWith(log);
   });
+
+  test('자동차의 전진 여부를 출력한다', () => {
+    const carName = 'pobi';
+    const forwardCount = 3;
+    const log = 'pobi : ---';
+    const logSpy = getLogSpy();
+
+    const outputView = new OutputView();
+    outputView.printCarProgress(carName, forwardCount);
+
+    expect(logSpy).toHaveBeenCalledWith(log);
+  });
+
+  test('모든 자동차의 전진 여부를 출력한다', () => {
+    const carName = ['pobi', 'woni'];
+    const forwardCount = [3, 1];
+    const logs = ['pobi : ---', 'woni : -'];
+    const logSpy = getLogSpy();
+
+    const outputView = new OutputView();
+    outputView.printAllCarProgress(carName, forwardCount);
+
+    logs.forEach(log => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
 });
