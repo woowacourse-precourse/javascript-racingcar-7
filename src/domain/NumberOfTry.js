bafc3b602fd43380c2a653babc6d053e79f15c1f
8leamnk <silkviewrk@gmail.com>
2024-10-25 20:52:22 +0900

    feat: 시도할 횟수 (src/domain/NumberOfTry)
    시도할 횟수 클래스 추가:
    - 인스턴스 생성 시 입력값을 매개변수에 넣어서 유효성 검사
    - getNumberOfTry을 호출 시 변환된 숫자를 반환

---
+++ src/domain/NumberOfTry.js
@@ -0,0 +1,46 @@
+class NumberOfTry {
+  static #ERROR_MESSAGE = {
+    empty: '[ERROR] 빈 문자열입니다.',
+    character: '[ERROR] 숫자가 아닌 문자를 입력했습니다.',
+    range: '[ERROR] 1 이상의 숫자를 입력해주세요.',
+  };
+
+  static #ONLY_NUMBER = /^-?\d+$/;
+
+  #input;
+
+  constructor(input) {
+    this.#input = input;
+    this.#validate();
+  }
+
+  #validate() {
+    this.#validateEmpty();
+    this.#validateCharacter();
+    this.#validateRange();
+  }
+
+  #validateEmpty() {
+    if (this.#input.length === 0) {
+      throw new Error(NumberOfTry.#ERROR_MESSAGE.empty);
+    }
+  }
+
+  #validateCharacter() {
+    if (!NumberOfTry.#ONLY_NUMBER.test(this.#input)) {
+      throw new Error(NumberOfTry.#ERROR_MESSAGE.character);
+    }
+  }
+
+  #validateRange() {
+    if (Number(this.#input) <= 0) {
+      throw new Error(NumberOfTry.#ERROR_MESSAGE.range);
+    }
+  }
+
+  getNumberOfTry() {
+    return Number(this.#input);
+  }
+}
+
+export default NumberOfTry;
