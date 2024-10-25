f2375c8f0d0192601eeabef489b6ffec35a4dbc3
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-10-25 23:19:25 +0900

    chore: 게임 진행 메시지에 '실행 결과' 추가

--- src/Message.js
+++ src/Message.js
@@ -2,5 +2,6 @@ export const GAME_PROGRESS_MESSAGE = Object.freeze({
   ENTER_CAR_NAMES:
     '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
   ENTER_TRIAL_COUNT: '시도할 횟수는 몇 회인가요?',
+  EXECUTION_RESULT: '실행 결과',
   FINAL_WINNER: '최종 우승자',
 });
