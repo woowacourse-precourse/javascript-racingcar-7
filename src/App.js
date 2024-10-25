4c6826ef6a82f9cceaaa8001a932cd1baa834e1b
ddozza54 <bananamayong@gmail.com>
2024-10-25 16:31:00 +0900

    feat: 경주할 자동차 이름과 시도할 횟수 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const Console = MissionUtils.Console;
+    const Random = MissionUtils.Random;
+
+    try {
+      const inputNames = await Console.readLineAsync(
+        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
+      );
+      const cars = inputNames.split(',');
+      cars.map((name) => {
+        if (name.length > 5) {
+          throw Error(
+            '자동차 이름은 5자 이하만 가능합니다.'
+          );
+        }
+      });
+      Console.print(cars);
+      const numberOfTry = Number(
+        await Console.readLineAsync(
+          '시도할 횟수는 몇 회인가요?'
+        )
+      );
+      Console.print(numberOfTry);
+    } catch (error) {
+      // reject 되는 경우
+      throw error;
+    }
+  }
 }
 
 export default App;
