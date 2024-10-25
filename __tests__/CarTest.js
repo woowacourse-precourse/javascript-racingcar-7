58d46c93a5b8e988086f4712e85d1605f2b164fb
TaewoooongHeo01 <chamjoeun0111@gmail.com>
2024-10-25 16:12:18 +0900

    test(Car): 자동차 이름 유효성 검사 함수 테스트 추가

--- __tests__/CarTest.js
+++ __tests__/CarTest.js
@@ -1,21 +1,21 @@
-// import Car from '../src/Car';
+import Car from '../src/Car';
 
-// describe('자동차 클래스', () => {
-//   test.each([
-//     ['pobi', true],
-//     [' jun ', true],
-//     ['pobiwonijun', new Error('[ERROR]')],
-//   ])('자동차 클래스 생성자에서 이름 유효성 검사 테스트', (inputs, expected) => {
-//     //given
-//     const carName = inputs;
+describe('자동차 클래스', () => {
+  test.each([
+    ['pobi', true],
+    [' jun ', true],
+    ['pobiwonijun', new Error('[ERROR]')],
+  ])('자동차 클래스 생성자에서 이름 유효성 검사 테스트', (inputs, expected) => {
+    //given
+    const carName = inputs;
 
-//     //when
-//     if (expected instanceof Error) {
-//       //then
-//       expect(() => new Car(carName)).toThrow('[ERROR]');
-//     } else {
-//       //then
-//       expect(() => new Car(carName)).not.toThrow();
-//     }
-//   });
-// });
+    //when
+    if (expected instanceof Error) {
+      //then
+      expect(() => new Car(carName)).toThrow('[ERROR]');
+    } else {
+      //then
+      expect(() => new Car(carName)).not.toThrow();
+    }
+  });
+});
