51f575a2919bdff722d12544aea81ceb7c591b49
moonseonghui <msh9980@naver.com>
2024-10-26 01:55:45 +0900

    refactor(getValidatedTryCount): 횟수 입력과 유효성 검사를 getValidatedTryCount 함수로 분리

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import { joinByComma } from './utils/stringUtils.js';
 import Car from './car/Car.js';
 import RacingGame from './racingGame/RacingGame.js';
-import { getUserInput, printKeyValueFormat } from './utils/ioModule.js';
-import { validateTryCount } from './validator/validatePipeline.js';
+import { printKeyValueFormat } from './utils/ioModule.js';
 import { PRINT_MESSAGES } from './constants/messages.js';
 import getValidatedCarNames from './functions/getValidatedCarNames.js';
+import getValidatedTryCount from './functions/getValidatedTryCount.js';
 
 class App {
   async run() {
     const carNamesArray = await getValidatedCarNames();
-
-    const inputForTryCount = await getUserInput(PRINT_MESSAGES.INPUT.TRY_COUNT);
-    validateTryCount(inputForTryCount);
+    const tryCount = await getValidatedTryCount();
 
     const racingCars = carNamesArray.map((name) => new Car(name));
-    const racingGame = new RacingGame(racingCars, Number(inputForTryCount));
+    const racingGame = new RacingGame(racingCars, Number(tryCount));
     racingGame.play();
     const winners = racingGame.getWinnerNames();
     printKeyValueFormat(PRINT_MESSAGES.OUTPUT.WINNER, joinByComma(winners));
