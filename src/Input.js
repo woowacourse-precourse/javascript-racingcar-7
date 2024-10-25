8746818aaee1e792895442ee6f71686435ca9826
KimYoungWoo <rladuddn3924@gmail.com>
2024-10-25 13:34:05 +0900

    feat: 현재값 반환 로직 추가
    enterValue에서 값을 반환하는 것이 아닌
    getValue 함수를 따로 생성하여 반환하도록 수정

--- src/Input.js
+++ src/Input.js
@@ -1,19 +1,25 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class Input {
+
   constructor(validate) {
     this.validate = validate;
+    this.value = '';
   }
   async enterValue(message) {
     const value = await Console.readLineAsync(message);
 
     this._validateValue(value);
 
-    return value;
+    this.value = value;
   }
 
   _validateValue(value) {
     this.validate(value);
+  }
+
+  getValue() {
+    return this.value;
   }
 }
 
