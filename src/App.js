036dea8dd286ac4c9ede7cde0b6845663fa1e96c
suhwa <tnghk9611@naver.com>
2024-10-25 22:34:48 +0900

    feat: 이름이 5자 이하가 아니라면 예외 처리
    사용자의 입력에서 ,를 구분자로 하여 잘랐을 때 각각의 자동차 이름의 길이가 5자 이하가 아니면 [ERROR] 문구로 시작하는 에러 객체를 생성

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ class App {
             "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
         );
 
+        const splittedCarNames = inputCarNames.split(",");
+
+        for (let car of splittedCarNames) {
+            if (car.length > 5) {
+                throw new Error("[ERROR] 자동차 이름은 5자 이하로 입력 가능합니다.");
+            }
+        }
+
         const inputTryCnt = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
     }
 }
