da857d4b1073cd7362a477f874a9cc24cb32dca5
Sohee <sh11govlday@gmail.com>
2024-10-25 12:11:21 +0900

    feat(App) : 자동차 이름 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const carNames = (await Console.readLineAsync('')).split(',');
+
+      const nameLengthCheck = carNames.some((name) => name.length > 5);
+      if (nameLengthCheck) {
+        throw new Error('[ERROR] 자동차 이름은 5자 이하여야 합니다.');
+      }
+    } catch (error) {
+      Console.print(error.message);
+      throw error;
+    }
+  }
 }
 
 export default App;
