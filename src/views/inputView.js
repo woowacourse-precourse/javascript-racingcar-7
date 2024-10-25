93eec4f85eb1071deda65894fe341d638373b428
HS <hasang2691@naver.com>
2024-10-25 20:05:32 +0900

    feat(inputView.js): 입력된 자동차 이름을 쉼표 기준으로 구분하는 기능 구현
    - 입력받은 문자열을 쉼표로 분리하고 공백 제거 후 그 배열을 반환하는 parseCarNames 메서드 구현
    - readCarNames에서 parseCarNames 메서드를 사용하여 return

--- src/views/inputView.js
+++ src/views/inputView.js
@@ -6,7 +6,11 @@ const InputView = {
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
     );
 
-    return input;
+    return this.parseCarNames(input);
+  },
+
+  parseCarNames(input) {
+    return input.split(',').map((name) => name.trim());
   },
 };
 
