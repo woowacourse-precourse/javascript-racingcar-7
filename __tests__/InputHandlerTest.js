e6c1ac912709c39f351349168efe5cd77f43a157
hyunyeee <5004ohy@naver.com>
2024-10-25 16:46:34 +0900

    test(input): 시도 횟수로 공백을 입력한 경우 에러를 던진다

--- __tests__/InputHandlerTest.js
+++ __tests__/InputHandlerTest.js
@@ -1,6 +1,7 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import { ReadName } from "../src/handler/ReadName.js";
-import { INPUT_ERROR_MESSAGE } from "../src/constants/Messages.js";
+import { ReadAttemptCount } from "../src/handler/ReadAttemptCount.js";
+import { ATTEMPT_COUNT_ERROR_MESSAGE, INPUT_ERROR_MESSAGE } from "../src/constants/Messages.js";
 
 const mockReadLineAsync = (input) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -10,10 +11,17 @@ const mockReadLineAsync = (input) => {
 }
 
 describe('InputHandler', () => {
-  test('공백을 입력한 경우 에러를 던진다.', () => {
+  test('ReadName Class 테스트: 이름으로 공백을 입력한 경우 에러를 던진다.', () => {
     const emptyInput = '';
     mockReadLineAsync(emptyInput);
     const input = new ReadName().read();
     expect(input).rejects.toThrow(INPUT_ERROR_MESSAGE);
+  })
+
+  test('ReadAttemptCount Class 테스트: 시도횟수로 공백을 입력한 경우 에러를 던진다.', () => {
+    const emptyInput = '';
+    mockReadLineAsync(emptyInput);
+    const input = new ReadAttemptCount().read();
+    expect(input).rejects.toThrow(ATTEMPT_COUNT_ERROR_MESSAGE);
   })
 })
