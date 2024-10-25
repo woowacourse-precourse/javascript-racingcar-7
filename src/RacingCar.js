da019f587840a7e08fe1ffa10b7ca7d6f1677674
dudcns02 <duddl2640@naver.com>
2024-10-25 20:44:14 +0900

    fix: Update the "split" position Change the input to separate and contain it in "carInput"

--- src/RacingCar.js
+++ src/RacingCar.js
@@ -1,11 +1,12 @@
 import { Console } from "@woowacourse/mission-utils";
-import { carCountLimitCheck } from "./Utils";
+import { carCountLimitCheck } from "./Utils.js";
 import { LOG_MESSAGE } from "./content.js";
 class RacingCar {
   constructor() {}
-  getCarName() {
-    const input = Console.readLineAsync(LOG_MESSAGE.START_MESSAGE, split(","));
-    carCountLimitCheck(input);
+  async getCarName() {
+    const input = await Console.readLineAsync(LOG_MESSAGE.START_MESSAGE);
+    const carInput = input.split(",");
+    carCountLimitCheck(carInput);
   }
 }
 
