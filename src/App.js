f10186e233a195916aeffc99a752b1ffb52c13f2
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-10-26 08:48:42 +0900

    feat: 자동차 경주 게임 실행 기능 추가
    - 자동차 이름과 이동 횟수를 입력 받아 게임을 준비한다.
    - 준비한 데이터로 게임을 진행한다.
    - 게임의 결과를 바탕으로 우승자를 발표한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import progressGame from './GamePlayHandler.js';
+import { prepareCarData, prepareMoveCount } from './GamePrepare.js';
+import pickWinner from './GameResult.js';
+import {
+  announceWinner,
+  inputCarNames,
+  inputNumberOfMove,
+} from './UserInterface.js';
+
 class App {
-  async run() {}
+  async run() {
+    const carNames = await inputCarNames();
+    const carDataList = prepareCarData(carNames);
+
+    const numberOfMove = await inputNumberOfMove();
+    const moveCount = prepareMoveCount(numberOfMove);
+
+    const raceResults = progressGame(carDataList, moveCount);
+    const winner = pickWinner(raceResults);
+
+    announceWinner(winner);
+  }
 }
 
 export default App;
