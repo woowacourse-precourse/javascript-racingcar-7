edc62262581459b155fe7d36f054c37c62480c18
KimYoungWoo <rladuddn3924@gmail.com>
2024-10-25 14:24:21 +0900

    test: 테스트 추가
    message.js에 작성된 예외 처리 케이스 테스트 코드 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -46,15 +46,56 @@ describe("자동차 경주", () => {
     });
   });
 
-  test("예외 테스트", async () => {
+  test.each([
+    ["", "자동차 이름을 입력해주세요!"],
+    ["java,javascript", "각각의 자동차 이름은 5자 이하로 입력해주세요!"],
+    ["java,,rust", "구분자를 연속 입력하였습니다!"],
+    [",java,rust", "구분자는 자동차 이름 사이에 입력되어야 합니다!"],
+    ["java,rust,", "구분자는 자동차 이름 사이에 입력되어야 합니다!"],
+  ])("(%s)는 %s 에러를 반환한다.", async (inputCars, errorMessage) => {
     // given
-    const inputs = ["pobi,javaji"];
-    mockQuestions(inputs);
+    mockQuestions([inputCars]);
+
 
     // when
     const app = new App();
 
     // then
-    await expect(app.run()).rejects.toThrow("[ERROR]");
+    await expect(app.run()).rejects.toThrow("[ERROR] " + errorMessage);
+  });
+
+  test.each([
+    ["java,rust", "", '시도할 횟수를 입력해주세요!'],
+    ["java,rust", "a", '시도할 횟수에는 숫자를 입력해야합니다!'],
+    ["java,rust", "0", '시도할 횟수에는 양의 정수만 입력해주세요!'],
+    ["java,rust", "-1", '시도할 횟수에는 양의 정수만 입력해주세요!'],
+    ["java,rust", "1.1", '시도할 횟수에는 정수만 입력해주세요!'],
+  ])("([%s],%s)는 %s 에러를 반환한다.", async (inputCars, inputCount, errorMessage) => {
+    // given
+    mockQuestions([inputCars,inputCount]);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR] " + errorMessage);
+  });
+
+  test('우승자가 없는 경우 에러를 반환한다.', async () => {
+    // given
+    const MOVING_FORWARD = 4;
+    const STOP = 3;
+    const inputs = ["pobi,woni", "3"];
+    const logs = ["pobi : --", "woni : -"];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([MOVING_FORWARD, STOP, MOVING_FORWARD, MOVING_FORWARD, STOP, STOP]);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR] 우승자가 없습니다!");
   });
 });
