6167b3597b5646e96289d3ae99c6bd1ca90d1fd3
beomjo <dongjun1315@naver.com>
2024-10-25 16:03:36 +0900

    feat: 자동차 이름을 입력할 때 잘못된 입력일 경우 오류 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Car from "./Car.js";
-import { inputNames, splitName } from "./InputValue.js";
+import { inputNames, splitName, inputNameValidation } from "./InputValue.js";
 
 class App {
   async run() {
     const inputs = await inputNames();
     const carNames = splitName(inputs);
+
+    carNames.forEach(name => inputNameValidation(name));
 
     const carList = carNames.map(name => new Car(name));
 
