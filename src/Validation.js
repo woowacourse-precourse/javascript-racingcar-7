c2b54af19b2ac23c0c09f1b04c3586545eb55c85
jeongeunLee <leeju1009@gmail.com>
2024-10-25 17:28:16 +0900

    feat(validation): 자동차 이름의 길이를 검사하는 예외 처리 함수 추가

--- src/Validation.js
+++ src/Validation.js
@@ -7,6 +7,18 @@ class Validation {
     this.#maxNameLength = maxNameLength;
     this.#nameSeparator = nameSeparator;
   }
+
+  #isValidLength(name) {
+    if (name.length > this.#maxNameLength) {
+      throw new Error('[ERROR] 자동차 이름은 5자를 초과할 수 없습니다.');
+    }
+
+    if (name.length < 1) {
+      throw new Error('[ERROR] 자동차 이름은 1자 이상이어야 합니다.');
+    }
+
+    return true;
+  }
 }
 
 export default Validation;
