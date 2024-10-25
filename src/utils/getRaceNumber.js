e72cf886b5bda0fbfb3f33b15fbe09741329590f
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-10-25 16:24:43 +0900

    feat: 경주 횟수 입력 처리

---
+++ src/utils/getRaceNumber.js
@@ -0,0 +1,19 @@
+import { Console } from "@woowacourse/mission-utils";
+import { ERROR_MESSAGES as ERROR } from "../constants/errorMsg.js";
+
+const getRaceNumber = async () => {
+  const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+  const num = Number(input.trim());
+
+  checkIsPositiveInteger(num);
+
+  return num;
+};
+
+const checkIsPositiveInteger = (num) => {
+  if (!Number.isInteger(num) || num <= 0) {
+    throw new Error(ERROR.INVALID_RACE_NUMBER);
+  }
+};
+
+export default getRaceNumber;
