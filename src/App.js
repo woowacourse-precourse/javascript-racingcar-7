975353f8bb03a42d06dca843418eb805448c76a1
haesa <xdhaesa@gmail.com>
2024-10-25 16:11:01 +0900

    feat: 경주할 자동차 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { Console } from '@woowacourse/mission-utils';
+import { ERROR_MESSAGE, INPUT_MESSAGE } from './constants/index.js';
+
+const CAR_NAME_MAX_LENGTH = 5;
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync(INPUT_MESSAGE.entries);
+    const carNames = this.#parseEntries(input);
+  }
+
+  #parseEntries(input) {
+    const carNames = input.split(',');
+    carNames.forEach(this.#validateCarName);
+    return carNames;
+  }
+
+  #validateCarName(name) {
+    if (name === '') {
+      throw Error(ERROR_MESSAGE.carNameWhitespace);
+    }
+
+    if (name.length > CAR_NAME_MAX_LENGTH) {
+      throw Error(ERROR_MESSAGE.carNameLength);
+    }
+  }
 }
 
 export default App;
---
+++ src/constants/error.js
@@ -0,0 +1,4 @@
+export const ERROR_MESSAGE = Object.freeze({
+  carNameWhitespace: '[ERROR] 자동차 이름에 공백은 허용하지 않습니다.',
+  carNameLength: '[ERROR] 자동차 이름은 5자 이하만 가능합니다.',
+});
