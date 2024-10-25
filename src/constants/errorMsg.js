a2e7ce5fd7a1fa949de29fd50f5cd8f35652af1a
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-10-25 16:24:55 +0900

    feat: 에러 메시지 상수화

---
+++ src/constants/errorMsg.js
@@ -0,0 +1,7 @@
+const ERROR_PREFIX = '[Error]';
+
+export const ERROR_MESSAGES = {
+  INVALID_CAR_NAME: `${ERROR_PREFIX} 자동차 이름은 5자 이하만 가능합니다.`,
+  DUPLICATED_CAR_NAME: `${ERROR_PREFIX} 중복된 이름이 존재합니다.`,
+  INVALID_RACE_NUMBER: `${ERROR_PREFIX} 양의 정수만 입력 가능합니다.`,
+}
