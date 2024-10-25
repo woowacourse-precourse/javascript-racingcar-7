e5222c00a6976b25fb9bc7592d9417287a925719
최영빈 <bin778@naver.com>
2024-10-26 02:05:39 +0900

    refactor: 주석 추가하기

--- src/Component/CarRacing.js
+++ src/Component/CarRacing.js
@@ -8,11 +8,13 @@ const indentPrint = () => {
   Console.print("");
 };
 
+// 중간 결과 출력
 const midResultScorePrint = (cur_car_player, cur_car_score) => {
   const cur_car_score_board = "-".repeat(cur_car_score);
   Console.print(cur_car_player + " : " + cur_car_score_board);
 };
 
+// 랜덤 번호에 따라서 자동차 전진 또는 멈춤
 const carScoreGain = (car_names_arr, car_score_arr) => {
   car_names_arr.forEach((_, player) => {
     if (Random.pickNumberInRange(0, 9) >= 4) car_score_arr[player] += 1;
--- src/Component/SearchMaxScore.js
+++ src/Component/SearchMaxScore.js
@@ -4,6 +4,7 @@ const WinnerCarPrint = (winner_car) => {
   Console.print("최종 우승자 : " + winner_car);
 };
 
+// 가장 많이 전진한 자동차 찾기
 export const maxScoreCar = (car_names_arr, car_racing_result) => {
   const max_score = Math.max(...car_racing_result);
   const winner_car = car_names_arr.filter(
