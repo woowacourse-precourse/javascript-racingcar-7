dd79e2d2649778521160790f698963c55a77a560
hoyyChoi <hoyeon8165@gmail.com>
2024-10-25 14:18:01 +0900

    feat(App): handleCarInputString 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
+  carObject = {};
+
   async run() {
     const carInput = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
@@ -9,8 +11,16 @@ class App {
       "시도할 횟수는 몇 회인가요?\n"
     );
 
-    Console.print(carInput);
-    Console.print(numberInput);
+    this.handleCarInputString(carInput);
+  }
+
+  // carInput을 받아서 carObject에 저장하는 함수
+  handleCarInputString(carInput) {
+    const carNames = carInput.split(",");
+    carNames.forEach((carName) => {
+      this.carObject[carName] = 0; // 임시로 0 대입
+    });
+    Console.print(carNames);
   }
 }
 
