571665f505f3bd7c95159ab7672091d60ef5dda5
jongbin26 <010jjj@naver.com>
2024-10-26 01:13:12 +0900

    feat(App): ValidationError 처리를 위한 try-catch문 작성
    - 에러 객체를 에러가 발생할 만한 곳에서 던지고, 적절한 곳에서 받아 처리하는 것이 중요하다는 것을 깨달았습니다.
    - 이를 해결하기 위해 애플리케이션 내에서 발생한 ValidationError를 App.js에서 잡아서 처리하는 로직을 구현했습니다.
    - 미션에서는 에러 발생 시 애플리케이션이 종료돼야 하기 때문에 한 곳에서 잡은 후 다시 에러를 던졌습니다.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { Console } from '@woowacourse/mission-utils';
 import { getGameInput, printGameResult } from './utils/ioHandler.js';
-import { Game } from './Game.js';
+import { ValidationError } from './Error/ValidationError.js';
 import { Record } from './Record.js';
+import { Game } from './Game.js';
 
 class App {
   async run() {
-    const { carString, count } = await getGameInput();
-    const record = new Record(carString);
-    const game = new Game(record, count);
-    game.play();
-    printGameResult(game.getWinner());
+    try {
+      const { carString, count } = await getGameInput();
+      const record = new Record(carString);
+      const game = new Game(record, count);
+      game.play();
+      printGameResult(game.getWinner());
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        throw error;
+      }
+    }
   }
 }
 
