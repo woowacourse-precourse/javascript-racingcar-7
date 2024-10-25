49d17e71a9e3ad2ae58374a7072e7f26bfcef84d
thumbthing <ilscc0902@gmail.com>
2024-10-26 03:54:05 +0900

    Feat: App에 구현 기능 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console } from "@woowacourse/mission-utils";
+import carNameInput from "./UI/carNameInput.js";
+import errorHandler from "./Error/errorHandler.js";
+import trialCountInput from "./UI/trialCountInput.js";
+import createRacerInformation from "./racerData/racer.js";
+import raceProgression from "./feature/raceProgression.js";
+import getWinner from "./feature/getWinner.js";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const CAR_LIST = await carNameInput();
+      const TRIAL_COUNT = await trialCountInput();
+
+      const INITIAL_RACE_HISTORY = createRacerInformation(CAR_LIST);
+      const RACE_PROGRESS = raceProgression(CAR_LIST, INITIAL_RACE_HISTORY, TRIAL_COUNT);
+      getWinner(CAR_LIST, RACE_PROGRESS);
+    } catch (error) {
+      errorHandler(error);
+    }
+  }
 }
 
 export default App;
