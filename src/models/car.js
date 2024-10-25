ff161eadfb9ab817977ed8813696f960fe6635f1
HS <hasang2691@naver.com>
2024-10-25 15:50:08 +0900

    feat(car.js): 자동차 상태 업데이트 구현
    - 4 이상의 무작위 값에서 전진하는 move 메서드 추가
    - 차량의 상태에 접근하기 위한 getter 메서드(getPosition, getName) 추가

--- src/models/car.js
+++ src/models/car.js
@@ -7,6 +7,20 @@ class Car {
     this.name = name;
     this.position = 0;
   }
+
+  move(number) {
+    if (number >= 4) {
+      this.position += 1;
+    }
+  }
+
+  getPosition() {
+    return this.position;
+  }
+
+  getName() {
+    return this.name;
+  }
 }
 
 export default Car;
