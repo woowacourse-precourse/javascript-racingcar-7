70f1ad95de0534ae008c2480cf417a30e1d4cfc6
ChanLee_KR <cl700120@gmail.com>
2024-10-25 17:23:41 +0900

    feat: 레이싱 결과 출력 기능 구현

--- src/view/Output.js
+++ src/view/Output.js
@@ -2,12 +2,38 @@ import { Console } from '@woowacourse/mission-utils';
 
 class Output {
   static OUTPUT_TITLE = '실행 결과';
+  static TRACK_LINE = '-';
 
   /**
    * @returns {void}
    */
   static printTitle() {
     Console.print('\n' + this.OUTPUT_TITLE);
+  }
+
+  /**
+   * @param {string} name
+   * @param {number} track
+   * @returns {string}
+   */
+  static getRacingTrack(name, track) {
+    return `${name} : ${this.TRACK_LINE.repeat(track)}`;
+  }
+
+  /**
+   * @param {Array<{ name: string; track: number; }[]>} racingResult
+   * @returns {void}
+   */
+  static printRacingResult(racingResult) {
+    const result = racingResult
+      .map((round) => {
+        return round
+          .map((car) => this.getRacingTrack(car.name, car.track))
+          .join('\n');
+      })
+      .join('\n\n');
+
+    Console.print(result + '\n');
   }
 }
 
