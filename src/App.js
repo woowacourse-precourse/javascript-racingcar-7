557dedcf247fd7f42b4cde4fb2758577c34af3b0
jeheecheon <jeheecheon@gmail.com>
2024-10-26 06:07:29 +0900

    style: 쌍따옴표에서 홀따옴표로 변경
    코드의 일관성을 위해 쌍따옴표에서 홀따옴표로 변경

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import RacingGame from './RacingGame.js';
 class App {
   async run() {
     const inputString = await Console.readLineAsync(QUERIES.CAR_NAMES);
-    const carNames = inputString.split(",").map((carName) => carName.trim());
+    const carNames = inputString.split(',').map((carName) => carName.trim());
     const race = new RacingGame(carNames);
 
     const seconds = Number(await Console.readLineAsync(QUERIES.SECONDS));
