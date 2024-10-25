4f3a629480e735d088167624c8c138169ff92672
moonseonghui <msh9980@naver.com>
2024-10-25 15:49:54 +0900

    feat(getMaxMoveCount): 전진 횟수가 가장 큰 수 구하는 함수  구현
    게임에서 가장 멀리 전진한 자동차를 결정하기 위해서 최대 전진 횟수 계산이 필요하다.

--- src/racinggame/racingGameUtils.js
+++ src/racinggame/racingGameUtils.js
@@ -2,4 +2,11 @@ const getMovePosition = function getMovePositionFunc(car) {
   return '-'.repeat(car.getMoveCount());
 };
 
-export default getMovePosition;
+const getMaxMoveCount = function getMaxMoveCountFunc(cars) {
+  return cars.reduce(
+    (maxCount, car) => Math.max(maxCount, car.getMoveCount()),
+    0,
+  );
+};
+
+export { getMovePosition, getMaxMoveCount };
