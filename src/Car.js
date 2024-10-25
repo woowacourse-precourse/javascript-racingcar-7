4ebfc3bbe813207e765a532951e790967f54abf9
haesa <xdhaesa@gmail.com>
2024-10-25 17:38:07 +0900

    feat(Car): 자동차 전진 기능

---
+++ src/Car.js
@@ -0,0 +1,31 @@
+import { Random } from '@woowacourse/mission-utils';
+
+const THRESHOLD_FOR_MOVING_FORWARD = 4;
+
+class Car {
+  #name;
+  #distance;
+
+  constructor(name) {
+    this.#name = name;
+    this.#distance = 0;
+  }
+
+  getDistance() {
+    return this.#distance;
+  }
+
+  raceLap() {
+    const randomNumber = Random.pickNumberInRange(0, 9);
+
+    if (randomNumber >= THRESHOLD_FOR_MOVING_FORWARD) {
+      this.#moveForward();
+    }
+  }
+
+  #moveForward() {
+    this.#distance += 1;
+  }
+}
+
+export default Car;
