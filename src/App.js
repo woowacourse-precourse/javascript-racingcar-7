01d3372f7ac9266f2cd084acaca7f1b0277df699
devjaeyoon <devjaeyoon@gmail.com>
2024-10-25 18:13:31 +0900

    feat: 입력받은 경주할 자동차 이름 문자열을 쉼표(,) 기준으로 분리 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import getCarNamesInput from './getCarNamesInput.js';
+import splitStringByComma from './splitIStringByComma.js';
 
 class App {
   async run() {
     const carNamesInput = await getCarNamesInput();
+    const carNames = splitStringByComma(carNamesInput);
   }
 }
 
---
+++ src/splitIStringByComma.js
@@ -0,0 +1,5 @@
+function splitStringByComma(input) {
+  return input.split(',');
+}
+
+export default splitStringByComma;
