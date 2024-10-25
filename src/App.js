3d7d37ef23c0fda99bb072fb85638b6eedacc32f
jeong922 <hwajeong922@naver.com>
2024-10-25 21:04:26 +0900

    feat(App): 입력값 처리 기능 구현
    - getCarNames 메소드에서 유효한 입력값인지 확인하는 로직 구현
    - getCarNames 메소드에서 쉼표(,)를 기준으로 입력값 split 연산 구현
    - getCarNames 메소드에서 유효하지 않은 입력값인 경우 Error를 발생시키는 로직 추가
    - getAttemptCount 메소드에서 입력값이 숫자인지 확인하는 로직 구현
    - getAttemptCount 메소드에서 유효하지 않은 입력값인 경우 Error를 발생시키는 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import { Console } from '@woowacourse/mission-utils';
 class App {
   async getCarNames() {
-    return Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const input = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+
+    const isComma = /^(\w+)(,\w+)*$/.test(input);
+
+    if (!input || !isComma) {
+      throw new Error('[ERROR] 잘못된 입력값 입니다.');
+    }
+
+    const carNames = input.split(',');
+
+    const validateCarNamesLength = carNames.every((carName) => carName.length <= 5);
+
+    if (!validateCarNamesLength) {
+      throw new Error('[ERROR] 잘못된 형식의 입력값입니다.');
+    }
+
+    return carNames;
   }
 
   async getAttemptCount() {
-    return Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    const input = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+
+    if (!input || Number.isNaN(parseInt(input))) {
+      throw new Error('[ERROR] 잘못된 형식의 입력값입니다.');
+    }
+
+    return parseInt(input);
   }
 
   async run() {
     const CarNames = await this.getCarNames();
     const count = await this.getAttemptCount();
-    console.log(CarNames);
-    console.log(count);
   }
 }
 
