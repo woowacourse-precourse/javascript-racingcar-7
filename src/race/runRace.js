6c41f13df803c050a49c92dc6bc0b1213b41b0c5
minji2219 <200146@jnu.ac.kr>
2024-10-25 15:43:59 +0900

    feat: 시도 횟수 입력에 대한 예외 처리 코드 작성
    시도 횟수 입력 타입은 숫자가 되어야하기에 숫자가 아닐 경우 Error를 출력한다.
    입력 받은 수가 string 타입의 숫자가 될 수 있으므로 숫자 타입으로 변경해준다.

--- src/race/runRace.js
+++ src/race/runRace.js
@@ -4,12 +4,15 @@ import { runRaceCycle } from './runRaceCycle.js';
 import { getWinner } from './getWinner.js';
 
 export const runRace = (cars, repeat) => {
+  if (!Number(repeat))
+    throw new Error('[ERROR] 시도할 횟수는 숫자로 입력해주세요.');
+
   const carsArray = seperateCar(cars);
   const cumulativeResults = new Array(carsArray.length).fill('');
 
   Console.print('\n실행 결과');
 
-  for (let i = 1; i <= repeat; i++) {
+  for (let i = 1; i <= Number(repeat); i++) {
     runRaceCycle(carsArray, cumulativeResults);
   }
   Console.print('최종 우승자 : ' + getWinner(carsArray, cumulativeResults));
