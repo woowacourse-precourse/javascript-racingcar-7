04c7d6b9c8b29f8dfd28549aa744edfcac97cfdd
choiihwa <chldlghk@gmail.com>
2024-10-25 22:59:28 +0900

    feat(validator): Error 호출 방법 변경

--- src/race/Validator.js
+++ src/race/Validator.js
@@ -1,11 +1,11 @@
 export function checkValidNameLength(name, length) {
     if (name.length > length) {
-        throw Error(`[ERROR] 이름은 최대 ${length}자까지 설정 가능합니다. ${name}`);
+        throw new Error(`[ERROR] 이름은 최대 ${length}자까지 설정 가능합니다. (입력값: ${name})`);
     }
 }
 
 export function checkLessThanOrEqualMaxCount(count, maxCount) {
     if (count > maxCount) {
-        throw Error(`[ERROR] 경기는 최대 ${maxCount}회 시도할 수 있습니다. ${count}`)
+        throw new Error(`[ERROR] 경기는 최대 ${maxCount}회 시도할 수 있습니다. (입력값: ${count})`)
     }
 }
