3ae6770bb1dc00cb4726144afc6f23ac3747d12b
thumbthing <ilscc0902@gmail.com>
2024-10-25 22:07:03 +0900

    Feat: 시행 횟수 새부 기능 구현

--- src/UI/trialCountInput.js
+++ src/UI/trialCountInput.js
@@ -1,3 +1,5 @@
+import { Console } from "@woowacourse/mission-utils";
+
 async function trialCountInput() {
 /**
  * TODO: 
@@ -8,4 +10,31 @@ async function trialCountInput() {
  *  - 유효성을 통과하지 못했을 경우 에러를 던진다
  * 4. 유효성을 통과한 정수 값을 반환한다.
  */
-}
+
+  try {
+    const USER_INPUT = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    const TRIAL_COUNT = Number(USER_INPUT);
+
+    if(Number.isNaN(TRIAL_COUNT)) {
+      const ERROR_MESSAGE = '입력하신 값이 숫자가 아닙니다.'
+      const ERROR = new Error(ERROR_MESSAGE);
+      throw ERROR;
+    }
+
+    if(TRIAL_COUNT < 1) {
+      const ERROR_MESSAGE = '입력하신 값이 1보다 작아서 레이스를 진행 할 수 없습니다.'
+      const ERROR = new Error(ERROR_MESSAGE);
+      throw ERROR;
+    }
+
+    if(TRIAL_COUNT !== Math.floor(TRIAL_COUNT) || TRIAL_COUNT !== Math.ceil(TRIAL_COUNT)) {
+      const ERROR_MESSAGE = '입력하신 값이 정수가 아닙니다.'
+      const ERROR = new Error(ERROR_MESSAGE);
+      throw ERROR;
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default trialCountInput;
