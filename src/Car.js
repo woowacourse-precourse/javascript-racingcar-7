882fff3b244b655245061809b54296d482142999
TaewoooongHeo01 <chamjoeun0111@gmail.com>
2024-10-25 16:13:17 +0900

    feat(Car): 자동차 이름 유효성 검사 함수 추가

--- src/Car.js
+++ src/Car.js
@@ -1,14 +1,14 @@
-// class Car {
-//   constructor(name) {
-//     if (!Car.validateCarName(name)) {
-//       throw new Error('[ERROR] 이름은 5자 이하만 가능합니다');
-//     }
-//     this.name = name;
-//   }
+class Car {
+  constructor(name) {
+    if (!Car.validateCarName(name)) {
+      throw new Error('[ERROR] 이름은 5자 이하만 가능합니다');
+    }
+    this.name = name;
+  }
 
-//   static validateCarName(name) {
-//     return name.length <= 5;
-//   }
-// }
+  static validateCarName(name) {
+    return name.length <= 5;
+  }
+}
 
-// export default Car;
+export default Car;
