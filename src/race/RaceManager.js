932c99ae021227ecb6dce9d7c856d9d2afd27062
choiihwa <chldlghk@gmail.com>
2024-10-26 00:32:02 +0900

    feat(race): 자동차명과 시도 횟수 입력값 검증내역 추가
    - 빈 값 입력 시 에러 발생 추가
    - 자동차명 영어를 제외한 문자 입력 시 에러 발생 추가
    - 시도 횟수 숫자를 제외한 문자 입력 시 에러 발생 추가

--- src/race/RaceManager.js
+++ src/race/RaceManager.js
@@ -1,5 +1,5 @@
 import IOHandler from "../utils/IOHandler.js";
-import { checkValidNameLength, checkLessThanOrEqualMaxCount } from "./Validator.js";
+import * as Validator from "./Validator.js";
 import RaceExcutor from "./RaceExecutor.js";
 import { getWinners } from "./WinnerSelector.js";
 import Car from "./Car.js";
@@ -21,11 +21,17 @@ class RaceManager {
         this.#cars = carNames.map((name) => new Car(name));
     }
 
+    #validateCarName(name) {
+        Validator.checkNotBlank(name);
+        Validator.checkOnlyEnglishCharacters(name);
+        Validator.checkValidNameLength(name, this.#MAX_NAME_LENGTH);
+    }
+
     async #setCarsFromInput() {
         const inputStr = await IOHandler.input("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
         const carNames = this.#getNamesFromStr(inputStr);
         carNames.forEach(name => {
-            checkValidNameLength(name, this.#MAX_NAME_LENGTH);
+            this.#validateCarName(name);
         });
 
         this.#setCarsFromCarNames(carNames);
@@ -35,9 +41,15 @@ class RaceManager {
         this.#racingCount = count;
     }
 
+    #validateRacingCount(count) {
+        Validator.checkNotBlank(count);
+        Validator.checkNumberValue(count);
+        Validator.checkLessThanOrEqualMaxCount(count, this.#MAX_RACE_COUNT);
+    }
+
     async #setRacingCountFromInput() {
         const inputCount = await IOHandler.input("시도할 횟수는 몇 회인가요?\n");
-        checkLessThanOrEqualMaxCount(inputCount, this.#MAX_RACE_COUNT);
+        this.#validateRacingCount(inputCount);
         this.#setRacingCount(inputCount);
     }
 
--- src/race/Validator.js
+++ src/race/Validator.js
@@ -6,6 +6,24 @@ export function checkValidNameLength(name, length) {
     }
 }
 
+export function checkNotBlank(str) {
+    if (!str) {
+        throw new Error(getErrorMessage(`빈 값은 입력할 수 없습니다.`));
+    }
+}
+
+export function checkOnlyEnglishCharacters(str) {
+    if (!/^[a-zA-Z]+$/.test(str)) {
+        throw new Error(getErrorMessage(`영어 이름만 입력 가능합니다. (입력값: ${str})`));
+    }
+}
+
+export function checkNumberValue(num) {
+    if (!/^[0-9]+$/.test(num)) {
+        throw new Error(getErrorMessage(`숫자만 입력할 수 있습니다. (입력값: ${num})`));
+    }
+}
+
 export function checkLessThanOrEqualMaxCount(count, maxCount) {
     if (count > maxCount) {
         throw new Error(getErrorMessage(`경기는 최대 ${maxCount}회 시도할 수 있습니다. (입력값: ${count})`));
