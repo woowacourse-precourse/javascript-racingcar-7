a8785b1b467a89b0ec99a500ed422ae178ef88cf
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-10-26 02:20:26 +0900

    feat: 자동차의 위치 값만큼 '-' 문자열로 반환한다

--- src/OutputUtils.js
+++ src/OutputUtils.js
@@ -1,3 +1,9 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 export function formatWinnerNames(winners) {
   return winners.map((winner) => winner).join(', ');
+}
+
+export function generateProgressBar(position) {
+  return '-'.repeat(position);
 }
