514ff38a4a3b30c7998a178bdc65b6dcb5ca4cd5
최영빈 <bin778@naver.com>
2024-10-25 12:35:40 +0900

    refactor(Input): 코드 길이 축소

--- src/Input.js
+++ src/Input.js
@@ -2,19 +2,13 @@ import { Console } from "@woowacourse/mission-utils";
 
 class Input {
   async getCarNames() {
-    const car_name = await Console.readLineAsync(
+    return await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
-
-    return car_name;
   }
 
   async getAttemptNumber() {
-    const attempt_number = await Console.readLineAsync(
-      "시도할 횟수는 몇 회인가요?\n"
-    );
-
-    return attempt_number;
+    return await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
   }
 }
 
