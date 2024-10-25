f4d8f05edf40244dfd424f0624abb5929fb28a6e
dragonite-lee <ross1222@naver.com>
2024-10-25 17:18:52 +0900

    feat(App): 경주할 자동차 이름 입력(,로 구분) - 경주 자동차 입력 받기 - ,로 구분해 리스트로 받음

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const participant_input = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+    );
+
+    if (!participant_input) {
+      // ERROR 경주할 자동차를 입력해주세요.
+    }
+    
+    const participant_list = participant_input.split(',');
+    
+  }
 }
 
 export default App;
