fd20ec91304e954c6a3936de7e172a6b861911b9
jeong922 <hwajeong922@naver.com>
2024-10-25 18:58:39 +0900

    feat(App): 입력값 받아오기 기능 구현
    - @woowacourse/mission-utils의 Console API를 이용해 비동기 입력값 받아오기
    - 경주할 자동차 이름을 입력받는 getCarNames 메소드 구현
    - 시도 횟수를 입력받는 getAttemptCount 메소드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console } from '@woowacourse/mission-utils';
 class App {
-  async run() {}
+  async getCarNames() {
+    return Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+  }
+
+  async getAttemptCount() {
+    return Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+  }
+
+  async run() {
+    const CarNames = await this.getCarNames();
+    const count = await this.getAttemptCount();
+    console.log(CarNames);
+    console.log(count);
+  }
 }
 
 export default App;
