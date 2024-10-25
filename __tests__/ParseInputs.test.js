5d01acc82a541345fc7d8dbf2a4cf951aa1ab127
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2024-10-26 01:26:09 +0900

    test :resolve  test code result incorrect input

--- __tests__/ParseInputs.test.js
+++ __tests__/ParseInputs.test.js
@@ -1,13 +1,12 @@
-describe('validate test', () => {
-  test('만욱,노미,재걸,동호', () => {
-    expect(() => {
-      validateError({ car: '만욱,노미,재걸,동호', count: 123123 });
-    }).toThrow(ERROR_MESSAGE.LENGTH_ERROR); // 두 인자를 전달
-  });
+import { parseInputs } from '../src/Parsing.js';
 
-  test('두루미,김수한무거북이와 , 123123', () => {
-    expect(() => {
-      validateError({ car: '123123,123123123', count: 3 });
-    }).toThrow(ERROR_MESSAGE.BIGNUM_ERROR); // 두 인자를 전달
+describe('parsing test', () => {
+  test('만욱,노미,재걸,동호', () => {
+    expect(parseInputs('만욱,노미,재걸,동호')).toEqual([
+      '만욱',
+      '노미',
+      '재걸',
+      '동호',
+    ]);
   });
 });
--- __tests__/Validate.test.js
+++ __tests__/Validate.test.js
@@ -1,16 +1,16 @@
-import validateError from '../src/Validate.js'; // default export를 중괄호 없이 가져오기
-import { ERROR_MESSAGE } from '../src/const/errorMessage.js';
+import { validateInput } from '../src/Validate.js'; // default export를 중괄호 없이 가져오기
+import ERROR_MESSAGE from '../src/const/index.js';
 
 describe('validate test', () => {
   test('만욱,노미,재걸,동호', () => {
     expect(() => {
-      validateError({ car: '만욱,노미,재걸,동호', count: 123123 });
-    }).toThrow(ERROR_MESSAGE.LENGTH_ERROR); // 두 인자를 전달
+      validateInput({ car: ['만욱', '노미', '재걸', '동호'], count: 123123 });
+    }).toThrow(ERROR_MESSAGE.COUNT_ERROR);
   });
 
-  test('두루미,김수한무거북이와 , 123123', () => {
+  test('두루미,김수한무거북이와, 123123', () => {
     expect(() => {
-      validateError({ car: '123123,123123123', count: 3 });
-    }).toThrow(ERROR_MESSAGE.BIGNUM_ERROR); // 두 인자를 전달
+      validateInput({ car: [123, 123, 12312123], count: 31 });
+    }).toThrow(ERROR_MESSAGE.LENGTH_ERROR);
   });
 });
