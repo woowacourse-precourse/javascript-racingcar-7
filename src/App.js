d43c805c5f41c2ce6a7874478cc7f1114ec97bde
Sohee <sh11govlday@gmail.com>
2024-10-25 12:24:18 +0900

    feat(App) : 시도할 횟수 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ class App {
       if (nameLengthCheck) {
         throw new Error('[ERROR] 자동차 이름은 5자 이하여야 합니다.');
       }
+
+      const attemps = await Console.readLineAsync('');
     } catch (error) {
       Console.print(error.message);
       throw error;
