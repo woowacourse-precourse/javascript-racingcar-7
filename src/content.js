d14cbe8565b013a7c6d3130322a2454ebeac6079
dudcns02 <duddl2640@naver.com>
2024-10-25 19:12:34 +0900

    feat: Output format declaration

---
+++ src/content.js
@@ -0,0 +1,8 @@
+const LOG_MESSAGE = Object.freeze({
+  START_MESSAGE:
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+  TRY_NUM_MESSAGE: "시도할 횟수는 몇 회인가요?",
+});
+const ERROR_MESSAGE = Object.freeze({
+  INCORRECT_VALUE: "[ERROR]",
+});
