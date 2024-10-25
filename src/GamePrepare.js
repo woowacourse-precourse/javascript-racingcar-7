414c742995d8739f6a5e16069ca83eb351c83878
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-10-25 21:55:33 +0900

    feat: 게임 시작 전 자동차 데이터를 준비하는 기능 추가
    - 자동차 이름 저장
    - 총 전진값을 0으로 초기화

--- src/GamePrepare.js
+++ src/GamePrepare.js
@@ -1,3 +1,15 @@
+export default function prepareGameData(carNames) {
+  const carNameArray = extractCarName(carNames);
+  const carDataList = carNameArray.map(initializeCarData);
+}
+
 function extractCarName(carNames) {
   return carNames.split(',');
+}
+
+function initializeCarData(carName) {
+  return {
+    name: carName,
+    totalMoveValue: 0,
+  };
 }
