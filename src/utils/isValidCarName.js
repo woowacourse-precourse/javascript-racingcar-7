89a737baecaee00ea3262d621270dcbe51df7638
subsub-e <ab13696802@gmail.com>
2024-10-25 21:50:38 +0900

    feat(isValidCarName) : isValidCarName 구현

---
+++ src/utils/isValidCarName.js
@@ -0,0 +1,13 @@
+import { eraseWhiteSpace } from '../utils/eraseWhiteSpace.js';
+import { extractCarName } from "../utils/extractCarName.js";
+import { hasDuplicateName } from "../utils/hasDuplicateName.js";
+import { isInputEmpty } from "../utils/isInputEmpty.js";
+import { isNameLongerThanFive } from "../utils/isNameLongerThanFive.js";
+
+export function isValidCarName(carNameString){
+    const carArray = eraseWhiteSpace(extractCarName(carNameString));
+    isInputEmpty(carArray);
+    hasDuplicateName(carArray);
+    isNameLongerThanFive(carArray);
+    return carArray;
+}
