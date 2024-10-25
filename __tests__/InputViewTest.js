8519fae4b33ecefecc358e31cb594e069c24bee3
Seio924 <seio9241@gmail.com>
2024-10-25 16:25:21 +0900

    test(InputViewTest): 시도할 횟수 입력 테스트 추가

--- __tests__/InputViewTest.js
+++ __tests__/InputViewTest.js
@@ -31,4 +31,19 @@ describe('InputView', () => {
     // then
     expect(carNames).toEqual(output);
   });
+
+  test('시도할 횟수를 입력받아 숫자로 변환하여 반환한다', async () => {
+    // given
+    const input = ['2'];
+    const output = 2;
+
+    mockQuestions(input);
+
+    // when
+    const inputView = new InputView();
+    const attemptCount = await inputView.readAttemptCount();
+
+    // then
+    expect(attemptCount).toBe(output);
+  });
 });
