a61cd4a8156527cf511bd010ad595745d55bd15d
ho8ae <xogh2242@gmail.com>
2024-10-25 16:52:18 +0900

    refactor: InputValidator를 const 객체로 변경

--- src/utils/InputValidator.js
+++ src/utils/InputValidator.js
@@ -1,35 +1,37 @@
 import { ERROR_MESSAGE } from "../static/Static.js";
 
-export default class InputValidator {
-    static validateCarNames(names) {
-      const carNames = names.split(',').map(name => name.trim());
-      
-      if (carNames.some(name => name.length > 5)) {
-        throw new Error(ERROR_MESSAGE.nameLengthError);
-      }
-      
-      if (carNames.some(name => name.length === 0)) {
-        throw new Error(ERROR_MESSAGE.nameInputError);
-      }
-      
-      if (new Set(carNames).size !== carNames.length) {
-        throw new Error(ERROR_MESSAGE.nameDuplicateError);
-      }
-      
-      return carNames;
+const InputValidator = {
+  validateCarNames(names) {
+    const carNames = names.split(',').map(name => name.trim());
+    
+    if (carNames.some(name => name.length > 5)) {
+      throw new Error(ERROR_MESSAGE.nameLengthError);
     }
-  
-    static validateAttempts(attempts) {
-      const number = Number(attempts);
-      
-      if (isNaN(number)) {
-        throw new Error(ERROR_MESSAGE.triesNumberError);
-      }
-      
-      if (number <= 0) {
-        throw new Error(ERROR_MESSAGE.triesZeroError);
-      }
-      
-      return number;
+    
+    if (carNames.some(name => name.length === 0)) {
+      throw new Error(ERROR_MESSAGE.nameInputError);
     }
+    
+    if (new Set(carNames).size !== carNames.length) {
+      throw new Error(ERROR_MESSAGE.nameDuplicateError);
+    }
+    
+    return carNames;
+  },
+
+  validateAttempts(attempts) {
+    const number = Number(attempts);
+    
+    if (isNaN(number)) {
+      throw new Error(ERROR_MESSAGE.triesNumberError);
+    }
+    
+    if (number <= 0) {
+      throw new Error(ERROR_MESSAGE.triesZeroError);
+    }
+    
+    return number;
   }
+};
+
+export default InputValidator;
