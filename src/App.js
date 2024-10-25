6477c2ce3fbd5a6d7e9ca601538aaa753ca114fa
ddozza54 <bananamayong@gmail.com>
2024-10-25 19:10:18 +0900

    feat: 자동차 별 전진 여부 결정하는 랜덤 함수 만들기

--- src/App.js
+++ src/App.js
@@ -5,19 +5,37 @@ class App {
     const Console = MissionUtils.Console;
     const Random = MissionUtils.Random;
 
+    const isMovingFoward = () => {
+      const randomNumber = Random.pickNumberInRange(0, 9);
+      if (randomNumber >= 4) {
+        return true;
+      }
+      return false;
+    };
+
     try {
       const inputNames = await Console.readLineAsync(
         '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
       );
-      const cars = inputNames.split(',');
-      cars.map((name) => {
+      const cars = inputNames
+        .split(',')
+        .filter((name) => name !== '');
+      const RacingCountObject = {};
+
+      cars.forEach((name) => {
         if (name.length > 5) {
           throw Error(
             '자동차 이름은 5자 이하만 가능합니다.'
           );
         }
+        RacingCountObject[name] = 0;
+        console.log(
+          name,
+          'isMovingFoward :',
+          isMovingFoward()
+        );
       });
-      Console.print(cars);
+
       const numberOfTry = Number(
         await Console.readLineAsync(
           '시도할 횟수는 몇 회인가요?'
@@ -25,8 +43,7 @@ class App {
       );
       Console.print(numberOfTry);
     } catch (error) {
-      // reject 되는 경우
-      throw error;
+      throw Error('[ERROR]' + error.message);
     }
   }
 }
