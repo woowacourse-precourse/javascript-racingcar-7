2819a6012932abf12b9bd06f065e31ae98984e0b
KIMJINYOUNG <k5444862@naver.com>
2024-10-25 15:40:37 +0900

    test: App 클래스에서 Input 메서드 사용 시 콘솔 출력 테스트 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Input from "./Input.js";
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const userInput = await Input.getUserInput();
+    const splitedUserInput = Input.splitUserInput(userInput);
+    const tryCount = await Input.getTryCount();
+
+    Console.print(userInput);
+    Console.print(splitedUserInput);
+    Console.print(tryCount);
+  }
 }
 
 export default App;
