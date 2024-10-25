9ea21b4f91ebc99b1eb053fdde8755216a6f27f7
rwaeng <rwaeng0421@gmail.com>
2024-10-25 17:22:21 +0900

    feat: 시도 횟수 유효성 검사 기능 추가

--- src/App.js
+++ src/App.js
@@ -26,7 +26,20 @@ class App {
   }
 
   async getNumber() {
-    this.number = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    const tempNumber = await Console.readLineAsync(
+      '시도할 횟수는 몇 회인가요?\n',
+    );
+    this.validateNumber(tempNumber);
+  }
+
+  validateNumber(tempNumber) {
+    if (!Number.isInteger(tempNumber)) {
+      throw new Error('[ERROR] 횟수는 정수로 입력해야 합니다.');
+    }
+    if (tempNumber < 1) {
+      throw new Error('[ERROR] 횟수는 1번 이상이어야 합니다.');
+    }
+    this.number = tempNumber;
   }
 
   async run() {
