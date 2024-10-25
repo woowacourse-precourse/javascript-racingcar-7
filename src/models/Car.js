d4749abe640c913359d178e95b65dc7790766b17
chungyo <kimchungy98@gmail.com>
2024-10-25 22:22:15 +0900

    feat: 무작위 조건에 따라 자동차가 전진하는 조건 추가
    - move 메서드를 정의하여 무작위 값이 4 이상일 때 위치 증가

--- src/models/Car.js
+++ src/models/Car.js
@@ -1,4 +1,5 @@
 // 1.	주어진 횟수 동안 여러 대의 자동차가 전진 또는 멈출 수 있다. 자동차 클래스를 먼저 만들어준다. 자동차는 이름과 위치가 필요하다.
+// 1-2. 0~9 사이의 랜덤한 숫자를 생성한 뒤 숫자가 4이상일 경우에만 전진한다.
 
 class Car {
     constructor(name) {
@@ -7,6 +8,11 @@ class Car {
     }
     getPosition(){
         return this.position;
+    }
+    move(randomNumber) {
+        if(randomNumber >= 4) {
+            this.position++;
+        }
     }
 }
 
