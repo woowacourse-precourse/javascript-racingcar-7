5218333c5fc4d1155e897e5ec70dc76b4fa1204d
ShinjungOh <tlswjd9260@gmail.com>
2024-10-26 01:32:24 +0900

    feat: 사용자에게 각 자동차의 이름을 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import {MissionUtils} from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const carNameInput = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const carArray = carNameInput.toString().split(',');
+
+    carArray.forEach((car) =>{
+      if (car.length > 5) {
+        throw Error('[ERROR] 자동차 이름은 5자 이하만 가능합니다.')
+      }
+    });
+  }
 }
 
 export default App;
