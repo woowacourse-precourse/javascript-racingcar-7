81daec0ecd45bbd937907025ecf412e717286966
JuYeong17 <dhwndud980@gmail.com>
2024-10-26 01:40:01 +0900

    feat: 자동차 입력 예외처리

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -6,10 +6,18 @@ class InputView{
     const tryNumber = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
 
     const car=carName.split(',');
-    
+    getinputexception();
+
     return {car, tryNumber};
   }
-
+  
+  async getinputexception(){
+    for(i=0;i<car.length;i++){
+      if(car[i].length < 6) {
+        throw new Error ("[Error] 자동차 이름을 5글자 이하로 작성해주세요.");
+      }
+    }
+  }
 }
 
 export default InputView;
