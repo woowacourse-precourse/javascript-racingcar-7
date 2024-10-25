ba7281491905f835bf652c57a2447d463fc63d71
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-10-26 00:32:59 +0900

    chore: 입력 검증 관련 에러 메시지를 상수로 정의

--- src/Message.js
+++ src/Message.js
@@ -5,3 +5,11 @@ export const GAME_PROGRESS_MESSAGE = Object.freeze({
   EXECUTION_RESULT: '실행 결과',
   FINAL_WINNER: '최종 우승자',
 });
+
+export const ERROR_MESSAGE = Object.freeze({
+  NO_WHITESPACE: '입력값에 공백이 포함되어서는 안 됩니다.',
+  INVALID_SEPARATOR: '쉼표(,) 외의 다른 구분자를 사용할 수 없습니다.',
+  MAX_LENGTH_EXCEEDED: '자동차 이름은 5자 이하만 가능합니다.',
+  INVALID_CHARACTERS: '자동차 이름은 문자만 포함해야 합니다.',
+  NON_POSITIVE_NUMBER: '시도할 횟수는 양수여야 합니다.',
+});
