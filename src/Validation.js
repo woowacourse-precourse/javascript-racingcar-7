4dfcc3fa6e21cfbbfa3ee57f07995451a8f2bb0e
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-10-26 00:45:59 +0900

    feat: 자동차 이름에 숫자를 사용하지 않았는지 검증하는 기능 추가

--- src/Validation.js
+++ src/Validation.js
@@ -3,3 +3,8 @@ import { ERROR_MESSAGE } from './Message.js';
 function validateNoWhiteSpace(carName) {
   if (carName.includes(' ')) throw new Error(`${ERROR_MESSAGE.NO_WHITESPACE}`);
 }
+
+function validateOnlyLetters(carName) {
+  if (/\d/.test(carName))
+    throw new Error(`${ERROR_MESSAGE.INVALID_CHARACTERS}`);
+}
