d92595006d2c300923f1eaed3acc5af1869ccf8c
HS <hasang2691@naver.com>
2024-10-25 20:08:47 +0900

    feat(inputView.js): 시도 횟수 입력 받기 기능 구현
    - Console.readLineAsync를 활용하여 시도 횟수를 입력받는 readGameRounds 메서드 구현

--- src/views/inputView.js
+++ src/views/inputView.js
@@ -12,6 +12,11 @@ const InputView = {
   parseCarNames(input) {
     return input.split(',').map((name) => name.trim());
   },
+
+  async readGameRounds() {
+    const input = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    return input;
+  },
 };
 
 export default InputView;
