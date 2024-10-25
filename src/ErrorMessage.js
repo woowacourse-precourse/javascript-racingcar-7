f259cb2e4346b05842e2b858c59e9ee7daf2f6ec
Xen-alpha <hky7548@naver.com>
2024-10-25 14:33:42 +0900

    feat(parseInput): add implementation

--- src/ErrorMessage.js
+++ src/ErrorMessage.js
@@ -1,7 +1,8 @@
 const errorMessage = {
-    NO_PLAYER: "[ERROR]: 주어진 플레이어가 없습니다.",
-    NEGATIVE_NUMBER: "[ERROR]: 플레이어 수가 음수로 주어졌습니다.",
-    NAME_IS_TOO_LONG: "[ERROR]: 플레이어 이름은 5자를 초과할 수 없습니다."
+    NO_PLAYER: '[ERROR]: 주어진 플레이어가 없습니다.',
+    NEGATIVE_NUMBER: '[ERROR]: 플레이어 수가 음수로 주어졌습니다.',
+    NAME_IS_TOO_LONG: '[ERROR]: 플레이어 이름은 5자를 초과할 수 없습니다.',
+    NOT_A_NUMBER: '[ERROR]: 플레이어 수 입력이 숫자가 아닙니다.'
 };
 
 export default errorMessage;
--- src/parseInput.js
+++ src/parseInput.js
@@ -1,13 +1,25 @@
+import errorMessage from './ErrorMessage';
+
 import { MissionUtils } from '@woowacourse/mission-utils'
 const Console = MissionUtils.Console;
 const Random = MissionUtils.Random;
 
 
-export function parsePlayers(inputstring)
+export function parsePlayers( inputstring )
 {
-        
+    if ( inputstring === '' )
+        throw new Error( errorMessage.NO_PLAYER )
+    const resultArray = inputstring.split( ',' );
+    if ( resultArray.some( ( value ) => value.length > 5 ) )
+        throw new Error( errorMessage.NAME_IS_TOO_LONG );
+    return resultArray;
 }
-export function parseRound(inputstring)
+export function parseRound( inputstring )
 {
-
+    const inputNumber = parseInt( inputstring );
+    if ( isNaN( inputNumber ) )
+        throw new Error( errorMessage.NOT_A_NUMBER );
+    else if ( inputNumber < 0 )
+        throw new Error( errorMessage.NEGATIVE_NUMBER );
+    return inputNumber;
 }
