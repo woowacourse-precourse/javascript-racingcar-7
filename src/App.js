6766b32fc7062635b0bf1b6d003ee33d09bccf16
yewonhihi <yewon1118@naver.com>
2024-10-25 18:00:02 +0900

    feat(App.js): 경주할 자동차 이름 입력받고 쉼표로 구분하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const inputCars = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n");
+  }
+
+  getCarList(str) {
+    const cars = str.split(",");
+
+    return cars
+  }
 }
 
 export default App;
