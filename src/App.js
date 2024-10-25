154320c401dc7cac0b41e16e729a9f178f0defaa
Yusi <yusi4781@naver.com>
2024-10-25 23:53:48 +0900

    refactor(input): 사용자 입력 함수 단순화

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import getUserInput from "./getUserInput.js";
 
 class App {
   async run() {
-    const { carNameString, numberOfAttemps } = getUserInput();
+    const carNameString = await getUserInput("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
   }
 }
 
--- src/getUserInput.js
+++ src/getUserInput.js
@@ -1,10 +1,9 @@
 import { Console } from "@woowacourse/mission-utils";
 
-const getUserInput = async () => {
-  const carNameString = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-  const numberOfAttemps = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+const getUserInput = async (text) => {
+  const userInput = await Console.readLineAsync(text);
 
-  return { carNameString, numberOfAttemps };
+  return userInput;
 }
 
 export default getUserInput;
