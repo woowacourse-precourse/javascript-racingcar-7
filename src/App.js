28c33e326ec08384214c4ef78c069952dc0fdfcf
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-10-25 21:28:01 +0900

    feat: App 클래스에 프로그램 실행 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import CarRacingGame from './classes/CarRacingGame.js';
 import CarRacingInputReader from './classes/CarRacingInputReader.js';
 
 class App {
   async run() {
-    const inputReader = new CarRacingInputReader();
-    const carNamesInput = await inputReader.getCarNames();
-    const totalRounds = await inputReader.getTotalRounds();
+    const carNames = await CarRacingInputReader.getCarNames();
+    const totalRounds = await CarRacingInputReader.getTotalRounds();
+
+    const carRacing = new CarRacingGame(carNames);
+
+    carRacing.startGame(totalRounds);
   }
 }
 
