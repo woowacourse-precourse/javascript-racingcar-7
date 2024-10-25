c2f5c8aa1efa40e8f6aa47d2dc0b2557438b51f1
thumbthing <ilscc0902@gmail.com>
2024-10-26 00:25:06 +0900

    Feat: 경주 데이터 초기상태 생성 기능 구현 목록 작성

---
+++ src/racerData/racer.js
@@ -0,0 +1,8 @@
+function createRacerInformation(racerList) {
+  /** TODO:
+   * 1. 유효성을 통과한 자동차 이름의 배열을 매개 변수로 받는다
+   * 2. 해당 배열을 map 메서드를 이용해서 ['자동차 이름', ''] 형태로 변환
+   * 3. 변환된 배열을 활용해서 Map 자료구조를 생성
+   * 4. Map을 반환
+   */
+}
