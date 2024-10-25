7e7d2ad70c713635c5efeb2543e908c27b9b1147
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-10-25 20:53:32 +0900

    refactor: 게임 진행 메시지를 상수화

---
+++ src/Message.js
@@ -0,0 +1,5 @@
+export const GAME_PROGRESS_MESSAGE = Object.freeze({
+  ENTER_CAR_NAMES:
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+  ENTER_TRIAL_COUNT: '시도할 횟수는 몇 회인가요?',
+});
--- src/UserInterface.js
+++ src/UserInterface.js
@@ -1,9 +1,14 @@
 import { Console } from '@woowacourse/mission-utils';
+import { GAME_PROGRESS_MESSAGE } from './Message.js';
 
 export async function inputCarNames() {
-  return await Console.readLineAsync('');
+  return await Console.readLineAsync(
+    `${GAME_PROGRESS_MESSAGE.ENTER_CAR_NAMES}\n`
+  );
 }
 
 export async function inputNumberOfMove() {
-  return await Console.readLineAsync('');
+  return await Console.readLineAsync(
+    `${GAME_PROGRESS_MESSAGE.ENTER_TRIAL_COUNT}\n`
+  );
 }
