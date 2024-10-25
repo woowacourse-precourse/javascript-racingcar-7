1cfcdc0c1c41b5da8cbcb50714504b8057ac6327
Chaesunbak <coanstjd0422@gmail.com>
2024-10-25 10:32:19 +0900

    fix : 자동차 이름 입력받기에 개행문자 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     const CAR_NAMES = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)'
+      '경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n'
     );
   }
 }
