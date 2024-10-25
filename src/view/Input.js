531b9302665d8fa687f0daff043fe99606c6ffb6
ChanLee_KR <cl700120@gmail.com>
2024-10-25 17:21:19 +0900

    feat: 횟수 입력 기능 구현

--- src/view/Input.js
+++ src/view/Input.js
@@ -15,6 +15,12 @@ class Input {
 
   static CAR_NAME_ERROR_MESSAGE_DUPLICATE = '자동차 이름은 중복될 수 없어요.';
 
+  static TIMES_GUIDE_MESSAGE = '시도할 횟수는 몇 회인가요? \n';
+  static TIMES_ERROR_MESSAGE_EMPTY = '횟수를 입력해 주세요.';
+  static TIMES_ERROR_MESSAGE_NAN = '횟수는 숫자로 입력해 주세요.';
+  static TIMES_ERROR_MESSAGE_INTEGER = '횟수는 정수로 입력해 주세요.';
+  static TIMES_ERROR_MESSAGE_POSITIVE_NUMBER = '횟수는 1 이상이어야 해요.';
+
   /**
    * @param {string[]} carNames
    * @returns {boolean}
@@ -53,6 +59,33 @@ class Input {
     }
 
     return carNames;
+  }
+
+  /**
+   * @returns {Promise<number>}
+   */
+  static async readTimes() {
+    /** @type {string} */
+    const input = await Console.readLineAsync(this.TIMES_GUIDE_MESSAGE);
+    const times = Number(input);
+
+    if (!input) {
+      throw new CustomError(this.TIMES_ERROR_MESSAGE_EMPTY);
+    }
+
+    if (Number.isNaN(times)) {
+      throw new CustomError(this.TIMES_ERROR_MESSAGE_NAN);
+    }
+
+    if (!Number.isSafeInteger(times)) {
+      throw new CustomError(this.TIMES_ERROR_MESSAGE_INTEGER);
+    }
+
+    if (times < 1) {
+      throw new CustomError(this.TIMES_ERROR_MESSAGE_POSITIVE_NUMBER);
+    }
+
+    return times;
   }
 }
 
