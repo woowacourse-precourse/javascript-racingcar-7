d15b4b518c79ef7c7b084f3a79bc8e43f049fbb8
seongmin <seongm22n@gmail.com>
2024-10-25 12:29:20 +0900

    refactor: 기능2 depth 수정
    - depth가 2이상인 코드 수정
    - 무작위 숫자가 4이상인지 판단하여 자동차가 앞으로 이동할지 결정하는 함수
    - 전진하는지 확인 후 해당 자동차의 위치 업데이트 함수
    - 한번의 시도에 모든 자동차의 움직임 처리하는 함수

--- src/services/startRacingGame.js
+++ src/services/startRacingGame.js
@@ -2,14 +2,25 @@ import { Console, Random } from '@woowacourse/mission-utils';
 
 export function startRacingGame(carNameList, tryNumber, positions) {
 	for (let i = 0; i < tryNumber; i++) {
-		// 전진 : 무작위 값 0-9까지 값의 4 이상의 값 -> 랜덤숫자
-		for (let j = 0; j < carNameList.length; j++) {
-			const randomNumber = Random.pickNumberInRange(0, 9);
-			if (randomNumber >= 4) {
-				positions[j] += 1;
-			}
-			Console.print(`${carNameList[j]} : ${'-'.repeat(positions[j])}`);
-		}
-		Console.print('');
+		playOneRound(carNameList, positions);
 	}
 }
+
+const shouldMoveForward = () => {
+	const randomNumber = Random.pickNumberInRange(0, 9);
+	return randomNumber >= 4;
+};
+
+const moveCarIfPossible = (carIndex, positions) => {
+	if (shouldMoveForward()) {
+		positions[carIndex] += 1;
+	}
+};
+
+const playOneRound = (carNameList, positions) => {
+	carNameList.forEach((carName, index) => {
+		moveCarIfPossible(index, positions);
+		Console.print(`${carName} : ${'-'.repeat(positions[index])}`);
+	});
+	Console.print('');
+};
