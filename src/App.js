c1a56fb59efe2544287a3e4d603944c53cb59995
Park JiSeop <141125424+HarrySeop@users.noreply.github.com>
2024-10-25 14:37:58 +0900

    ✨feat: 게임을 위한 자동차 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const userInputCarNames = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+    );
+
+    const carArray = userInputCarNames
+      .split(',')
+      .map(carName => carName.trim());
+    console.log(carArray);
+  }
 }
 
 export default App;
