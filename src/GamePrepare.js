54fa53c8abb45c5c20d7ef3ee9d463e893af6cef
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-10-26 02:10:31 +0900

    feat: 입력값 검증 기능을 추가하고 게임 데이터와 이동할 횟수를 반환하도록 수정

--- src/GamePrepare.js
+++ src/GamePrepare.js
@@ -1,6 +1,12 @@
-export default function prepareGameData(carNames) {
+import { validateAllCarNames, validatePositiveNumber } from './Validation';
+
+export default function prepareGameData(carNames, numberOfMove) {
   const carNameArray = extractCarName(carNames);
+  validateAllCarNames(carNameArray);
   const carDataList = carNameArray.map(initializeCarData);
+  numberOfMove = Number(numberOfMove);
+  validatePositiveNumber(numberOfMove);
+  return { carDataList, numberOfMove };
 }
 
 function extractCarName(carNames) {
