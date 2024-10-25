c65ddb567ab4a29fcae5a64b2644a9fb08ada3bf
jeongeunLee <leeju1009@gmail.com>
2024-10-25 20:50:44 +0900

    feat(racingManager): 경주 게임을 관리하는 RacingManager 클래스 생성

---
+++ src/RacingManager.js
@@ -0,0 +1,12 @@
+class RacingManager {
+  #nameInput;
+
+  #totalRoundsInput;
+
+  constructor(nameInput, totalRoundsInput) {
+    this.#nameInput = nameInput;
+    this.#totalRoundsInput = totalRoundsInput;
+  }
+}
+
+export default RacingManager;
