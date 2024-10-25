076fae8550231ecda9782d3a30a6936f7bcf2a2f
moonseonghui <msh9980@naver.com>
2024-10-25 16:34:20 +0900

    feat(run): App의 run을 통해 전체적인 로직이 실행되도록 함수 조합

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import splitByComma from './utils/splitByComma.js';
+import { splitByComma, joinByComma } from './utils/stringUtils.js';
 import {
   checkOnlyAlphabetAndComma,
   checkValidNameLength,
   checkDuplicateNames,
+  checkOnlyNumber,
+  checkValidRange,
 } from './validator/validator.js';
-import getUserInput from './utils/getUserInput.js';
+import Car from './car/Car.js';
+import Game from './racingGame/RacingGame.js';
+import { getUserInput, printCarsMoving } from './utils/ioModule.js';
 
 class App {
   async run() {
     const inputForRacingCars = await getUserInput('입력');
-
     checkOnlyAlphabetAndComma(inputForRacingCars);
     const carNamesArray = splitByComma(inputForRacingCars);
     checkValidNameLength(carNamesArray);
     checkDuplicateNames(carNamesArray);
-
     const inputForTryCount = await getUserInput('시도할 횟수를 입력하세요');
+    checkOnlyNumber(inputForTryCount);
+    checkValidRange(inputForTryCount);
+    const racingCars = carNamesArray.map((name) => new Car(name));
+    const game = new Game(racingCars, Number(inputForTryCount));
+    game.play();
+    const winners = game.getWinnerNames();
+    printCarsMoving('최종 우승자', joinByComma(winners));
   }
 }
 
