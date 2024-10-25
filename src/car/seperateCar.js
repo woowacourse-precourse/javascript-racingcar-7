1795a9042034ec234579fe2c928b03da1637da4b
minji2219 <200146@jnu.ac.kr>
2024-10-25 15:45:55 +0900

    feat: 에러 처리를 위해 앞에 [ERROR] 작성
    요구 사항이므로 [ERROR] 문구 추가

--- src/car/seperateCar.js
+++ src/car/seperateCar.js
@@ -1,7 +1,7 @@
 export const seperateCar = (cars) => {
   const carArray = cars.split(',').map((car) => {
     if (car.length > 5) {
-      throw new Error('자동차 이름은 다섯글자 이하로 작성해주세요.');
+      throw new Error('[ERROR] 자동차 이름은 다섯글자 이하로 작성해주세요.');
     }
     return car;
   });
