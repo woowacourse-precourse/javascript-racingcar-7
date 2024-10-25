190e1ea6c3bb16d1229b077402b8114e788477cb
dragonite-lee <ross1222@naver.com>
2024-10-25 17:35:45 +0900

    feat(App): 시도할 횟수 입력

--- src/App.js
+++ src/App.js
@@ -5,9 +5,16 @@ class App {
     const participant_input = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
     );
+    const try_input = await Console.readLineAsync(
+      '시도할 횟수는 몇 회인가요?\n'
+    );
 
     if (!participant_input) {
       // ERROR 경주할 자동차를 입력해주세요.
+    }
+
+    if (try_input <= 0) {
+      // ERROR 1 이상의 숫자부터 입력가능
     }
     
     const participant_list = participant_input.split(',');
