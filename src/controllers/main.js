b90826578dc8100e07d53bfa2370cf9bec375299
dev-dino22 <wlsskdud02@naver.com>
2024-10-25 21:08:04 +0900

    feat(main.js): Add input for car names and try number

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,7 +1,12 @@
 import { Console } from "@woowacourse/mission-utils";
 
-const main = () => {
+const main = async () => {
   Console.print("프로그램이 실행되었습니다.");
+
+  const isCarName = await Console.readLineAsync("경주할 차 이름 입력");
+  const isTryNumber = await Console.readLineAsync("시도할 횟수 입력");
+
+  Console.print(`${isCarName}, ${isTryNumber}`);
 };
 
 export default main;
