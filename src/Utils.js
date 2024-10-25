226b2d438f4db13398c64b9990f23a47d489f9be
dudcns02 <duddl2640@naver.com>
2024-10-25 20:56:14 +0900

    feat: Update parameter Update to get in input

--- src/Utils.js
+++ src/Utils.js
@@ -1,23 +1,28 @@
 import { ERROR_MESSAGE } from "./content.js";
-export function carCountLimitCheck() {
-  if (input.lenght < 5) {
-    // 자동차 개수 체크 5이하만 가능
-    checkSpace();
-  } else {
-    throw new error(ERROR_MESSAGE.INCORRECT_VALUE);
+class Utils {
+  constructor(carInput) {
+    this.carInput = new carInput();
+  }
+  carCountLimitCheck(carInput) {
+    if (carInput.lenght < 5) {
+      // 자동차 개수 체크 5이하만 가능
+      checkSpace(carInput);
+    } else {
+      throw new Error(ERROR_MESSAGE.INCORRECT_VALUE);
+    }
+  }
+
+  checkSpace(carInput) {
+    // 공백 확인
+    if (/\s/.test(carInput) === false) {
+      duplicateCheck();
+    } else {
+      throw new Error(ERROR_MESSAGE.INCORRECT_VALUE);
+    }
+  }
+  duplicateCheck() {
+    // 중복 검사
   }
 }
-
-function checkSpace() {
-  // 공백 확인
-  if (/\s/.test(input) === false) {
-    duplicateCheck();
-  } else {
-    throw new error(ERROR_MESSAGE.INCORRECT_VALUE);
-  }
-}
-function duplicateCheck() {
-  // 중복 검사
-}
-
-// export {carCountLimitCheck, checkCpace};
+// export default Utils;
+export { carCountLimitCheck };
