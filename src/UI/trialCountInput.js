e8cda4f8e2d0d47e7c0c7a986c4b5cfcab9c2c4f
thumbthing <ilscc0902@gmail.com>
2024-10-26 04:30:53 +0900

    Style: 에러 변수명 구체화

--- src/UI/trialCountInput.js
+++ src/UI/trialCountInput.js
@@ -16,22 +16,22 @@ async function trialCountInput() {
     const TRIAL_COUNT = Number(USER_INPUT);
 
     if(Number.isNaN(TRIAL_COUNT)) {
-      const ERROR_MESSAGE = '입력하신 값이 숫자가 아닙니다.'
-      const ERROR = new Error(ERROR_MESSAGE);
+      const INPUT_IS_NaN = '입력하신 값이 숫자가 아닙니다.'
+      const ERROR = new Error(INPUT_IS_NaN);
       throw ERROR;
-    }
+    };
 
     if(TRIAL_COUNT < 1) {
-      const ERROR_MESSAGE = '입력하신 값이 1보다 작아서 레이스를 진행 할 수 없습니다.'
-      const ERROR = new Error(ERROR_MESSAGE);
+      const INPUT_IS_TOO_SMALL = '입력하신 값이 1보다 작아서 레이스를 진행 할 수 없습니다.'
+      const ERROR = new Error(INPUT_IS_TOO_SMALL);
       throw ERROR;
-    }
+    };
 
     if(TRIAL_COUNT !== Math.floor(TRIAL_COUNT) || TRIAL_COUNT !== Math.ceil(TRIAL_COUNT)) {
-      const ERROR_MESSAGE = '입력하신 값이 정수가 아닙니다.'
-      const ERROR = new Error(ERROR_MESSAGE);
+      const INPUT_IS_DECIMAL = '입력하신 값이 정수가 아닙니다.'
+      const ERROR = new Error(INPUT_IS_DECIMAL);
       throw ERROR;
-    }
+    };
 
     return TRIAL_COUNT;
   } catch (error) {
