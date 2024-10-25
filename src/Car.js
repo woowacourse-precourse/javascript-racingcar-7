017c50541447a8cdd72fe591024005e4e81d463a
TaewoooongHeo01 <chamjoeun0111@gmail.com>
2024-10-25 16:20:29 +0900

    feat(Car): Car 클래스 getName 추가

--- src/Car.js
+++ src/Car.js
@@ -1,9 +1,15 @@
 class Car {
+  #name;
+
   constructor(name) {
     if (!Car.validateCarName(name)) {
       throw new Error('[ERROR] 이름은 5자 이하만 가능합니다');
     }
-    this.name = name;
+    this.#name = name;
+  }
+
+  getName() {
+    return this.#name;
   }
 
   static validateCarName(name) {
