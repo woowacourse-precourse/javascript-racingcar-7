0bc09bf7d5ac12604a4de66188219df7caf29575
thumbthing <ilscc0902@gmail.com>
2024-10-26 03:52:46 +0900

    Feat: 레이스 결과 출력 기능 구현

--- src/feature/getWinner.js
+++ src/feature/getWinner.js
@@ -1,4 +1,6 @@
-function getWinner() {
+import { Console } from "@woowacourse/mission-utils";
+
+function getWinner(carList, raceProgress) {
   /** TODO: 
    * 1. 매개변수로 레이스 결과가 저장된 Map, 자동차 리스트를 받는다
    * 2. 출력할 결과 문구의 변수를 초기화 한다 (최종 우승자: 자동차 이름)
@@ -9,4 +11,38 @@ function getWinner() {
    * 6. 결과 문구와 우승자 문구를 합친다
    * 7. 우승자를 출력한다
    */
-}
+
+  const RESULT_DEFAULT_TEXT = '최종 우승자 : ';
+  const WINNERS = [];
+
+  const RACE_RESULT_COUNT = carList.map((car) => {
+    const RACE_BAR = raceProgress.get(car);
+    const RACE_LENGTH = RACE_BAR.length;
+    return ([car, RACE_LENGTH]);
+  });
+
+  RACE_RESULT_COUNT.sort((carA, carB) => carB[1] - carA[1]);
+
+  RACE_RESULT_COUNT.forEach((car, index) => {
+    const RACE_LENGTH = car[1];
+
+    if(index === 0) {
+      WINNERS.push(car);
+      return;
+    };
+
+    if(WINNERS[0][1] === RACE_LENGTH) {
+      WINNERS.push(car);
+      return;
+    };
+  });
+
+  const WINNER_NAMES = WINNERS.map((winner) => winner[0]);
+
+  const WINNER_TEXT = WINNER_NAMES.join(', ');
+  const FINAL_TEXT = `${RESULT_DEFAULT_TEXT}${WINNER_TEXT}`;
+
+  Console.print(FINAL_TEXT);
+};
+
+export default getWinner;
