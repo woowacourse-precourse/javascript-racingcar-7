4daf30a117ebcb528fed80bcf6043d3fddd205c6
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-10-26 14:48:11 +0900

    test: 자동차는 1대 이상 있어야 한다

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,4 +1,4 @@
-import { splitCarName, validateCarNameLength } from '../src/InputUtils';
+import { splitCarName, validateCarNameLength, validateCarsLength } from '../src/InputUtils';
 
 describe('입력', () => {
   test('자동차 이름은 쉼표(,)를 기준으로 구분한다.', () => {
@@ -11,5 +11,10 @@ describe('입력', () => {
   test('자동차 이름은 5자 이하만 가능하다.', () => {
     const inputs = ['pobi, javaji'];
     expect(() => validateCarNameLength(inputs)).toThrow('[ERROR]');
+  });
+
+  test('자동차는 1대 이상 있어야 한다.', () => {
+    const inputs = [];
+    expect(() => validateCarsLength(inputs)).toThrow('[ERROR]');
   });
 });
