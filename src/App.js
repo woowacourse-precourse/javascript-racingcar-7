bcc7857951b5042be6b8ec661d11bf86054200b6
hwangheetae <tejaia@naver.com>
2024-10-27 02:46:39 +0900

    feat: 경주 점수 산출을 위한 객체 생성

--- src/App.js
+++ src/App.js
@@ -19,14 +19,21 @@ class App {
     }
   }
 
+  setGame(carNames) {
+    const scoreBoard = {};
+    carNames.forEach((name) => {
+      scoreBoard[name] = 0;
+    });
+    return scoreBoard;
+  }
+
   async run() {
     const carNames = await this.getCarNames(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
     );
     const tryNumber = await this.getTryNumber('시도할 횟수는 몇 회인가요?\n');
 
-    Console.print(carNames);
-    Console.print(tryNumber);
+    const scoreBoard = this.setGame(carNames);
   }
 }
 
