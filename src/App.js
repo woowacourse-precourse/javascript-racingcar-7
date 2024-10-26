a9dffeada6272506b1decace6f35164b6e35035a
joarthvr <ioi456859@gmail.com>
2024-10-27 00:20:35 +0900

    feat: 구현한 기능들을 종합 하는 함수 구현 및 app.js에 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from "@woowacourse/mission-utils";
+import racingSituation from "./utils/racingSituation.js";
+import carInput from "./utils/carInput.js";
+import totalTryInput from "./utils/totalTryInput.js";
 class App {
-  async run() {}
+  async run() {
+    const carNames = await carInput();
+    const totalTries = await totalTryInput();
+    const result = await racingSituation(carNames, totalTries);
+    Console.print(`최종 우승자 : ${result}`);
+    return result;
+  }
 }
 
 export default App;
