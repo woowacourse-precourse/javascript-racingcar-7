dd0d2e12e7b30da35eb333de420c3e77068c17e4
jjeongsu <jeongsu.dev@gmail.com>
2024-10-26 15:28:03 +0900

    feat(GAME) : 게임에 필요한 상수 작성

---
+++ src/constants/Game.js
@@ -0,0 +1,13 @@
+const GAME = Obejct.freeze({
+  minNameLength: 1,
+  maxNameLength: 5,
+  minNameCount: 1,
+  maxNameCount: 20,
+  minTryCount: 1,
+  maxTryCount: 100,
+  inputDelimeterSign: ',',
+  inputDelimeterName: '쉼표',
+  minRandomNumber: 0,
+  maxRandomNumber: 9,
+  moveThreshold: 4,
+});
