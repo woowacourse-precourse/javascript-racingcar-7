58d7201c45f127c77a035bd3df6e241550d5c9ee
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-10-26 20:49:37 +0900

    feat: 에러 처리 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import { Console } from '@woowacourse/mission-utils';
-
-// Random.pickNumberInRange() --> 랜덤 값 추출
-// Console.readLineAsync(), Console.print()
-// MissionUtils.Random.pickNumberInRange(0, 9); --> 0에서 9까지의 정수 중 한 개의 정수 반환
+import checkError from './checkError.js';
 
 class App {
   async run() {
     const carNames = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
     const attemptNumber = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
 
-    Console.print(carNames);
-    Console.print(attemptNumber);
+    const carNameArray = carNames.split(',');
+    checkError(carNameArray);
   }
 }
 
