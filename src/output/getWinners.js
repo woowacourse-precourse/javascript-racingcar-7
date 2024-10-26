cbde07415c805f1767d22aefb2875ef325bc7eb7
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-10-26 20:44:44 +0900

    feat: 우승자 유무 검사
    우승자 유무 검사 기능 추가
    
    - 전진 횟수 최대값 구함
    - 전진횟수가 최대인 자동차들로 구성된 우승자 배열 생성
    - 배열이 비어있다면 에러 메세지 출력
    - 배열이 비어있지 않다면 우승자 배열 반환
    
    추가사항: 우승자 출력 로직 추가 예정

--- src/output/getWinners.js
+++ src/output/getWinners.js
@@ -1,7 +1,20 @@
+import ERROR_MESSAGES from '../constraints/errorMessages.js';
+
 function getMaxMoveCount(moveCountArray) {
   return Math.max(...moveCountArray);
 }
 
 function findWinners(carNames, moveCountArray, maxMoveCount) {
   return carNames.filter((_, index) => moveCountArray[index] === maxMoveCount);
+}
+
+export function getWinners(carNames, moveCountArray) {
+  const MAX_MOVE_COUNT = getMaxMoveCount(moveCountArray);
+  const WINNERS = findWinners(carNames, moveCountArray, MAX_MOVE_COUNT);
+
+  if (!WINNERS) {
+    throw new Error(ERROR_MESSAGES.NO_WINNERS);
+  }
+
+  return WINNERS;
 }
