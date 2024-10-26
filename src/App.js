bc90920451f013a82055e602b1527f3a20e2c229
jiminseong <iamjms4237@gachon.ac.kr>
2024-10-26 20:26:38 +0900

    feat(App.js): 자동차 이름 유효성 검사 기능
    자동차의 이름이 5글자 이하인지, 즉 6글자 이상인지 확인하는 validateCarNames 함수를 정의했습니다.
    이 함수를 통해서 error를 message와 함께 throw 합니다.

--- src/App.js
+++ src/App.js
@@ -6,12 +6,27 @@ function getCarArray(CAR_STRING) {
   );
 }
 
+function validateCarNames(CAR_ARRAY) {
+  CAR_ARRAY.forEach((element) => {
+    if (element.length >= 6) {
+      throw new Error(
+        "[ERROR] 자동차의 이름이 6자 이상입니다."
+      );
+    }
+  });
+}
+
 class App {
   async run() {
-    const CAR_STR = await Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
-    );
-    const CAR_ARRAY = getCarArray(CAR_STR);
+    try {
+      const CAR_STR = await Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+      );
+      const CAR_ARRAY = getCarArray(CAR_STR);
+      validateCarNames(CAR_ARRAY);
+    } catch (error) {
+      Console.print(error.message);
+    }
   }
 }
 
