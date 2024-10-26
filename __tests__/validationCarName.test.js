b790e35943af9e6dd8f8a1da2205304aa33a4655
jjang-aaa <jadoit473@gmail.com>
2024-10-27 04:33:57 +0900

    test(validationCarName): 중복된 이름이 입력되었을 때 에러 테스트

--- __tests__/validationCarName.test.js
+++ __tests__/validationCarName.test.js
@@ -6,3 +6,10 @@ test('조건에 맞는 자동차 이름이 입력되었을 때 검증 테스트'
     validateCarName(carNameList);
   }).not.toThrow('[ERROR]');
 });
+
+test('중복된 이름이 입력되었을 때 에러 테스트', () => {
+  expect(() => {
+    const carNameList = ['apple', 'apple'];
+    validateCarName(carNameList);
+  }).toThrow('[ERROR]');
+});
