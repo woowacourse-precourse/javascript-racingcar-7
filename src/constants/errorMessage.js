1a3c80e5181419e92fa0cf5e30f1fe077e5c0f56
jiyun1006 <jiyun4251@gmail.com>
2024-10-26 20:56:11 +0900

    ✍️ rename : 오타 수정
    WRONG_NAME_FORM_EMPTH --> WRONG_NAME_FORM_EMPTY 로 수정

--- src/constants/errorMessage.js
+++ src/constants/errorMessage.js
@@ -1,7 +1,7 @@
 const ERRORMESAGE = {
   WRONG_NAME_LENGTH: '[ERROR] 자동차 이름은 최대 5자까지입니다.',
   WRONG_NAME_FORM_SPACE: '[ERROR] 자동차 이름에는 공백이 들어갈 수 없습니다.',
-  WRONG_NAME_FORM_EMPTH: '[ERROR] 자동차 이름은 필수로 입력해야 합니다.',
+  WRONG_NAME_FORM_EMPTY: '[ERROR] 자동차 이름은 필수로 입력해야 합니다.',
   NOT_DUPLICATE_NAME: '[ERROR] 자동차 이름은 중복될 수 없습니다.',
   NOT_POSITIVE_INTEGER: '[ERROR] 시도 횟수는 1이상의 정수로 입력해야 합니다.',
 };
