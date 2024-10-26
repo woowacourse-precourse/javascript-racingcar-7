7b5e64e195545566beb28583a462af8f2860dee2
spoyodevelop <kjy9505@gmail.com>
2024-10-27 06:51:49 +0900

    fix: fix slight logic error in validateCars

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -51,7 +51,7 @@ function isCarLegit(car) {
 
 // 실제로 발생할 확률은 매우 낮지만, type sanity check
 function validateCars(cars) {
-  if (cars.length === 0) {
+  if (cars.length < 0) {
     throwError(ERROR_MESSAGES.cars.ILLEGAL_CAR_ARRAY);
   }
   cars.forEach((car) => {
