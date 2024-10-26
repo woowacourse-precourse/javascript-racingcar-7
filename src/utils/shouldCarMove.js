6bf6d88f44998d8c366144092449059184b0f5cb
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-10-27 03:16:16 +0900

    refactor(shouldCarMove): 리턴문 간략화

--- src/utils/shouldCarMove.js
+++ src/utils/shouldCarMove.js
@@ -1,9 +1,9 @@
-import { Random } from "@woowacourse/mission-utils"
+import { Random } from "@woowacourse/mission-utils";
 
 const shouldCarMove = () => {
   const randomNum = Random.pickNumberInRange(0, 9);
-  if (randomNum < 4) return false;
-  return true;
-}
+  
+  return randomNum >= 4;
+};
 
 export default shouldCarMove;
