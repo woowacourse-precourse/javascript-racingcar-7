a823882068c57ee1ede88fede8d59c445d309887
ho8ae <xogh2242@gmail.com>
2024-10-26 22:47:48 +0900

    test: 자동차 경주 게임 도메인 및 유효성 검증 테스트 추가

---
+++ __tests__/CarRacingController.js
@@ -0,0 +1,40 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import CarRacingController from "../src/controller/CarRacingController.js";
+
+const getLogSpy = () => {
+    const logSpy = jest.spyOn(MissionUtils.Console, "print");
+    logSpy.mockClear();
+    return logSpy;
+  };
+
+describe('CarRacingController 테스트', () => {
+  test('여러 명이 동시 우승하는 경우', async () => {
+    const controller = new CarRacingController();
+    controller.cars = [
+      { getName: () => 'pobi', getPosition: () => 5 },
+      { getName: () => 'jun', getPosition: () => 5 },
+      { getName: () => 'woni', getPosition: () => 4 }
+    ];
+
+    const logSpy = getLogSpy();
+    controller.announceWinners();
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining('최종 우승자 : pobi, jun')
+    );
+  });
+
+  test('단독 우승하는 경우', async () => {
+    const controller = new CarRacingController();
+    controller.cars = [
+      { getName: () => 'pobi', getPosition: () => 5 },
+      { getName: () => 'jun', getPosition: () => 4 },
+      { getName: () => 'woni', getPosition: () => 3 }
+    ];
+
+    const logSpy = getLogSpy();
+    controller.announceWinners();
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining('최종 우승자 : pobi')
+    );
+  });
+});
