db7bc49a9c87396b6c59c0fc865d742acf4a759e
Seio924 <seio9241@gmail.com>
2024-10-26 09:53:12 +0900

    feat(RaceService): 조건에 따른 전진 기능 추가

--- src/Service/RaceService.js
+++ src/Service/RaceService.js
@@ -1,21 +1,39 @@
 import Car from '../Model/Car.js';
 
 class RaceService {
-  #car;
+  #cars;
 
   constructor() {
-    this.#car = [];
+    this.#cars = [];
   }
 
   start(carNames, attemptCount) {
     for (let i = 0; i <= carNames.length; i++) {
-      this.#car.push(new Car(carNames[i]));
+      this.#cars.push(new Car(carNames[i]));
     }
-    this.generateRandomNumber();
+
+    for (let i = 0; i <= attemptCount; i++) {
+      this.moveCar(this.#cars);
+    }
   }
 
   generateRandomNumber() {
     return Math.floor(Math.random() * 10);
+  }
+
+  decideMoveForward() {
+    if (this.generateRandomNumber() >= 4) {
+      return true;
+    }
+    return false;
+  }
+
+  moveCar(cars) {
+    cars.map(car => {
+      if (this.decideMoveForward()) {
+        car.moveForward();
+      }
+    });
   }
 }
 
