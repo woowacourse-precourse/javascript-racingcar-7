9dc4ab246f9a4b1ac158dc649844e78a49e3b6e8
지태 <wlxo402@naver.com>
2024-10-26 17:05:51 +0900

    feat:입력받은 문자열을 배열에 저장하기

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const InputCarNames = await Console.readLineAsync(
+    // 자동차 문자열 입력
+    const inputCarNames = await Console.readLineAsync(
       `경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n`
     );
-    Console.print(InputCarNames);
+
+    // 입력받은 문자열을 "," 기준으로 나눔
+    const carNamesArray = inputCarNames.split(",");
+
+    // 테스트 코드
+    Console.print(carNamesArray);
   }
 }
 
