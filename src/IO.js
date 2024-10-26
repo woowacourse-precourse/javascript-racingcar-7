f86c220f7a496f65b8f2f9c4744a015b4a4c0806
ahhyun <wndkgus0225@naver.com>
2024-10-26 13:29:00 +0900

    refactor(IO) : 경주 게임이 포함된 입출력 코드 리펙토링

--- src/IO.js
+++ src/IO.js
@@ -1,14 +1,25 @@
 import { Console } from '@woowacourse/mission-utils';
+import { validateCarNames } from "./error.js";
+import { validateCount } from "./error.js";
 
-export async function getCarName(){
+export async function getCarNames() {
   const input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-  const carName = input.split(",");
-  return carName;
+  const carNames = input.split(",");
+  validateCarNames(carNames);
+  return carNames;
 }
 
-export async function getCount(){
+export async function getCount() {
   const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
   const count = Number(input);
+  validateCount(count);
   return count;
+}
+
+export function displayResults(results) {
+  results.forEach(car => {
+    Console.print(`${car.name} : ${car.position}`);
+  });
+  Console.print("");
 }
 
