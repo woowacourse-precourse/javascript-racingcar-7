a9e210c53ae9a7ca823b984920fd9d2b4e70126d
bbjbc <jobc4550@gmail.com>
2024-10-26 23:39:13 +0900

    feat: 사용자 입력 처리, 출력 메서드 구현

---
+++ src/InputView.js
@@ -0,0 +1,17 @@
+import { Console } from "@woowacourse/mission-utils";
+
+class InputView {
+  async inputCarNames() {
+    const input = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    return input;
+  }
+
+  async inputTryCount() {
+    const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    return input;
+  }
+}
+
+export default InputView;
---
+++ src/OutputView.js
@@ -0,0 +1,9 @@
+import { Console } from "@woowacourse/mission-utils";
+
+class OutputView {
+  printGameStartMessage() {
+    Console.print("\n실행 결과");
+  }
+}
+
+export default OutputView;
