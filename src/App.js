42875272cabdbf65d48c78d9fa304600daaac6b0
glyph8 <newdy0808@gmail.com>
2024-10-27 00:24:15 +0900

    feat(input) 자동차 이름입력 기능 추가
    쉼표(,) 기준으로 자동차 이름을 구분하여 입력받고, 5자 이상의 경우 ERROR 발생하도록 처리.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import {Console, Random} from "@woowacourse/mission-utils"
 
+function checkCarName(carNames){
+    carNames.map((name)=>{
+        if(name.length > 5){
+            throw new Error("[ERROR] : 자동차 이름은 5자 이하만 가능합니다.");
+        }
+    })
+    return true;
+}
+
 class App {
   async run() {
     Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
-    const carNames = await Console.readLineAsync("");
+    const carNamesInput = await Console.readLineAsync("");
+
+    const carNames = carNamesInput.split(",");
+    checkCarName(carNames);
 
     Console.print("시도할 횟수는 몇 회인가요?");
     const moveCount = await Console.readLineAsync("");
@@ -12,7 +24,6 @@ class App {
     Console.print("실행 결과");
 
     Console.print("최종 우승자 : ");
-
   }
 }
 
