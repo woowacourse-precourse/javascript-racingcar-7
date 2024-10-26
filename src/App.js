0555448837916292fa696a8a64e8168804e74290
Yun-Jinwoo <yjw3480@naver.com>
2024-10-27 01:23:43 +0900

    ♻️ refactor: 코드 상수화 진행

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import { Console, Random } from "@woowacourse/mission-utils"
+import { ERROR_MESSAGES } from "./ErrorMessages.js";
 
 class App {
   async run() {
     const inputCar = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"); // 자동차 이름 입력받음
     if (inputCar.length === 0) {
-      throw new Error("[ERROR] 입력된 자동차가 없습니다.");
+      throw new Error(ERROR_MESSAGES.empty_car_list);
     }
     const cars = inputCar.split(",");
     if (cars.length === 1) {
-      throw new Error("[ERROR] 입력된 자동차가 한 개 뿐입니다.")
+      throw new Error(ERROR_MESSAGES.only_one_car)
     }
     const uniqueCars = new Set(cars);
     if (uniqueCars.size !== cars.length) {
-      throw new Error("[ERROR] 중복된 자동차 이름이 있습니다.");
+      throw new Error(ERROR_MESSAGES.duplicate_car);
     }
     cars.forEach((car) => { // 자동차 이름 5자 이하로 제한
       if (car.length > 5) {
-        throw new Error(`[ERROR] "${car}"의 길이가 5를 초과합니다.`);
+        throw new Error(ERROR_MESSAGES.long_name_car);
       }
     })
     const inputNum = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
     const count = parseInt(inputNum);
     if (count < 0) {
-      throw new Error('[ERROR] 양수를 입력해주세요.')
+      throw new Error(ERROR_MESSAGES.not_positive_number)
     }
     if (isNaN(count)) {
-      throw new Error(`[ERROR] 숫자가 아닌 값을 입력하면 안됩니다. `)
+      throw new Error(ERROR_MESSAGES.not_number)
     }
 
 
