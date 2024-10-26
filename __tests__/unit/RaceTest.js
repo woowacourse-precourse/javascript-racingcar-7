5cbcbda871d542d7743559db3da8dc5f0bbc60a2
Daeuning <happyjde3@gmail.com>
2024-10-26 23:58:45 +0900

    test(RaceTest): beforeEach에서 Race 인스턴스를 생성하도록 수정
    beforeEach를 사용함으로서 테스트마다 매번 Race 인스턴스를 생성하지 않도록 합니다

--- __tests__/unit/RaceTest.js
+++ __tests__/unit/RaceTest.js
@@ -7,12 +7,15 @@ jest.mock('../../src/utils.js', () => ({
 }));
 
 describe("Race 클래스 테스트", () => {
+  let race;
+
+  beforeEach(() => {
+    race = new Race();
+  });
 
   describe("setCarName() 테스트", () => {
 
     test("자동차 이름마다 Car 객체가 생성되어야 한다", async () => {
-      const race = new Race();
-
       await race.setCarName(); 
 
       expect(race.cars).toHaveLength(3); 
