12fd3698f735e56eeda6627303e3697bcebe87d0
ekkk1126 <cabi0203@naver.com>
2024-10-27 00:44:04 +0900

    feat: 자동차별 랜덤값 생성하여 자동차 경주 실행

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Input from './console/input.js';
+import Output from './console/output.js';
+import Racing from './racing.js';
+
 class App {
-  async run() {}
+    async run() {
+        const { cars, tryCount } = await Input();
+
+        const result = Racing(cars, tryCount);
+
+        Output(result);
+    }
 }
 
 export default App;
