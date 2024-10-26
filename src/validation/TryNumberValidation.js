17b4cbbf4c02f1e8a7e56f86bcc5edd87c25e60c
jjeongsu <jeongsu.dev@gmail.com>
2024-10-26 18:59:54 +0900

    fix : 0,101입력시 숫자검증에서 에러가 반환되는 오류 해결

--- src/validation/TryNumberValidation.js
+++ src/validation/TryNumberValidation.js
@@ -1,10 +1,13 @@
+import GAME from '../constants/Game.js';
+
 class TryNumberValidation {
   static checkIsEmpty(tryNumber) {
     return !tryNumber;
   }
 
   static checkIsNaN(tryNumber) {
-    return !Number.isInteger(tryNumber);
+    const number = Number(tryNumber);
+    return !Number.isInteger(number) || isNaN(number);
   }
 }
 export default TryNumberValidation;
