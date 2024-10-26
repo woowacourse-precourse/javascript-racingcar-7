18b67942e16aecfd6693bab00a98301ac2d1a647
leejaejoon <jaejoon0721@ajou.ac.kr>
2024-10-27 01:55:07 +0900

    feat: 자동차 이름, 시도 횟수 입력 함수
    검증 함수 추후 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const { carNames, attemptCount } = await this.getUserInput();
+  }
+
+  async getUserInput() {
+    const carNamesInput = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분): ',
+    );
+    const attemptCountInput =
+      await Console.readLineAsync('시도할 횟수는 몇 회인가요?: ');
+
+    const carNames = this.validateCarNames(carNamesInput);
+    const attemptCount = this.validateAttemptCount(attemptCountInput);
+
+    return { carNames, attemptCount };
+  }
 }
 
 export default App;
