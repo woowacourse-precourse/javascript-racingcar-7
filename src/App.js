558b4d479be0866c483f25f9eea89badf81e0215
gyur1kim <qri1114@gmail.com>
2024-10-26 21:16:39 +0900

    feat(input): 자동차 이름 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console } from '@woowacourse/mission-utils'
+
 class App {
-  async run() {}
+  constructor () {
+    this.carNames = []
+    this.output = ''
+  }
+
+  async getCarNames () {
+    try {
+      const input = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+      this.carNames = input.split(',');
+    } catch(e) {
+      throw e;
+    }
+  }
+
+  async run() {
+    await this.getCarNames();
+    Console.print(this.carNames)
+  }
 }
 
 export default App;
