18fe3e46acf7e0e614205620b833bd7854636059
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-10-27 03:42:01 +0900

    feat(inputHandler.js): 레이싱 횟수 입력 함수 구현

--- src/views/inputHandler.js
+++ src/views/inputHandler.js
@@ -6,9 +6,24 @@ export const inputHandler = {
     const carNames = await Console.readLineAsync(GAME_MESSAGES.START);
 
     if (carNames.trim() === "") {
-      return new Error(ERROR_MESSAGES.CAR_ERROR_MESSAGES.BLANK);
+      throw new Error(ERROR_MESSAGES.CAR_ERROR_MESSAGES.BLANK);
     }
-
     return carNames;
+  },
+
+  racingTryCountInput: async () => {
+    const racingTryCount = await Console.readLineAsync(
+      GAME_MESSAGES.ANSWER_COUNT
+    );
+    if (racingTryCount.trim() === "") {
+      throw new Error(ERROR_MESSAGES.CAR_ERROR_MESSAGES.BLANK);
+    }
+    if (isNaN(racingTryCount)) {
+      throw new Error(ERROR_MESSAGES.COUNT_ERROR_MESSAGES.ONLY_NUMBER);
+    }
+    if (racingTryCount < 1) {
+      throw new Error(ERROR_MESSAGES.COUNT_ERROR_MESSAGES.NOT_NEGATIVE);
+    }
+    return racingTryCount;
   },
 };
