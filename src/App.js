741445ff33baa75d13fecc0920bda359bb62c985
Sohee <sh11govlday@gmail.com>
2024-10-26 18:53:06 +0900

    feat(App) : 추가적인 예외 처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Console } from '@woowacourse/mission-utils';
 import { MissionUtils } from '@woowacourse/mission-utils';
 
+const NUMBER_REGEX = /^[0-9]*$/;
+const ENGLISH_REGEX = /^[a-zA-Z]*$/;
+const SPACE_REGEX = /\s/g;
+const ATTEMPTS_LIMIT = 50;
 const MOVE_MINIMUM = 4;
 
 class App {
   async run() {
     try {
       const cars = await this.getCars();
-      const attemps = await this.getAttemps();
-      const winners = this.racingCar(cars, attemps);
+      const attempts = await this.getAttempts();
+      const winners = this.racingCar(cars, attempts);
       Console.print(`최종 우승자 : ${winners.join(', ')}`);
     } catch (error) {
       Console.print(error.message);
@@ -18,7 +22,7 @@ class App {
 
   async getCars() {
     const carNames = await this.inputCarNames();
-    await this.checkNameLength(carNames);
+    await this.checkCarNameError(carNames);
     const cars = this.carNamesToObject(carNames);
     return cars;
   }
@@ -28,14 +32,25 @@ class App {
       await Console.readLineAsync(
         '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
       )
-    ).split(',');
+    )
+      .split(',')
+      .map((name) => name.trim());
+
     return carNames;
   }
 
-  async checkNameLength(carNames) {
-    const nameLength = carNames.some((name) => name.length > 5);
-    if (nameLength)
-      throw new Error('[ERROR] 자동차 이름은 5자 이하여야 합니다.');
+  async checkCarNameError(carNames) {
+    let errorType = '';
+
+    if (carNames.some((name) => name.length > 5)) errorType = 'carNameLength';
+
+    if (carNames.some((name) => SPACE_REGEX.test(name)))
+      errorType = 'carNameSpace';
+
+    if (carNames.some((name) => !ENGLISH_REGEX.test(name)))
+      errorType = 'carNameEnglish';
+
+    await this.checkMessage(errorType);
   }
 
   carNamesToObject(carNames) {
@@ -43,18 +58,35 @@ class App {
       acc[name] = 0;
       return acc;
     }, {});
+
     return cars;
   }
 
-  async getAttemps() {
-    return await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+  async getAttempts() {
+    const attempts = parseInt(
+      await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n')
+    );
+    await this.checkAttemptsError(attempts);
+    return attempts;
   }
 
-  racingCar(cars, attemps) {
+  async checkAttemptsError(attempts) {
+    let errorType = '';
+
+    if (!NUMBER_REGEX.test(attempts)) errorType = 'attemptsNotNumber';
+
+    if (attempts > ATTEMPTS_LIMIT) errorType = 'attemptsLimit';
+
+    await this.checkMessage(errorType);
+  }
+
+  racingCar(cars, attempts) {
     Console.print('\n실행 결과');
-    for (let i = 0; i < attemps; i++) {
+
+    for (let i = 0; i < attempts; i++) {
       this.eachRound(cars);
     }
+
     return this.getWinners(cars);
   }
 
@@ -63,11 +95,13 @@ class App {
       const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
       this.decideMovement(cars, randomNumber, key, value);
     });
+
     Console.print('');
   }
 
   decideMovement(cars, randomNumber, key, value) {
     if (randomNumber >= MOVE_MINIMUM) cars[key] = value + 1;
+
     Console.print(`${key} : ${cars[key]}`);
   }
 
@@ -75,6 +109,21 @@ class App {
     const max = Math.max(...Object.values(cars));
     const winners = Object.keys(cars).filter((name) => cars[name] === max);
     return winners;
+  }
+
+  async checkMessage(type) {
+    switch (type) {
+      case 'carNameLength':
+        throw new Error('[ERROR] 자동차 이름은 5자 이하여야 합니다.');
+      case 'carNameSpace':
+        throw new Error('[ERROR] 자동차 이름은 공백 없이 입력해주세요.');
+      case 'carNameEnglish':
+        throw new Error('[ERROR] 자동차 이름은 영어로 입력해주세요.');
+      case 'attemptsNotNumber':
+        throw new Error('[ERROR] 시도할 횟수는 숫자로 입력해주세요.');
+      case 'attemptsLimit':
+        throw new Error('[ERROR] 시도할 횟수는 최대 50회까지 가능합니다.');
+    }
   }
 }
 
