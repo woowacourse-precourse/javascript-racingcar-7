df21d6989755a5d69822e9e08c72fb42e01fa60a
ahhyun <wndkgus0225@naver.com>
2024-10-26 15:42:40 +0900

    feat(racing) : 기존 randomRacing 함수에 최종 우승자를 고르는 함수도 추가

--- src/racing.js
+++ src/racing.js
@@ -1,15 +1,24 @@
 import * as MissionUtils from '@woowacourse/mission-utils';
+import { displayWinner } from "./IO.js";
 
 export function randomRacing(carNames, count, displayResults) {
-  let results = carNames.map(name => ({ name, position: "" })); 
+  let results = carNames.map(name => ({ name, position: "", advanceCount: 0 })); 
 
   for (let i = 1; i <= count; i++) {
-    results.forEach(car => { 
-      const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
-      if (randomNumber >= 4) { 
-        car.position += "-";
-      }
-    });
+    results = updatePositions(results);
     displayResults(results);
   }
+  displayWinner(results);
 }
+
+function updatePositions(results) {
+  return results.map(car => {
+    const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+    if (randomNumber >= 4) { 
+      car.position += "-";
+      car.advanceCount ++;
+    }
+    return car;
+  });
+}
+
