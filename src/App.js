76b99036f7db6d12e01e6ec4e9c6dbb4102aa4d8
Ko Daeun <drechsu@gmail.com>
2024-10-26 17:07:55 +0900

    feat(App): 기본적인 입력 함수 뼈대 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async getCarNameAndAttempts() {
+    try {
+      const carName = await Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+      );
+      const attempts = await Console.readLineAsync(
+        "시도할 횟수는 몇 회인가요?\n"
+      );
+    } catch (error) {}
+  }
+  async run() {
+    this.getCarNameAndAttempts();
+  }
 }
 
 export default App;
