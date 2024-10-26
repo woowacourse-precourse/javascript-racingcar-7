bc0e4225247d2c216d5bf441721cb2aeef07dcde
sanghyeonLee <hyeon012366@gmail.com>
2024-10-27 02:36:25 +0900

    refactor: 프롬프트 메세지 전역 변수 출력 순회를 위한 키값이 존재하는 배열로 수정

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,4 +1,4 @@
-export const PROMPT_MESSAGE = {
-    INPUT_CAR_NAME:"경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
-    INPUT_TRY_NUMBER:"시도할 횟수는 몇 회인가요?\n"
-}
+export const PROMPT_MESSAGE = [
+    {key: 0, FIRST :"경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"},
+    {key :1, SECOND : "시도할 횟수는 몇 회인가요?\n"}
+]
