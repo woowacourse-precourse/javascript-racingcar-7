e5dfd3be7526e0f48d5f3c56dc16035a89aac911
jiyun1006 <jiyun4251@gmail.com>
2024-10-26 20:26:08 +0900

    ✨ feat : 자동차 이름 중복되는지 체크
    isNotDuplicate 메서드에서 자동차 이름 중복 체크

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,11 +1,46 @@
 import { MAGICNUMBER } from '../constants/index.js';
 
 const validation = {
+  /**
+   *
+   * @param {Array} inputArr
+   * @returns
+   */
   isLengthBelowFive: function isLengthBelowFive(inputArr) {
     return inputArr.every((input) => input.length < 6);
   },
+  /**
+   *
+   * @param {Array} inputArr
+   * @returns {Boolean}
+   */
   hasSpace: function hasSpace(inputArr) {
     return inputArr.some((input) => /\s/g.test(input));
+  },
+  /**
+   *
+   * @param {Array} inputArr
+   * @returns {Boolean}
+   */
+  isEmpty: function isEmpty(inputArr) {
+    return inputArr.some((input) => !input);
+  },
+  /**
+   *
+   * @param {Array} inputArr
+   * @returns {Boolean}
+   */
+  isNotDuplicate: function isNotDuplicate(inputArr) {
+    const tmpSet = new Set(inputArr);
+    return inputArr.length === tmpSet.size;
+  },
+  /**
+   *
+   * @param {Number} input
+   * @returns {Boolean}
+   */
+  isPositiveInteger: function isPositiveInteger(input) {
+    return input > 0 && Number.isInteger(input);
   },
 };
 
