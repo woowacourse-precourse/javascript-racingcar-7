aff8a3482c4b9416388e97eb06fb61b8256b1a2d
daeun <gbajs754@gmail.com>
2024-10-27 01:08:03 +0900

    refactor : private 메서드로 레이싱 로직 수정

--- src/Racing.js
+++ src/Racing.js
@@ -5,38 +5,31 @@ class Racing {
 
   #racers = [];
 
-  #resultByRound = [];
-
   constructor({ totalRound, racers }) {
     this.#totalRound = totalRound;
     this.#racers = racers;
   }
 
-  go() {
+  start() {
+    for (let i = 0; i < this.#totalRound; i += 1) {
+      this.#moveForward();
+      Console.print(this.#getResultByRound().join('\n'));
+    }
+    return this;
+  }
+
+  #moveForward() {
     this.#racers.forEach((racer) => {
       if (Random.pickNumberInRange(0, 9) >= 4) {
         racer.addScore();
       }
     });
-
-    return this;
   }
 
-  start() {
-    for (let i = 0; i < this.#totalRound; i++) {
-      this.#resultByRound = [];
-      this.go();
-      Console.print(this.getResultByRound().join('\n'));
-    }
-    return this;
-  }
-
-  getResultByRound() {
-    this.#racers.forEach((racer) => {
-      this.#resultByRound.push(`${racer.getName()} : ${racer.getRaceLine()}`);
-    });
-
-    return this.#resultByRound;
+  #getResultByRound() {
+    return this.#racers.map(
+      (racer) => `${racer.getName()} : ${racer.getRaceLine()}`,
+    );
   }
 }
 
