fe8e3ae7ec5ca6e9cfb3942971795806c0c1e8f3
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-10-26 23:35:44 +0900

    refactor: 불필요한 주석, import 삭제

--- src/views/View.js
+++ src/views/View.js
@@ -1,7 +1,5 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import App from '../App.js';
 
 export function getUserInput() {
-  // 입력을 비동기적으로 받아오는 함수
-  return MissionUtils.Console.readLineAsync(); // promist 반환
+  return MissionUtils.Console.readLineAsync(''); // promist 반환
 }
