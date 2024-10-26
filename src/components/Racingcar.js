a66e7dd617a9df49ddb1a15636f0a2705f866424
jiyun1006 <jiyun4251@gmail.com>
2024-10-26 22:41:16 +0900

    ✨ feat : 게임 진행 & 진행 과정 보드판 생성
    createBoard를 이용해 게임 진행판을 만듦.
    진행 횟수에 따른 진행 상황을 표시

--- src/components/Racingcar.js
+++ src/components/Racingcar.js
@@ -1,13 +1,18 @@
-import { validation } from '../utils/index.js';
-import { ERRORMESAGE } from '../constants/index.js';
+import { validation, pickRandomNumber } from '../utils/index.js';
+import { ERRORMESAGE, MAGICNUMBER } from '../constants/index.js';
 
 export default class Racingcar {
+  #inputArr;
+  #tryNum;
+  #board;
   /**
    *
    * @param {Array} inputArr
    */
   constructor(inputArr, tryNum) {
     this.#validate(inputArr, tryNum);
+    [this.#inputArr, this.#tryNum] = [inputArr, tryNum];
+    this.#board = this.#createBoard();
   }
 
   #validate(inputArr, tryNum) {
@@ -21,5 +26,25 @@ export default class Racingcar {
       throw new Error(`${ERRORMESAGE.NOT_DUPLICATE_NAME}`);
     if (!validation.isPositiveInteger(tryNum))
       throw new Error(`${ERRORMESAGE.NOT_POSITIVE_INTEGER}`);
+  }
+
+  play() {
+    for (const car of this.#inputArr) {
+      if (pickRandomNumber() >= MAGICNUMBER.GOCONDITIONNUM)
+        this.#board[car] += '-';
+    }
+    return this.#board;
+  }
+
+  #createBoard() {
+    const board = {};
+    this.#inputArr.map((car) => {
+      board[car] = '';
+    });
+    return board;
+  }
+
+  get() {
+    return [this.#inputArr, this.#tryNum];
   }
 }
