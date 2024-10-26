9fb2f8bd5785281db03a2c09232b580166ca7e24
영 <77565980+abyss-s@users.noreply.github.com>
2024-10-27 01:35:06 +0900

    feat(validators.js): 예외 처리 로직 함수 구현

---
+++ src/validators.js
@@ -0,0 +1,26 @@
+import ERRORS from './datas/error.js';
+
+export const validateCarNames = (carNames) => {
+  const trimmedNames = carNames.map((name) => name.trim());
+
+  if (trimmedNames.some((name) => name === '')) {
+    throw new Error(ERRORS.EMPTY_CAR_NAME);
+  }
+
+  const nameArray = [];
+  trimmedNames.forEach((name) => {
+    if (name.length > 5) {
+      throw new Error(ERRORS.INVALID_CAR_NAME);
+    }
+    if (nameArray.includes(name)) {
+      throw new Error(ERRORS.DUPLICATE_CAR_NAME);
+    }
+    nameArray.push(name);
+  });
+};
+
+export const validateTrialCount = (count) => {
+  if (count == null || isNaN(count) || count <= 0) {
+    throw new Error(ERRORS.INVALID_TRIAL_COUNT);
+  }
+};
