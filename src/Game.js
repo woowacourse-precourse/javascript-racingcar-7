c1b5f97fe582e4ad7544f5b4eb9a1be41ca834b2
조승우 <choseung97@gmail.com>
2024-10-26 22:37:56 +0900

    feat(game): Round 별 결과를 출력하는 기능을 추가한다
    매 Round가 종료된 직후 Round 결과를 출력한다.

--- src/Game.js
+++ src/Game.js
@@ -19,8 +19,10 @@ class Game {
   }
 
   play() {
+    Console.print('\n실행 결과');
     while (this.currentRepeat !== this.repetitionNumber) {
       this.startRound();
+      this.printRoundResult();
       this.currentRepeat += 1;
     }
   }
@@ -30,6 +32,15 @@ class Game {
       if (!this.canMoveForward()) return;
       car.moveForward();
     });
+  }
+
+  printRoundResults() {
+    this.#CARS_LIST.forEach((car) => {
+      const distanceString = '-'.repeat(car.currentDistance);
+      Console.print(`${car.name} : ${distanceString}`);
+    });
+
+    Console.print(''); // Round 별 구분을 위한 공백
   }
 
   canMoveForward() {
