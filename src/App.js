553987e4ccefaa84c8bcd7dc19e19bfc45cf9109
gyur1kim <qri1114@gmail.com>
2024-10-26 21:37:30 +0900

    feat(input): 몇 번의 이동을 할 것인지 입력받기
    - 숫자가 아닌 수를 입력 시 에러 처리
    - 1 미만의 숫자 에러 처리

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Console } from '@woowacourse/mission-utils'
 class App {
   constructor () {
     this.carNames = []
+    this.rounds = 0
     this.output = ''
   }
 
@@ -18,16 +19,28 @@ class App {
   async getCarNames () {
     try {
       const input = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+      
       this.carNames = input.split(',').map(this.isValidCarName);
     } catch(e) {
       throw e;
     }
   }
 
+  async getRounds () {
+    const input = Number(await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n'));
+     
+    if (Number.isNaN(input)) throw new Error('[ERROR] 시도할 횟수는 숫자만 입력해주세요')
+    if (input <= 0) throw new Error('[ERROR] 시도 횟수는 최소 1회입니다.')
+    this.rounds = input
+  }
+
   async run() {
     try {
       await this.getCarNames();
+      await this.getRounds();
+
       Console.print(this.carNames);
+      Console.print(this.rounds);
     } catch(e) {
       Console.print(e.message);
       throw e;
