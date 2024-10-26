3e8bfeaca8ad1ed44f7f800e65f08eabba5bf91a
MyungJiwoo <1206jiwoo@gmail.com>
2024-10-26 17:47:24 +0900

    feat: 입력된 자동차 이름별 인스턴스 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { Console } from "@woowacourse/mission-utils";
+class Car {
+    constructor(name) {
+        this.name = name;
+        this.moveScore = 0;
+    }
+
+    printName() {
+        Console.print(this.name);
+    }
+
+    printMoveScore() {
+        Console.print(this.moveScore);
+    }
+}
+
 class App {
-  async run() {}
+    async run() {
+        try {
+            const inputCar = await Console.readLineAsync(
+                "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+            );
+
+            const cars = inputCar.split(",").map((car) => new Car(car.trim()));
+        } catch (error) {
+            Console.print(`${error.message}`);
+            throw error;
+        }
+    }
 }
 
 export default App;
