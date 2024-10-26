3ad42b262273f4b05c6bec04da6c78e5a4b8f6a2
HS <hasang2691@naver.com>
2024-10-26 15:58:02 +0900

    feat(validator.js): Validator에서 자동차 이름이 빈 문자열이나 공백인지 확인하는 기능 구현
    - carNames에 자동차 이름이 빈 문자열이나 공백인지 확인하는 validateEmptyNames 메서드 구현

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -6,6 +6,14 @@ const Validator = {
       OutputView.throwError('자동차가 최소 1대 이상 있어야 합니다.');
     }
   },
+
+  validateEmptyNames(carNames) {
+    const hasEmptyName = carNames.some((name) => name.trim().length === 0);
+
+    if (hasEmptyName) {
+      OutputView.throwError('자동차 이름은 빈 문자열이나 공백일 수 없습니다.');
+    }
+  },
 };
 
 export default Validator;
