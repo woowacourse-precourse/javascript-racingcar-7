7862660ef8350bcf879ec888e2c0c12708890932
thumbthing <ilscc0902@gmail.com>
2024-10-27 00:59:24 +0900

    Refactor: 자동차 이름 유효성 검사 기능 분리

--- src/UI/carNameInput.js
+++ src/UI/carNameInput.js
@@ -1,42 +1,14 @@
 import { Console } from "@woowacourse/mission-utils"
+import checkCarName from "../feature/validate/carNameValidate.js";
 
 async function carNameInput() {
-  /**
-   * TODO: 
-   * 1. 사용자의 입력을 비동기로 입력받고 변수에 저장한다
-   * 2. 변수의 문자열을 쉼표로(,) split해서 배열로 변환한다
-   * 3. 배열로 변환된 값의 각 요소의 유효성을 검사한다
-   *  - 5자 이하인 값인지 검사한다
-   *  - 유효성을 통과하지 못했을 경우 에러를 반환한다
-   * 4. 유효성을 통과한 값의 배열을 반환한다
-   */
   try {
     const USER_INPUT = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-    const CAR_NAME = USER_INPUT.split(',');
+    const CAR_LIST = USER_INPUT.split(',');
 
-    CAR_NAME.forEach((name) => {
-      if(name.length >= 6) {
-        const OVER_FIVE_CHARACTOR = '자동차 이름이 5글자를 초과 합니다.';
-        const ERROR = new Error(OVER_FIVE_CHARACTOR);
-        throw ERROR;
-      };
-
-      if(name.length === 0) {
-        const EMPTY_INPUT = '자동차 이름이 설정되지 않았습니다.';
-        const ERROR = new Error(EMPTY_INPUT);
-        throw ERROR;
-      };
-
-      const DUPLICATE_CHECK = CAR_NAME.filter((car) => car === name);
-
-      if(DUPLICATE_CHECK.length > 1) {
-        const HAS_DUPLICATE_NAME = '중복된 자동차 이름이 존재 합니다.';
-        const ERROR = new Error(HAS_DUPLICATE_NAME);
-        throw ERROR;
-      }
-    });
+    checkCarName(CAR_LIST);
   
-    return CAR_NAME;
+    return CAR_LIST;
   } catch (error) {
     throw error;
   }
