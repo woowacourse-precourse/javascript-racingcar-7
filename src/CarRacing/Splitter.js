e057b9d04c765f4b178f8b0e2b74c1d1524e8a1a
Lee <dltmdgns0508@gmail.com>
2024-10-27 00:23:01 +0900

    style(Splitter): 코드의 화살표 함수 양옆 공백 추가

--- src/CarRacing/Splitter.js
+++ src/CarRacing/Splitter.js
@@ -1,6 +1,6 @@
 class Splitter {
   static split(rawInput) {
-    const inputs = rawInput.split(',').map(input=>input.trim());
+    const inputs = rawInput.split(',').map(input => input.trim());
     return inputs;
   }
 }
