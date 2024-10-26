65832532b67f79cdad9b88d7a0af09f7140794ce
dudcns02 <duddl2640@naver.com>
2024-10-26 13:10:21 +0900

    docs: Add error message

--- src/Utils.js
+++ src/Utils.js
@@ -3,26 +3,26 @@ class Utils {
   constructor(carInput) {
     this.carInput = new carInput();
   }
-  carCountLimitCheck(carInput) {
-    if (carInput.lenght < 5) {
-      // 자동차 개수 체크 5이하만 가능
-      checkSpace(carInput);
-    } else {
-      throw new Error(ERROR_MESSAGE.INCORRECT_VALUE);
-    }
-  }
-
-  checkSpace(carInput) {
-    // 공백 확인
-    if (/\s/.test(carInput) === false) {
-      duplicateCheck();
-    } else {
-      throw new Error(ERROR_MESSAGE.INCORRECT_VALUE);
-    }
-  }
-  duplicateCheck() {
-    // 중복 검사
+}
+export function carCountLimitCheck(carInput) {
+  if (carInput.length < 5) {
+    // 자동차 개수 체크 5이하만 가능
+    checkSpace(carInput);
+  } else {
+    throw new Error(ERROR_MESSAGE.INCORRECT_VALUE);
   }
 }
-// export default Utils;
-export { carCountLimitCheck };
+
+function checkSpace(carInput) {
+  // 공백 확인
+  if (/\s/.test(carInput) || carInput.includes("")) {
+    throw new Error(ERROR_MESSAGE.INCORRECT_VALUE);
+  } else {
+    duplicateCheck(carInput);
+  }
+}
+function duplicateCheck(carInput) {
+  // 중복 검사
+}
+// }
+export default Utils;
