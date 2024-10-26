ce33027a6c04a1bb7bd045265f2e1dd9d4dd935a
jsy <suyeonb1db@gmail.com>
2024-10-26 22:50:14 +0900

    feat(App): 자동차 이름 입력

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
+async function getCarname() {
+  const input = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n");
+  return input;
+}
+
 class App {
-  async run() {}
+  async run() {
+    const carnames = await getCarname();
+  }
 }
 
 export default App;
