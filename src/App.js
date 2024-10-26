6575291b21bc81af8e05471615b08007901733c6
govl0407 <govl0407@gmail.com>
2024-10-26 23:31:32 +0900

    feat : 1차 구현
    입력 자동차명 split구현
    반복 횟수 만큼 게임 진행 구현
    자동차별 점수 구현
    우승자 판별 및 출력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import {Console, Random} from '@woowacourse/mission-utils'
+
 class App {
-  async run() {}
+  async run() {
+    let cars = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");//사용자 입력
+    cars = cars.split(',');
+    let iter = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    let sco =[];
+    for(let i = 0;i<cars.length;i++) {
+      sco.push(0);
+    }
+    Console.print("실행결과");
+    for(let i = 0;i<iter;i++){
+      for(let j=0; j<cars.length;j++){
+        if(Random.pickNumberInRange(0,9)>=4){
+          sco[j]++;
+        }
+        Console.print(cars[j] + " : "+"-".repeat(sco[j]));
+      }
+      Console.print(" ");
+    }
+
+    let max = 0;
+    let winner = [];
+    for(let i =0;i<cars.length;i++){
+      if(sco[i]>=max){
+        if(sco[i]>max){
+          winner.length = 0;
+        }
+        max = sco[i];
+        winner.push(cars[i]);
+      }
+    }
+
+    Console.print("최종 우승자 : "+winner.join(', '));
+  }
 }
 
 export default App;
