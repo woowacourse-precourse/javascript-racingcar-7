d027fba79df877f251c0b3d760992e64f9baf8cc
yusuShin <tlsdbtn0507@naver.com>
2024-10-26 21:33:48 +0900

    feat(gameFunc) Add makeCarClassArr - Make an array that contains car instances using class Car - Use the array returned by makeCarClassArr in startGame

--- src/game/gameFunc.js
+++ src/game/gameFunc.js
@@ -3,18 +3,9 @@ import Car from "./Car.js";
 
 export default function startGame(carNames, raceRap) {
   PrintFunc.printParams("실행결과");
-
   const carClassArr = makeCarClassArr(carNames);
 
-  for (let i = 0; i < raceRap; i++) {
-    carClassArr.forEach((carObj) => {
-      carObj.moveCar();
-      PrintFunc.printParams(`${carObj.carName} : ${carObj.carMoveLength}`);
-    });
-    PrintFunc.printParams("");
-  }
 
-  checkLastWinner()
 }
 
 const makeCarClassArr = (names) => names.map((carName) => new Car(carName));
