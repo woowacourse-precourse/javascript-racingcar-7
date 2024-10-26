2d366abca87655f57f523ad6f3552abe0a285f15
yiju <yiju1755@naver.com>
2024-10-26 22:54:48 +0900

    feat: 2.자동차 전진 조건 설정 - 랜덤 전진 조건 값 입력받아 4 이상이면 전진하는 함수 carMoveForward 구현

--- src/App.js
+++ src/App.js
@@ -13,7 +13,10 @@ class App {
     const cars = this.saveCarConstructor(name);
     MissionUtils.Console.print(name);
     const tryNum = await this.getNumberOfTry();
-    MissionUtils.Console.print(tryNum);
+    MissionUtils.Console.print("실행 결과");
+    for (let i = 0; i < tryNum; i += 1) {
+      this.carMoveForward(cars);
+    }
   }
   async getCarName() {
     const name = await MissionUtils.Console.readLineAsync();
@@ -39,7 +42,18 @@ class App {
       return tryNum;
     }
   }
-  async run() {}
+  carMoveForward(cars) {
+    cars.forEach((car) => {
+      const randomNum = MissionUtils.Random.pickNumberInRange(0, 9);
+      if (randomNum >= 4) {
+        // 전진
+        MissionUtils.Console.print(`${car.carName}전진`);
+      } else {
+        // 전진 X
+        MissionUtils.Console.print(`${car.carName}전진X`);
+      }
+    });
+  }
 }
 
 export default App;
