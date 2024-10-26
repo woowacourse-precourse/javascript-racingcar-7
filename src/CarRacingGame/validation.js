c5bdf3e4277026f08e65244c6c28861318708092
jjang-aaa <jadoit473@gmail.com>
2024-10-27 03:28:45 +0900

    feat(validation): validateCarName함수 생성
    자동차 이름이 중복되었는지, 5자 이하인지,마지막이 쉼표로 입력되었는지 검증

--- src/CarRacingGame/validation.js
+++ src/CarRacingGame/validation.js
@@ -1,3 +1,21 @@
+export function validateCarName(carNameList) {
+  for (const carName of carNameList) {
+    const distinctArr = new Set(carNameList);
+
+    if (carNameList.length !== distinctArr.size) {
+      throw new Error('[ERROR] 이름이 중복되었습니다.');
+    }
+
+    if (carName.length > 6) {
+      throw new Error('[ERROR] 이름은 5자 이하로 작성해 주세요.');
+    }
+
+    if (carName.length === 0) {
+      throw new Error('[ERROR] 이름은 1자 이상으로 작성해 주세요.');
+    }
+  }
+}
+
 export function validateTryCount(tryCount) {
   if (tryCount % 1 !== 0) {
     throw new Error('[ERROR] 정수만 입력해 주세요.');
