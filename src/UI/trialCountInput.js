188b99b857985020c41d4b4c132febdf84d13e7e
thumbthing <ilscc0902@gmail.com>
2024-10-27 01:23:40 +0900

    Refactor: 주행 횟수 입력의 유효성 기능 분리

--- src/UI/trialCountInput.js
+++ src/UI/trialCountInput.js
@@ -1,37 +1,12 @@
 import { Console } from "@woowacourse/mission-utils";
+import checkTrialCount from "../feature/validate/trialCountValidate";
 
 async function trialCountInput() {
-/**
- * TODO: 
- * 1. 사용자의 입력을 비동기로 입력 받는다
- * 2. 입력받은 문자열을 Number로 변환한다
- * 3. 변환한 입력값의 유효성을 검사한다.
- *  - NaN, 1 미만, decimal, 
- *  - 유효성을 통과하지 못했을 경우 에러를 던진다
- * 4. 유효성을 통과한 정수 값을 반환한다.
- */
-
   try {
     const USER_INPUT = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
     const TRIAL_COUNT = Number(USER_INPUT);
 
-    if(Number.isNaN(TRIAL_COUNT)) {
-      const INPUT_IS_NaN = '입력하신 값이 숫자가 아닙니다.'
-      const ERROR = new Error(INPUT_IS_NaN);
-      throw ERROR;
-    };
-
-    if(TRIAL_COUNT < 1) {
-      const INPUT_IS_TOO_SMALL = '입력하신 값이 1보다 작아서 레이스를 진행 할 수 없습니다.'
-      const ERROR = new Error(INPUT_IS_TOO_SMALL);
-      throw ERROR;
-    };
-
-    if(TRIAL_COUNT !== Math.floor(TRIAL_COUNT) || TRIAL_COUNT !== Math.ceil(TRIAL_COUNT)) {
-      const INPUT_IS_DECIMAL = '입력하신 값이 정수가 아닙니다.'
-      const ERROR = new Error(INPUT_IS_DECIMAL);
-      throw ERROR;
-    };
+    checkTrialCount(TRIAL_COUNT);
 
     return TRIAL_COUNT;
   } catch (error) {
