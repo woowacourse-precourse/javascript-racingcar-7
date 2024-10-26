845bd6120897bf7d9be4c384c3fe45bb7e3bd95f
백지윤 <jy.baek079@gmail.com>
2024-10-27 04:48:48 +0900

    feat: 자동차 이름 유효성 검사 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class App {
-  async run() {
+  run() {
     this.getCarNames();
   }
 
@@ -10,6 +10,15 @@ class App {
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
     );
     const names = input.split(',');
+    if (this.validateCarNames(names)) {
+      return names;
+    } else {
+      throw new Error('[ERROR] 자동차 이름은 1자 이상 5자 이하만 가능합니다.');
+    }
+  }
+
+  validateCarNames(names) {
+    return names.every((name) => name.length > 0 && name.length <= 5);
   }
 }
 
