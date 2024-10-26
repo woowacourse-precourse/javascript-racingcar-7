0ef900a35d3af292decfffc8a7875e0f50384c0e
seocylucky <seocylucky@github.com>
2024-10-26 17:42:34 +0900

    feat: 자동차 이름과 실행할 횟수 입력 받는 함수 작성
    + 다섯 글자 초과 입력 시 에러 처리 추후 작업 예정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const cars = await inputCars();
+    const count = await inputCount();
+  }
 }
+
+const inputCars = async () => {
+  const inputCars = await Console.readLineAsync(
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+  );
+  const cars = inputCars.split(",").map((car) => car.trim());
+
+  return cars;
+};
+
+const inputCount = async () => {
+  const count = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+
+  return count;
+};
 
 export default App;
