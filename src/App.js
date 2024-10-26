d254dd8e9e19780a93d2d88d5349a350c04983dd
bbjbc <jobc4550@gmail.com>
2024-10-26 23:40:37 +0900

    feat: App 클래스 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
+import InputView from "./InputView.js";
+import RacingGame from "./RacingGame.js";
+import OutputView from "./OutputView.js";
+
 class App {
-  async run() {}
+  constructor() {
+    this.inputView = new InputView();
+    this.outputView = new OutputView();
+    this.game = new RacingGame();
+  }
+
+  async run() {
+    const game = this.game;
+
+    try {
+      await this.initializeGame(game);
+      await this.playGame(game);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  async initializeGame(game) {
+    const namesInput = await this.inputView.inputCarNames();
+    this.game.createCars(namesInput);
+
+    const countInput = await this.inputView.inputTryCount();
+    this.tryCount = game.validateTryCount(countInput);
+  }
+
+  async playGame(game) {
+    this.outputView.printGameStartMessage();
+
+    for (let i = 0; i < this.tryCount; i++) {
+      game.moveAllCars();
+      game.printAllStatus();
+    }
+
+    game.printWinners();
+  }
 }
 
 export default App;
