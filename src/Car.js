05ca921a661085d2cd1fa4b34bb9bde79cda3e07
zzz664 <zzz664>
2024-10-26 21:38:42 +0900

    chore(Car): Car클래스의 name setter 삭제
    자동차 생성 후 자동차에 배정된 이름을 수정할 일이 없으므로 setter를 삭제함

--- src/Car.js
+++ src/Car.js
@@ -1,24 +1,20 @@
 class Car {
-    constructor(name) {
-        this._name = name;
-        this._mileage = 0;
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    set name(name) {
-        this._name = name;
-    }
-
-    get _mileage() {
-        return this._mileage;
-    }
-
-    move() {
-        ++this._mileage;
-    }
+  constructor(name) {
+    this._name = name;
+    this._mileage = 0;
   }
-  
-  export default Car;
+
+  get name() {
+    return this._name;
+  }
+
+  get _mileage() {
+    return this._mileage;
+  }
+
+  move() {
+    ++this._mileage;
+  }
+}
+
+export default Car;
