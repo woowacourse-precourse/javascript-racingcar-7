f6e9ba75825905308049a26e6bab075360f66dad
백지윤 <jy.baek079@gmail.com>
2024-10-27 04:49:24 +0900

    feat: 시도 횟수 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -17,6 +17,11 @@ class App {
     }
   }
 
+  async getTryCount() {
+    const input = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    const count = Number(input);
+  }
+
   validateCarNames(names) {
     return names.every((name) => name.length > 0 && name.length <= 5);
   }
