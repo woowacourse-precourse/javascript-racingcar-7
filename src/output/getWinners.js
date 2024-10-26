d3968a2c59b9ac691d725d0bb5c3c96d0278c5dc
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-10-26 20:41:44 +0900

    feat: 횟수를 기준으로 우승자 결정
    횟수를 기준으로 우승자 결정 기능 추가
    
    - 전진 횟수가 최대값인 자동차를 필터링 한 뒤 배열로 반환

--- src/output/getWinners.js
+++ src/output/getWinners.js
@@ -1,3 +1,7 @@
 function getMaxMoveCount(moveCountArray) {
   return Math.max(...moveCountArray);
 }
+
+function findWinners(carNames, moveCountArray, maxMoveCount) {
+  return carNames.filter((_, index) => moveCountArray[index] === maxMoveCount);
+}
