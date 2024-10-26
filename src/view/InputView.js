b30e81c925b8383aed9e2eefcb202de9ef451b28
yeeun426 <thsudkcla7@naver.com>
2024-10-26 13:20:30 +0900

    feat : 경주할 자동차 배열 리턴

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -5,7 +5,7 @@ class InputView {
     const carArr = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
-    return carArr;
+    return carArr.split(",");
   }
 }
 
