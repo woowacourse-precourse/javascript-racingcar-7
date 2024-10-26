bd136215a63bd35b02a7b0aabe1285af3889ef5f
hwangheetae <tejaia@naver.com>
2024-10-27 01:15:13 +0900

    feat: 경주할 자동차 이름을 쉼표로 구분하여 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console, Random } from '@woowacourse/mission-utils';
 class App {
-  async run() {}
+  async getCarNames(query) {
+    try {
+      const input = await Console.readLineAsync(query);
+      const carNames = input.split(',').map((name) => name.trim());
+      return carNames;
+    } catch (error) {
+      throw new Error(`[ERROR] ${error}`);
+    }
+  }
+
+  async run() {
+    const carNames = await this.getCarNames(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+    );
+    Console.print(carNames);
+  }
 }
 
 export default App;
