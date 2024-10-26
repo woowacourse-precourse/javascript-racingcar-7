bc85f2d3ec3364638ce2ac4b784c0e5c15c941e3
thumbthing <ilscc0902@gmail.com>
2024-10-27 02:24:29 +0900

    Style: 구현 기획 주석 삭제

--- src/feature/raceProgression.js
+++ src/feature/raceProgression.js
@@ -2,17 +2,6 @@ import { Console } from "@woowacourse/mission-utils";
 import goStopResult from "./goStopResult.js";
 
 function raceProgression(carList, initialRaceHistory, trialCount) {
-  /** TODO: 
-   * 1. 매개 변수로 자동차 배열, 레이스 초기 데이터, 시행 횟수를 받는다
-   * 2. 레이스 결과를 저장할 레이스 데이터를 선언
-   * 3. 
-   * 4. 반복문을 활용하여 시행 횟수와 레이서의 수만큼 레이스 결과를 실행한다
-   *  - goStopResult의 결과를 변수에 저장
-   *  - 해당 결과를 레이스 데이터에 set
-   *  - 한 사이클을 돌은 결과를 출력
-   * 5. 레이스 결과를 반환
-   */
-
   const RACE_PROGRESS = initialRaceHistory;
   
   for(let count = 0; count < trialCount; count++) {
@@ -30,6 +19,7 @@ function raceProgression(carList, initialRaceHistory, trialCount) {
       singleRace = `${singleRace}
       ${car} : ${PROGRESS_RESULT}`;
     });
+    
     Console.print(singleRace);
   };
 
