28b3a894c96fdc3120728752cd365e3f906f65b8
jeongeunLee <leeju1009@gmail.com>
2024-10-26 22:43:23 +0900

    test(displayTest): 실행 결과 헤더의 출력 여부 테스트 추가

---
+++ __tests__/DisplayTest.js
@@ -0,0 +1,27 @@
+import { Console } from '@woowacourse/mission-utils';
+import Display from '../src/Display.js';
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe('Display 메서드 테스트', () => {
+  test.each([
+    [1, true],
+    [2, false],
+    [5, false],
+  ])('실행 결과 헤더 출력이 동작하는지 테스트', (round, isCalled) => {
+    const HEADER = '실행 결과';
+    const headerSpy = getLogSpy();
+
+    Display.showRoundResultHeader(round);
+
+    if (isCalled) {
+      expect(headerSpy).toHaveBeenCalledWith(HEADER);
+    } else {
+      expect(headerSpy).not.toHaveBeenCalled();
+    }
+  });
+});
