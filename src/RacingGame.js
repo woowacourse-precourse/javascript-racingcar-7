b44ad576021a67b47c272511842e00d361a5103e
sanghyeonLee <hyeon012366@gmail.com>
2024-10-27 03:35:24 +0900

    feat: 자동차 전처리 메소드 분리 및 객체 우승 횟수 속성 삽입

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -7,17 +7,22 @@ class RacingGame {
         for (const message of PROMPT_MESSAGE) {
             const input = await this.input(message.input)
             if(message.key === 0){
-                const carArray = this.sliceString(input)
+                const carArray = this.setCars(input)
                 arr.push(carArray)
             }
             if(message.key === 1){
                 arr.push(Number(input))
             }
         }
+        console.log(arr)
         this.racing(arr)
     }
 
-    racing(){
+    racing(arr){
+        const cars = arr[0]
+        for (const car of cars) {
+
+        }
         const randomNum = this.setRandomNum()
 
     }
@@ -26,18 +31,23 @@ class RacingGame {
         return Console.Random.pickNumberInRange(0,9)
     }
 
-
-
-
-     async input(message) {
-        return await Console.readLineAsync(message)
+    setCars(arr){
+        const carArr = this.sliceString(arr)
+        return this.setWinCnt(arr, carArr)
     }
-
     sliceString(param){
         return param.split(",")
     }
 
+    setWinCnt(arr,carArr){
+        return carArr.map((elem) =>
+            {return {cnt: 0,carName:elem}}
+        )
+    }
 
+     async input(message) {
+        return await Console.readLineAsync(message)
+    }
 
 }
 
