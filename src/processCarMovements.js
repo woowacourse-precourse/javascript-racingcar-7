4f3319197ebb10f47eb053f752771fec920d57b0
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-10-26 11:26:21 +0900

    feat: 전체 경주 과정 처리 및 출력

---
+++ src/processCarMovements.js
@@ -0,0 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
+import moveCarEachTurn from "./utils/moveCarEachTurn.js";
+import showRaceState from "./utils/showRaceState.js";
+
+const processCarMovements = async (cars, raceNum) => {
+  Console.print("\n실행 결과\n");
+
+  let carMovements = Array.from({ length: cars.length }, () => 0);
+
+  for (let i = 0; i < raceNum; i++) {
+    await moveCarEachTurn(cars, carMovements);
+    showRaceState(cars, carMovements);
+  }
+};
+
+export default processCarMovements;
