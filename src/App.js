d3a648ce0a5906216830f8f854744de7e5e8de06
kimdayeon37 <kdy37912@naver.com>
2024-10-26 15:57:13 +0900

    feat: 자동차 이름, 시도횟수  입력값 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-class App {
-  async run() {}
-}
+import { Console } from "@woowacourse/mission-utils";
 
+class App {
+  async run() {
+    this.getUserInput();
+  }
+
+  async getUserInput() {
+    try {
+      const inputName = await Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n"
+      );
+      const names = inputName.split(",").map((name)=>name.trim())
+      const inputTry = await Console.readLineAsync(
+        "시도할 횟수는 몇 회인가요? \n"
+      );
+      Console.print(`실행 결과 \n`);
+    } catch (error) {
+      Console.print(error.message);
+      throw error;
+    }
+  }
+}
 export default App;
