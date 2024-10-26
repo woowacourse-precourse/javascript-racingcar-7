9f1434abaa47b88562e1d23914046d1838e0ad00
zzz664 <zzz664>
2024-10-26 23:17:42 +0900

    feat(CarRacing): 시도 횟수를 입력받는 기능 구현
    시도횟수를 입력받는 기능을 추가했다. 또한 private 변수에 접근할 때,
    this키워드가 빠져있어 해당 부분을 추가했다.

--- src/CarRacing.js
+++ src/CarRacing.js
@@ -4,10 +4,11 @@ import { Console } from '@woowacourse/mission-utils';
 class CarRacing {
   #carList;
   #carNames;
+  #attemptNumber;
 
   async #getCarNames() {
     const userInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-    carNames = [...this.#validationNames(userInput)];
+    this.carNames = [...this.#validationNames(userInput)];
   }
 
   #validationNames(str) {
@@ -20,15 +21,29 @@ class CarRacing {
   }
 
   #generateCars() {
-    carList = carNames.map((element) => {
+    this.carList = carNames.map((element) => {
       car = new Car(element);
       return car;
     })
+  }
+  
+  async #getAttemptNumber() {
+    const userInput = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    this.#attemptNumber = this.#validationAttemptNumber(userInput);
+  }
+
+  #validationAttemptNumber(number) {
+    const isSatisfyCondition = !Number.isNaN(number) && number % 1 === 0 && number > 0;
+    if(!isSatisfyCondition) {
+      throw new Error('[Error]');
+    }
+    return number;
   }
 
   initCarRacing() {
     this.#getCarNames();
     this.#generateCars();
+    this.#getAttemptNumber();
   }
 }
 
