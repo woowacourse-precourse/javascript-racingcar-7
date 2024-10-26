183c0a08e80fab5f143e7b8e9009cdec0ad5abb5
Yena Kim <120614713+ynkim-03@users.noreply.github.com>
2024-10-27 04:18:26 +0900

    feat: 입력 검증을 위한 InputValidator 클래스 추가

---
+++ src/InputValidator.js
@@ -0,0 +1,25 @@
+export default class InputValidator {
+  constructor(input) {
+    this.input = input;
+  }
+
+  validateInput() {
+    this.validateSpace();
+    this.inputList = this.input.split(',');
+    this.validateLength();
+  }
+
+  validateSpace() {
+    if (this.input.split(' ')[0] !== this.input) {
+      throw new Error('[ERROR] 잘못된 입력입니다.');
+    }
+  }
+
+  validateLength() {
+    this.inputList.forEach((item) => {
+      if (item.length > 5) {
+        throw new Error('[ERROR] 잘못된 입력입니다.');
+      }
+    });
+  }
+}
