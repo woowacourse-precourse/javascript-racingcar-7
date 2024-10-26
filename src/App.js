2513891fcb3831abfee53f7ea1a31e0732d3e0cf
Ko Daeun <drechsu@gmail.com>
2024-10-26 17:32:09 +0900

    feat(App):입력받은 자동차 이름을 파싱하여 객체 배열을 생성하는 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { Console } from "@woowacourse/mission-utils";
 class App {
+  parseCarNames(carNames) {
+    return carNames.split(",").map((carName) => {
+      return { name: carName.trim(), result: "" };
+    });
+  }
+
   async getCarNameAndAttempts() {
     try {
       const carName = await Console.readLineAsync(
         "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
       );
+
+      const cars = this.parseCarNames(carName);
+
       const attempts = await Console.readLineAsync(
         "시도할 횟수는 몇 회인가요?\n"
       );
