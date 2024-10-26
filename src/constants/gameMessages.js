a294238a026dc3831f8de2a016ebb1fe3b3f5b06
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-10-27 03:25:58 +0900

    fix(gameMessages.js): 개행 추가

--- src/constants/gameMessages.js
+++ src/constants/gameMessages.js
@@ -1,7 +1,7 @@
 export const GAME_MESSAGES = Object.freeze({
-  START: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
-  ANSWER_COUNT: "시도할 횟수는 몇 회인가요?",
-  RESULT: "실행 결과",
+  START: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+  ANSWER_COUNT: "시도할 횟수는 몇 회인가요?\n",
+  RESULT: "실행 결과\n",
   WINNER: "최종 우승자 : ",
   PROGRESS_BAR: "-",
 });
