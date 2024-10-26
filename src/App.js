f547a422cbed733f9017b469448490b7211901d6
Jiny <moc24@naver.com>
2024-10-26 17:49:35 +0900

    feat(App.js) : 자동차 이름 입력 예외 추가

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ class App {
     // * 자동차의 거리만큼 '-' 출력 //pobi : --
     const printCurrentCarDistanceResult = (car) => {
       Console.print(`${car.name} : ${"-".repeat(car.distance)}`); 
-      
     }
 
     // ! 결과 함수 ===//
@@ -65,16 +64,73 @@ class App {
       
     }
 
+    // ! 예외 처리
+    const throughErrorMessage = (message) =>{
+      throw new Error(`[ERROR] ${message}`);
+    }
+
+    // * == 자동차 입력 이름 처리 == // 
+
+    // * 이름이 5개 이상인가?
+    const isCarNamesCharMoreThanFive = (carArray) =>{ 
+      let result = carArray.some((car)=> (car.name).length >5 ); // 하나라도 5개 이상이면 true 반환.
+      return result;
+    }
+
+    // * 이름에 쉼표(,)외의 다른 기호가 들어오는가?
+    const isCarsInputSymbolOtherThanComma = (carsInput) => { // str = cars
+      const pattern = /[^,가-힣\w\s]/g; // ,제외한 특수기호 모두 찾기.
+      const result = pattern.test(carsInput); // 나머지 특수기호가 있는 경우 true , 없으면 false
+      
+      return result;
+    }
+
+    //* 아무것도 입력을 받지 않은 경우, Falsy인 경우
+    const isInputFalsy = (carsInput) => {
+      if (!carsInput) { //Falsy한 값인 경우, 
+        return true;
+      }
+
+      if (typeof carsInput === 'string' && carsInput.trim() === ''){ // 공백만 들어온 경우
+        return true;
+      }
+
+      return false;
+    }
+
+    // * 자동차 Input Error 체크
+    const checkCarInputError = (carsInput,carArray) => {
+
+      if (isInputFalsy(carsInput)){
+        throughErrorMessage("경주할 자동차 이름을 입력하세요");
+      }
+
+      if (isCarsInputSymbolOtherThanComma(carsInput)){
+        throughErrorMessage("이름은 쉼표(,)로 구분 가능합니다.");
+      }
+
+      if (isCarNamesCharMoreThanFive(carArray)){
+        throughErrorMessage("자동차 이름은 5글자 이하로 입력해주세요.");
+      }
+
+    }
+    
+    
+
     try{
       // ! 입력  ============///
-      const cars = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'); // pobi,woni,jun
+      
+      const carsInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n') || ""; // pobi,woni,jun
       // *자동차는 (,) 쉼표를 기준으로 배열로 만들어준다.
-      const carInputNameArray = cars.split(',');// [pobi,woni,jun]
+      const carInputNameArray = carsInput.split(',');// [pobi,woni,jun]
+
 
       // * 자동차 객체 배열을 만든다. 
       const carArray = carInputNameArray.map( 
         (nameElement, index) => (new Car({name:nameElement}))
       );
+
+      checkCarInputError(carsInput, carArray);
 
       const n = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n'); // 경주할 자동차의 이름을 입력받는다.
       
@@ -93,7 +149,7 @@ class App {
             increaseCarDistance(car);
           }
         });
-        
+
         // * 경주가 끝나면 자동차를 순회하며 각 자동차 거리 출력    
         carArray.forEach((car) => {
           printCurrentCarDistanceResult(car);
