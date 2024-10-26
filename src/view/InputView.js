1ca88074ee276acb5d4f9b88d212cdc2a9080a24
JuYeong17 <dhwndud980@gmail.com>
2024-10-26 17:23:18 +0900

    fix: 입력 결과 값 수정

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -3,11 +3,11 @@ import { Console } from "@woowacourse/mission-utils";
 class InputView{
   async getInput(){
     const carName= await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n");
-    const tryNumber = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-
     const car=carName.split(',');
-
     await this.getinputexception(car);
+
+    const tryNumber = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    Console.print('\n');
 
     return {car, tryNumber};
   }
