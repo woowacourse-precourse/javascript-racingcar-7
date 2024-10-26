3568708e7af2c7140d2e282bbdc4ee83af07a39c
Parksangwoo <psu8430@gmail.com>
2024-10-27 04:00:10 +0900

    feat(userInput) : Add User Input Fields
    - Add the ability to enter the name of the car
    - Add input function for number of car move

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    // 자동차 이름 받기
+    const userInput = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+    );
+
+    // 시도 횟수 받기
+    const tryCount =
+      await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+  }
 }
 
 export default App;
