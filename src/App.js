a0ffb03306b6cfa9fc7b8e98306ee0adf9d8dd53
jiyun1006 <jiyun4251@gmail.com>
2024-10-26 23:51:20 +0900

    ✨ feat : 메인 실행 함수 App.js 단순 기능 구현
    앱이 돌아가게끔 단순한 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import Racingcar from './components/Racingcar.js';
+import {
+  inputCarName,
+  inputTryNum,
+  printGameProgress,
+  printNewLine,
+  printProgressResult,
+} from './utils/index.js';
+import { MAGICNUMBER } from './constants/index.js';
+import { printFirstPlayer } from './utils/inputoutputMethod.js';
+
 class App {
-  async run() {}
+  async run() {
+    const inputs = await inputCarName();
+    const inputArr = inputs.split(MAGICNUMBER.SEPARATOR);
+    const tryNum = Number(await inputTryNum());
+    const racingcar = new Racingcar(inputArr, tryNum);
+    printProgressResult();
+    let board;
+    for (let idx = 0; idx < tryNum; idx += 1) {
+      board = racingcar.play();
+      printGameProgress(board);
+      printNewLine();
+    }
+    const firstPlayer = racingcar.rank();
+    printFirstPlayer(firstPlayer);
+  }
 }
 
 export default App;
