d61b2bb3fd7b96ba3d9f49385908c08f78e8dc72
yewonhihi <yewon1118@naver.com>
2024-10-27 01:47:48 +0900

    feat(App.js): 이동횟수 입력받기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Random, Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const inputCars = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n");
+    const inputCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
   }
 
   getCarList(str) {
@@ -11,7 +12,7 @@ class App {
     if (!cars.every(car => car.length <= 5)) {
       this.throwError('자동차 이름은 5자 이하만 가능합니다.')
     }
-    
+
     return cars
   }
 
