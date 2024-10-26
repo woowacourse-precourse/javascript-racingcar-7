1a04ed4b9514f6a83391723b54ff41cbdaf01e6b
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-10-27 04:52:16 +0900

    feat : winner 구현

--- src/model/racingModel.js
+++ src/model/racingModel.js
@@ -9,5 +9,12 @@ class RacingModel {
   rece() {
     this.cars.forEach((element) => element.move());
   }
+
+  getWinners() {
+    const maxPosition = Math.max(...this.cars.map((car) => car.getPosition()));
+    return this.cars
+      .filter((car) => car.getPosition() === maxPosition)
+      .map((car) => car.getName());
+  }
 }
 export default RacingModel;
