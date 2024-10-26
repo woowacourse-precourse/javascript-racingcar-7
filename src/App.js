e84ba8dd89a81d13627b6aedaa8411e645a8f7fd
Rigu1 <dlsrbtla@gmail.com>
2024-10-27 02:29:35 +0900

    fix(App): Correct import path, parameter, and position in App.js
    - 1번 줄: 잘못된 import 경로 수정 - 모듈을 제대로 찾지 못하는 오류를
      방지하기 위해 경로를 올바르게 설정
    - 6번 줄: createCarProgressRecords 메서드 잘못된 인자값 수정 - carsStr 값을
      전달하여 올바른 데이터로 자동차 정보를 생성하기 위함
    - 9번 줄: 자동차 위치를 단순히 숫자로 표현하도록 수정 - 배열 대신
      단일 숫자로 표현하여 메모리 사용을 줄이고 코드 가독성을 높임
    - 15번, 17번 줄: 입출력 요구사항에 충족되도록 수정 - 요구사항에 맞지 않는
      포맷을 올바르게 변경하여 충족

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import RacingCar from "./models/CarRaceGame";
+import RacingCar from "./models/CarRaceGame.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 const DELIMITER = ",";
 class App {
-  createCarProgressRecords() {
+  createCarProgressRecords(carsStr) {
     return carsStr.split(DELIMITER).map((name) => ({
       name,
-      position: Array(tryCount).fill(0),
+      position: 0,
     }));
   }
 
   async run() {
     const carsStr = await MissionUtils.Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분"
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
     const tryCount = await MissionUtils.Console.readLineAsync(
-      "시도할 횟수는 몇 회인가요?"
+      "시도할 횟수는 몇 회인가요?\n"
     );
 
     const carProgressRecords = this.createCarProgressRecords(carsStr);
