af32518733a0722144dcd4eb6b9867aac09b2d45
ahhyun <wndkgus0225@naver.com>
2024-10-26 14:55:45 +0900

    refactor(message) : 일반메시지와 에러메시지를 나누어 정리

--- src/message.js
+++ src/message.js
@@ -1,3 +1,9 @@
+export const MESSAGE = {
+  ASK_CARNAMES: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+  ASK_COUNT: "시도할 횟수는 몇 회인가요?",
+  EXECUTION_RESULT: "실행 결과"
+}
+
 export const ERROR_MESSAGE = {
   INVALID_DELIMITER: '[ERROR] 쉼표 이외의 구분자는 사용할 수 없습니다.',
   NAME_LENGTH_EXCEED: '[ERROR] 이름은 5자 이하만 가능합니다.',
