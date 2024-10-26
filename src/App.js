92d023fce73936d681388bae9bb584bb263eedc6
jicho <jIcho6412@gmail.com>
2024-10-27 02:07:20 +0900

    코드 정리

--- src/App.js
+++ src/App.js
@@ -40,6 +40,7 @@ class App {
         throw new Error("[ERROR] 숫자를 입력해주새요.");
       }
     };
+
     checkCount();
 
     // 2. 입력받은 값을 통해 게임 구현
@@ -71,6 +72,7 @@ class App {
       }
       return dash;
     };
+
     racing();
 
     const checkMaxLength = () => {
