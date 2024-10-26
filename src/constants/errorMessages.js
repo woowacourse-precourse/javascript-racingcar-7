0f4b93800968ab5b4b14b2529ec7b8ab5524e20b
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-10-27 01:14:19 +0900

    feat: 에러메시지 상수화

---
+++ src/constants/errorMessages.js
@@ -0,0 +1,13 @@
+export const ERROR_MESSAGES = Object.freeze({
+  CAR_ERROR_MESSAGES: {
+    TOO_LONG: "[ERROR] 자동차 이름이 너무 깁니다. 5자 이하로 입력해주세요.",
+    BLANK: "[ERROR] 입력하지 않았습니다.",
+    DUPLICATE: "[ERROR] 자동차 이름이 중복됩니다. 중복되지 않게 입력해주세요.",
+    NO_NAME:
+      "[ERROR] 중간에 이름이 없는 자동차가 있습니다. 이름을 모두 입력해주세요.",
+  },
+  COUNT_ERROR_MESSAGES: {
+    ONLY_NUMBER: "[ERROR] 숫자만 입력 가능합니다.",
+    NOT_NEGATIVE: "[ERROR] 1이상의 수만 입력 가능합니다.",
+  },
+});
