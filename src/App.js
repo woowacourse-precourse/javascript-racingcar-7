3dad06c03c7fe22ea3f32d8163cbe2eb641a9e2b
gyur1kim <qri1114@gmail.com>
2024-10-26 21:24:56 +0900

    feat(input): 자동차 이름의 길이 제한
    - trim을 이용해 이름 양 옆 공백 제거
    - 0글자일 시 에러
    - 5글자를 초과해도 에러 발생

--- src/App.js
+++ src/App.js
@@ -6,18 +6,32 @@ class App {
     this.output = ''
   }
 
+  isValidCarName(name) {
+    const carName = name.trim();
+    if (carName.length <=0)
+      throw new Error('[ERROR] 자동차 이름을 제대로 입력하세욧.');
+    if (carName.length > 5)
+      throw new Error('[ERROR] 자동차 이름은 5자 이하입니다.');
+    return carName;
+  }
+
   async getCarNames () {
     try {
       const input = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-      this.carNames = input.split(',');
+      this.carNames = input.split(',').map(this.isValidCarName);
     } catch(e) {
       throw e;
     }
   }
 
   async run() {
-    await this.getCarNames();
-    Console.print(this.carNames)
+    try {
+      await this.getCarNames();
+      Console.print(this.carNames);
+    } catch(e) {
+      Console.print(e.message);
+      throw e;
+    }
   }
 }
 
