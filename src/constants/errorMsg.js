a31131934e7b42cc1ea69835fc8d1ac00e23cf5e
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-10-26 12:21:06 +0900

    fix: 경주 횟수 미입력 시의 에러 핸들링

--- src/constants/errorMsg.js
+++ src/constants/errorMsg.js
@@ -5,4 +5,5 @@ export const ERROR_MESSAGES = {
   NO_INPUT_CAR_NAME: `${ERROR_PREFIX} 자동차 이름을 입력해주세요.`,
   DUPLICATED_CAR_NAME: `${ERROR_PREFIX} 중복된 이름이 존재합니다.`,
   INVALID_RACE_NUMBER: `${ERROR_PREFIX} 양의 정수만 입력 가능합니다.`,
+  NO_INPUT_RACE_NUMBER: `${ERROR_PREFIX} 횟수를 입력해주세요.`,
 }
--- src/utils/getRaceNumber.js
+++ src/utils/getRaceNumber.js
@@ -5,6 +5,7 @@ const getRaceNumber = async () => {
   const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
   const num = Number(input.trim());
 
+  if (!num) throw new Error(ERROR.NO_INPUT_RACE_NUMBER);
   checkIsPositiveInteger(num);
 
   return num;
