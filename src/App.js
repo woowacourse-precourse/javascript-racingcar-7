7cbef8386722266c0d8069e1d619bb6dd0570133
jiminseong <iamjms4237@gachon.ac.kr>
2024-10-26 17:53:30 +0900

    feat(App.js): 경주할 자동차 이름 입력 기능 추가
    경주 게임을 위한 자동차 이름을 쉼표로 구분하여 입력받도록 구현했습니다.
    이를 통해 여러 자동차 이름을 한 줄에 입력받아 getCarArray함수로 배열로 반환합니다.
    향후 개별 요소로 활용하기 위하여 배열로 반환하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
+
+function getCarArray(CAR_STRING) {
+  return CAR_STRING.split(",").map((car) =>
+    car.trim()
+  );
+}
+
 class App {
-  async run() {}
+  async run() {
+    const CAR_STR = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    const CAR_ARRAY = getCarArray(CAR_STR);
+  }
 }
 
 export default App;
