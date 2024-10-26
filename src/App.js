02e4b47c4b1a5f0dc3c3b857b87849a8cf32bf7b
jsy <suyeonb1db@gmail.com>
2024-10-26 23:03:29 +0900

    feat(App): 시도 횟수 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
+async function getNumber() {
+  const input = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요? \n");
+  return input;
+}
+
 async function getCarname() {
   const input = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n");
   return input;
@@ -8,6 +13,7 @@ async function getCarname() {
 class App {
   async run() {
     const carnames = await getCarname();
+    const number = await getNumber();
   }
 }
 
