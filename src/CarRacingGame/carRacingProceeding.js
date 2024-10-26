9210be7d828087e7b3f1d9ce35f45efa17ee055e
jjang-aaa <jadoit473@gmail.com>
2024-10-27 06:04:15 +0900

    feat(carRacingProceeding): 자동차 이름과 실행결과들을 담아두는 class Car 생성

--- src/CarRacingGame/carRacingProceeding.js
+++ src/CarRacingGame/carRacingProceeding.js
@@ -5,6 +5,19 @@ export async function carRacingProceed() {
   const carNameList = await inputCarNames();
   validateCarName(carNameList);
 
+  class Car {
+    constructor(name, number) {
+      (this.name = name), (this.number = number);
+    }
+  }
+
+  const carList = [];
+
+  for (const carName of carNameList) {
+    const car = new Car(carName, 0);
+    carList.push(car);
+  }
+
   const tryCount = await inputTryCount();
   validateTryCount(tryCount);
 }
