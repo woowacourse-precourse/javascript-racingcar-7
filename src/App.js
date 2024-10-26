91f85bf0f64f5a3dba22ed1ac73a3b3cc8c89913
LEE DONG GYU <tenenger7125@gmail.com>
2024-10-26 22:29:42 +0900

    feat(method): convert carNamesInput to array

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class App {
+  constructor() {
+    this.cars = [];
+  }
+
   async run() {
     const carNamesInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    this.cars = this.createCars(carNamesInput);
+  }
+
+  createCars(carNamesInput) {
+    return carNamesInput.split(',').map(name => name);
   }
 }
 
