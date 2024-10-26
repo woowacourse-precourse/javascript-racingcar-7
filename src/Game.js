8ffc2a28eeea6d96b65660d2f31726dee6c71db5
조승우 <choseung97@gmail.com>
2024-10-26 20:45:26 +0900

    feat(game): 자동차 객체를 배열로 관리하는 기능을 추가한다
    각각의 이름을 갖는 자동차 객체를 생성하여 배열로 관리한다.

--- src/Game.js
+++ src/Game.js
@@ -2,13 +2,18 @@ class Game {
   #CARS_LIST = [];
 
   constructor(names, repetitionNumber) {
-    this.carNames = this.parseNames(names);
+    this.nameList = this.parseNames(names);
+    this.#CARS_LIST = this.allocateCars(this.nameList);
     this.repetitionNumber = repetitionNumber;
     this.currentRepeat = 0;
   }
 
   parseNames(names) {
     return names.split(',');
+  }
+
+  allocateCars(nameList) {
+    return nameList.map((name) => new Car(name));
   }
 }
 
