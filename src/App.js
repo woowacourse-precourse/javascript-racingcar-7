5d9c07a217cbcf21d7c545b369bd6fee902f084a
Park JiSeop <141125424+HarrySeop@users.noreply.github.com>
2024-10-27 03:41:33 +0900

    ✨feat: Console 입출력 기능 유틸 파일로 분리

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { Console } from '@woowacourse/mission-utils';
 import CarNameValidator from './validators/CarNameValidator.js';
+import { getUserInput } from './utils/Console.js';
 
 class App {
   async run() {
-    const userInputCarNames = await Console.readLineAsync(
+    const userInputCarNames = await getUserInput(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
     );
 
