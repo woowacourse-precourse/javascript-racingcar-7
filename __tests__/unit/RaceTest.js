4716e0f53fe0d14c8301681d1aa0737fa87f7032
Daeuning <happyjde3@gmail.com>
2024-10-26 23:47:27 +0900

    test(RaceTest): Car 객체 생성 테스트 코드 추가

---
+++ __tests__/unit/RaceTest.js
@@ -0,0 +1,22 @@
+import Race from '../../src/Race.js';
+import Car from '../../src/Car.js';
+
+jest.mock('../../src/utils.js', () => ({
+  getCarName: jest.fn().mockResolvedValue("Audi,BMW,Ford"),
+  getAttempt: jest.fn().mockResolvedValue("5"),
+}));
+
+describe("Race 클래스 테스트", () => {
+
+  describe("setCarName() 테스트", () => {
+
+    test("자동차 이름마다 Car 객체가 생성되어야 한다", async () => {
+      const race = new Race();
+
+      await race.setCarName(); 
+
+      expect(race.cars).toHaveLength(3); 
+      expect(race.cars.map(car => car.name)).toEqual(["Audi", "BMW", "Ford"]); 
+    });
+  });
+});
