5b4aad7f9c8d634545b636dc65bd0ad7f38aec25
seongmin <seongm22n@gmail.com>
2024-10-27 00:28:59 +0900

    feat: 기능4 예외 처리
    - 시도할 횟수가 숫자가 아닌 문자열을 입력했을 경우 에러 추가

--- src/errors/commonError.js
+++ src/errors/commonError.js
@@ -3,3 +3,10 @@ export const checkEmptyInput = (input) => {
 		throw new Error(`[ERROR]`);
 	}
 };
+
+// 시도할 횟수가 숫자가 아닌 문자열(알파벳,구분자 등)입력
+export const checkContents = (input) => {
+	if (isNaN(input)) {
+		throw new Error(`[ERROR]`);
+	}
+};
