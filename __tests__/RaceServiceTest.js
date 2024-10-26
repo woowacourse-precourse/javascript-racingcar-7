b0f1e7f414194228af81a5e562b7c29ee5bc3809
Seio924 <seio9241@gmail.com>
2024-10-26 09:51:58 +0900

    test(RaceServiceTest): 조건에 따른 전진 테스트 추가

--- __tests__/RaceServiceTest.js
+++ __tests__/RaceServiceTest.js
@@ -1,3 +1,4 @@
+import Car from '../src/Model/Car';
 import RaceService from '../src/Service/RaceService';
 
 describe('RaceService', () => {
@@ -9,5 +10,25 @@ describe('RaceService', () => {
       expect(randomValue).toBeGreaterThanOrEqual(0);
       expect(randomValue).toBeLessThanOrEqual(9);
     }
+  });
+
+  test('전진 여부를 결정한다', () => {
+    const raceService = new RaceService();
+
+    jest.spyOn(raceService, 'generateRandomNumber').mockReturnValue(5);
+    expect(raceService.decideMoveForward()).toBe(true);
+
+    jest.spyOn(raceService, 'generateRandomNumber').mockReturnValue(3);
+    expect(raceService.decideMoveForward()).toBe(false);
+  });
+
+  test('무작위 값이 4 이상일 때 전진한다', async () => {
+    const car = [new Car()];
+
+    const raceService = new RaceService();
+    jest.spyOn(raceService, 'decideMoveForward').mockReturnValue(true);
+    raceService.moveCar(car);
+
+    expect(car[0].getForwardCount()).toBe(1);
   });
 });
