e27b7afdc3fee17f6249b8e93dcfa50faa57c7a1
sanghyeonLee <hyeon012366@gmail.com>
2024-10-27 05:16:43 +0900

    feat: 레이싱 진행 메서드 생성후 임시 빈 배열 생성후 우승횟수, 차 이름 객체 생성 후 결과 배열 구현

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -1,51 +1,58 @@
-import {Console} from "@woowacourse/mission-utils";
+import {Console, MissionUtils} from "@woowacourse/mission-utils";
 import {PROMPT_MESSAGE} from "./constants/message.js";
 
 class RacingGame {
-    async start(){
+    async start() {
         const arr = []
         for (const message of PROMPT_MESSAGE) {
             const input = await this.input(message.input)
-            if(message.key === 0){
+            if (message.key === 0) {
                 const carArray = this.setCars(input)
                 arr.push(carArray)
             }
-            if(message.key === 1){
+            if (message.key === 1) {
                 arr.push(Number(input))
             }
         }
-        console.log(arr)
         this.racing(arr)
     }
 
-    racing(arr){
-        const cars = arr[0]
-        for (const car of cars) {
-
+    racing(arr) {
+        const [carArr, tryCnt] = arr
+        const resultArr = []
+        for (const car of carArr) {
+            for (let i = 0; i < tryCnt; i++) {
+                const randomNum = this.setRandomNum()
+                if (randomNum >= 4) {
+                    car.cnt++
+                }
+            }
+            resultArr.push(car)
         }
-        const randomNum = this.setRandomNum()
-
+        console.log(resultArr)
     }
 
-    setRandomNum(){
-        return Console.Random.pickNumberInRange(0,9)
+    setRandomNum() {
+        return MissionUtils.Random.pickNumberInRange(0, 9);
     }
 
-    setCars(arr){
+    setCars(arr) {
         const carArr = this.sliceString(arr)
         return this.setWinCnt(arr, carArr)
     }
-    sliceString(param){
+
+    sliceString(param) {
         return param.split(",")
     }
 
-    setWinCnt(arr,carArr){
-        return carArr.map((elem) =>
-            {return {cnt: 0,carName:elem}}
+    setWinCnt(arr, carArr) {
+        return carArr.map((elem) => {
+                return {cnt: 0, carName: elem}
+            }
         )
     }
 
-     async input(message) {
+    async input(message) {
         return await Console.readLineAsync(message)
     }
 
