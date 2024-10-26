9c05b86f442a9072f41c7073b7dae313c4312c54
wo-o29 <woogur29@gmail.com>
2024-10-27 01:22:59 +0900

    refactor: 콤마(`,`) 상수로 변경

--- src/utils/winner.js
+++ src/utils/winner.js
@@ -1,3 +1,5 @@
+import { GAME_RULES } from "../constants/index.js";
+
 // 전진 카운트가 가장 높은 자동차들을 문자열(우승자 형식)로 변환하여 리턴
 export const getWinner = (advanceCounts, carsInfoEntries) => {
   const maxCount = Math.max(...advanceCounts);
@@ -5,5 +7,5 @@ export const getWinner = (advanceCounts, carsInfoEntries) => {
     .filter(([_, advanceCount]) => advanceCount === maxCount)
     .map(([carName, _]) => carName);
 
-  return winners.join(", ");
+  return winners.join(`${GAME_RULES.COMMA} `);
 };
