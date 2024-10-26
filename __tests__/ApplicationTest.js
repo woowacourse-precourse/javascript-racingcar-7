64cd3b679ef78140887b01962fedf0ef024a453b
yheseul <kangyheseul@naver.com>
2024-10-26 18:13:32 +0900

    test: test.each를 사용하여 자동차 이름 유효성 검사 반복 테스트 코드로 리팩토링

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,12 +1,13 @@
 import App from "../src/App.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
-import { validateCarNameLength } from "../src/validation/validateCarName/validateCarNameLength.js";
 import { ERROR_MESSAGES } from "../src/constants/constants.js";
-import { validateCarNameSeparator } from "../src/validation/validateCarName/validateCarNameSeparator.js";
-import { validateCarNameOnlyLetters } from "../src/validation/validateCarName/validateCarNameOnlyLetters.js";
 import { validateCarNameCount } from "../src/validation/validateCarName/validateCarNameCount.js";
+import { validateCarNameLength } from "../src/validation/validateCarName/validateCarNameLength.js";
+import { validateCarNameSeparator } from "../src/validation/validateCarName/validateCarNameSeparator.js";
 import { validateDuplicateCarNames } from "../src/validation/validateCarName/validateDuplicateCarNames.js";
 import { validateAttemptsNumber } from "../src/validation/validateAttemptsNumber/validateAttemptsNumber.js";
+import { validateCarNameOnlyLetters } from "../src/validation/validateCarName/validateCarNameOnlyLetters.js";
+import { validateCarName } from "../src/validation/validateCarName/index.js";
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -53,39 +54,19 @@ describe("자동차 경주", () => {
     });
   });
 
-  test("입력된 자동차 이름이 쉼표(,)가 아닌 다른 구분자를 포함한 경우", () => {
-    expect(() => validateCarNameSeparator("emma/sophia/mia")).toThrow(
-      ERROR_MESSAGES.INVALID_NAME_SEPARATOR
-    );
-  });
-
-  test("경주할 자동차 이름이 5자를 초과하는 경우", () => {
-    expect(() => validateCarNameLength(["emma", "sophia"])).toThrow(
-      ERROR_MESSAGES.CAR_NAME_TOO_LONG
-    );
-  });
-
-  test("자동차 이름은 한글 또는 영어만 입력할 수 있다. 그렇지 않은 경우", () => {
-    expect(() => validateCarNameOnlyLetters(["123", "^&*"])).toThrow(
-      ERROR_MESSAGES.NAME_MUST_BE_KOREAN_OR_ENGLISH
-    );
-  });
-
-  test("경주할 자동차가 2대 미만일 경우", () => {
-    expect(() => validateCarNameCount(["emma"])).toThrow(
-      ERROR_MESSAGES.MINIMUM_TWO_CARS_REQUIRED
-    );
-  });
-
-  test("경주할 자동차 이름이 중복된 경우", () => {
-    expect(() => validateDuplicateCarNames(["emma", "sophia", "emma"])).toThrow(
-      ERROR_MESSAGES.DUPLICATE_CAR_NAMES
-    );
-  });
-
   test("시도 횟수 입력 값이 숫자가 아닌 경우", () => {
     expect(() => validateAttemptsNumber("a")).toThrow(
       ERROR_MESSAGES.ATTEMPTS_MUST_BE_NUMERIC
     );
+  });
+
+  test.each([
+    ["emma/sophia/mia", ERROR_MESSAGES.INVALID_NAME_SEPARATOR],
+    ["emma,sophia", ERROR_MESSAGES.CAR_NAME_TOO_LONG],
+    ["123, ^&*", ERROR_MESSAGES.NAME_MUST_BE_KOREAN_OR_ENGLISH],
+    ["emma", ERROR_MESSAGES.MINIMUM_TWO_CARS_REQUIRED],
+    ["emma,jin,emma", ERROR_MESSAGES.DUPLICATE_CAR_NAMES],
+  ])("validateCarName(%s) throws %s", (input, expected) => {
+    expect(() => validateCarName(input)).toThrow(expected);
   });
 });
