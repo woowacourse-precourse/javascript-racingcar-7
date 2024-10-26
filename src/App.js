a209f8fa5809b7c17c8ad8550f7901bc18c9af48
kimdayeon37 <kdy37912@naver.com>
2024-10-26 15:57:49 +0900

    feat: 자동차 이름 확인 함수

--- src/App.js
+++ src/App.js
@@ -11,14 +11,22 @@ class App {
         "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n"
       );
       const names = inputName.split(",").map((name)=>name.trim())
+      this.validateCarNames(names);
       const inputTry = await Console.readLineAsync(
         "시도할 횟수는 몇 회인가요? \n"
       );
       Console.print(`실행 결과 \n`);
+      // Console.print(`결과 : ${names}`);
     } catch (error) {
       Console.print(error.message);
       throw error;
     }
+  }
+
+  validateCarNames(names) {
+    names.forEach((name) => {
+      if (name.length > 5) throw new Error("[ERROR]");
+    });
   }
 }
 export default App;
