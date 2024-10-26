c993e91d58129238a8b43f5d96d5312635885885
jiyun1006 <jiyun4251@gmail.com>
2024-10-26 19:38:09 +0900

    ✅ test : 자동차 이름에 대한 공백 테스트
    hasSpace 메서드를 이용한 공백 테스트 함수 작성
    - test.each 를 이용해 3가지의 경우 생성

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,6 +1,16 @@
 import { validation } from '../src/utils/index.js';
 
-test('자동차 이름의 길이가 5를 넘는지', () => {
-  const input = 'abc,bfdg,grjhi';
-  expect(validation.isLengthBelowFive(input)).toBe(true);
+describe('입력에 대한 테스트', () => {
+  test('자동차 이름의 길이가 5를 넘는지', () => {
+    const input = ['abc', 'bfdg', 'grjhi'];
+    expect(validation.isLengthBelowFive(input)).toBe(true);
+  });
+
+  test.each([
+    [['abc', 'b sf', 'asw'], true],
+    [[' asd', 'bbs ', 'ag'], true],
+    [['absc,', 'asfw', 'bje'], false],
+  ])('자동차 이름에 공백이 있는지', (inputs, expected) => {
+    expect(validation.hasSpace(inputs)).toBe(expected);
+  });
 });
