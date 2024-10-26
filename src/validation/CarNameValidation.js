12fc827706847543131c26f53e14ad919eeff457
jjeongsu <jeongsu.dev@gmail.com>
2024-10-26 17:31:31 +0900

    style : 구분자를 GAME객체에서 가져오도록 변경

--- src/validation/CarNameValidation.js
+++ src/validation/CarNameValidation.js
@@ -7,10 +7,12 @@ class CarNameValidation {
   static checkNameLength(name) {
     const min = GAME.minNameLength;
     const max = GAME.maxNameLength;
-    return name.split(',').some((car) => car.trim().length < min || car.trim().length > max);
+    const delimeter = GAME.inputDelimeterSign;
+    return name.split(delimeter).some((car) => car.trim().length < min || car.trim().length > max);
   }
   static checkDuplicate(name) {
-    return new Set(name.split(',')).size !== name.split(',').length;
+    const delimeter = GAME.inputDelimeterSign;
+    return new Set(name.split(delimeter)).size !== name.split(delimeter).length;
   }
 }
 
