b90483dda206b6785710a5f9dd8858e3c3efd3af
jiyun1006 <jiyun4251@gmail.com>
2024-10-26 17:49:25 +0900

    ✍️ rename : 변수명 변경(nameArr -> name)
    배열이 아닌 문자열을 받아오기 때문에 적절하지 않은 변수명인 nameArr을 name으로 변경.

--- src/utils/InputoutputMethod.js
+++ src/utils/InputoutputMethod.js
@@ -1,8 +1,8 @@
 import { Console } from '@woowacourse/mission-utils';
 
 export const inputMethod = async () => {
-  const nameArr = await Console.readLineAsync(
+  const name = await Console.readLineAsync(
     '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
   );
-  return nameArr;
+  return name;
 };
