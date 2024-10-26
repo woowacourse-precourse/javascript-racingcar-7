40d9e6ee075997b97fbe9766fec5b6f1d338d34b
seonae-j <wkdtjsdo135@naver.com>
2024-10-26 19:17:23 +0900

    feat(app): 자동차 이름 쉼표로 구분하기
    - gameCar : 해당 클래스에서 사용자가 입력한 userInputGameStart 변수 값을 가져와서 쉼표로 배열 생성

--- src/App.js
+++ src/App.js
@@ -7,6 +7,25 @@ class App {
         '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
       )
     ).trim();
+
+    // userInput <= userInputGameStart
+    class gameCar {
+      constructor(userInput) {
+        this.name = userInput.split(',');
+
+        this.name.forEach((name, index, arr) => {
+          arr[index] = name.trim();
+        });
+
+        Console.print(this.name);
+      }
+    }
+
+    try {
+      new gameCar(userInputGameStart);
+    } catch (error) {
+      throw new Error('[ERROR]' + error.message);
+    }
   }
 }
 
