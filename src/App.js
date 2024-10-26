4e82cf023fb82347377a7ad39d9ecdb6d8162e0d
Yun-Jinwoo <yjw3480@naver.com>
2024-10-27 01:04:32 +0900

    ✨ 사용자 입력에 대한 오류 처리 코드 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import { Console, Random } from "@woowacourse/mission-utils"
 class App {
   async run() {
     const inputCar = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"); // 자동차 이름 입력받음
+    if (inputCar.length === 0) {
+      throw new Error("[ERROR] 입력된 자동차가 없습니다.");
+    }
     const cars = inputCar.split(",");
+    if (cars.length === 1) {
+      throw new Error("[ERROR] 입력된 자동차가 한 개 뿐입니다.")
+    }
+    const uniqueCars = new Set(cars);
+    if (uniqueCars.size !== cars.length) {
+      throw new Error("[ERROR] 중복된 자동차 이름이 있습니다.");
+    }
     cars.forEach((car) => { // 자동차 이름 5자 이하로 제한
       if (car.length > 5) {
         throw new Error(`[ERROR] "${car}"의 길이가 5를 초과합니다.`);
@@ -11,9 +21,13 @@ class App {
     })
     const inputNum = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
     const count = parseInt(inputNum);
-    if (isNaN(count) || count < 0) {
-      throw new Error(`[ERROR] 시도할 횟수가 올바르지 않습니다. `)
+    if (count < 0) {
+      throw new Error('[ERROR] 양수를 입력해주세요.')
     }
+    if (isNaN(count)) {
+      throw new Error(`[ERROR] 숫자가 아닌 값을 입력하면 안됩니다. `)
+    }
+
 
     const distances = Array(cars.length).fill(0); // 각 자동차 거리 저장할 배열
     for (let i = 0; i < count; i++) {
