a23e3a3453fb0c429a672be81f4735dc71f1a4c8
silver93hq <gkagjsrb@gmail.com>
2024-10-26 16:54:45 +0900

    feat: 입력된 자동차에 대한 유효성 검사 기능 구현

--- src/Controller.js
+++ src/Controller.js
@@ -1,8 +1,16 @@
 import view from './view.js';
+import validator from './utils/validator.js';
+import { STRINGS } from './constants/values.js';
 
 class Controller {
   async play() {
+    await this.readCars();
+  }
+
+  async readCars() {
     const cars = await view.readCars();
+    const carNames = cars.split(STRINGS.inputNameDelimiter);
+    validator.validateCarNames(carNames);
   }
 }
 
