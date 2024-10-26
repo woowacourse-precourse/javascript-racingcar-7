1ca2bf3717b1111afca94ac522711db97de8d8b8
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-10-26 19:35:15 +0900

    refactor: getUserInputCarName 함수 제거

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ import { Console, Random } from '@woowacourse/mission-utils';
 class App {
   async run() {
     try {
-      const playerNames = await getUserInputCarName();
+      const userInput = await Console.readLineAsync(
+        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n)'
+      );
+
+      const playerNames = userInput.split(',').map((e) => e.trim());
+      validatePlayerNames(playerNames);
+      const formattedUserInput = formatUserInputForGameResult(playerNames);
+
       const moveCount = await Console.readLineAsync(
         '시도할 횟수는 몇 회인가요?'
       );
-      const playerScore = createCarRacing(playerNames, moveCount);
+
+      const playerScore = createCarRacing(formattedUserInput, moveCount);
       Console.print(`최종 우승자 : ${findGameWinner(playerScore)}`);
     } catch (error) {
       Console.print(error.message);
@@ -20,17 +28,6 @@ class App {
 }
 
 export default App;
-
-// [ 'pobi', 'woni' ]
-export async function getUserInputCarName() {
-  const userInput = await Console.readLineAsync(
-    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n)'
-  );
-  const playerNames = userInput.trim().split(',');
-
-  validatePlayerNames(playerNames);
-  return formatUserInputForGameResult(playerNames);
-}
 
 function validatePlayerNames(playerNames) {
   // [ERROR] 5자를 초과
