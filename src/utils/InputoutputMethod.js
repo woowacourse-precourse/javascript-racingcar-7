26b2aab5c6ee92e7c5c9c7477f136b8cd7d89ec5
jiyun1006 <jiyun4251@gmail.com>
2024-10-26 19:54:32 +0900

    ✨ feat : 시도 횟수 입력문 메서드 생성
    inputTryNum 시도 횟수 입력 기능

--- src/utils/InputoutputMethod.js
+++ src/utils/InputoutputMethod.js
@@ -1,8 +1,13 @@
 import { Console } from '@woowacourse/mission-utils';
 
-export const inputMethod = async () => {
-  const name = await Console.readLineAsync(
+export const inputCarName = async () => {
+  const carName = await Console.readLineAsync(
     '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
   );
-  return name;
+  return carName;
+};
+
+export const inputTryNum = async () => {
+  const tryNum = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+  return tryNum;
 };
