b752db5a6d756848300e027c0ad2ee58c116431c
Park JiSeop <141125424+HarrySeop@users.noreply.github.com>
2024-10-27 03:40:03 +0900

    ♻️refactor: 자동차 이름 검증 기능 클래스 분리

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
+import CarNameValidator from './validators/CarNameValidator.js';
 
 class App {
   async run() {
@@ -10,26 +11,7 @@ class App {
       .split(',')
       .map(carName => carName.trim());
 
-    carNameList.forEach(carName => {
-      if (carName === '') {
-        throw new Error('[ERROR] 자동차 이름은 공백일 수 없습니다.');
-      }
-      if (carName.includes(' ')) {
-        throw new Error('[ERROR] 자동차 이름에 공백이 포함되면 안됩니다.');
-      }
-      if (carName.length > 5) {
-        throw new Error('[ERROR] 자동차 이름은 5자 이하여야 합니다.');
-      }
-    });
-
-    if (carNameList.length < 2) {
-      throw new Error('[ERROR] 자동차는 2대 이상이어야 합니다.');
-    }
-
-    const checkDuplicateCarName = new Set(carNameList);
-    if (checkDuplicateCarName.size !== carNameList.length) {
-      throw new Error('[ERROR] 자동차 이름은 중복되선 안됩니다.');
-    }
+    CarNameValidator.validate(carNameList);
   }
 }
 
---
+++ src/validators/CarNameValidator.js
@@ -0,0 +1,26 @@
+class CarNameValidator {
+  static validate(carNameList) {
+    carNameList.forEach(carName => {
+      if (carName === '') {
+        throw new Error('[ERROR] 자동차 이름은 공백일 수 없습니다.');
+      }
+      if (carName.includes(' ')) {
+        throw new Error('[ERROR] 자동차 이름에 공백이 포함되면 안됩니다.');
+      }
+      if (carName.length > 5) {
+        throw new Error('[ERROR] 자동차 이름은 5자 이하여야 합니다.');
+      }
+    });
+
+    if (carNameList.length < 2) {
+      throw new Error('[ERROR] 자동차는 2대 이상이어야 합니다.');
+    }
+
+    const checkDuplicateCarName = new Set(carNameList);
+    if (checkDuplicateCarName.size !== carNameList.length) {
+      throw new Error('[ERROR] 자동차 이름은 중복되선 안됩니다.');
+    }
+  }
+}
+
+export default CarNameValidator;
