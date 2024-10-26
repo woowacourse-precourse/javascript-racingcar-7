4c22adf9ada7e1cf4b90c0c90e5399d0ff6b0f4c
jeongeunLee <leeju1009@gmail.com>
2024-10-27 02:45:21 +0900

    test(racingTest): play 메서드의 로직이 변경됨에 따라 테스트 코드 수정

--- __tests__/RacingTest.js
+++ __tests__/RacingTest.js
@@ -18,26 +18,16 @@ const getLogSpy = () => {
 };
 
 describe('Racing 메서드 테스트', () => {
-  test('play 메서드 테스트', () => {
-    const MOVING_FORWARD = 5;
-    const STOP = 2;
+  test('play 메서드에서 driving 메서드가 매 라운드 실행되는지 테스트', () => {
     const TOTAL_ROUNDS = 3;
     const CAR_NAME = 'woowa';
-    const results = ['woowa : -', 'woowa : --', 'woowa : --'];
-    const logSpy = getLogSpy();
 
-    mockRandoms([MOVING_FORWARD, MOVING_FORWARD, STOP]);
+    const drivingSpy = jest.spyOn(Car.prototype, 'driving');
 
     const car = new Car(CAR_NAME);
     const racing = new Racing(TOTAL_ROUNDS, [car]);
-
     racing.play();
 
-    results.forEach((result, index) => {
-      expect(logSpy).toHaveBeenNthCalledWith(
-        index + 1,
-        expect.stringContaining(result),
-      );
-    });
+    expect(drivingSpy).toHaveBeenCalledTimes(TOTAL_ROUNDS);
   });
 });
