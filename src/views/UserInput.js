8c079ba866f63c9947d3ab9dc7df88ab422a03d0
Murphybread <nargene@gmail.com>
2024-10-26 20:39:43 +0900

    refactor(UserInput.js): delete error obj

--- src/views/UserInput.js
+++ src/views/UserInput.js
@@ -8,9 +8,9 @@ class UserInput {
     try {
       const userInput = await Console.readLineAsync(RACING_VARIABLES.INPUT_PROMPT);
       return userInput;
-    } catch (error) {
-      Console.print('[ERROR]: 경주할 자동차를 입력을 받는 중 문제가 발생했습니다.');
-      return null;
+    } catch {
+      throw new Error('[ERROR]: 경주할 자동차를 입력을 받는 중 문제가 발생했습니다.');
+
     }
   }
 
@@ -18,9 +18,9 @@ class UserInput {
     try {
       const userInput = await Console.readLineAsync(RACING_VARIABLES.INPUT_COUNT_PROMPT);
       return Number(userInput);
-    } catch (error) {
-      Console.print('[ERROR]: 횟수를 입력을 받는 중 문제가 발생했습니다.');
-      return null;
+    } catch {
+      throw new Error('[ERROR]: 횟수를 입력을 받는 중 문제가 발생했습니다.');
+
     }
   }
 }
