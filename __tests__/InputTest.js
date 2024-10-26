d4ba6b71f39eea306a4b58e0b3d07e893b9ea157
jiyun1006 <jiyun4251@gmail.com>
2024-10-26 20:26:50 +0900

    ✅ test : 자동차 이름 중복 테스트 케이스 작성
    isNotDuplicate를 이용하여 자동차 이름 중복에 대한 테스트 함수 작성

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -10,7 +10,32 @@ describe('입력에 대한 테스트', () => {
     [['abc', 'b sf', 'asw'], true],
     [[' asd', 'bbs ', 'ag'], true],
     [['ab4c,', 'as@w', 'bj%#'], false],
+    [[' ', '@*j', ')G9d'], true],
   ])('자동차 이름에 공백이 있는지', (inputs, expected) => {
     expect(validation.hasSpace(inputs)).toBe(expected);
+  });
+
+  test.each([
+    [['abc', 'bj@', '20Gs'], false],
+    [['', 'sow', '!@GH'], true],
+  ])('자동차 이름이 작성 안됐는지', (input, expected) => {
+    expect(validation.isEmpty(input)).toBe(expected);
+  });
+
+  test.each([
+    [['pobi', 'jun', 'pobi'], false],
+    [['asb', 'j#os', 'j9j'], true],
+    [['pobi', 'Pobi', 'jun'], true],
+  ])('자동차 이름이 중복 안되는지', (input, expected) => {
+    expect(validation.isNotDuplicate(input)).toBe(expected);
+  });
+
+  test.each([
+    [0, false],
+    [1, true],
+    [1.7, false],
+    [10, true],
+  ])('시도 횟수가 1이상의 정수인지', (input, expected) => {
+    expect(validation.isPositiveInteger(input)).toBe(expected);
   });
 });
