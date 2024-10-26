8770cc2a91991e909a6d755d5de5f509b9f94153
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-10-26 15:03:26 +0900

    feat:  입력 도중에 잘못된 값이 발생하면 예외처리를 한다

--- src/InputUtils.js
+++ src/InputUtils.js
@@ -1,7 +1,11 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 export async function getCarName() {
-  return await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+  try {
+    return await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+  } catch (error) {
+    throw new Error('[ERROR]');
+  }
 }
 
 export function splitCarName(names) {
@@ -9,7 +13,11 @@ export function splitCarName(names) {
 }
 
 export async function getAttemptCount() {
-  return await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+  try {
+    return await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+  } catch (error) {
+    throw new Error('[ERROR]');
+  }
 }
 
 export function validateCarNameLength(names) {
