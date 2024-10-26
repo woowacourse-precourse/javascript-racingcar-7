4425e62a791de9d69116d6d6d76cd44a222c4bb7
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-10-26 18:12:27 +0900

    feat : 시도 횟수 검증 1. 시도 횟수에 숫자이외의 값이 들어왔다면 예외처리 2. 시도 횟수가 공백인지 검사

--- src/util/validation.js
+++ src/util/validation.js
@@ -2,5 +2,9 @@ import { TRYNUMBER_ERROR } from './constant.js';
 
 export const validTryNumber = (number) => {
   const isNotNumber = isNaN(number);
+  const isEmpty = number.length === 0;
+  const isOver50 = number > 50;
   if (isNotNumber) throw Error(TRYNUMBER_ERROR.NOT_NUMBER);
+  if (isEmpty) throw Error(TRYNUMBER_ERROR.NOT_BLANK);
+  if (isOver50) throw Error(TRYNUMBER_ERROR.NOT_OVER_50);
 };
