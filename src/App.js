84851d7e0358fb5c588de9d2b6087d5b672c47e4
glyph8 <newdy0808@gmail.com>
2024-10-26 12:28:50 +0900

    feat(userInput) 사용자 문자열 입출력 기능 추가
    필요 라이브러리 import, 사용자 입력 안내문구 출력 및 사용자의 문자열 입력받는 기능추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import {Console, Random} from "@woowacourse/mission-utils"
+
 class App {
-  async run() {}
+  async run() {
+    Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+    const carNames = await Console.readLineAsync("");
+
+    Console.print("시도할 횟수는 몇 회인가요?");
+    const moveCount = await Console.readLineAsync("");
+
+    Console.print("실행 결과");
+
+    Console.print("최종 우승자 : ");
+
+  }
 }
 
 export default App;
