0cb432a181e8300aebfaab0a0fd0cd5b470e75f0
hwangheetae <tejaia@naver.com>
2024-10-27 01:49:50 +0900

    feat: 총 시도할 횟수를 입력받기

--- src/App.js
+++ src/App.js
@@ -10,11 +10,23 @@ class App {
     }
   }
 
+  async getTryNumber(query) {
+    try {
+      const input = await Console.readLineAsync(query);
+      return input;
+    } catch (error) {
+      throw new Error(`[ERROR] ${error}`);
+    }
+  }
+
   async run() {
     const carNames = await this.getCarNames(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
     );
+    const tryNumber = await this.getTryNumber('시도할 횟수는 몇 회인가요?\n');
+
     Console.print(carNames);
+    Console.print(tryNumber);
   }
 }
 
