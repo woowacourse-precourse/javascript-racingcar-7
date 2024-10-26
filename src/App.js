bef52d686288ef54e68ca82e701badaed5ae98be
seonae-j <wkdtjsdo135@naver.com>
2024-10-26 20:24:39 +0900

    refactor(app): gameCar의 constructor indent 줄이기

--- src/App.js
+++ src/App.js
@@ -7,20 +7,18 @@ class App {
         '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
       )
     ).trim();
+
     const userInputGameOfTime = (
       await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n')
     ).trim();
 
-    // userInput <= userInputGameStart
     class gameCar {
       constructor(userInput) {
-        this.name = userInput.split(',');
+        this.name = this.splitName(userInput);
+      }
 
-        this.name.forEach((name, index, arr) => {
-          arr[index] = name.trim();
-        });
-
-        Console.print(this.name);
+      splitName(userInput) {
+        return userInput.split(',').map((name) => name.trim());
       }
     }
 
