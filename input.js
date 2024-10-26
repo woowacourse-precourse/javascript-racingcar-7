34b302032f27d9cda0ea2cdf8d6c278b9c36322d
silver0108 <kimsoeun0108@gmail.com>
2024-10-26 17:22:48 +0900

    feat: 경주할 자동차 이름 입력

---
+++ input.js
@@ -0,0 +1,8 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+export const inputCarName = async () => {
+  const carNames = await MissionUtils.Console.readLineAsync(
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+  );
+  return carNames;
+};
