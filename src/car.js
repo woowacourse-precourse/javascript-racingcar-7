e4f01c650f300fcca0ef0279d7047a92910d5409
LEE DONG GYU <tenenger7125@gmail.com>
2024-10-26 22:35:09 +0900

    feat(field): add name, moveCount field in Car class

--- src/car.js
+++ src/car.js
@@ -1,3 +1,10 @@
-class Car {}
+import { Random } from '@woowacourse/mission-utils';
+
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.moveCount = 0;
+  }
+}
 
 export default Car;
