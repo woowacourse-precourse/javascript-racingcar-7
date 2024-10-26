3fd8d16074aaf4c9e587983c86792afcc37968ff
kimywann <Kimywan10@gmail.com>
2024-10-26 21:15:46 +0900

    refactor: run 메서드에서 주요 로직을 함수 호출로 분리

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import { Validator } from "./Validator.js";
 
 class App {
   async run() {
+    const carNames = await this.getCarNames();
+    const raceRounds = await this.getRaceRounds();
+    const distances = this.initializeDistances(carNames);
+
+    this.printRaceResults(carNames, raceRounds, distances);
+    this.printWinners(carNames, distances);
+  }
+
     const carName = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     const carNames = carName.split(",");
     Validator.validateCarNames(carNames);
