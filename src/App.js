3aaa0701f5874930ea6a077ae8a12aa5eb75472a
jjeongsu <jeongsu.dev@gmail.com>
2024-10-26 16:11:52 +0900

    feat(Input) : 시도 횟수를 입력받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Input from './io/Input.js';
 class App {
   async run() {
     const carName = await Input.getCarName();
+    const tryNumber = await Input.getTryNumber();
   }
 }
 
--- src/io/Input.js
+++ src/io/Input.js
@@ -4,5 +4,9 @@ class Input {
   static async getCarName() {
     return await Console.readLineAsync(GAME_MESSAGE.readCarName);
   }
+
+  static async getTryNumber() {
+    return await Console.readLineAsync(GAME_MESSAGE.readTryCount);
+  }
 }
 export default Input;
