98237525d9bed106baa17a926c62c3e1b93ffaa3
sanghyeonLee <hyeon012366@gmail.com>
2024-10-27 03:16:27 +0900

    feat: 레이싱 메소드 생성 및 난수 반환 메서드 생성

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -3,11 +3,31 @@ import {PROMPT_MESSAGE} from "./constants/message.js";
 
 class RacingGame {
     async start(){
+        const arr = []
         for (const message of PROMPT_MESSAGE) {
-            const input = await this.input(message)
-            this.sliceString(input)
+            const input = await this.input(message.input)
+            if(message.key === 0){
+                const carArray = this.sliceString(input)
+                arr.push(carArray)
+            }
+            if(message.key === 1){
+                arr.push(Number(input))
+            }
         }
+        this.racing(arr)
     }
+
+    racing(){
+        const randomNum = this.setRandomNum()
+
+    }
+
+    setRandomNum(){
+        return Console.Random.pickNumberInRange(0,9)
+    }
+
+
+
 
      async input(message) {
         return await Console.readLineAsync(message)
