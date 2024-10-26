959a196e25133e26646ea70f479c2e8a4535f002
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-10-27 00:30:59 +0900

    feat: 자동차 이동 수 만큼 '-'로 변환

--- src/models/Model.js
+++ src/models/Model.js
@@ -1,17 +1,20 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
+// 차 이름 구분
 export function divisionCarName(input) {
   const delimiter = ',';
   const carNames = input.split(delimiter); // 입력을 ,로 분리하여 배열로 변환
   return carNames;
 }
 
+// 차 생성
 export function createCarObject(carNames) {
   const names = carNames.map((name) => name.trim()); // 자동차 이름 배열
   const positions = new Array(carNames.length).fill(0); // 전진 수 배열, 초기값은 0
   return { names, positions };
 }
 
+// 차 전진 여부 결정
 export function moveCars(carData) {
   const updatedPositions = carData.positions.map((position) => {
     const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
@@ -24,4 +27,9 @@ export function moveCars(carData) {
 
   // 새로운 배열 반환
   return { ...carData, positions: updatedPositions };
+}
+
+// 차 전진 -로 표현
+export function getCarPositionsRepresentation(carData) {
+  return carData.positions.map((position) => '-'.repeat(position));
 }
