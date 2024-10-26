83f20c182ee80c1a2281b0db8ffa29218a5c3a44
ahhyun <wndkgus0225@naver.com>
2024-10-26 15:51:46 +0900

    fear(message) : 횟수 입력 시 소수점과 정수에 관한 에러 추가

--- src/message.js
+++ src/message.js
@@ -7,5 +7,7 @@ export const MESSAGE = {
 export const ERROR_MESSAGE = {
   INVALID_DELIMITER: '[ERROR] 쉼표 이외의 구분자는 사용할 수 없습니다.',
   NAME_LENGTH_EXCEED: '[ERROR] 이름은 5자 이하만 가능합니다.',
-  NOT_NUMBER: '[ERROR] 숫자 이외의 입력은 사용할 수 없습니다.'
+  NOT_NUMBER: '[ERROR] 숫자 이외의 입력은 사용할 수 없습니다.',
+  NOT_INTEGER: '[ERROR] 정수 이외의 입력은 사용할 수 없습니다.',
+  NEGATIVE_NUMBER: '[ERROR] 음수는 입력할 수 없습니다.'
 }
