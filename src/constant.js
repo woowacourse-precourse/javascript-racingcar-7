ade5d0edc9c06b12766a5026ce0bf7f3a9ff2757
seocylucky <seocylucky@github.com>
2024-10-27 02:15:26 +0900

    feat: 예외 처리 메시지 상수 처리

---
+++ src/constant.js
@@ -0,0 +1,6 @@
+export const ERROR_MESSAGES = {
+  NO_CAR_INPUT: "아무 것도 입력하지 않으셨습니다.",
+  EXCEED_CAR_LENGTH: "자동차 이름은 5자 이하여야 합니다.",
+  NO_COUNT: "시도 횟수를 1 이상 입력해주세요.",
+  NO_WINNER: "전진한 사람이 아무도 없습니다.",
+};
