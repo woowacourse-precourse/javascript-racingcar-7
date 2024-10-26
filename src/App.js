7c2736be27d90ef55cccd5c27bbfb6c307c90fe9
yeeun426 <thsudkcla7@naver.com>
2024-10-26 13:19:01 +0900

    feat : 경주할 자동차 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import InputView from "./view/InputView.js";
+
 class App {
-  async run() {}
+  async run() {
+    const inputView = new InputView();
+    let carName = await inputView.getCarName();
+  }
 }
 
 export default App;
