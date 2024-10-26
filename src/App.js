0b20ec8611cb3d5077816e0973d87846e959306b
norangsuji <sujihong93@gmail.com>
2024-10-26 10:26:06 +0900

    feat<app.js/getCarNames> 자동차 이름 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  constructor() {
+    this.carNames = []; // 자동차 이름 목록
+  }
+
+  async run() {
+    await this.getCarNames(); // 자동차 이름 요청
+  }
+
+  async getCarNames() {
+    const input = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    this.carNames = this.validateCarNames(input.split(",")); // 자동차 이름 검증
+  }
 }
 
 export default App;
