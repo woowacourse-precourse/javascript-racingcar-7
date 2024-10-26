58dc30b70ec2b14b6c5a8eff8d2e217ddaacd379
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-10-27 00:15:54 +0900

    feat: 차 전진 수 증가 기능 구현

--- src/models/Model.js
+++ src/models/Model.js
@@ -11,3 +11,17 @@ export function createCarObject(carNames) {
   const positions = new Array(carNames.length).fill(0); // 전진 수 배열, 초기값은 0
   return { names, positions };
 }
+
+export function moveCars(carData) {
+  const updatedPositions = carData.positions.map((position) => {
+    const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+    // 랜덤 값이 4 이상이면 전진 수를 증가
+    if (randomNumber >= 4) {
+      return position + 1;
+    }
+    return position;
+  });
+
+  // 새로운 배열 반환
+  return { ...carData, positions: updatedPositions };
+}
