896d127e4f75b5bcfee5436cacc001ca84bbe83b
ahhyun <wndkgus0225@naver.com>
2024-10-26 14:58:54 +0900

    refactor(App) : message.js를 이용하여 간결하게 정리

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { Console } from '@woowacourse/mission-utils';
 import { getCarNames, getCount } from "./IO.js";
 import { randomRacing } from "./racing.js";
 import { displayResults } from "./IO.js";
+import { MESSAGE } from "./message.js";
 
 class App {
   async run() {
     try {
       const carNames = await getCarNames();
       const chance = await getCount();
-      Console.print("\n실행 결과");
+      Console.print("\n"+MESSAGE.EXECUTION_RESULT);
       const results = randomRacing(carNames, chance, displayResults);
       } catch (error) {
         Console.print(`오류가 발생했습니다: ${error.message}`);
