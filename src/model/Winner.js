cffcb60e4efa3748e61693a3387a1163cdd605e8
JuYeong17 <dhwndud980@gmail.com>
2024-10-26 17:23:54 +0900

    refactor: 필요없는 코드 삭제

--- src/model/Winner.js
+++ src/model/Winner.js
@@ -1,12 +1,9 @@
-import { Console } from "@woowacourse/mission-utils";
 
 class Winner{
   async winner(car,arr){
     let maxNumber=0;
     let winnerarr=[];
 
-    Console.print(car);
-    Console.print(arr);
     for(let i=0; i<arr.length;i++){
       if(maxNumber < arr[i]){
         maxNumber=arr[i];
