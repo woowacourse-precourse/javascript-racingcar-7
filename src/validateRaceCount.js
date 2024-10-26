514eca338a9ef1bd8366d397bd49246cb43a2702
Yusi <yusi4781@naver.com>
2024-10-27 04:55:14 +0900

    feat(validateRaceCount): 예외 상황 추가

--- src/validateRaceCount.js
+++ src/validateRaceCount.js
@@ -1,6 +1,9 @@
 const validateRaceCount = (raceCount) => {
   const VALID_NUM_OF_ATTEMPS = /^-?[0-9]*$/;
 
+  if (!raceCount) {
+    throw new Error('[ERROR] 시도 횟수를 입력해주세요.');
+  }
   if (!VALID_NUM_OF_ATTEMPS.test(raceCount)) {
     throw new Error('[ERROR] 시도 횟수는 숫자로만 이루어져 합니다.');
   }
