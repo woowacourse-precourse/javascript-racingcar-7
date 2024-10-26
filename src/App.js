c678c9695405e21681980edcecae5c0bba841691
yiju <yiju1755@naver.com>
2024-10-26 22:39:04 +0900

    feat: 1.사용자 입력 - 경주할 자동차에 대한 생성자와 속성 초기화 - 경주할 자동차 이름을 입력 받는 함수 getCarName 구현 - 입력 받은 자동차 이름을 생성자를 이용해 데이터 생성하는 함수 saveCarConstructor 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,28 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
+  constructor(carName, forward) {
+    this.carName = carName;
+    this.forward = forward;
+  }
+  async play() {
+    MissionUtils.Console.print(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+    );
+    const name = await this.getCarName();
+    const cars = this.saveCarConstructor(name);
+    MissionUtils.Console.print(name);
+  }
+  async getCarName() {
+    const name = await MissionUtils.Console.readLineAsync();
+    return name;
+  }
+  saveCarConstructor(name) {
+    const nameList = name.split(",");
+    const cars = nameList.map((carName) => new App(carName, ""));
+    return cars;
+  }
+
   async run() {}
 }
 
