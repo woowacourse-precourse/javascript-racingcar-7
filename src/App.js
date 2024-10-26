e525c561dba8e25ee69b7d5245be9875a21d5608
LEE DONG GYU <tenenger7125@gmail.com>
2024-10-26 22:39:04 +0900

    feat(input): read tryCount input

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ class App {
   async run() {
     const carNamesInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
     this.cars = this.createCars(carNamesInput);
+
+    const tryCountInput = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
   }
 
   createCars(carNamesInput) {
