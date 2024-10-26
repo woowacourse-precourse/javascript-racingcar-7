763042d32181168842e9f0d162c85fa1e666b263
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-10-26 20:28:34 +0900

    feat: 경주할 자동차의 입력이 유효한지 확인

--- src/App.js
+++ src/App.js
@@ -4,8 +4,19 @@ class App {
       "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)"
     );
     const INPUT = await Console.readLineAsync("");
+
     Console.print("시도할 횟수는 몇 회인가요?");
     const TIMES_INPUT = await Console.readLineAsync("");
+    
+    let cars = this.validateCarInput(INPUT);
+
+  }
+  validateCarInput(input) {
+    const cars = input.split(",");
+    if (input.indexOf(",") == -1 || input == "") {
+      throw new Error("[ERROR] 잘못된 입력입니다.");
+    }
+    cars.forEach(this.validateCarNameLength);
   }
 }
 
