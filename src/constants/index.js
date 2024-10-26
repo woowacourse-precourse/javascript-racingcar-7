cf491d06c2fa9d61966b289891a32eaa534f444b
jiyun1006 <jiyun4251@gmail.com>
2024-10-26 23:52:00 +0900

    ✨ feat : 상대경로를 위한 index.js
    constants, utils 폴더의 상대경로 간소화를 위한 index.js 설정

---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export { default as MAGICNUMBER } from './magicnumber.js';
+export { default as ERRORMESAGE } from './errorMessage.js';
+export { default as IOMESSAGE } from './ioMessage.js';
---
+++ src/utils/index.js
@@ -0,0 +1,10 @@
+export {
+  inputCarName,
+  inputTryNum,
+  printProgressResult,
+  printGameProgress,
+  printNewLine,
+} from './inputoutputMethod.js';
+export { default as validation } from './validation.js';
+export { default as pickRandomNumber } from './pickRandomNumber.js';
+export { default as sortArr } from './sortArr.js';
