e0960d3fb3779d1339d7b6ea3d050ff220f4e27c
devjaeyoon <devjaeyoon@gmail.com>
2024-10-26 11:51:29 +0900

    feat: 게임 실행 기능 구현

--- src/playGame.js
+++ src/playGame.js
@@ -1,9 +1,26 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import printProgress from './printProgress.js';
+import isRandomSingleDigitHigherThanThree from './isRandomSingleDigitHigherThanThree.js';
+
 function playGame(carNames, attempts) {
   const record = {};
 
   carNames.forEach((carName) => {
     record[carName] = 0;
   });
+
+  MissionUtils.Console.print('');
+  MissionUtils.Console.print('실행 결과');
+
+  for (let i = 0; i < attempts; i += 1) {
+    carNames.forEach((carName) => {
+      if (isRandomSingleDigitHigherThanThree()) {
+        record[carName] += 1;
+      }
+    });
+    printProgress(record);
+    MissionUtils.Console.print('');
+  }
 }
 
 export default playGame;
