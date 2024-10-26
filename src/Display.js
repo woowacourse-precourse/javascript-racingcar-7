451d41f75a8702c8f10d0645cc3ac1bd9245c8aa
jeongeunLee <leeju1009@gmail.com>
2024-10-26 20:19:50 +0900

    feat(display): 라운드 실행 결과 header를 출력하는 함수 추가

--- src/Display.js
+++ src/Display.js
@@ -1,3 +1,14 @@
-class Display {}
+import { Console } from '@woowacourse/mission-utils';
+
+class Display {
+  static showRoundResultHeader(round) {
+    const FIRST_ROUND = 1;
+    const HEADER = '실행 결과';
+
+    if (round === FIRST_ROUND) {
+      Console.print(HEADER);
+    }
+  }
+}
 
 export default Display;
