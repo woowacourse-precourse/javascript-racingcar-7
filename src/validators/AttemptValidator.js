20dff9f94527f2016de886ad15965d78244699ba
Park JiSeop <141125424+HarrySeop@users.noreply.github.com>
2024-10-27 05:58:24 +0900

    🎨style: 가독성을 위한 줄바꿈 추가

--- src/validators/AttemptValidator.js
+++ src/validators/AttemptValidator.js
@@ -3,9 +3,11 @@ class AttemptValidator {
     if (roundAttempt === '') {
       throw new Error('[ERROR] 시도 횟수에 공백만 입력하시면 안됩니다.');
     }
+
     if (Number.isNaN(roundAttempt)) {
       throw new Error('[ERROR] 숫자가 아닌 값을 입력하시면 안됩니다.');
     }
+
     if (roundAttempt < 1) {
       throw new Error('[ERROR] 1 이상의 숫자를 입력하시면 안됩니다.');
     }
--- src/validators/CarNameValidator.js
+++ src/validators/CarNameValidator.js
@@ -4,9 +4,11 @@ class CarNameValidator {
       if (carName === '') {
         throw new Error('[ERROR] 자동차 이름은 공백일 수 없습니다.');
       }
+
       if (carName.includes(' ')) {
         throw new Error('[ERROR] 자동차 이름에 공백이 포함되면 안됩니다.');
       }
+
       if (carName.length > 5) {
         throw new Error('[ERROR] 자동차 이름은 5자 이하여야 합니다.');
       }
