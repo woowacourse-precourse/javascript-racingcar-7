94098b0bfe1fd7630e97cf946fc580aa2751a9d7
jjang-aaa <jadoit473@gmail.com>
2024-10-27 03:36:08 +0900

    feat(carRacingProceeding): input함수들과 validation함수 추가
    자동차 이름과 시도할 횟수를 입력받고 각각 검증하는 함수를 자동차 경주를 진행하는 파일에 추가

--- src/CarRacingGame/carRacingProceeding.js
+++ src/CarRacingGame/carRacingProceeding.js
@@ -1,1 +1,10 @@
-export function carRacingProceed() {}
+import { inputCarNames, inputTryCount } from './input.js';
+import { validateCarName, validateTryCount } from './validation.js';
+
+export async function carRacingProceed() {
+  const carNameList = await inputCarNames();
+  validateCarName(carNameList);
+
+  const tryCount = await inputTryCount();
+  validateTryCount(tryCount);
+}
