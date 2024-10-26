0ab8700a19b156f96e41641d127af0dbb410b77d
gyur1kim <qri1114@gmail.com>
2024-10-26 23:33:18 +0900

    feat(input): 자동차 이름 중복 처리
    - 자동차 이름을 map 객체로 관리
    - 추후 전진 횟수를 관리하기 위해 map객체 선택

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Console } from '@woowacourse/mission-utils'
 
 class App {
   constructor () {
-    this.carNames = []
+    this.carNames = new Map()
     this.rounds = 0
     this.output = ''
   }
@@ -10,17 +10,24 @@ class App {
   isValidCarName(name) {
     const carName = name.trim();
     if (carName.length <=0)
-      throw new Error('[ERROR] 자동차 이름을 제대로 입력하세욧.');
+      throw new Error('[ERROR] 자동차 이름을 입력해주세요.');
     if (carName.length > 5)
       throw new Error('[ERROR] 자동차 이름은 5자 이하입니다.');
     return carName;
   }
 
+  hasDuplicateName(list) {
+    for (const element of list) {
+      this.carNames.set(element, 0);
+    }
+    if (this.carNames.size !== list.length) throw new Error('[ERROR] 자동차 이름은 중복될 수 없습니다.');
+  }
+
   async getCarNames () {
     try {
       const input = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-      
-      this.carNames = input.split(',').map(this.isValidCarName);
+      const carNameList = input.split(',').map(this.isValidCarName);
+      this.hasDuplicateName(carNameList);
     } catch(e) {
       throw e;
     }
@@ -29,9 +36,9 @@ class App {
   async getRounds () {
     const input = Number(await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n'));
      
-    if (Number.isNaN(input)) throw new Error('[ERROR] 시도할 횟수는 숫자만 입력해주세요')
-    if (input <= 0) throw new Error('[ERROR] 시도 횟수는 최소 1회입니다.')
-    this.rounds = input
+    if (Number.isNaN(input)) throw new Error('[ERROR] 시도할 횟수는 숫자만 입력해주세요');
+    if (input <= 0) throw new Error('[ERROR] 시도 횟수는 최소 1회입니다.');
+    this.rounds = input;
   }
 
   async run() {
