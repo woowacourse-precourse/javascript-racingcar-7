5c15dcb1ad5749bb6c1363096aaf09cf56920ee8
최영빈 <bin778@naver.com>
2024-10-26 15:33:36 +0900

    test: 최종 우승자 테스트 케이스 작성

---
+++ __tests__/WinnerTest.js
@@ -0,0 +1,43 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { maxScoreCar } from "../src/Component/SearchMaxScore.js";
+
+describe("최종 우승자 테스트", () => {
+  test("단독 우승", () => {
+    const logSpy = jest.spyOn(MissionUtils.Console, "print");
+
+    const car_names_arr_input = ["kim", "lee", "choi", "park"];
+    const car_racing_result_input = [4, 3, 5, 4];
+    const output = ["최종 우승자 : choi"];
+
+    maxScoreCar(car_names_arr_input, car_racing_result_input);
+
+    output.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(output);
+    });
+  });
+
+  test("공동 우승", () => {
+    const logSpy = jest.spyOn(MissionUtils.Console, "print");
+
+    const car_names_arr_input = [
+      "kim",
+      "lee",
+      "choi",
+      "john",
+      "peter",
+      "jack",
+      "mark",
+      "yoon",
+      "lim",
+      "paul",
+    ];
+    const car_racing_result_input = [5, 7, 7, 6, 5, 4, 7, 3, 4, 6];
+    const output = ["최종 우승자 : lee, choi, mark"];
+
+    maxScoreCar(car_names_arr_input, car_racing_result_input);
+
+    output.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(output);
+    });
+  });
+});
