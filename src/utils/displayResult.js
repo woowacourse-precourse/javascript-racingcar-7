8263f0170cb4867cf718e6de71364c504ead4b1d
minseok <alstjrwjd99@naver.com>
2024-10-26 23:42:02 +0900

    fix(test): Assign MissionUtils.Console.print to a variable for better test handling

--- src/utils/displayResult.js
+++ src/utils/displayResult.js
@@ -1,22 +1,19 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import { MESSAGE } from "./constants";
-
-const woowahanPrint = MissionUtils.Console.print;
+import { MESSAGE } from "./constants.js";
 
 function displayEndOfStart() {
-    woowahanPrint(MESSAGE.DISPLAY_END_OF_START);
+    MissionUtils.Console.print(MESSAGE.DISPLAY_END_OF_START);
 }
 
 function displayStatus(cars) {
     cars.forEach((car) => {
-        woowahanPrint(`${car.getName()} : ${'-'.repeat(car.getMoved())}`);
+        MissionUtils.Console.print(`${car.getName()} : ${'-'.repeat(car.getMoved())}`);
     });
-    woowahanPrint(MESSAGE.DISPLAY_EMPTY_LINE);
+    MissionUtils.Console.print(MESSAGE.DISPLAY_EMPTY_LINE);
 }
 
 function displayResult(result) {
-    woowahanPrint(`${MESSAGE.DISPLAY_WINNER}${result.join(', ')}`);
+    MissionUtils.Console.print(`${MESSAGE.DISPLAY_WINNER}${result.join(', ')}`);
 }
-
 
 export { displayResult, displayStatus, displayEndOfStart };
