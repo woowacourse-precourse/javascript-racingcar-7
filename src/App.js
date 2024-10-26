2b2536c1b42b5884a495dd87a9bcbaea2d7649f5
Jun Hee Jung <wjdtka1325@naver.com>
2024-10-27 07:25:39 +0900

    feat(racingGame): 우승자 출력 메서드(printWinners) 구현

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App {
     );
 
     const racingGame = new RacingGame(cars, attemptCount);
+    
     racingGame.play()
   }
 }
--- src/RacingGame.js
+++ src/RacingGame.js
@@ -1,4 +1,4 @@
-import { Random } from '@woowacourse/mission-utils';
+import { Random, Console } from '@woowacourse/mission-utils';
 
 class RacingGame {
   static FORWARD_INDICATOR = '-';
@@ -9,25 +9,34 @@ class RacingGame {
   }
 
   play() {
-    let count = 0;
-    while (count < this.attemptCount) {
+    let currentStep = 1;
+    while (currentStep <= this.attemptCount) {
       this.cars.forEach((car) => {
         if (Random.pickNumberInRange(0, 9) >= 4) {
-          car.forwardCount++;
+          car.forward();
         }
       });
-      this.printResultsByStep(this.cars);
-      count++;
+      this.printResultByStep(this.cars);
+      currentStep++;
     }
+    this.printWinners(this.cars);
   }
 
   printResultByStep(cars) {
-    this.cars.forEach((car) => {
-      console.log(
+    cars.forEach((car) => {
+      Console.print(
         `${car.name} : ${RacingGame.FORWARD_INDICATOR.repeat(car.forwardCount)}`
       );
     });
-    console.log('\n');
+    Console.print('');
+  }
+
+  printWinners(cars) {
+    const maxForwardCount = Math.max(...cars.map((car) => car.forwardCount));
+    const winners = cars
+      .filter((car) => car.forwardCount === maxForwardCount)
+      .map((car) => car.name);
+    Console.print(`최종 우승자 : ${winners.join(', ')}`);
   }
 }
 
