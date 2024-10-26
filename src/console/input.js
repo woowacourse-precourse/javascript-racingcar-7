47f01907b4f2ea9de6e65c91da9af2ff5eb962f2
ekkk1126 <cabi0203@naver.com>
2024-10-27 00:43:22 +0900

    fix: 오류시 반복 입력 제거

--- src/console/input.js
+++ src/console/input.js
@@ -2,33 +2,16 @@ import { Console } from '@woowacourse/mission-utils';
 import { ERROR_MESSAGE, IO_MESSAGE } from '../constants/message.js';
 
 async function Input() {
-    let cars, tryCount;
-
-    while (true) {
-        try {
-            cars = await Console.readLineAsync(IO_MESSAGE.INPUT_CAR);
-            cars = cars.split(',');
-            if (cars.length < 2 || cars.some((car) => car.trim() === '')) {
-                throw new Error(ERROR_MESSAGE.ERROR_CAR);
-            }
-            cars.map((val) => {
-                if (val.length > 5) throw new Error(ERROR_MESSAGE.ERROR_CAR);
-            });
-            break;
-        } catch (error) {
-            Console.print(error.message);
-        }
+    let cars = await Console.readLineAsync(IO_MESSAGE.INPUT_CAR);
+    cars = cars.split(',');
+    if (cars.length < 2 || cars.some((car) => car.trim() === '' || car.length > 5)) {
+        throw new Error(ERROR_MESSAGE.ERROR_CAR);
     }
 
-    while (true) {
-        try {
-            tryCount = await Console.readLineAsync(IO_MESSAGE.INPUT_TRY);
-            tryCount = Number(tryCount);
-            if (isNaN(tryCount) || tryCount <= 0) throw new Error(ERROR_MESSAGE.ERROR_TRY);
-            break;
-        } catch (error) {
-            Console.print(error.message);
-        }
+    let tryCount = await Console.readLineAsync(IO_MESSAGE.INPUT_TRY);
+    tryCount = Number(tryCount);
+    if (isNaN(tryCount) || tryCount <= 0) {
+        throw new Error(ERROR_MESSAGE.ERROR_TRY);
     }
 
     return { cars, tryCount };
