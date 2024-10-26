cbb672e91d005bd7b5c19ca6fbf72791e2cac34e
ahhyun <wndkgus0225@naver.com>
2024-10-26 11:00:40 +0900

    feat(message) : 에러메시지 선언

---
+++ src/message.js
@@ -0,0 +1,5 @@
+export const ERROR_MESSAGE = {
+  INVALID_DELIMITER: '[ERROR] 쉼표 이외의 구분자는 사용할 수 없습니다.',
+  NAME_LENGTH_EXCEED: '[ERROR] 이름은 5자 이하만 가능합니다.',
+  NOT_NUMBER: '[ERROR] 숫자 이외의 입력은 사용할 수 없습니다.'
+}
