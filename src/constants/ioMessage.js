f52419da16d4a3477767c1894ed505c2084491a1
jiyun1006 <jiyun4251@gmail.com>
2024-10-26 23:00:15 +0900

    ✨ feat : 입출력문구 상수화
    입출력시에 질문글 및 출력문을 상수화

---
+++ src/constants/ioMessage.js
@@ -0,0 +1,9 @@
+const IOMESSAGE = {
+  INPUTCARNAMEMESSAGE:
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+  TRYNUMBERMESSAGE: '시도할 횟수는 몇 회인가요?',
+  PROGRESSRESULT: '실행 결과',
+  NEWLINE: '\n',
+};
+
+export default IOMESSAGE;
