1b5d11b8960578b64ed3759e568dbcf74f057bb4
guddnboy <hung3018@naver.com>
2024-10-26 16:55:16 +0900

    refactor: 애매모호한 메소드 명 과 변수 및 상수 이름 명확하게 수정

--- src/App.js
+++ src/App.js
@@ -1,108 +1,112 @@
 import { Console, Random } from '@woowacourse/mission-utils';
 
+const LIMIT_NAME_LENGTH = 5;
+
 function getCarNames(userInput) {
   const carNames = userInput.split(',');
   carNames.map((carName) => {
-    handleBlankError(carName);
-    handleLengthError(carName);
+    handleBlankNameError(carName);
+    handleNameLengthError(carName);
   });
   return carNames;
 }
 
 function permissionToGo() {
   const RANDOM_NUMBER = Random.pickNumberInRange(0, 9);
-  let go = false;
+  let permission = false;
 
   if (RANDOM_NUMBER >= 4) {
-    go = true;
+    permission = true;
   }
-  return go;
+  return permission;
 }
 
-function proceedOneCycle(permissionToGo, N, carsResults) {
-  for (let i = 0; i < N; i++) {
+function proceedOneCycle(permissionToGo, count, carsResults) {
+  for (let i = 0; i < count; i++) {
     const permission = permissionToGo();
     moveCar(permission, carsResults, i);
     Console.print(`${carsResults[i].name} : ${carsResults[i].position}`);
   }
 }
 
-function moveCar(permission, value, index) {
+function moveCar(permission, car, index) {
   if (permission) {
-    value[index].position += '-';
+    car[index].position += '-';
   }
 }
 
-function maxPositionUpdater(value, maxValue) {
-  if (value >= maxValue) {
-    maxValue = value;
+function maxPositionUpdater(position, maxPosition) {
+  if (position >= maxPosition) {
+    maxPosition = position;
   }
-  return maxValue;
+  return maxPosition;
 }
 
-function findMaxPosition(carsResults) {
+function findMaxPosition(carList) {
   let MAX_POSITION = 0;
-  carsResults.map((car) => {
+  carList.map((car) => {
     MAX_POSITION = maxPositionUpdater(car.position.length, MAX_POSITION);
   });
   return MAX_POSITION;
 }
 
-function selectWinner(value, maxValue, winners) {
-  if (value.position.length == maxValue) {
-    winners.push(value.name);
+function selectWinners(position, maxPosition, winners) {
+  if (position.position.length == maxPosition) {
+    winners.push(position.name);
   }
   return winners;
 }
 
-function getWinners(carsResults, maxPosition) {
+function returnWinners(carsResults, maxPosition) {
   let winners = [];
   carsResults.forEach((car) => {
-    winners = selectWinner(car, maxPosition, winners);
+    winners = selectWinners(car, maxPosition, winners);
   });
   return winners;
 }
 
-function checkDuplication(carsNames) {
+function checkDuplicationName(carsNames) {
   let trimedNames = [];
   carsNames.forEach((carName) => {
     trimedNames.push(carName.trim());
   });
   const CARS_NAMES = new Set(trimedNames);
 
-  handleDuplicationError(trimedNames, CARS_NAMES);
+  handleDuplicationNameError(trimedNames, CARS_NAMES);
 }
 
-function handleBlankError(value) {
-  if (value.trim() === '') {
+function handleBlankNameError(name) {
+  if (name.trim() === '') {
     throw new Error('[ERROR] 공백은 이름이 될 수 없습니다.');
   }
   return;
 }
 
-function handleLengthError(name) {
-  if (name.length > 5) {
-    throw new Error('[ERROR] 이름은 5글자를 초과할 수 없습니다.');
+function handleNameLengthError(name) {
+  if (name.length > LIMIT_NAME_LENGTH) {
+    throw new Error(
+      `[ERROR] 이름은 ${LIMIT_NAME_LENGTH}글자를 초과할 수 없습니다.`
+    );
   }
   return;
 }
 
-function handleTypeError(N) {
+function handleCountTypeError(count) {
   const MINUS_REGEX = /-\d+/;
 
-  if (MINUS_REGEX.test(N)) {
+  if (MINUS_REGEX.test(count)) {
     throw new Error('[ERROR] 음수를 입력할 수 없습니다.');
   }
 
-  if (isNaN(N)) {
+  if (isNaN(count)) {
     throw new Error('[ERROR] 숫자를 입력해주세요.');
   }
 
-  return N;
+  return count;
 }
 
-function handleDuplicationError(firstValue, secondValue) {
-  if (firstValue.length !== secondValue.size) {
+function handleDuplicationNameError(nameList, nameListSet) {
+  if (nameList.length !== nameListSet.size) {
     throw new Error('[ERROR] 중복된 이름이 있습니다.');
   }
   return;
@@ -119,16 +123,16 @@ class App {
       name: carName.trim(),
       position: '',
     }));
-    checkDuplication(carNames);
-    const N = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
-    handleTypeError(N);
+    checkDuplicationName(carNames);
+    const count = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    handleCountTypeError(count);
 
     Console.print('실행 결과');
-    for (let i = 0; i < N; i++) {
+    for (let i = 0; i < count; i++) {
       proceedOneCycle(permissionToGo, carNames.length, carsResults);
       Console.print('');
     }
-    const winners = getWinners(carsResults, findMaxPosition(carsResults));
+    const winners = returnWinners(carsResults, findMaxPosition(carsResults));
     Console.print(`최종 우승자 : ${winners.join()}`);
   }
   return;
