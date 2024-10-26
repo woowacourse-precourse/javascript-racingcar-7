8322459730f1b5c6461d3d45c75a7c02b77c6565
Daeuning <happyjde3@gmail.com>
2024-10-27 02:30:59 +0900

    feat(Race): 최종 우승자를 출력하는 기능 구현

--- src/Race.js
+++ src/Race.js
@@ -12,11 +12,14 @@ class Race {
   async startRace() {
     await this.setCarName();
     await this.setAttemptCount();
-    
+
     for (let i = 0; i < this.attemptCount; i++) {
       this.generateRandomDistances();
       this.printRaceStatus();
     }
+
+    const winners = this.getWinners();
+    console.log(`최종 우승자: ${winners.join(', ')}`);
   }
 
   async setCarName() {
@@ -46,6 +49,13 @@ class Race {
       console.log(`${car.getName()} : ${distanceSymbol}`);
     });
     console.log(''); 
+  }
+
+  getWinners() {
+    const maxDistance = Math.max(...this.cars.map(car => car.getDistance()));
+    return this.cars
+      .filter(car => car.getDistance() === maxDistance)
+      .map(car => car.getName());
   }
 }
 
