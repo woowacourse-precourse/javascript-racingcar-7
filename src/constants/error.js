b7d5ae63181818664635daa27e9c06b8acce8bb0
daeun <gbajs754@gmail.com>
2024-10-26 23:26:52 +0900

    refactor : constants 폴더 생성후 상수 분리

---
+++ src/constants/error.js
@@ -0,0 +1,7 @@
+import { MAX_NAME_LENGTH } from './index.js';
+
+const ERROR_MESSAGES = {
+  LESS_THAN_FIVE_LETTERS: `이름은 ${MAX_NAME_LENGTH}글자 이하로 입력해주세요`,
+};
+
+export default ERROR_MESSAGES;
---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+const FIRST_USER_INPUT_PLACEHOLDER =
+  '경주할 자동차 이름을 입력하세요. 이름은 쉼표를 기준으로 구분합니다. \n';
+const SECOND_USER_INPUT_PLACEHOLDER = '시도할 횟수는 몇 회인가요? \n';
+
+const NAME_SEPARATOR = ',';
+
+const MAX_NAME_LENGTH = 5;
+
+export {
+  FIRST_USER_INPUT_PLACEHOLDER,
+  SECOND_USER_INPUT_PLACEHOLDER,
+  NAME_SEPARATOR,
+  MAX_NAME_LENGTH,
+};
