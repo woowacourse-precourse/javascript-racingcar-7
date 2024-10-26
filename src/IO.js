2f847370049f9b12f337c1f8a01e93a10fcd7205
ahhyun <wndkgus0225@naver.com>
2024-10-26 14:57:52 +0900

    refactor(IO) : message.js를 이용하여 간결하게 정리

--- src/IO.js
+++ src/IO.js
@@ -2,15 +2,17 @@ import { Console } from '@woowacourse/mission-utils';
 import { validateCarNames } from "./error.js";
 import { validateCount } from "./error.js";
 
+import { MESSAGE } from "./message.js";
+
 export async function getCarNames() {
-  const input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+  const input = await Console.readLineAsync(MESSAGE.ASK_CARNAMES+"\n");
   const carNames = input.split(",");
   validateCarNames(carNames);
   return carNames;
 }
 
 export async function getCount() {
-  const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+  const input = await Console.readLineAsync(MESSAGE.ASK_COUNT+"\n");
   const count = Number(input);
   validateCount(count);
   return count;
