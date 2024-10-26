d89f6565cad0afb47c211c45b1fefe7603fa14b3
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-10-27 03:17:22 +0900

    refactor(getRaceNumber): 에러 핸들 로직 한곳에 통합

--- src/utils/getRaceNumber.js
+++ src/utils/getRaceNumber.js
@@ -5,15 +5,21 @@ const getRaceNumber = async () => {
   const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
   const num = Number(input.trim());
 
-  if (!num) throw new Error(ERROR.NO_INPUT_RACE_NUMBER);
-  checkIsPositiveInteger(num);
+  checkNumberError(num);
 
   return num;
 };
 
-const checkIsPositiveInteger = (num) => {
-  if (!Number.isInteger(num) || num <= 0) {
-    throw new Error(ERROR.INVALID_RACE_NUMBER);
+const checkNumberError = (num) => {
+  const isInvalidNumber = !Number.isInteger(num) || num <= 0;
+
+  switch (true) {
+    case !num:
+      throw new Error(ERROR.NO_INPUT_RACE_NUMBER);
+    case isInvalidNumber:
+      throw new Error(ERROR.INVALID_RACE_NUMBER);
+    default:
+      break;
   }
 };
 
