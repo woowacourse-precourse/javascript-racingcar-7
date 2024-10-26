cf53c7389479116c4105cd13fff7168408bb9fdc
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-10-26 14:17:22 +0900

    test: 자동차 이름은 5자 이하만 가능하다

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,4 +1,4 @@
-import { splitCarName } from '../src/InputUtils';
+import { splitCarName, validateCarName } from '../src/InputUtils';
 
 describe('입력', () => {
   test('자동차 이름은 쉼표(,)를 기준으로 구분한다.', () => {
@@ -6,5 +6,10 @@ describe('입력', () => {
     const NAMES = ['pobi', 'woni', 'jun'];
 
     expect(splitCarName(INPUT)).toEqual(NAMES);
+  });
+
+  test('자동차 이름은 5자 이하만 가능하다.', () => {
+    const inputs = ['pobi, javaji'];
+    expect(() => validateCarName(inputs)).toThrow('[ERROR]');
   });
 });
