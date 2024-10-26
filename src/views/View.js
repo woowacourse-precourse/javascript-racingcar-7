c42dba6dfa80990be82ea05dcfc9c4113e61321e
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-10-27 03:18:06 +0900

    feat: 현재 경주 상태 출력 함수 구현

--- src/views/View.js
+++ src/views/View.js
@@ -3,3 +3,11 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 export function getUserInput() {
   return MissionUtils.Console.readLineAsync(''); // promist 반환
 }
+
+export function printRacingState(carData) {
+  carData.forEach((line) => {
+    MissionUtils.Console.print(line);
+  });
+  // 한 턴이 끝난 후 빈 줄을 출력하여 구분
+  MissionUtils.Console.print('');
+}
