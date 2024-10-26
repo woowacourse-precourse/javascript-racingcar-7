7629f51a130676c82db865ab6729a26087721790
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-10-26 23:32:39 +0900

    feat: 자동차 객체 생성 기능

--- src/models/Model.js
+++ src/models/Model.js
@@ -2,6 +2,16 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 export function divisionCarName(input) {
   const delimiter = ',';
-  const car = input.split(delimiter);
-  return car;
+  const carNames = input.split(delimiter); // 입력을 ,로 분리하여 배열로 변환
+  return carNames;
+}
+
+export function createCarObject(carNames) {
+  // 각 이름마다 car 객체를 생성하여 배열로 반환
+  const cars = carNames.map((name) => ({
+    name: name.trim(),
+    forwardNum: 0,
+  }));
+
+  return cars;
 }
