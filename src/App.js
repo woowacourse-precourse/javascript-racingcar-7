977d2ec154ecbc9bf84960fa87d2e877f02c7fdd
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-10-26 17:55:06 +0900

    feat : 시도횟수, 자동차 이름 입력 및 자동차 객체화

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Race from './Race.js';
+
 class App {
-  async run() {}
+  async run() {
+    const race = new Race();
+    await race.handleCar();
+    await race.handleTryNumber();
+  }
 }
 
 export default App;
