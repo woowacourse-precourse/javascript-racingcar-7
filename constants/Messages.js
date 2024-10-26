2126d0a7630377430b6b698dbcea17660ac9135c
이동준 <idongjun@idongjun-ui-MacBookPro.local>
2024-10-27 08:41:59 +0900

    feat(constants) : 메세지 추가

---
+++ constants/Messages.js
@@ -0,0 +1,6 @@
+const MESSAGES = Object.freeze({
+  CAR_INPUT: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+  TRY_COUNT: "시도할 횟수는 몇 회인가요?\n",
+  EXECUTION_RESULT: "\n실행 결과\n",
+  FINAL_WINNER: "최종우승자 : ",
+});
