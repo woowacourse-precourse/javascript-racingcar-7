3ef884f5a6846589a202bf869b70f7808f2f2f30
jsy <suyeonb1db@gmail.com>
2024-10-26 23:27:10 +0900

    fix(App): 자동차 이름 입력 수정
    자동차 이름 입력시 모든 공백 제거, 구분자(,)로 분리한 자동차 이름 배열로 값 전달

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ async function getNumber() {
 
 async function getCarname() {
   const input = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n");
-  return input;
+  const blank = /(\s*)/g;
+  const splitInput = input.replace(blank,'').split(',');
+  return splitInput;
 }
 
 class App {
