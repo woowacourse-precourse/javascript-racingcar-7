36c0498730923f6502440652534dd31412fe7170
Seio924 <seio9241@gmail.com>
2024-10-26 10:22:56 +0900

    test(RaceServiceTest): 한 경기가 끝날때마다 결과 출력 테스트 추가

--- __tests__/RaceServiceTest.js
+++ __tests__/RaceServiceTest.js
@@ -1,5 +1,7 @@
+import App from '../src/App';
 import Car from '../src/Model/Car';
 import RaceService from '../src/Service/RaceService';
+import { getLogSpy, mockQuestions, mockRandoms } from './ApplicationTest';
 
 describe('RaceService', () => {
   test('0에서 9사이의 무작위 값을 생성한다', async () => {
@@ -30,5 +32,38 @@ describe('RaceService', () => {
     raceService.moveCar(car);
 
     expect(car[0].getForwardCount()).toBe(1);
+  });
+
+  test('한 경기가 끝날때마다 결과를 출력한다.', async () => {
+    const MOVING_FORWARD = 4;
+    const STOP = 3;
+    const inputs = ['pobi,woni', '3'];
+    const logs = [
+      '실행 결과',
+      'pobi : -',
+      'woni : ',
+      'pobi : --',
+      'woni : ',
+      'pobi : --',
+      'woni : -',
+    ];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([
+      MOVING_FORWARD,
+      STOP,
+      MOVING_FORWARD,
+      STOP,
+      STOP,
+      MOVING_FORWARD,
+    ]);
+
+    const app = new App();
+    await app.run();
+
+    logs.forEach(log => {
+      expect(logSpy).toHaveBeenCalledWith(log);
+    });
   });
 });
