f50dc20dda84ecae0aaa5fe23219d4f32e31f4d9
moonseonghui <msh9980@naver.com>
2024-10-27 17:34:22 +0900

    feat(validateRacingCarInputTest): 자동차 이름에 숫자도 포함함에 따라 테스트 케이스 문항 변경

--- __tests__/validator/validateRacingCarInput.test.js
+++ __tests__/validator/validateRacingCarInput.test.js
@@ -3,9 +3,11 @@ import { ERROR_MESSAGE } from '../../src/constants/messages';
 
 // 유효한 문자열 테스트 케이스
 test.each([
-  ['알파벳과 쉼표만 포함된 경우1', 'carA,carB,carC'],
-  ['알파벳과 쉼표만 포함된 경우2', 'car,ra,carB'],
+  ['알파벳, 숫자 쉼표만 포함된 경우', 'car1,car2,car3'],
+  ['알파벳 쉼표만 포함된 경우', 'carA,carB,carC'],
+  ['숫자 쉼표만 포함된 경우', '1,2,3'],
   ['알파벳만 포함된 경우', 'CaR'],
+  ['숫자만 포함된 경우', '12345'],
 ])('[유효한 문자열 검사] %s : %s', (description, input) => {
   expect(validateRacingCarInput(input)).toBe(input);
 });
@@ -13,17 +15,12 @@ test.each([
 // 유효하지 않은 문자열 테스트 케이스
 test.each([
   [
-    '알파벳과 쉼표 외의 문자가 포함된 경우(숫자)',
-    'carA,carB,car3',
+    '알파벳,숫자,쉼표 외의 문자가 포함된 경우(특수 문자)',
+    'car,carA,carC!',
     ERROR_MESSAGE.INVALID_CAR_INPUT,
   ],
   [
-    '알파벳과 쉼표 외의 문자가 포함된 경우(특수 문자)',
-    'car,carA.carC',
-    ERROR_MESSAGE.INVALID_CAR_INPUT,
-  ],
-  [
-    '알파벳과 쉼표 외의 문자가 포함된 경우(한글)',
+    '알파벳,숫자,쉼표 외의 문자가 포함된 경우(한글)',
     'car,carA,자동차',
     ERROR_MESSAGE.INVALID_CAR_INPUT,
   ],
