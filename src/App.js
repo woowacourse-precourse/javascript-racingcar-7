6744b478cfcf37619cf9f8064914e742047b1cda
HYUNJIN105 <hyunjin.011005@gmail.com>
2024-10-27 21:21:38 +0900

    feat: 자동차 이름 입력 기능 구현
    자동차의 이름을 입력받고, 이름은 쉼표(,)를 기준으로 구분하여
    5자 이하의 이름만 입력 가능하게 구현했다.
    5자를 초과하는 이름을 입력했을 경우, [ERROR] 처리를 했다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const car_names = await App.get_car_names();
+      Console.print(`${car_names.join(', ')}`);
+    } catch (error) {
+      Console.print(`[ERROR] ${error.message}`);
+    }
+  }
+
+  static async get_car_names() {
+    const input_names = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ');
+    const car_names = input_names.split(',').map(name => name.trim());
+    const MAX_CAR = 5;
+
+    car_names.forEach(name => {
+      if (name.length > MAX_CAR) {
+        throw new Error('자동차 이름은 5자 이하만 가능합니다.');
+      }
+    });
+
+    return car_names;
+  }
 }
 
 export default App;
