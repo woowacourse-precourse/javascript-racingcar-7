65279c480bbe84a5a97ca3b9cb6bf34f7a2dff9c
erica-jeong <wjddbwjd1205@gmail.com>
2024-10-28 01:07:41 +0900

    feat: 시도 횟수만큼 경기 진행 및 결과 반복 출력 기능 추가
    - 입력받은 시도 횟수만큼 라운드를 반복하며 자동차 전진 여부 결정
    - 각 라운드 결과를 출력하여 진행 상황 표시

--- src/CarRacingManager.js
+++ src/CarRacingManager.js
@@ -1,14 +1,26 @@
+import CarRacingGame from "./CarRacingGame.js";
+
 class CarRacingManager {
-  constructor(inputHandler, carRacingGame, outputHandler) {
+  constructor(inputHandler, outputHandler) {
     this.inputHandler = inputHandler;
-    this.carRacingGame = carRacingGame;
     this.outputHandler = outputHandler;
+    this.carRacingGame = null;
   }
 
   async startGame() {
     try {
       const carNames = await this.inputHandler.readCarsName();
       const attemptCounts = await this.inputHandler.readAttemptCount();
+
+      this.carRacingGame = new CarRacingGame(carNames);
+      this.outputHandler.printResultMessage();
+
+      for(let i = 0; i < attemptCounts; i++) {
+        const roundResult = this.carRacingGame.playOneRound();
+        this.outputHandler.printOneRoundResult(roundResult);
+      }
+
+
     } catch (error) {
       this.outputHandler.printErrorMessage(error.message);
       throw error;
