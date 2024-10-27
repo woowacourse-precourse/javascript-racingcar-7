7ab2944cbf50d80e8be9b39f2a6e623c4c4b45df
yoojin <everywavyday@gmail.com>
2024-10-28 03:19:09 +0900

    feat: 경주할 자동차 이름 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    await this.getCarNames();
+  }
+
+  async getCarNames() {
+    const input = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+    );
+
+    let carList = [];
+    carList = input.split(',');
+    Console.print(carList);
+  }
 }
 
 export default App;
