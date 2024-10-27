90c5dc9f28ce323724880b1301b7acb93f77325e
Junseo Kim <semiwest2@gmail.com>
2024-10-28 06:08:46 +0900

    feat: 자동차 경주 앱 실행 로직 구현
    - 사용자로부터 자동차 이름과 시도 횟수를 입력받는 기능 추가
    - 입력된 자동차 이름의 유효성을 검증하는 기능 추가
    - 경주 실행 및 최종 우승자 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import getInput from './input/getInput.js';
+import parseCarNameInput from './input/parseCarNameInput.js';
+import { checkCarName, checkMoveCount } from './input/validateInput.js';
+import executeRace from './game/executeRace.js';
+import printString from './output/printString.js';
+
+
 class App {
-  async run() {}
+  async run() {
+    const carNameList = parseCarNameInput(await getInput('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'));
+    checkCarName(carNameList);
+
+    const moveCount = Number(await getInput('시도할 횟수는 몇 회인가요?'));
+    checkMoveCount(moveCount);
+
+    const winnerList = executeRace(carNameList, moveCount);
+    printString(`최종 우승자 : ${winnerList.join(', ')}`);
+
+  }
 }
 
 export default App;
