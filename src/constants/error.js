a16755f678a23dbe0876f5074f048db0d942d497
minbo <smb5174@naver.com>
2024-10-27 16:02:36 +0900

    feat: 시도할 횟수가 숫자가 아닌 경우 에러를 발생

--- src/constants/error.js
+++ src/constants/error.js
@@ -1,3 +1,4 @@
 export const ERROR = {
   EMPTY_STRING: "[ERROR] : 쉼표로 구분된 배열에 빈 문자열이 있습니다.",
+  NAN: "[ERROR] : 시도할 횟수가 숫자가 아닙니다.",
 };
--- src/utils/convertStringToNumber.js
+++ src/utils/convertStringToNumber.js
@@ -1,3 +1,8 @@
+import { ERROR } from "../constants/error.js";
+
 export const convertStringToNumber = (string) => {
+  if (isNaN(Number(string))) {
+    throw new Error(ERROR.NAN);
+  }
   return Number(string);
 };
