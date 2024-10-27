5eb5e2589c5ed528acabbf065796c8ffb601af63
KimPuro <puro@protonmail.ch>
2024-10-28 01:45:05 +0900

    feat(App): 랜덤 값을 이용한 전진 기능

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import {Console} from '@woowacourse/mission-utils'
+import {Console, MissionUtils} from '@woowacourse/mission-utils';
 
 class App {
     constructor() {
-        this.carArray = [];
+        this.result = [];
+        this.rounds = 0;
     }
+
     async run() {
-        this.carArray = await this.carNameInput();
-        await this.countInput();
-        this.printResult();
-
-
+        this.result = await this.carNameInput();
+        this.rounds = await this.countInput();
+        Console.print('실행 결과');
+        await this.playGame();
     }
 
     async carNameInput() {
         const inputArray = await Console.readLineAsync("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)");
-        const carArray = inputArray.split(',');
+        return inputArray.split(',').map(this.formatCarName);
+    }
 
-        carArray.forEach(carName => {
-            if (carName.length > 5) {
-                throw new Error("[ERROR] 자동차 이름은 5글자를 초과할 수 없습니다.");
-            }
-        });
-        return carArray;
+    formatCarName(carName) {
+        if (carName.length > 5) {
+            throw new Error("[ERROR] 자동차 이름은 5글자를 초과할 수 없습니다.");
+        }
+        return carName + ' : ';
     }
 
     async countInput() {
@@ -29,12 +30,36 @@ class App {
         if (count < 1) {
             throw new Error("[ERROR] 시도 횟수는 1 이상이어야 합니다.");
         }
-        return count;
+        return parseInt(count, 10);
     }
+
     printResult() {
-        this.carArray.forEach(car => {
-            Console.print(car + " : ");
-    })
+        this.result.forEach(car => {
+            Console.print(car);
+        });
+    }
+
+    isForward() {
+        return MissionUtils.Random.pickNumberInRange(0, 9) >= 4;
+    }
+
+    updateCarPosition(car) {
+        if (this.isForward()) {
+            return car + '-';
+        }
+        return car;
+    }
+
+    game() {
+        return this.result.map(car => this.updateCarPosition(car));
+    }
+
+    async playGame() {
+        for (let i = 0; i < this.rounds; i++) {
+            this.result = this.game();
+            this.printResult();
+            Console.print('');
+        }
     }
 }
 
