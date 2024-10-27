7d8242a497851954ac3704dd57b62e230a53e8c7
kimdayeon37 <kdy37912@naver.com>
2024-10-27 22:50:56 +0900

    test: 자동차 경기 시작 테스트

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -43,6 +43,7 @@ describe("자동차 경주", () => {
     });
   });
 
+  // 1. 자동차 이름 확인
   test("5자를 초과하는 이름이나 잘못된 입력에 대해 함수에서 오류가 발생한다.", () => {
     const app = new App();
     expect(() => app.validateCarNames(["pobi,javaji"])).toThrow("[ERROR] 자동차 이름은 최대 5자 이하이어야 합니다.");
@@ -54,6 +55,7 @@ describe("자동차 경주", () => {
   });
 
 
+  // 2. 이동 횟수 확인
   test("음수 또는 정수가 아닌 입력값은 함수에서 오류가 발생한다.", () => {
     const app = new App();
     expect(() => app.validateMoveCount("0")).toThrow("[ERROR] 시도 횟수는 1 이상의 정수여야 합니다.");
@@ -65,9 +67,19 @@ describe("자동차 경주", () => {
   });
 
 
+  // 3. 자동차 초기화
   test("이름과 위치가 0으로 설정된 자동차를 초기화하는지 확인한다.", () => {
     const app = new App();
     const cars = app.initializeCars(["pobi", "woni"]);
     expect(cars).toEqual([{ name: "pobi", position: 0 }, { name: "woni", position: 0 }]);
+  });
+
+
+  // 4. 자동차 경기 시작
+  test("라운드 실행하는 함수가 올바른 횟수만큼 호출되었는지 확인한다.", () => {
+    const app = new App();
+    const runRoundSpy = jest.spyOn(app, "runRound");
+    app.startRace([{ name: "pobi", position: 0 }], 3);
+    expect(runRoundSpy).toHaveBeenCalledTimes(3);
   });
 });
