c28633f8759e0e49ef2e7b0a5baa2537540980cc
songhj10207@naver.com <songhj10207@naver.com>
2024-10-27 21:47:59 +0900

    feat: 사용자의 입력을 받는 기능구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async getCarNames() {
+    return Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n'
+    );
+  }
+
+  async getTryCount() {
+    return Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+  }
+
+  async run() {
+    const userCarInput = await this.getCarNames();
+    const tryCount = await this.getTryCount();
+  }
 }
 
 export default App;
