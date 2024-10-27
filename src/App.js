1f80e40928ce28ce7ef9fc00fce8ddf20ea6f608
pengdyuo <121721388+Pengdyuo@users.noreply.github.com>
2024-10-28 05:15:06 +0900

    사용자 입력 처리 및 검증

--- src/App.js
+++ src/App.js
@@ -14,4 +14,26 @@ class Car {
   }
 }
 
-export default Car;
+class App {
+  async run() {
+    const carNames = await this.readCarNames();
+    this.validateCarNames(carNames);
+  }
+  async readCarNames() {
+    const input = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    return input.split(",").map((name) => name.trim());
+  }
+  validateCarNames(carNames) {
+    if (carNames.length === 0) {
+      throw new Error("[ERROR] 자동차 이름을 입력해야 합니다.");
+    }
+    carNames.forEach((name) => {
+      if (name.length === 0 || name.length > 5) {
+        throw new Error("[ERROR] 자동차 이름은 1자 이상 5자 이하만 가능합니다.");
+      }
+    });
+  }
+}
+export default App;
