49e60cd701ea16d40bed85f8692c09e86ee68245
yeeun426 <thsudkcla7@naver.com>
2024-10-28 02:40:51 +0900

    feat : 자동차 이름이 없는 경우 예외 처리

--- src/view/CheckValid.js
+++ src/view/CheckValid.js
@@ -3,6 +3,8 @@ class CheckValid {
     carArr.forEach((car) => {
       if (car.length > 5) {
         throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+      } else if (car.trim().length == 0) {
+        throw new Error("[ERROR] 자동차 이름을 1자 이상 입력해주세요.");
       }
     });
   }
