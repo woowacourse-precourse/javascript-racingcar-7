9041d1a90d947e833a607044918fb3f01bd2ca0d
Heesu01 <rulru0413@naver.com>
2024-10-28 00:23:39 +0900

    refactor(App): 에러 처리 추가 및 구조 개선

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import Input from "./Input.js";
 import Race from "./Race.js";
+import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const input = new Input();
-    const carList = await input.getCarNames();
-    const cnt = await input.getCnt();
-    const race = new Race(carList, cnt);
-    race.start();
+    try {
+      const input = new Input();
+      const carList = await input.getCarNames();
+      const cnt = await input.getCnt();
+      const race = new Race(carList, cnt);
+      race.start();
+    } catch (error) {
+      Console.print(error.message);
+      throw error;
+    }
   }
 }
 
