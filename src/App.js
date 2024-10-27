a86fa5c8428ab16ac3d395db992eb22ace7260f0
rhtjddls123 <jjjk0605@naver.com>
2024-10-28 00:30:50 +0900

    feat: 자동차의 이름을 입력받아 쉼표로 구분하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
-    
+    const carsInput = await this.input('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+    const cars = this.separateCar(carsInput);
   }
 
   async input(msg){
     const input = await Console.readLineAsync(`${msg}\n`);
     return input
+  }
+
+  separateCar(cars){
+    return cars.split(',');
   }
 }
 
