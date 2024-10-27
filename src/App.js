8764842e2ffb494f67ec781fc31ba2c92be60db8
minbo <smb5174@naver.com>
2024-10-27 13:35:09 +0900

    refactor: 사용자에게 입력 받은 값에서 양 끝 공백 제거

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const carName = await Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
-    );
+    const carName = (
+      await Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+      )
+    ).trim();
 
-    const tryNumber = await Console.readLineAsync(
-      "시도할 횟수는 몇 회인가요?\n"
-    );
+    const tryNumber = (
+      await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n")
+    ).trim();
   }
 }
 
