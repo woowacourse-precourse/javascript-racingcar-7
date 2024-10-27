04474c5bab098508ee801820e44287ddbdbac659
suubiin <127467411+Suubiin@users.noreply.github.com>
2024-10-28 05:49:15 +0900

    feat: 자동차 이름 입력 검증 기능 추가
    - 입력값이 없는 경우 에러 발생
    - 쉼표만 입력한 경우 에러 발생
    - 6자 이상의 이름 포함 시 에러 발생
    - 공백 포함 시 에러 발생
    - 쉼표와 쉼표 사이에 이름 없는 경우 에러 발생
    - 중복된 이름이 있는 경우 에러 발생

--- src/App.js
+++ src/App.js
@@ -2,22 +2,57 @@ import { Console, Random } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
-    const carNamesInput = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
-    );
-    const carNames = this.splitCarNames(carNamesInput);
+    try {
+      const carNamesInput = await Console.readLineAsync(
+        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+      );
+      this.validateInput(carNamesInput);
+      const carNames = this.splitCarNames(carNamesInput);
+      this.validateCarNames(carNames);
 
-    const attempts = await Console.readLineAsync(
-      '시도할 횟수는 몇 회인가요?\n'
-    );
+      const attempts = await Console.readLineAsync(
+        '시도할 횟수는 몇 회인가요?\n'
+      );
 
-    const cars = carNames.map((name) => ({ name, position: 0 }));
-    this.runRace(cars, attempts);
-    this.printWinners(cars);
+      const cars = carNames.map((name) => ({ name, position: 0 }));
+      this.runRace(cars, attempts);
+      this.printWinners(cars);
+    } catch (error) {
+      Console.print(error.message);
+      throw error;
+    }
+  }
+
+  validateInput(carNamesInput) {
+    if (carNamesInput === '') {
+      throw new Error('[ERROR] 입력된 값이 없습니다.');
+    }
+    if (carNamesInput === ',') {
+      throw new Error('[ERROR] 이름을 필수로 입력해야 합니다.');
+    }
   }
 
   splitCarNames(carNamesInput) {
     return carNamesInput.split(',');
+  }
+
+  validateCarNames(carNames) {
+    const carNameSet = new Set(carNames);
+    if (carNameSet.size !== carNames.length) {
+      throw new Error('[ERROR] 중복된 이름이 존재합니다.');
+    }
+
+    carNames.forEach((car) => {
+      if (car.includes(' ')) {
+        throw new Error('[ERROR] 공백은 입력할 수 없습니다.');
+      }
+      if (car.length > 5) {
+        throw new Error('[ERROR] 이름은 5자 이하만 가능합니다.');
+      }
+      if (car === '') {
+        throw new Error('[ERROR] 쉼표 다음에 반드시 이름을 입력해야 합니다.');
+      }
+    });
   }
 
   runRace(cars, attempts) {
