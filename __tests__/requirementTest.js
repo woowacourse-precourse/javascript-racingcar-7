9ee297e3a723ea110604b41d91034f56a9e4bd55
Jinny <ggab.challenge@gmail.com>
2024-10-27 22:42:15 +0900

    test: 자동차 이름이 빈 값으로 입력시 에러를 던지는 기능 테스트 코드 생성

--- __tests__/requirementTest.js
+++ __tests__/requirementTest.js
@@ -1,8 +1,14 @@
 import splitAndTrimCarName from '../src/services/carNamesTrimmer.js';
+import { validateNotEmpty } from '../src/validations/carNamesValidator.js';
 
 describe('자동차 이름 입력 받기', () => {
   test('쉼표로 구분된 자동차 이름을 배열로 반환한다.', async () => {
     const inputs = 'pobi, jinny';
     expect(splitAndTrimCarName(inputs)).toEqual(['pobi', 'jinny']);
+  });
+
+  test('빈 입력값은 에러를 던진다.', () => {
+    const inputs = '';
+    expect(() => validateNotEmpty(inputs)).toThrow('[ERROR]');
   });
 });
