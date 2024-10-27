0b47d95d31e34de8d74dc24130956e319cda6890
이은호 <eunho@ieunhoui-MacBookAir.local>
2024-10-27 19:49:06 +0900

    refactor : checkWinner 함수명 변경 및 리팩토링

--- src/outputs/checkWinner.js
+++ src/outputs/checkWinner.js
@@ -1,23 +1,22 @@
 import { Console } from "@woowacourse/mission-utils";
 
-export function checkWinner(cars) {
-    const maxScore = getMaxDistance(cars);
-    const finalWinner = determineWinners(cars, maxScore);
-  
-    Console.print(`최종 우승자 : ${finalWinner.join(", ")}`);
+export function checkWinners(cars) {
+  Console.print(
+    `최종 우승자 : ${getWinners(cars, getMaxDistance(cars)).join(", ")}`
+  );
+}
+
+function getMaxDistance(cars) {
+  return cars.reduce(compareDistance, "");
+}
+
+function compareDistance(maxScore, car) {
+  if (maxScore.length < car.distance.length) {
+    return car.distance;
   }
-  
-  function getMaxDistance(cars) {
-    return cars.reduce(compareDistance, "");
-  }
-  
-  function compareDistance(maxScore, car) {
-    if (maxScore.length < car.distance.length) {
-      return car.distance;
-    }
-    return maxScore;
-  }
-  
-  function determineWinners(cars, maxScore) {
-    return cars.filter((car) => car.distance === maxScore).map((car) => car.name);
-  }
+  return maxScore;
+}
+
+function getWinners(cars, maxScore) {
+  return cars.filter((car) => car.distance === maxScore).map((car) => car.name);
+}
