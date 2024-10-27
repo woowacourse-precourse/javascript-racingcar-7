bf7e7288d25fc9c08015f7e0a3db5fd534a67edc
parkyugyeong <qkrb8019@gmai.com>
2024-10-28 08:59:31 +0900

    feat : 5. 결과출력 기능구현

--- src/controller/CarRacingController.js
+++ src/controller/CarRacingController.js
@@ -1,12 +1,56 @@
+import InputValidator from "../utils/InputValidator.js";
 import InputView from "../view/InputView.js";
+import OutputView from "../view/OutputView.js";
+import RaceManager from "../domain/RaceManager.js";
 
-class CarRacingController {
+export default class CarRacingController {
+  #raceManager;
+
+  constructor() {
+    this.#raceManager = new RaceManager();
+  }
+
   async play() {
-    const carNames = await InputView.readCarNames();
-    const attempts = await InputView.readAttempts();
+    try {
+      await this.#initializeGame();
+      await this.#raceLoop();
+      this.announceWinners();
+    } catch (error) {
+      throw error;
+    }
+  }
 
-    // 추후에 여기서 입력값을 처리하는 메서드를 연결할 예정입니다.
+  async #initializeGame() {
+    await this.#initializeCars();
+    await this.#initializeAttempts();
+  }
+
+  async #initializeCars() {
+    const carNamesInput = await InputView.readCarNames();
+    const carNames = InputValidator.validateCarNames(carNamesInput);
+    this.#raceManager.createCars(carNames);
+  }
+
+  async #initializeAttempts() {
+    const attemptsInput = await InputView.readAttempts();
+    const validatedAttempts = InputValidator.validateAttempts(attemptsInput);
+    this.#raceManager.setAttempts(validatedAttempts);
+  }
+
+  async #raceLoop() {
+    const attempts = this.#raceManager.getAttempts();
+    for (let i = 0; i < attempts; i++) {
+      await this.#processRaceRound();
+    }
+  }
+
+  async #processRaceRound() {
+    this.#raceManager.moveAllCars();
+    OutputView.printRaceStatus(this.#raceManager.getCars());
+  }
+
+  announceWinners() {
+    const winners = this.#raceManager.findWinners();
+    OutputView.printWinners(winners);
   }
 }
-
-export default CarRacingController;
