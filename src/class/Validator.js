a8c2c7e696badd75c0e38792fe638c92090eff41
정해성 <gotjd0230@naver.com>
2024-10-27 16:05:58 +0900

    refactor: Validator 규칙 분리

--- src/class/Validator.js
+++ src/class/Validator.js
@@ -1,26 +1,38 @@
 import { ERROR_MESSAGES } from '../constants/errorMessages';
 
 class Validator {
+  static rules = {
+    isShorterThanTwo: (str) => str.length < 2,
+    hasDuplication: (str) => new Set(str).size !== str.length,
+    isEmpty: (str) => str === '',
+    hasSpacing: (str) => str.includes(' '),
+    isLongerThanFive: (str) => str.length > 5,
+    isNotNaturalNumber: (number) => number.toString().includes('.'),
+    isNotNumber: (number) => !Number.isInteger(number),
+    isLessThanOne: (number) => number < 1,
+    exceedsMaxDigits: (number) => number.toString().length > 15,
+  };
+
   static validateName(nameString) {
     const names = nameString.split(',');
 
-    if (names.length < 2) throw new Error(ERROR_MESSAGES.name.MIN_NAMES);
-    if (new Set(names).size !== names.length) throw new Error(ERROR_MESSAGES.name.DUPLICATEED);
+    if (this.rules.isShorterThanTwo(names)) throw new Error(ERROR_MESSAGES.name.MIN_NAMES);
+    if (this.rules.hasDuplication(names)) throw new Error(ERROR_MESSAGES.name.DUPLICATEED);
 
     names.some((name) => {
-      if (name === '') throw new Error(ERROR_MESSAGES.name.EMPTY);
-      if (name.includes(' ')) throw new Error(ERROR_MESSAGES.name.HAS_SPACING);
-      if (name.length > 5) throw new Error(ERROR_MESSAGES.name.MAX_LENGTH);
+      if (this.rules.isEmpty(name)) throw new Error(ERROR_MESSAGES.name.EMPTY);
+      if (this.rules.hasSpacing(name)) throw new Error(ERROR_MESSAGES.name.HAS_SPACING);
+      if (this.rules.isLongerThanFive(name)) throw new Error(ERROR_MESSAGES.name.MAX_LENGTH);
     });
 
     return names;
   }
 
   static validateAttempt(attempts) {
-    if (attempts < 1) throw new Error(ERROR_MESSAGES.count.MIN_ATTEMPTS);
-    if (isNaN(attempts)) throw new Error(ERROR_MESSAGES.count.NOT_NUMBER);
-    if (attempts.toString().includes('.')) throw new Error(ERROR_MESSAGES.count.NOT_NATURAL_NUMBER);
-    if (attempts.toString().length > 15) throw new Error(ERROR_MESSAGES.count.MAX_LENGTH);
+    if (this.rules.isNotNaturalNumber(attempts)) throw new Error(ERROR_MESSAGES.count.NOT_NATURAL_NUMBER);
+    if (this.rules.isNotNumber(attempts)) throw new Error(ERROR_MESSAGES.count.NOT_NUMBER);
+    if (this.rules.isLessThanOne(attempts)) throw new Error(ERROR_MESSAGES.count.MIN_ATTEMPTS);
+    if (this.rules.exceedsMaxDigits(attempts)) throw new Error(ERROR_MESSAGES.count.MAX_LENGTH);
 
     return attempts;
   }
--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -7,9 +7,9 @@ export const ERROR_MESSAGES = {
     MAX_LENGTH: '[ERROR] 5자 이내의 이름을 입력해주세요.',
   },
   count: {
-    MIN_ATTEMPTS: '[ERROR] 1 이상의 시도 횟수를 입력해주세요.',
-    NOT_NUMBER: '[ERROR] 시도 횟수에 숫자만 입력해주세요.',
     NOT_NATURAL_NUMBER: '[ERROR] 시도 횟수에 자연수를 입력해주세요.',
+    NOT_NUMBER: '[ERROR] 시도 횟수에 숫자만 입력해주세요.',
+    MIN_ATTEMPTS: '[ERROR] 1 이상의 시도 횟수를 입력해주세요.',
     MAX_LENGTH: '[ERROR] 15자리수 이내의 시도 횟수를 입력해주세요.',
   },
 };
