63c63d2c270153a97e2c0c6bfb3906985fa64ae5
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-10-27 17:51:29 +0900

    refactor(ALL_method): 모든 메서드들을 App 클래스 외부에 정의하도록 리팩토링

--- src/App.js
+++ src/App.js
@@ -24,9 +24,21 @@ class Car {
   }
 }
 
-function getNames(str) {
+async function getNameInput() {
+  const input = Console.readLineAsync(
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+  );
+  return input;
+}
+
+async function getNames(str) {
   const names = str.split(',');
   return names;
+}
+
+async function getTrial() {
+  const num = Console.readLineAsync(`시도할 횟수는 몇 회인가요?\n`);
+  return num;
 }
 
 function checkNameInput(arr) {
@@ -48,11 +60,10 @@ function checkNameInput(arr) {
 }
 
 function checkCountInput(input) {
-  const count = Number(input);
   if (isNaN(input)) {
     throw new Error('[ERROR] 실행 횟수 입력은 숫자여야 합니다.');
   } else {
-    if (count % 1 === 0 && count > 0) return count;
+    if (input % 1 === 0 && input > 0) return true;
     else throw new Error('[ERROR] 실행 횟수 입력은 1 이상의 정수여야 합니다.');
   }
 }
@@ -80,33 +91,38 @@ function whoIsWinner(cars) {
   Console.print(`최종 우승자 : ${winnerNames.join(', ')}`);
 }
 
+function getCars(names) {
+  const cars = names.map((name) => {
+    return new Car(name);
+  });
+  return cars;
+}
+
+function race(trial, cars) {
+  for (let i = 0; i < trial; i++) {
+    cars.forEach((car) => {
+      car.forwardOrNot();
+      car.getProgress();
+    });
+    Console.print('');
+  }
+  return cars;
+}
+
 class App {
   async run() {
-    const inputNames = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
-    );
-    const names = getNames(inputNames);
+    const inputNames = await getNameInput();
+    const names = await getNames(inputNames);
     checkNameInput(names);
 
-    const inputCount = await Console.readLineAsync(
-      `시도할 횟수는 몇 회인가요?\n`
-    );
-    const trial = checkCountInput(inputCount);
+    const trial = Number(await getTrial());
+    checkCountInput(trial);
 
-    //TODO: 정상적인 입력 시 수행할 것 (자동차 배열 만들기)
-    const cars = names.map((name) => {
-      return new Car(name);
-    });
+    const cars = getCars(names);
 
     Console.print('\n실행 결과');
 
-    for (let i = 0; i < trial; i++) {
-      cars.forEach((car) => {
-        car.forwardOrNot();
-        car.getProgress();
-      });
-      Console.print('');
-    }
+    race(trial, cars);
 
     whoIsWinner(cars);
   }
