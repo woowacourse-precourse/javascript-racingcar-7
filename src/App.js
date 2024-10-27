df8c5d286ae58dbe722a6d7454211dd36f9d1719
zzzzzuuuuu <agu0830@naver.com>
2024-10-27 14:26:55 +0900

    feat: 자동차 이름 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import {Console} from "@woowacourse/mission-utils";
+import {Input} from "./Input.js";
+
+
 class App {
-  async run() {}
+  async run() {
+    const input = new Input();
+    const cars = await input.getInput();
+    Console.print(cars);
+  }
 }
 
 export default App;
---
+++ src/Input.js
@@ -0,0 +1,25 @@
+import { Console } from "@woowacourse/mission-utils";
+
+export class Input {
+  async getInput() {
+    Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분")
+    const input = await Console.readLineAsync("");
+    const cars = input.split(",").map(car => car.trim());
+
+    this.validateCarNames(cars);
+
+    return cars;
+  }
+
+  validateCarNames(cars) {
+    if (cars.length < 0 || cars.length > 9) {
+      Console.print("올바른 자동차 개수를 입력해주세요.");
+    }
+
+    cars.forEach(car => {
+      if (car.length < 0 || car.length > 6) {
+        Console.print("자동차 이름은 1자 이상 5자 이하로 입력할 수 있습니다.");
+      }
+    });
+  }
+}
