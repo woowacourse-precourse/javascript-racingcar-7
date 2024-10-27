e2c7c968bd877d26d24986d56552c7a3c0292b0c
COMPOSEDKIM <composedkim@naver.com>
2024-10-28 02:42:50 +0900

    feat: 사용자에게 시도할 횟수 입력받기

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const userCars = this.getCarName();
+    const userCars = await this.getCarName();
+    const tryRaceNum = await this.getTryRaceNum();
   }
 
   async getCarName() {
@@ -10,6 +11,16 @@ class App {
       const carname = await MissionUtils.Console.readLineAsync(
         "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
       );
+      return carname;
+    } catch (error) {}
+  }
+
+  async getTryRaceNum() {
+    try {
+      const tryRaceNum = await MissionUtils.Console.readLineAsync(
+        "시도할 횟수는 몇 회인가요?\n"
+      );
+      return tryRaceNum;
     } catch (error) {}
   }
 }
