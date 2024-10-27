7b29592df7c8b848f30f36f07cfc9bf13e36d0d4
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-10-27 21:22:40 +0900

    docs: 출력값  띄어쓰기 제거

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Console, Random } from "@woowacourse/mission-utils";
 class App {
   async run() {
     Console.print(
-      "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)"
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
     const INPUT = await Console.readLineAsync("");
 
