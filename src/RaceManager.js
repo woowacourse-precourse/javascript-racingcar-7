1e4d33fca8645791e6d5964cfe5abbf98e17fcd7
kanghaeun <xgkg0330@jnu.ac.kr>
2024-10-28 00:38:13 +0900

    feat: 자동차 전진 가능 여부 판단 기능 추가

--- src/RaceManager.js
+++ src/RaceManager.js
@@ -5,8 +5,13 @@ class RaceManager {
     this.name = name;
     this.position = 0;
   }
+
   createRandomNumber() {
     return MissionUtils.Random.pickNumberInRange(0, 9);
+  }
+
+  canMoveForward(randomNumber) {
+    return randomNumber >= 4;
   }
 }
 
