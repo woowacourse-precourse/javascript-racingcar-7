f37d15b4d2dc0bab53aab621b91ffe823f8f1e98
YongHyunKing <yhkim8246@gmail.com>
2024-10-28 04:28:05 +0900

    feat(App): 주어진 횟수가 문자열인 경우에도 에러 발생 시키기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import RacingCar from './RacingCar';
+
 class App {
   async run() {
     const NAMES = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-    const TIMES = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    const TIMES = Number(await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n'));
 
-    if (!Number.isInteger(TIMES) || TIMES <= 0) throw new Error('[Error] 시도할 횟수는 양의 정수만 가능합니다.');
+    if (isNaN(TIMES) || !Number.isInteger(TIMES) || TIMES <= 0) throw new Error('[Error] 시도할 횟수는 양의 정수만 가능합니다.');
 
     const splitNames = NAMES.split(',').map(name => name.trim());
 
     const cars = [];
     for (carName of splitNames) cars.push(new RacingCar(carName));
+
   }
 }
 
