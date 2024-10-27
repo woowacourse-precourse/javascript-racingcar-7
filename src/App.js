a3214ab5e26dd30c4dacbf652ef23a714bb32aa7
Won JongIn <wonggamggik@naver.com>
2024-10-28 03:25:29 +0900

    feat: Step0 - Processing initial inputs for vehicle name and attempt
    - Implemented Console to receive car names and attempt count as input
    - Added code to verify correct input handling and display

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const carNames = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분)\n"
+    );
+
+    const attempts = await Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    );
+
+    Console.print(`step0 - 입력 자동차: ${carNames}`);
+    Console.print(`step0 - 입력 시도: ${attempts}`);
+  }
 }
 
 export default App;
