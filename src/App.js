6bc51a38348c5d3221bb9987e1a6dd3772e48b65
YongHyunKing <yhkim8246@gmail.com>
2024-10-28 03:47:48 +0900

    feat(App): 자동차 이름, 주어진 횟수 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
 class App {
-  async run() {}
+  async run() {
+    const NAMES = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const TIMES = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+  }
 }
 
 export default App;
