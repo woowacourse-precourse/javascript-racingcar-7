a7de85923561bb9624f735b0462157ce4e5c43f0
0Jaemin0 <jaemin2167@naver.com>
2024-10-27 21:28:46 +0900

    feat: 시도할 횟수 입력에 대한 에러 메시지 추가

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -4,6 +4,8 @@ const ERROR_MESSAGES = {
   LESS_THAN_TWO_CARS: '두 대 이상의 자동차 이름을 입력해야 합니다.',
   DUPLICATE_CAR_NAME: '자동차 이름은 중복될 수 없습니다.',
   CAR_NAME_TOO_LONG: '자동차 이름은 5자를 초과할 수 없습니다.',
+  NOT_A_NUMBER: '시도할 횟수는 정수를 입력해야 합니다. ',
+  LESS_THAN_ONE: '시도할 횟수는 1 이상을 입력해야 합니다.',
 };
 
 export default ERROR_MESSAGES;
