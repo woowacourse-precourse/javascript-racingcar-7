fd2be3b89ab9644e63877602348dc7fd6050e65e
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-10-27 10:35:49 +0900

    docs: Car 클래스에 의미 있는 주석 추가
    - 이름 길이 검증, move 메서드의 기능을 설명하는 주석 추가
    - 코드의 의도를 명확히 전달하여 가독성 개선

--- src/Car.js
+++ src/Car.js
@@ -1,14 +1,14 @@
 class Car {
   constructor(name) {
     if (name.length > 5) {
-      throw new Error('[ERROR]');
+      throw new Error('[ERROR]'); // 이름 길이가 5자를 초과하면 에러 발생
     }
     this.name = name;
     this.position = 0;
   }
 
   move() {
-    this.position++;
+    this.position++; // 자동차의 위치를 1 증가시킴
   }
 
   getName() {
