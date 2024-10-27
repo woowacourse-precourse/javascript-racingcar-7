fc724d839c348a3bcd644e072141557f14277671
luii0415 <becky45210193@gmail.com>
2024-10-27 22:50:11 +0900

    feat : create
    자동차 이동에 따른 출력 기능 완성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Console, MissionUtils } from "@woowacourse/mission-utils";
 class App {
+  // 현재 자동차 이동 유무 함수까지 (rolling_num 포함) commit 했음 + 아래 가이드용 주석까지
+  // 이후 이동횟수만큼 - 출력하는 함수 완성(아마도 완성되었음..?) 해서 커밋하고 아래 for문 loop 완성해서 출력하기
   async run() {
     const rolling_number = () => {
       return MissionUtils.Random.pickNumberInRange(0, 9);
@@ -16,7 +18,12 @@ class App {
       }
     };
     // 이동 횟수만큼 -를 출력하는 함수
-
+    const print_points = (car_names, cars_score) => {
+      for (let i = 0; i < car_names.length; i++) {
+        let point = "-".repeat(cars_score[i]);
+        Console.print(`${car_names[i]} : ${point}`);
+      }
+    };
     // 자동차 이름 입력 및 이동 횟수 입력
     Console.print(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
@@ -24,7 +31,7 @@ class App {
     let name_input = await Console.readLineAsync("");
     let car_names = name_input.split(",");
     car_names = car_names.map((name) => name.trim());
-    let cars_score = [0] * car_names.length;
+    let cars_score = new Array(car_names.length).fill(0);
     // 에러 조건 1 : 입력된 이름이 5자를 넘어가는가?
     for (let name of car_names) {
       if (name.length > 5) {
@@ -35,8 +42,10 @@ class App {
     const loop = await Console.readLineAsync("");
 
     // 자동차 이동
-    for (i = 0; i < loop; i++) {
-      // plus point 호출
+    for (let i = 0; i < loop; i++) {
+      plus_point(car_names);
+      print_points(car_names, cars_score);
+      Console.print("");
       // print point 호출 (전진한 거리만큼 - 호출하는 함수)
     }
   }
