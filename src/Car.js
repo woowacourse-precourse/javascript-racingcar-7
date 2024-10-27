60117d9cc4fea694afcb37e84551ecbc16d629f2
boxi <boxiyoun@gmail.com>
2024-10-27 23:45:29 +0900

    생성자로 차 만들기, 랜덤 숫자 만들기, 움직이는 조건 만들기

---
+++ src/Car.js
@@ -0,0 +1,17 @@
+import { Random } from "@woowacourse/mission-utils";
+
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.position = "";
+  }
+  getRandomNumber() {
+    const randomNumber = Random.pickNumberInRange(0, 9);
+    return randomNumber;
+  }
+  move() {
+    const randomNumber = this.getRandomNumber();
+    if (randomNumber >= 4) { this.position += "-"; }
+  }
+}
+export default Car;
