7df07995acd7c7038d9868d7fbab5afd7c338f4d
moonseonghui <msh9980@naver.com>
2024-10-27 17:08:27 +0900

    test(validateRacingCarInput): validateRacingCarInput 함수의 유효하지 않은 문자열 테스트 케이스 추가

--- __tests__/validator/validateRacingCarInput.test.js
+++ __tests__/validator/validateRacingCarInput.test.js
@@ -1,4 +1,5 @@
 import { validateRacingCarInput } from '../../src/validator/validatePipeline';
+import { ERROR_MESSAGE } from '../../src/constants/messages';
 
 // 유효한 문자열 테스트 케이스
 test.each([
@@ -8,3 +9,28 @@ test.each([
 ])('[유효한 문자열 검사] %s : %s', (description, input) => {
   expect(validateRacingCarInput(input)).toBe(input);
 });
+
+// 유효하지 않은 문자열 테스트 케이스
+test.each([
+  [
+    '알파벳과 쉼표 외의 문자가 포함된 경우(숫자)',
+    'carA,carB,car3',
+    ERROR_MESSAGE.INVALID_CAR_INPUT,
+  ],
+  [
+    '알파벳과 쉼표 외의 문자가 포함된 경우(특수 문자)',
+    'car,carA.carC',
+    ERROR_MESSAGE.INVALID_CAR_INPUT,
+  ],
+  [
+    '알파벳과 쉼표 외의 문자가 포함된 경우(한글)',
+    'car,carA,자동차',
+    ERROR_MESSAGE.INVALID_CAR_INPUT,
+  ],
+  ['입력이 비어있는 경우', '', ERROR_MESSAGE.NULL_INPUT],
+])(
+  '[유효하지 않은 문자열 검사] %s : %s',
+  (description, input, expectedError) => {
+    expect(() => validateRacingCarInput(input)).toThrow(expectedError);
+  },
+);
