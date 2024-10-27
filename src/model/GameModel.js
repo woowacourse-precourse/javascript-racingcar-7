6093be681fda30bb556cf835d83c27a7ee4eb2f9
ryanbae94 <ryanbae94@gmail.com>
2024-10-28 01:35:28 +0900

    feat (GameModel): 라운드 별 결과 업데이트
    - cars로 반복문을 돌아 각 자동차를 move하고 반복문이 끝난 뒤 현재 라운드를 1만큼 더함

--- src/model/GameModel.js
+++ src/model/GameModel.js
@@ -3,8 +3,17 @@ import CarModel from './CarModel';
 class GameModel {
 	#cars;
 	#rounds;
+	#currentRound;
 	constructor(cars, rounds) {
 		this.#cars = cars;
 		this.#rounds = rounds;
+		this.#currentRound = 0;
+	}
+
+	runRound() {
+		this.#cars.forEach((car, _) => {
+			car.move();
+		});
+		this.#currentRound++;
 	}
 }
