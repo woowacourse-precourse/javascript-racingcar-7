5023b673e3285dc702cee796243d6ddf5d7cead4
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-10-27 19:27:17 +0900

    feat : 결과 메세지 상수화

--- src/util/constant.js
+++ src/util/constant.js
@@ -3,7 +3,10 @@ export const START_MESSAGE = Object.freeze({
     '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
   TRY_NUMBER_INPUT: '시도할 횟수는 몇 회인가요?\n',
 });
-
+export const RESULT_MESSAGE = Object.freeze({
+  EXECUTE: '\n실행결과',
+  WINNER: '최종 우승자 :',
+});
 export const TRYNUMBER_ERROR = Object.freeze({
   NOT_NUMBER: '[ERROR] 입력하신 시도 횟수가 숫자가 아닙니다.',
   NOT_OVER_50: '[ERROR] 입력하신 시도 횟수가 50을 초과합니다.',
