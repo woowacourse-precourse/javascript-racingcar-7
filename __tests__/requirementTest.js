c5d55d2a69787ead7eef621c195888fb45d38dcb
Jinny <ggab.challenge@gmail.com>
2024-10-28 00:40:09 +0900

    test: 경기 횟수에 문자열 입력시 에러를 던지는 기능 테스트 코드 생성

--- __tests__/requirementTest.js
+++ __tests__/requirementTest.js
@@ -4,6 +4,7 @@ import {
   validateStringInput,
   validateArrNotDuplication,
 } from '../src/validations/carNamesValidator.js';
+import { isRacingCountTypeNumber } from '../src/validations/racingCountValidator.js';
 
 describe('자동차 이름 입력 받기', () => {
   test('쉼표로 구분된 자동차 이름을 배열로 반환한다.', async () => {
@@ -16,6 +17,7 @@ describe('자동차 이름 입력 받기', () => {
     expect(splitAndTrimCarName(inputs)).toEqual(['pobi', 'jinny']);
   });
 
+  // TO-DO: refactor, - [style]
   test('빈 입력값은 에러를 던진다.', () => {
     const inputs = '';
     expect(() => validateNotEmpty(inputs)).toThrow('[ERROR]');
@@ -29,5 +31,12 @@ describe('자동차 이름 입력 받기', () => {
   test('자동차 이름이 중복되면 에러를 던진다.', () => {
     const inputs = 'pobi, jinny, jinny';
     expect(() => validateArrNotDuplication(inputs)).toThrow('[ERROR]');
+  });
+});
+
+describe('경기 횟수 입력 받기', () => {
+  test('경기 횟수에 문자열을 입력하면 에러를 던진다.', () => {
+    const input = 'FiveTimes';
+    expect(() => isRacingCountTypeNumber(input)).toThrow('[ERROR] 숫자가 아닙니다. ');
   });
 });
