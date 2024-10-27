34ecb2da5af2f5da5a1c31004c3c52cf1d0aa1c8
silver93hq <gkagjsrb@gmail.com>
2024-10-27 19:17:15 +0900

    test: 테스트 케이스 변수 수정

--- __tests__/AcceptedTest.js
+++ __tests__/AcceptedTest.js
@@ -1,18 +1,16 @@
+import { VALUES } from '../src/constants/values.js';
 import { expectResult } from '../src/utils/testUtils.js';
-
-const MOVING_FORWARD = 4;
-const STOP = 3;
 
 describe('정상 작동하는 App 테스트', () => {
   test.each([
     {
       inputs: ['a,b,c', '1'],
-      randomValues: [MOVING_FORWARD, MOVING_FORWARD, STOP],
+      randomValues: [VALUES.moveIfOrMore, VALUES.moveIfOrMore, VALUES.moveIfOrMore - 1],
       logs: ['a : -', 'b : -', 'c : ', '최종 우승자 : a, b'],
     },
     {
       inputs: ['b,a,c', '1'],
-      randomValues: [MOVING_FORWARD, MOVING_FORWARD, STOP],
+      randomValues: [VALUES.moveIfOrMore, VALUES.moveIfOrMore, VALUES.moveIfOrMore - 1],
       logs: ['b : -', 'a : -', 'c : ', '최종 우승자 : b, a'],
     },
   ])('공동 우승 케이스', async ({ inputs, randomValues, logs }) => {
@@ -21,12 +19,12 @@ describe('정상 작동하는 App 테스트', () => {
   test.each([
     {
       inputs: ['pobi,woni,jun', '1'],
-      randomValues: [MOVING_FORWARD, STOP, STOP],
+      randomValues: [VALUES.moveIfOrMore, VALUES.moveIfOrMore - 1, VALUES.moveIfOrMore - 1],
       logs: ['pobi : -', 'woni : ', 'jun : ', '최종 우승자 : pobi'],
     },
     {
       inputs: ['pobi,woni,jun', '1'],
-      randomValues: [STOP, MOVING_FORWARD, STOP],
+      randomValues: [VALUES.moveIfOrMore - 1, VALUES.moveIfOrMore, VALUES.moveIfOrMore],
       logs: ['pobi : ', 'woni : -', 'jun : ', '최종 우승자 : woni'],
     },
   ])('단독 우승 케이스', async ({ inputs, randomValues, logs }) => {
