a7259b826e8cad0cf65b294f78cae48983e561b8
Won JongIn <wonggamggik@naver.com>
2024-10-28 04:49:38 +0900

    docs: Add JSDoc comments for InputHandler methods
    - Added JSDoc comments to parseCarNames and parseAttempts methods in InputHandler to improve code readability and provide better context on input parameters and return types.

--- src/InputHandler.js
+++ src/InputHandler.js
@@ -2,8 +2,8 @@
 class InputHandler {
   /**
    * 쉼표로 구분된 자동차 이름을 배열로 분리
-   * @param {*} input
-   * @returns
+   * @param {string} input 자동차 이름들
+   * @returns {Array} 자동차 이름들을 ","를 기준으로 나눈 배열
    */
   parseCarNames(input) {
     const carNames = input.split(",").map((name) => name.trim());
@@ -12,8 +12,8 @@ class InputHandler {
 
   /**
    * 시도 횟수를 숫자로 변환
-   * @param {*} input
-   * @returns
+   * @param {string} input 시도 횟수
+   * @returns 숫자로 변환된 시도 횟수
    */
   parseAttempts(input) {
     const attempts = Number(input);
