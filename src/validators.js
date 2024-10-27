221e828ebed32102167b6dd36ff644061df1fb68
Daeuning <happyjde3@gmail.com>
2024-10-28 02:21:47 +0900

    refactor(validators): 유효성 검사 함수 리팩토링

--- src/validators.js
+++ src/validators.js
@@ -1,25 +1,37 @@
 import { throwError, splitByDelimiter } from './utils.js';
 import { ERROR_MESSAGE } from './constants.js'; 
 
+export const isEmpty = (input) => !input;
+
+export const isMaxLength = (input, maxLength) => input.length <= maxLength;
+
+export const hasDuplicates = (arr) => new Set(arr).size !== arr.length;
+
+export const isPositiveInteger = (input) => /^[1-9]\d*$/.test(input);
+
+
 const validateCarName = (input) => {
-  if (!input) {
+  if (isEmpty(input)) {
     throwError(ERROR_MESSAGE.EMPTY_INPUT);
   }
 
-  splitByDelimiter(input) 
-  .filter((name, i, arr) => {
-    if (name.length > 5) throwError(ERROR_MESSAGE.INVALID_INPUT);
-    if (arr.indexOf(name) !== i) throwError(ERROR_MESSAGE.DUPLICATE_INPUT);
-    return true;
-  });  
+  const carNames = splitByDelimiter(input);
+
+  if (!carNames.every(name => isMaxLength(name, 5))) {
+    throwError(ERROR_MESSAGE.INVALID_INPUT);
+  }
+
+  if (hasDuplicates(carNames)) {
+    throwError(ERROR_MESSAGE.DUPLICATE_INPUT);
+  }
 };
 
 const validateAttemptCount = (input) => {
-  if (!input) {
+  if (isEmpty(input)) {
     throwError(ERROR_MESSAGE.EMPTY_INPUT);
   }
 
-  if (!/^[1-9]\d*$/.test(input)) {
+  if (!isPositiveInteger(input)) {
     throwError(ERROR_MESSAGE.INVALID_ATTEMPTS_COUNT);
   }
 
