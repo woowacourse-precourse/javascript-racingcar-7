eb2c97b1cbc316851121c2b8fcfb06a8e81dc931
boxi <boxiyoun@gmail.com>
2024-10-27 23:48:52 +0900

    값 입력 받는 변수명 변경

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App {
   async run() {
     try {
       const inputCarName = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-      const attemptNumber = await Console.readLineAsync('시도할 횟수를 입력하세요.\n');
+      const inputAttemptNumber = await Console.readLineAsync('시도할 횟수를 입력하세요.\n');
     } catch (error) {
       Console.print(error.message);
       throw error;
---
+++ src/Racing.js
@@ -0,0 +1,8 @@
+import { Console } from "@woowacourse/mission-utils";
+import Car from './Car.js';
+
+class Racing {
+  constructor(inputCarName, inputAttemptNumber) {
+
+  }
+}
