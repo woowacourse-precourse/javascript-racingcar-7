ab447d03f081bb7201aaf402ec879c9cbf78cc2c
MERx88 <wjang07@gmail.com>
2024-10-27 10:11:35 +0900

    Feat(App): add feature to read user car names
    - Add readCarNames() function to receive car names for the car racing
    - Utilize readLineAsync() from @woowacourse/mission-utils to handle car name input.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async readCarNames() {
+    const userInput = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    return userInput;
+  }
+
+  async run() {
+    const output = await this.readCarNames();
+    Console.print(output);
+  }
 }
 
 export default App;
