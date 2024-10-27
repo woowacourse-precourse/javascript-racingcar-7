628007ad5f8d86c57dd820824cd64a53eb043b6e
elect10 <wlgnsakswp@naver.com>
2024-10-28 08:17:12 +0900

    chore(app): app to import game

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import GetInput from './io/GetInput.js';
+import StartGame from './play/StartGame.js';
 
 class App {
   async run() {
     try{
       const carNames = await GetInput.getCarNames();
       const tryCount = await GetInput.getTryCount();
+      StartGame.start(carNames, tryCount);
     } catch (error) {
       // 에러 처리
     }
