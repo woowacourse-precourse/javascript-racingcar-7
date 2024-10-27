79b3c3125c9aab2a699650e851008e468a56506b
DongjaJ <jdh981118@naver.com>
2024-10-27 23:21:26 +0900

    test: 자동차 이름 입력중 중복된 이름 예외 테스트

--- __tests__/validation.spec.js
+++ __tests__/validation.spec.js
@@ -2,8 +2,16 @@ import InputValidation from "../src/validation.js";
 
 describe("validation 클래스 테스트", () => {
   describe("자동차 이름 유효성 검사", () => {
-    it("입력받은 자동차의 길이가 5자보다 길면 에러를 반환한다.", () => {
+    it("입력받은 자동차 이름의 길이가 5자보다 길면 에러를 반환한다.", () => {
       const mockCarNameList = ["dongdong"];
+
+      expect(() =>
+        InputValidation.validateCarNameList(mockCarNameList)
+      ).toThrow("[ERROR]");
+    });
+
+    it("입력받은 자동차 이름 리스트 중 중복된 이름이 있다면 에러를 반환한다.", () => {
+      const mockCarNameList = ["dong", "dong"];
 
       expect(() =>
         InputValidation.validateCarNameList(mockCarNameList)
