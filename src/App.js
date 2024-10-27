f512209ef843d476fac88c79a15bc13f3ea49e49
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-10-27 13:58:42 +0900

    feat: 입력 초기화 기능 추가
    자동차이름 및 시도횟수 초기화
    
    - 자동차이름 초기화: initializeCarNames()
    - 시도횟수 초기화: initializeTryCount()

--- src/App.js
+++ src/App.js
@@ -16,7 +16,22 @@ import { runRaceRound } from './raceLogic/runRaceRound.js';
 import { getWinners } from './output/getWinners.js';
 
 class App {
-  async run() {}
+  async run() {
+    const CAR_NAMES = await this.initializeCarNames();
+    const TRY_COUNT = await this.initializeTryCount();
+
+  async initializeCarNames() {
+    const PARSED_CAR_NAMES = await getCarNames();
+    this.validateCarNames(PARSED_CAR_NAMES);
+    return PARSED_CAR_NAMES;
+  }
+
+  async initializeTryCount() {
+    const TRY_COUNT = await getTryCount();
+    this.validateTryCount(TRY_COUNT);
+    return TRY_COUNT;
+  }
+
 }
 
 export default App;
