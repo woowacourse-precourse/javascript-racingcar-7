e29c6336c4dd2e82c2da1b026e2620c3e95f6d7d
ZUITOPIA <korhwngml123@naver.com>
2024-10-27 17:11:50 +0900

    feat(APP): 경주할 자동차 이름 입력받고 유효성 검사

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+    async getCarNameList() {
+        const carName = await Console.readLineAsync("경주할 자동차 이름을 쉼표로 구분하여 입력하세요 : ");
+        const carNameList = carName.split(",");
+
+        if (carNameList.some(name => name.length > 5)) {
+            throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+        }
+        return carNameList;
+    }
+
+    async run() {
+        const result = await this.getCarNameList();
+        Console.print(result);
+    }
 }
 
 export default App;
