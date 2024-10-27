67e8a6c129c220f51fdef7e6adedb73abba1bbad
elect10 <wlgnsakswp@naver.com>
2024-10-28 08:15:32 +0900

    fix(io): fix typo

--- src/io/GetInput.js
+++ src/io/GetInput.js
@@ -2,12 +2,12 @@ import { Console } from '@woowacourse/mission-utils';
 
 class GetInput {
   static async getCarNames() {
-    const carNames = await Console.readLineAsync('경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분) \n');
+    const carNames = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n');
     return carNames.split(',');
   }
 
   static async getTryCount() {
-    return await Console.readLineAsync('시도할 횟수는 몇회인가요? \n');
+    return await Console.readLineAsync('시도할 횟수는 몇 회인가요? \n');
   }
 }
 
