bcc005118be38b0d5af09597455dab2bfca8c0d9
ParkjuYoung <kwn03111@gmail.com>
2024-10-27 17:21:23 +0900

    feat (race.js) 자동차 경주 클래스 및 기능 구현
    - 자동차 경주를 시작해 자동차 상태를 변경하는 기능
    - 경주가 끝났는지 확인하는 기능
    - 현재 라운드의 상태를 출력하는 기능
    - 경기의 승자를 반환하는 기능

---
+++ src/model/race.js
@@ -0,0 +1,54 @@
+import { randomNumber } from '../utils/randomNumberMaker.js';
+import { DEFAULT_OBJECT_VALUES, RACE } from '../utils/constants.js';
+import { printRaceRound, printCarStats, printLineSpace } from '../view/raceView.js';
+
+export class Race {
+  constructor(carList, raceMaxRound) {
+    this.carList = [...carList];
+    this.raceCurrentRound = DEFAULT_OBJECT_VALUES;
+    this.raceMaxRound = raceMaxRound;
+  }
+
+  roundStart() {
+    this.carList.forEach((car) => {
+      if (RACE.RACE_MOVE_CONDITION <= randomNumber(RACE.MIN_RANDOM_NUMBER, RACE.MAX_RANDOM_NUMBER)) {
+        car.move();
+      }
+    });
+    this.raceCurrentRound++;
+  }
+
+  isRaceOver() {
+    if (this.raceMaxRound <= this.raceCurrentRound) {
+      return false;
+    }
+    return true;
+  }
+
+  showRaceStatusByRound() {
+    printLineSpace();
+    printRaceRound(this.raceCurrentRound);
+    this.carList.forEach((car) => {
+      printCarStats(car.getName(), car.getLocation());
+    });
+  }
+
+  getFinalWinner() {
+    let furthestLocation = DEFAULT_OBJECT_VALUES;
+    let winner = [];
+
+    this.carList.forEach((car) => {
+      if (furthestLocation < car.getLocation()) {
+        furthestLocation = car.getLocation();
+      }
+    });
+
+    this.carList.forEach((car) => {
+      if (furthestLocation == car.getLocation()) {
+        winner.push(car.getName());
+      }
+    });
+
+    return winner;
+  }
+}
