2b0f3d6fe249a423fcc503e485c9e3de3ed6e045
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-28 05:50:40 +0900

    ✨ feat(input): 사용자에게 자동차 이름 입력받는 기능 구현
    - `readUserInput` 함수를 사용하여 사용자로부터 자동차 이름을 입력받는 기능을 구현했습니다.
    - `MESSAGE.PROMPT_NAME_USER_INPUT` 상수를 통해 입력을 안내했습니다.
    - 입력받은 값을 `input` 변수에 저장하여 이후 로직에서 활용 가능하도록 구현했습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { MESSAGE } from './constant';
+import { readUserInput } from './util/io.js';
+
 class App {
-  async run() {}
+  async run() {
+    const input = await readUserInput(MESSAGE.PROMPT_NAME_USER_INPUT);
+  }
 }
 
 export default App;
