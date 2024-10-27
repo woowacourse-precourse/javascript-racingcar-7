8ddc314a4eeae938f868076686fc28505f4fa75a
immaemi <nam14785@soongsil.ac.kr>
2024-10-27 22:37:15 +0900

    refactor: Format code with Prettier and change variable names

--- src/App.js
+++ src/App.js
@@ -2,41 +2,41 @@ import { Console, MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    let input = await Console.readLineAsync(
+    let carNamesInput = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
 
-    let cars = input.split(",").map((name) => name.trim());
+    let carNames = carNamesInput.split(",").map((name) => name.trim());
 
-    for (let i = 0; i < cars.length; i += 1) {
-      if (cars[i].length > 5) {
+    for (let i = 0; i < carNames.length; i += 1) {
+      if (carNames[i].length > 5) {
         throw new Error(`[ERROR] 이름은 5글자 이하로 입력해주세요.`);
-      }
-      else if(cars[i].length === 0){
-        throw new Error(`[ERROR] 이름이 입력되지 않았습니다.`)
+      } else if (carNames[i].length === 0) {
+        throw new Error(`[ERROR] 이름이 입력되지 않았습니다.`);
       }
     }
 
-    let numberOfMatches = await Console.readLineAsync(
+    let roundCount = await Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?\n"
     );
 
-    if(Number.isNaN(Number(numberOfMatches))){
-      throw new Error(`[ERROR] ${numberOfMatches}는 숫자가 아닙니다.`)
+    if (Number.isNaN(Number(roundCount))) {
+      throw new Error(`[ERROR] ${roundCount}는 숫자가 아닙니다.`);
+    } else if (
+      Number(roundCount) === 0 ||
+      Number(roundCount) === " "
+    ) {
+      throw new Error(`[ERROR] 1 이상의 숫자를 입력해주세요.`);
     }
-    else if(Number(numberOfMatches) === 0 || Number(numberOfMatches) ===" "){
-      throw new Error(`[ERROR] 1 이상의 횟수를 입력해주세요.`);
-    }
-
 
     Console.print("\n실행 결과");
 
-    let positions = cars.map((car) => ({ name: car, position: "" }));
-    for (let i = 0; i < numberOfMatches; i += 1) {
-      positions.forEach((car) => {
-        let randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+    let carStates = carNames.map((car) => ({ name: car, position: "" }));
+    for (let i = 0; i < roundCount; i += 1) {
+      carStates.forEach((car) => {
+        let moveDecision = MissionUtils.Random.pickNumberInRange(0, 9);
 
-        if (randomNumber >= 4) {
+        if (moveDecision >= 4) {
           car.position += "-";
         }
         Console.print(car.name + " : " + car.position);
@@ -44,12 +44,13 @@ class App {
       Console.print("");
     }
 
-    let maxPosition = Math.max(...positions.map(car => car.position.length));
-    let winners = positions.filter(car => car.position.length === maxPosition);
+    let maxDistance = Math.max(...carStates.map((car) => car.position.length));
+    let winningCars = carStates.filter(
+      (car) => car.position.length === maxDistance
+    );
 
-    Console.print("최종 우승자 : " + winners.map(car => car.name).join(', '));
+    Console.print("최종 우승자 : " + winningCars.map((car) => car.name).join(", "));
   }
 }
 
 export default App;
- 
