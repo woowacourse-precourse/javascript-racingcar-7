f3b11b2b9e506c65ac7590db4b45a8dd9b0167a6
dawn0314 <sdawn0314@gmail.com>
2024-10-27 11:23:23 +0900

    feat: get user Input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    let carNamesInput = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+
+    let tryCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+
+    Console.print(`${carNamesInput} ${tryCount}`);
+  }
 }
 
 export default App;
