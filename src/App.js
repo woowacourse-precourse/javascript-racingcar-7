c21efb840260d5653924da8a80fd7855b833e1ae
rhtjddls123 <jjjk0605@naver.com>
2024-10-28 00:56:24 +0900

    feat: 차량이름 입력 함수로 분리, 이름 유효성 검사 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class App {
+  static ERROR_MESSAGE = {
+    CAR_NAME_LENGTH_ERROR:
+      '차량 이름이 너무 깁니다! 각 차량 이름은 5글자 이하로 입력해주세요!',
+  };
+
   async run() {
-    const carsInput = await this.input(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
-    );
-    const cars = this.separateCar(carsInput);
+    const cars = this.inputCars();
   }
 
   async input(msg) {
@@ -13,8 +15,25 @@ class App {
     return input;
   }
 
+  async inputCars() {
+    const carsInput = await this.input(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+    );
+    const cars = this.separateCar(carsInput);
+
+    if (!this.isCarNameValid(cars)) {
+      throw Error(`[ERROR] ${App.ERROR_MESSAGE.CAR_NAME_LENGTH_ERROR}`);
+    }
+
+    return cars;
+  }
+
   separateCar(cars) {
     return cars.split(',');
+  }
+
+  isCarNameValid(cars) {
+    return cars.every(car => car.length <= 5);
   }
 }
 
