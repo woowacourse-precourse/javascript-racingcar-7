d373cb08f083a7e7b96c5002fcc19bac8358cb2b
seunghyeon77 <jdyjsh77@naver.com>
2024-10-27 15:58:33 +0900

    feat: app.js에 실행 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { Console } from "@woowacourse/mission-utils";
+import { RacingGame } from "./RacingGame";
 
 class App {
   async run() {
     const CARNAMES = await Console.readLineAsync("자동차 이름을 입력하세요 : ")
-      .trim("")
+      .trim()
       .split(",");
     const ROUNDS = parseInt(
       await Console.readLineAsync("이동 횟수를 입력하세요 : "),
       10
     );
+    const RESULT = new RacingGame(CARNAMES, ROUNDS);
+    RESULT.play();
   }
 }
 
