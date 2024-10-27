fa186c59e6a73a4a0e150d0563afad52d89c0c28
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-10-27 20:45:59 +0900

    feat(outputHandler.js): 결과 출력 함수

--- src/views/outputHandler.js
+++ src/views/outputHandler.js
@@ -1,6 +1,16 @@
 import { Console } from "@woowacourse/mission-utils";
+
+import { GAME_MESSAGES } from "../constants/index.js";
+
 export const outputHandler = {
   printMessage: (message) => {
     return Console.print(message);
+  },
+
+  printRoundResult: (cars) => {
+    cars.forEach((car) => {
+      const progress = GAME_MESSAGES.PROGRESS_BAR.repeat(car.getCarPosition());
+      Console.print(`${car.getCarName()} : ${progress}`);
+    });
   },
 };
