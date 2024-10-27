c39cb9ffbcf687a594033d2a8e0fe997ea966740
전성모 <jeonsm22@daum.net>
2024-10-27 19:09:13 +0900

    feat : 차 이름 길이가 5 이하인지 확인하는 함수 추가

--- src/functions.js
+++ src/functions.js
@@ -1,4 +1,13 @@
 export function splitComma(string){
     let commaSplitList = string.split(",");
+    isFiveLength(commaSplitList);
     return commaSplitList;
+}
+
+function isFiveLength(list){
+    list.forEach((carName) => {
+        if(carName.length > 5){
+            throw new Error("[ERROR] 차 이름 길이가 5를 초과했습니다.")
+        }
+    })
 }
