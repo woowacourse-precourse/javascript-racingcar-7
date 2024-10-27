eb6ae9e101b75c6681d0fb80fb18aeea2db064b3
mun-kyeong <horse6953@naver.com>
2024-10-27 13:36:22 +0900

    [FEAT] (raceCar.js) : "실행결과" 출력
    - 게임이 시작되었음을 알리는 "실행결과" 출력
    - constant 상수로 선언하여 호출하는 방식
    
    사용자로부터 입력을 다 받은 후 "실행결과" 메시지를 출력합니다

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import inputCarList from "./Components/Input/inputCarList.js";
 import inputTryNum from "./Components/Input/inputTryNum.js";
+import raceCar from "./Components/RaceCar/raceCar.js";
 
 class App {
   async run() {
     try {
       const carList = await inputCarList();
       const tryNumber = await inputTryNum();
+      const result = raceCar(carList, tryNumber);
     } catch (error) {
       throw new Error(error.message);
     }
---
+++ src/Components/RaceCar/raceCar.js
@@ -0,0 +1,7 @@
+import { Console } from "@woowacourse/mission-utils";
+import { GIDE_MESSAGE } from "../../Constants/constant.js";
+
+const raceCar = () => {
+  Console.print(GIDE_MESSAGE.result);
+};
+export default raceCar;
