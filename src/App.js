919716375c60ad6672af98c22cf44b20cc3f7e95
mun-kyeong <horse6953@naver.com>
2024-10-27 13:02:47 +0900

    [REFACTOR] (inputCarList.js) : 상수 전달 방식 수정
    - (수정)
    App.js 에서 inputCarList 로 GIDE_MESSAGE 를 전달하던 방식을
    inputCarList 에서 GIDE_MESSAGE 를 바로 호출하는 방식으로 변경
    
    - (사유)
    - inputCarList 와 inputTryNum이라는 함수 이름에서 역할이 드러난다고 판단
    -  각 함수 안에서 직접 상수를 호출함으로써 상수를 전달하는 깊이(depth)를 줄일수 있다고 판단
    
    상수 전달 방식을 각 input 함수에서 직접호출하는 방식으로 수정했습니다.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import { GIDE_MESSAGE } from "./Constants/constant.js";
 import inputCarList from "./Components/Input/inputCarList.js";
 import inputTryNum from "./Components/Input/inputTryNum.js";
 
 class App {
   async run() {
     try {
-      const carList = await inputCarList(GIDE_MESSAGE.carList);
+      const carList = await inputCarList();
       const tryNumber = await inputTryNum();
     } catch (error) {
       throw new Error(error.message);
--- src/Components/Input/inputCarList.js
+++ src/Components/Input/inputCarList.js
@@ -1,7 +1,8 @@
+import { GIDE_MESSAGE } from "../../Constants/constant.js";
 import PrintGideMessage from "../Output/PrintGideMessage.js";
 
-const inputCarList = async (gideMessage) => {
-  const carList = PrintGideMessage(gideMessage);
+const inputCarList = async () => {
+  const carList = PrintGideMessage(GIDE_MESSAGE.carList);
   return carList;
 };
 export default inputCarList;
