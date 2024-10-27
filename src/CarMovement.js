855ee9e9a4e3caa5a5238eff87b8780466872354
Yun-Jinwoo <yjw3480@naver.com>
2024-10-28 00:51:42 +0900

    ♻️ refactor: 함수 모듈화 - RandomMove.js: 랜덤한 값에 따라 전진하는 함수 구현

--- src/CarMovement.js
+++ src/CarMovement.js
@@ -1,10 +1,8 @@
-import { Random } from "@woowacourse/mission-utils"
+import randomMove from "./RandomMove.js";
+
 const updateCarDistances = distances => {
     for (let j = 0; j < distances.length; j++) {
-        let randInt = Random.pickNumberInRange(0, 9);
-        if (randInt >= 4) { // 4 이상인 경우 자동차 전진
-            distances[j]++;
-        }
+        randomMove(distances, j);
     }
 };
 
