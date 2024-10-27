47f4d829b1eeb25f32867fbc34545273406d628b
조승우 <choseung97@gmail.com>
2024-10-27 23:15:53 +0900

    refactor(validator): 직접 입력된 정규표현식을 resources 폴더로 분리한다
    resources/RegularExpression.js에 정규 표현식을 모아서 관리한다.

--- src/utils/validation/validateNames.js
+++ src/utils/validation/validateNames.js
@@ -1,12 +1,13 @@
 import ErrorMessage from '../../resources/ErrorMessage.js';
+import RegularExpression from '../../resources/RegularExpression.js';
 
 function validateOneName(names) {
-  if (names.match(/^[^,]{1,5}$/))
+  if (names.match(RegularExpression.REGEX_SINGLE_NAME))
     throw new Error(ErrorMessage.MIN_TWO_NAMES_REQUIRED);
 }
 
 function validateLongName(names) {
-  if (names.match(/^[^,]+(,[^,]+)*$/))
+  if (names.match(RegularExpression.REGEX_LONG_NAME))
     throw new Error(ErrorMessage.NAME_LENGTH_EXCEEDED);
 }
 
@@ -16,7 +17,7 @@ function validateCommaEnding(names) {
 
 export default function validateNames(names) {
   if (!names) throw new Error(ErrorMessage.EMPTY_NAMES_NOT_ALLOW);
-  if (!names.match(/^[^,]{1,5}(,[^,]{1,5})+$/)) {
+  if (!names.match(RegularExpression.REGEX_VALID_NAMES_FORMAT)) {
     validateOneName(names);
     validateLongName(names);
     validateCommaEnding(names);
--- src/utils/validation/validateRepetitionString.js
+++ src/utils/validation/validateRepetitionString.js
@@ -1,12 +1,13 @@
 import ErrorMessage from '../../resources/ErrorMessage.js';
+import RegularExpression from '../../resources/RegularExpression.js';
 
 function validateNotNumber(repetitionString) {
-  if (repetitionString.match(/[^0-9]+/))
+  if (repetitionString.match(RegularExpression.REGEX_NOT_NUMBER))
     throw new Error(ErrorMessage.INVALID_NUMBER_INPUT);
 }
 
 function validateZeroNumber(repetitionString) {
-  if (repetitionString.match(/^[0]$/))
+  if (repetitionString.match(RegularExpression.REGEX_ONLY_ZERO))
     throw new Error(ErrorMessage.GREATER_THAN_ZERO);
 }
 
@@ -17,7 +18,7 @@ function validateZeroStarting(repetitionString) {
 
 export default function validateRepetitionString(repetitionString) {
   if (!repetitionString) throw new Error(ErrorMessage.EMPTY_NAMES_NOT_ALLOW);
-  if (!repetitionString.match(/^[1-9](\d+)*$/)) {
+  if (!repetitionString.match(RegularExpression.REGEX_VALID_NUMBER_FORMAT)) {
     validateNotNumber(repetitionString);
     validateZeroNumber(repetitionString);
     validateZeroStarting(repetitionString);
