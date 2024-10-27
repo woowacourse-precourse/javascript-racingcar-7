c5ea2b23c20c5674f7074ce51d8b9556d62c91c3
조혜린 <hyer1205@gmail.com>
2024-10-27 09:57:16 +0900

    Feat(Validator): 자동차 이름 중 숫자로 시작하는 이름이 있는 경우 에러 처리
    Validator 클래스에 숫자로 이름이 시작하는 경우 에러 처리하는 메서드 정의

--- src/Validator.js
+++ src/Validator.js
@@ -11,6 +11,10 @@ class Validator {
         '자동차의 이름은 한글, 숫자, 영어로만 구성되어야 합니다.',
       );
     }
+
+    if (this.isStartsWithNumber(carNames)) {
+      throw new Error('자동차 이름은 숫자로 시작할 수 없습니다.');
+    }
   }
 
   isNameLengthValid(carNames) {
@@ -19,6 +23,10 @@ class Validator {
 
   isCarNameValid(carNames) {
     return carNames.every(name => /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]*$/.test(name));
+  }
+
+  isStartsWithNumber(carNames) {
+    return carNames.some(name => /^\d/.test(name));
   }
 }
 
