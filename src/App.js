521ead18e3e7f4c9ea1bdde91240229bb32ceb9b
Yena Kim <120614713+ynkim-03@users.noreply.github.com>
2024-10-28 00:37:17 +0900

    feat: run 메서드에서 InputValidator, RaceGame 인스턴스 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import { Random, Console } from '@woowacourse/mission-utils';
+import { Console } from '@woowacourse/mission-utils';
+import InputValidator from './InputValidator.js';
+import RaceGame from "./RaceGame.js";
 
 class App {
   async run() {
     const carNames = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
     const raceCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+
+    const validator = new InputValidator(carNames);
+    const race = new RaceGame(carNames, raceCount);
   };
 }
 
