ff572f0ffad68b6935a15f11b52951c783c95143
함윤식 <43927226+Ham3798@users.noreply.github.com>
2024-10-27 19:27:26 +0900

    feat: 입출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    MissionUtils.Console.print(MissionUtils.Random.pickNumberInRange(0, 9));
+
+    const carNamesInput = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+
+    const attemptsInput = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    if (!this.isValidAttempts(attemptsInput)) {
+      throw new Error("[ERROR] Invalid attempts number");
+    }
+
+    const carNames = carNamesInput.split(',');
+    const attempts = parseInt(attemptsInput, 10);
+    const gameLogs = this.playGame(carNames, attempts);
+
+    gameLogs.forEach((log) => {
+      MissionUtils.Console.print(log);
+    });
+  }
+
+  playGame(carNames, attempts) {
+    return ["pobi : -", "woni : ", "최종 우승자 : pobi"];
+  }
+
+  isValidAttempts(attempts) {
+    const number = parseInt(attempts, 10);
+    return !isNaN(number) && number > 0;
+  }
 }
 
 export default App;
