20ebadf433b1bf2defd3158b14eb821949fe3025
0Jaemin0 <jaemin2167@naver.com>
2024-10-28 01:18:46 +0900

    refactor: handleError 함수 반영

--- src/CarValidator.js
+++ src/CarValidator.js
@@ -1,4 +1,5 @@
 import ERROR_MESSAGES from './constants/errorMessages.js';
+import handleError from './utils/handleError.js';
 
 class CarValidator {
   static #splitString(inputString) {
@@ -7,32 +8,32 @@ class CarValidator {
 
   static #isEmptyCarName(cars) {
     if (cars.some((car) => car === '')) {
-      throw new Error(ERROR_MESSAGES.EMPTY_CAR_NAME);
+      handleError(ERROR_MESSAGES.EMPTY_CAR_NAME);
     }
   }
 
   static #containWhitespaceInCarName(cars) {
     if (cars.some((car) => car.includes(' '))) {
-      throw new Error(ERROR_MESSAGES.CONTAINS_WHITESPACE);
+      handleError(ERROR_MESSAGES.CONTAINS_WHITESPACE);
     }
   }
 
   static #isLessThanTwoCars(cars) {
     if (cars.length < 2) {
-      throw new Error(ERROR_MESSAGES.LESS_THAN_TWO_CARS);
+      handleError(ERROR_MESSAGES.LESS_THAN_TWO_CARS);
     }
   }
 
   static #hasDuplicateCarName(cars) {
     const uniqueCars = new Set(cars);
     if (uniqueCars.size < cars.length) {
-      throw new Error(ERROR_MESSAGES.DUPLICATE_CAR_NAME);
+      handleError(ERROR_MESSAGES.DUPLICATE_CAR_NAME);
     }
   }
 
   static #isCarNameTooLong(cars) {
     if (cars.some((car) => car.length > 5)) {
-      throw new Error(ERROR_MESSAGES.CAR_NAME_TOO_LONG);
+      handleError(ERROR_MESSAGES.CAR_NAME_TOO_LONG);
     }
   }
 
