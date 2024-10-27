e06eed439012be09d90d8e5d2b3128f530bf6cfa
최영빈 <bin778@naver.com>
2024-10-27 15:13:55 +0900

    feat(Check): 한글, 영어, 숫자, 특정 특수문자인지 검사

--- src/Component/Check.js
+++ src/Component/Check.js
@@ -1,4 +1,10 @@
-import { ERR_LENGTH, ERR_POSITIVE, ERR_ISDUP, ERR_ISINT } from "./Error.js";
+import {
+  ERR_LENGTH,
+  ERR_COR_NAME,
+  ERR_POSITIVE,
+  ERR_ISDUP,
+  ERR_ISINT,
+} from "./Error.js";
 
 function isDuplicate(arr) {
   const isDup = arr.some(function (x) {
@@ -14,10 +20,15 @@ export const checkCarNames = (car_names_arr) => {
     (str) => str.length <= 0 || str.length >= 6
   );
 
+  // 입력한 값이 한글, 영어, 숫자, 특수문자(.^*#!;)만 있는지 검사
+  const reg = /^[ㄱ-ㅎ가-힣a-zA-Z0-9.^*#!;]+$/;
+  const str_reg_check = car_names_arr.every((str) => reg.test(str));
+
   // 자동차 이름에 중복이 있는지 검사
   const car_name_dup = isDuplicate(car_names_arr);
 
   if (str_len_check) throw new Error(ERR_LENGTH());
+  if (!str_reg_check) throw new Error(ERR_COR_NAME());
   if (car_name_dup) throw new Error(ERR_ISDUP());
 };
 
--- src/Component/Error.js
+++ src/Component/Error.js
@@ -4,6 +4,12 @@ export const ERR_LENGTH = () => {
   return ERR_MESSAGE + " 자동차 이름은 1~5자 사이로 입력해야 합니다.";
 };
 
+export const ERR_COR_NAME = () => {
+  return (
+    ERR_MESSAGE + " 한글, 영어, 숫자, 특수문자(.^*#!;)만 입력이 가능합니다."
+  );
+};
+
 export const ERR_ISDUP = () => {
   return ERR_MESSAGE + " 자동차 이름은 각각 달라야 합니다.";
 };
