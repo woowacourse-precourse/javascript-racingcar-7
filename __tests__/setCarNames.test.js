be3dce21ddb66142e621b327b9d0dcdd0c8fc304
Jaeho Cha <ischar@naver.com>
2024-10-27 14:09:53 +0900

    test(carList): add unit tests for setCarNames
    - setCarNames 유닛 테스트 추가
      - 정상적으로 값을 할당하고 Car 리스트를 반환하는 지 검증
      - 문자열 리스트가 공백일 때 에러를 발생시키는 지 검증

---
+++ __tests__/setCarNames.test.js
@@ -0,0 +1,21 @@
+import { setCarNames } from "../src/features/setCarNames";
+import { MESSAGES } from "../src/config/config";
+import { Car } from "../src/models/Car";
+
+describe("setCarNames 테스트", () => {
+  test("정상적으로 값을 할당하고, Car 리스트를 반환하는 지 확인", () => {
+    const INPUT = ["pobi", "woni", "jun"];
+    const carList = setCarNames(INPUT);
+
+    carList.forEach((car, index) => {
+      expect(car).toBeInstanceOf(Car);
+      expect(car.name).toBe(INPUT[index]);
+      expect(car.forwardCount).toBe(0);
+    });
+  });
+
+  test("문자열 리스트가 공백일 때 에러를 발생시키는 지 확인", () => {
+    const INPUT = [];
+    expect(() => setCarNames(INPUT)).toThrow(MESSAGES.ERROR_INPUT_EMPTY);
+  });
+});
