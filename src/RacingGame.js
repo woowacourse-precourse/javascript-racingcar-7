aebecfd1c3732757bc4f6f9b1ad476e44fb0ef4b
조혜린 <hyer1205@gmail.com>
2024-10-27 15:36:16 +0900

    Refactor(racinggame): RacingGame 클래스의 메서드들 SRP 지키도록 리팩토링
    play() 메서드에서 이동한 결과를 출력하는 코드 printCarMovement()로 분리
    printResult() 메서드에서 자동차들의 최대 이동 횟수를 구하는 로직을 Math.max() 메서드를 사용하도록 수정

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -1,29 +1,36 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class RacingGame {
-  constructor(cars, tryCnt) {
+  constructor(cars, moveAttempts) {
     this.cars = cars;
-    this.tryCnt = tryCnt;
+    this.moveAttempts = moveAttempts;
   }
 
   play() {
     Console.print('\n실행 결과');
-    for (let i = 0; i < this.tryCnt; i += 1) {
+
+    for (let i = 0; i < this.moveAttempts; i += 1) {
       this.cars.forEach(car => {
         car.moveForward();
-        Console.print(
-          `${car.getCarName()} : ${'-'.repeat(car.getMoveForwardCnt())}`,
-        );
       });
+      this.printCarMovement();
+
       Console.print('\n');
     }
   }
 
+  printCarMovement() {
+    this.cars.forEach(car => {
+      Console.print(
+        `${car.getCarName()} : ${'-'.repeat(car.getMoveForwardCnt())}`,
+      );
+    });
+  }
+
   printResult() {
-    const maxMoveForward = this.cars.reduce((acc, curr) => {
-      if (acc < curr.getMoveForwardCnt()) return curr.getMoveForwardCnt();
-      return acc;
-    }, 0);
+    const maxMoveForward = Math.max(
+      ...this.cars.map(car => car.getMoveForwardCnt()),
+    );
 
     Console.print(
       `최종 우승자 : ${this.cars
