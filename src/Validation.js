21119617acc0519f8251fb3f2e2fab5bdab0a47e
Heesu01 <rulru0413@naver.com>
2024-10-27 23:55:21 +0900

    feat(Validaion): 입력값 유효성 검사 추가

---
+++ src/Validation.js
@@ -0,0 +1,23 @@
+class Validation {
+  static validateCarNames(carList) {
+    carList.forEach((name) => {
+      // 자동차 이름이 5자 이상일 경우
+      if (name.length > 5) {
+        throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+      }
+    });
+    // 자동차 이름이 없을 경우
+    if (carList.length === 0 || carList.some((name) => name === "")) {
+      throw new Error("[ERROR] 자동차 이름을 입력해야 합니다.");
+    }
+  }
+
+  // 경주 횟수가 0이하일 경우
+  static validateCnt(cnt) {
+    if (isNaN(cnt) || cnt <= 0) {
+      throw new Error("[ERROR] 시도할 횟수는 1 이상의 숫자여야 합니다.");
+    }
+  }
+}
+
+export default Validation;
