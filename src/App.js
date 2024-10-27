aa1ebcf77d97d7ec962f97fa0180697ce82624c9
jiHeeFlee <esder1310@naver.com>
2024-10-27 15:19:52 +0900

    feat(App.js) : 3. declare function for generate random integer / assign variables

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Console} from "@woowacourse/mission-utils"
+import {Console, MissionUtils} from "@woowacourse/mission-utils"
 
 class App{
   async run(){
@@ -11,10 +11,17 @@ class App{
 
     // 2. 게임을 몇 번 할건지 입력 받기(이후 라운드라고 표현)
     const number_input=await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+    // 3. 랜덤 정수 변수 할당
+    let random_numbers=[];
 
     // 1. 자동차 입력 : 입력받은 데이터를 쉼표(,)로 구분하여 리스트 요소로 저장
     if(car_input.includes(",")){
       car_names=car_input.split(",");
+    }
+
+    // 3. 게임에서 사용할 랜덤한 정수 함수 선언
+    function random_number(){
+      return MissionUtils.Random.pickNumberInRange(0,9);
     }
   }
 }
