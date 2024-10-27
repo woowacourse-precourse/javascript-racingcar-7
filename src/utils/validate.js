b279423ceaa05ca827e05418b4253ba47175670e
RohSungKyun <songkyun1108@naver.com>
2024-10-28 02:38:02 +0900

    feat : 유효성 검사 자동차 이름 조건 추가

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -8,11 +8,28 @@ class Validate{
   }
 
   nameLength(input){
-    const cars = input.split(',');
+    const cars = input.split(',').map(car => car.trim());
     for (let car of cars){
-      if(car.length>5){
+      if(car.length>5 || car.length < 1){
         throw new Error(ERROR.INPUT_STRING_SIZE_ERROR);
       }
+    }
+  }
+  minimumNumber(input) {
+    const carList = input.split(',');
+    if(carList.length === 1){
+      throw new Error("[ERROR] 최소 두 개 이상의 차를 입력하세요");
+    }
+  }
+  hasDuplicateName(input) {
+    const noSpaces = input.split(' ').join('');
+    const carList = noSpaces.split(',');
+    const cars = new Set();
+    for (const carName of carList){
+      if(cars.has(carName)) {
+        throw new Error("[ERROR] 이름은 중복되지 않고 공백 없이 입력하세요");
+      }
+      cars.add(carName);
     }
   }
   countType(input){
@@ -29,6 +46,8 @@ class Validate{
   carValidate(input){
     this.inputExist(input);
     this.nameLength(input);
+    this.minimumNumber(input);
+    this.hasDuplicateName(input);
   }
 
   countValidate(input){
