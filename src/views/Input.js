0bb448dcbd61b6f251ee0fd1031b3286747f9823
Eunicekk <cho_lyn@naver.com>
2024-10-27 23:30:00 +0900

    [refactor] 삼항연산자 제거

--- src/views/Input.js
+++ src/views/Input.js
@@ -4,7 +4,8 @@ export class Input {
 	static async getCarNames() {
 		Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 		const input = Console.readLineAsync("");
-		return input ? input.split(",") : [];
+		if (!input) return [];
+		return input.split(",");
 	}
 
 	static async getMoveCount() {
