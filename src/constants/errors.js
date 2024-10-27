7f11e36f6f4293fff50d438382ace85c55a9b55c
CJY <cjy3458@naver.com>
2024-10-28 04:56:30 +0900

    feat: 에러처리용 커스텀 에러 구현

---
+++ src/constants/errors.js
@@ -0,0 +1,10 @@
+const ERROR_TAG = '[ERROR]';
+
+const ERROR = {
+  INVALID_INPUT: {
+    name: 'InvalidInputError',
+    message: `${ERROR_TAG} 자동차 목록이 유효하지 않습니다.`,
+  },
+};
+
+export default ERROR;
