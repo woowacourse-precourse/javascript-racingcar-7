55bb431c17960b3cc55aca7a9cac487bbbb49ebd
Hina0222 <hina0222@naver.com>
2024-10-28 02:23:23 +0900

    feat: 문자열 입력 받기 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const CARS_INPUT = await MissionUtils.Console.readLineAsync("경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분)\n");
+    const TRY_INPUT = await MissionUtils.Console.readLineAsync("시도할 횟수\n");
+
+  }
 }
 
 export default App;
