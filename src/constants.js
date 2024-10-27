b6b7b45c00073ec40e9178ce2cc45f5ce8f1bc26
dawn0314 <sdawn0314@gmail.com>
2024-10-27 11:40:58 +0900

    feat: add constants

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+const ERROR_MESSAGE = {
+  LENGTH_OVER_INPUT: "[ERROR] 자동차 이름을 5자 이하로 입력해주세요.",
+  EMPTY_NAME_INPUT: "[ERROR] 자동차 이름을 입력해주세요.",
+  OVER_TEN_INPUT: "[ERROR] 최대 9개의 이름을 입력해주세요.",
+  EMPTY_TRY_INPUT: "[ERROR] 시도할 횟수를 입력해주세요.",
+  NAN_TRY_INPUT: "[ERROR] 시도할 횟수를 1 이상의 정수로 입력해주세요.",
+};
+
+export { ERROR_MESSAGE };
