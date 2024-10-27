c56aff850f1643119267210d936358d874396b61
jjang-aaa <jadoit473@gmail.com>
2024-10-27 17:17:30 +0900

    refactor(carRacingProceeding): runCarRacing함수를 생성해서 carRacingProceed함수 분리
    함수를 작게 하기 위해서 따로 분리
    시도횟수 입력받고, 검증하고, 실행해서 결과를 보여주는 것으로 자동차 경주를 하는 runCarRacing함수를 생성

--- src/CarRacingGame/carRacingProceeding.js
+++ src/CarRacingGame/carRacingProceeding.js
@@ -6,16 +6,7 @@ import { Car } from './car.js';
 
 export async function carRacingProceed() {
   const carList = await creatCarList();
-
-  const tryCount = await inputTryCount();
-  validateTryCount(tryCount);
-
-  Console.print('\n실행 결과');
-
-  for (let i = 0; i < tryCount; i++) {
-    carList.forEach((car) => car.move());
-    printExecutionResult(carList);
-  }
+  await runCarRacing(carList);
 
   const maxMoveNum = Math.max(...carList.map((car) => car.number));
   printWinner(carList, maxMoveNum);
@@ -32,4 +23,16 @@ async function creatCarList() {
   }
 
   return carList;
+}
+
+async function runCarRacing(carList) {
+  const tryCount = await inputTryCount();
+  validateTryCount(tryCount);
+
+  Console.print('\n실행 결과');
+
+  for (let i = 0; i < tryCount; i++) {
+    carList.forEach((car) => car.move());
+    printExecutionResult(carList);
+  }
 }
