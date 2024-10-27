d47795cd1c4d1dbe78b1d52d14f73f22ce099582
sanghyeonLee <hyeon012366@gmail.com>
2024-10-28 03:52:54 +0900

    feat: 프롬프트 순서에 따른 출력 기능 추가

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -1,34 +1,34 @@
 import {Console, MissionUtils} from "@woowacourse/mission-utils";
 import {PROMPT_MESSAGE} from "./constants/message.js";
-import {setWinnerCnt, sliceString} from "./preprocessingInputData.js";
 import {Car} from "./Car.js";
 
 //입력 -> 전처리 -> 레이싱 -> 우승자 산정 -> 출력
 class RacingGame {
     promptSequence = 1
     carArr = []
+
     async start(promptMessage) {
         const prompt = await this.input(promptMessage)
-        if(this.promptSequence === 1){
+        if (this.promptSequence === 1) {
             this.carArr = new Car().setCar(prompt)
             this.promptSequence++
             return this.start(PROMPT_MESSAGE.SECOND)
         }
-        if(this.promptSequence === 2){
+        if (this.promptSequence === 2) {
             const tryNum = Number(prompt)
             this.totalRace(tryNum)
             this.promptSequence++
         }
+        const result = this.setWinner()
+        this.output(result)
     }
 
     totalRace(tryNum) {
-       // this.output("실행결과")
+        // this.output("실행결과")
         for (let i = 0; i < tryNum; i++) {
             this.individualRace(this.carArr)
-            this.carArr.map((car) => this.output(`${car.carName} : ${"-".repeat(car.winCnt)}`))
-            this.output("\n")
+            this.output()
         }
-        this.setWinner(this.carArr)
     }
 
     individualRace(carArr) {
@@ -40,13 +40,12 @@ class RacingGame {
         }
     }
 
-    setWinner(carArr) {
-        const winner = carArr
+    setWinner() {
+        return this.carArr
             .sort((a, b) => b.winCnt - a.winCnt)
-            .filter((elem) => elem.winCnt === carArr[0].winCnt)
+            .filter((elem) => elem.winCnt === this.carArr[0].winCnt)
             .map((elem) => elem.carName)
             .join(", ")
-        this.output(`최종 우승자 : ${winner}`)
     }
 
     setRandomNum() {
@@ -59,17 +58,23 @@ class RacingGame {
         return input
     }
 
-    output(message) {
-        return Console.print(message)
+    output(data) {
+        if (this.promptSequence === 2) {
+            this.carArr.map((car) => Console.print(`${car.carName} : ${"-".repeat(car.winCnt)}`))
+            Console.print("\n")
+        }
+        if (this.promptSequence === 3) {
+            Console.print(`최종 우승자 : ${data}`)
+        }
     }
 
-    isError(string){
+    isError(string) {
         const firstErrorCondition = string.includes(" ") || string.length > 5
         const secondErrorCondition = string.includes(" ") || isNaN(string)
-        if(this.promptSequence === 1 && firstErrorCondition){
+        if (this.promptSequence === 1 && firstErrorCondition) {
             throw new Error("[ERROR]: 잘못된 값을 입력 하셨습니다.")
         }
-        if(this.promptSequence === 2 && secondErrorCondition){
+        if (this.promptSequence === 2 && secondErrorCondition) {
             throw new Error("[ERROR]: 잘못된 값을 입력 하셨습니다.")
         }
     }
