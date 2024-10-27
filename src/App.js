93d7284729211b92fa24abdf637f388f47250aa2
@u_limit_ <yurim@ajou.ac.kr>
2024-10-27 18:33:50 +0900

    feat: 이름을 쉼표로 구분해 입력 받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from '@woowacourse/mission-utils'
+
+const determineCarName = (input) => {
+  const names = input.split(',');
+}
+
 class App {
-  async run() {}
+  async run() {
+  const input = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\\n');
+  determineCarName(input);
+  const raceNumber = await  Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+  }
 }
 
 export default App;
