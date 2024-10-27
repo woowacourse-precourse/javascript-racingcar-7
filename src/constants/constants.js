b035567289efbadf92bb70cd31b6ddf86c1de3a6
leelsj2007 <leelsj2007@gmail.com>
2024-10-28 01:21:13 +0900

    feat : 입출력 상수 작성

---
+++ src/constants/constants.js
@@ -0,0 +1,9 @@
+export const INPUT_MESSAGE = {
+  carName : "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n" ,
+  numberOfAttempts : "시도할 횟수는 몇 회인가요?\n" ,
+};
+
+export const OUTPUT_MESSAGE = {
+  executionResult : "\n실행 결과\n" ,
+  finalWinner : "\n최종 우승자 : " ,
+};
