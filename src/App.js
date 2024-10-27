2028046c228e68ff418a48697530f95fad82cf5d
Jimin Lee <jmncrk04@gmail.com>
2024-10-27 10:38:23 +0900

    feat : 입력 메서드 구현
    - 참가할 자동차 입력 및 반환
    - 시도할 횟수 입력 및 반환

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+// racing.js
+import { getCarsInfo, getCountInfo } from '../utils/inout.js';
+import { startRacing } from '../utils/racing.js';
+
 class App {
-  async run() {}
+  async run() {
+    const carArray = await getCarsInfo(); // 참가자 받기
+    const count = await getCountInfo(); // 시도 횟수 받기
+
+    startRacing(carArray, count);
+  }
 }
 
 export default App;
---
+++ utils/inout.js
@@ -0,0 +1,35 @@
+import { Console } from '@woowacourse/mission-utils';
+
+export async function getCarsInfo() {
+    const input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+
+    if (!input || input.trim() === "") {
+        throw new Error("[ERROR] 유효하지 않은 입력입니다.");
+    }
+
+    const carArray = input.split(",").map(car => car.trim()); // 공백제거하여 추가
+
+    for (const car of carArray) {
+        if (car.length == 0 || car.length > 5) {
+            throw new Error("[ERROR] 유효하지 않은 입력입니다.");
+        }
+    }
+
+    return carArray;
+}
+
+export async function getCountInfo() {
+    const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+
+    if (!input || input.trim() === "") {
+        throw new Error("[ERROR] 유효하지 않은 입력입니다.");
+    }
+
+    const count = parseInt(input.trim(), 10);  // 입력값을 10진수 정수로 변환
+
+    if (isNaN(count) || count <= 0) {
+        throw new Error("[ERROR] 유효하지 않은 입력입니다.");
+    }
+
+    return count;
+}
