602f76f9bcd1e33c5c4a850157f5881cd161763a
@u_limit_ <yurim@ajou.ac.kr>
2024-10-27 19:04:29 +0900

    style: double quote에서 single quote로 변경

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import { Console } from '@woowacourse/mission-utils'
+import { Console } from '@woowacourse/mission-utils';
 
 const validateCarName = (input) => {
   const regex = /^[A-Za-z0-9]+$/;
 
-  if (input.length > 5){
-    throw new Error("[ERROR] 자동차 이름은 5자 이하로 입력해주세요.");
+  if (input.length > 5) {
+    throw new Error('[ERROR] 자동차 이름은 5자 이하로 입력해주세요.');
   }
 
-  if(!regex.test(input)){
-    throw new Error("[ERROR] 자동차 이름에는 영어, 숫자만 사용할 수 있어요.")
+  if (!regex.test(input)) {
+    throw new Error('[ERROR] 자동차 이름에는 영어, 숫자만 사용할 수 있어요.');
   }
-}
+};
 const determineCarName = (input) => {
   const names = input.split(',');
-  names.forEach((name)=>validateCarName(name));
-  if(new Set(names).size!==names.length){
-    throw new Error("[ERROR] 자동차 이름은 중복되지 않게 입력해주세요.")
+  names.forEach((name) => validateCarName(name));
+  if (new Set(names).size !== names.length) {
+    throw new Error('[ERROR] 자동차 이름은 중복되지 않게 입력해주세요.');
   }
-}
+};
 
 class App {
   async run() {
-  const input = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-  determineCarName(input);
-  const raceNumber = await  Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    const input = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+    );
+    determineCarName(input);
+    const raceNumber =
+      await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
   }
 }
 
