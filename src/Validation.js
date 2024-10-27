ff087bf59e3abf2d18c3f34d642aa77dde73381c
Heesu01 <rulru0413@naver.com>
2024-10-28 00:22:28 +0900

    feat(Validaion): 자동차 이름 중복, 구분자 검증 추가

--- src/Validation.js
+++ src/Validation.js
@@ -10,6 +10,20 @@ class Validation {
     if (carList.length === 0 || carList.some((name) => name === "")) {
       throw new Error("[ERROR] 자동차 이름을 입력해야 합니다.");
     }
+
+    // 자동차 이름이 쉼표로 구분되지 않을 경우
+    const validateSeparator = carList.find((name) => /[^a-zA-Z0-9]/.test(name));
+    if (validateSeparator) {
+      throw new Error(
+        "[ERROR] 자동차 이름은 쉼표(,)로만 구분되어야 하며, 다른 기호는 사용할 수 없습니다."
+      );
+    }
+
+    // 자동차 이름이 중복될 경우
+    const uniqueNames = new Set(carList);
+    if (uniqueNames.size !== carList.length) {
+      throw new Error("[ERROR] 자동차 이름은 중복될 수 없습니다.");
+    }
   }
 
   // 경주 횟수가 0이하일 경우
