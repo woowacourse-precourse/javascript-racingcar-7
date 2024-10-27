19ea7cf46620e6263087c3b3637ab9af87f9a51b
Minji Kim <minjikim2000@hotmail.com>
2024-10-28 04:48:03 +0900

    feat: 사용자 이름 및 시도 횟수 입출력 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const playerNames = await this.getPlayerNames();
+    const trials = await this.getTrials();
+
+    console.log(playerNames);
+    console.log(trials);
+  }
+
+  async getPlayerNames() {
+    const input = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+    );
+    const names = input.split(",");
+
+    return names;
+  }
+
+  async getTrials() {
+    const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+
+    return input;
+  }
 }
 
 export default App;
