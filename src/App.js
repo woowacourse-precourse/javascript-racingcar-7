9dcaa1252e69d7695ddb27587912a0fc8e9b3967
daun-up <awhekdns@kookmin.ac.kr>
2024-10-27 16:28:33 +0900

    feat : 경주할 자동차 입력, 자동차 별 무작위 값 할당 클래스 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
+
+// 경주할 자동차 이름 입력
+class InputHandler {
+  async getInput(message) {
+    return await Console.readLineAsync(message);
+  }
+
+  printMessage(message) {
+    Console.print(message);
+  }
+}
+
+// 각 자동차 별 무작위 값 할당
+class RandomNumberGenerator {
+  static pickNumberInRange(min = 0, max = 9) {
+    return Random.pickNumberInRange(min, max);
+  }
+}
+
 class App {
-  async run() {}
+  constructor() {
+    this.inputHandler = new InputHandler();
+    this.cars = [];
+  }
+
+  async run() {
+
+  }
+
+
 }
 
 export default App;
