40774b7659fbf5844e2b71ff770a00edf47d5381
sky121016 <sky121016@naver.com>
2024-10-27 20:22:28 +0900

    feat: 시도 횟수 입력

--- src/App.js
+++ src/App.js
@@ -8,12 +8,15 @@ class App {
 
     const CARS = this.splitCars(CARS_INPUT);
     const CARS_COUNT = CARS.length;
+
+    const ATTEMPTS = await this.getAttempts();
+
     
   }
 
   async getCarsInput() {
     const CARS_INPUT = Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
     );
 
     return CARS_INPUT;
@@ -24,22 +27,23 @@ class App {
     // 영어 문자만 가능
     const CAR_NAME_PATTERN = /^[a-zA-Z]+$/;
     if (!CAR_NAME_PATTERN.test(NAME)) {
-      throw new Error('[ERROR] 자동차 이름은 영문자만 가능합니다. ');
+      throw new Error('[ERROR] 자동차 이름은 영문자만 가능합니다. \n');
     }
 
     // 5자 이하만 가능
     if (NAME.length > 5) {
-      throw new Error('[ERROR] 자동차 이름은 5자 이하만 가능합니다. ');
+      throw new Error('[ERROR] 자동차 이름은 5자 이하만 가능합니다. \n');
     }
 
     // 중복 불가능
     if (CARS.includes(NAME)) {
-      throw new Error('[ERROR] 자동차 이름은 중복되면 안됩니다. ');
+      throw new Error('[ERROR] 자동차 이름은 중복되면 안됩니다. \n');
     }
 
     return true;
   }
 
+  // , 기준 분리
   splitCars(CARS_INPUT) {
     const CARS_SPLIT = CARS_INPUT.split(',');
 
@@ -53,6 +57,17 @@ class App {
     });
 
     return CARS;
+  }
+
+  // 시도 횟수 입력
+  async getAttempts(){
+    const ATTEMPTS = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+
+    if(isNaN(ATTEMPTS)){
+      throw new Error ('[ERROR] 시도할 횟수는 숫자여야 합니다. \n');
+    }
+
+    return ATTEMPTS;
   }
 }
 
