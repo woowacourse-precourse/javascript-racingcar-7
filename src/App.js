521718871d3d16590f59e9d094e1f2910ea6d7f4
daun-up <awhekdns@kookmin.ac.kr>
2024-10-27 18:15:19 +0900

    refactor : App.js 메소드 분리

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import OutputHandler from "./utils/OutputHandler.js";
 import InputValidator from "./utils/InputValidator.js";
 import RacingGame from "./racingcar/RacingGame.js";
 
+// Q. App.js 에 어디까지 구현해도 되는 걸까요?
+
 class App {
   constructor() {
     this.inputHandler = new InputHandler();
@@ -10,24 +12,29 @@ class App {
 
   async run() {
     try {
-      const input = await this.inputHandler.getInput(
-          "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)"
-      );
-      const carNames = input.split(",").map((name) => name.trim());
-
-      InputValidator.validateCarNames(carNames);
-
-      const attemptsInput = await this.inputHandler.getInput("시도할 횟수는 몇 회인가요?");
-      const attempts = InputValidator.validateAttempts(attemptsInput);
-
-      const racingGame = new RacingGame(carNames);
-      racingGame.start(attempts);
-
+      const carNames = await this.getCarNames();
+      const attempts = await this.getAttempts();
+      this.startGame(carNames, attempts);
     } catch (error) {
       OutputHandler.printMessage(error.message);
       throw error;
     }
   }
-}
 
-export default App;
+  async getCarNames() {
+    const input = await this.inputHandler.getInput("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+    const carNames = input.split(",").map((name) => name.trim());
+    InputValidator.validateCarNames(carNames);
+    return carNames;
+  }
+
+  async getAttempts() {
+    const attemptsInput = await this.inputHandler.getInput("시도할 횟수는 몇 회인가요?");
+    return InputValidator.validateAttempts(attemptsInput);
+  }
+
+  startGame(carNames, attempts) {
+    const racingGame = new RacingGame(carNames);
+    racingGame.start(attempts);
+  }
+}
