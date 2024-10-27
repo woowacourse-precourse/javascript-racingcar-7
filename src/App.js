d4986746a7734ff7be2eec64dacc9c49752bcff3
COMPOSEDKIM <composedkim@naver.com>
2024-10-28 02:27:51 +0900

    feat: 사용자에게 자동차 이름 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const userCars = this.getCarName();
+  }
+
+  async getCarName() {
+    try {
+      const carname = await MissionUtils.Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+      );
+    } catch (error) {}
+  }
 }
 
 export default App;
