0f1bea73cb723ecc52b7be75dc3fa6786b0ed339
crolvlee <leecrolv@gmail.com>
2024-10-27 22:39:20 +0900

    feat: 사용자로부터 자동차 이름, 시도할 횟수 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from "@woowacourse/mission-utils";
+import { getCars, getRound } from "./InputHandler.js";
+
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const cars = await getCars();
+      const round = await getRound();
+
+    } catch(error) {
+      throw error;
+    }
+  }
 }
 
 export default App;
