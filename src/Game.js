c3639fa61a8b1586b84825c82ff92b2247f4bb9d
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-10-27 10:36:07 +0900

    docs: Game 클래스의 게임 진행 및 우승자 판별 로직에 주석 추가
    - 최고 위치 갱신 및 공동 우승자 추가 로직에 대한 주석 추가
    - updateWinners 메서드의 동작을 명확히 설명

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,5 @@
-import Car from './Car.js';
 import OutputView from './OutputView.js';
+import Car from './Car.js';
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 class Game {
@@ -15,15 +15,19 @@ class Game {
         const randomValue = MissionUtils.Random.pickNumberInRange(0, 9);
         if (randomValue >= 4) {
           car.move();
-          if (car.getPosition() > this.maxPosition) {
-            this.maxPosition = car.getPosition();
-            this.winners = [car.getName()];
-          } else if (car.getPosition() === this.maxPosition) {
-            this.winners.push(car.getName());
-          }
+          this.updateWinners(car); // 우승자 리스트 갱신
         }
       });
       OutputView.printPositions(this.cars);
+    }
+  }
+
+  updateWinners(car) {
+    if (car.getPosition() > this.maxPosition) {
+      this.maxPosition = car.getPosition();
+      this.winners = [car.getName()]; // 최고 위치가 변경되면 새 우승자 목록 초기화
+    } else if (car.getPosition() === this.maxPosition) {
+      this.winners.push(car.getName()); // 기존 최고 위치와 같다면 공동 우승자 추가
     }
   }
 
