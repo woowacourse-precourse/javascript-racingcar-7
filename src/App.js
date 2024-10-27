1ae57fbd00c0d63f66bdf50b9fb8c3ce5a11485b
yuri295 <jin_astro0302@naver.com>
2024-10-27 18:21:40 +0900

    feat:숫자 입력받기

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ class App {
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
 
+    const numberInput = await MissionUtils.Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    );
+
     const nameList = this.splitNameInput(nameInput);
     const carNames = this.validateNameList(nameList);
   }
@@ -23,10 +27,12 @@ class App {
     if (nameList.length === 1) {
       throw new Error("자동차 이름은 쉼표로 구분하여 2개 이상 입력해 주세요.");
     }
+
     const emptyName = nameList.find((name) => name === "");
     if (emptyName !== undefined) {
       throw new Error("자동차 이름 사이에 공백이 포함되어 있습니다.");
     }
+
     const longName = nameList.find((name) => name.length > 5);
     if (longName !== undefined) {
       throw new error(`"${longName}"은(는) 5자를 초과합니다.`);
