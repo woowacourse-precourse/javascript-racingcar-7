354d59c5c7cb7111cc52a6d715dca8993c7fa951
seunghyeon77 <jdyjsh77@naver.com>
2024-10-27 17:04:11 +0900

    refactor: length() 메소드에서 length 메소드로 변경

--- src/Car.js
+++ src/Car.js
@@ -5,7 +5,7 @@ export class Car {
     this.carName = carName;
     this.position = 0;
 
-    if (this.carName.length() >= 6) {
+    if (carName.length >= 6) {
       throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
     }
   }
