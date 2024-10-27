40f6df0bb31c2602e48d13626b8fa8506e89148e
YongHyunKing <yhkim8246@gmail.com>
2024-10-28 03:57:30 +0900

    feat(APP): 자동차 이름을 쉼표(,)로 구분하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
+import RacingCar from './RacingCar';
 class App {
   async run() {
     const NAMES = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
     const TIMES = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+
+    const splitNames = NAMES.split(',');
+
   }
 }
 
