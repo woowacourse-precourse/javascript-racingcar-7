a74a8cc11b3b55c47d1cf14d3aaae69314144783
kanghaeun <xgkg0330@jnu.ac.kr>
2024-10-28 00:36:21 +0900

    feat: 랜덤 숫자 생성 기능 추가

--- src/RaceManager.js
+++ src/RaceManager.js
@@ -1,7 +1,12 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class RaceManager {
   constructor(name) {
     this.name = name;
     this.position = 0;
+  }
+  createRandomNumber() {
+    return MissionUtils.Random.pickNumberInRange(0, 9);
   }
 }
 
