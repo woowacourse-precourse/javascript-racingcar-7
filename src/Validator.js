2f2219c22f1d52e0a3f59b2ed2e0c5463098329d
suhwa <tnghk9611@naver.com>
2024-10-28 02:36:14 +0900

    refactor(Validator): 입력에 대한 예외 처리 케이스 추가
    - some() 내장 메서드로 모든 자동차들에 대해 검사
    - 에러 메세지 추가

--- src/Validator.js
+++ src/Validator.js
@@ -2,16 +2,22 @@ import { CAR_NAME_ERROR, TRY_COUNT_ERROR } from "./constants/Message.js";
 
 class Validator {
     checkCarName(carNames) {
-        for (let car of carNames) {
-            if (car.length > 5) {
-                throw new Error(CAR_NAME_ERROR.OVER_LENGTH);
-            }
+        if (carNames.some((car) => car.length > 5)) {
+            throw new Error(CAR_NAME_ERROR.OVER_LENGTH);
+        }
+
+        if (carNames.some((car) => car.length === 0)) {
+            throw new Error(CAR_NAME_ERROR.NO_NAMED_CAR);
         }
     }
 
     checkTryCount(tryCount) {
-        if (isNaN(tryCount) || Number(tryCount) <= 0) {
+        if (isNaN(tryCount)) {
             throw new Error(TRY_COUNT_ERROR.NOT_NUMBER);
+        }
+
+        if (tryCount <= 0) {
+            throw new Error(TRY_COUNT_ERROR.MINUS_OR_ZERO);
         }
     }
 }
--- src/constants/Message.js
+++ src/constants/Message.js
@@ -15,8 +15,10 @@ export const PRINT_MESSAGE = Object.freeze({
 
 export const CAR_NAME_ERROR = Object.freeze({
     OVER_LENGTH: `${ERROR_PREFIX} 자동차 이름은 5자 이하로 입력 가능합니다.`,
+    NO_NAMED_CAR: `${ERROR_PREFIX} 입력되지 않은 자동차 이름이 있습니다.`,
 });
 
 export const TRY_COUNT_ERROR = Object.freeze({
     NOT_NUMBER: `${ERROR_PREFIX} 시도할 횟수는 숫자로 입력해야 합니다.`,
+    MINUS_OR_ZERO: `${ERROR_PREFIX} 시도할 횟수는 양의 정수만 가능합니다.`,
 });
