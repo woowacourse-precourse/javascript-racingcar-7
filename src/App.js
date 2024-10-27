af0b88dcd306233e0500283dadf326e88522d064
suubiin <127467411+Suubiin@users.noreply.github.com>
2024-10-28 04:10:19 +0900

    feat: 자동차 이름 입력 및 쉼표 기준 분리 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const carNamesInput = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+    );
+    const carNames = this.splitCarNames(carNamesInput);
+  }
+
+  splitCarNames(carNamesInput) {
+    return carNamesInput.split(',');
+  }
 }
 
 export default App;
