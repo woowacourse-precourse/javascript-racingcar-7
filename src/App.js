243f3404f7188d126d99b2370805df3ee6eeb890
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-10-27 18:06:58 +0900

    refactor(Car): Car 클래스를 외부 파일로 분리

--- src/App.js
+++ src/App.js
@@ -1,28 +1,5 @@
-import { Console, Random } from '@woowacourse/mission-utils';
-
-class Car {
-  constructor(name) {
-    this.name = name;
-    this.moveCount = 0;
-  }
-
-  getName() {
-    return this.name;
-  }
-
-  forwardOrNot() {
-    let num = Random.pickNumberInRange(0, 9);
-    if (num >= 4) this.moveCount++;
-  }
-
-  getProgress() {
-    Console.print(`${this.name} : ${'-'.repeat(this.moveCount)}`);
-  }
-
-  getCount() {
-    return this.moveCount;
-  }
-}
+import Console from '@woowacourse/mission-utils';
+import Car from './Car.js';
 
 async function getNameInput() {
   const input = Console.readLineAsync(
