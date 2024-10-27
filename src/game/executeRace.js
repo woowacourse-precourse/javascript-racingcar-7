a07bcf7036f6ccb8c9c851b1f6be3baeb600a48b
Junseo Kim <semiwest2@gmail.com>
2024-10-28 05:57:54 +0900

    feat: 자동차 경주 로직 구현 및 우승자 결정 기능 구현
    - 자동차 이름 목록으로 Car 객체 생성 기능 추가
    - 자동차의 이동을 처리하는 기능 추가
    - 각 자동차의 현재 위치를 출력하는 기능 추가
    - 최종 우승자를 결정하는 기능 추가

---
+++ src/game/executeRace.js
@@ -0,0 +1,44 @@
+import { Random } from '@woowacourse/mission-utils'
+import Car from './Car.js';
+import { RANDOM_MIN_VALUE, RANDOM_MAX_VALUE } from '../constant/constants.js';
+import printString from '../output/printString.js';
+
+export default function executeRace(carNameList, moveCount){
+  const cars = createCarsFromNames(carNameList);
+  printString('실행 결과');
+
+  for(let count = 0; count < moveCount; count++){
+    moveCars(cars);
+    printCarPosition(cars);
+  }
+
+  return getWinnerList(cars);
+}
+
+
+function createCarsFromNames(carNameList){
+  return carNameList.map(carName => new Car(carName));
+}
+
+
+function moveCars(cars){
+  cars.forEach(car => {
+    car.checkAndMoveForward(Random.pickNumberInRange(RANDOM_MIN_VALUE, RANDOM_MAX_VALUE));
+  });
+}
+
+
+function printCarPosition(cars){
+  cars.forEach(car => {
+    printString(`${car.name} : ${'-'.repeat(car.position)}`);
+  })
+  printString('');
+}
+
+
+function getWinnerList(cars){
+  const carPositions = cars.map(car => car.position);
+  const maxPosition = Math.max(...carPositions);
+  const winnerList = cars.filter(car => car.position === maxPosition).map(car => car.name);
+  return winnerList;
+}
