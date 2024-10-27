d668f3ebbe130bb91e62a80e3844d40d1ae824ff
AlgeMoya <kjhobd2@naver.com>
2024-10-28 00:28:05 +0900

    feat: 자동차 이름 길이 검증 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { Console } from "@woowacourse/mission-utils";
+import CheckValue from "./CheckValue";
 
 class App {
   async run() {
+    const checkValue = new CheckValue();
+
     // 자동차 이름을 입력
     Console.print(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
@@ -10,6 +13,11 @@ class App {
     let userInput = await Console.readLineAsync("");
     let userNames = userInput.split(",");
     let userCounts = new Array(userNames.length).fill(0);
+
+    // 파라미터 추가해서 5자 이외의 길이도 검사할 수 있게 해보자.
+    for (let i = 0; i < userNames.length; i++) {
+      checkValue.checkLength(userNames[i]);
+    }
   }
 }
 
