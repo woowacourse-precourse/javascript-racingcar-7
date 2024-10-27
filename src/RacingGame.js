f7d43d645e0e89017962d73718b322f62eb9fc2f
seunghyeon77 <jdyjsh77@naver.com>
2024-10-27 15:43:24 +0900

    feat: 각 자동차의 위치를 출력하는 메소드 추가

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -8,9 +8,18 @@ export class RacingGame {
     }
   }
 
+  // 자동차 경주 로직
   play() {
     for (let i = 0; i <= this.rounds; i++) {
       this.cars.forEach((car) => car.move());
     }
+  }
+
+  // 각 자동차의 위치 출력
+  printCarPosition() {
+    this.cars.forEach((car) => {
+      console.print(`${car.name} : ${"-".repeat(car.position)}`);
+    });
+    console.print("\n");
   }
 }
