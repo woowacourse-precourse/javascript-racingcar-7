b15265a0cb64eaec6d80866e5e386d7805f7536e
subsub-e <ab13696802@gmail.com>
2024-10-27 23:54:47 +0900

    fix(test) : test.each 를 활용하여 테스트코드 리팩토링

--- __tests__/determineWinners.test.js
+++ __tests__/determineWinners.test.js
@@ -1,47 +1,42 @@
 import { determineWinners } from "../src/utils/determineWinners.js";
 
 describe("determineWinners 함수 테스트 : ", () => {
-  test("우승자가 한 명일 경우 해당 우승자를 배열로 반환한다", () => {
-    const cars = [
-      { getName: () => "pobi", getCount: () => 3 },
-      { getName: () => "woni", getCount: () => 2 },
-      { getName: () => "dev", getCount: () => 4 },
-    ];
-
+  test.each([
+    {
+      description: "우승자가 한 명일 경우 해당 우승자를 배열로 반환한다",
+      cars: [
+        { getName: () => "pobi", getCount: () => 3 },
+        { getName: () => "woni", getCount: () => 2 },
+        { getName: () => "dev", getCount: () => 4 },
+      ],
+      expected: ["dev"],
+    },
+    {
+      description: "우승자가 여러 명일 경우 해당 우승자들을 배열로 반환한다",
+      cars: [
+        { getName: () => "pobi", getCount: () => 4 },
+        { getName: () => "woni", getCount: () => 2 },
+        { getName: () => "dev", getCount: () => 4 },
+      ],
+      expected: ["pobi", "dev"],
+    },
+    {
+      description:
+        "모든 참가자의 이동 횟수가 동일할 경우 모든 참가자를 배열로 반환한다",
+      cars: [
+        { getName: () => "pobi", getCount: () => 3 },
+        { getName: () => "woni", getCount: () => 3 },
+        { getName: () => "dev", getCount: () => 3 },
+      ],
+      expected: ["pobi", "woni", "dev"],
+    },
+    {
+      description: "참가자가 한 명일 경우 해당 참가자를 우승자로 반환한다",
+      cars: [{ getName: () => "pobi", getCount: () => 5 }],
+      expected: ["pobi"],
+    },
+  ])("$description", ({ cars, expected }) => {
     const result = determineWinners(cars);
-
-    expect(result).toEqual(["dev"]);
-  });
-
-  test("우승자가 여러 명일 경우 해당 우승자들을 배열로 반환한다", () => {
-    const cars = [
-      { getName: () => "pobi", getCount: () => 4 },
-      { getName: () => "woni", getCount: () => 2 },
-      { getName: () => "dev", getCount: () => 4 },
-    ];
-
-    const result = determineWinners(cars);
-
-    expect(result).toEqual(["pobi", "dev"]);
-  });
-
-  test("모든 참가자의 이동 횟수가 동일할 경우 모든 참가자를 배열로 반환한다", () => {
-    const cars = [
-      { getName: () => "pobi", getCount: () => 3 },
-      { getName: () => "woni", getCount: () => 3 },
-      { getName: () => "dev", getCount: () => 3 },
-    ];
-
-    const result = determineWinners(cars);
-
-    expect(result).toEqual(["pobi", "woni", "dev"]);
-  });
-
-  test("참가자가 한 명일 경우 해당 참가자를 우승자로 반환한다", () => {
-    const cars = [{ getName: () => "pobi", getCount: () => 5 }];
-
-    const result = determineWinners(cars);
-
-    expect(result).toEqual(["pobi"]);
+    expect(result).toEqual(expected);
   });
 });
--- __tests__/isValidRound.test.js
+++ __tests__/isValidRound.test.js
@@ -1,19 +1,31 @@
 import { isValidRound } from "../src/validations/isValidRound.js";
 
 describe("isValidRound 함수 테스트 : ", () => {
-  test("아무런 입력도 하지 않은 경우 EMPTY_INPUT_ROUND 에러를 발생시킨다.", () => {
-    expect(() => isValidRound("")).toThrowError(
-      "[ERROR] 숫자를 입력해 주세요. 빈 입력은 유효하지 않습니다."
-    );
-  });
-
-  test("숫자를 입력하지 않은 경우 INVALID_ROUND_NUMBER 에러를 발생시킨다.", () => {
-    expect(() => isValidRound("p")).toThrowError(
-      "[ERROR] 유효하지 않은 시도 횟수입니다. 숫자만 입력해 주세요."
-    );
-  });
-
-  test("숫자를 입력한 경우 에러를 발생시키지 않는다.", () => {
-    expect(() => isValidRound("5")).not.toThrowError();
+  test.each([
+    {
+      description:
+        "아무런 입력도 하지 않은 경우 EMPTY_INPUT_ROUND 에러를 발생시킨다.",
+      input: "",
+      expectedError:
+        "[ERROR] 숫자를 입력해 주세요. 빈 입력은 유효하지 않습니다.",
+    },
+    {
+      description:
+        "숫자를 입력하지 않은 경우 INVALID_ROUND_NUMBER 에러를 발생시킨다.",
+      input: "p",
+      expectedError:
+        "[ERROR] 유효하지 않은 시도 횟수입니다. 숫자만 입력해 주세요.",
+    },
+    {
+      description: "숫자를 입력한 경우 에러를 발생시키지 않는다.",
+      input: "5",
+      expectedError: null,
+    },
+  ])("$description", ({ input, expectedError }) => {
+    if (expectedError) {
+      expect(() => isValidRound(input)).toThrowError(expectedError);
+    } else {
+      expect(() => isValidRound(input)).not.toThrowError();
+    }
   });
 });
