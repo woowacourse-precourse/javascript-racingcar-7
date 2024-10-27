ea1a3923cb8cebb8fb95e166dad494f862f87d33
seunghyeon77 <jdyjsh77@naver.com>
2024-10-27 14:57:15 +0900

    refactor: 이동 횟수 오류 메세지 코드 위치 수정

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -1,9 +1,10 @@
 export class RacingGame {
   constructor(CARNAMES, ROUNDS) {
-    if (!Number.isInteger(ROUNDS) || ROUNDS <= 0) {
-      throw new Error("[ERROR] 이동 횟수는 양의 정수여야 합니다.");
-    }
     this.cars = CARNAMES.map((carName) => new Car(carName));
     this.rounds = ROUNDS;
+
+    if (!Number.isInteger(this.rounds) || this.rounds <= 0) {
+      throw new Error("[ERROR] 이동 횟수는 양의 정수여야 합니다.");
+    }
   }
 }
