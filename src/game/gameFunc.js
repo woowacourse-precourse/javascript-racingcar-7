66c269e1124d323f40e495a95d9a69fcbe0fb565
yusuShin <tlsdbtn0507@naver.com>
2024-10-27 15:50:45 +0900

    feat(gameFunc) Add startRaceGame - Take carArray and rap as a param - With a for loop loop the code as many as rap and activate moveCar method - After moveCar method print the result of try - Print empty string to check the result more easily and advance UI

--- src/game/gameFunc.js
+++ src/game/gameFunc.js
@@ -4,8 +4,17 @@ import Car from "./Car.js";
 export default function startGame(carNames, raceRap) {
   PrintFunc.printParams("실행결과");
   const carClassArr = makeCarClassArr(carNames);
-
-
+  startRaceGame(carClassArr, raceRap);
 }
 
 const makeCarClassArr = (names) => names.map((carName) => new Car(carName));
+
+const startRaceGame = (carArray, rap) => {
+  for (let i = 0; i < rap; i++) {
+    carArray.forEach((carObj) => {
+      carObj.moveCar();
+      PrintFunc.printParams(`${carObj.carName} : ${carObj.carMoveLength}`);
+    });
+    PrintFunc.printParams("");
+  };
+}
