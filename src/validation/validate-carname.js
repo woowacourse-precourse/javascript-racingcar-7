77d9b004b8f187190b6fc520f1f2ee38d47ffbec
youdame <112458620+youdame@users.noreply.github.com>
2024-10-28 01:51:37 +0900

    feat : 인풋이 하나인 경우에 대한 예외 처리 추가

--- src/validation/validate-carname.js
+++ src/validation/validate-carname.js
@@ -1,14 +1,11 @@
 import throwError from '../util/throw-error.js';
 import runValidators from './run-validator.js';
-/* 
-1. ,만 쓰고 값을 입력하지 않았을 때 
-2. 
 
-*/
 export const ERROR_MESSAGES = {
   EMPTY_INPUT: '입력이 비어 있습니다.',
-  EMPTY_CAR_NAME: '자동차 이름에 빈 값이 포함되어 있습니다. 모든 이름을 입력해 주세요.',
+  EMPTY_CAR_NAME: '자동차 이름에 빈 값이 포함되어 있습니다.',
   NAME_LENGTH_EXCEEDED: '자동차 이름은 5자를 넘을 수 없습니다.',
+  SINGLE_CAR_NAME: '두 대 이상의 자동차 이름을 입력해 주세요.',
 };
 
 const checkForEmptyInput = (carNameList) => {
@@ -23,6 +20,13 @@ const checkEmptyValue = (carNameList) => {
   return carNameList;
 };
 
+const checkOneValue = (carNameList) => {
+  if (carNameList.length === 1) {
+    throwError(ERROR_MESSAGES.SINGLE_CAR_NAME);
+  }
+  return carNameList;
+};
+
 const checkNameLengthLimit = (carNameList) => {
   carNameList.forEach((carName) => {
     if (carName.length >= 5) {
@@ -33,7 +37,7 @@ const checkNameLengthLimit = (carNameList) => {
 };
 
 const validateCarName = (carNameList) => {
-  runValidators([checkForEmptyInput, checkEmptyValue, checkNameLengthLimit], carNameList);
+  runValidators([checkForEmptyInput, checkEmptyValue, checkOneValue, checkNameLengthLimit], carNameList);
 };
 
 export default validateCarName;
