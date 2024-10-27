f27372374f9454a946b5e45cf56ae838ac8b7f95
sanghyeonLee <hyeon012366@gmail.com>
2024-10-28 04:49:01 +0900

    refactor: 출력 메소드 전역변수로 선언

--- src/constants/message.js
+++ src/constants/message.js
@@ -3,3 +3,9 @@ export const PROMPT_MESSAGE =
         FIRST:"경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
         SECOND:"시도할 횟수는 몇 회인가요?\n"
     }
+
+export const OUTPUT_MESSAGE =
+    {
+        ERROR:"[ERROR]: 잘못된 값을 입력 하셨습니다.",
+        WINNER:"최종 우승자 : "
+    }
