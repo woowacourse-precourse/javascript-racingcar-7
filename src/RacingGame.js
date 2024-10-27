9c5ecf924440214b6b080c5a92d001e0566b8f9a
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-10-28 02:11:43 +0900

    refactor(RacingGame.js): 체이닝 코드 변경

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -12,29 +12,36 @@ class RacingGame {
   #tryCount;
 
   async startGame() {
+    await this.#initialize();
+    await this.#play();
+    await this.#finish();
+  }
+
+  async #initialize() {
     const carNames = await inputHandler.carNameInput();
     const parsedNames = carNameParser(carNames);
     validateCarNames(parsedNames);
 
     this.#cars = parsedNames.map((name) => new Car(name));
     this.#tryCount = Number(await inputHandler.racingTryCountInput());
-
-    await this.playGame();
   }
 
-  async playGame() {
+  async #play() {
     outputHandler.printMessage(GAME_MESSAGES.RESULT);
 
     for (let i = 0; i < this.#tryCount; i++) {
-      this.playRound();
+      this.#playRound();
       outputHandler.printRoundResult(this.#cars);
       outputHandler.printMessage('');
     }
-
-    this.endGame();
   }
 
-  playRound() {
+  async #finish() {
+    const winners = this.#getWinners();
+    outputHandler.printMessage(`${GAME_MESSAGES.WINNER}${winners.join(', ')}`);
+  }
+
+  #playRound() {
     this.#cars.forEach((car) => {
       const number = randomNumber(
         RULES.RANDOM_MIN_NUMBER,
@@ -50,16 +57,10 @@ class RacingGame {
     return number >= RULES.MIN_PROGRESS_STEPS;
   }
 
-  endGame() {
-    const winners = this.#getWinners();
-    outputHandler.printMessage(`${GAME_MESSAGES.WINNER}${winners.join(', ')}`);
-  }
-
   #getWinners() {
     const maxPosition = Math.max(
       ...this.#cars.map((car) => car.getCarPosition()),
     );
-
     return this.#cars
       .filter((car) => car.getCarPosition() === maxPosition)
       .map((car) => car.getCarName());
