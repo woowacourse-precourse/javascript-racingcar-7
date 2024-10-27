781809d5b7b15bbac3c2223796673e3ba83491c5
suhwa <tnghk9611@naver.com>
2024-10-27 23:18:56 +0900

    refactor(CarGameController): 필드를 캡슐화하기 위해 private 추가
    - #을 추가하여 모든 필드를 외부에서 접근하지 못하도록 수정

--- src/CarGameController.js
+++ src/CarGameController.js
@@ -4,33 +4,32 @@ import Printer from "./Printer.js";
 import Utils from "./Utils.js";
 import Validator from "./Validator.js";
 
-// 자동차 경주 게임 진행 클래스
 class CarGameController {
-    inputHandler; // 입력 처리 객체
-    validator; // 예외 처리 객체
-    printer; // 출력 처리 객체
+    #inputHandler;
+    #validator;
+    #printer;
 
     constructor() {
-        this.inputHandler = new InputHandler();
-        this.validator = new Validator();
-        this.printer = new Printer();
+        this.#inputHandler = new InputHandler();
+        this.#validator = new Validator();
+        this.#printer = new Printer();
     }
 
     async play() {
-        const inputCarNames = await this.inputHandler.enterCarNames();
+        const inputCarNames = await this.#inputHandler.enterCarNames();
         const splittedCarNames = Utils.transformCarNamesStringToArray(inputCarNames);
-        this.validator.isValidCarName(splittedCarNames);
+        this.#validator.isValidCarName(splittedCarNames);
 
-        const inputTryCnt = await this.inputHandler.enterTryCount();
-        this.validator.isValidTryCount(inputTryCnt);
+        const inputTryCnt = await this.#inputHandler.enterTryCount();
+        this.#validator.isValidTryCount(inputTryCnt);
 
         const carGame = new CarGame(splittedCarNames, inputTryCnt);
         const resultLogs = carGame.startRace();
         const winner = carGame.pickWinner();
 
-        this.printer.printResultText();
-        this.printer.printLogs(resultLogs);
-        this.printer.printWinner(winner);
+        this.#printer.printResultText();
+        this.#printer.printLogs(resultLogs);
+        this.#printer.printWinner(winner);
     }
 }
 
