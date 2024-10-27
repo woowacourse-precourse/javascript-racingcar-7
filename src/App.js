ca8fc3993031f2de33fbeef940cfc39b17fb41a7
jeonhyeonseon <goodpretty0325@gmail.com>
2024-10-27 19:07:34 +0900

    feat(app): 사용자 입력 및 Race 클래스 연동 기능 추가
    - 사용자 입력을 받아 전진하는 자동차 이름과 이동 횟수 설정
    - 입력받은 데이터를 통한 Race 클래스 인스턴스 생성 및 경주 실행

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import Race from './Race.js';
 
 class App {
   async run() {
 
+    try {
     const carNames = await this.getInput("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ");
     const position = await this.getInput("시도할 횟수를 입력하세요: ");
     
+      const race = new Race(carNames, parseInt(position, 10));
+      race.showResult();
+    } catch (error) {
+      MissionUtils.Console.print(`[ERROR] ${error.message}`);
+      throw error;
+    }
   }
 
   async getInput(promptText) {
