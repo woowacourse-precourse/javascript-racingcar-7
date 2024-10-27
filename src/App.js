c410f0cae06d97bc8f3a9e8e8d15bbec80ffabc2
youngju6143 <youngju6143@naver.com>
2024-10-27 15:12:01 +0900

    feat(Exceptionhandling): 예외 처리(이름이 5자 초과인 경우) 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
 async function getNames() {
+  let nameArray = [];
+
   try {
     let input = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-    return input;
+    nameArray = input.split(',').map(name => name.trim());
+
+    for (const name of nameArray) {
+      if (name.length > 5) {
+        throw new Error('[ERROR] 이름은 5자 이하이어야 합니다.');
+      }
+    }
+
+    return nameArray; // 이름 배열을 반환
+
   } catch (error) {
     throw new Error('[ERROR]');
   }
@@ -21,10 +32,9 @@ async function getCount() {
 class App {
   async run() {
     try {
-      let input = await getNames();
+      let nameArray = await getNames();
       let count = await getCount();
 
-      let nameArray = input.split(',');
       let moveArray = Array.from({length: nameArray.length}, () => '');
       let winnerArray = [];
 
