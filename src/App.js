db8ef16db7c4fa3d03099f8cc006f1d9724568fd
wowalswjd <alswjd5491@naver.com>
2024-10-28 04:34:48 +0900

    feat(App): 3~4. 시도할 횟수 입력 및 유효성 검증 구현

--- src/App.js
+++ src/App.js
@@ -10,6 +10,14 @@ class App {
 
     // 2. 자동차 이름 유효성 검증
     this.validateCarName(carNameArray);
+
+    // 3. 시도할 횟수 입력
+    const tryNumberInput = await Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    );
+
+    // 4. 시도할 횟수 유효성 검증
+    this.validateTryNumber(tryNumberInput);
   }
 
   validateCarName(carNameArray) {
@@ -37,6 +45,26 @@ class App {
 
     Console.print("자동차 이름이 유효합니다.");
     Console.print(carNameArray);
+  }
+
+  validateTryNumber(tryNumberInput) {
+    const tryNumber = Number(tryNumberInput);
+    // 숫자가 아닌 경우
+    if (Number.isNaN(tryNumber)) {
+      throw new Error("[ERROR] 시도할 횟수로 문자를 입력할 수 없습니다.");
+    }
+
+    // 정수가 아닌 경우
+    if (!Number.isInteger(tryNumber)) {
+      throw new Error("[ERROR] 시도할 횟수로 실수를 입력할 수 없습니다.");
+    }
+
+    // 음수나 0인 경우
+    if (tryNumber <= 0) {
+      throw new Error("[ERROR] 시도할 횟수로 음수 및 0을 입력할 수 없습니다.");
+    }
+
+    Console.print("시도할 횟수가 유효합니다.");
   }
 }
 
