d8c29281e9aa2a1069db31cb85cd7a24990ab7bb
@u_limit_ <yurim@ajou.ac.kr>
2024-10-27 19:11:36 +0900

    refactor: validateCarName 모듈화

---
+++ src/validation/index.js
@@ -0,0 +1,17 @@
+export const validateCarName = (input) => {
+  const regex = /^[A-Za-z0-9]+$/;
+  const numberName = Number(input);
+  if (input.length > 5) {
+    throw new Error('[ERROR] 자동차 이름은 5자 이하로 입력해주세요.');
+  }
+
+  if (!regex.test(input)) {
+    throw new Error(
+      '[ERROR] 자동차 이름에는 공백을 제외한 영어, 숫자만 사용할 수 있어요.'
+    );
+  }
+
+  if (numberName === 0) {
+    throw new Error('[ERROR] 자동차 이름에는 공백을 입력할 수 없어요.');
+  }
+};
