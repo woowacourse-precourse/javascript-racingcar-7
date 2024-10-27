a37439b8ff6fe3e914653b366859eb775a1a4c0a
sky121016 <sky121016@naver.com>
2024-10-27 19:30:52 +0900

    docs: README 수정. 이름이 중복일 경우 에러 처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import {Console} from "@woowacourse/mission-utils";
+import {Random} from "@woowacourse/mission-utils";
+
+
 class App {
-  async run() {}
+  async run() {
+    
+
+    
+
+  }
+
+
+
+
 }
 
 export default App;
