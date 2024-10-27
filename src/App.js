df9cb0704c9441d8980fc15630d04a77193aa85c
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-28 06:13:26 +0900

    ✨ feat(input): 사용자에게 시도 횟수를 입력받는 기능 구현
    - `readUserInput` 함수를 사용하여 사용자로부터 시도 횟수를 입력받는 기능을 구현했습니다.
    - `MESSAGE.PROMPT_COUNT_USER_INPUT` 상수를 통해 입력을 안내했습니다.
    - 입력받은 값을 attemptCount 변수에 저장하여 이후 로직에서 활용 가능하도록 구현했습니다.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { readUserInput } from './util/io.js';
 class App {
   async run() {
     const input = await readUserInput(MESSAGE.PROMPT_NAME_USER_INPUT);
+    const attemptCount = await readUserInput(MESSAGE.PROMPT_COUNT_USER_INPUT);
   }
 }
 
