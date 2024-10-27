76d9da97a48282921b6b562544dfe1029bb9b9f8
limielife <selim0401@gmail.com>
2024-10-27 15:26:42 +0900

    [feat] Execute App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { RacingGame } from "./RacingGame.js";
+
 class App {
-  async run() {}
-}
+    async run() {
+        const game = new RacingGame();
+        await game.getInput();  // 입력 비동기 처리
+        game.startRace();
+    }
+};
 
 export default App;
