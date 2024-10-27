d63e3a5e8b88aa6bc1ce5b967a4fc1c4f7cfa77e
hyeongdong <khd941018@gmail.com>
2024-10-27 22:07:43 +0900

    refactor(ValidatorTest): InputValidator 코드 수정으로 인한 테스트 코드 수정
    예외 값이 입력되었을 시 Error를 발생시키는지 확인하도록 수정

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -5,99 +5,97 @@ describe("사용자 입력 자동차 이름 검증 테스트", () => {
     const inputs = [1, 5, 10];
 
     inputs.forEach((input) => {
-      expect(InputValidator.isPositiveNumber(input)).toBeTruthy();
+      expect(() => InputValidator.checkPositive(input)).toBeTruthy();
     });
   });
 
-  test("입력값이 양의 정수가 아니라면 false를 리턴할 수 있다.", () => {
+  test("입력값이 양의 정수가 아니라면 Error를 발생시킬 수 있다.", () => {
     const inputs = [-1, 0, NaN];
 
     inputs.forEach((input) => {
-      expect(InputValidator.isPositiveNumber(input)).toBeFalsy();
+      expect(() => InputValidator.checkPositive(input)).toThrow("[ERROR]");
     });
   });
 
-  test("중복 이름이 있으면 true를 리턴할 수 있다.", () => {
+  test("중복 이름이 있으면 Error를 발생시킬 수 있다.", () => {
     const inputs = [
       ["kim", "kim", "ronaldo"],
       ["lee", "lee", "ronaldo"],
     ];
 
     inputs.forEach((input) => {
-      console.log(input);
-      expect(InputValidator.hasSameName(input)).toBeTruthy();
+      expect(() => InputValidator.checkDuplication(input)).toThrow("[ERROR]");
     });
   });
 
-  test("중복 이름이 없으면 false를 리턴할 수 있다.", () => {
+  test("중복 이름이 없으면 true를 리턴할 수 있다.", () => {
     const inputs = [
       ["kim", "lee", "ronaldo"],
       ["lee", "son", "ronaldo"],
     ];
 
     inputs.forEach((input) => {
-      console.log(input);
-      expect(InputValidator.hasSameName(input)).toBeFalsy();
+      expect(() => InputValidator.checkDuplication(input)).toBeTruthy();
     });
   });
 
-  test("길이가 5를 넘는 이름이 있으면 true를 리턴할 수 있다.", () => {
+  test("길이가 5를 넘는 이름이 있으면 Error를 발생시킬 수 있다.", () => {
     const inputs = [
       ["kim", "lee", "ronaldo"],
       ["kim", "son", "jidane"],
     ];
 
     inputs.forEach((input) => {
-      expect(InputValidator.hasLongName(input)).toBeTruthy();
+      expect(() => InputValidator.checkNameLength(input)).toThrow("[ERROR]");
     });
   });
 
-  test("길이가 5를 넘는 이름이 없으면 false를 리턴할 수 있다.", () => {
+  test("길이가 5를 넘는 이름이 없으면 true를 리턴할 수 있다.", () => {
     const inputs = [
       ["kim", "lee", "re"],
       ["kim", "son", "pobi"],
     ];
 
     inputs.forEach((input) => {
-      expect(InputValidator.hasLongName(input)).toBeFalsy();
+      expect(() => InputValidator.checkNameLength(input)).toBeTruthy();
     });
   });
 
-  test("이름이 하나만 있으면 true를 리턴할 수 있다.", () => {
+  test("이름이 하나만 있으면 Error를 발생시킬 수 있다.", () => {
     const inputs = [["kim"], ["son"]];
 
     inputs.forEach((input) => {
-      expect(InputValidator.hasSingleName(input)).toBeTruthy();
+      expect(() => InputValidator.checkMultiple(input)).toThrow("[ERROR]");
     });
   });
 
-  test("이름이 두 개 이상 있다면 false를 리턴할 수 있다.", () => {
+  test("이름이 두 개 이상 있다면 true를 리턴할 수 있다.", () => {
     const inputs = [
       ["kim", "son"],
       ["son", "johnson", "romero"],
     ];
 
     inputs.forEach((input) => {
-      expect(InputValidator.hasSingleName(input)).toBeFalsy();
+      expect(() => InputValidator.checkMultiple(input)).toBeTruthy();
     });
   });
 
-  test("이름이 공백이라면 true를 리턴할 수 있다.", () => {
+  test("이름이 공백이라면 Error를 발생시킬 수 있다.", () => {
     const inputs = [[""], [" "], ["  "]];
 
     inputs.forEach((input) => {
-      expect(InputValidator.hasNoName(input)).toBeTruthy();
+      expect(() => InputValidator.checkBlank(input)).toThrow("[ERROR]");
     });
   });
 
-  test("이름이 공백이 아니라면 false를 리턴할 수 있다.", () => {
+  test("이름이 공백이 아니라면 true를 리턴할 수 있다.", () => {
     const inputs = [
       ["kim", "lee", "re"],
       ["kim", "son", "pobi"],
     ];
 
     inputs.forEach((input) => {
-      expect(InputValidator.hasNoName(input)).toBeFalsy();
+      expect(() => InputValidator.checkBlank(input)).toBeTruthy();
     });
   });
 });
