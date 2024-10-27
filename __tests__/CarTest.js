f604b919adc7365976c403edad8af806156ea294
jeongwoo903 <www8565@naver.com>
2024-10-28 02:28:15 +0900

    test: 자동차 유효성 검사 테스트
    - 올바른 값, 빈 값, 5글자 이상에 관한 테스트를 생성했습니다.

---
+++ __tests__/CarTest.js
@@ -0,0 +1,36 @@
+import { validateCars } from '../src/utils/validator/car.js';
+import { parseCars } from '../src/utils/parser.js';
+import { ERROR } from '../src/constants/message.js';
+
+describe('자동차 생성 기능 테스트', () => {
+	test('자동차 생성 테스트 - 올바른 이름 값', async () => {
+		// given
+		const cars = 'Ray1,Ray2,Ray3';
+		const carsArray = parseCars(cars);
+
+		// when, then
+		expect(() => validateCars(carsArray)).not.toThrow();
+	});
+
+	test('자동차 생성 테스트 - 빈 값', async () => {
+		// given
+		const cars = '';
+		const carsArray = parseCars(cars);
+
+		// when, then
+		await expect(() => validateCars(carsArray)).toThrow(
+			`[ERROR] ${ERROR.EMPTY}`,
+		);
+	});
+
+	test('자동차 생성 테스트 - 5글자 이상', async () => {
+		// given
+		const cars = 'Ferrari';
+		const carsArray = parseCars(cars);
+
+		// when, then
+		await expect(() => validateCars(carsArray)).toThrow(
+			`[ERROR] ${ERROR.OVER_MAX_LENGTH}`,
+		);
+	});
+});
