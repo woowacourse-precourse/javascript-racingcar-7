8ab5352339f1ba782f45aec57c6d5a3816667e0a
erica-jeong <wjddbwjd1205@gmail.com>
2024-10-28 00:58:44 +0900

    feat: 경기 결과 출력 기능 추가
    - "실행 결과" 문구를 처음에만 출력
    - 각 라운드마다 자동차 이름과 이동 횟수를 '-'로 표시하여 출력
    - 라운드 종료 후 줄바꿈 처리로 결과 구분

--- src/OutputHandler.js
+++ src/OutputHandler.js
@@ -4,6 +4,18 @@ class OutputHandler {
   printErrorMessage(errorMessage) {
     Console.print(errorMessage);
   }
+
+  printResultMessage() {
+    Console.print('\n실행 결과');
+  }
+
+  printOneRoundResult(roundResult) {
+    roundResult.forEach(car => {
+      const dashCounts = '-'.repeat(car.moveCount);
+      Console.print(`${car.name} : ${dashCounts}`);
+    })
+    Console.print('');
+  }
 }
 
 export default OutputHandler;
