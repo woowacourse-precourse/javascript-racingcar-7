3a883387a6c0fc22d550c010f8dad8036345f48b
rhtjddls123 <jjjk0605@naver.com>
2024-10-28 00:18:09 +0900

    feat: message를 전달받아 사용자에게 입력값을 받는 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    
+  }
+
+  async input(msg){
+    const input = await Console.readLineAsync(`${msg}\n`);
+    return input
+  }
 }
 
 export default App;
