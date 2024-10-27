78c813112d1f965db820f0188f37640d39710be6
Kimyerim <yeye092302@gmail.com>
2024-10-27 23:41:19 +0900

    fix: printRaceResult 메서드 수정에 따른 테스트 코드 수정
    - 자동차 이름과 이동 거리가 한 줄에 출력되는지 확인하도록 수정

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -59,19 +59,10 @@ describe('printRaceResult', () => {
 
     app.printRaceResult();
 
-    expect(printSpy).toHaveBeenCalledWith("pobi : ");
-    expect(printSpy).toHaveBeenCalledWith("-");
-    expect(printSpy).toHaveBeenCalledWith("-");
-    expect(printSpy).toHaveBeenCalledWith("-");
-    expect(printSpy).toHaveBeenCalledWith("\n");
-
-    expect(printSpy).toHaveBeenCalledWith("woni : ");
-    expect(printSpy).toHaveBeenCalledWith("-");
-    expect(printSpy).toHaveBeenCalledWith("-");
-    expect(printSpy).toHaveBeenCalledWith("\n");
-
+    expect(printSpy).toHaveBeenCalledWith("pobi : ---");
+    expect(printSpy).toHaveBeenCalledWith("woni : --");
     expect(printSpy).toHaveBeenCalledWith("jun : ");
-    expect(printSpy).toHaveBeenCalledWith("\n");
+    expect(printSpy).toHaveBeenCalledWith(" ");
 
     printSpy.mockRestore();
   });
@@ -126,37 +117,37 @@ describe('printWinner', () => {
   });
 });
 
-// describe("자동차 경주", () => {
-//   test("기능 테스트", async () => {
-//     // given
-//     const MOVING_FORWARD = 4;
-//     const STOP = 3;
-//     const inputs = ["pobi,woni", "1"];
-//     const logs = ["pobi : -", "woni : ", "최종 우승자 : pobi"];
-//     const logSpy = getLogSpy();
+describe("자동차 경주", () => {
+  test("기능 테스트", async () => {
+    // given
+    const MOVING_FORWARD = 4;
+    const STOP = 3;
+    const inputs = ["pobi,woni", "1"];
+    const logs = ["pobi : -", "woni : ", "최종 우승자 : pobi"];
+    const logSpy = getLogSpy();
 
-//     mockQuestions(inputs);
-//     mockRandoms([MOVING_FORWARD, STOP]);
+    mockQuestions(inputs);
+    mockRandoms([MOVING_FORWARD, STOP]);
 
-//     // when
-//     const app = new App();
-//     await app.run();
+    // when
+    const app = new App();
+    await app.run();
 
-//     // then
-//     logs.forEach((log) => {
-//       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-//     });
-//   });
+    // then
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
 
-//   test("예외 테스트", async () => {
-//     // given
-//     const inputs = ["pobi,javaji"];
-//     mockQuestions(inputs);
+  test("예외 테스트", async () => {
+    // given
+    const inputs = ["pobi,javaji"];
+    mockQuestions(inputs);
 
-//     // when
-//     const app = new App();
+    // when
+    const app = new App();
 
-//     // then
-//     await expect(app.run()).rejects.toThrow("[ERROR]");
-//   });
-// });
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+});
