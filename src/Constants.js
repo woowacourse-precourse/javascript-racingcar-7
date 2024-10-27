37c483646a9b0b9ec82db996f4d35f58be02681d
seonae-j <wkdtjsdo135@naver.com>
2024-10-28 07:25:10 +0900

    feat(Constants): 이름 5자 초과 시 '[ERROR]' 추가
    - nameLimit5 추가

--- src/Constants.js
+++ src/Constants.js
@@ -3,3 +3,9 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 export const getRandomNumber = (min, max) => {
   return MissionUtils.Random.pickNumberInRange(min, max);
 };
+
+export const nameLimit5 = (name) => {
+  if (name.length > 5) {
+    throw new Error('[ERROR] 이름은 5자 이하만 가능합니다.');
+  }
+};
