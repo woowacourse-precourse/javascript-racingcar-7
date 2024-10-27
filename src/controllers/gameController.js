444698c796ff093e48d4e4a8047150c30974377a
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-10-27 16:49:24 +0900

    fix(gameController.js): tryCount 입력 받기 및 racingGame 생성

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -1,9 +1,19 @@
 import { inputHandler, outputHandler } from "../views/index.js";
 import { GAME_MESSAGES, ERROR_MESSAGES } from "../constants/index.js";
+import { randomNumber, carNameParser } from "../utils/index.js";
+import { RacingGame } from "../models/index.js";
 
 const gameController = {
   startGame: async () => {
     const carNames = await inputHandler.carNameInput();
+    const racingTryCount = await inputHandler.racingTryCountInput();
+
+    const game = new RacingGame(carNames, racingTryCount);
+
+    await gameController.playGame(game);
   },
+
+  playGame: (game) => {},
+  endGame: () => {},
 };
 export default gameController;
