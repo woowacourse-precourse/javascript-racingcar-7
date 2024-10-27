82b3e114a5384db1b28ff95707d97ec2871ff1dd
seungwonme <seungwonan.me@gmail.com>
2024-10-28 06:37:05 +0900

    feat: 게임 초기화 로직 및 자동차 객체 생성 추가
    RacingGame 클래스에 게임을 초기화하는 메소드를 추가했습니다.
    - `initializeGame` 메소드를 통해 자동차 이름과 시도 횟수를 입력받고, Car 객체를 생성합니다.
    - 자동차 이름 입력을 위한 `getCarNames` 메소드를 추가하여, 입력값을 쉼표(,)로 구분하여 배열로 반환합니다.
    - 시도 횟수를 입력받는 `getCount` 메소드를 추가하여 입력값을 정수로 변환해 반환합니다.

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -1,4 +1,5 @@
 import { Console, Random } from '@woowacourse/mission-utils';
+import Car from './Car.js';
 
 class RacingGame {
   constructor() {
@@ -6,11 +7,30 @@ class RacingGame {
   }
 
   async play() {
-    const line = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    try {
+      await this.initializeGame();
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
+
+  async initializeGame() {
+    const carNames = await this.getCarNames();
+    const count = await this.getCount();
+    const cars = carNames.map((name) => new Car(name));
+    Console.print(cars);
+  }
+
+  async getCarNames() {
+    const line = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+    );
+    return line.split(',');
+  }
+
+  async getCount() {
     const count = await Console.readLineAsync('시도할 회수는 몇회인가요?\n');
-    const carNames = line.split(',');
-    Console.print(carNames);
-    // Console.print(Random.pickNumberInRange(0, 9));
+    return parseInt(count, 10);
   }
 }
 
