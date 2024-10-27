5b088b150cdef45eb8c76aea7b648f5713e3a242
moonseonghui <msh9980@naver.com>
2024-10-27 16:48:22 +0900

    feat(validateRacingCarInput): 구분자 없이 자동차 이름만 입력한 경우 테스트 케이스 추가

--- __tests__/validator/validateRacingCarInput.test.js
+++ __tests__/validator/validateRacingCarInput.test.js
@@ -4,6 +4,7 @@ import { validateRacingCarInput } from '../../src/validator/validatePipeline';
 describe.each([
   ['알파벳과 쉼표만 포함된 경우1', 'carA,carB,carC'],
   ['알파벳과 쉼표만 포함된 경우2', 'car,ra,carB'],
+  ['알파벳만 포함된 경우2', 'CAR'],
 ])('유효한 문자열 검사: %s', (description, input) => {
   test(`${description} : ${input} `, () => {
     expect(validateRacingCarInput(input)).toBe(input);
