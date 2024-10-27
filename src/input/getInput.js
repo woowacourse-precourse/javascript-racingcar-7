8f2b11fec189e5df5e7dd5cf19274dd00a8d9afe
Junseo Kim <semiwest2@gmail.com>
2024-10-28 06:03:45 +0900

    refactor: 비동기 입력 함수 getInput에서 async/await 제거
    - getInput 함수에서 async/await를 제거하고 프로미스를 직접 반환하도록 변경

--- src/input/getInput.js
+++ src/input/getInput.js
@@ -1,6 +1,6 @@
 import { Console } from '@woowacourse/mission-utils'
 
-export default async function getInput (question) {
-  const response = await Console.readLineAsync(question);
+export default function getInput (question) {
+  const response = Console.readLineAsync(question);
   return response;
 }
