2731e562c2841ceee0395a70141f314b4a79f184
이동준 <idongjun@idongjun-ui-MacBookPro.local>
2024-10-28 00:24:27 +0900

    feat(controllers) : 일단 돌아가게 최소한의 컨트롤러 구현

---
+++ controllers/Game.js
@@ -0,0 +1,61 @@
+import InputViews from "../views/InputViews.js";
+import OutputViews from "../views/OutputViews.js";
+import { Random } from "@woowacourse/mission-utils";
+import ERRORS from "../constants/Errors.js";
+import Race from "../models/Race.js";
+import Car from "../models/Car.js";
+import CONDITIONS from "../constants/Conditions.js";
+
+class Game {
+  #game;
+
+  async start() {
+    const cars = await InputViews.carNameInput();
+    const totalRound = await InputViews.tryCountInput();
+
+    const carNameArr = cars.split(",").map((car) => new Car(car));
+    this.#game = new Race(+totalRound, carNameArr);
+
+    OutputViews.printExecutionResult();
+    for (let i = 0; i < this.#game.getTotalRound(); i++) {
+      Game.updateNumberOfAdvance(carNameArr);
+      OutputViews.printEachRoundResult(carNameArr);
+    }
+
+    const winners = Game.calculateWinners(carNameArr);
+    OutputViews.printFinalWinners(winners);
+  }
+
+  static selectRandomNumber() {
+    try {
+      return Random.pickNumberInRange(
+        CONDITIONS.RANDOM_MIN_NUM,
+        CONDITIONS.RANDOM_MAX_NUM
+      );
+    } catch {
+      throw new Error(ERRORS.GENERAL);
+    }
+  }
+
+  static canCarAdvance(randomNumber) {
+    return randomNumber >= CONDITIONS.ADVANCE_NUM;
+  }
+
+  static updateNumberOfAdvance(racingCars) {
+    racingCars.forEach((racingCar) => {
+      const randomNumber = Game.selectRandomNumber();
+      racingCar.Advance(Game.canCarAdvance(randomNumber));
+    });
+  }
+
+  static calculateWinners(racingCars) {
+    const maxNumberOfAdvance = Math.max(
+      ...racingCars.map((racingCar) => racingCar.getNumberOfAdvance())
+    );
+    return racingCars.filter(
+      (racingCar) => racingCar.getNumberOfAdvance() === maxNumberOfAdvance
+    );
+  }
+}
+
+export default Game;
