03584dd36382fd088be5bfd4b8f3610f7a8c1fb7
kanghaeun <xgkg0330@jnu.ac.kr>
2024-10-27 19:40:01 +0900

    feat: 자동차 이름 입력 처리 및 공백 제거 기능 추가
    - 사용자로부터 입력받은 자동차 이름을 쉼표(,)를 기준으로 분리
    - 각 자동차 이름의 앞뒤 공백을 제거하는 기능 추가

--- src/UserInput.js
+++ src/UserInput.js
@@ -2,5 +2,7 @@ import { Console } from "@woowacourse/mission-utils";
 import { INPUT_MESSAGE } from "./constants/gameMessage.js";
 
 export const enterCarNames = async () => {
-  const carNames = await Console.readLineAsync(INPUT_MESSAGE.carNames);
+  const carNames = (await Console.readLineAsync(INPUT_MESSAGE.carNames))
+    .split(",")
+    .map((carName) => carName.trim());
 };
