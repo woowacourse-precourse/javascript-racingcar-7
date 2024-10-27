13b7eacf55ab3f623df4c05e930389d75f1a6949
전성모 <jeonsm22@daum.net>
2024-10-27 19:16:22 +0900

    feat : 차 이름에 공백이 존재하는지 확인하는 함수 추가

--- src/functions.js
+++ src/functions.js
@@ -1,13 +1,24 @@
 export function splitComma(string){
     let commaSplitList = string.split(",");
-    isFiveLength(commaSplitList);
+    isCorrectList(commaSplitList);
     return commaSplitList;
 }
 
-function isFiveLength(list){
+function isCorrectList(list){
     list.forEach((carName) => {
-        if(carName.length > 5){
-            throw new Error("[ERROR] 차 이름 길이가 5를 초과했습니다.")
-        }
+        isFiveLengthOver(carName);
+        isExistGap(carName);
     })
+}
+
+function isFiveLengthOver(carName){
+    if(carName.length > 5){
+        throw new Error("[ERROR] 차 이름 길이가 5를 초과했습니다.")
+    }
+}
+
+function isExistGap(carName){
+    if(/\s/.test(carName)){
+        throw new Error("[ERROR] 차 이름에 공백이 있습니다.")
+    }
 }
