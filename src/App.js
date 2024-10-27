2f7e2e83bedda8909b42b8be8950b9d600cd994c
Hina0222 <hina0222@naver.com>
2024-10-28 02:28:59 +0900

    feat: 쉼표를 기준으로 이름 부여 구현

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const CARS_INPUT = await MissionUtils.Console.readLineAsync("경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분)\n");
-    const TRY_INPUT = await MissionUtils.Console.readLineAsync("시도할 횟수\n");
+    //const TRY_INPUT = await MissionUtils.Console.readLineAsync("시도할 횟수\n");
+    const carList = CARS_INPUT.split(',');
 
   }
 }
