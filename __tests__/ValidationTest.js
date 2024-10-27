08b446b349a1fd0516994f80b12b8586f79b9afb
jeongeunLee <leeju1009@gmail.com>
2024-10-27 17:46:39 +0900

    refactor(validation): 테스트에 실패하는 case 파악을 위해 설명 추가

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -2,16 +2,16 @@ import Validation from '../src/Validation.js';
 
 describe('유저 입력값 예외 처리를 검사하는 테스트', () => {
   test.each([
-    ['woowa', true],
-    ['tech', true],
-    ['course', false],
-    ['1', true],
-    ['123', true],
-    ['', false],
-    [' ', false],
-    ['5자 초과 이름', false],
-    ['5자 이하', true],
-  ])('자동차 이름 길이를 예외 처리하는 함수 테스트', (name, expected) => {
+    ['woowa', true, '5자'],
+    ['tech', true, '4자'],
+    ['course', false, '5자 초과'],
+    ['1', true, '1자'],
+    ['123', true, '1자 이상 5자 이하'],
+    ['', false, '1자 미만'],
+    [' ', false, '공백 포함 1자'],
+    ['5자 초과 이름', false, '공백 포함 5자 초과'],
+    ['5자 이하', true, '공백 포함 5자'],
+  ])('자동차 이름의 길이 예외 처리 테스트 (%s)', (name, expected) => {
     const ERROR_PREFIX = '[ERROR]';
 
     if (expected) {
