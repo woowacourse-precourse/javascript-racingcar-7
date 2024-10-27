dd1acb9a1badaf3ca8a953c222e54f6ced94f4ca
이은호 <eunho@ieunhoui-MacBookAir.local>
2024-10-27 19:50:03 +0900

    fix : 함수명 변경으로 인한 App.js 함수명 변경

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { inputCars } from "./inputs/inputCars.js";
 import { inputMoveCount } from "./inputs/inputMoveCount.js";
 import { startRace } from "./outputs/startRace.js";
-import { checkWinner } from "./outputs/checkWinner.js";
+import { checkWinners } from "./outputs/checkWinner.js";
 
 class App {
   async run() {
     const cars = await inputCars();
     const moveCount = await inputMoveCount();;
     startRace(cars, moveCount);
-    checkWinner(cars);
+    checkWinners(cars);
   }
 }
 
