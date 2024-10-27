43cc2714c064d9632f34a982fc947fcd70b36410
minji2219 <200146@jnu.ac.kr>
2024-10-27 21:43:42 +0900

    feat: 함수명, 변수명 수정
    불분명한 함수명 분명하게 재지정

--- src/car/splitCarNames.js
+++ src/car/splitCarNames.js
@@ -1,4 +1,4 @@
-export const seperateCar = (cars) => {
+export const splitCarNames = (cars) => {
   const carArray = cars.split(',').map((car) => {
     if (car.length > 5) {
       throw new Error('[ERROR] 자동차 이름은 다섯글자 이하로 작성해주세요.');
--- src/race/runRace.js
+++ src/race/runRace.js
@@ -1,13 +1,13 @@
 import { Console } from '@woowacourse/mission-utils';
-import { seperateCar } from '../car/seperateCar.js';
+import { splitCarNames } from '../car/splitCarNames.js';
 import { runRaceCycle } from './runRaceCycle.js';
-import { getWinner } from './getWinner.js';
+import { findWinners } from './findWinners.js';
 
 export const runRace = (cars, repeat) => {
   if (!Number(repeat))
     throw new Error('[ERROR] 시도할 횟수는 숫자로 입력해주세요.');
 
-  const carsArray = seperateCar(cars);
+  const carsArray = splitCarNames(cars);
   const cumulativeResults = new Array(carsArray.length).fill('');
 
   Console.print('\n실행 결과');
@@ -15,5 +15,5 @@ export const runRace = (cars, repeat) => {
   for (let i = 1; i <= Number(repeat); i++) {
     runRaceCycle(carsArray, cumulativeResults);
   }
-  Console.print('최종 우승자 : ' + getWinner(carsArray, cumulativeResults));
+  Console.print('최종 우승자 : ' + findWinners(carsArray, cumulativeResults));
 };
