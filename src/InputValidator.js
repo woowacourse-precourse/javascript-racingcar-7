d8ecb5163936747504be8166590b74ea8a060a89
hyeongdong <khd941018@gmail.com>
2024-10-27 21:52:59 +0900

    refactor(InputValidator): 유효하지 않은 값인 경우 에러를 발생시키도록 코드 수정

--- src/InputValidator.js
+++ src/InputValidator.js
@@ -1,27 +1,48 @@
 class InputValidator {
-  static isPositiveNumber(number) {
-    return Number.isInteger(number) && number > 0;
-  }
-
-  static hasNoName(carNames) {
-    return carNames.some((carName) => carName.trim().length === 0);
-  }
-
-  static hasSameName(carNames) {
-    const differentNames = new Set(carNames);
-    if (differentNames.size < carNames.length) {
-      return true;
+  static checkPositive(number) {
+    if (!Number.isInteger(number) || number < 1) {
+      throw new Error("이동횟수는 양의 정수로 입력해주세요.");
     }
 
-    return false;
+    return true;
   }
 
-  static hasSingleName(carNames) {
-    return carNames.length < 2;
+  static checkBlank(carNames) {
+    const hasBlankName = carNames.some((carName) => carName.trim().length === 0);
+
+    if (hasBlankName) {
+      throw new Error("[ERROR]자동차 이름을 입력해주세요.");
+    }
+
+    return true;
   }
 
-  static hasLongName(carNames) {
-    return carNames.some((carName) => carName.length > 5);
+  static checkDuplication(carNames) {
+    const differentNames = new Set(carNames);
+
+    if (differentNames.size < carNames.length) {
+      throw new Error("[ERROR]서로 다른 이름을 입력해주세요.");
+    }
+
+    return true;
+  }
+
+  static checkMultiple(carNames) {
+    if (carNames.length < 2) {
+      throw new Error("[ERROR]두 대 이상의 자동차를 콤마(,)로 구분해서 입력해주세요.");
+    }
+
+    return true;
+  }
+
+  static checkNameLength(carNames) {
+    const hasLongName = carNames.some((carName) => carName.length > 5);
+
+    if (hasLongName) {
+      throw new Error("[ERROR]자동차 이름은 5자 이하로 입력해주세요.");
+    }
+
+    return true;
   }
 }
 
