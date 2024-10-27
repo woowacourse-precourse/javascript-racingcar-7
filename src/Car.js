1ab79af1f2ca10090aad8aefe62d0c9e12adef69
seonae-j <wkdtjsdo135@naver.com>
2024-10-28 06:22:06 +0900

    refactor(Car): location이 움직이는 조건 추가
    - 기존의 Controller에 있던 location 조건을 Car로 가져와서 각 자동차마다 적용되도록 생성

--- src/Car.js
+++ src/Car.js
@@ -1,6 +1,15 @@
+import { getRandomNumber } from './Constants.js';
+
 export class Car {
   constructor(name) {
     this.name = name;
     this.location = 0;
+  }
+
+  move() {
+    const randomNumber = getRandomNumber(0, 9);
+    if (randomNumber >= 4) {
+      this.location++;
+    }
   }
 }
--- src/Controller.js
+++ src/Controller.js
@@ -1,18 +1,7 @@
-import { getRandomNumber } from './Random.js';
-
-// 경주 진행
 export class raceController {
-  // 자동차 이름, 경주 횟수
   constructor(inputNames, inputAttemps) {
     this.cars = inputNames.split(',').map((name) => name.trim());
     this.attemps = inputAttemps;
     this.location = 0;
-  }
-
-  moveLocation() {
-    const randomNumber = getRandomNumber(0, 9);
-    if (randomNumber >= 4) {
-      this.location++;
-    }
   }
 }
