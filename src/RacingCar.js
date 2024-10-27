0452f3f224b677db29c8ee227a61591c047540cf
dudcns02 <duddl2640@naver.com>
2024-10-27 12:09:46 +0900

    feat: Add forward conditions Take a random number and add it to the forward condition

--- src/RacingCar.js
+++ src/RacingCar.js
@@ -1,13 +1,31 @@
 import { Console } from "@woowacourse/mission-utils";
-import { carCountLimitCheck } from "./Utils.js";
+import { carCountLimitCheck, checkTryNum } from "./Utils.js";
 import { LOG_MESSAGE } from "./content.js";
 class RacingCar {
-  constructor() {}
+  constructor(tryNum) {
+    tryNum = new tryNum();
+  }
   async getCarName() {
+    //자동차 입력 받기
     const input = await Console.readLineAsync(LOG_MESSAGE.START_MESSAGE);
     const carInput = input.split(",");
     carCountLimitCheck(carInput);
   }
+  static async getTryNum() {
+    //시도할 횟수 입력 받기
+    const numInput = await Console.readLineAsync(LOG_MESSAGE.TRY_NUM_MESSAGE);
+    const tryNum = numInput;
+    checkTryNum(tryNum);
+  }
 }
-
+export function startRace(tryNum) {
+  for (let i = 0; i < tryNum; i++) {
+    for (const currentCar in carName) {
+      const randomNum = Random.pickNumberInRange(0, 9);
+      if (randomNum >= 4) {
+        carName[currentCar]++;
+      }
+    }
+  }
+}
 export default RacingCar;
