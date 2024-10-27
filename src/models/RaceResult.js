11c1cc2f57e381f5ba361ebead86c3ad2d91aa61
CJY <cjy3458@naver.com>
2024-10-28 03:20:40 +0900

    feat: 최종 우승자 출력 기능 구현

--- src/models/RaceResult.js
+++ src/models/RaceResult.js
@@ -1,6 +1,8 @@
+import getMaxAdvance from '../utils/getMaxAdvance.js';
+
 class RacingGameResult {
   static determineWinners(cars) {
-    const maxAdvance = Math.max(...cars.map((car) => car.advance));
+    const maxAdvance = getMaxAdvance();
     return cars
       .filter((car) => car.advance === maxAdvance)
       .map((car) => car.name);
