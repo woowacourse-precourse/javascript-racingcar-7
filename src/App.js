ee4fb6aa67976e1e9e14c9eb071b0da32c083d71
JAESEUNGJAE <valkyrie9923@gmail.com>
2024-10-28 04:40:04 +0900

    feat: getCarNames() 함수 구현

--- src/App.js
+++ src/App.js
@@ -13,6 +13,14 @@ class App {
       throw error;
     }
   }
+  async getCarNames() {
+    const input = await MissionUtils.Console.readLineAsync("자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분).");
+    const names = input.split(",");
+    if (names.some(name => name.length > 5)) {
+      throw new Error("[ERROR] 자동차 이름은 5자 이하로 입력해야 합니다.");
+    }
+    return names;
+  }
 
   
 }
