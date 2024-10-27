63e5c829bb62059b8f9cdd63f37a6a8afe6ee039
jiHeeFlee <esder1310@naver.com>
2024-10-27 15:08:19 +0900

    feat(App.js) 1. add car input : save separate by commas / assign variables

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-class App {
-  async run() {}
-}
+import {Console} from "@woowacourse/mission-utils"
 
-export default App;
+class App{
+  async run(){
+    // 1. 자동차 입력 : 자동차 입력 받기
+    const car_input=await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    Console.print(car_input)
+    // 1. 변수 할당 : 각 자동차의 게임 진행 상황 반영 리스트
+    let car_names=[]
+    let game_result=[]
+
+    // 1. 자동차 입력 : 입력받은 데이터를 쉼표(,)로 구분하여 리스트 요소로 저장
+    if(car_input.includes(",")){
+      car_names=car_input.split(",");
+    }
+  }
+}
