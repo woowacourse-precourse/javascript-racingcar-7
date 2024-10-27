6417de09a60e8635cc18d6747fe2a9321881ad0a
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-10-27 19:57:39 +0900

    feat: 자동차 이름 5자 초과면 에러 발생시키는 메소드 추가

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -2,4 +2,13 @@ const INPUT_MESSAGE = Object.freeze({
   carNames: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
 });
 
-export { INPUT_MESSAGE };
+const CAR_NAME_LENGTH = Object.freeze({
+  minRange: 1,
+  maxRange: 5,
+});
+
+const ERROR_MESSAGE = Object.freeze({
+  carNameError: '[ERROR] 이름은 5자 이하로 적어주세요.',
+});
+
+export { INPUT_MESSAGE, CAR_NAME_LENGTH, ERROR_MESSAGE };
--- src/models/Car.js
+++ src/models/Car.js
@@ -1,8 +1,20 @@
+import { CAR_NAME_LENGTH, ERROR_MESSAGE } from '../constants/constant';
+
 class Car {
   #carName;
 
   constructor(inputName) {
+    this.#isCarNameValidLength(inputName);
     this.#setCarName(inputName);
+  }
+
+  #isCarNameValidLength(carName) {
+    if (
+      carName.length > CAR_NAME_LENGTH.maxRange ||
+      carName.length < CAR_NAME_LENGTH.minRange
+    ) {
+      throw new Error(ERROR_MESSAGE.carNameError);
+    }
   }
 
   #setCarName(inputName) {
