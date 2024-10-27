edf8e201e002e77bad25d0742505420fe60379ff
Jun Hee Jung <wjdtka1325@naver.com>
2024-10-28 01:15:12 +0900

    feat(car): 사용자 입력값 예외 처리

--- src/car.js
+++ src/car.js
@@ -1,9 +1,11 @@
 class Car {
   constructor(name) {
+    if (name.length > 5 || name.length === 0)
+      throw new Error('[ERROR] 잘못 입력하였습니다.');
     this.name = name;
     this.forwardCount = 0;
   }
-  
+
   forward() {
     this.forwardCount++;
   }
