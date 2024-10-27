e84e0a7ca81674fbf15df7c729a408345db1cbf5
MERx88 <wjang07@gmail.com>
2024-10-27 11:08:24 +0900

    Feat(App): add feature to verify multiple names
    - Add feature to check for at least two cars for the race
    - Display an error message if fewer than two cars are provided.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async readCarNames() {
-    const userInput = await Console.readLineAsync(
+    const userCarNames = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
-    return userInput;
+    if (!userCarNames.includes(",")) {
+      throw new Error("[ERROR] 자동차 이름을 두개 이상 입력해주세요");
+    }
+    return userCarNames;
   }
 
   async run() {
