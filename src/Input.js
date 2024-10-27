a5abd93d25d7f06b710121d0b6e72845ef3cc42e
Kim_Yjoo <pas4976@gmail.com>
2024-10-28 02:57:27 +0900

    feat(Race) : 경주에 참가하는 자동차의 객체를 생성하고, 회차의 이동여부를 출력하는 기능 추가

--- src/Input.js
+++ src/Input.js
@@ -2,35 +2,24 @@ import { Console } from '@woowacourse/mission-utils';
 import * as cs from './constants.js';
 class UserInput {
     constructor(){
+        this.carsNameString = '';
+        this.carsNameList = [];
+        this.attemptNum = 0;
     }
     async initialize(){
-        this.carNames = await this.setCarNames();
-        this.attemptNum = await this.setAttemptNum();
-    }
-    async setCarNames(){
-        let namesString = await Console.readLineAsync('경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n');
-        return await this.setNameProcess(namesString);
-    }
-    async setAttemptNum(){
+        this.carsNameString = await Console.readLineAsync('경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n');
+        await this.separateCarsNameString();
+        await this.checkCarNames();
         this.attemptNum = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
     }
-    async setNameProcess(namesString){
-        let separatedString = this.separateCarsName(namesString);
-        this.checkCarNames(await separatedString);
-        return await separatedString;
-    }
-
-    async separateCarsName(namesString){
-        let separatedString = [];
-        let namesNominees = namesString.split(',');
-        for(let carName of namesNominees) {
-            separatedString.push(carName.trim());
+    async separateCarsNameString(){
+        this.carsNameList = this.carsNameString.split(',');
+        for(let nameIndex in this.carsNameList) {
+            this.carsNameList[nameIndex] = this.carsNameList[nameIndex].trim();
         }
-        
-        return await separatedString;
-    }
-    async checkCarNames(carNameCandidates){
-        for(let candidate of carNameCandidates){
+    }   
+    async checkCarNames(){
+        for(let candidate of this.carsNameList){
             await this.checkNamingRules(candidate);
         }
     }
@@ -43,6 +32,21 @@ class UserInput {
             throw new Error('[ERROR] 자동차 이름 에러. (특수문자 사용 불가)')
         }   
     }   
+
+    async viewAttemptResult(cars){
+        for(let car of cars){
+            let printString = car.name + ' : ' + await this.makeDistanceHyphen(car);
+            Console.print(printString);
+        }
+        Console.print('');
+    }
+    async makeDistanceHyphen(car){
+        let hyphens = '';
+        for(let index = 0; index < car.distance; index++){
+            hyphens += '-';
+        }
+        return hyphens;
+    }
 }
 
 export default UserInput;
--- src/Race.js
+++ src/Race.js
@@ -1,27 +1,61 @@
-// import { Console } from '@woowacourse/mission-utils';
+import { Console } from '@woowacourse/mission-utils';
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 // import UserInput from "./Input.js";
 
 class Car{
     constructor(name) {
         this.name = name;
+        this.distance = 0;
     }
     async getName(){
         return this.name;
+    }
+    async getDistance(){
+        return this.distance;
+    }
+    async goOrStop(){
+        let rand = MissionUtils.Random.pickNumberInRange(0, 9);
+        if(rand >= 4){
+            this.distance++;
+        }
+        Console.print(this.distance);
     }
 }
 
 class Race{
     constructor(input){
-        this.candidateCars = this.setCars(input.carNames);
+        this.input = input;
+        this.doRace();
     }
 
-    async setCars(carNames) {
-        let cars = [];
-        for(let carName of carNames){
-            cars.push(new Car(carName));
+    async doRace(){
+        await this.setRace();
+        for(let nowAttempt = 0; nowAttempt < this.input.attemptNum; nowAttempt++){
+            await this.startRaceAttempt();
         }
-        return cars;
     }
+    async setRace() {
+        this.cars = await this.setCars();
+        
+    }
+
+    async setCars() {
+        let carObjects = [];
+        for(let carName of this.input.carsNameList){
+            carObjects.push(new Car(carName));
+        }
+        return carObjects;
+    }
+
+    async startRaceAttempt() {
+        for(let carIndex in this.cars){
+            await this.cars[carIndex].goOrStop();
+            await this.input.viewAttemptResult(this.cars);
+        }
+    }
+
+    
 }
 
 export default Race;
