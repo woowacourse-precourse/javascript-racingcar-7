02d105a724b6ef4cb3aa8e3db0ac7f66db45b88e
Minji Kim <minjikim2000@hotmail.com>
2024-10-28 06:29:07 +0900

    refactor: racingGame에 우승자 출력 함수 추가

--- src/App.js
+++ src/App.js
@@ -6,17 +6,8 @@ class App {
   async run() {
     const playerNames = await InputHandlers.getPlayerNames();
     const trials = await InputHandlers.getTrials();
-    const players = racingGame(playerNames, trials);
-    this.getWinners(players);
-  }
 
-  getWinners(players) {
-    const maxPosition = Math.max(...players.map((player) => player.position));
-    const winners = players
-      .filter((player) => player.position === maxPosition)
-      .map((player) => player.name);
-
-    Console.print(`최종 우승자 : ${winners.join(", ")}`);
+    racingGame(playerNames, trials);
   }
 }
 
--- src/racingGame.js
+++ src/racingGame.js
@@ -11,11 +11,21 @@ export function racingGame(playerNames, trials) {
     });
     Console.print("");
   }
-  return players;
+
+  getWinners(players);
 }
 
 function movePlayer(player) {
   if (Random.pickNumberInRange(0, 9) >= 4) {
     player.position += 1;
   }
+}
+
+function getWinners(players) {
+  const maxPosition = Math.max(...players.map((player) => player.position));
+  const winners = players
+    .filter((player) => player.position === maxPosition)
+    .map((player) => player.name);
+
+  Console.print(`최종 우승자 : ${winners.join(", ")}`);
 }
