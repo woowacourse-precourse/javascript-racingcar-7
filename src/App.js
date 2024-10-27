6d3d3ffa5592634571c555bb6b8c46a148ab4992
YongHyunKing <yhkim8246@gmail.com>
2024-10-28 05:05:58 +0900

    feat(App): 자동차 이름을 입력하는 즉시 검증하기 위한 코드 순서 변경

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import RacingCar from './RacingCar.js';
 class App {
   async run() {
     const NAMES = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-    const TIMES = Number(await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n'));
-
-    if (isNaN(TIMES) || !Number.isInteger(TIMES) || TIMES <= 0) throw new Error('[Error] 시도할 횟수는 양의 정수만 가능합니다.');
-
     const splitNames = NAMES.split(',').map(name => name.trim());
-
     const cars = [];
     for (const carName of splitNames) cars.push(new RacingCar(carName));
+
+    const TIMES = Number(await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n'));
+    if (isNaN(TIMES) || !Number.isInteger(TIMES) || TIMES <= 0) throw new Error('[Error] 시도할 횟수는 양의 정수만 가능합니다.');
 
     for (let i = 0; i < TIMES; i++) {
       for (const car of cars) {
@@ -26,6 +24,7 @@ class App {
       .filter(car => car.getDistance() === maxDist)
       .map(car => car.getName())
       .join(`, `);
+
     MissionUtils.Console.print("최종 우승자 : " + maxDistCars);
   }
 }
