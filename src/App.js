dc1a697fa6872be9e46a1ceee991b976c4f38095
ohgus <123801385+ohgus@users.noreply.github.com>
2024-10-27 22:36:14 +0900

    [style]: 공백 라인 추가

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App {
 
     const raceResult = this.getRaceResult(carNamesInput, moveAttemptCountInput);
     const winners = this.getWinners(raceResult);
+
     this.printWinners(winners);
   }
 
