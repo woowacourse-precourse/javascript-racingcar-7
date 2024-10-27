0071fff75eb32b8a710093c5ecf130cb7ed54321
yuri295 <jin_astro0302@naver.com>
2024-10-27 17:34:53 +0900

    feat: 자동차 이름 쉼표로 구분 및 개수 확인

--- src/App.js
+++ src/App.js
@@ -5,6 +5,25 @@ class App {
     const nameInput = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
+
+    const nameList = this.splitNameInput(nameInput);
+    const carNames = this.validateNameList(nameList);
+  }
+  catch(error) {
+    throw new Error(`[ERROR] ${error.message}`);
+  }
+
+  splitNameInput(nameInput) {
+    const carNames = nameInput.split(",");
+
+    return carNames;
+  }
+
+  validateNameList(nameList) {
+    if (nameList.length === 1) {
+      throw new Error("자동차 이름은 쉼표로 구분하여 2개 이상 입력해 주세요.");
+    }
   }
 }
+
 export default App;
