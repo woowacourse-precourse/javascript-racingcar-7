3a2d80226023ba83e63e1fba1180dfa5df9467f6
최영빈 <bin778@naver.com>
2024-10-27 15:33:26 +0900

    feat(Check): 자동차가 2개인지 검사 추가

--- src/Component/Check.js
+++ src/Component/Check.js
@@ -1,11 +1,13 @@
 import {
   ERR_LENGTH,
   ERR_COR_NAME,
-  ERR_POSITIVE,
   ERR_ISDUP,
+  ERR_TWO_CAR,
+  ERR_POSITIVE,
   ERR_ISINT,
 } from "./Error.js";
 
+// 자동차 이름에 중복이 있는지 검사
 function isDuplicate(arr) {
   const isDup = arr.some(function (x) {
     return arr.indexOf(x) !== arr.lastIndexOf(x);
@@ -14,28 +16,29 @@ function isDuplicate(arr) {
   return isDup;
 }
 
-export const checkCarNames = (car_names_arr) => {
-  // 자동차 이름 길이가 1~5 사이인지 검사
-  const str_len_check = car_names_arr.some(
-    (str) => str.length <= 0 || str.length >= 6
-  );
+// 자동차 이름 길이가 1~5 사이인지 검사
+export const carLenCheck = (arr) => {
+  return arr.some((str) => str.length <= 0 || str.length >= 6);
+};
 
-  // 입력한 값이 한글, 영어, 숫자, 특수문자(.^*#!;)만 있는지 검사
+// 입력한 값이 한글, 영어, 숫자, 특수문자(.^*#!;)만 있는지 검사
+export const carNameCheck = (arr) => {
   const reg = /^[ㄱ-ㅎ가-힣a-zA-Z0-9.^*#!;]+$/;
-  const str_reg_check = car_names_arr.every((str) => reg.test(str));
+  return arr.every((str) => reg.test(str));
+};
 
-  // 자동차 이름에 중복이 있는지 검사
+export const checkCarNames = (car_names_arr) => {
+  const str_len_check = carLenCheck(car_names_arr);
+  const str_reg_check = carNameCheck(car_names_arr);
   const car_name_dup = isDuplicate(car_names_arr);
 
   if (str_len_check) throw new Error(ERR_LENGTH());
   if (!str_reg_check) throw new Error(ERR_COR_NAME());
   if (car_name_dup) throw new Error(ERR_ISDUP());
+  if (car_names_arr.length <= 1) throw new Error(ERR_TWO_CAR());
 };
 
 export const checkAttemptNumber = (attempt_number) => {
-  // 입력한 값이 숫자인지 검사
-  const reg = /^[0-9\s]*$/;
-
   if (attempt_number <= 0) throw new Error(ERR_POSITIVE());
-  if (!reg.test(attempt_number)) throw new Error(ERR_ISINT());
+  if (!/^[0-9\s]*$/.test(attempt_number)) throw new Error(ERR_ISINT());
 };
--- src/Component/Error.js
+++ src/Component/Error.js
@@ -14,6 +14,10 @@ export const ERR_ISDUP = () => {
   return ERR_MESSAGE + " 자동차 이름은 각각 달라야 합니다.";
 };
 
+export const ERR_TWO_CAR = () => {
+  return ERR_MESSAGE + " 자동차 이름은 2개 이상이여야 합니다.";
+};
+
 export const ERR_POSITIVE = () => {
   return ERR_MESSAGE + " 1 이상의 숫자를 입력해야 합니다.";
 };
