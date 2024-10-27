c765407806266bbdeb05b07ea60c2e8b69a241cc
0Jaemin0 <jaemin2167@naver.com>
2024-10-27 20:49:43 +0900

    feat: 에러 메시지 상수로 정의

---
+++ src/constants/errorMessages.js
@@ -0,0 +1,9 @@
+const ERROR_MESSAGES = {
+  EMPTY_CAR_NAME: '자동차 이름은 한 글자 이상이어야 합니다.',
+  CONTAINS_WHITESPACE: '자동차 이름에 공백이 포함되어 있습니다.',
+  LESS_THAN_TWO_CARS: '두 대 이상의 자동차 이름을 입력해야 합니다.',
+  DUPLICATE_CAR_NAME: '자동차 이름은 중복될 수 없습니다.',
+  CAR_NAME_TOO_LONG: '자동차 이름은 5자를 초과할 수 없습니다.',
+};
+
+export default ERROR_MESSAGES;
