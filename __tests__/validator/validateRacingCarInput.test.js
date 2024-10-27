af4d44edbcc349b61bdd99f34754a4a0b81a841b
moonseonghui <msh9980@naver.com>
2024-10-27 16:45:33 +0900

    test(validateRacingCarInput): validateRacingCarInput 함수의 유효한 문자열 테스트 케이스 추가

---
+++ __tests__/validator/validateRacingCarInput.test.js
@@ -0,0 +1,11 @@
+import { validateRacingCarInput } from '../../src/validator/validatePipeline';
+
+// 유효한 문자열 테스트 케이스
+describe.each([
+  ['알파벳과 쉼표만 포함된 경우1', 'carA,carB,carC'],
+  ['알파벳과 쉼표만 포함된 경우2', 'car,ra,carB'],
+])('유효한 문자열 검사: %s', (description, input) => {
+  test(`${description} : ${input} `, () => {
+    expect(validateRacingCarInput(input)).toBe(input);
+  });
+});
