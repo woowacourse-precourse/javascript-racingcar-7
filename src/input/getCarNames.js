692ff042f26e210a5ca14c2d33282bd3c4195d41
minjeong9919 <minjeong9919@naver.com>
2024-10-28 03:00:08 +0900

    fix: 입력 값 공백 제거

--- src/input/getCarNames.js
+++ src/input/getCarNames.js
@@ -5,7 +5,9 @@ export const getCarNames = async () => {
   const racingCarsInput = await Console.readLineAsync(
     "경주할 자동차 이름을 입력 하세요.(이름은 쉼표(,) 기준 으로 구분)\n",
   );
-  const racingCars = racingCarsInput.split(",");
+  const racingCars = racingCarsInput
+    .split(",")
+    .map(racingCar => racingCar.trim());
 
   racingCars.map(racingCar => {
     if (racingCar.length > 5) {
