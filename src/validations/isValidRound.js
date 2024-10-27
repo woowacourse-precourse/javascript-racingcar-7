3e60ad1228f140b82fe54ea303e69db5ea857539
subsub-e <ab13696802@gmail.com>
2024-10-28 00:21:08 +0900

    fix(isValidRound) : 맨 뒤에 숫자 대신 문자가 와도 에러로 출력하지 않는 버그 해결

--- src/validations/isValidRound.js
+++ src/validations/isValidRound.js
@@ -1,10 +1,11 @@
-import { ERROR_MESSAGES } from "../constants/errorConstants";
+import { ERROR_MESSAGES } from "../constants/errorConstants.js";
 
 export function isValidRound(round) {
   if (round === "") {
     throw new Error(ERROR_MESSAGES.EMPTY_INPUT_ROUND);
   }
-  if (!(parseInt(round, 10) > 0)) {
+  if (!(Number(round, 10) > 0)) {
     throw new Error(ERROR_MESSAGES.INVALID_ROUND_NUMBER);
   }
+  console.log(parseInt(round, 10));
 }
