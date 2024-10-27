083effae9307d113cac92db0a9836a9c90a151f3
@u_limit_ <yurim@ajou.ac.kr>
2024-10-27 18:42:27 +0900

    fix: 자동차 이름 입력 input 메세지 수정

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const determineCarName = (input) => {
 
 class App {
   async run() {
-  const input = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\\n');
+  const input = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
   determineCarName(input);
   const raceNumber = await  Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
   }
