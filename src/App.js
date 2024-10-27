422363c38f03717e58a3d3a2af58bc813386f752
JAESEUNGJAE <valkyrie9923@gmail.com>
2024-10-28 04:39:17 +0900

    feat: run 함수에서 기본 입력값 받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const names = await this.getCarNames();
+      const attempts = await this.getAttempts();
+      const cars = this.initializeCars(names);
+      this.startRace(cars, attempts);
+      this.printWinners(cars);
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+      throw error;
+    }
+  }
+
+  
 }
 
 export default App;
