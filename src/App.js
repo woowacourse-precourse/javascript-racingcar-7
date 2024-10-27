63e540a34a490f6b8076e8ef9b2e2f2f3ffaaadb
devjaeyoon <devjaeyoon@gmail.com>
2024-10-27 11:09:01 +0900

    refactor: 입력 검증 함수를 Validator의 정적 메소드로 통합하여 수정

--- src/App.js
+++ src/App.js
@@ -3,17 +3,16 @@ import getAttemptsInput from './getAttemptsInput.js';
 import getCarNamesInput from './getCarNamesInput.js';
 import playGame from './playGame.js';
 import splitStringByComma from './splitIStringByComma.js';
-import validateAttempts from './validateAttempts.js';
-import validateCarNames from './validateCarNames.js';
+import Validator from './utils/Validator.js';
 
 class App {
   async run() {
     const carNamesInput = await getCarNamesInput();
     const carNames = splitStringByComma(carNamesInput);
-    validateCarNames(carNames);
+    Validator.validateCarNames(carNames);
 
     const attemptsInput = await getAttemptsInput();
-    validateAttempts(attemptsInput);
+    Validator.validateAttempts(attemptsInput);
     const attempts = converStringToNumber(attemptsInput);
 
     playGame(carNames, attempts);
