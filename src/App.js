048817a48d4743c943bdd9a5d4310921257d1ad6
ockko <ocko119@gmail.com>
2024-10-27 10:05:25 +0900

    feat: 자동차 이름 5자 초과 입력 시 오류 처리

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ class App {
     const carname = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
+
+    const removedWhitespace = carname.replace(/ /g, "");
+    const cars = removedWhitespace.split(",");
+
+    cars.forEach((car) => {
+      if(car.length > 5) {
+        throw new Error("[ERROR] 이름은 5자 이하만 가능해요.")
+      }
+    });
+
     const tryCount = await MissionUtils.Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?\n"
     );
@@ -23,8 +33,6 @@ class App {
       };
     }
 
-    const removedWhitespace = carname.replace(/ /g, "");
-    const cars = removedWhitespace.split(",");
     const carObjs = cars.map((name) => new Car(name));
 
     function printResult() {
