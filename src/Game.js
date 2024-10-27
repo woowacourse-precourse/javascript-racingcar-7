a9447cc9571e4a944a9562fa8ecf5ee6e82bf8f8
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-10-27 10:24:35 +0900

    feat: 우승자 판별 로직 추가 (Game)
    - 각 라운드마다 최고 위치를 갱신하고, 공동 우승자 리스트를 관리
    - 게임 종료 후 최종 우승자를 OutputView에 전달하여 출력

--- src/Game.js
+++ src/Game.js
@@ -5,16 +5,30 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 class Game {
   constructor(carNames) {
     this.cars = carNames.map((name) => new Car(name));
+    this.maxPosition = 0;
+    this.winners = [];
   }
 
   start(rounds) {
     for (let i = 0; i < rounds; i++) {
       this.cars.forEach((car) => {
         const randomValue = MissionUtils.Random.pickNumberInRange(0, 9);
-        if (randomValue >= 4) car.move();
+        if (randomValue >= 4) {
+          car.move();
+          if (car.getPosition() > this.maxPosition) {
+            this.maxPosition = car.getPosition();
+            this.winners = [car.getName()];
+          } else if (car.getPosition() === this.maxPosition) {
+            this.winners.push(car.getName());
+          }
+        }
       });
       OutputView.printPositions(this.cars);
     }
+  }
+
+  getWinners() {
+    return this.winners;
   }
 }
 
