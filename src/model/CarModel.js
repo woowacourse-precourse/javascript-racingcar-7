780953f6c707ce03c1aa3e9e094fa9dcc0c93c3e
ryanbae94 <ryanbae94@gmail.com>
2024-10-28 00:22:31 +0900

    feat (CarModel, numbers): 이동 기능
    - CarModel의 이동 기능 구현
    	- randomNumber를 받아서 이동 가능할 경우 현재 위치를 1만큼 더함
    - 이동 가능한 숫자(4)를 저장하기 위한 상수 분리

--- src/model/CarModel.js
+++ src/model/CarModel.js
@@ -1,8 +1,20 @@
+import { MINIMUM_RANDOMNUMBER_TO_MOVE } from '../constatns/numbers';
+
 class CarModel {
 	#name;
 	#currentPosition;
 	constructor(name) {
 		this.#name = name;
 		this.#currentPosition = 0;
+	}
+
+	move(randomNumber) {
+		if (canMove(randomNumber)) {
+			this.#currentPosition++;
+		}
+	}
+
+	canMove(randomNumber) {
+		return randomNumber >= MINIMUM_RANDOMNUMBER_TO_MOVE;
 	}
 }
