1894befc99d7a22cb5b77cf96a97571bc2c05113
erica-jeong <wjddbwjd1205@gmail.com>
2024-10-28 02:38:54 +0900

    refactor: 불필요한 CarRacingGame import 제거
    - App.js에서 사용되지 않는 CarRacingGame import 삭제

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import CarRacingManager from './CarRacingManager.js';
 import InputHandler from './InputHandler.js';
 import OutputHandler from './OutputHandler.js';
-import CarRacingGame from './CarRacingGame.js';
 
 class App {
   async run() {
     const inputHandler = new InputHandler();
     const outputHandler = new OutputHandler();
     const carRacingManager = new CarRacingManager(inputHandler, outputHandler);
-    
+
     await carRacingManager.startGame();
   }
 }
