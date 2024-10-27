627146f90ad3fbd8b6ad3c9d638635037a4d5b5b
Heesu01 <rulru0413@naver.com>
2024-10-27 23:35:33 +0900

    refactor(App): 애플리케이션 클래스 구조 정리

--- src/App.js
+++ src/App.js
@@ -1,62 +1,13 @@
-import { Console, Random } from "@woowacourse/mission-utils";
+import Input from "./Input.js";
+import Race from "./Race.js";
 
 class App {
   async run() {
-    const { carList, cnt } = await this.getInput();
-    const cars = carList.map((name) => ({ name, position: 0 }));
-
-    Console.print("\n실행 결과");
-    for (let i = 0; i < cnt; i++) {
-      this.moveCars(cars);
-      this.printPositions(cars);
-    }
-    this.printWinners(cars);
-  }
-
-  // 입력 받기
-  async getInput() {
-    const cars = await Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
-    );
-    const carList = cars.split(",");
-    const cnt = Number(
-      await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n")
-    );
-    return { carList, cnt };
-  }
-
-  // 자동차 전진 여부 결정
-  moveCars(cars) {
-    cars.forEach((car) => {
-      const randomNum = Random.pickNumberInRange(0, 9);
-      if (randomNum >= 4) {
-        car.position += 1;
-      }
-    });
-  }
-
-  // 경주 결과 출력
-  printPositions(cars) {
-    cars.forEach((car) => {
-      Console.print(`${car.name} : ${"-".repeat(car.position)}`);
-    });
-    Console.print("");
-  }
-
-  // 우승자 출력
-  printWinners(cars) {
-    let maxPosition = 0;
-    let winners = [];
-
-    for (const car of cars) {
-      if (car.position > maxPosition) {
-        maxPosition = car.position;
-        winners = [car.name];
-      } else if (car.position === maxPosition) {
-        winners.push(car.name); // 공동 우승자 추가
-      }
-    }
-    Console.print(`최종 우승자 : ${winners.join(", ")}`);
+    const input = new Input();
+    const carList = await input.getCarNames();
+    const cnt = await input.getCnt();
+    const race = new Race(carList, cnt);
+    race.start();
   }
 }
 
