01f577c7fa892060266135c2c19632787e6da88c
JuYeong17 <dhwndud980@gmail.com>
2024-10-27 20:04:16 +0900

    fix: 입력 수정

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -2,7 +2,7 @@ import { Console } from "@woowacourse/mission-utils";
 
 class InputView{
   async getInput(){
-    const carName= await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n");
+    const carName= await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     const car=carName.split(',');
     await this.getinputexception(car);
 
