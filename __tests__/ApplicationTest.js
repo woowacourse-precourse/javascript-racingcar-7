4109ca51cbf9ef35ebdfac516e7f23252ad6361c
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-10-27 21:25:47 +0900

    test: 경주할 자동차에 ''만 입력한 경우

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -72,6 +72,15 @@ describe("자동차 경주", () => {
     await expect(app.run()).rejects.toThrow("[ERROR]");
   });
 
+  test("예외 테스트 - 경주할 자동차에 ''만 입력한 경우", async () => {
+    const inputs = [""];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
   test("예외 테스트 - 경주할 자동차에 쉼표를 구분자로 입력하지 않은 경우", async () => {
     const inputs = ["pobi; javaji"];
     mockQuestions(inputs);
