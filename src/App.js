e97f95d37f21a321f1502b5371f124c999f80579
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-10-27 16:03:09 +0900

    feat: 이름 입력 기능
    1. App 내 이름 입력 기능 구현
    
    2. 빈 문자열을 입력하는 경우 에러처리
    
    3. 같은 이름을 입력하는 경우 에러처리
    
    4. 5자를 초과하는 이름을 입력하는 경우 에러처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const inputCarNames = await Console.readLineAsync(
+      "경주할 자동차 이름(이름은 쉽표(,) 기준으로 구분)\n"
+    );
+
+    if (inputCarNames === "") {
+      throw new Error("[ERROR]: 빈 문자열은 입력할 수 없습니다.");
+    }
+
+    const carNames = inputCarNames.split(",");
+
+    const uniqueCarNames = [];
+
+    carNames.forEach((carName) => {
+      if (uniqueCarNames.includes(carName)) {
+        throw new Error("[ERROR]: 같은 이름은 불가능합니다.");
+      }
+      if (carName.length > 5) {
+        throw new Error("[ERROR]: 이름은 5자 이하만 가능합니다.");
+      }
+      uniqueCarNames.push(carName);
+    });
+  }
 }
 
 export default App;
