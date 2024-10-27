489bdb34f0976e7ed90450772a8a0876df80223d
jongbin26 <010jjj@naver.com>
2024-10-27 11:58:02 +0900

    test(Game.test): getMovedCars 테스트 코드 추가
    - test.each 함수를 통해 getMovedCars 함수에 대한 단위 테스트를 추가했습니다.

--- src/test/Game.test.js
+++ src/test/Game.test.js
@@ -1,5 +1,9 @@
-import { Game } from '../Game.js';
+import { MissionUtils } from '@woowacourse/mission-utils';
 import { Record } from '../Record.js';
+import { Game } from '../Game.js';
+
+const MOVING_FORWARD = 8;
+const STOP = 2;
 
 test('update 함수 테스트', () => {
   const movingCars = ['pobi', 'woni'];
@@ -11,4 +15,31 @@ test('update 함수 테스트', () => {
   };
   record.update(movingCars);
   expect(record.table).toEqual(nextRecord);
+});
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    MissionUtils.Random.pickNumberInRange,
+  );
+};
+
+test.each([
+  [[MOVING_FORWARD, STOP, STOP], ['pobi']],
+  [[STOP, MOVING_FORWARD, STOP], ['woni']],
+  [[STOP, STOP, MOVING_FORWARD], ['jun']],
+  [
+    [MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD],
+    ['pobi', 'woni', 'jun'],
+  ],
+  [[STOP, STOP, STOP], []],
+])('getMovedCars 테스트', (mockData, result) => {
+  const record = new Record('pobi,woni,jun');
+  const game = new Game(record, 5);
+  mockRandoms(mockData);
+
+  const getMovedCars = game.getMovedCars();
+  expect(getMovedCars).toEqual(result);
 });
