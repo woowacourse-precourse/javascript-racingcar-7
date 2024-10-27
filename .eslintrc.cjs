d4834dd2025db69f25e1c802cf5c91a66191aae2
Chaesunbak <coanstjd0422@gmail.com>
2024-10-27 16:58:12 +0900

    chore : 불필요한 파일 삭제

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['airbnb-base', 'prettier'],
-  rules: {
-    // 들여쓰기 깊이 제한
-    'max-depth': ['error', 2],
-    'import/extensions': ['error', 'ignorePackages'],
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
-};
