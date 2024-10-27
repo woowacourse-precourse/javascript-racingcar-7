74135b5c4417b6a5ca2fcd3655acfa1676b08205
sanghee01 <sang6299@naver.com>
2024-10-27 20:46:13 +0900

    feat(App): 경주에 참여할 자동차를 입력하는 기능 구현
    경주에 참여할 자동차를 입력하고, 배열에 쉼표로 구분해서 저장함
    예외처리도 함께 진행함
    
    경주에 참여할 자동차 입력 시 예외 처리한 목록
    - 쉼표가 없을 때
    - 쉼표가 맨 앞이나 맨 뒤에 있을 때
    - 자동차 이름이 5자 이하가 아닐 때
    - 자동차 이름에 숫자, 영어, 쉼표 이외의 문자가 들어갈 때
    - 자동차가 2대 이상이 아닐 때

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    let participantInput = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+    );
+
+    if (!participantInput.includes(',')) {
+      throw new Error('[ERROR] 참가자는 두명 이상이어야합니다.');
+    }
+
+    if (participantInput[0] === ',' || participantInput.at(-1) === ',') {
+      throw new Error('[ERROR] 쉼표(,)는 맨 앞, 혹은 맨 뒤에 올 수 없습니다.');
+    }
+
+    participantInput.split('').forEach((x) => {
+      if (
+        !(
+          (x.charCodeAt() >= 48 && x.charCodeAt() <= 57) ||
+          (x.charCodeAt() >= 65 && x.charCodeAt() <= 90) ||
+          (x.charCodeAt() >= 97 && x.charCodeAt() <= 122) ||
+          x.charCodeAt() === 44
+        )
+      ) {
+        throw new Error(
+          '[ERROR] 경주할 자동차 이름 입력 시 숫자, 영어, 쉼표 이외의 문자는 입력할 수 없습니다.',
+        );
+      }
+    });
+
+    const participantList = participantInput.split(',');
+
+    participantList.forEach((participant) => {
+      if (participant.length > 5) {
+        throw new Error('[ERROR] 자동차 이름은 5자 이하이어야 합니다.');
+      }
+    });
+  }
 }
 
 export default App;
