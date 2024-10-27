f1c7b51c8a06b81a6d9a31f6a854f73c8b543780
crolvlee <leecrolv@gmail.com>
2024-10-27 23:49:14 +0900

    feat: 입력받은 횟수만큼 게임을 진행하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Console } from "@woowacourse/mission-utils";
 import { getCars, getRound } from "./InputHandler.js";
-
+import { runGame } from "./GameHandler.js";
 
 class App {
   async run() {
     try {
       const cars = await getCars();
       const round = await getRound();
+      const result = await runGame(cars, round);
 
     } catch(error) {
       throw error;
--- src/InputHandler.js
+++ src/InputHandler.js
@@ -2,8 +2,9 @@ import { Console } from "@woowacourse/mission-utils";
 
 export async function getCars() {
     const carsInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
-    
-    return carsInput;
+    const cars = carsInput.split(',').map((car) => car.trim());
+
+    return cars;
 }
 
 export async function getRound() {
