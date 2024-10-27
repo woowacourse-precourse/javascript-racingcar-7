4f6de5fa2c96370661ad5e493fac55e52705ecd4
suhwa <tnghk9611@naver.com>
2024-10-28 01:31:55 +0900

    fix(Constants): 상수의 불변성 보장
    - Object.freeze() 메서드로 상수 변경 시도 방지

--- src/constants/Message.js
+++ src/constants/Message.js
@@ -1,22 +1,22 @@
 import { PRINT_SETTING } from "./Setting";
 
-export const INPUT_MESSAGE = {
+const ERROR_PREFIX = Object.freeze("[ERROR]");
+
+export const INPUT_MESSAGE = Object.freeze({
     CAR_NAMES_PROMPT: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
     TRY_COUNT_PROMPT: "시도할 횟수는 몇 회인가요?",
-};
+});
 
-export const PRINT_MESSAGE = {
+export const PRINT_MESSAGE = Object.freeze({
     RESULT_TEXT: "\n실행 결과",
     TOTAL_LOGS: (logs) => `${logs.join(PRINT_SETTING.LOG_JOIN_SEPARATOR)}`,
     FINAL_WINNER: (winner) => `최종 우승자 : ${winner.join(PRINT_SETTING.WINNER_JOIN_SEPARATOR)}`,
-};
+});
 
-export const ERROR_PREFIX = "[ERROR]";
-
-export const CAR_NAME_ERROR = {
+export const CAR_NAME_ERROR = Object.freeze({
     OVER_LENGTH: `${ERROR_PREFIX} 자동차 이름은 5자 이하로 입력 가능합니다.`,
-};
+});
 
-export const TRY_COUNT_ERROR = {
+export const TRY_COUNT_ERROR = Object.freeze({
     NOT_NUMBER: `${ERROR_PREFIX} 시도할 횟수는 숫자로 입력해야 합니다.`,
-};
+});
