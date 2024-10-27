e36dc439f4627675cd1fb6e6c419d974cca5c835
Euiyeong <105384780+snsdl0905@users.noreply.github.com>
2024-10-27 15:30:02 +0900

    feat: 몇 번의 이동을 할 것인지 입력받는다

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const names = await this.getNames();
+    const move = await this.getMove();
   }
 
   async getNames() {
     try{
       const input = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
       return this.splitNames(input);
+    } catch(error) {
+      throw new Error(`[ERROR]: ${error.message}`);
+    }
+  }
+
+  async getMove() {
+    try{
+      const input = Number(await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n"));
+      return input;
     } catch(error) {
       throw new Error(`[ERROR]: ${error.message}`);
     }
