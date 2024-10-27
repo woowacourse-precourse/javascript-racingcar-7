83c1a376cf557b488d4427f427dfe522f7d9fedb
조승우 <choseung97@gmail.com>
2024-10-27 15:42:19 +0900

    feat(validator): 이름이 하나만 입력되는 경우를 예외 처리한다

--- src/Validator.js
+++ src/Validator.js
@@ -2,9 +2,14 @@ export default {
   validateNames(names) {
     if (!names) throw new Error('[ERROR] 이름을 입력해주세요.');
     if (!names.match(/^[^,]{1,5}(,[^,]{1,5})+$/)) {
-      // 어떤 유형의 에러인지 구분하여 작성 <- 사용자를 위함
+      this.OneName(names);
       // 맨 마지막에는 일반적인 에러 메시지로 작성
       throw new Error('[ERROR] 이름 입력을 다시 확인해주세요.');
+    }
+  },
+  OneName(names) {
+    if (names.match(/^[^,]{1,5}$/)) {
+      throw new Error('[ERROR] 하나 이상의 이름을 입력해주세요.');
     }
   },
 
