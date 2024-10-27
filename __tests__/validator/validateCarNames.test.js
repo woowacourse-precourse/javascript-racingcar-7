c7497cd0926bbcf9023f1cf0defea8b655f6d4e4
moonseonghui <msh9980@naver.com>
2024-10-27 17:54:25 +0900

    test(validateCarNamesTest): validateCarNames함수의 유효하지 않은 자동차 이름 테스트 케이스 추가

--- __tests__/validator/validateCarNames.test.js
+++ __tests__/validator/validateCarNames.test.js
@@ -1,4 +1,5 @@
 import { validateCarNames } from '../../src/validator/validatePipeline';
+import { ERROR_MESSAGE } from '../../src/constants/messages';
 
 const createCarArray = (size) =>
   Array.from({ length: size }, (_, i) => `a${i + 1}`);
@@ -14,3 +15,32 @@ test.each([
 ])('[유효한 자동차 이름 검사] %s : %s', (description, names) => {
   expect(validateCarNames(names)).toBe(names);
 });
+
+// 유효하지 않은 자동차 이름 테스트 케이스
+test.each([
+  [
+    '길이가 6자 이상의 이름이 포함된 경우',
+    ['car', 'racingCar', 'jeep'],
+    ERROR_MESSAGE.INVALID_CAR_NAME_LENGTH,
+  ],
+  [
+    '비어 있는 이름이 포함된 경우',
+    ['car', '', 'jeep'],
+    ERROR_MESSAGE.INVALID_CAR_NAME_LENGTH,
+  ],
+  [
+    '중복되는 이름이 포함된 경우',
+    ['car1', 'car2', 'car3', 'car1'],
+    ERROR_MESSAGE.DUPLICATE_NAME,
+  ],
+  [
+    '유효한 자동차 개수를 넘어간 경우',
+    createCarArray(101),
+    ERROR_MESSAGE.LIMIT_CAR_COUNT,
+  ],
+])(
+  '[유효하지 않은 자동차 이름 검사] %s : %s',
+  (description, names, expectedError) => {
+    expect(() => validateCarNames(names)).toThrow(expectedError);
+  },
+);
