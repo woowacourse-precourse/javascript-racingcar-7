29bd0d53f6c7187ecbfab8479a628d551c4a69a1
Junseo Kim <semiwest2@gmail.com>
2024-10-28 05:27:43 +0900

    test: Car 클래스 이동 기능 테스트 추가
    - 초기 위치가 0인지 검증
    - checkAndMoveForward 메서드가 임계값에 따라 이동하는지 확인
    - 임계값보다 낮음, 같음, 높음에 대한 경계 케이스 포함

---
+++ __tests__/game/Car.test.js
@@ -0,0 +1,24 @@
+import Car from '../../src/game/Car.js';
+import { FORWARD_THRESHOLD } from '../../src/constant/constants.js';
+
+const testCases = [
+  ['Hello', 3, 0, '랜덤 값이 낮아 이동하지 않음'],       // randomNumber < FORWARD_THRESHOLD
+  ['Haki', FORWARD_THRESHOLD, 1, '랜덤 값이 임계값과 같음'], // randomNumber == FORWARD_THRESHOLD
+  ['wap', 10, 1, '랜덤 값이 임계값보다 높아 이동함'],       // randomNumber > FORWARD_THRESHOLD
+];
+
+describe('Car 클래스 테스트', () => {
+  test('초기 위치는 0이어야 합니다.', () => {
+    const car = new Car('Hello');
+    expect(car.position).toBe(0);
+  });
+
+  test.each(testCases)(
+    '%s: 임의 값 %d -> 예상 위치 %d (%s)',
+    (name, randomNumber, expectedPosition) => {
+      const car = new Car(name);
+      car.checkAndMoveForward(randomNumber);
+      expect(car.position).toBe(expectedPosition);
+    }
+  );
+});
