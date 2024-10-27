0f8cf8c15260e14ffcf9159ecec22facd61db23e
@u_limit_ <yurim@ajou.ac.kr>
2024-10-27 19:09:08 +0900

    feat: 자동차 이름에 0 또는 공백을 입력했을 경우 예외 처리

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { Console } from '@woowacourse/mission-utils';
 
 const validateCarName = (input) => {
   const regex = /^[A-Za-z0-9]+$/;
-
+  const numberName = Number(input);
   if (input.length > 5) {
     throw new Error('[ERROR] 자동차 이름은 5자 이하로 입력해주세요.');
   }
 
   if (!regex.test(input)) {
     throw new Error('[ERROR] 자동차 이름에는 영어, 숫자만 사용할 수 있어요.');
+
+  if (numberName === 0) {
+    throw new Error('[ERROR] 자동차 이름에는 공백을 입력할 수 없어요.');
   }
 };
 const determineCarName = (input) => {
