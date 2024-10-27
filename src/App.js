68c7ce3342d7b643cca546373b89d3ca7bb62a80
yoojin <everywavyday@gmail.com>
2024-10-28 03:52:33 +0900

    feat: 잘못된 형식의 자동차 이름이 들어올 경우 에러 메시지를 출력

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import { Console } from "@woowacourse/mission-utils";
+
 class App {
   async run() {
-    await this.getCarNames();
-    await this.getGameRound();
+    try {
+      await this.getCarNames();
+      await this.getGameRound();
+    } catch (error) {
+      Console.print(`[ERROR] ${error.message}`);
+    }
   }
 
   async getCarNames() {
-    const input = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
-    );
+    try {
+      const input = await Console.readLineAsync(
+        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+      );
+      const carList = input.split(',').map((carName) => this.isValidCarName(carName));
+      Console.print(carList);
+    } catch (error) {
+      throw error;
+    }
+  }
 
-    let carList = [];
-    carList = input.split(',');
-    Console.print(carList);
+  isValidCarName(carName) {
+    if (!carName || carName.trim() === '') {
+      throw new Error('자동차 이름은 비어 있을 수 없습니다.');
+    }
+    if (carName.length > 5) {
+      throw new Error('자동차 이름은 5자 이하이어야 합니다.');
+    }
+    return carName.trim();
   }
 
   async getGameRound() {
