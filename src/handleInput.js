c551bdcdbf52fec8cb960db9defa8aeae7b58b86
H0ngJu <hongju4385@gmail.com>
2024-10-27 15:16:57 +0900

    refactor: try-catch 제거

--- src/handleInput.js
+++ src/handleInput.js
@@ -2,7 +2,7 @@ import { Console } from "@woowacourse/mission-utils";
 import { ERROR_MESSAGES } from "./errorMessages.js";
 
 function validateCarNames(carNames) {
-  const names = carNames.split(",").map((name) => name.trim());
+  const names = carNames.split(",");
 
   if (names.some((name) => name.length === 0)) {
     throw new Error(ERROR_MESSAGES.EMPTY_NAME_ERROR);
@@ -33,23 +33,13 @@ function validateRound(round) {
 }
 
 export async function getCarName() {
-  try {
-    const carNames = await Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n"
-    );
-    return validateCarNames(carNames);
-  } catch (error) {
-    Console.print("[ERROR]" + error.message);
-    return getCarName();
-  }
+  const carNames = await Console.readLineAsync(
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n"
+  );
+  return validateCarNames(carNames);
 }
 
 export async function getRound() {
-  try {
-    const round = await Console.readLineAsync("시도할 횟수는 몇 회 인가요?\n");
-    return validateRound(round);
-  } catch (error) {
-    Console.print("[ERROR]" + error.message);
-    return getRound();
-  }
+  const round = await Console.readLineAsync("시도할 횟수는 몇 회 인가요?\n");
+  return validateRound(round);
 }
