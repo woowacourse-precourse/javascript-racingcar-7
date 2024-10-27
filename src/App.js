c5c6f6a0ef4355e1586bc98013c5f31a299bbcb3
ParkjuYoung <kwn03111@gmail.com>
2024-10-27 17:23:23 +0900

    feat (App.js) 자동차 경주에 필요한 기능과 로직을 제어하는 애플리케이션 클래스 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { getCars, getNumberOfRaces } from './view/getUserInput.js';
+import { carNameValidate } from './utils/carNameValidator.js';
+import { raceCountValidate } from './utils/raceCountValidator.js';
+import { printRaceWinner } from './view/raceView.js';
+import { Car } from './model/car.js';
+import { Race } from './model/race.js';
+
 class App {
-  async run() {}
+  async run() {
+    const cars = await getCars();
+    const carNames = carNameValidate(cars);
+
+    const numberOfMatches = await getNumberOfRaces();
+    const raceRound = raceCountValidate(numberOfMatches);
+
+    const carLists = carNames.map((name) => new Car(name));
+    const race = new Race(carLists, raceRound);
+
+    while (race.isRaceOver()) {
+      race.roundStart();
+      race.showRaceStatusByRound();
+    }
+
+    const winner = race.getFinalWinner();
+    printRaceWinner(winner);
+  }
 }
 
 export default App;
