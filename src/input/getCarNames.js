eb569915ebc4c8c996d6574185e63f5a6ccc10b0
minjeong9919 <minjeong9919@naver.com>
2024-10-28 02:45:08 +0900

    feat: 이름 중복 에러 처리

--- src/input/getCarNames.js
+++ src/input/getCarNames.js
@@ -6,11 +6,17 @@ export const getCarNames = async () => {
     "경주할 자동차 이름을 입력 하세요.(이름은 쉼표(,) 기준 으로 구분)\n",
   );
   const racingCars = racingCarsInput.split(",");
+
   racingCars.map(racingCar => {
     if (racingCar.length > 5) {
       throw new Error(ERROR_MESSAGE.WORD_LIMIT);
     }
   });
+
+  const setRacingCars = new Set(racingCars);
+  if (setRacingCars.size !== racingCars.length) {
+    throw new Error(ERROR_MESSAGE.DUPLICATED_NAME);
+  }
 
   return racingCars;
 };
