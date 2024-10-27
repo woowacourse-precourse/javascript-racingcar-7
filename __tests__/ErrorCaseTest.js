64035d7539b17a11ce26555a2fc141ed74acc244
dmson1218 <dmson1218@naver.com>
2024-10-27 20:15:49 +0900

    test(ErrorCaseTest): 에러 케이스 테스트 코드 추가
    - InputValidator의 에러 메시지 정적 프로퍼티 사용
    - 자동차 이름 및 전진 시도 횟수 미입력의 경우 테스트 실패

---
+++ __tests__/ErrorCaseTest.js
@@ -0,0 +1,80 @@
+import App from "../src/App.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
+import InputValidator from "../src/InputValidator.js";
+
+const mockQuestions = (inputs) => {
+    MissionUtils.Console.readLineAsync = jest.fn();
+    MissionUtils.Console.readLineAsync.mockImplementation(() => {
+        const input = inputs.shift();
+        return Promise.resolve(input);
+    });
+};
+
+describe("에러 발생 케이스", () => {
+    test.each([
+        {
+            description: "자동차 이름이 미입력된 경우",
+            inputs: [""],
+            error: InputValidator.ERROR_MESSAGES.CAR_NAME_EMPTY,
+        },
+        {
+            description: "자동차 이름 사이에 공백이 존재하는 경우",
+            inputs: ["pobi, woni", "1"],
+            error: InputValidator.ERROR_MESSAGES.INVALID_NAME,
+        },
+        {
+            description: "자동차 이름에 특수 문자가 포함된 경우",
+            inputs: ["pobi,woni!", "1"],
+            error: InputValidator.ERROR_MESSAGES.INVALID_NAME,
+        },
+        {
+            description: "자동차 이름이 5자를 초과한 경우",
+            inputs: ["pobi,woniwoni", "1"],
+            error: InputValidator.ERROR_MESSAGES.INVALID_NAME,
+        },
+        {
+            description: "쉼표로 시작하는 경우",
+            inputs: [",pobi,woni", "1"],
+            error: InputValidator.ERROR_MESSAGES.INVALID_NAME,
+        },
+        {
+            description: "쉼표로 끝나는 경우",
+            inputs: ["pobi,woni,", "1"],
+            error: InputValidator.ERROR_MESSAGES.INVALID_NAME,
+        },
+        {
+            description: "쉼표가 연속으로 입력된 경우",
+            inputs: ["pobi,,woni", "1"],
+            error: InputValidator.ERROR_MESSAGES.INVALID_NAME,
+        },
+        {
+            description: "시도 횟수가 미입력된 경우",
+            inputs: ["pobi,woni", ""],
+            error: InputValidator.ERROR_MESSAGES.TRY_COUNT_EMPTY,
+        },
+        {
+            description: "시도 횟수가 0인 경우",
+            inputs: ["pobi,woni", "0"],
+            error: InputValidator.ERROR_MESSAGES.INVALID_TRY_COUNT,
+        },
+        {
+            description: "시도 횟수가 음수인 경우",
+            inputs: ["pobi,woni", "-1"],
+            error: InputValidator.ERROR_MESSAGES.INVALID_TRY_COUNT,
+        },
+        {
+            description: "시도 횟수가 문자인 경우",
+            inputs: ["pobi,woni", "a"],
+            error: InputValidator.ERROR_MESSAGES.INVALID_TRY_COUNT,
+        },
+    ])("$description", async ({ inputs, error }) => {
+        // given
+        mockQuestions(inputs);
+
+        // when
+        const app = new App();
+
+        // then
+        await expect(app.run()).rejects.toThrow(`[ERROR] ${error}`);
+    });
+});
