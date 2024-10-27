26c87a11a10749f8aeb4e51dcf011ed2583784fa
thisisyejin <yejink321@gmail.com>
2024-10-27 23:44:58 +0900

    refactor: race 메서드 역할 분리
    전체 게임을 진행하는 race 메서드의 역할을 더욱 명료하게
    정리하였습니다. moveOrStop 메서드는 Car 인스턴스들의
    movement 값을 증가시키는 역할만을 수행하도록 하였고,
    printMovement 메서드를 추가하여 출력을 담당하도록 하였습니다.

--- src/Car.js
+++ src/Car.js
@@ -8,28 +8,32 @@ export default class Car {
   }
 
   static makeCarList(str) {
-    let carList = str.split(',');
-    carList = carList.map(element => element.trim())
-    isValidName(carList); // 유효성 검사
-
-    // 이름마다 각각 Car 인스턴스 생성하기
+    let carList = str.split(',').map(element => element.trim())
+    isValidName(carList);
     carList = carList.map(car => new Car(car));
     return carList;
   }
 
-  static moveOrStop(carList) {
+  static #printMovement(carList) {
     carList.forEach(element => {
-      if (MissionUtils.Random.pickNumberInRange(0, 9) >= 4) {
-        element.movement += 1;
-      }
       MissionUtils.Console.print(`${element.name} : ${'-'.repeat(element.movement)}`);
     });
     MissionUtils.Console.print('');
   }
 
+  static #moveOrStop(carList) {
+    carList.forEach(element => {
+      if (MissionUtils.Random.pickNumberInRange(0, 9) >= 4)
+        element.movement += 1;
+    });
+  }
+
   static race(carList, tryTime) {
     MissionUtils.Console.print('\n실행 결과');
-    Array.from({ length: tryTime }).forEach(() => this.moveOrStop(carList))
+    Array.from({ length: tryTime }).forEach(() => {
+      this.#moveOrStop(carList);
+      this.#printMovement(carList);
+    });
   }
 
   static whoIsWinner(carList) {
