3bb110e4a07d0d688b86b3af689b59948f9d438c
suubiin <127467411+Suubiin@users.noreply.github.com>
2024-10-28 04:13:54 +0900

    feat: 시도 횟수 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ class App {
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
     );
     const carNames = this.splitCarNames(carNamesInput);
+
+    const attempts = await Console.readLineAsync(
+      '시도할 횟수는 몇 회인가요?\n'
+    );
   }
 
   splitCarNames(carNamesInput) {
