8313eb233fa660d6a30eefd4102e9f2197e353d3
Park JiSeop <141125424+HarrySeop@users.noreply.github.com>
2024-10-27 17:02:13 +0900

    ✅test: MoveCondition 클래스 테스트 작성

---
+++ __tests__/MoveConditionTest.js
@@ -0,0 +1,47 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import getMoveForward from '../src/utils/MoveCondition.js';
+
+const mockRandoms = numbers => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    MissionUtils.Random.pickNumberInRange,
+  );
+};
+
+describe('4 이상이면 자동차를 전진하게 해주는 MoveCondition 테스트', () => {
+  describe('랜덤 값이 4 이상일 경우', () => {
+    test('랜덤 값이 4일 경우 전진할 수 있습니다.', () => {
+      mockRandoms([4]);
+      expect(getMoveForward()).toBe(true);
+    });
+
+    test('랜덤 값이 7일 경우 전진할 수 있습니다.', () => {
+      mockRandoms([7]);
+      expect(getMoveForward()).toBe(true);
+    });
+
+    test('랜덤 값이 9일 경우 전진할 수 있습니다.', () => {
+      mockRandoms([9]);
+      expect(getMoveForward()).toBe(true);
+    });
+  });
+
+  describe('랜덤 값이 4 미만일 경우', () => {
+    test('랜덤 값이 3일 경우 전진할 수 없습니다.', () => {
+      mockRandoms([3]);
+      expect(getMoveForward()).toBe(false);
+    });
+
+    test('랜덤 값이 0일 경우 전진할 수 없습니다.', () => {
+      mockRandoms([0]);
+      expect(getMoveForward()).toBe(false);
+    });
+
+    test('랜덤 값이 1일 경우 전진할 수 없습니다.', () => {
+      mockRandoms([1]);
+      expect(getMoveForward()).toBe(false);
+    });
+  });
+});
