d68689dd18a6af93b329fa9bd9b153e55127b05c
hidanbeing <nd6162bv@gmail.com>
2024-10-27 23:25:51 +0900

    fix: 들여쓰기 2번 이하로 줄이기

--- src/App.js
+++ src/App.js
@@ -3,22 +3,18 @@ import { Console, MissionUtils } from "@woowacourse/mission-utils";
 class Car {
   constructor(name) {
     this.name = name;
-    this.race = []; // 전진 저장할 배열변수
+    this.race = []; // 전진 저장할 배열 변수
   }
 }
 
 class App {
   startGame(cars) {
-    for (const car of cars) {
+    cars.forEach((car) => {
       let rand = MissionUtils.Random.pickNumberInRange(0, 9);
-      if (rand >= 4) {
-        car.race.push("-");
-      }
-    }
-
-    for (const car of cars) {
+      if (rand >= 4) car.race.push("-");
       Console.print(car.name + " : " + car.race.join(""));
-    }
+    });
+    Console.print(""); // 각 시도 끝에 공백 줄 추가
   }
 
   splitInput(input) {
@@ -27,37 +23,22 @@ class App {
 
     for (let i = 0; i < input.length; i++) {
       if (input[i] === ",") {
-        if (tmp !== "") {
-          // 길이 제한 예외 처리
-          if (tmp.length > 6) {
-            throw new Error("[ERROR]");
-          }
-
-          // 중복 검사 예외 처리
-          if (carNames.includes(tmp)) {
-            throw new Error("[ERROR]");
-          }
-
-          carNames.push(tmp);
-        }
-        tmp = ""; // 초기화
+        if (tmp !== "") this.validateCarName(tmp, carNames);
+        carNames.push(tmp);
+        tmp = "";
       } else {
         tmp += input[i];
       }
     }
 
-    // 마지막 이름 추가
-    if (tmp !== "") {
-      if (tmp.length > 6) {
-        throw new Error("[ERROR]");
-      }
-      if (carNames.includes(tmp)) {
-        throw new Error("[ERROR]");
-      }
-      carNames.push(tmp);
-    }
+    if (tmp !== "") this.validateCarName(tmp, carNames);
+    carNames.push(tmp);
 
     return carNames;
+  }
+
+  validateCarName(name, carNames) {
+    if (name.length > 6 || carNames.includes(name)) throw new Error("[ERROR]");
   }
 
   isNumber(num) {
@@ -68,40 +49,22 @@ class App {
     const input = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
-
     const numInput = await Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?\n"
     );
 
-    const carNames = this.splitInput(input);
-
-    if (this.isNumber(numInput)) {
-      if (parseInt(numInput) < 0) {
-        throw new Error("[ERROR]");
-      }
-    } else {
+    if (!this.isNumber(numInput) || parseInt(numInput) < 0)
       throw new Error("[ERROR]");
-    }
-
+    const carNames = this.splitInput(input);
     const cars = carNames.map((name) => new Car(name));
-    for (let i = 0; i < numInput; i++) {
-      this.startGame(cars);
-      Console.print("");
-    }
 
-    const longestRaceCar = cars.reduce((maxCar, currentCar) => {
-      return currentCar.race.length > maxCar.race.length ? currentCar : maxCar;
-    });
+    for (let i = 0; i < numInput; i++) this.startGame(cars);
 
-    // 가장 긴 race 배열 길이 찾기
     const maxRaceLength = Math.max(...cars.map((car) => car.race.length));
-
-    // 가장 긴 길이를 가진 Car 객체들 필터링
     const longestRaceCars = cars.filter(
       (car) => car.race.length === maxRaceLength
     );
 
-    // RESULT
     const winners = longestRaceCars.map((car) => car.name).join(", ");
     Console.print("최종 우승자 : " + winners);
   }
