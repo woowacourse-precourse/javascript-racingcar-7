288f2732bb9a8e3b2a2c6bb6462f5b194f3e9fef
seunghyeon77 <jdyjsh77@naver.com>
2024-10-27 14:25:25 +0900

    feat: 자동차 이름과 이동 횟수 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const CARNAMES = await Console.readLineAsync("자동차 이름을 입력하세요 : ")
+      .trim("")
+      .split(",");
+    const ROUNDS = parseInt(
+      await Console.readLineAsync("이동 횟수를 입력하세요 : "),
+      10
+    );
+  }
 }
 
 export default App;
