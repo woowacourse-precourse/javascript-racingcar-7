b9274cb2007bb33ae432b9682a20e074634b3419
choeeun-in <cei8645@naver.com>
2024-10-27 16:16:46 +0900

    Comment: 테스트 코드 주석 추가

--- test/App.test.js
+++ test/App.test.js
@@ -14,12 +14,16 @@ describe("App 테스트", () => {
     jest.clearAllMocks();
   });
 
+  //async와 await으로 비동기 코드 관리
   test("정상적으로 우승자가 출력되는지 확인", async () => {
-    Console.readLineAsync.mockResolvedValueOnce("judy,jaden");
-    Console.readLineAsync.mockResolvedValueOnce("6");
+    //테스트 입력값 설정 : 자동차에 부여 이름과 이동 횟수
+    Console.readLineAsync.mockResolvedValueOnce("judy,jaden"); //첫 번째 호출 : 자동차에 부여한 이름
+    Console.readLineAsync.mockResolvedValueOnce("6"); //두 번째 호출 : 자동차 이동 횟수
 
+    //App 실행
     await app.run();
 
+    //출력값 확인 : 각 자동차 입력 결과와 우승자 출력
     expect(Console.print).toHaveBeenCalledWith(
       expect.stringContaining("judy :")
     );
