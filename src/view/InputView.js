0409b20dc1933dd65624ec8a26e87e2130a070f6
hanheel <hanhee630630@gmail.com>
2024-10-27 17:14:15 +0900

    feat(InputView) : Check if the name of the car is valid

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -6,6 +6,11 @@ class InputView {
       "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n"
     );
     const carNames = carInput.split(",");
+    return this.isValidCarNames(carNames) ? carNames : "[ERROR]";
+  }
+  isValidCarNames(carNames) {
+    const isValid = carNames.map((i) => i != "");
+    return isValid;
   }
 }
 
