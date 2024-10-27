be0512e3195089c0e7019647fbcbfc7d100eecfb
@u_limit_ <yurim@ajou.ac.kr>
2024-10-27 19:22:51 +0900

    refactor: validateRaceNumber 함수 모듈화

--- src/App.js
+++ src/App.js
@@ -3,29 +3,12 @@ import {
   checkDuplicateCarName,
   validateCarName,
 } from './validation/validateCarName.js';
+import { validateRaceNumber } from './validation/validateRaceNumber.js';
 
 const determineCarName = (input) => {
   const names = input.split(',');
   names.forEach((name) => validateCarName(name));
   checkDuplicateCarName(names);
-};
-
-const validateRaceNumber = (raceNum) => {
-  const numValue = Number(raceNum);
-
-  if (Number.isNaN(numValue)) {
-    throw new Error('[ERROR] 숫자를 입력해주세요.');
-  }
-
-  if (numValue < 0) {
-    throw new Error('[ERROR] 시도할 횟수는 양수만 입력 가능해요.');
-  }
-
-  if (numValue === 0) {
-    throw new Error(
-      '[ERROR] 0 또는 공백은 입력할 수 없어요. 유효한 숫자를 입력해주세요.'
-    );
-  }
 };
 
 class App {
