68325fd7b334ea965831ec3238e3994917fa7556
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-10-27 10:25:02 +0900

    feat: App 클래스 및 게임 실행 흐름 관리
    - InputView에서 입력을 받아 Game 클래스에 전달하고 게임 시작
    - 게임 종료 후 OutputView로 최종 우승자와 오류 메시지 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import InputView from './InputView.js';
+import OutputView from './OutputView.js';
+import Game from './Game.js';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const carNames = await InputView.getCarNames();
+      const rounds = await InputView.getRounds();
+
+      const game = new Game(carNames);
+      game.start(rounds);
+
+      OutputView.printWinners(game.getWinners());
+    } catch (error) {
+      OutputView.printError(error.message);
+      throw new Error(error.message);
+    }
+  }
 }
 
 export default App;
