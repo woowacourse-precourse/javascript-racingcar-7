41cdc618126a9d8cb4f96ce0eb45c3e663c59695
Parksangwoo <psu8430@gmail.com>
2024-10-27 21:02:12 +0900

    feat(run) : throw error when car name length is bigger than 5

--- src/App.js
+++ src/App.js
@@ -4,60 +4,75 @@ import RacingCar from './Models/RacingCar.js';
 
 class App {
   async run() {
-    // 자동차 이름 받기
-    const userInput = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
-    );
+    try {
+      // 자동차 이름 받기
+      const userInput = await Console.readLineAsync(
+        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+      );
 
-    // 자동차 이름 분리
-    const carNameArr = userInput.split(',');
+      // 자동차 이름 분리
+      const carNameArr = userInput.split(',');
 
-    // 자동차 인스턴스 생성
-    const carInstanceArr = carNameArr.map((name) => new RacingCar(name));
-
-    // 시도 횟수 받기
-    const tryCount =
-      await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
-
-    // 줄 구분용 빈칸
-    Console.print('');
-
-    // 실행 결과 출력
-    Console.print('실행 결과');
-
-    for (let curCount = 0; curCount < tryCount; curCount++) {
-      carInstanceArr.forEach((car) => {
-        // 각 자동차 이동
-        const randomNumber = RandomUtil.getRandomNumber();
-
-        if (randomNumber >= 4) {
-          car.move();
+      // 자동차 인스턴스 생성
+      const carInstanceArr = carNameArr.map((name) => {
+        if (name.length > 5) {
+          throw new Error(
+            '[ERROR] 자동차의 이름은 5글자 이내로 작성되어야 합니다.',
+          );
+        } else {
+          return new RacingCar(name);
         }
-
-        // 각 자동차 위치 출력
-        const distanceString = new Array(car.movedDistance).fill('-').join('');
-        Console.print(`${car.name} : ${distanceString}`);
       });
+
+      // 시도 횟수 받기
+      const tryCount =
+        await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
 
       // 줄 구분용 빈칸
       Console.print('');
-    }
 
-    // 우승자 계산
-    let winner = [carInstanceArr[0]];
-    for (let carIndex = 1; carIndex < carInstanceArr.length; carIndex += 1) {
-      const curCarInstance = carInstanceArr[carIndex];
-      if (curCarInstance.movedDistance > winner[0].movedDistance) {
-        winner = [curCarInstance];
-      } else if (curCarInstance.movedDistance == winner[0].movedDistance) {
-        winner.push(curCarInstance);
-      } else {
-        continue;
+      // 실행 결과 출력
+      Console.print('실행 결과');
+
+      for (let curCount = 0; curCount < tryCount; curCount++) {
+        carInstanceArr.forEach((car) => {
+          // 각 자동차 이동
+          const randomNumber = RandomUtil.getRandomNumber();
+
+          if (randomNumber >= 4) {
+            car.move();
+          }
+
+          // 각 자동차 위치 출력
+          const distanceString = new Array(car.movedDistance)
+            .fill('-')
+            .join('');
+          Console.print(`${car.name} : ${distanceString}`);
+        });
+
+        // 줄 구분용 빈칸
+        Console.print('');
       }
-    }
 
-    // 우승자 출력
-    Console.print(`최종 우승자 : ${winner.map((car) => car.name).join(',')}`);
+      // 우승자 계산
+      let winner = [carInstanceArr[0]];
+      for (let carIndex = 1; carIndex < carInstanceArr.length; carIndex += 1) {
+        const curCarInstance = carInstanceArr[carIndex];
+        if (curCarInstance.movedDistance > winner[0].movedDistance) {
+          winner = [curCarInstance];
+        } else if (curCarInstance.movedDistance == winner[0].movedDistance) {
+          winner.push(curCarInstance);
+        } else {
+          continue;
+        }
+      }
+
+      // 우승자 출력
+      Console.print(`최종 우승자 : ${winner.map((car) => car.name).join(',')}`);
+    } catch (error) {
+      Console.print(error.message);
+      throw error;
+    }
   }
 }
 
