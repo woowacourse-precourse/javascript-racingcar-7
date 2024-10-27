bdfd4f8acb6dd60233cb1e779affe2c9cd5f1c30
underthemo-onlight <underthemo_onlight@naver.com>
2024-10-28 00:43:47 +0900

    fix: 클래스 분리에 따른 코드 수정 및 오타 수정

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -1,15 +1,14 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import Car from "./Car.js"
-
+import InputParser from './InputParser.js'
+import Car from './Car.js'
 
 class RacingGame {
     async start() {
         let cars = [];
 
-        const inputCarNames = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-        const inputNumberOfRounds = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+        const carNames = await InputParser.getCarNames();
+        const numberOfRounds = await InputParser.getNumberOfRounds();
 
-        const carNames = inputCarNames.trim().split(',');
         //경주할 자동차 이름대로 차의 객체 생성
         for (let i = 0; i < carNames.length; i++) {
             cars[i] = new Car(carNames[i]);
@@ -18,7 +17,7 @@ class RacingGame {
         MissionUtils.Console.print("\n"); //출력 포멧에 맞게 하기 위해 추가
 
         //라운드별 레이싱 진행
-        for (let i = 0; i < inputNumberOfRounds; i++) {
+        for (let i = 0; i < numberOfRounds; i++) {
             //각 차량마다 레이싱 수행
             for (let j = 0; j < cars.length; j++) {
                 cars[j].move();
