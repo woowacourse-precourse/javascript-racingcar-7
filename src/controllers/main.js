12158c67bda779eba6fa2474bdff2aa4ea7a118c
dev-dino22 <wlsskdud02@naver.com>
2024-10-27 18:16:39 +0900

    feat(Main.js): Add function to return winner

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -3,15 +3,16 @@ import { Console, Random } from "@woowacourse/mission-utils";
 const Main = async () => {
   Console.print("프로그램이 실행되었습니다.");
 
-  const isCarName = await Console.readLineAsync("경주할 차 이름 입력");
+  const carInputName = await Console.readLineAsync("경주할 차 이름 입력");
+
   const isTryNumber = await Console.readLineAsync("시도할 횟수 입력");
 
   // 검증 - 공백
-  if (isCarName.trim().length === 0 || isTryNumber.trim().length === 0) {
+  if (carInputName.trim().length === 0 || isTryNumber.trim().length === 0) {
     throw new Error("[ERROR] 입력값이 비었습니다.");
   }
 
-  if (isCarName === null || isTryNumber === null) {
+  if (carInputName === null || isTryNumber === null) {
     throw new Error("[ERROR] 입력값이 비었습니다.");
   }
 
@@ -38,7 +39,24 @@ const Main = async () => {
     }
   }
 
-  const myCar = new Car("nayoung");
+  class CarList {
+    constructor() {
+      this.cars = [];
+    }
+
+    addCar(name) {
+      this.cars.push(new Car(name));
+    }
+  }
+
+  const carList = new CarList();
+
+  const createCarList = () => {
+    const getCarList = carInputName.split(",");
+    getCarList.forEach((name) => carList.addCar(name.trim()));
+  };
+
+  createCarList();
 
   const gameController = (parseNumber) => {
     for (let i = 0; i < parseNumber; i++) {
@@ -47,28 +65,45 @@ const Main = async () => {
   };
 
   const raceController = () => {
-    raceRound();
+    carList.cars.forEach((car) => {
+      raceRound(car);
+    });
     raceRoundResult();
   };
 
-  const raceRound = () => {
+  const raceRound = (car) => {
     const randomResult = Random.pickNumberInRange(0, 9);
-
     if (randomResult >= 4) {
-      myCar.addAdvance();
+      car.addAdvance();
     }
   };
 
   const raceRoundResult = () => {
-    const generateAdvanceSymbol = (advance) => {
-      const ADVANCE_SYMBOL = "-";
-      return ADVANCE_SYMBOL.repeat(advance);
-    };
-    const resultAdvanceSymbol = generateAdvanceSymbol(myCar.advance);
-    Console.print(`${myCar.name} : ` + `${resultAdvanceSymbol}`);
+    carList.cars.forEach((car) => {
+      const generateAdvanceSymbol = (advance) => "-".repeat(advance);
+      const resultAdvanceSymbol = generateAdvanceSymbol(car.advance);
+      Console.print(`${car.name} : ${resultAdvanceSymbol}`);
+    });
   };
 
   gameController(parseNumber);
+
+  const gameResultController = () => {
+    const getWinner = () => {
+      const isArrayAdvance = carList.cars.map((car) => car.advance);
+      const isMaxAdvance = Math.max(...isArrayAdvance);
+      const isWinnerCar = carList.cars.filter(
+        (car) => car.advance === isMaxAdvance
+      );
+
+      // 우승자 이름 배열을 생성하고 출력
+      const isWinnerName = isWinnerCar.map((car) => car.name);
+    };
+
+    getWinner();
+  };
+
+  gameResultController();
 };
 
 export default Main;
