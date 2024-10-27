745d98ecee5090dc105801c9d5b160000da97027
YongHyunKing <yhkim8246@gmail.com>
2024-10-28 04:01:30 +0900

    feat(App): 자동차 이름 양끝 여백 제거

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App {
     const NAMES = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
     const TIMES = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
 
-    const splitNames = NAMES.split(',');
+    const splitNames = NAMES.split(',').map(name => name.trim());
 
   }
 }
