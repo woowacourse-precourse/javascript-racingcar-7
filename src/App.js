6acad23333e9e77b979c7364b463336481cc044f
Heesu01 <rulru0413@naver.com>
2024-10-27 22:24:14 +0900

    feat(App): 자동차 전진 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
-  async run() {}
+  async run() {
+    const { carList, cnt } = await this.getInput();
+    const cars = carList.map((name) => ({ name, position: 0 }));
+  }
 
   // 입력 받기
   async getInput() {
@@ -13,6 +16,16 @@ class App {
       await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n")
     );
     return { carList, cnt };
+  }
+
+  // 자동차 전진 여부 결정
+  moveCars(cars) {
+    cars.forEach((car) => {
+      const randomNum = Random.pickNumberInRange(0, 9);
+      if (randomNum >= 4) {
+        car.position += 1;
+      }
+    });
   }
 }
 
