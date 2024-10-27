9f6bf6f8d127f9a07eea087791833d3aae6eb38d
dawn0314 <sdawn0314@gmail.com>
2024-10-27 12:22:16 +0900

    refactor: run RacingGameController in App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import { Console } from "@woowacourse/mission-utils";
-import inputParser from "./utils/inputParser.js";
-import RacingGame from "./RacingGame.js";
-import { nameValidation, tryValidation } from "./utils/validators.js";
+import RacingGameController from "./controllers/RacingGameController.js";
 
 class App {
   async run() {
-    let carNamesInput = await Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
-    );
-    const carNames = inputParser(carNamesInput);
-    nameValidation(carNames);
-
-    let tryCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-    tryValidation(tryCount);
-
-    const game = new RacingGame(carNames, Number(tryCount));
-    const result = game.raceCars();
-
-    return result;
+    const controller = new RacingGameController();
+    await controller.run();
   }
 }
 
