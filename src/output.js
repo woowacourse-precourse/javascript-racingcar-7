d0113a8f0ce35cc6ec9a7d410aca7fecbe7a3f61
Jeong-Ag <kjungu1105@gmail.com>
2024-10-27 16:51:36 +0900

    feat: 최종 우승자 출력 함수 구현

--- src/output.js
+++ src/output.js
@@ -5,3 +5,8 @@ export function raceOutput(carList) {
     Console.print(`${carName} : ${"-".repeat(value)}`);
   });
 }
+
+export function raceWinnerOutput(winnerList) {
+  const winners = winnerList.join(", ");
+  Console.print(`최종 우승자 : ${winners}`);
+}
