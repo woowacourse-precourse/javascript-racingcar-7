5e1b82f887d3e84cbfb41e583f0e4f19faed4115
seunghyeon77 <jdyjsh77@naver.com>
2024-10-27 15:50:45 +0900

    feat: 우승자를 출력하는 메소드 추가

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -12,7 +12,9 @@ export class RacingGame {
   play() {
     for (let i = 0; i <= this.rounds; i++) {
       this.cars.forEach((car) => car.move());
+      this.printCarPosition();
     }
+    this.printWinnerCar();
   }
 
   // 각 자동차의 위치 출력
@@ -21,5 +23,15 @@ export class RacingGame {
       console.print(`${car.name} : ${"-".repeat(car.position)}`);
     });
     console.print("\n");
+  }
+
+  // 우승자를 출력하는 로직
+  printWinnerCar() {
+    const MAXPOSITION = Math.max(...this.cars.map((car) => car.position));
+    const WINNERS = this.cars
+      .filter((car) => car.position === MAXPOSITION)
+      .map((car) => car.name);
+
+    console.print(`우승자: ${WINNERS.join(", ")}`);
   }
 }
