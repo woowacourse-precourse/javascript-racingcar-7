4a394811eb2092c2f08d0290f2b573861e858884
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-10-27 13:55:51 +0900

    feat: import 추가
    index.js로 import 간략화
    
    - getInput 폴더 : 자동차이름 및 시도횟수 입력
    - validations 폴더 : 자동차이름 및 시도횟수 유효성 검사
    - raceLogic 폴더: 자동차 경주 실행
    - output 폴더: 우승자 출력

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
+import { Console } from '@woowacourse/mission-utils';
+
+import { getCarNames, getTryCount } from './getInput/index.js';
+import {
+  checkDuplicateNames,
+  checkEmptyString,
+  checkMinimumCars,
+  validateCarNames,
+  tryCountEmptyCheck,
+  tryCountIntegerCheck,
+  tryCountNumberCheck,
+  validateTryCountIsPositive,
+} from './validations/index.js';
+
+import { runRaceRound } from './raceLogic/runRaceRound.js';
+import { getWinners } from './output/getWinners.js';
+
 class App {
   async run() {}
 }
