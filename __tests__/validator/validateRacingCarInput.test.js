18190eaf2d3307731a3dee16f3f94373e5951451
moonseonghui <msh9980@naver.com>
2024-10-27 17:06:06 +0900

    refactor(validateRacingCarInput): 문자열 유효성 검사에서 test.each로 변경
    validateRacingCarInput이라는 함수에 대해서 단일 테스트를 진행하기 때문에 test.each가 더 적합하다고 판단

--- __tests__/validator/validateRacingCarInput.test.js
+++ __tests__/validator/validateRacingCarInput.test.js
@@ -1,12 +1,10 @@
 import { validateRacingCarInput } from '../../src/validator/validatePipeline';
 
 // 유효한 문자열 테스트 케이스
-describe.each([
+test.each([
   ['알파벳과 쉼표만 포함된 경우1', 'carA,carB,carC'],
   ['알파벳과 쉼표만 포함된 경우2', 'car,ra,carB'],
-  ['알파벳만 포함된 경우2', 'CAR'],
-])('유효한 문자열 검사: %s', (description, input) => {
-  test(`${description} : ${input} `, () => {
-    expect(validateRacingCarInput(input)).toBe(input);
-  });
+  ['알파벳만 포함된 경우', 'CaR'],
+])('[유효한 문자열 검사] %s : %s', (description, input) => {
+  expect(validateRacingCarInput(input)).toBe(input);
 });
