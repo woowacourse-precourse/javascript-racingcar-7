87aa61bd1e7fcdd4b95ec21217c459b8049e581c
Jeong-Ag <kjungu1105@gmail.com>
2024-10-27 16:51:14 +0900

    feat: 우승자 확인 함수 추가

--- src/utils/calculator.js
+++ src/utils/calculator.js
@@ -19,3 +19,21 @@ export function calculateRandomScores(carList) {
 
   return carList;
 }
+
+/**
+ *
+ * 주어진 점수 객체를 기반으로 최고 점수를 찾고,
+ * 해당 점수를 가진 자동차의 이름을 반환합니다.
+ *
+ * @param {Object} carList - 자동차 이름이 key, 점수가 value로 이루어진 객체
+ * @returns {Array<string>} 우승한 자동차의 이름을 가진 배열입니다.
+ */
+export function findWinner(carList) {
+  const maxScore = Math.max(...Object.values(carList));
+
+  const winnerList = Object.entries(carList)
+    .filter(([key, value]) => value === maxScore)
+    .map(([key]) => key);
+
+  return winnerList;
+}
