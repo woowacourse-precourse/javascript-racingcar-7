0ffbb9c3792058d0d58437a9b3c001e927bdd388
thisisyejin <yejink321@gmail.com>
2024-10-27 23:18:35 +0900

    refactor: 불필요한 print 제거
    입력을 받기 전 설명을 출력하는 print를 제거하고, readLineAsync에서
    출력하도록 하였습니다.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import { isValidNumber } from "./validation.js";
 class App {
   async run() {
     try {
-      MissionUtils.Console.print('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
-      const carName = await MissionUtils.Console.readLineAsync('');
+      const carName = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
       const carList = Car.makeCarList(carName);
 
-      MissionUtils.Console.print('시도할 횟수는 몇 회인가요?');
-      const tryTime = await MissionUtils.Console.readLineAsync('');
-      isValidNumber(tryTime); // 유효성 검사
+      const tryTime = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+      isValidNumber(tryTime);
 
       Car.race(carList, tryTime);
 
