025510a9f559ed45d9cc6e3bea66c730d29ef548
ahhyun <wndkgus0225@naver.com>
2024-10-27 15:10:37 +0900

    feat(error) : 0에 관한 에러 추가

--- src/error.js
+++ src/error.js
@@ -12,5 +12,6 @@ export function validateCarNames(names) {
 export function validateCount(count) {
   if (isNaN(count)) throw new Error(ERROR_MESSAGE.NOT_NUMBER);
   if (!Number.isInteger(Number(count))) throw new Error(ERROR_MESSAGE.NOT_INTEGER);
-  if (count <= 0) throw new Error(ERROR_MESSAGE.NEGATIVE_NUMBER);
+  if (count = 0) throw new Error(ERROR_MESSAGE.NUM_IS_ZERO);
+  if (count < 0) throw new Error(ERROR_MESSAGE.NEGATIVE_NUMBER);
 }
