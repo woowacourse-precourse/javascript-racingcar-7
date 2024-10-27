6cedb57eaef392808edf60c212169f353ea9ac6d
jeong922 <hwajeong922@naver.com>
2024-10-27 17:06:15 +0900

    refactor(App): 메소드 분리 및 코드 개선
    - 랜덤값과 자동차 이름을 #MIN_RANDOM_NUMBER 및 #MAX_CAR_NAME_LENGTH와 같은 private fields로 정의
    - 일부 메소드 private fields로 변경
    - 중복된 코드 개선
    - 단일 책임 원칙을 준수하기 위해 일부 메소드를 분리

--- src/App.js
+++ src/App.js
@@ -1,75 +1,82 @@
 import { Console, Random } from '@woowacourse/mission-utils';
 
 class App {
-  async getCarNames() {
+  #MIN_RANDOM_NUMBER = 4;
+  #MAX_CAR_NAME_LENGTH = 5;
+
+  async #getCarNames() {
     const input = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
 
-    const isComma = /^(\w+)(,\w+)*$/.test(input);
-
-    if (!input || !isComma) {
-      throw new Error('[ERROR] 잘못된 입력값 입니다.');
+    if (!this.#validateCarNames(input)) {
+      throw new Error(`[ERROR] 잘못된 입력값 입니다. : ${input}`);
     }
+
+    return input.split(',');
+  }
+
+  #validateCarNames(input) {
+    const isCommaSeparated = /^(\w+)(,\w+)*$/.test(input);
 
     const carNames = input.split(',');
 
-    const validateCarNamesLength = carNames.every((carName) => carName.length <= 5);
+    const validateCarNamesLength = carNames.every((carName) => carName.length <= this.#MAX_CAR_NAME_LENGTH);
 
-    if (!validateCarNamesLength) {
-      throw new Error('[ERROR] 잘못된 형식의 입력값입니다.');
-    }
-
-    return carNames;
+    return input && isCommaSeparated && validateCarNamesLength;
   }
 
-  async getAttemptCount() {
+  async #getAttemptCount() {
     const input = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
 
-    if (!input || Number.isNaN(parseInt(input))) {
-      throw new Error('[ERROR] 잘못된 형식의 입력값입니다.');
+    const attemptCount = parseInt(input);
+
+    if (!attemptCount || Number.isNaN(attemptCount)) {
+      throw new Error(`[ERROR] 잘못된 형식의 입력값입니다. : ${input}`);
     }
 
-    return parseInt(input);
+    return attemptCount;
   }
 
-  async getWinners(carNames, n) {
+  async #printAttemptResults(carNames, cars, n) {
+    let result = '\n실행 결과\n';
+
+    for (let i = 0; i < n; i++) {
+      const attemptResults = carNames.map((carName) => {
+        const randomNumber = Random.pickNumberInRange(0, 9);
+
+        if (randomNumber >= this.#MIN_RANDOM_NUMBER) {
+          cars[carName] += randomNumber;
+        }
+
+        return `${carName} : ${'-'.repeat(randomNumber)}`;
+      });
+
+      result += attemptResults.join('\n') + '\n\n';
+    }
+
+    Console.print(result.trim() + '\n');
+  }
+
+  async #calculateScores(carNames, n) {
     const cars = {};
 
     carNames.forEach((car) => (cars[car] = 0));
 
-    Console.print('\n실행 결과');
+    await this.#printAttemptResults(carNames, cars, n);
 
-    for (let i = 0; i < n; i++) {
-      const result = [];
-      for (const carName of carNames) {
-        const randomNumber = Random.pickNumberInRange(0, 9);
+    return cars;
+  }
 
-        if (randomNumber >= 4) {
-          cars[carName] += randomNumber;
-        }
+  #getWinners(cars) {
+    const maxScore = Math.max(...Object.values(cars));
 
-        result.push(`${carName} : ${'-'.repeat(randomNumber)}`);
-      }
-
-      Console.print(result.join(' \n') + '\n');
-    }
-
-    const max = Math.max(...Object.values(cars));
-
-    const result = [];
-
-    for (const [car, score] of Object.entries(cars)) {
-      if (score === max) {
-        result.push(car);
-      }
-    }
-
-    return result;
+    return Object.keys(cars).filter((car) => cars[car] === maxScore);
   }
 
   async run() {
-    const carNames = await this.getCarNames();
-    const count = await this.getAttemptCount();
-    const winnners = await this.getWinners(carNames, count);
+    const carNames = await this.#getCarNames();
+    const attemptCount = await this.#getAttemptCount();
+    const scores = await this.#calculateScores(carNames, attemptCount);
+    const winnners = this.#getWinners(scores);
 
     Console.print(`최종 우승자 : ${winnners.join(', ')}`);
   }
