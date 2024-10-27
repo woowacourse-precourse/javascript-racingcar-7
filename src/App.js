c5e2d2deaa589c45c29aef97f7d35937c1181a12
@u_limit_ <yurim@ajou.ac.kr>
2024-10-27 19:19:57 +0900

    feat: 시도할 횟수 입력시 0 또는 공백을 입력하는 경우 예외 처리

--- src/App.js
+++ src/App.js
@@ -12,11 +12,19 @@ const determineCarName = (input) => {
 
 const validateRaceNumber = (raceNum) => {
   const numValue = Number(raceNum);
+
   if (Number.isNaN(numValue)) {
     throw new Error('[ERROR] 숫자를 입력해주세요.');
   }
+
   if (numValue < 0) {
     throw new Error('[ERROR] 시도할 횟수는 양수만 입력 가능해요.');
+  }
+
+  if (numValue === 0) {
+    throw new Error(
+      '[ERROR] 0 또는 공백은 입력할 수 없어요. 유효한 숫자를 입력해주세요.'
+    );
   }
 };
 
