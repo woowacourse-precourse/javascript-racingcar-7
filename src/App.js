564d88893d98eb32db35d158a5092dfff89e72f1
HYUNJIN105 <hyunjin.011005@gmail.com>
2024-10-27 21:54:41 +0900

    feat: 이동 횟수 입력 기능 구현
    자동차를 몇번 전진 또는 멈출 것인지에 대한
    이동 횟수를 사용자가 입력할 수 있는 기능을 구현했다.
    입력한 이동 횟수가 양의 정수여야하는 조건을 추가해,
    양의 정수가 아닌 입력일 경우, [ERROR] 처리를 했다.
    
    또, 변수명을 camelCase 네이밍으로 통일하고, 상수는
    snakeCase 네이밍 규칙을 지키도록 변수명을 수정했다.

--- src/App.js
+++ src/App.js
@@ -3,25 +3,36 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     try {
-      const car_names = await App.get_car_names();
-      Console.print(`${car_names.join(', ')}`);
+      const carNames = await App.getCarNames();
+      const moveAttempts = await App.getMoveAttempts();
     } catch (error) {
       Console.print(`[ERROR] ${error.message}`);
     }
   }
 
-  static async get_car_names() {
-    const input_names = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ');
-    const car_names = input_names.split(',').map(name => name.trim());
+  static async getCarNames() {
+    const inputNames = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const carNames = inputNames.split(',').map(name => name.trim());
     const MAX_CAR = 5;
 
-    car_names.forEach(name => {
+    carNames.forEach(name => {
       if (name.length > MAX_CAR) {
-        throw new Error('자동차 이름은 5자 이하만 가능합니다.');
+        throw new Error('[ERROR] 자동차 이름은 5자 이하만 가능합니다.');
       }
     });
 
-    return car_names;
+    return carNames;
+  }
+
+  static async getMoveAttempts() {
+    const inputAttempts = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    const moveAttempts = parseInt(inputAttempts, 10);
+
+    if (Number.isNaN(moveAttempts) || moveAttempts <= 0) {
+      throw new Error('[ERROR] 시도할 횟수는 양의 정수여야 합니다.');
+    }
+
+    return moveAttempts;
   }
 }
 
