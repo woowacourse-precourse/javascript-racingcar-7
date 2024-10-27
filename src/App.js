2faf9fdc712a3abd331127b964f3ff87fddeb200
suubiin <127467411+Suubiin@users.noreply.github.com>
2024-10-28 04:42:04 +0900

    feat: 입력받은 횟수만큼 경주 게임 진행 및 차수별 결과 출력

--- src/App.js
+++ src/App.js
@@ -10,10 +10,36 @@ class App {
     const attempts = await Console.readLineAsync(
       '시도할 횟수는 몇 회인가요?\n'
     );
+
+    const cars = carNames.map((name) => ({ name, position: 0 }));
+    this.runRace(cars, attempts);
   }
 
   splitCarNames(carNamesInput) {
     return carNamesInput.split(',');
+  }
+
+  runRace(cars, attempts) {
+    for (let attempt = 0; attempt < attempts; attempt++) {
+      this.updateCarPositions(cars);
+      this.printRaceStatus(cars);
+    }
+  }
+
+  updateCarPositions(cars) {
+    cars.forEach((car) => {
+      const randomNumber = Random.pickNumberInRange(0, 9);
+      if (randomNumber >= 4) {
+        car.position += 1;
+      }
+    });
+  }
+
+  printRaceStatus(cars) {
+    cars.forEach((car) => {
+      Console.print(`${car.name} : ${'-'.repeat(car.position)}`);
+    });
+    Console.print('');
   }
 }
 
