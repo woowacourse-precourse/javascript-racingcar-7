2b924278fffeea4aacec42f848563cb64c180a89
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-10-27 19:11:50 +0900

    refactor: CarRacingInputValidator 클래스 함수 분리
    - 검증 클래스의 메소드역시 단일 책임을 지킬 수 있도록 메소드를 분리

--- src/validator/CarRacingInputValidator.js
+++ src/validator/CarRacingInputValidator.js
@@ -9,16 +9,29 @@ class CarRacingInputValidator {
 
   static validateCarNames(input) {
     const carNames = input.split(',');
+    
+    this.validateEmptyCarNames(carNames);
+    this.validateCarNameLength(carNames);
+    this.validateUniqueCarNames(carNames);
+  }
 
+  static validateEmptyCarNames(carNames) {
     carNames.forEach((name) => {
       if (!name.trim()) {
         throw new Error(ERROR_MESSAGES.INPUT.EMPTY_CAR_NAME);
       }
+    });
+  }
+
+  static validateCarNameLength(carNames) {
+    carNames.forEach((name) => {
       if (name.length > 5) {
         throw new Error(ERROR_MESSAGES.INPUT.INVALID_CAR_NAME);
       }
     });
+  }
 
+  static validateUniqueCarNames(carNames) {
     const uniqueNames = new Set(carNames);
     if (uniqueNames.size !== carNames.length) {
       throw new Error(ERROR_MESSAGES.INPUT.DUPLICATE_CAR_NAME);
@@ -26,11 +39,19 @@ class CarRacingInputValidator {
   }
 
   static validateTotalRounds(input) {
-    const parsedInput = parseInt(input, 10);
+    this.validateIsNumber(input);
+    this.validatePositiveRounds(input);
+  }
 
+  static validateIsNumber(input) {
+    const parsedInput = parseInt(input, 10);
     if (isNaN(parsedInput)) {
       throw new Error(ERROR_MESSAGES.INPUT.INVALID_NUMBER);
     }
+  }
+
+  static validatePositiveRounds(input) {
+    const parsedInput = parseInt(input, 10);
     if (parsedInput <= 0) {
       throw new Error(ERROR_MESSAGES.INPUT.INVALID_ROUNDS);
     }
