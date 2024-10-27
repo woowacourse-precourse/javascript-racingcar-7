c0c1b594287d10484eaa26f62363b088358bb89a
immaemi <nam14785@soongsil.ac.kr>
2024-10-27 19:42:33 +0900

    feat: Input car names and number of matches
    Added new feature to App.js
    - Input the car names
    - If the car name exceeds 5 characters, an ERROR is output
    - Enter the total number of matches to be played

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    let input = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    let cars = input.split(',').map(name => name.trim());
+
+    for(let i = 0; i < cars.length; i += 1){
+      if(cars[i].length > 5 ){
+        throw new Error('[ERROR] 이름은 5글자 이하로 입력해주세요.');
+      }
+    }
+
+    let numberOfMatches = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+  }
 }
 
 export default App;
