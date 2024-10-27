c4e9b1e9d54308134c7510d2634a5121615ae891
yheseul <kangyheseul@naver.com>
2024-10-27 16:08:53 +0900

    refactor: 반복문 최적화로 우승자 추출 성능 개선
    - 객체의 데이터를 배열로 변환 및 정렬 후 최고 이동 거리를 가진 플레이어를 찾는 과정을 단일 반복문으로 리팩토링하여 성능 향상

--- src/carRace/runResult.js
+++ src/carRace/runResult.js
@@ -2,24 +2,19 @@ import { Console } from "@woowacourse/mission-utils";
 import { CAR_RACE, GAP } from "../constants/constants.js";
 
 export function runResult(playersObject) {
-  let arr = [];
+  let maxMove = 0;
+  let winners = [];
 
-  for (const player in playersObject) {
-    arr.push([player, playersObject[player]]);
+  for (const [player, moves] of Object.entries(playersObject)) {
+    const moveCount = moves.length;
+
+    if (moveCount > maxMove) {
+      maxMove = moveCount;
+      winners = [player];
+    }
+    if (!winners.includes(player) && moveCount === maxMove)
+      winners.push(`${GAP}${player}`);
   }
 
-  const sortArray = arr.sort(function (a, b) {
-    return b[1].length - a[1].length;
-  });
-
-  let winners = [sortArray[0][0]];
-
-  for (let i = 1; i < sortArray.length; i++) {
-    const moveResult = sortArray[0][1];
-    const nextNMoveResult = sortArray[i][1];
-    const name = sortArray[i][0];
-    if (moveResult === nextNMoveResult) winners.push(`${GAP}${name}`);
-  }
-
-  Console.print(`${CAR_RACE.FINAL_WINNER}${winners}`);
+  Console.print(`${CAR_RACE.FINAL_WINNER}${winners.join(",")}`);
 }
