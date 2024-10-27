33f6ea312affe6ff7bf3ad3b25c3f3cc5b6831f9
suhwa <tnghk9611@naver.com>
2024-10-28 01:46:04 +0900

    refactor(Validator): 예외처리 메서드명 수정
    - isValid는 boolean 값을 기대하는 의미를 가지나, 현재 함수는 아무런 값을 반환하지 않으므로 검사의 의미를 나타내는 check로 수정

--- src/Validator.js
+++ src/Validator.js
@@ -1,7 +1,7 @@
 import { CAR_NAME_ERROR, TRY_COUNT_ERROR } from "./constants/Message";
 
 class Validator {
-    isValidCarName(carNames) {
+    checkCarName(carNames) {
         for (let car of carNames) {
             if (car.length > 5) {
                 throw new Error(CAR_NAME_ERROR.OVER_LENGTH);
@@ -9,7 +9,7 @@ class Validator {
         }
     }
 
-    isValidTryCount(tryCount) {
+    checkTryCount(tryCount) {
         if (isNaN(tryCount) || Number(tryCount) <= 0) {
             throw new Error(TRY_COUNT_ERROR.NOT_NUMBER);
         }
