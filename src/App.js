aee050d6d12f3e17fbe31190c71db7be899b55ea
seonae-j <wkdtjsdo135@naver.com>
2024-10-27 15:35:02 +0900

    refactor(App): 입력 변수명 변경
    - gameCarName 삭제 : Controller.js 파일 생성하여 게임 진행 함수 따로 관리

--- src/App.js
+++ src/App.js
@@ -2,28 +2,17 @@ import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
-    const userInputGameStart = (
+    const inputNames = (
       await Console.readLineAsync(
         '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
       )
     ).trim();
 
-    const userInputGameOfTime = (
+    const inputAttemps = (
       await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n')
     ).trim();
 
-    class gameCarName {
-      constructor(userInput) {
-        this.name = this.splitName(userInput);
-      }
-
-      splitName(userInput) {
-        return userInput.split(',').map((name) => name.trim());
-      }
-    }
-
     try {
-      new gameCarName(userInputGameStart);
     } catch (error) {
       throw new Error('[ERROR]' + error.message);
     }
