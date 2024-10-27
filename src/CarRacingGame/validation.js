c04ecaf8ac0d199ad06b1e4be35c5443e5cdf469
jjang-aaa <jadoit473@gmail.com>
2024-10-27 18:27:43 +0900

    fix(validation): validateTryCount함수에서 음수를 체크하지 못했던 오류 수정

--- src/CarRacingGame/validation.js
+++ src/CarRacingGame/validation.js
@@ -21,7 +21,7 @@ export function validateTryCount(tryCount) {
     throw new Error('[ERROR] 정수만 입력해 주세요.');
   }
 
-  if (tryCount === 0) {
+  if (tryCount <= 0) {
     throw new Error('[ERROR] 0보다 큰 수를 입력해 주세요.');
   }
 }
