0bd451fc8687cf60d3d7543dd9b41778de28cc3a
yoo_min <vendetta12@naver.com>
2024-10-27 20:14:31 +0900

    refactor: 시도횟수 유효성 검사 기능 분리
    문자, 음수, 공백을 한 함수에서 한번에 검사를 하던 것을 기능 분리를 위해 분리

--- src/utils/CountNumValidator.js
+++ src/utils/CountNumValidator.js
@@ -1,14 +1,28 @@
 import ErrorMessages from "../constant/ErrorMessage";
 
 class CountNumValidator {
-    checkCountNumIsNonePositiveNumber(countNum) {
-        if (isNaN(countNum) || countNum <= 0 || countNum === "") {
+    checkCountNumIsCharacter(countNum) {
+        if (isNaN(countNum)) {
+            throw new Error(ErrorMessages.COUNT_NUM_POSITIVE);
+        }
+    }
+
+    checkCountNumIsNegative(countNum) {
+        if (countNum <= 0) {
+            throw new Error(ErrorMessages.COUNT_NUM_POSITIVE);
+        }
+    }
+
+    checkCountNumIsEmpty(countNum) {
+        if (countNum === "") {
             throw new Error(ErrorMessages.COUNT_NUM_POSITIVE);
         }
     }
 
     validateCountNum(countNum) {
-        this.checkCountNumIsNonePositiveNumber(countNum);
+        this.checkCountNumIsCharacter(countNum);
+        this.checkCountNumIsEmpty(countNum);
+        this.checkCountNumIsNegative(countNum);
     }
 }
 export default CountNumValidator;
