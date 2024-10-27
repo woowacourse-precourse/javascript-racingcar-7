6150e3627766e52111d20d1622ae656efb006ec4
parkseyoung <hellosy76@naver.com>
2024-10-27 13:42:13 +0900

    refactor: 입력받는 것 함수로 빼기

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import { shouldMoveForward } from "./Random.js";
 
 class App {
   async run() {
+    try {
+      const input = await this.getUserInput();
+      const winner = PlayGame(carlist, repeat);
+      Console.print(`최종 우승자 : ${winner}`);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+  async getUserInput() {
     const carlist = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분).\n"
     );
     const repeat = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-
-    try {
-    } catch (error) {
-      throw new Error(error.message);
-    }
+    return input;
   }
 }
 
