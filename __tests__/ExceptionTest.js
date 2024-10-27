ae4dcda5751d9ef55abb2951c000471c24dfb893
최영빈 <bin778@naver.com>
2024-10-27 15:33:42 +0900

    test: 자동차가 2개인지 여부 검사 테스트 케이스 추가

--- __tests__/ExceptionTest.js
+++ __tests__/ExceptionTest.js
@@ -1,8 +1,9 @@
 import {
   ERR_LENGTH,
   ERR_COR_NAME,
-  ERR_POSITIVE,
   ERR_ISDUP,
+  ERR_TWO_CAR,
+  ERR_POSITIVE,
   ERR_ISINT,
 } from "../src/Component/Error.js";
 import App from "../src/App.js";
@@ -35,7 +36,7 @@ describe("예외 테스트", () => {
     [["두 산"], ERR_COR_NAME()],
     [["+==,!!*"], ERR_COR_NAME()],
     [["a1,a2,a4,a+10"], ERR_COR_NAME()],
-  ])("자동차 이름이 1~5자 사이가 아닌 경우", async (input, expectedError) => {
+  ])("자동차 이름이 적절하지 않은 경우", async (input, expectedError) => {
     mockQuestions(input);
     const app = new App();
     await expect(app.run()).rejects.toThrow(expectedError);
@@ -53,9 +54,18 @@ describe("예외 테스트", () => {
   });
 
   test.each([
+    [["a"], ERR_TWO_CAR()],
+    [["최씨"], ERR_TWO_CAR()],
+  ])("자동차가 1개만 있을 경우", async (input, expectedError) => {
+    mockQuestions(input);
+    const app = new App();
+    await expect(app.run()).rejects.toThrow(expectedError);
+  });
+
+  test.each([
     [["choi,lee", "0"], ERR_POSITIVE()],
-    [["kim", "-2"], ERR_POSITIVE()],
-    [["kim", "-2.5"], ERR_POSITIVE()],
+    [["kim,park", "-2"], ERR_POSITIVE()],
+    [["kim,bug", "-2.5"], ERR_POSITIVE()],
     [["a,e,u", "0.0"], ERR_POSITIVE()],
   ])("1 이상의 숫자를 입력하지 않은 경우", async (input, expectedError) => {
     mockQuestions(input);
@@ -64,8 +74,8 @@ describe("예외 테스트", () => {
   });
 
   test.each([
-    [["choi", "a"], ERR_ISINT()],
-    [["kim", "+:/|-"], ERR_ISINT()],
+    [["choi,mark", "a"], ERR_ISINT()],
+    [["kim,john", "+:/|-"], ERR_ISINT()],
     [["haha,hehe", "페라리"], ERR_ISINT()],
     [["a,b,c,d,e", "+10"], ERR_ISINT()],
     [["choi,lee", "1.5"], ERR_ISINT()],
