0cda667ed18518eeebccf20b6935e09424f7f883
mun-kyeong <horse6953@naver.com>
2024-10-27 14:19:50 +0900

    [STYLE] (raceCar) : 띄워쓰기 추가
    - 띄워쓰기 추가
    
    출력 형식을 맞추기 위해 띄워쓰기를 추가했습니다.

--- src/Components/Output/printCurrRaceState.js
+++ src/Components/Output/printCurrRaceState.js
@@ -3,7 +3,7 @@ import { Console } from "@woowacourse/mission-utils";
 const printCurrRaceState = (raceCarList) => {
   Console.print(
     raceCarList
-      .map((car) => `${car.name}: ${"-".repeat(car.position)}`)
+      .map((car) => `${car.name} : ${"-".repeat(car.position)}`)
       .join("\n")
   );
   Console.print("\n");
