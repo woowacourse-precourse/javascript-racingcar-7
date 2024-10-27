6225846c0f438dcbc024707b8e0c7c7cfa7948bc
Kim_Yjoo <pas4976@gmail.com>
2024-10-27 18:19:15 +0900

    feat(UserInput) : 사용자 입력을 문자열 처리하고, 이름 규칙에 따른 에려 발생 로직 추가 feat(constants) : 특수문자 확인을 위한 특수문자 정규식 상수 추가

--- src/Input.js
+++ src/Input.js
@@ -1,19 +1,49 @@
 import { Console } from '@woowacourse/mission-utils';
-
+import * as cs from './constants.js';
 class UserInput {
     constructor(){
         this.initialize();
     }
-    async initialize() {
-        await this.setCarsName();
-        await this.setAttemptNum();
+    async initialize(){
+        this.carNames = await this.setCarNames();
+        this.attemptNum = await this.setAttemptNum();
     }
-    async setCarsName(){
-        this.carNames = await Console.readLineAsync('경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n');
+    async setCarNames(){
+        let namesString = await Console.readLineAsync('경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n');
+        return await this.setNameProcess(namesString);
     }
     async setAttemptNum(){
         this.attemptNum = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
     }
+    async setNameProcess(namesString){
+        let separatedString = this.separateCarsName(namesString);
+        this.checkCarNames(await separatedString);
+        return await separatedString;
+    }
+
+    async separateCarsName(namesString){
+        let separatedString = [];
+        let namesNominees = namesString.split(',');
+        for(let carName of namesNominees) {
+            separatedString.push(carName.trim());
+        }
+        
+        return separatedString;
+    }
+    async checkCarNames(carNameCandidates){
+        for(let candidate of carNameCandidates){
+            this.checkNamingRules(candidate);
+        }
+    }
+    async checkNamingRules(inputName){
+        if(inputName.length > 5){
+            throw new Error("[ERROR] 자동차 이름 에러. (5자 이상 자동차 이름 사용)");
+        }
+
+        if(cs.SPECIALCHARACTER.test(inputName)){
+            throw new Error('[ERROR] 자동차 이름 에러. (특수문자 사용 불가)')
+        }   
+    }   
 }
 
 export default UserInput;
