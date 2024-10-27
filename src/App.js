ba8245885634a4e825a20d58dadeca23d9f80295
Rigu1 <dlsrbtla@gmail.com>
2024-10-28 02:50:44 +0900

    refactor(App): Improve readability by renaming variables and modularizing input handling
    - Line 5: PROMPT_MESSAGES 객체로 메시지를 상수화하여 가독성과 유지보수성 향상
    - Line 8: getCarProgressRecords 메서드 내 변수명 name을 carName으로 변경하여
      직관성 향상; 객체의 name 속성을 name: carName 형식으로 수정
    - Line 16: getCarNames와 getTryCount 메서드로 입력 로직 분리하여 코드 가독성 개선
    - Line 23: 변수명 carsList를 carNameList로 수정하여 데이터 의미 명확하게 개선

--- src/App.js
+++ src/App.js
@@ -3,26 +3,39 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 const DELIMITER = ",";
 const DEFAULT_POSITION = 0;
+const PROMPT_MESSAGES = {
+  CAR_NAMES: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+  TRY_COUNT: "시도할 횟수는 몇 회인가요?\n",
+};
 
 class App {
-  #getCarProgressRecords(carsList) {
-    return carsList.map((name) => ({
-      name,
+  async #getCarNames() {
+    const carsStr = await MissionUtils.Console.readLineAsync(
+      PROMPT_MESSAGES.CAR_NAMES
+    );
+    const carNameList = carsStr.split(DELIMITER).map((name) => name.trim());
+    return carNameList;
+  }
+
+  async #getTryCount() {
+    const tryCountStr = await MissionUtils.Console.readLineAsync(
+      PROMPT_MESSAGES.TRY_COUNT
+    );
+    return parseFloat(tryCountStr);
+  }
+
+  #getCarProgressRecords(carNameList) {
+    return carNameList.map((carName) => ({
+      name: carName,
       position: DEFAULT_POSITION,
     }));
   }
 
   async run() {
-    const carsStr = await MissionUtils.Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
-    );
-    const carsList = carsStr.split(DELIMITER).map((name) => name.trim());
+    const carNameList = await this.#getCarNames();
+    const tryCount = await this.#getTryCount();
 
-    const tryCount = await MissionUtils.Console.readLineAsync(
-      "시도할 횟수는 몇 회인가요?\n"
-    );
-
-    const carProgressRecords = this.#getCarProgressRecords(carsList);
+    const carProgressRecords = this.#getCarProgressRecords(carNameList);
     const carRaceGame = new RacingCar(carProgressRecords, tryCount);
 
     carRaceGame.startRace();
