78a6b61031d1c464c8835bda3e325ad327bd1a27
dawn0314 <sdawn0314@gmail.com>
2024-10-27 11:28:25 +0900

    feat: add input parser to split car names

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { Console } from "@woowacourse/mission-utils";
+import inputParser from "./inputParser.js";
 
 class App {
   async run() {
     let carNamesInput = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
-
+    const carNames = inputParser(carNamesInput);
     let tryCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
 
-    Console.print(`${carNamesInput} ${tryCount}`);
+    Console.print(`${carNames} ${tryCount}`);
   }
 }
 
---
+++ src/inputParser.js
@@ -0,0 +1,8 @@
+import { Console } from "@woowacourse/mission-utils";
+
+function inputParser(input) {
+  const names = input.split(",");
+  return names;
+}
+
+export default inputParser;
