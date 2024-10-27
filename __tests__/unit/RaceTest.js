c1153ede707be7bfdec359ac8bf5401a196a0035
Daeuning <happyjde3@gmail.com>
2024-10-28 01:14:38 +0900

    test(RaceTest): Race 클래스 분리에 따라 RaceTest 코드 수정

--- __tests__/unit/RaceTest.js
+++ __tests__/unit/RaceTest.js
@@ -1,32 +1,31 @@
 import Race from '../../src/Race.js';
+import Car from '../../src/Car.js';
 import { getRandomNumber, printMessage } from '../../src/utils.js';
-import { GAME_RULES } from '../../src/constants.js';
 
 jest.mock('../../src/utils.js', () => ({
-  ...jest.requireActual('../../src/utils.js'), 
-  getCarName: jest.fn().mockResolvedValue("Audi,BMW,Ford"), 
-  getAttempt: jest.fn().mockResolvedValue("5"),
+  ...jest.requireActual('../../src/utils.js'),
   getRandomNumber: jest.fn(),
   printMessage: jest.fn(),
 }));
-
 
 describe("Race 클래스 테스트", () => {
   let race;
 
   beforeEach(() => {
     race = new Race();
-    jest.clearAllMocks(); 
+    race.cars = [
+      new Car("Audi"),
+      new Car("BMW"),
+      new Car("Ford")
+    ];
+    jest.clearAllMocks();
   });
 
   describe("generateRandomDistances() 테스트", () => {
-
-    test("무작위 값이 4 이상일 때만 Car 객체가 이동해야 한다", async () => {
+    test("무작위 값이 4 이상일 때만 Car 객체가 이동해야 한다", () => {
       getRandomNumber.mockReturnValueOnce(3)
                      .mockReturnValueOnce(5)
                      .mockReturnValueOnce(7);
-      
-      await race.setCarName();
 
       race.generateRandomDistances();
 
@@ -37,33 +36,22 @@ describe("Race 클래스 테스트", () => {
   });
 
   describe("getWinners() 테스트", () => {
-
-    test("최종 우승자를 올바르게 결정해야 한다", async () => {
-      await race.setCarName();
-
+    test("최종 우승자를 올바르게 결정해야 한다", () => {
       race.cars[0].move(6); 
       race.cars[1].move(4); 
       race.cars[2].move(2); 
 
       const winners = race.getWinners();
       expect(winners).toEqual(["Audi"]);
-
-      race.printWinners();
-      expect(printMessage).toHaveBeenCalledWith("최종 우승자 : Audi");
     });
 
-    test("최종 우승자가 여러 명일 때도 올바르게 결정해야 한다", async () => {
-      await race.setCarName();
-
+    test("최종 우승자가 여러 명일 때도 올바르게 결정해야 한다", () => {
       race.cars[0].move(5); 
       race.cars[1].move(5); 
       race.cars[2].move(3); 
 
       const winners = race.getWinners();
       expect(winners).toEqual(["Audi", "BMW"]);
-
-      race.printWinners();
-      expect(printMessage).toHaveBeenCalledWith("최종 우승자 : Audi, BMW");
     });
   });
 });
