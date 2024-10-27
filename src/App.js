62fc9d2dce9d97ad4565cdcc2b70811ecc82594f
Euiyeong <105384780+snsdl0905@users.noreply.github.com>
2024-10-27 15:16:30 +0900

    feat: 쉼표로 구분된 자동차 이름을 입력받으며, 각 이름은 5자 이하이다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const names = await this.getNames();
+  }
+
+  async getNames() {
+    try{
+      const input = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+      return this.splitNames(input);
+    } catch(error) {
+      throw new Error(`[ERROR]: ${error.message}`);
+    }
+  }
+
+  splitNames(input) {
+    let names = input.split(',');
+    for(let name of names){
+      if (name.length > 6){
+        throw new Error ('[ERROR]: 자동차의 이름은 5자 이하여야한다.');
+      }
+    }
+    return names;
+  }
 }
 
 export default App;
