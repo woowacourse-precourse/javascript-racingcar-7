b1144ecfaa8203b069d6cffd159db15646a1ae3a
minjeong9919 <minjeong9919@naver.com>
2024-10-28 02:57:50 +0900

    refactor: 전진 크기 수정
    - 무작위 값에 따른 움직임을 무작위 값에서 1로 수정

--- src/utils/calculateMovement.js
+++ src/utils/calculateMovement.js
@@ -3,7 +3,7 @@ import { Random } from "@woowacourse/mission-utils";
 export const calculateMovement = () => {
   const randomNumber = Random.pickNumberInRange(1, 9);
   if (randomNumber >= 4) {
-    return randomNumber;
+    return 1;
   }
   return 0;
 };
