cb45d7d73e694796af3e1ff3d835c6ba774c783f
pengdyuo <121721388+Pengdyuo@users.noreply.github.com>
2024-10-28 05:14:26 +0900

    자동차 이름 위치 관리 및 이동

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-class App {
-  async run() {}
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.position = 0;
+  }
+
+  move() {
+    const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+    if (randomNumber >= 4) {
+      this.position += 1;
+    }
+  }
 }
 
-export default App;
+export default Car;
