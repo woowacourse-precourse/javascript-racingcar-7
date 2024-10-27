7107e63aa9a139caccc4e5fd6b10a1d150cb3668
MERx88 <wjang07@gmail.com>
2024-10-27 11:14:18 +0900

    Feat(App): add feature to split car names
    - Add feature to split car names from user input to utilize the entered car names.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
+  async parseCarNames(userCarNames) {
+    return userCarNames.split(",");
+  }
   async readCarNames() {
     const userCarNames = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
@@ -8,7 +11,8 @@ class App {
     if (!userCarNames.includes(",")) {
       throw new Error("[ERROR] 자동차 이름을 두개 이상 입력해주세요");
     }
-    return userCarNames;
+    const parsedCarNames = this.parseCarNames(userCarNames);
+    return parsedCarNames;
   }
 
   async run() {
