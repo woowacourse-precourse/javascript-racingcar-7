87fea6a9c05a3a23e7a9c97b566b4f8337895e42
boxi <boxiyoun@gmail.com>
2024-10-28 03:51:11 +0900

    test파일 추가

--- __tests__/carTest.js
+++ __tests__/carTest.js
@@ -23,5 +23,33 @@ const getLogSpy = () => {
   logSpy.mockClear();
   return logSpy;
 };
+describe("자동차 경주", () => {
+  test("콤마 뒤에 아무 이름도 없다면 예외 발생", async () => {
+    const inputs = ["pobi,"];
+    mockQuestions(inputs);
+    const app = new App();
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
 
-// 
+  test("자동차 이름을 5자 이상 입력하면 예외 발생", async () => {
+    const inputs = ["pobipobi, woni, junjun"];
+    mockQuestions(inputs);
+    const app = new App();
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("시도 횟수가 0 일경우 예외 발생", async () => {
+    const inputs = ["0,aaa,bbbbb"];
+    mockQuestions(inputs);
+    const app = new App();
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+
+  test("시도 횟수가 음수일경우 예외 발생", async () => {
+    const inputs = ["-5,aaaa,bbbb"];
+    mockQuestions(inputs);
+    const app = new App();
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+});
