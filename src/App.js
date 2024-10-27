cb65a8325f14955f792bc08b35bff8c41413dd79
jeonhyeonseon <goodpretty0325@gmail.com>
2024-10-27 19:01:16 +0900

    feat(app): 사용자 입력 기능 추가
    - 사용자 입력을 위한 getInput 메소드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+
+    const carNames = await this.getInput("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ");
+    const position = await this.getInput("시도할 횟수를 입력하세요: ");
+    
+  }
+
+  async getInput(promptText) {
+    return MissionUtils.Console.readLineAsync(promptText);
+  }
+
 }
 
 export default App;
