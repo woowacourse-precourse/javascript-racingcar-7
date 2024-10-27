5d7b98622ddbc047fc11df2cc8fed5512bf09f6c
KimPuro <puro@protonmail.ch>
2024-10-28 00:33:12 +0900

    feat(App): 자동차 이름 입력 기능 추가
    carNameInput() 함수로 5자 이하의 자동차 이름을 입력 받습니다. 5자를 초과한다면 Error를 발생시킵니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import {Console} from '@woowacourse/mission-utils'
+
 class App {
-  async run() {}
+    async run() {
+
+    }
+
+    async carNameInput() {
+        const inputArray = await Console.readLineAsync("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)");
+        const carArray = Array.split(inputArray, ',');
+
+        carArray.forEach(carName => {
+            if (carName.length > 5) {
+                throw new Error("[ERROR] 자동차 이름은 5글자를 초과할 수 없습니다.");
+            }
+        });
+        return carArray;
+    }
 }
 
 export default App;
