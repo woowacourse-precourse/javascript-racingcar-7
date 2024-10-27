6bd538d0ddd62098cc159d51c1dc653a6b63e86f
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2024-10-27 17:36:38 +0900

    fix:fix winner logic before winner repeated

--- src/game/carAction.js
+++ src/game/carAction.js
@@ -22,7 +22,10 @@ export const getWinner = (carData) => {
     if (car.location > winner[0].location) {
       winner.length = 0; // 새로운 우승자가 있으면 기존 우승자 목록을 비움
       winner.push(car);
-    } else if (car.location === winner[0].location) {
+    } else if (
+      car.location === winner[0].location &&
+      car.name !== winner[0].name
+    ) {
       winner.push(car); // 동일한 거리를 달린 경우 추가
     }
   });
