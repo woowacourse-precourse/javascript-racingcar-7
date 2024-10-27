e0e105cdcddfd814f3d3b6b9284fea67c39cd647
CJY <cjy3458@naver.com>
2024-10-28 04:23:51 +0900

    fix: 함수명 변경

--- src/controllers/displayResult.js
+++ src/controllers/displayResult.js
@@ -1,9 +1,9 @@
 import RacingResult from '../models/RacingResult.js';
 import OutputView from '../views/OutputView.js';
 
-function displayRaceResult(race) {
+function displayResult(race) {
   const winners = RacingResult.determineWinners(race.getCars());
   OutputView.printResult(race.getCars(), winners);
 }
 
-export default displayRaceResult;
+export default displayResult;
