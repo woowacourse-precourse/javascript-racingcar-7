842ad12ad049de5b807f3749fc0ab89410d086f5
seunghyeon77 <jdyjsh77@naver.com>
2024-10-27 16:13:33 +0900

    refactor: 자동차 이름 입력값 따로 변수에 저장

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { RacingGame } from "./RacingGame";
 
 class App {
   async run() {
-    const CARNAMES = await Console.readLineAsync("자동차 이름을 입력하세요 : ")
-      .trim()
-      .split(",");
+    const INPUT = await Console.readLineAsync("자동차 이름을 입력하세요 : ");
+    const CARNAMES = INPUT.trim().split(",");
+
     const ROUNDS = parseInt(
       await Console.readLineAsync("이동 횟수를 입력하세요 : "),
       10
