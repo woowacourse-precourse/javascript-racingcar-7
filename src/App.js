1ac2a55cb1de72c90a280afae2c086bc21542d4c
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2024-10-27 17:24:56 +0900

    refactor :refactor text app

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import { validateInput } from './parsers/Validate.js';
 class App {
   async run() {
     try {
-      const carInput = await input('경주할 자동차의 이름을 입력한다 : ');
-      const countInput = await input('시도할 횟수를 입력한다 : ');
+      const carInput = await input(
+        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+      );
+      const countInput = await input('시도할 횟수는 몇 회인가요?');
       const parsedCarInput = parseInputs(carInput);
       validateInput({ car: parsedCarInput, count: countInput });
       initGame({ car: parsedCarInput, count: countInput });
