5620c31f6f71e83a5c9e71ca9f309437507b7ab5
minjeong9919 <minjeong9919@naver.com>
2024-10-27 23:22:05 +0900

    feat: 사용자 입력 처리
    - 경주할 자동차 이름과 시도 횟수에 대한 비동기 입력 처리 구현
    - 입력 파싱과 디버깅을 통한 값 확인 완료

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
   async run() {
-    console.log("dd");
+    try {
+      const racingCarsInput = await Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+      );
+      const racingCars = racingCarsInput.split(",");
+      const tryNumber =
+        await Console.readLineAsync("시도할 횟수는 몇 회 인가요?\n");
+    } catch {
+      Console.error;
+    }
   }
 }
 
