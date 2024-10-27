07876f810809f6920218704b304abdfde9bc2f3a
youdame <112458620+youdame@users.noreply.github.com>
2024-10-27 22:02:29 +0900

    refactor : App을 컨트롤러로 설정 후 불필요한 파일 제거

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import Game from './controller/game.js';
+import InputView from './view/input-view.js';
+import validateCarName from './validation/validate-carname.js';
+import validateAttemptCount from './validation/validate-attempt-count.js';
+import Race from './model/race.js';
 
 class App {
   constructor() {
-    this.game = new Game();
+    this.inputView = new InputView();
   }
 
   async run() {
-    await this.game.process();
+    const carNamesInput = await this.inputView.getCarNames();
+    const carNameList = carNamesInput.split(',');
+    validateCarName(carNameList);
+
+    const attemptCountInput = await this.inputView.getAttemptCount();
+    const attemptCount = parseInt(attemptCountInput, 10);
+    validateAttemptCount(attemptCount);
+
+    const race = new Race(carNameList, attemptCount);
+    race.run();
   }
 }
 
