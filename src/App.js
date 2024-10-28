b66cd070269584e58a0af094b9a0f1cd1d1c6e02
gwansikk <gwansik.kim@gwansik.dev>
2024-10-28 20:59:33 +0900

    refactor: Race 모델 사용

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
-import { Console } from '@woowacourse/mission-utils';
-import { readUserInput } from './utils/readUserInput.js';
-import {
-  isCarNames,
-  isPositiveNumber,
-  isUniqueCarNames,
-} from './utils/validators.js';
-import Car from './models/Car.js';
+import Race from './models/Race.js';
 
 export default class App {
   async run() {
     try {
-      const carNames = await readUserInput(
-        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
-        [(str) => isCarNames(str, ','), (str) => isUniqueCarNames(str, ',')]
-      );
-      const tryNumber = await readUserInput('시도할 횟수는 몇 회인가요?', [
-        isPositiveNumber,
-      ]);
+      const race = new Race();
 
-      const cars = carNames.split(',').map((name) => new Car(name));
-
-      for (let index = 0; index < tryNumber; index++) {
-        cars.forEach((car) => {
-          Console.print(`${car.getName()} : ${'-'.repeat(car.move())}`);
-        });
-        Console.print('');
-      }
+      await race.ready();
+      race.start();
+      race.printWinners();
     } catch (error) {
       throw new Error(`[ERROR] ${error.message}`);
     }
