39e67203279295759183e9c56b248afcac09433c
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2024-10-28 20:01:43 +0900

    feat: 자동차 경주 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import Game from "./Game.js";
+import UserInput from "./UserInput.js";
+import { validateUserAttempts } from "./validate.js";
+
 class App {
-  async run() {}
+  constructor() {
+    this.userInput = new UserInput();
+    this.game = new Game();
+  }
+
+  async run() {
+    const getUserCarNames = await this.userInput.askCarNames();
+    this.game.initializeCars(getUserCarNames);
+    const askForAttempts = await this.userInput.askAttempts();
+    const availableAttempts = validateUserAttempts(askForAttempts);
+    this.game.play(availableAttempts);
+  }
 }
 
 export default App;
