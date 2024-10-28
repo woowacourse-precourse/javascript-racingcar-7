292d36f1a0d84c969d5532057dbed11b12aed931
Ray <rlawlsdn9583@gmail.com>
2024-10-28 23:28:49 +0900

    feat: 자동차 이름 입력과 시도 횟수 입력에 대한 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import { Console } from "@woowacourse/mission-utils";
 import Car from "./models/Car.js";
 import Race from "./models/Race.js";
+import { validateAttempts, validateCarNames } from "./validators.js";
+import { MESSAGES } from "./constant.js";
 
 class App {
   async run() {
-    try {
       // 경주에 참가할 자동차 이름 입력 받기
       Console.print(
-        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+        MESSAGES.PLEASE_CAR_NAMES
       );
       const carNames = await Console.readLineAsync("");
+      validateCarNames(carNames);
       const cars = carNames.split(",").map((carName) => new Car(carName));
 
       // 시도할 주행 횟수 입력
-      Console.print("시도할 횟수는 몇 회인가요?");
+      Console.print(MESSAGES.PLEASE_NUMBER_OF_ATTEMPTS);
       const numOfAttempts = await Console.readLineAsync("");
+      validateAttempts(numOfAttempts);
       const race = new Race(cars, numOfAttempts);
 
-      race.start(); // 입력값을 바탕으로 주행 시작
-    } catch (error) {
-      Console.print(`[ERROR] ${error.message}`);
-    }
+      // 입력값을 바탕으로 주행 시작!
+      race.start();
   }
 }
 
