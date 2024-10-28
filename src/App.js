9d2b11010aa455a1d5e761f7e90d5575c4ece144
이창준 <2018110021@dongguk.edu>
2024-10-28 21:49:24 +0900

    feat(App): 자동차 이름 입력 검증

--- src/App.js
+++ src/App.js
@@ -4,17 +4,38 @@ class App {
   async run() {
     try {
       let carNameInput = await Console.readLineAsync(`경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n`);
-      let carNames = carNameInput.split(",");
+      let carNames = this.validateCarNames(carNameInput);
       let raceCountInput = await Console.readLineAsync(`시도할 횟수는 몇 회인가요?\n`);
       let raceCount = parseInt(raceCountInput, 10);
 
-      console.log("names: ",carNames);
-      console.log("count: ",raceCount);
     } catch (error) {
-        Console.print(error.message);
-        throw error;
+      Console.print(`[ERROR] ${error.message}`);
+      throw error;
     }
   }
+
+  /**
+   * @author CWDll
+   * @describe 입력받은 문자열을 계산하는 함수
+   * @parameter {carNameInput: string[]}
+   * @returnValue {carNames}
+   */
+  validateCarNames(carNameInput) {
+    let carNames = carNameInput.split(",").map(name => name.trim());
+    if (carNames.length < 2) {
+      throw new Error("자동차 이름은 2개 이상 입력해야 합니다.");
+    }
+    carNames.forEach(name => {
+      if(name.length === 0 || name.length > 5) {
+        throw new Error("자동차 이름은 1자 이상 5자 이하만 가능합니다.");
+      }
+    })
+
+    return carNames;
+  }
 }
+
+
+
 
 export default App;
