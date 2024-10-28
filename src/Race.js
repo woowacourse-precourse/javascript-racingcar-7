de64fc7f7f919844d0e42440405aeab6bf0766f5
Seoyeon Kim <kseoy0607@gmail.com>
2024-10-28 15:39:49 +0900

    feat: 시도 횟수 입력 및 검증 기능 구현

--- src/Race.js
+++ src/Race.js
@@ -1,6 +1,7 @@
 import { splitByComma } from './utils/splitByComma.js';
 import InputView from './views/InputView.js';
 import Car from './Car.js';
+import Validator from './Validator.js';
 
 const MOVE_THRESHOLD = 4;
 
@@ -11,6 +12,14 @@ class Race {
     const carNames = await InputView.inputNames();
     const carsList = splitByComma(carNames);
     this.#carInstance = carsList.map((name) => new Car(name));
+
+    const raceAttempt = await InputView.inputAttempt();
+    const validAttempt = this.#validateAttempt(raceAttempt);
+  }
+
+  #validateAttempt(attempt) {
+    const validator = new Validator();
+    return validator.validateAttempt(attempt);
   }
 }
 
