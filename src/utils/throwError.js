63ed884dbe9121ce2e65ca126f16e9411da87c90
pjc0179 <pjc0179@gmail.com>
2024-10-28 22:30:57 +0900

    chore:"프리티어 수정 적용"

--- src/utils/throwError.js
+++ src/utils/throwError.js
@@ -1,4 +1,4 @@
-import MESSAGES from '../constants/messages.js';
+import MESSAGES from "../constants/messages.js";
 
 const throwError = (message) => {
   throw new Error(`${MESSAGES.ERROR} ${message}`);
