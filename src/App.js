840fa80345eab727aa78201c670bc3e9eb5527b5
asdfghjk1052@naver.com <asdfghjk1052@naver.com>
2024-10-28 21:03:32 +0900

    feature(App) 자동차 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const carNames = await this.getCarName();
+    Console.print(carNames);
+  }
+
+  // 자동차 입력
+  async getCarName() {
+    Console.print(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n"
+    );
+    const input = await Console.readLineAsync();
+    const carNames = input.split(",");
+    return carNames;
+  }
 }
 
 export default App;
