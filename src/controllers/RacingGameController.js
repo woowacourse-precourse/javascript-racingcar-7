1df5d1846a797b29658100ed3ca5eefead3bd0ef
dawn0314 <sdawn0314@gmail.com>
2024-10-28 10:58:40 +0900

    refactor: split run method in controller

--- src/controllers/RacingGameController.js
+++ src/controllers/RacingGameController.js
@@ -5,29 +5,51 @@ import RacingGameModel from "../models/RacingGameModel.js";
 
 class RacingGameController {
   async run() {
+    const carNames = await this.getCarNames();
+    const tryCount = await this.getTryCount();
+
+    RacingGameView.printStartMessage();
+
+    const game = new RacingGameModel(carNames, Number(tryCount));
+
+    this.playRounds(game, carNames);
+    this.displayWinners(game);
+  }
+
+  async getCarNames() {
     const carNamesInput = await RacingGameView.getInput(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
     const carNames = inputParser(carNamesInput);
     nameValidation(carNames);
 
+    return carNames;
+  }
+
+  async getTryCount() {
     const tryCountInput = await RacingGameView.getInput(
       "시도할 횟수는 몇 회인가요?\n"
     );
     tryValidation(tryCountInput);
 
-    RacingGameView.printStartMessage();
+    return tryCountInput;
+  }
 
-    const game = new RacingGameModel(carNames, Number(tryCountInput));
-
+  playRounds(game, carNames) {
     for (let i = 0; i < game.tryCount; i++) {
       const roundResult = game.raceCars();
-      roundResult.forEach((progress, index) => {
-        RacingGameView.printCarProgress(carNames[index], progress);
-      });
+      this.displayRoundProgress(roundResult, carNames);
       RacingGameView.printRoundEnd();
     }
+  }
 
+  displayRoundProgress(roundResult, carNames) {
+    roundResult.forEach((progress, index) => {
+      RacingGameView.printCarProgress(carNames[index], progress);
+    });
+  }
+
+  displayWinners(game) {
     const winners = game.getWinner();
     RacingGameView.printWinner(winners);
   }
