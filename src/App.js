aa4861814db4b082010d12888bf7d83fea7f68ad
Heesoo Jun <acesk123@gmail.com>
2024-10-28 21:13:59 +0900

    feat: 시도할 횟수 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -8,20 +8,21 @@ class App {
     this.rounds = 0;
   }
 
-  // 비동기 입력
   async run() {
     const carNames = await this.promptCarNames();
     this.setCars(carNames);
+
+    const rounds = await this.promptRounds();
+    this.setRounds(rounds);
   }
 
-  // 자동차 이름 입력 받기
-  async promptCarNames() {
+  // 시도할 횟수 입력 받기
+  async promptRounds() {
     return new Promise((resolve) => {
-      Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n').then(resolve);
+      Console.readLineAsync('시도할 횟수는 몇 회인가요?\n').then(resolve);
     });
   }
 
-  // 입력받은 자동차 이름 설정
   setCars(input) {
     const names = input.split(',');
     if (this.validateNames(names)) {
@@ -31,9 +32,12 @@ class App {
     }
   }
 
-  // 이름 유효성 검사
-  validateNames(names) {
-    return names.every((name) => name.length <= 5);
+  setRounds(input) {
+    const rounds = parseInt(input, 10);
+    if (isNaN(rounds) || rounds <= 0) {
+      throw new Error('[ERROR]');
+    }
+    this.rounds = rounds;
   }
 }
 
