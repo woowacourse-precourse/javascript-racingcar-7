344af772b1c0580b0724d011e1f74187a7487a5c
vgotu99 <vgotu99@gmail.com>
2024-10-28 11:28:24 +0900

    feat: 자동차 생성 및 이동 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
+const createCar = (name) => ({
+  name,
+  position: 0,
+  move(number) {
+    if (number >= 4) {
+      this.position += 1;
+    }
+  },
+  getProgress() {
+    return '-'.repeat(this.position);
+  }
+});
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      // 향후 구현
+    } catch (error) {
+      Console.print(`[ERROR] ${error.message}`);
+    }
+  }
 }
 
 export default App;
