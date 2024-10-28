ad1fa8d0d231c53526d07d38617be0a8ca696f48
Oliy <74090200+eunsoA@users.noreply.github.com>
2024-10-28 19:05:50 +0900

    [style] 코드 큰 틀 잡기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      // 사용자로부터 자동차 이름 입력 받기
+      
+      // 사용자로부터 시도 횟수 입력 받기
+      
+      // 자동차 데이터를 초기화 (각 자동차의 이름과 위치)
+      
+      // 게임 진행
+      
+      // 최종 우승자 발표
+      
+    } catch (error) {
+      // 오류 발생 시 오류 메시지 출력
+      Console.print(`[ERROR] ${error.message}`);
+    }
+  }
 }
 
 export default App;
