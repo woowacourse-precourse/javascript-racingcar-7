8c812df2ac94a8056419323a7d46d1be7c792256
PangKiYeon <pdrkeamy@gmail.com>
2024-10-28 19:02:17 +0900

    feat: 이동 결과 배열 초기화하는 함수 구현

--- src/racingCar.js
+++ src/racingCar.js
@@ -1,3 +1,7 @@
 export function separateCarNames(carNames) {
     return carNames.split(',').map((carName) => carName.trim());
 }
+
+export function initializeMove(carNames) {
+    return Array(carNames.length).fill(0);
+}
