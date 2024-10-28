5eee926752d541c0503a9857ab3c6be96e929065
soyeooooo <alththdus1@naver.com>
2024-10-28 18:03:07 +0900

    feat: 에러 메세지 작성

--- src/Message.js
+++ src/Message.js
@@ -8,8 +8,17 @@ export const OUTPUT_MESSAGE = {
 };
 
 export const NUMBER = {
-    startPosition: 0,
-    pickNumberMin: 0,
-    pickNumberMax: 9,
-    fowardCondition: 4,
-  };
+  startPosition: 0,
+  pickNumberMin: 0,
+  pickNumberMax: 9,
+  fowardCondition: 4,
+};
+
+export const ERROR = {
+  nameNull: '[ERROR] 이름을 입력해주세요.',
+  nameLength: '[ERROR] 이름은 5자 이하만 가능합니다.',
+  nameDuplicate: '[ERROR] 이름은 중복되지 않아야 합니다.',
+  tryCountNull: '[ERROR] 시도 횟수를 입력해주세요.',
+  tryCountNan: '[ERROR] 시도 횟수는 숫자여야 합니다.',
+  tryCountMin: '[ERROR] 시도 횟수는 0이상이어야 합니다.',
+};
