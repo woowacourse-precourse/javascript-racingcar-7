84fe555d4976a0b820795aab1d898a32c3507205
Erin <fls4804@gmail.com>
2024-10-28 20:20:04 +0900

    feat: 자동차 이름 입력값 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-class App {
-  async run() {}
-}
+import { Console, Random } from "@woowacourse/mission-utils";
 
-export default App;
+class App {
+  async run()
+
+async getUserCarNames(){
+  Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+  const input = await Console.readLineAsync("");
+  const carNames = input.split(",").map(name => name.trim());
+
+  this.checkMinCars(carNames);
+  this.checkDuplicateNames(carNames);
+  this.validCarNameLengthAndBlank(carNames);
+
+  return carNames;
+}
+}
