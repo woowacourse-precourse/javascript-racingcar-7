ab386e9f37beb632543f3f2978e76b98f3f24c66
심우석 <dntjr5741@naver.com>
2024-10-28 19:38:45 +0900

    feat : 자동차 이름 및 이동 횟수 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const carsNames = await Console.readLineAsync(
+        "경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분)\n"
+      );
+
+      const nameList = carsNames.split(",").map((car) => car.trim());
+
+      console.log(nameList);
+      const playNum = await Console.readLineAsync(
+        "시도할 횟수는 몇 회인가요?\n"
+      );
+    } catch (error) {
+      Console.readLineAsync(error.message);
+      throw error;
+    }
+  }
 }
 
 export default App;
