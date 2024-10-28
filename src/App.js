c90e54b0625ce4edc47d37076ca454f3a5b18398
youngsu <everdudtn@gmail.com>
2024-10-28 18:14:31 +0900

    feat(app): 문자열 입력 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const getInput = async () => {
+      MissionUtils.Console.print("문자열을 입력해 주세요");
+      const input = await MissionUtils.Console.readLineAsync("");
+      return input.split(",");
+    };
+
+    const input = await getInput();
+    MissionUtils.Console.print(input);
+  }
 }
 
 export default App;
