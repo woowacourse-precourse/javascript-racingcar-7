2124518cc7b72f2b88ef85222d69ff42b30a47f4
choisuyeon <stellasue@naver.com>
2024-10-28 17:17:06 +0900

    feat: Move forward if the random value is more than 4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 // Random.pickNumberInRange(0, 9);   : 0에서 9까지의 정수 중 한 개의 정수 반환
 import { Random, Console } from "@woowacourse/mission-utils";
 
+function forward(cnt) {
+  if (Random.pickNumberInRange(0, 9) >= 4)
+    cnt = (cnt || 0) + 1;       // cnt가 false(undefined, null, 0)일 경우 0으로 초기화 후 1을 더함, true일 경우 cnt를 그대로 사용 후 1을 더함
+  return cnt;
+}
+
 class App {
   async run() {
     try {
@@ -13,9 +19,11 @@ class App {
 
       if (cars.some(car => car.length > 5))
         throw new Error("[ERROR]");
-
+      
+      let cnt = [];
       while (tryNum) {  // 입력 받은 값만큼 시도
         for (let i = 0; i < cars.length; i++) 
+          cnt[i] = forward(cnt[i]);
         
         tryNum--;
       }
