28b967ef97f9043d1288e3be0410bef0adf7ac1a
Jiny <moc24@naver.com>
2024-10-28 20:16:32 +0900

    refactor : 전체 코드 포맷팅 (prettierrc)

--- src/function/carRacingResultFunctions.js
+++ src/function/carRacingResultFunctions.js
@@ -1,30 +1,26 @@
-import {  Console, MissionUtils   } from "@woowacourse/mission-utils";
+import { Console, MissionUtils } from '@woowacourse/mission-utils';
 
 // ! 결과 함수 ===//
 export const getMaxDistance = (carArray) => {
-    let carDistanceArray =[];
+    let carDistanceArray = [];
     carArray.map((car) => {
         carDistanceArray.push(car.distance);
-    })
-    
+    });
+
     const maxDistance = Math.max(...carDistanceArray);
     return maxDistance;
-
-}
+};
 
 export const getWinnerCarNameArrayWithMaxValue = (carArray, maxDistance) => {
-    const winnerCars= carArray.filter((car) => 
-        car.distance === maxDistance
-    ); 
+    const winnerCars = carArray.filter((car) => car.distance === maxDistance);
 
     const winnerCarNameArray = winnerCars.map((car) => car.name);
 
     return winnerCarNameArray;
-}
+};
 
 export const printWinnerCars = (winnerCarNameArray) => {
-    const CarNames = winnerCarNameArray.join(', '); 
+    const CarNames = winnerCarNameArray.join(', ');
 
     Console.print(`최종 우승자 : ${CarNames}`);
-    
-}
+};
