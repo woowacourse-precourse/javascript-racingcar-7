0a965554536858639220f8adcf208edc5a93bb65
keemsebeen <sebin0580@gmail.com>
2024-10-28 13:40:08 +0900

    feat : 시도할 횟수 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import { EXECUTION_RESULT, FINAL_WINNER, INPUT } from "./constants/race.js";
-import { simulateRace } from "./race/race.js";
+import { INPUT } from "./constants/race.js";
 import { parseCarNames } from "./car/parseCarNames.js";
-import { attemptValidator } from "./validation/attemptValidator.js";
 import { getInput } from "./view/input.js";
-import { getOutput, getResult, getWinner } from "./view/output.js";
 
 class App {
   async run() {
     const carNamesInput = await getInput(INPUT.RACE_CAR_NAME);
     const carNames = parseCarNames(carNamesInput);
+    const attemptCountInput = await getInput(INPUT.ATTEMPT_COUNT_PROMPT);
   }
 }
 
