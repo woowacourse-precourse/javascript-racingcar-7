49f43833a139d071216d1e8237f1c0163599b937
Lim-shweety <smile70200@naver.com>
2024-10-28 20:41:03 +0900

    feat(App.js) : 경주할 자동차 입력 받고 저장
    경주할 자동차 입력 받고 ,를 기준으로 쪼개서 배열에 저장

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Console } from "@woowacourse/mission-utils"
+
 class App {
-  async run() {}
+  async run() {
+    let inputNames = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+    const NAMES=inputNames.split(',');
+
+  }
 }
 
 export default App;
