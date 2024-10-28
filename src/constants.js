b9297085d4072f1e5d573a1a78cfc94d4680ad20
mywhzy <cmj2393a@naver.com>
2024-10-28 20:46:40 +0900

    feat: 에러 메세지, 자동차 이름 글자 수 정규식 상수 추가 feat: 이름 구분자 별도 상수로 분리

--- src/constants.js
+++ src/constants.js
@@ -3,11 +3,22 @@ export const INPUT_PROMPT = {
   readRoundCount: '시도할 횟수는 몇 회인가요?',
 };
 
+export const REGEX = {
+  carName: /^.{1,5}$/g,
+};
+
+export const BEFORE_ERROR_MESSAGE = '[ERROR]';
+
+export const ERROR_MESSAGE = {
+  carNameCharacterCountError: `${BEFORE_ERROR_MESSAGE} 경주에 참여 가능한 자동차 이름은 최소 1자 이상, 최대 5자 이하입니다.`,
+};
+
 export const FORWARD_SYMBOL = '-';
+
+export const NAME_SEPARATOR = ',';
 
 export const OUTPUT_MESSAGE = {
   messageSeparator: ':',
-  winnerSeparator: ',',
   executionResult: '실행 결과',
   winner: '최종 우승자',
 };
