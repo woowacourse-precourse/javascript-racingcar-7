05d987f181a54cfc9f5502a44843d821ec3eb58e
zzzzzuuuuu <agu0830@naver.com>
2024-10-28 15:06:26 +0900

    fix: 쉼표를 연달아서 입력했을 경우 공백 문자가 이름으로 취급되는 현상 수정

--- src/CarsInput.js
+++ src/CarsInput.js
@@ -4,7 +4,7 @@ export class CarsInput {
   async getInput() {
     Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분")
     const input = await Console.readLineAsync("");
-    let cars = input.split(",").map(car => car.trim());
+    let cars = input.split(",").map(car => car.trim()).filter(car => car !== "");
 
     cars = [...new Set(cars)];
     this.validateCarNames(cars);
