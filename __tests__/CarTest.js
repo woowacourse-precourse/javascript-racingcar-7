ae4675d5ad96db9d65a7c774f009f1f9b803a00f
mywhzy <cmj2393a@naver.com>
2024-10-28 16:57:06 +0900

    test: 입력된 자동차 이름으로 Car 인스턴스 여러 개 생성 테스트 구현

--- __tests__/CarTest.js
+++ __tests__/CarTest.js
@@ -8,4 +8,16 @@ describe('Car 클래스 테스트', () => {
     expect(car).toBeInstanceOf(Car);
     expect(car.name).toBe(expectedCarName);
   });
+
+  test('입력된 자동차 이름을 가진 자동차 인스턴스 여러 개를 생성할 수 있다.', () => {
+    const input = 'minji,minhyuk';
+    const expectedCarNames = ['minji', 'minhyuk'];
+
+    const cars = input.split(',').map((carName) => new Car(carName));
+    const carNames = cars.map((car) => car.name);
+
+    expect(cars[0]).toBeInstanceOf(Car);
+    expect(cars[1]).toBeInstanceOf(Car);
+    expect(carNames).toEqual(expectedCarNames);
+  });
 });
