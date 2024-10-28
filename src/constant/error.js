45f7f1d6cb543ac8d2f5418e127c53dfedaab8f9
kimgho <kgh67811@naver.com>
2024-10-28 20:56:21 +0900

    feat(validation): 에러 케이스 추가
    - 자동차 이름이 중복되는 경우 에러 처리
    - error.js 관련 메시지 추가

--- src/constant/error.js
+++ src/constant/error.js
@@ -8,6 +8,7 @@ export const ERROR_MESSAGES = Object.freeze(
   {
     EMPTY_CAR_NAME: "[ERROR] : 자동차 이름은 공백일 수 없습니다. ",
     LENGTH_OVER_CAR_NAME: "[ERROR] : 자동차 이름은 5자 이하만 가능합니다.",
+    DUPLICATE_CAR_NAME: "[ERROR] : 자동차 이름은 중복될 수 없습니다.",
     EMPTY_ATTEMPTS: "[ERROR] : 시도 횟수는 공백일 수 없습니다.",
     NEGATIVE_ATTEMPTS: "[ERROR] : 시도 횟수는 양수만 가능합니다.",
     NAN_ATTEMPTS: "[ERROR] : 시도 횟수는 숫자만 가능합니다.",
--- src/validate/InputValidation.js
+++ src/validate/InputValidation.js
@@ -1,4 +1,4 @@
-import {ERROR_MESSAGES} from "../constant/error.js"
+import { ERROR_MESSAGES } from "../constant/error.js"
 
 /**
  * 사용자 입력값의 유효성을 검사하는 클래스
@@ -7,16 +7,18 @@ import {ERROR_MESSAGES} from "../constant/error.js"
 class InputValidation {
   /**
    * 자동차 이름들의 유효성을 검사
-   * 이름은 5자 이하여야 하며, 공백일 수 없음
+   * 이름은 5자 이하여야 하며, 공백일 수 없음, 중복을 허용하지 않음
    * @param {string[]} names - 검사할 자동차 이름들의 배열
    * @returns {Promise<void>}
    * @throws {Error} 이름이 5자를 초과하는 경우
    * @throws {Error} 이름이 공백인 경우가 있는 경우
+   * @throws {Error} 이름이 중복인 경구
    * @async
    * @example
    * await InputValidation.validationName(["pobi", "woni"]) // valid
    * await InputValidation.validationName(["pobiiii"]) // throws Error
    * await InputValidation.validationName(["", "woni"]) // throws Error
+   * await InputValidation.validationName(["pobi","pobi","woni"]) // throws Error
    */
   static async validationName(names) {
     if (names.some(name => name.length > 5)) {
@@ -24,6 +26,9 @@ class InputValidation {
     }
     if (names.some(name => name === "")) {
       throw new Error(ERROR_MESSAGES.EMPTY_CAR_NAME);
+    }
+    if (names.length!==new Set(names).size){
+      throw new Error(ERROR_MESSAGES.DUPLICATE_CAR_NAME)
     }
   }
 
@@ -34,11 +39,15 @@ class InputValidation {
    * @returns {Promise<void>}
    * @throws {Error} 시도 횟수가 숫자가 아닌 경우
    * @throws {Error} 시도 횟수가 음수인 경우
+   * @throws {Error} 시도 횟수가 0인 경우
+   * @throws {Error} 시도 횟수가 공백인 경우
    * @async
    * @example
    * await InputValidation.validationAttempts(3) // valid
    * await InputValidation.validationAttempts("abc") // throws Error
    * await InputValidation.validationAttempts(-1) // throws Error
+   * await InputValidation.validationAttempts(0) // throws Error
+   * await InputValidation.validationAttempts("") // throws Error
    */
   static async validationAttempts(attempts) {
     if (isNaN(attempts)) {
@@ -47,7 +56,7 @@ class InputValidation {
     if (attempts < 0) {
       throw new Error(ERROR_MESSAGES.NEGATIVE_ATTEMPTS);
     }
-    if(attempts === 0){
+    if (attempts === 0) {
       throw new Error(ERROR_MESSAGES.ZERO_ATTEMPTS)
     }
     if (attempts === "") {
