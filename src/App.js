dbe39bc0cbc55b6ee110499c2d668c8a17d88bb9
COYO <dayeon.yoon@gmail.com>
2024-10-28 18:09:19 +0900

    feat: 순차별 경기 진행

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { Console } from "@woowacourse/mission-utils";
-import { Random } from "@woowacourse/mission-utils/src";
+import {Console, Random} from "@woowacourse/mission-utils";
 
 class App {
   async run() {
     const carNames = await this.getCarNames();
     const times = await this.getTimes();
     const scores = this.setScores(carNames);
+    await this.playGame(times, carNames, scores)
   }
 
   async getCarNames() {
@@ -57,6 +57,33 @@ class App {
     let scores = {};
     carNames.forEach((car) => (scores[car] = 0));
     return scores;
+  }
+
+  isMovingForward() {
+    const n = Random.pickNumberInRange(0, 9);
+    return n >= 4
+  }
+
+  playRound(carNames, scores) {
+    carNames.forEach((carName) => {
+      if (this.isMovingForward()) {
+        scores[carName]++;
+      }
+    })
+
+    this.showRoundResult(carNames, scores)
+  }
+
+  showRoundResult(carNames, scores) {
+    carNames.forEach((carName) => Console.print(`${carName} : ${"-".repeat(scores[carName])}`));
+    Console.print(" ");
+  }
+
+  playGame(times, carNames, scores) {
+    Console.print("\n실행결과");
+    for (let i = 0; i < times; i++) {
+      this.playRound(carNames, scores);
+    }
   }
 }
 
