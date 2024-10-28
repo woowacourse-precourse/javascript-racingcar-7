eca68cf35c1e46b3921d7a87c7134daff7287b9e
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2024-10-28 17:41:05 +0900

    feat: 현재 게임 진행상황 출력기능 구현

--- src/Game.js
+++ src/Game.js
@@ -12,6 +12,21 @@ class Game {
       this.cars.push(car);
     });
   }
+
+  play(availableAttempts) {
+    Console.print("\n실행결과");
+    for (let i = 0; i < availableAttempts; i++) {
+      this.cars.forEach((car) => car.move());
+      this.showRacingStatus();
+    }
+  }
+
+  showRacingStatus() {
+    this.cars.forEach((car) => {
+      Console.print(`${car.getName()} : ${"-".repeat(car.getDistance())}`);
+    });
+    Console.print("");
+  }
 }
 
 export default Game;
