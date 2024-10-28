b55e3658e28ea38d94a3aa8365dd453b5bd5ffaa
Dobbymin <gmin990624@gmail.com>
2024-10-28 16:14:09 +0900

    fix: 비동기 표현식 제거 및 출력문에 공백 추가

--- src/view/functions/progressRacingCar.js
+++ src/view/functions/progressRacingCar.js
@@ -2,9 +2,9 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 import { OTHERS } from '../../constants/constants.js';
 
-export const progressRacingCar = async (cars) => {
+export const progressRacingCar = (cars) => {
   cars.forEach((car) => {
-    const carInfo = `${car.name}: ${OTHERS.PROGRESS_BAR.repeat(car.go)}`;
+    const carInfo = `${car.name} : ${OTHERS.PROGRESS_BAR.repeat(car.go)}`;
 
     MissionUtils.Console.print(carInfo);
   });
