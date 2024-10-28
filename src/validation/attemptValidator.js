7d7ad91ff0ee4d13a5f69acc1508cf5d6f1e5a67
keemsebeen <sebin0580@gmail.com>
2024-10-28 14:12:26 +0900

    feat : attemptValidator 함수에 count가 존재하지 않을 경우 조건 추가

--- src/validation/attemptValidator.js
+++ src/validation/attemptValidator.js
@@ -1,3 +1,10 @@
+import { ATTEMPT, ERROR_MESSAGE } from "../constants/error.js";
+import { isNumber } from "./validators.js";
+
 export const attemptValidator = (count) => {
+  if (count == "") {
+    throw new Error(ERROR_MESSAGE + ATTEMPT.EMPTY_TRY);
+  }
+
   return Number(count);
 };
