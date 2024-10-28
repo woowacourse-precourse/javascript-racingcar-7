5e6677ea06749a3bb5e1489a79d749a317438ea3
Seoyeon Kim <kseoy0607@gmail.com>
2024-10-28 14:48:00 +0900

    feat: 자동차 이름이 한글 또는 영어인지 검증하는 메서드 추가

--- src/Validator.js
+++ src/Validator.js
@@ -6,6 +6,17 @@ class Validator {
       throw new Error('[ERROR] 이름은 5자 이하만 가능합니다.');
     }
   }
+
+  #checkName(carName) {
+    const validPattern = /^[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣]+$/;
+    const isValid = validPattern.test(carName);
+
+    if (carName === '') return;
+
+    if (!isValid) {
+      throw new Error('[ERROR] 이름은 한글 또는 영어만 가능합니다.');
+    }
+  }
 }
 
 export default Validator;
