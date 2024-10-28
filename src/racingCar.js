3be4353c97bdde779a945fe6a5191f036b046619
PangKiYeon <pdrkeamy@gmail.com>
2024-10-28 19:06:35 +0900

    feat: 자동차의 전진 여부 결정하는 기능 구현

--- src/racingCar.js
+++ src/racingCar.js
@@ -1,7 +1,18 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { Console } from '@woowacourse/mission-utils';
+
 export function separateCarNames(carNames) {
     return carNames.split(',').map((carName) => carName.trim());
 }
 
 export function initializeMove(carNames) {
     return Array(carNames.length).fill(0);
+}
+
+function oneCarMove(moveResult) {
+    const CHALLENGE = MissionUtils.Random.pickNumberInRange(0, 9);
+    if (CHALLENGE >= 4) {
+        return moveResult + 1;
+    }
+    return moveResult;
 }
