9538286c4f5864c7914487087ca4bd2875605439
mywhzy <cmj2393a@naver.com>
2024-10-28 18:00:20 +0900

    feat: 랜덤 값이 4이상인 경우 자동차 전진 기능 구현

--- src/Car.js
+++ src/Car.js
@@ -2,17 +2,29 @@ import { Random } from '@woowacourse/mission-utils';
 
 class Car {
   #name;
+  #forwardCount;
 
   constructor(name) {
     this.#name = name;
+    this.#forwardCount = 0;
   }
 
   getRandomNumber() {
     return Random.pickNumberInRange(0, 9);
   }
 
+  moveForward(randomNumber) {
+    if (randomNumber < 4) return;
+
+    this.#forwardCount += 1;
+  }
+
   get name() {
     return this.#name;
+  }
+
+  get forwardCount() {
+    return this.#forwardCount;
   }
 }
 
