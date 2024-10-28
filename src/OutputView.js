e0bc02979c5011cd88fab88aab3e7530a4b33bce
mywhzy <cmj2393a@naver.com>
2024-10-28 21:41:45 +0900

    feat: 결과 출력에 필요한 출력 메소드 추가 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -2,6 +2,12 @@ import { Console } from '@woowacourse/mission-utils';
 import { FORWARD_SYMBOL, NAME_SEPARATOR, OUTPUT_MESSAGE } from '../src/constants.js';
 
 const OutputView = {
+  printExecutionResultMessage() {
+    Console.print(OUTPUT_MESSAGE.executionResult);
+  },
+  printEmptyLine() {
+    Console.print('');
+  },
   printCurrentCarInfo(name, forwardCount) {
     const forwardResult = FORWARD_SYMBOL.repeat(forwardCount);
 
