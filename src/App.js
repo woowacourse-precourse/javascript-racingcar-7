0b6ee9f1c560912ea453b00b6374abfc91aa31ab
yuri295 <jin_astro0302@naver.com>
2024-10-28 10:21:56 +0900

    feat: 게임 진행 로직 구현

--- src/App.js
+++ src/App.js
@@ -13,58 +13,64 @@ class App {
     const nameList = this.splitNameInput(nameInput);
     this.validateNameList(nameList);
     const number = this.validateNumber(numberInput);
-    const randomNumbers = this.generateRandomNumbers(nameList, number);
-    console.log(randomNumbers);
-  }
-  catch(error) {
-    throw new Error(`[ERROR] ${error.message}`);
+    this.generateGameProgress(nameList, number);
   }
 
   splitNameInput(nameInput) {
-    const carNames = nameInput.split(",");
-
-    return carNames;
+    return nameInput.split(",");
   }
 
   validateNameList(nameList) {
-    if (nameList.length === 1) {
+    if (nameList.length < 2) {
       throw new Error("자동차 이름은 쉼표로 구분하여 2개 이상 입력해 주세요.");
     }
 
-    const emptyName = nameList.find((name) => name === "");
-    if (emptyName !== undefined) {
+    const hasEmptyName = nameList.some((name) => name.trim() === "");
+    if (hasEmptyName) {
       throw new Error("자동차 이름 사이에 공백이 포함되어 있습니다.");
     }
 
-    const longName = nameList.find((name) => name.length > 5);
-    if (longName !== undefined) {
-      throw new error(`"${longName}"은(는) 5자를 초과합니다.`);
+    const tooLongName = nameList.find((name) => name.length > 5);
+    if (tooLongName) {
+      throw new Error(`"${tooLongName}"은(는) 5자를 초과합니다.`);
     }
   }
 
   validateNumber(numberInput) {
-    if (!numberInput.trim() || isNaN(Number(numberInput))) {
-      throw new Error("숫자를 입력해 주세요.");
-    }
-
     const number = Number(numberInput);
-
-    if (number <= 0) {
+    if (!number || number <= 0) {
       throw new Error("시도할 횟수는 1 이상의 양수여야 합니다.");
     }
-
     return number;
   }
 
-  generateRandomNumbers(nameList, number) {
-    const randomNumbers = [];
+  generateGameProgress(nameList, targetNumber) {
+    const results = Array.from({ length: nameList.length }, () => "");
+    let isGameFinished = false;
 
-    for (let i = 0; i < nameList.length * number; i++) {
-      const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
-      randomNumbers.push(randomNumber);
+    while (!isGameFinished) {
+      this.runOneRound(nameList, results);
+
+      this.printRoundResults(nameList, results);
+
+      isGameFinished = results.some((result) => result.length >= targetNumber);
     }
+  }
 
-    return randomNumbers;
+  runOneRound(nameList, results) {
+    nameList.forEach((_, index) => {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+      if (randomNumber >= 4) {
+        results[index] += "-";
+      }
+    });
+  }
+
+  printRoundResults(nameList, results) {
+    nameList.forEach((name, index) => {
+      MissionUtils.Console.print(`${name} : ${results[index]}`);
+    });
+    MissionUtils.Console.print("\n");
   }
 }
 
