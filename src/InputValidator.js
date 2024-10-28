388a12ee5abdd047187d7479962f3023c540fef0
Yena Kim <120614713+ynkim-03@users.noreply.github.com>
2024-10-28 19:11:44 +0900

    fix: 입력값이 비어있거나 경주 시도 횟수가 0 이하인 경우 에러를 발생시키는 validateEmpty 메서드 추가

--- src/InputValidator.js
+++ src/InputValidator.js
@@ -1,24 +1,32 @@
 export default class InputValidator {
-  constructor(input) {
-    this.input = input;
-    this.inputList = [];
+  constructor(carNames, raceCount) {
+    this.carNames = carNames;
+    this.raceCount = raceCount;
+    this.carNameList = [];
     this.validateInput();
   }
 
   validateInput() {
+    this.validateEmpty();
     this.validateSpace();
-    this.inputList = this.input.split(',');
+    this.carNameList = this.carNames.split(',');
     this.validateLength();
   }
 
+  validateEmpty() {
+    if (this.carNames === '' || this.raceCount < 1) {
+      throw new Error('[ERROR] 잘못된 입력입니다.');
+    }
+  }
+
   validateSpace() {
-    if (this.input.split(' ')[0] !== this.input) {
+    if (this.carNames.split(' ')[0] !== this.carNames) {
       throw new Error('[ERROR] 잘못된 입력입니다.');
     }
   }
 
   validateLength() {
-    this.inputList.forEach((item) => {
+    this.carNameList.forEach((item) => {
       if (item.length > 5) {
         throw new Error('[ERROR] 잘못된 입력입니다.');
       }
