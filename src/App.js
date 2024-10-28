95f6fa9feb17d43914085e8860be7c66752ebf64
syg0629 <syg0629@hanmail.net>
2024-10-28 23:55:11 +0900

    refactor: raceRounds 변수를 숫자형으로 변환

--- src/App.js
+++ src/App.js
@@ -38,20 +38,23 @@ class App {
       const raceRounds = await MissionUtils.Console.readLineAsync(
         '시도할 횟수는 몇 회인가요?\n'
       );
-      if (!raceRounds || raceRounds < 0) {
+
+      const roundsNumber = Number(raceRounds);
+
+      if (!raceRounds || roundsNumber < 1) {
         throw new Error(App.ERROR_MESSAGES.INVALID_ROUNDS_EMPTY);
       }
-      if (isNaN(raceRounds)) {
+      if (isNaN(roundsNumber)) {
         throw new Error(App.ERROR_MESSAGES.INVALID_ROUNDS_NAN);
       }
-      if (!Number.isInteger(raceRounds)) {
+      if (!Number.isInteger(roundsNumber)) {
         throw new Error(App.ERROR_MESSAGES.INVALID_ROUNDS_DECIMAL);
       }
 
       await MissionUtils.Console.print('\n실행 결과');
 
-      for (let i = 0; i < raceRounds; i++) {
-        await MissionUtils.Console.print('\n');
+      for (let i = 0; i < roundsNumber; i++) {
+        await MissionUtils.Console.print('');
         for (const [carName, distance] of map) {
           const pickNumber = MissionUtils.Random.pickNumberInRange(0, 9);
           if (pickNumber >= 4) {
