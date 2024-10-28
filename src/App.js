55e4822c8cef93ffc15a19bf537fb37c172c1aa6
gumbi <gusn9719@gmail.com>
2024-10-28 21:01:12 +0900

    refactor: App.js 모듈화 및 가독성 향상
    - App.js에서 각 기능을 별도의 모듈로 분리하여 불러오는 방식으로 리팩토링
    - 코드의 가독성을 높이고, App.js를 단순히 기능의 흐름을 보여주는 형태로 변환
    - 주요 기능: 자동차 이름 입력, 유효성 검사, 자동차 객체 생성, 이동 및 결과 출력

--- src/App.js
+++ src/App.js
@@ -1,112 +1,49 @@
-import { Console, Random } from '@woowacourse/mission-utils';
-
-class Car {
-  constructor(name) {
-    this.name = name;
-    this.position = 0;
-  }
-
-  move() {
-    this.position += 1;
-  }
-}
+import { userInputCarNames, userInputTryCount } from './utils/getInputUtils.js';
+import { splitAndRemoveWhitespace } from './utils/carNameUtils.js';
+import {
+  printRoundStatus,
+  printToStartGame,
+  printWinners,
+} from './utils/printUtils.js';
+import { validateCarNames, validateTryCount } from './validate/validateion.js';
+import { createCarObjectsFromNames } from './createCarObjects.js';
+import { moveCars } from './gameLogic.js';
+import {
+  getMaxPosition,
+  filterCarsByMaxPosition,
+  extractCarNames,
+} from './utils/raceResultUtils.js';
 
 class App {
   async run() {
     try {
-      // 자동차 이름 입력
-      const inputCarList = await Console.readLineAsync(
-        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
-      );
+      const inputCarNames = await userInputCarNames();
+      const processedCarNames = splitAndRemoveWhitespace(inputCarNames);
 
-      // 자동차 이름 분리, 이름 공백 제거
-      const carNames = inputCarList.split(',').map((name) => name.trim());
+      validateCarNames(processedCarNames);
 
-      this.validateCarNames(carNames);
+      const cars = createCarObjectsFromNames(processedCarNames);
 
-      // 자동차 객체 생성
-      const cars = carNames.map((name) => new Car(name));
+      const inputTryCount = await userInputTryCount();
 
-      // 시도 횟수 입력
-      const inputTryCount = await Console.readLineAsync(
-        '시도할 횟수는 몇 회인가요?\n'
-      );
-
-      // 시도 횟수 숫자로 변환, 유효성 검사
       const tryCount = Number(inputTryCount);
-      this.validateTryCount(tryCount);
+      validateTryCount(tryCount);
 
-      // 경주 시작
-      Console.print('\n실행 결과');
-      //자동차 전진 기능
-      this.moveCars(cars, tryCount);
+      printToStartGame();
+      for (let i = 0; i < tryCount; i++) {
+        moveCars(cars);
+        printRoundStatus(cars);
+      }
 
-      // 우승자 출력 기능
-      this.printWinners(cars);
+      const maxPosition = getMaxPosition(cars);
+
+      const result = filterCarsByMaxPosition(cars, maxPosition);
+
+      const winners = extractCarNames(result);
+      printWinners(winners);
     } catch (error) {
       throw error;
     }
-  }
-
-  // 클래스 메서드로 유효성 검사 정의
-  validateCarNames(carNames) {
-    if (carNames.length < 2) {
-      throw new Error('[ERROR] 자동차는 2개 이상 입력해야 합니다.');
-    }
-    if (carNames.some((name) => name.length > 5)) {
-      throw new Error(
-        '[ERROR] 자동차 이름은 1글자 이상 5글자 이하로 입력해야 합니다.'
-      );
-    }
-    if (new Set(carNames).size !== carNames.length) {
-      throw new Error('[ERROR] 중복되는 자동차 이름은 입력할 수 없습니다.');
-    }
-  }
-
-  validateTryCount(tryCount) {
-    if (isNaN(tryCount) || tryCount <= 0 || !Number.isInteger(tryCount)) {
-      throw new Error('[ERROR] 시도할 횟수는 양의 정수여야 합니다.');
-    }
-  }
-  moveCars(cars, tryCount) {
-    for (let i = 0; i < tryCount; i++) {
-      this.moveEachcar(cars);
-      this.printRoundStatus(cars);
-    }
-  }
-
-  moveEachcar(cars) {
-    for (const car of cars) {
-      if (this.shouldMove()) {
-        car.move();
-      }
-    }
-  }
-
-  // 랜덤 숫자가 4 이상이면 전진
-  shouldMove() {
-    const randomNumber = Random.pickNumberInRange(0, 9);
-    return randomNumber >= 4;
-  }
-
-  printRoundStatus(cars) {
-    for (const car of cars) {
-      Console.print(`${car.name} : ${'-'.repeat(car.position)}`);
-    }
-    Console.print('');
-  }
-
-  printWinners(cars) {
-    // 최대 이동거리 찾기
-    const maxPosition = Math.max(...cars.map((car) => car.position));
-
-    // 승자 선정
-    const winners = cars
-      .filter((car) => car.position === maxPosition)
-      .map((car) => car.name);
-
-    // 결과 출력
-    Console.print(`최종 우승자 : ${winners.join(', ')}`);
   }
 }
 
