6fd35e0546ab05c927f3ff9531f858209c0be1d6
seongmin <seongm22n@gmail.com>
2024-10-28 17:06:28 +0900

    refactor : 결과 출력 로직을 view폴더로 이동
    - 전반적인 코드 수정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import { Console, Random } from '@woowacourse/mission-utils';
 import { carNameInputHandler, tryNumberHandler } from './utils/userInput.js';
-import { initializePositions } from './utils/initializePositions.js';
-import { calculateWinners } from './utils/calculateWinners.js';
+import { initializePositions } from './services/initializePositions.js';
+import { calculateWinners } from './services/calculateWinners.js';
 import { startRacingGame } from './services/startRacingGame.js';
+import { displayWinners } from './views/displayWinners.js';
 
 class App {
 	async run() {
 		const carNameList = await carNameInputHandler();
 		const tryNumber = await tryNumberHandler();
 
-		// 각 자동차 위치 초기화
 		const positions = initializePositions(carNameList.length);
 
-		// 게임 시작
 		startRacingGame(carNameList, tryNumber, positions);
 
-		// 경주 결과 출력
 		const winners = calculateWinners(carNameList, positions);
-		Console.print(`최종 우승자 : ${winners.join(', ')}`);
+		displayWinners(winners);
 	}
 }
 
