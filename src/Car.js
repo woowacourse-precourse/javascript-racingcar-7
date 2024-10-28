33152449f9a5f94fa2b500b7e566478d9eb8e6f0
0Jaemin0 <jaemin2167@naver.com>
2024-10-28 16:08:29 +0900

    refactor: Car 클래스의 내부 접근 방지를 통한 안정성 강화

--- src/Car.js
+++ src/Car.js
@@ -1,27 +1,30 @@
 import getRandomNumber from './utils/getRandomNumber.js';
 
 class Car {
+  #name;
+  #position;
+
   constructor(name) {
-    this.name = name;
-    this.position = 0;
+    this.#name = name;
+    this.#position = 0;
   }
 
-  isMove() {
+  #isMove() {
     return getRandomNumber() >= 4;
   }
 
   moveForward() {
-    if (this.isMove()) {
-      this.position += 1;
+    if (this.#isMove()) {
+      this.#position += 1;
     }
   }
 
   getPosition() {
-    return this.position;
+    return this.#position;
   }
 
   getName() {
-    return this.name;
+    return this.#name;
   }
 }
 
