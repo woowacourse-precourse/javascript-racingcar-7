ef4d961d93186b789d3d3aff025470873c751f28
김나연 <apinksky00@gmail.com>
2024-10-28 15:01:45 +0900

    feat(input) : 자동차 이름, 실행횟수  차례대로 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const carsNames = await Console.readLineAsync(
+        "경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분)\n"
+      );
+      const playsNumber = await Console.readLineAsync("시도할 횟수\n");
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
 }
 
 export default App;
