1d0e94307199b365ed4413fd6958daaaa444700c
YSW2 <jrgyber@gmail.com>
2024-10-28 17:13:34 +0900

    feat(run): 입력 받는 기능 구현
    - 이름 및 시도 횟수 입력기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  parse_input(input) {
+    const result = input.split(',').map((str) => str.trim());
+    return result;
+  }
+
+  async run() {
+    const input_names = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
+    );
+    const input_number = await Console.readLineAsync(
+      '시도할 횟수는 몇 회인가요?'
+    );
+    const name_list = this.parse_input(input_names);
+    Console.print(name_list);
+  }
 }
 
 export default App;
