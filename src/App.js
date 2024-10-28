736ef3cc5900b0e4ac592cc8cfe1f1d463164bec
geunhwa37 <jacika37@naver.com>
2024-10-28 20:28:33 +0900

    feat: 자동차의 이동 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const { Random, Console } = MissionUtils;
+
+// 자동차 클래스
+class Car{
+  constructor(name){
+    this.name = name;
+    this.position = 0;
+  }
+  //자동차의 이동 메서드 구현
+  move(){
+    const randomNum = Random.pickNumberInRange(0, 9);
+      if (randomNum >= 4) {
+        this.position += 1;
+      }
+  }
+}
+
+
 class App {
+
+
   async run() {}
 }
+
 
 export default App;
