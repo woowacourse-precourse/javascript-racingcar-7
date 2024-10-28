f7c585e353630f17c3ceb22f002ab4540c427ce5
o2jsg <oji5656@naver.com>
2024-10-28 10:29:15 +0900

    feat: 경주할 자동차 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    async function getCarNames() {
+      try {
+        const CARS_NAME = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n");
+        MissionUtils.Console.print(`${CARS_NAME}`);
+        const CARS_NAME_ARR = CARS_NAME.split(",");
+        return CARS_NAME_ARR
+      } catch (error) {
+        console.error('[ERROR]');
+      }
+    }
+    getCarNames()
+  }
 }
 
 export default App;
