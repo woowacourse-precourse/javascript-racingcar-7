489fa6b60422ee5461fdfae6110f744544c8156e
mywhzy <cmj2393a@naver.com>
2024-10-28 19:14:17 +0900

    test: 자동차 경주 게임 여러 우승자 구하기 테스트 구현

--- __tests__/GameTest.js
+++ __tests__/GameTest.js
@@ -20,4 +20,26 @@ describe('Game 클래스 테스트', () => {
 
     expect(winners).toEqual(expectedWinners);
   });
+
+  test('전진 횟수가 가장 많은 여러 자동차(우승자)를 구할 수 있다.', () => {
+    const carNames = ['zzi', 'hyuk', 'kki'];
+    const roundsRandomNumbers = [
+      [4, 6, 3],
+      [3, 4, 6],
+      [5, 7, 2],
+      [8, 3, 9],
+    ];
+    const expectedWinners = ['zzi', 'hyuk'];
+
+    const game = new Game();
+    game.cars = carNames.map((carName) => new Car(carName));
+
+    roundsRandomNumbers.map((roundRandomNumbers) => {
+      roundRandomNumbers.map((randomNumber, index) => game.cars[index].moveForward(randomNumber));
+    });
+
+    const winners = game.getWinners();
+
+    expect(winners).toEqual(expectedWinners);
+  });
 });
