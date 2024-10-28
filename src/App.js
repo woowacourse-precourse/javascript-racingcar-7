62f9830da0e1e2dd9f71f5ac74de250c07278ca4
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-10-28 14:57:09 +0900

    feat(IOProcessor): 입력 처리 구현
    - IOProcessor 클래스 InputProcessor 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import IOProcessor from "./InputProcessor.js";
+
 class App {
-  async run() {}
+  constructor() {
+    this.ioProcessor = new IOProcessor();
+  }
+
+  async run() {
+    const {cars, count} = await this.ioProcessor.processInput();
+    console.log(cars, count);
+  }
 }
 
 export default App;
---
+++ src/InputProcessor.js
@@ -0,0 +1,19 @@
+import { Console } from '@woowacourse/mission-utils';
+import { INPUT_MESSAGE } from './constants.js';
+
+/**
+ * 사용자로부터 입출력을 처리하는 클래스
+ */
+class IOProcessor {
+  /**
+   *
+   */
+  async processInput() {
+    const { INPUT_CARS, INPUT_COUNT } = INPUT_MESSAGE;
+    const cars = await Console.readLineAsync(INPUT_CARS);
+    const count = await Console.readLineAsync(INPUT_COUNT);
+    return { cars, count };
+  }
+}
+
+export default IOProcessor;
