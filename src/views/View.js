e80bebb4430e61fb638faebe5e930a7ebee2e9bc
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-10-28 18:07:38 +0900

    refector: View파일 함수명 변경 및 주석 제거

--- src/views/View.js
+++ src/views/View.js
@@ -3,19 +3,20 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 export function getUserCarName() {
   return MissionUtils.Console.readLineAsync(
     '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
-  ); // promist 반환
+  );
 }
-export function getUserRacingNum() {
+
+export function getUserRaceAttempts() {
   return MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n'); // promist 반환
 }
-export function printRacingState(carData) {
+
+export function displayRaceState(carData) {
   carData.forEach((line) => {
     MissionUtils.Console.print(line);
   });
-  // 한 턴이 끝난 후 빈 줄을 출력하여 구분
   MissionUtils.Console.print('');
 }
 
-export function printRacingWinner(winnerName) {
+export function displayWinners(winnerName) {
   MissionUtils.Console.print(`최종 우승자 : ${winnerName.join(', ')}`);
 }
