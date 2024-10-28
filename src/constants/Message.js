3f79a4736f2b49dff0fc1f822a02c6292c0bd9d5
MinGu Jeong <mingu830@gmail.com>
2024-10-28 09:05:18 +0900

    feat: 일반 메시지 작성

---
+++ src/constants/Message.js
@@ -0,0 +1,8 @@
+export const INPUT_MESSAGE = {
+  carName: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  tryCount: '시도할 횟수는 몇 회인가요?\n',
+};
+
+export const OUTPUT_MESSAGE = {
+  winner: '최종 우승자 : ',
+};
