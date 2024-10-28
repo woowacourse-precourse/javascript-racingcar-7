d0aa4b19cbd80c1a28ed85d13cd7e8bd99d76bc5
hyunyeee <5004ohy@naver.com>
2024-10-28 12:26:15 +0900

    test(car): 이름이 최대 글자수 초과인 경우 에러를 던진다

--- __tests__/CarTest.js
+++ __tests__/CarTest.js
@@ -1,5 +1,8 @@
 import { Car } from "../src/Car.js";
-import { INPUT_ERROR_MESSAGE, INVALID_CAR_ERROR_MESSAGE } from "../src/constants/Messages.js";
+import {
+  INPUT_ERROR_MESSAGE,
+  OVER_NAME_LENGTH_ERROR_MESSAGE
+} from "../src/constants/Messages.js";
 
 describe('Car Test', () => {
   const name = 'Jelly';
@@ -19,5 +22,10 @@ describe('Car Test', () => {
   test('Car 클래스의 매개변수로 공백이 전달되면 에러를 던진다.', () => {
     const emptyName = '';
     expect(() => new Car(emptyName)).toThrow(INPUT_ERROR_MESSAGE);
+  })
+
+  test('이름이 최대 글자수 초과인 경우 에러를 던진다.', () => {
+    const overLengthName = '123456';
+    expect(() => new Car(overLengthName)).toThrow(OVER_NAME_LENGTH_ERROR_MESSAGE);
   })
 })
