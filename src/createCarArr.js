f47f313dc6cdd7879a11c8a725f057285ca8cfbf
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2024-10-28 21:50:23 +0900

    fix: 변수명 알맞게 변경

--- src/createCarArr.js
+++ src/createCarArr.js
@@ -7,7 +7,7 @@ export const createCarArr = (NAMESTR) => {
     for (let i = 0; nameStr !== ''; i += 2) {
         let carName = nameStr.slice(0, separatorIndex)
 
-        if (car.length > 5) {
+        if (carName.length > 5) {
             throw new Error("차 이름이 5자 초과입니다.")
         }
 
