ab75e766e0b75cb0edb5653d2d64b35f1c081689
PangKiYeon <pdrkeamy@gmail.com>
2024-10-28 19:18:39 +0900

    feat: App.js 에서 실행 가능하도록 구현

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import { Console } from '@woowacourse/mission-utils';
+import {
+    separateCarNames,
+    initializeMove,
+    runAllTrials,
+    displayWinners,
+} from './racingCar.js';
 
 class App {
     async run() {
         const RACING_CARS = await Console.readLineAsync(
             '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
         );
+        const CAR_NAMES = separateCarNames(RACING_CARS);
 
         const TRIAL_NUMBER = await Console.readLineAsync(
             '시도할 횟수는 몇 회인가요?\n'
         );
+
+        const MOVE_RESULTS = initializeMove(CAR_NAMES);
+
+        Console.print('\n실행 결과');
+        const FINAL_RESULT = runAllTrials(
+            TRIAL_NUMBER,
+            CAR_NAMES,
+            MOVE_RESULTS
+        );
+        displayWinners(CAR_NAMES, FINAL_RESULT);
     }
 }
 
