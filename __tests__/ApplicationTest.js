d1aca9c80af1cca165ce9b9a8969666cdd6b9cce
jllee000 <jllee000@naver.com>
2024-10-28 17:04:34 +0900

    feat : test코드 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -46,7 +46,7 @@ describe("자동차 경주", () => {
     });
   });
 
-  test("예외 테스트", async () => {
+  test("예외 테스트: 이름이 5자 초과", async () => {
     // given
     const inputs = ["pobi,javaji"];
     mockQuestions(inputs);
@@ -55,6 +55,61 @@ describe("자동차 경주", () => {
     const app = new App();
 
     // then
-    await expect(app.run()).rejects.toThrow("[ERROR]");
+    await expect(app.run()).rejects.toThrow(
+      "[ERROR] 자동차 이름은 5자 이하만 가능합니다."
+    );
+  });
+  test("예외 테스트: 시도 횟수에 0 이하 입력", async () => {
+    // given
+    const inputs = ["pobi,woni", "0"];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR] 잘못된 입력입니다.");
+  });
+
+  test("예외 테스트: 중복된 자동차 이름", async () => {
+    // given
+    const inputs = ["pobi,pobi"];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow(
+      "[ERROR] 자동차 이름은 중복될 수 없습니다."
+    );
+  });
+
+  test("여러 번의 시도 테스트", async () => {
+    // given
+    const MOVING_FORWARD = 4;
+    const STOP = 3;
+    const inputs = ["pobi,woni", "2"];
+    const logs = [
+      "pobi : -",
+      "pobi : --",
+      "woni : ",
+      "woni : ",
+      "최종 우승자 : pobi",
+    ];
+    const logSpy = getLogSpy();
+
+    // 두 번의 시도에 맞춘 랜덤 수 시퀀스
+    mockQuestions(inputs);
+    mockRandoms([MOVING_FORWARD, STOP, MOVING_FORWARD, STOP]);
+
+    // when
+    const app = new App();
+    await app.run();
+
+    // then
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
   });
 });
