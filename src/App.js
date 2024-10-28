c1e9586df45fed79eb1e26b392a1fa9f384fd6b7
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2024-10-28 18:11:08 +0900

    feat: 입출력기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from '@woowacourse/mission-utils'
+import { NAMESTRValid, RUNTIMESValid } from './Valid.js';
+import { createCarArr } from './createCarArr.js';
+
 class App {
-  async run() {}
+
+  async run() {
+    const NAMESTR = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    NAMESTRValid(NAMESTR)
+    const RUNTIMES = await Console.readLineAsync("시도할 횟수는 몇 회인가요?")
+    RUNTIMESValid(RUNTIMES)
+    createCarArr(NAMESTR)
+  }
+
 }
 
 export default App;
