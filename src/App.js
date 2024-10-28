3d9fc84dd05b729318b63b3658ec53a7df267d0a
JINWOOKIM <rlawlsdn9583@gmail.com>
2024-10-28 15:04:56 +0900

    feat: 입력값을 바탕으로 Car 클래스 정의

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App {
     try {
       Console.print('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
       const carNames = await Console.readLineAsync("");
-      const cars = carNames.split(',');
+      const cars = carNames.split(',').map(carName => new Car(carName));
 
       Console.print('시도할 횟수는 몇 회인가요?');
       const numOfAttempts = await Console.readLineAsync("");
