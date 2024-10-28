a028726afe1bd135be0034f587be46716810096a
rhtjddls123 <jjjk0605@naver.com>
2024-10-28 18:22:40 +0900

    test: test코드 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -46,6 +46,95 @@ describe('자동차 경주', () => {
     });
   });
 
+  test('기능 테스트2', async () => {
+    // given
+    const MOVING_FORWARD = 4;
+    const STOP = 3;
+    const inputs = ['pobi,woni,jun', '5'];
+    const logs = [
+      'pobi : -----',
+      'woni : ----',
+      'jun : -----',
+      '최종 우승자 : pobi, jun',
+    ];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([
+      MOVING_FORWARD,
+      STOP,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+    ]);
+
+    // when
+    const app = new App();
+    await app.run();
+
+    // then
+    logs.forEach(log => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test('기능 테스트 차량이 1대일 경우', async () => {
+    // given
+    const MOVING_FORWARD = 4;
+    const STOP = 3;
+    const inputs = ['pobi', '5'];
+    const logs = ['pobi : ----', '최종 우승자 : pobi'];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([
+      MOVING_FORWARD,
+      STOP,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+    ]);
+
+    // when
+    const app = new App();
+    await app.run();
+
+    // then
+    logs.forEach(log => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test('기능 테스트 아무 차량도 움직이지 않았을 경우', async () => {
+    // given
+    const STOP = 3;
+    const inputs = ['a,b,c', '3'];
+    const logs = ['a : ', 'b : ', 'c : ', '최종 우승자 : a, b, c'];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([STOP, STOP, STOP, STOP, STOP, STOP, STOP, STOP, STOP]);
+
+    // when
+    const app = new App();
+    await app.run();
+
+    // then
+    logs.forEach(log => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
   test('예외 테스트', async () => {
     // given
     const inputs = ['pobi,javaji'];
@@ -56,5 +145,61 @@ describe('자동차 경주', () => {
 
     // then
     await expect(app.run()).rejects.toThrow('[ERROR]');
+  });
+
+  test('예외 테스트 동일한 차량 이름이 있을 경우', async () => {
+    // given
+    const inputs = ['a,b,c,d,a'];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow(
+      '[ERROR] 차량 이름이 중복되었습니다!',
+    );
+  });
+
+  test('예외 테스트 차량 이름이 없는 경우가 있는 경우', async () => {
+    // given
+    const inputs = ['a,b,,d'];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow(
+      '[ERROR] 차량 이름 길이가 잘못되었습니다! 각 차량 이름은 1글자 이상 5글자 이하로 입력해주세요!',
+    );
+  });
+
+  test('예외 테스트 시도 횟수가 0이하일 경우', async () => {
+    // given
+    const inputs = ['a,b,c', '-2'];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow(
+      '[ERROR] 1이상의 숫자를 입력해주세요!',
+    );
+  });
+
+  test('예외 테스트 시도 횟수가 숫자가 아닐 경우', async () => {
+    // given
+    const inputs = ['a,b,c', '-h'];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow(
+      '[ERROR] 올바른 숫자를 입력해주세요!',
+    );
   });
 });
