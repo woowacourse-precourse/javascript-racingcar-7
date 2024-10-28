c30a6c1c81cd81bb3fc21a7908da834be1932237
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2024-10-28 21:12:53 +0900

    feat : 경주 객체 생성

--- src/models/Race.js
+++ src/models/Race.js
@@ -1,5 +1,36 @@
-class Race {
+import Car from './Car.js';
+import { MissionUtils } from '@woowacourse/mission-utils';
 
+class Race {
+  constructor(carNames, attemptCount) {
+    this.cars = carNames.split(",").map((name) => new Car(name));
+    this.attemptCount = attemptCount;
+  }
+
+  start() {
+    for (let i = 0; i< this.attemptCount; i++) {
+      this.cars.array.forEach((car) => {
+        const randomNumber = MissionUtils.Random.pickNumberInRange(0,9);
+        car.move(randomNumber);
+      });
+      this.printStatus();
+    }
+    this.printWinners();
+  }
+
+  printStatus() {
+    this.cars.forEach((car) => {
+      console.log(`${car.name} : ${'-'.repeat(car.getPosition())}`);
+    });
+    console.log("");
+  }
+
+  printWinners() {
+    const maxPosition = Math.max(...this.cars.map((car) =>  car.getPosition()));
+    const winners = this.car.filter((car) => car.getPosition() === maxPosition).map((car) => car.name);
+    console.log(`최종 우승자 : ${winners.join(", ")}`);
+  }
 }
+
 
 export default Race;
