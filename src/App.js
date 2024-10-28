0f576c8ee8796bd273d6d61806d84336dcaa2b63
rheefine <rheefine@gmail.com>
2024-10-28 20:19:41 +0900

    feat: App.js 에 ConsoleOutput 의존성 주입

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import ConsoleInput from './presentation/ConsoleInput.js';
+import ConsoleOutput from './presentation/ConsoleOutput.js';
 import RacingService from './application/RacingService.js';
 
 class App {
   #racingService;
 
   constructor () {
-    this.#racingService = new RacingService(ConsoleInput);
+    this.#racingService = new RacingService(ConsoleInput, ConsoleOutput);
   }
 
   async run () {
