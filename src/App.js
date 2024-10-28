91977c333a8b2858d7e95755800a1489c01c475e
chan8632 <duscks8632@gmail.com>
2024-10-28 19:13:19 +0900

    feat(App): 초기 자동차 경주 게임 설정 및 유효성 검사 기능 추가
    - Car 클래스의 기본 구조 정의
    - App 클래스에서 자동차 이름 유효성 검사 기능 구현
    - Console을 이용한 사용자 입력 처리 준비

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { Console } from "@woowacourse/mission-utils";
+
+class Car {
+  constructor(name, distance) {
+    this.name = name;
+    this.distance = 0;
+  }
+  move() {}
+}
+
 class App {
-  async run() {}
+  constructor(cars, rounds) {
+    this.cars = [];
+  }
+  setCars(input) {
+    const carList = input.split(",").map((name) => name.trim());
+    if (carList.some((name) => name.length > 5 || name.length == 0)) {
+      throw new Error("자동차 이름은 1자 이상 5자 이하만 가능합니다.");
+    }
+    Console.print(carList);
+  }
+  startRace() {}
+  announceWinner() {}
+  async run() {
+    const cars = await Console.readLineAsync("");
+    this.setCars(cars);
+  }
 }
 
 export default App;
