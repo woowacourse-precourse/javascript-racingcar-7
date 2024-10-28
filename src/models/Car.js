d277bac5fb4833051f81ce9c570da40d4e0fb3e6
LeeJinyoung218 <amy021899@gmail.com>
2024-10-28 17:50:54 +0900

    feat: Car 클래스에 전진 메소드 구현

--- src/models/Car.js
+++ src/models/Car.js
@@ -1,6 +1,16 @@
+import getRandomNumber from "../utils/RandomNumberGenerator";
+
 export default class Car {
     constructor(name) {
         this.name = name;
         this.move = 0;
+    }
+
+    move() {
+        const number = getRandomNumber();
+
+        if (number >= 4) {
+            this.move++;
+        }
     }
 };
