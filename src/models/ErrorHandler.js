b29be50e7ffe68f3576987901ebb70728fe2fbf5
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-10-28 16:44:24 +0900

    feat: 경주 횟수 입력 예외처리 기능 구현

--- src/models/ErrorHandler.js
+++ src/models/ErrorHandler.js
@@ -12,3 +12,15 @@ export function validateNotEmpty(carNames) {
     throw new Error('[ERROR] 정확한 이름을 입력해주세요.');
   }
 }
+
+export function validateRaceCountIsPositiveInteger(value) {
+  // 정규식을 사용하여 양의 정수만 허용
+  const positiveIntegerPattern = /^[1-9]\d*$/;
+
+  if (!positiveIntegerPattern.test(value)) {
+    throw new Error(
+      '[ERROR] 경주 횟수는 양의 정수로 입력해야 합니다. (0, 음수, 문자, 띄어쓰기 입력 불가)',
+    );
+  }
+}
+
