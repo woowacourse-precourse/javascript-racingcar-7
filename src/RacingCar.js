a60db71b58105fba8558c04ba1be70121fbbfcfa
rkdcodus <rcd7275@naver.com>
2024-10-28 16:59:47 +0900

    fix(RacingCar.js): change instance data structure to array from Map object and create count input

--- src/RacingCar.js
+++ src/RacingCar.js
@@ -3,19 +3,28 @@ import { Console } from '@woowacourse/mission-utils';
 class RacingCar {
   #cars;
 
+  #status;
+
+  #count;
+
   constructor() {
-    this.#cars = new Map();
+    this.#cars = [];
+    this.#status = [];
   }
 
   async receiveInput() {
-    const input = await Console.readLineAsync(
+    const cars = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
     );
+    const count = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
 
-    input.split(',').forEach((car) => {
+    cars.split(',').forEach((car) => {
       car.trim();
-      this.#cars.set(car, '');
+      this.#cars.push(car);
+      this.#status.push('');
     });
+
+    this.#count = count;
   }
 }
 
