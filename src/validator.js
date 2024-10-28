f4bebdac41f1d255b6662b16d237a81cfc3206d7
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-10-28 14:36:33 +0900

    refactor: 검증 함수 분리

--- src/validator.js
+++ src/validator.js
@@ -1,23 +1,36 @@
 import { ERROR_MESSAGES } from "./constants.js";
 
+function hasEmptyName(names) {
+  return names.some((name) => name === "");
+}
+
+function hasInvalidSeparator(inputString) {
+  return /[^가-힣a-zA-Z0-9,]/.test(inputString);
+}
+
+function hasTooLongName(names) {
+  return names.some((name) => name.length > 5);
+}
+
+function hasNames(names) {
+  return new Set(names).size !== names.length;
+}
+
 export function validateCarNames(names) {
-  if (names.some((name) => name === "")) {
+  if (hasEmptyName(names)) {
     throw new Error(ERROR_MESSAGES.CAR_NAME_LENGTH);
   }
 
   const inputString = names.join(",");
-  const invalidSeparator = /[^가-힣a-zA-Z0-9,]/.test(inputString);
-  if (invalidSeparator) {
+  if (hasInvalidSeparator(inputString)) {
     throw new Error(ERROR_MESSAGES.INVALID_SEPARATOR);
   }
 
-  names.forEach((name) => {
-    if (name.length > 5) {
-      throw new Error(ERROR_MESSAGES.CAR_NAME_LENGTH);
-    }
-  });
+  if (hasTooLongName(names)) {
+    throw new Error(ERROR_MESSAGES.CAR_NAME_LENGTH);
+  }
 
-  if (new Set(names).size !== names.length) {
+  if (hasNames(names)) {
     throw new Error(ERROR_MESSAGES.SAME_CAR_NAME);
   }
 }
