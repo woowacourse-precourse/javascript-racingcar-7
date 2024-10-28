e21c8b641d853d96d7aa61a47470bac8649c00df
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-10-28 18:35:04 +0900

    refector: App.js 함수 기능 분리

--- src/App.js
+++ src/App.js
@@ -1,55 +1,57 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import {
   getUserCarName,
-  getUserRacingNum,
-  printRacingState,
-  printRacingWinner,
+  getUserRaceAttempts,
+  displayRaceState,
+  displayWinners,
 } from './views/View.js';
 import {
-  divisionCarName,
-  createCarObject,
-  shouldMoveForward,
-  getCarPositionsRepresentation,
-  findWinners,
+  splitCarNamesByDelimiter,
+  createCarDataArray,
+  updateCarDataPositions,
+  formatAllCarPositions,
+  findCarWinners,
 } from './models/Model.js';
-import {
-  validateCarNames,
-  validateNotEmpty,
-  validateRaceCountIsPositiveInteger,
-} from './models/ErrorHandler.js';
+import { validateCarInputs } from './models/ErrorHandler.js';
 
 class App {
   async run() {
     try {
-      const userInputCar = await getUserCarName();
-      const carNames = divisionCarName(userInputCar.toString());
+      const carNames = await this.getValidatedCarNames();
+      const raceAttempts = await this.getValidatedRaceAttempts();
+      validateCarInputs(carNames, raceAttempts);
 
-      validateCarNames(carNames);
-      validateNotEmpty(carNames);
-      const userInputRacingNum = await getUserRacingNum();
-      validateRaceCountIsPositiveInteger(userInputRacingNum);
-
-      const initialCarData = createCarObject(carNames);
-
+      const initialCarData = createCarDataArray(carNames);
       MissionUtils.Console.print(`\n실행 결과`);
 
-      const finalCarData = Array.from({ length: userInputRacingNum }).reduce(
-        (acc) => {
-          const updatedCarData = shouldMoveForward(acc);
-          const carPositionsRepresentation =
-            getCarPositionsRepresentation(updatedCarData);
-          printRacingState(carPositionsRepresentation); // 상태 출력
-          return updatedCarData; // 다음 반복에 누적된 값 사용
-        },
-        initialCarData,
-      );
+      const finalCarData = this.executeRace(initialCarData, raceAttempts);
+      const winnerName = findCarWinners(finalCarData);
 
-      const winnerName = findWinners(finalCarData);
-      return printRacingWinner(winnerName);
+      displayWinners(winnerName);
     } catch (error) {
       MissionUtils.Console.print(error.message);
-      throw error; // 에러를 다시 던져서 애플리케이션이 종료되도록 함
+      throw error;
     }
+  }
+
+  async getValidatedCarNames() {
+    const userInputCar = await getUserCarName();
+    const carNames = splitCarNamesByDelimiter(userInputCar.toString());
+    return carNames;
+  }
+
+  async getValidatedRaceAttempts() {
+    const userInputRacingNum = await getUserRaceAttempts();
+    return userInputRacingNum;
+  }
+
+  executeRace(initialCarData, raceAttempts) {
+    return Array.from({ length: raceAttempts }).reduce((acc) => {
+      const updatedCarData = updateCarDataPositions(acc);
+      const carPositionsRepresentation = formatAllCarPositions(updatedCarData);
+      displayRaceState(carPositionsRepresentation);
+      return updatedCarData;
+    }, initialCarData);
   }
 }
 
