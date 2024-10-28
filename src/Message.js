29c4936d896364b1aaa980fff0917f4a3fb1364f
soyeooooo <alththdus1@naver.com>
2024-10-28 18:00:28 +0900

    feat: 입출력 관리

---
+++ src/Message.js
@@ -0,0 +1,8 @@
+export const INPUT_MESSAGE = {
+  carName: '경주할 자동차 이름을 입력하세요.(이름은 (,)기준으로 구분됩니다. 마지막은 붙이지마세요.)\n',
+  tryCount: '시도할 횟수는 몇 회인가요?\n',
+};
+
+export const OUTPUT_MESSAGE = {
+  winner: '최종 우승자 : ',
+};
