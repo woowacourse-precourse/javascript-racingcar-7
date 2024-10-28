7d8268d44d9b8ce0e31f9ea6cc10cb0a002260bf
jiHeeFlee <esder1310@naver.com>
2024-10-28 20:54:40 +0900

    refactor(App.js) : exception 1  declare and call a function to ensure that car names are limited to 5 characters or less

--- src/App.js
+++ src/App.js
@@ -50,6 +50,12 @@ class App {
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
     
+    // 5. 예외 처리 1 : 자동차 이름은 각각 5자 이하만 가능하게 확인하는 함수 호출
+    if (car_input.includes(",")) {
+      this.car_names = car_input.split(",");
+      this.car_name_length(this.car_names);
+    }
+    
     // 2. 게임을 몇번 시도할 건지 횟수 입력 받기(횟수를 라운드로 표현)
     const number_input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
     const rounds = Number(number_input);
