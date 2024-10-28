ac0ce019705bc098a20f89a24c6d91a5f8b43b28
조승우 <choseung97@gmail.com>
2024-10-28 23:17:40 +0900

    fix(validator): 유효한 시간 복잡도 이상인 경우 에러로 처리하도록 수정한다
    기존 유효한 시간 복잡도 초과인 경우 에러 처리되던 것을 이상인 경우 에러 처리되도록 수정한다.

--- src/utils/validation/validateTimeComplexity.js
+++ src/utils/validation/validateTimeComplexity.js
@@ -3,7 +3,7 @@ import Rules from '../../resources/Rules.js';
 
 export default function validateTimeComplexity(names, repetitionString) {
   const N = names.split(',').length * Number(repetitionString);
-  if (N > Rules.VALID_TIMECOMPLEXITY) {
+  if (N >= Rules.VALID_TIMECOMPLEXITY) {
     throw new Error(ErrorMessage.OVER_VALID_TIMECOMPLEXITY);
   }
 }
