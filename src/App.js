4cbc39c49ce2a549f23fd2c0498231c8ec1f9dfc
MERx88 <wjang07@gmail.com>
2024-10-28 13:01:31 +0900

    Feat(App): add feature to verify user input exists
    - Handle the case where user input is empty by asking for input again

--- src/App.js
+++ src/App.js
@@ -32,9 +32,11 @@ class App {
 
   async readAttemptCount() {
     let userAttemptCount = 0;
-    userAttemptCount = await Console.readLineAsync(
-      "시도할 횟수는 몇 회인가요?\n"
-    );
+    while (Number(userAttemptCount) === 0) {
+      userAttemptCount = await Console.readLineAsync(
+        "시도할 횟수는 몇 회인가요?\n"
+      );
+    }
 
     return userAttemptCount;
   }
