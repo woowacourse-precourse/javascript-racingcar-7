ace7700052c3c8599c0a50e9f309069998a5960b
장정안 <wjddks96@naver.com>
2024-10-28 16:44:26 +0900

    test: 시도 횟수 유효성 검사 테스트 코드 each로 구조화

--- src/Validator/validateAttempts.test.js
+++ src/Validator/validateAttempts.test.js
@@ -2,22 +2,11 @@ const { ATTEMPT_ERROR_MESSAGE } = require('../constants/messages');
 const { validateAttempts } = require('./validateAttempts');
 
 describe('시도할 횟수 유효성 검증 테스트', () => {
-  test('빈 값으로 입력하면 에러를 던진다.', () => {
-    expect(() => validateAttempts('')).toThrow(ATTEMPT_ERROR_MESSAGE.NO_INPUT);
-  });
-
-  test('숫자가 아닌 값을 입력하면 에러를 던진다.', () => {
-    expect(() => validateAttempts('a')).toThrow(
-      ATTEMPT_ERROR_MESSAGE.NOT_A_NUMBER
-    );
-  });
-
   test.each([
-    ['-1', '음수'],
-    ['0', '최소값 미만'],
-  ])('%s를 입력하면 에러를 던진다. (%s)', (input) => {
-    expect(() => validateAttempts(input)).toThrow(
-      ATTEMPT_ERROR_MESSAGE.OUT_OF_RANGE
-    );
+    ['-1', ATTEMPT_ERROR_MESSAGE.OUT_OF_RANGE, '음수'],
+    ['', ATTEMPT_ERROR_MESSAGE.NO_INPUT, '빈 값'],
+    ['a', ATTEMPT_ERROR_MESSAGE.NOT_A_NUMBER, '숫자가 아닌 값'],
+  ])('%s를 입력하면 에러를 던진다. (%s)', (input, message) => {
+    expect(() => validateAttempts(input)).toThrow(message);
   });
 });
