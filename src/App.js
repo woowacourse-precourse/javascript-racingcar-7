91ad3cf7a4b2fc6015a8793b7e166edc0e12fa7e
asdfghjk1052@naver.com <asdfghjk1052@naver.com>
2024-10-28 22:33:37 +0900

    feature(App) 자동차 이름을 입력 받아 유효성 검사 구현

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const carNames = await this.getCarName();
-    Console.print(carNames);
+    try {
+      const carNames = await this.getCarNames();
+      const attemptCount = await this.getAttemptCount();
+      const results = this.startRace(carNames, attemptCount);
+      this.displayRaceResults(results);
+      this.displayWinner(results);
+    } catch (error) {
+      Console.print(error);
+    }
   }
 
-  // 자동차 입력
-  async getCarName() {
+  async getCarNames() {
     Console.print(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n"
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
     const input = await Console.readLineAsync();
-    const carNames = input.split(",");
+    const carNames = input.split(",").map((name) => name.trim());
+
+    this.validateCarNames(carNames);
     return carNames;
+  }
+
+  validateCarNames(carNames) {
+    const nameSet = new Set();
+    for (const name of carNames) {
+      if (!name) {
+        throw "[ERROR] 이름이 공백입니다.";
+      }
+      if (name.length > 5) {
+        throw "[ERROR] 이름은 5자 이하로 입력해야 합니다.";
+      }
+      if (nameSet.has(name)) {
+        throw "[ERROR] 중복된 이름이 있습니다.";
+      }
+      nameSet.add(name);
+    }
   }
 }
 
