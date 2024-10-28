f6b0a4206c196d77ceff7837bebf9a1d61f932ed
elect10 <wlgnsakswp@naver.com>
2024-10-28 21:32:07 +0900

    fix: print function in error part

--- src/io/GetInput.js
+++ src/io/GetInput.js
@@ -8,7 +8,7 @@ class GetInput {
 
     carNamesArray.forEach((carName) => {
       if (carName.length > 5) {
-        throw new Error('[ERROR] : 5글자 이하로 입력해주세요.');
+        throw new Error("[ERROR] 5글자 이하로 입력해주세요.");
       }
     });
     return carNamesArray;
@@ -17,7 +17,7 @@ class GetInput {
   static async getTryCount() {
     const tryCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요? \n');
     if (isNaN(tryCount)) {
-      throw new Error('[ERROR] : 숫자를 입력해주세요.');
+      throw new Error("[ERROR] 숫자를 입력해주세요.");
     }
     return Number(tryCount);
   }
