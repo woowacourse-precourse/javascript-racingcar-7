5797ca2ae72638b3630cd0df0e4684b91ea80e7a
rhtjddls123 <jjjk0605@naver.com>
2024-10-28 16:03:08 +0900

    fix: 차량 이름 중복시 오류를 발생시키도록 수정

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ class App {
   static ERROR_MESSAGE = {
     CAR_NAME_LENGTH_ERROR:
       '차량 이름 길이가 잘못되었습니다! 각 차량 이름은 1글자 이상 5글자 이하로 입력해주세요!',
+    CAR_NAME_DUPLICATE_ERROR: '차량 이름이 중복되었습니다!',
     ATTEMPT_COUNT_ERROR: '올바른 숫자를 입력해주세요!',
     ATTEMPT_COUNT_MINIMUM_ERROR: '1이상의 숫자를 입력해주세요!',
   };
@@ -94,6 +95,9 @@ class App {
     if (!this.isCarNameValid(cars)) {
       throw Error(`[ERROR] ${App.ERROR_MESSAGE.CAR_NAME_LENGTH_ERROR}`);
     }
+    if (this.isDuplicateCarName(cars)) {
+      throw Error(`[ERROR] ${App.ERROR_MESSAGE.CAR_NAME_DUPLICATE_ERROR}`);
+    }
   }
 
   separateCar(cars) {
@@ -102,6 +106,12 @@ class App {
 
   isCarNameValid(cars) {
     return cars.every(car => car.name.length > 0 && car.name.length <= 5);
+  }
+
+  isDuplicateCarName(cars) {
+    const carsNames = cars.map(car => car.name);
+    const uniqueNames = new Set(carsNames);
+    return carsNames.length !== uniqueNames.size;
   }
 }
 
