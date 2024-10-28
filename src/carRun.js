dcfb24ec1bcfa4ab5c0278664a1066447e0b75db
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2024-10-28 19:41:28 +0900

    feat: 레이싱 프린트 함수 추가

--- src/carRun.js
+++ src/carRun.js
@@ -1,24 +1,39 @@
 import { MissionUtils } from "@woowacourse/mission-utils"
+import { Console } from "@woowacourse/mission-utils";
 
 export const carRun = (carArr, RUNTIMES) => {
-    const isRun = () => {
-        let randomNumber = 0;
+    const racing = () => {
 
         for (let i = 1; i <= carArr.length; i += 2) {
-            randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
-            console.log("random: ", randomNumber)
+            let isRun = MissionUtils.Random.pickNumberInRange(0, 9) >= 4
 
-            if (randomNumber >= 4) {
+            if (isRun) {
+                //i-1번째에 저장된 car이동
                 carArr[i] += 1
-                console.log("추가됨")
             }
         }
+
     }
+
+    const printRacing = () => {
+        let result = ''
+
+        for (let i = 0; i < carArr.length; i += 2) {
+            const CARNAME = carArr[i]
+            const CARMOVENODE = '-'.repeat(carArr[i + 1])
+
+            result += `${CARNAME} : ${CARMOVENODE}'\n'`
+        }
+
+        Console.print(result)
+    }
+
+    Console.print("실행 결과")
 
     for (let i = 0; i < RUNTIMES; i++) {
-        isRun()
+        racing()
+        printRacing()
     }
 
-    console.log("carArr: ", carArr)
 }
 
