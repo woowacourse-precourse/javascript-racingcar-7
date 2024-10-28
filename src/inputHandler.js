e8eede3d68a5dad5b3c9293966ac937a9b54fcb9
xxxjinn <tlsguswls319@naver.com>
2024-10-28 11:38:48 +0900

    refactor(inputHandler.js): module 가져오는 방식 변경

--- src/inputHandler.js
+++ src/inputHandler.js
@@ -1,8 +1,8 @@
-import { Console } from "@woowacourse/mission-utils";
+import { MissionUtils } from "@woowacourse/mission-utils";
 import { ERROR_MESSAGES, INPUT_MESSAGES } from "./constants.js";
 
 export async function getValidatedCarNames() {
-  const CARS_INPUT = await Console.readLineAsync(INPUT_MESSAGES.CAR_NAME_MESSAGE);
+  const CARS_INPUT = await MissionUtils.Console.readLineAsync(INPUT_MESSAGES.CAR_NAME_MESSAGE);
   const carNames = CARS_INPUT.split(",").map(carName => carName.trim());
 
   carNames.forEach((carName) => {
@@ -15,7 +15,7 @@ export async function getValidatedCarNames() {
 }
 
 export async function getValidatedAttemptCount() {
-    const ATTEMPT_COUNT_INPUT = await Console.readLineAsync(INPUT_MESSAGES.ATTEMPT_COUNT_MESSAGE);
+    const ATTEMPT_COUNT_INPUT = await MissionUtils.Console.readLineAsync(INPUT_MESSAGES.ATTEMPT_COUNT_MESSAGE);
     const ATTEMPT_COUNT = Number(ATTEMPT_COUNT_INPUT);
     
     if (isNaN(ATTEMPT_COUNT ) || ATTEMPT_COUNT  <= 0 || !Number.isInteger(ATTEMPT_COUNT )) {
