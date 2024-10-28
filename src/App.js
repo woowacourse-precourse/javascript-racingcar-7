9707fbd9077f034b10ef7b816a75cd3c048dc38b
Doyeon <duseh729@naver.com>
2024-10-28 17:19:35 +0900

    feat: 레이스 구현 입력 받은 count 만큼 레이스 결과를 raceResultMap 변수에 저장

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
     const names = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
     const count = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n")
     
-    const nameArray = this.separateNameHandler(names);
+    const nameMap = this.separateNameHandler(names);
 
-    Console.print(nameArray);
+    const raceResultMap = this.raceHanlder(nameMap, count);
+    Console.print(raceResultMap);
   }
 
   separateNameHandler(names){
-    return names.split(',');
+    const nameArray = names.split(',');
+    const nameMap = new Map();
+
+    for(let i of nameArray){
+      nameMap.set(i, 0);
+    }
+
+    return nameMap;
+  }
+
+  raceHanlder(nameMap, count){
+    for(let i=0; i<count; i++){
+      for(let name of nameMap.keys()){
+        const random = Random.pickNumberInRange(0,9);
+        if(random>=4){
+          nameMap.set(name, nameMap.get(name)+1);
+        }
+      }
+    }
+
+    return nameMap;
   }
 }
 
