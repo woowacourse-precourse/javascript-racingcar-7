36c32cba5c9006640c63b24979c15d44f600b319
Dobbymin <gmin990624@gmail.com>
2024-10-28 16:13:46 +0900

    fix: 코드 포맷팅 수정

--- src/view/functions/inputTryNumber.js
+++ src/view/functions/inputTryNumber.js
@@ -3,9 +3,7 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 import { INPUT } from '../../constants/constants.js';
 
 export const inputTryNumber = async () => {
-  const tryCount = await MissionUtils.Console.readLineAsync(
-    INPUT.RACING_COUNT_INPUT,
-  );
+  const tryCount = await MissionUtils.Console.readLineAsync(INPUT.RACING_COUNT_INPUT);
 
   return tryCount;
 };
