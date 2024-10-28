8725c61a64a273c656a84f9d5d8e5efd0c10e4b5
Dobbymin <gmin990624@gmail.com>
2024-10-28 14:42:18 +0900

    feat(constants): 상수 모듈화 구현

---
+++ src/constants/constants.js
@@ -0,0 +1,6 @@
+import { ERROR } from './message/error-message.js';
+import { INPUT } from './message/input-message.js';
+import { OUTPUT } from './message/output-message.js';
+import { OTHERS } from './message/others-message.js';
+
+export { ERROR, INPUT, OUTPUT, OTHERS };
