9e3f28af1d7d7eaaa10608421b59860cc24ede94
Dobbymin <gmin990624@gmail.com>
2024-10-28 16:08:45 +0900

    fix(controller): 포맷팅 수정 및 함수 분리

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,39 +1,37 @@
-import {
-  verifyRacingCarName,
-  verifyTryNumber,
-  isWinner,
-  getRace,
-} from '../model/model.js';
+import { verifyRacingCarName, verifyTryNumber, isWinner, getRace } from '../model/model.js';
 
-import {
-  inputRacingCarName,
-  inputTryNumber,
-  printWinner,
-  progressRacingCar,
-  printRunRacingCar,
-} from '../view/view.js';
+import { inputRacingCarName, inputTryNumber, printWinner, progressRacingCar, printRunRacingCar } from '../view/view.js';
 
 class Controller {
   async run() {
-    const inputCarName = await inputRacingCarName();
-
-    const cars = await verifyRacingCarName(inputCarName);
-
-    const inputNumber = await inputTryNumber();
-
-    let tryNumber = verifyTryNumber(inputNumber);
+    const cars = await this.getValidatedCars();
+    const tryNumber = await this.getValidatedTryNumber();
 
     printRunRacingCar();
-
-    while (tryNumber--) {
-      await getRace(cars);
-
-      await progressRacingCar(cars);
-    }
+    await this.runRaces(cars, tryNumber);
 
     const winner = isWinner(cars);
 
     printWinner(winner);
+  }
+
+  async getValidatedCars() {
+    const inputCarName = await inputRacingCarName();
+
+    return verifyRacingCarName(inputCarName);
+  }
+
+  async getValidatedTryNumber() {
+    const inputNumber = await inputTryNumber();
+
+    return verifyTryNumber(inputNumber);
+  }
+
+  async runRaces(cars, tryNumber) {
+    Array.from({ length: tryNumber }).forEach(async () => {
+      getRace(cars);
+      progressRacingCar(cars);
+    });
   }
 }
 
