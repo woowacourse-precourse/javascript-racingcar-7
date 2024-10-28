a00d59e9214d9001d6f4b57bfdf003c193d008d3
xxxjinn <tlsguswls319@naver.com>
2024-10-28 11:15:30 +0900

    refactor(constants.js): 입력 메세지 상수로 관리

--- src/inputHandler.js
+++ src/inputHandler.js
@@ -1,8 +1,8 @@
 import { Console } from "@woowacourse/mission-utils";
-import { ERROR_MESSAGES } from "./constants.js";
+import { ERROR_MESSAGES, INPUT_MESSAGES } from "./constants.js";
 
 export async function getValidatedCarNames() {
-  const CARS_INPUT = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+  const CARS_INPUT = await Console.readLineAsync(INPUT_MESSAGES.CAR_NAME_MESSAGE);
   const carNames = CARS_INPUT.split(",").map(carName => carName.trim());
 
   carNames.forEach((carName) => {
@@ -15,7 +15,7 @@ export async function getValidatedCarNames() {
 }
 
 export async function getValidatedAttemptCount() {
-    const ATTEMPT_COUNT_INPUT = await Console.readLineAsync('시도할 횟수는 몇 회인가요?.\n');
+    const ATTEMPT_COUNT_INPUT = await Console.readLineAsync(INPUT_MESSAGES.ATTEMPT_COUNT_MESSAGE);
     const ATTEMPT_COUNT = Number(ATTEMPT_COUNT_INPUT);
     
     if (isNaN(ATTEMPT_COUNT ) || ATTEMPT_COUNT  <= 0 || !Number.isInteger(ATTEMPT_COUNT )) {
