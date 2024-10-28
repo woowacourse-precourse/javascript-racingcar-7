bca225a94255fe6f63670e190875f72052aa62ef
김나연 <apinksky00@gmail.com>
2024-10-28 15:16:01 +0900

    feat : 랜덤숫자 기반으로 전진시키기

--- src/App.js
+++ src/App.js
@@ -8,11 +8,30 @@ class Car {
     this.name = carName;
     this.position = 0;
   }
+
+  move() {
+    if (Random.pickNumberInRange(0, 9) >= 4) {
+      this.position++;
+    }
+  }
 }
 
 class RacingGame {
   constructor(carsNames) {
     this.cars = carsNames.split(",").map((name) => new Car(name.trim()));
+  }
+
+  play(number) {
+    const rounds = Number(number);
+    if (isNaN(rounds)) {
+      throw new Error("[ERROR]");
+    }
+    for (let i = 0; i < rounds; i++) {
+      this.moveAllCars();
+    }
+  }
+  moveAllCars() {
+    this.cars.forEach((car) => car.move());
   }
 }
 
@@ -23,7 +42,9 @@ class App {
         "경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분)\n"
       );
       const playsNumber = await Console.readLineAsync("시도할 횟수\n");
+
       const game = new RacingGame(carsNames);
+      game.play(playsNumber);
     } catch (error) {
       throw new Error(error.message);
     }
