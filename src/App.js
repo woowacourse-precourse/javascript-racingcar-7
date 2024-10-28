8d58ace178352e5105bd6ee095d28a7f7c6ec7c0
keemsebeen <sebin0580@gmail.com>
2024-10-28 14:26:13 +0900

    feat : 실행 결과와 최종 우승자 출력 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { EXECUTION_RESULT, INPUT } from "./constants/race.js";
+import { EXECUTION_RESULT, FINAL_WINNER, INPUT } from "./constants/race.js";
 import { simulateRace } from "./race/race.js";
-import { INPUT } from "./constants/race.js";
 import { parseCarNames } from "./car/parseCarNames.js";
 import { attemptValidator } from "./validation/attemptValidator.js";
 import { getInput } from "./view/input.js";
+import { getOutput, getResult, getWinner } from "./view/output.js";
 
 class App {
   async run() {
@@ -13,7 +13,11 @@ class App {
     const attemptCount = attemptValidator(attemptCountInput);
 
     getOutput("\n" + EXECUTION_RESULT);
+
     const { raceStates, winners } = simulateRace(carNames, attemptCount);
+
+    await getResult(raceStates);
+    getWinner(FINAL_WINNER, winners);
   }
 }
 
