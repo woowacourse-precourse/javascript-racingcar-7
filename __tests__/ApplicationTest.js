c49f3af77f244b5cf8c80e4376aff9a865b10188
deokisys <sjb378@me.com>
2024-10-28 23:12:49 +0900

    test: 레이싱게임 테스트 항목 추가
    공동우승자일경우 테스트
    우승자가 없을경우 테스트

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -46,6 +46,46 @@ describe("자동차 경주", () => {
     });
   });
 
+  test("공동우승자 확인", async () => {
+    // given
+    const MOVING_FORWARD = 4;
+    const inputs = ["pobi,woni", "2"];
+    const logs = ["pobi : -", "woni : -", "최종 우승자 : pobi, woni"];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([MOVING_FORWARD, MOVING_FORWARD]);
+
+    // when
+    const app = new App();
+    await app.run();
+
+    // then
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test("이동을 둘다 못해서 우승자 없음", async () => {
+    // given
+    const STOP = 3;
+    const inputs = ["pobi,woni", "2"];
+    const logs = ["pobi : ", "woni : ", "최종 우승자 : 없음"];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([STOP, STOP]);
+
+    // when
+    const app = new App();
+    await app.run();
+
+    // then
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
   test("[예외]5글자를 넘은 자동차 이름 예외", async () => {
     // given
     const inputs = ["pobi,javaji"];
