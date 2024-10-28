b9e75f0e781f9f172a711a3631825c2f4c272820
Parksangwoo <psu8430@gmail.com>
2024-10-28 15:16:54 +0900

    refactor(run) : isolation of constant values related to the service implemented
    - Separation of service-related constants
    - Separation error prefix string

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
 import { Console } from '@woowacourse/mission-utils';
 import RandomUtil from './utils/Random.js';
 import RacingCar from './Models/RacingCar.js';
+import { ERROR_PREFIX, SERVICE_CONSTSANSTS } from './assets/constants.js';
 
 class App {
   async run() {
     try {
       // 자동차 이름 받기
       const userInput = await Console.readLineAsync(
-        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+        `경주할 자동차 이름을 입력하세요.(이름은 쉼표(${SERVICE_CONSTSANSTS.DELIMITER}) 기준으로 구분)\n`,
       );
 
       if (userInput == '')
-        throw new Error('[ERROR] 자동차 이름이 입력되지 않았습니다.');
+        throw new Error(`${ERROR_PREFIX} 자동차 이름이 입력되지 않았습니다.`);
 
       // 자동차 이름 분리
-      const carNameArr = userInput.split(',');
+      const carNameArr = userInput.split(SERVICE_CONSTSANSTS.DELIMITER);
 
       if (new Set(carNameArr).size != carNameArr.length)
-        throw new Error('[ERROR] 같은 이름을 가진 자동차가 존재합니다.');
+        throw new Error(
+          `${ERROR_PREFIX} 같은 이름을 가진 자동차가 존재합니다.`,
+        );
 
-      if (carNameArr.length < 2)
-        throw new Error('[ERROR] 경주를 위해 2대 이상의 자동차가 필요합니다.');
+      if (carNameArr.length < SERVICE_CONSTSANSTS.MINIMAL_CAR_COUNT)
+        throw new Error(
+          `${ERROR_PREFIX} 경주를 위해 ${SERVICE_CONSTSANSTS.MINIMAL_CAR_COUNT}대 이상의 자동차가 필요합니다.`,
+        );
 
       // 자동차 인스턴스 생성
       const carInstanceArr = carNameArr.map((name) => {
-        if (name.length > 5 || name.length == 0) {
+        if (
+          name.length > SERVICE_CONSTSANSTS.MAXIMAL_CAR_NAME_LENGTH ||
+          name.length < SERVICE_CONSTSANSTS.MINIMAL_CAR_NAME_LENGTH
+        ) {
           throw new Error(
-            '[ERROR] 자동차의 이름은 1~5글자로 작성되어야 합니다.',
+            `${ERROR_PREFIX} 자동차의 이름은 ${SERVICE_CONSTSANSTS.MINIMAL_CAR_NAME_LENGTH}~${SERVICE_CONSTSANSTS.MAXIMAL_CAR_NAME_LENGTH}글자로 작성되어야 합니다.`,
           );
         } else {
           return new RacingCar(name);
@@ -40,9 +48,11 @@ class App {
       tryCount = Number(tryCount);
 
       // 시도 횟수에 대한 예외 케이스
-      if (isNaN(tryCount)) throw new Error('[ERROR] 숫자만 입력해주세요.');
+      if (isNaN(tryCount))
+        throw new Error(`${ERROR_PREFIX} 숫자만 입력해주세요.`);
 
-      if (tryCount < 0) throw new Error('[ERROR] 0보다 큰 수를 입력해주세요.');
+      if (tryCount < 0)
+        throw new Error(`${ERROR_PREFIX} 0보다 큰 수를 입력해주세요.`);
 
       // 줄 구분용 빈칸
       Console.print('');
@@ -55,13 +65,13 @@ class App {
           // 각 자동차 이동
           const randomNumber = RandomUtil.getRandomNumber();
 
-          if (randomNumber >= 4) {
+          if (randomNumber >= SERVICE_CONSTSANSTS.MOVING_REFERENCE_NUMBER) {
             car.move();
           }
 
           // 각 자동차 위치 출력
           const distanceString = new Array(car.movedDistance)
-            .fill('-')
+            .fill(SERVICE_CONSTSANSTS.MOVING_INDICATOR)
             .join('');
           Console.print(`${car.name} : ${distanceString}`);
         });
@@ -84,7 +94,9 @@ class App {
       }
 
       // 우승자 출력
-      Console.print(`최종 우승자 : ${winner.map((car) => car.name).join(',')}`);
+      Console.print(
+        `최종 우승자 : ${winner.map((car) => car.name).join(`${SERVICE_CONSTSANSTS.DELIMITER} `)}`,
+      );
     } catch (error) {
       Console.print(error.message);
       throw error;
