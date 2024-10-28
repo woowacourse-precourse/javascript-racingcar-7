64b8cc52b855a02c02173aa76e3ddf838a3f9d91
ejinn1 <ejinn1@naver.com>
2024-10-28 18:59:29 +0900

    fix: print format change

--- src/view/View.js
+++ src/view/View.js
@@ -16,7 +16,7 @@ export class View {
 
   displayCurrentPosition(cars) {
     cars.forEach((car) => {
-      Console.print(`${car.getName()}: ${'-'.repeat(car.getPosition())}`);
+      Console.print(`${car.getName()} : ${'-'.repeat(car.getPosition())}`);
     });
     Console.print('\n');
   }
