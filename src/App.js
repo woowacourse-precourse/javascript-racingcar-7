811767cadffc4e3b2b789c68870083eba92d736c
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-10-28 14:04:28 +0900

    feat: 입출력 로직 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import {
+  getUserCarName,
+  getUserRacingNum,
+  printRacingState,
+  printRacingWinner,
+} from './views/View.js';
+import {
+  divisionCarName,
+  createCarObject,
+  shouldMoveForward,
+  getCarPositionsRepresentation,
+  findWinners,
+} from './models/Model.js';
+import { validateCarNames } from './models/ErrorHandler.js';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const userInputCar = await getUserCarName();
+      const carNames = divisionCarName(userInputCar.toString());
+
+      validateCarNames(carNames);
+      const userInputRacingNum = parseInt(await getUserRacingNum(), 10);
+      
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+      throw error; // 에러를 다시 던져서 애플리케이션이 종료되도록 함
+    }
+  }
 }
 
 export default App;
