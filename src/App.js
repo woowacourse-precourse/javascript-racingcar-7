87f062ad117a557e16ab9aa9695230ff2e878159
(hoooooony) <(cjh41820@gmail.com)>
2024-10-28 19:57:22 +0900

    feat : 시도 횟수 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {Console} from "@woowacourse/mission-utils";
 import getCars from "./userIo/getCars.js";
 import Car from "./logic/Car.js";
 
@@ -6,6 +7,7 @@ class App {
     const carNames = await getCars();
     const cars = carNames.map((name) => new Car(name));
 
+    const attemptCount = await Console.readLineAsync("시도 횟수를 입력하시오.\n");
   }
 }
 
