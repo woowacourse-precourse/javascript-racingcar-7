9b4113627f2ff98e9d7bbe86751e72231e905be1
kimgho <kgh67811@naver.com>
2024-10-28 20:38:18 +0900

    feat(message): 에러 및 게임 메시지 작성
    - error.js를 통해 에러 메시지 관리
    - message.js를 통해 게임 안내 메시지 관리

---
+++ src/constant/error.js
@@ -0,0 +1,17 @@
+/** 에러 메시지 관리
+ *
+ * @type {Readonly<{EMPTY_ATTEMPTS: string, EMPTY_CAR_NAME: string,
+ *        NEGATIVE_ATTEMPTS: string, LENGTH_OVER_CAR_NAME: string,
+ *        NAN_ATTEMPTS: string, ZERO_ATTEMPTS: string}>}
+ */
+export const ERROR_MESSAGES = Object.freeze(
+  {
+    EMPTY_CAR_NAME: "[ERROR] : 자동차 이름은 공백일 수 없습니다. ",
+    LENGTH_OVER_CAR_NAME: "[ERROR] : 자동차 이름은 5자 이하만 가능합니다.",
+    EMPTY_ATTEMPTS: "[ERROR] : 시도 횟수는 공백일 수 없습니다.",
+    NEGATIVE_ATTEMPTS: "[ERROR] : 시도 횟수는 양수만 가능합니다.",
+    NAN_ATTEMPTS: "[ERROR] : 시도 횟수는 숫자만 가능합니다.",
+    ZERO_ATTEMPTS: "[ERROR] : 시도 횟수는 0이 불가능합니다.",
+
+  }
+)
