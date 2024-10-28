5392e4f4e567c98bab9f09dc2a0699049487eb1c
YeWon Seo <syw1236@naver.com>
2024-10-28 21:56:30 +0900

    refactor(errorCarNameTest): 테스트 값 수정

--- __tests__/errorCarNameTest.js
+++ __tests__/errorCarNameTest.js
@@ -4,7 +4,7 @@ import { errorMessage } from '../src/constants';
 describe('Validator', () => {
     test('이름 에러 테스트', () => {
         // 각 자동차의 progress를 업데이트하는 함수를 호출
-        const inputCarName = ['one','','two'];
+        const inputCarName = '';
         expect(() => {
             Validator.isFiveOrLess(inputCarName);
         }).toThrow(new Error(errorMessage.isFiveOrMoreErrorMessage));
