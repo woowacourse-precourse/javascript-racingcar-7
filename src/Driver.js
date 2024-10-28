c3ff1c3da2cfeb5179cd8a71568a168f546510db
Hyeok Lee <c235335@g.hongik.ac.kr>
2024-10-28 21:26:17 +0900

    fix: 반복 오류 수정

--- src/Driver.js
+++ src/Driver.js
@@ -16,18 +16,27 @@ function ArrObjConvertor(list) {
   return resultObj;
 }
 
-function EachTimePrinter(score, number) {
+function EachTimePrinter(_score, number) {
+  let score = _score;
   if (number === 0) return score;
-  for (let car of score) {
-    score[car] += Random.pickNumberInRange(0, 9);
-    Console.print(`${car} : ${"-" * score[bar]}\n`);
+  for (const car in score) {
+    if (Random.pickNumberInRange(0, 9) > 3) score[car] += 1;
+    let output = "";
+    if (score[car] != 0) output = "-".repeat(score[car]);
+    Console.print(`${car} : ${output}\n`);
   }
   Console.print("\n");
+  return EachTimePrinter(score, number - 1);
 }
 
 function WinnerPrinter(score) {
   let max_score = 0;
-  for (let car of score) max_score = Math.max(score[car], max_score);
-  let winner = score.map((car) => score[car] == max_score);
+  for (const car in score) max_score = Math.max(score[car], max_score);
+
+  let winner = [];
+  for (const car in score) {
+    if (score[car] == max_score) winner.push(car);
+  }
+
   Console.print("최종 우승자 : " + winner.join(", "));
 }
