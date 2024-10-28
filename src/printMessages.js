dbfd4275c13c9b986d42ee7b82e81a27a086d8a8
hienjoy <wisdom16no@naver.com>
2024-10-28 11:21:23 +0900

    feat: 출력 문구 관리를 위한 printMessages 파일 추가

---
+++ src/printMessages.js
@@ -0,0 +1,9 @@
+const printMessages = {
+  askCarName: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+  askTryNumber: '시도할 횟수는 몇 회인가요?',
+  showRoundResults: '실행 결과',
+  announceWinner: '최종 우승자 : ',
+  newLine: '\n',
+};
+
+export default printMessages;
