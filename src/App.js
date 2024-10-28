50a78c83e57436351850191ad983613ea7fcf4f0
YEONSEO <yeonseochoi0717@gmail.com>
2024-10-28 16:06:27 +0900

    refactor: 필요 없는 주석 제거

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import { Console } from "@woowacourse/mission-utils";
-// Console.readLineAsync() / Console.print() / Random.pickNumberInRange()
 import Validate from "./Validate.js";
 import Race from "./Race.js";
 import Winner from "./Winner.js";
-
-// 사용자가 잘못된 값을 입력할 경우 "[ERROR]"로 시작하는 메시지와 함께 Error를 발생시킨 후 애플리케이션은 종료되어야 한다.
 
 class App {
   async run() {
