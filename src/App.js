d68f883ff167161cb9f7eefb23069ab51c407671
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-28 19:04:46 +0900

    ✨ feat(error): `Validator` 클래스 구현
    - validate 메서드를 통해 입력값 검증 기능을 추가할 예정입니다.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import { MESSAGE } from './constant';
 import Simulator from './Simulator.js';
 import { readUserInput } from './util/io.js';
 import splitByComma from './util/splitByComma';
+import Validator from './Validator';
 
 class App {
   async run() {
     const input = await readUserInput(MESSAGE.PROMPT_NAME_USER_INPUT);
     const cars = splitByComma(input);
-    const simulator = new Simulator(cars);
+    Validator.validate(cars);
+
     const attemptCount = await readUserInput(MESSAGE.PROMPT_COUNT_USER_INPUT);
+    Validator.validate(attemptCount);
+
+    const simulator = new Simulator(cars);
     for (let i = 0; i < attemptCount; i++) {
       simulator.simulate();
     }
---
+++ src/Validator.js
@@ -0,0 +1,5 @@
+class Validator {
+  static validate(value) {}
+}
+
+export default Validator;
