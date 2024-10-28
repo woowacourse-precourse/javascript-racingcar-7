0a07b27884e35adbbf1cd5f34a34c18d46fd6f9d
Apoliasm <syjsir@gmail.com>
2024-10-28 18:22:56 +0900

    TEST : Add some test codes
    - Execute boundary test when string length is 5
    - Test blank car name
    - Simplify 4th function code, attempt only one time

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -47,7 +47,7 @@ describe("자동차 경주", () => {
   });
 
 
-  test("1. 차 이름 입력 - 예외 테스트", async () => {
+  test("1. 차 이름 입력 - 경곗값 테스트 1 (다섯 글자 초과)", async () => {
     // given
     const inputs = ["pobi,javaji"];
     mockQuestions(inputs);
@@ -59,7 +59,85 @@ describe("자동차 경주", () => {
     await expect(app.run()).rejects.toThrow("[ERROR]");
   });
 
-  test("2. 시도 횟수 입력 - 예외 테스트", async ()=>{
+  test("1. 차 이름 입력 - 경곗값 테스트 2(딱 다섯 자 )", async () => {
+    
+    // given
+    const inputs = ["abcde,j", "1"];
+    const MOVING_FORWARD = 4;
+    const STOP = 3;
+    const logs = ["abcde : -", "j : ", "최종 우승자 : abcde"];
+    const logSpy = getLogSpy();
+ 
+    mockQuestions(inputs);
+    mockRandoms([MOVING_FORWARD, STOP]);
+ 
+    // when
+    const app = new App();
+    await app.run();
+ 
+    // then
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+   });
+
+  test("1. 차 이름 입력 - 공백 테스트 1 (공백 포함 다섯 자 초과)", async () => {
+    // given
+    const inputs = ["pobi,ja  va"];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("1. 차 이름 입력 - 공백 테스트 2 (공백 포함 다섯 자 이하)", async () => {
+    
+   // given
+   const inputs = ["fi ve,j", "1"];
+   const MOVING_FORWARD = 4;
+   const STOP = 3;
+   const logs = ["fi ve : -", "j : ", "최종 우승자 : fi ve"];
+   const logSpy = getLogSpy();
+
+   mockQuestions(inputs);
+   mockRandoms([MOVING_FORWARD, STOP]);
+
+   // when
+   const app = new App();
+   await app.run();
+
+   // then
+   logs.forEach((log) => {
+     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+   });
+  });
+
+  test("1. 차 이름 입력 - 공백 테스트 3 (공백만 나올 때)", async () => {
+    
+    // given
+    const inputs = [",java", "1"];
+    const MOVING_FORWARD = 4;
+    const STOP = 3;
+    const logs = [" : -", "java : ", "최종 우승자 : "];
+    const logSpy = getLogSpy();
+ 
+    mockQuestions(inputs);
+    mockRandoms([MOVING_FORWARD, STOP]);
+ 
+    // when
+    const app = new App();
+    await app.run();
+ 
+    // then
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+   });
+
+  test("2. 시도 횟수 입력 - 예외 테스트 (숫자 이외의 값이 입력될 때", async ()=>{
     //given
     const inputs = ["pobi,woni", "a"];
     mockQuestions(inputs);
@@ -97,13 +175,13 @@ describe("자동차 경주", () => {
   test("4. 우승자 출력 테스트 - 공통으로 있을 때", async () => {
     
     //given
-    const inputs = ["pobi,woni,kona", "2"];
+    const inputs = ["pobi,woni,kona", "1"];
     const FORWARD = 4 
     const STOP = 3
-    const logs = ["pobi : -","woni : ","kona : -","","pobi : --","woni : ","kona : --","최종 우승자 : pobi, kona"]
+    const logs = ["pobi : -","woni : ","kona : -","최종 우승자 : pobi, kona"]
     const logSpy = getLogSpy();
     mockQuestions(inputs);
-    mockRandoms([FORWARD,STOP,FORWARD,FORWARD,STOP,FORWARD])
+    mockRandoms([FORWARD,STOP,FORWARD])
 
     //when
     const app = new App();
