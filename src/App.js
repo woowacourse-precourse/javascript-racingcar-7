d703f481243953bd43910511a063ce7e44d8c1c4
thwlstn <thwlstn23@gmail.com>
2024-10-28 22:50:19 +0900

    feat : App.js 파일 작성
    - 구현한 기능들이 실제로 적용되도록
    - run() 메서드 안에 Race 인스턴스 생성한 뒤, 초기 설정(initializeGame), 경주 실행(runRace), 우승자 출력(printWinners) 순서로 전체 게임의 흐름을 관리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const Race = require('./Race');
+
 class App {
-  async run() {}
+    async run() {
+        const race = new Race();
+        await race.initializeGame(); // 게임 초기화
+        race.runRace(); // 경주 실행
+        race.printWinners(); // 최종 우승자 출력
+    }
 }
 
-export default App;
+const app = new App();
+app.run();
