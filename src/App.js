3feb7f0005ae5dbecbb60fee514da30b2b87967f
dev-dino22 <wlsskdud02@naver.com>
2024-10-28 23:01:26 +0900

    fix: modify code for test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import MainTest from './controllers/MainTest.js';
+import { StartMessage } from './view/ConsoleView.js';
+import { userInput } from './controllers/userInput.js';
+import gameController from './controllers/gameController.js';
+import { createCarList } from './utils/createCarList.js';
 
 class App {
   async run() {
     try {
-      await MainTest();
+      StartMessage();
+      const { carNames, parsedTryNumber } = await userInput();
+      const carList = createCarList(carNames);
+      gameController(carList, parsedTryNumber);
     } catch(error) {
       throw error;
     }
