9e0140e9b539f1de1aff657d9095188a7b1827f6
lecto17 <khwoo_rpsit@naver.com>
2024-10-28 16:21:59 +0900

    feat: 사용자 텍스트 입력 및 화면 출력 작업

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  INPUT_CAR_NAME =
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
+  INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?\n";
+
+  async getUserInput(message) {
+    return await Console.readLineAsync(message);
+  }
+  async showInput(message) {
+    return await Console.print(message);
+  }
+
+  async run() {
+    const names = await this.getUserInput(this.INPUT_CAR_NAME);
+    const tryCount = await this.getUserInput(this.INPUT_TRY_COUNT);
+  }
 }
 
 export default App;
