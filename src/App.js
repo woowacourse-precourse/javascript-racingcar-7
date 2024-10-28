7f3e15872f50587de0e250ed8247b0b4f271383f
alicehrk <81912837+alicehrk@users.noreply.github.com>
2024-10-28 23:41:58 +0900

    refactor: 입력 형식 검사 함수 분리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Console, MissionUtils } from "@woowacourse/mission-utils";
 
 function validateCarsInput(carsInput) {
-  if (carsInput.length === 0)
+  if (carsInput.length === 0) {
     throw new Error("[ERROR] 자동차를 1개 이상 입력하세요");
+  }
   const clearedInput = carsInput.split(",").map((car) => car.trim());
-  for (let i = 0; i < clearedInput.length; i++) {
-    if (!(clearedInput[i].length > 0 && clearedInput[i].length <= 5)) {
-      throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다");
-    }
+  clearedInput.forEach(validateCarName);
+}
+
+function validateCarName(car) {
+  if (!(car.length > 0 && car.length <= 5)) {
+    throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다");
   }
 }
 
