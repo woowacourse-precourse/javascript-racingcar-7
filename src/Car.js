0cc5714bf68e55f09f612f134e164b444cd371ea
hienjoy <wisdom16no@naver.com>
2024-10-28 12:25:37 +0900

    feat: 랜덤 값 생성 추가

--- src/Car.js
+++ src/Car.js
@@ -1,7 +1,17 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class Car {
   constructor(name) {
     this.name = name;
     this.dist = 0;
+  }
+
+  getRandomNumber() {
+    return MissionUtils.Random.pickNumberInRange(0, 9);
+  }
+
+  move() {
+    const randomNumber = this.getRandomNumber();
   }
 }
 
