37d699ffcf88601e4d3f628f408eb6da14e04ace
YeWon Seo <syw1236@naver.com>
2024-10-28 20:47:05 +0900

    refactor(AppController): 함수로 기능 구조 분할

--- src/AppController.js
+++ src/AppController.js
@@ -4,41 +4,77 @@ import Parser from './Parser.js';
 import CarController from './CarController.js';
 import ViewOutput from './ViewOutput.js';
 import Calculator from './Calculator.js';
+import Validator from './Validator.js';
 
 export default class AppController {
     carList = [];
 
     async control() {
-        const inputCarName = await Console.readLineAsync(gameMessage.inputCarNameMessage);
-        Parser.separateCarName(inputCarName);
-        
-        const inputTime = await Console.readLineAsync(gameMessage.inputTimeMessage);
+        const [carNameList, inputTime] = await this.userInput();
 
-        const carNameList = Parser.separateCarName(inputCarName);
+        this.createCar(carNameList);
 
-        this.carList = CarController.createCar(carNameList);
-
-        ViewOutput.printExecutionResult();
         this.playRacingCar(inputTime);
 
+        this.selectWinners();
+ 
+    }
+
+    async userInput() {
+        const inputCarName = await Console.readLineAsync(gameMessage.inputCarNameMessage);
+        const carNameList = this.checkCarName(inputCarName);
+
+        const inputTime = await Console.readLineAsync(gameMessage.inputTimeMessage);
+        this.checkTime(inputTime);
+
+        return [carNameList, inputTime];
+    }
+    
+    // 입력한 자동차 이름이 올바른지 체크
+    checkCarName(inputCarName) {
+        Validator.isEmpty(inputCarName);
+        const carNameList = Parser.separateCarName(inputCarName);
+        Validator.isFiveOrLess(carNameList);
+        return carNameList;
+    }
+
+    // 입력한 숫자가 올바른지 체크
+    checkTime(inputTime) {
+        Validator.checkInputTime(inputTime);
+    }
+
+    // 자동차 생성
+    createCar(carNameList) {
+        this.carList = CarController.createCar(carNameList);
+    }
+
+    // 자동차 게임 시작
+    playRacingCar(inputTime) {
+        ViewOutput.printExecutionResult();
+        this.repeatRace(inputTime);
+    }
+
+    // 레이스 반복
+    repeatRace(inputTime) {
+        for (let i = 0; i < inputTime; i++) {
+            this.moveCar();
+        }
+    }
+
+    // 자동차 이동
+    moveCar() {
+        CarController.upgradeCarProgress(this.carList);
+        ViewOutput.printProgressResult(this.carList);
+    }
+
+    // 우승자를 선정
+    selectWinners() {
         // 가장 큰 진행도 
         const maxProgress = Calculator.getMax(this.carList);
 
         // 우승자 선정 및 출력
         const winnerList = CarController.finalWinner(this.carList, maxProgress);
         ViewOutput.printWinner(winnerList); 
-    }
-
-    playRacingCar(inputTime) {
-        // 사용자가 입력한 횟수 만큼 게임 진행
-        for (let i = 0; i < inputTime; i++) {
-            this.moveCar();
-            ViewOutput.printProgressResult(this.carList);
-        }
-    }
-
-    moveCar() {
-        CarController.upgradeCarProgress(this.carList);
     }
 
 }
