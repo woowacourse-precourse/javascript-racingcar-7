47716f5845ca0887ac8451ce40103b7b26d416ca
kimzini <rlawlsdl11111@gmail.com>
2024-10-28 11:35:07 +0900

    feat: 추가 에러 메시지 정의

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,6 @@
 
 export const ERROR_MESSAGES = {
   INVALID_CAR_NAME: "[ERROR] 자동차 이름은 쉼표(,)로 구분하고, 각각 5자 이하로 입력해야 합니다.",
-  INVALID_RACE_COUNT: "[ERROR] 이동 횟수는 숫자로만 입력할 수 있습니다."
+  INVALID_RACE_COUNT: "[ERROR] 이동 횟수는 숫자로만 입력할 수 있습니다.",
+  INVALID_RACE_COUNT_ZERO: "[ERROR] 경주가 시작되지 않았습니다."
 };
