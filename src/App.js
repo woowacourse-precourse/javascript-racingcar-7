d1b2317f93aee1163ac5e43a3305cd5199744730
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2024-10-28 21:57:18 +0900

    rename: 함수명 통일성 있게 변경

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { Console } from '@woowacourse/mission-utils'
-import { NAMESTRValid, RUNTIMESValid } from './Valid.js';
+import { nameStrValid, runTimesValid } from './Valid.js';
 import { createCarArr } from './createCarArr.js';
+import { carRun } from './carRun.js';
 
 class App {
 
   async run() {
     const NAMESTR = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-    NAMESTRValid(NAMESTR)
+    nameStrValid(NAMESTR)
     const RUNTIMES = await Console.readLineAsync("시도할 횟수는 몇 회인가요?")
-    RUNTIMESValid(RUNTIMES)
-    createCarArr(NAMESTR)
+    runTimesValid(RUNTIMES)
+
+    carRun(createCarArr(NAMESTR), RUNTIMES)
   }
 
 }
