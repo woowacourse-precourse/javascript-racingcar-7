c2fc6b9195cc9cb100f7529359e4eef2f15c6d0a
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2024-10-28 23:24:46 +0900

    fix : 비동기 오류 해결

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,34 +1,22 @@
 import { Console } from '@woowacourse/mission-utils';
 import Validator from '../utils/Validator.js';
+import Constants from '../utils/constants.js';
 
 class InputView {
-  static readCarNames() {
-    return new Promise((resolve, reject) => {
-      Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)", (input) => {
-        try {
-          const carNames = input.split(',').map((name) => name.trim());
-          Validator.validateCarNames(carNames);
-          resolve(carNames);
-        } catch (error) {
-          Console.print(error.message);
-          reject(error);
-        }
-      });
-    });
+  static async readCarNames() {
+    try {
+      return await Console.readLineAsync(Constants.INPUT_MESSAGES.CAR_NAME_QUESTION);
+    } catch (error) {
+      throw new error(Constants.ERROR_MESSAGES.CAR_NAME_ERROR)
+    }
   }
 
-  static readAttemptCount() {
-    return new Promise((resolve, reject) => {
-      Console.readLineAsync("시도할 횟수는 몇 회인가요?", (input) => {
-        try {
-          const attempts = Validator.validateAttemptCount(parseInt(input, 10));
-          resolve(attempts);
-        } catch (error) {
-          Console.print(error.message);
-          reject(error);
-        }
-      });
-    });
+  static async readAttemptCount() {
+    try {
+      return await Console.readLineAsync(Constants.INPUT_MESSAGES.NUMBER_QUESTION);
+    } catch (error) {
+      throw new error(Constants.ERROR_MESSAGES.NUMBER_ERROR)
+    }
   }
 }
 
