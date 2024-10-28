12c60f71c93a98648ebcb9f9ab6804e3b1570976
mywhzy <cmj2393a@naver.com>
2024-10-28 17:26:15 +0900

    feat: 랜덤 수 구하기 기능 구현

--- src/Car.js
+++ src/Car.js
@@ -1,8 +1,14 @@
+import { Random } from '@woowacourse/mission-utils';
+
 class Car {
   #name;
 
   constructor(name) {
     this.#name = name;
+  }
+
+  getRandomNumber() {
+    return Random.pickNumberInRange(0, 9);
   }
 
   get name() {
