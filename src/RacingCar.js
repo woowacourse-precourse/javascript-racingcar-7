38fd30c7647b9d821026e594024749777effbbc2
ChangOKIM <kk104202@naver.com>
2024-10-28 15:10:53 +0900

    feat: startRace 메서드 구현

--- src/RacingCar.js
+++ src/RacingCar.js
@@ -5,6 +5,12 @@ class RacingCar {
         this.cars = carNames.map((name) => ({ name, position: 0 }));
     }
 
+    startRace(tryCount) {
+        for (let i = 0; i < tryCount; i++) {
+            this.runRound();
+        }
+    }
+
     moveCar(car) {
         const randomNumber = Random.pickNumberInRange(0, 9);
         if (this.shouldMove(randomNumber)) {
