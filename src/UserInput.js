224707c1abb4e552405f859a71b96ffaff379008
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2024-10-28 16:54:02 +0900

    feat: 사용자에게 경기를 위한 시도할 횟수 입력받기 구현

--- src/UserInput.js
+++ src/UserInput.js
@@ -2,7 +2,8 @@ import { Console } from "@woowacourse/mission-utils";
 
 class UserInput {
   static MESSAGES = {
-    carNamePrompt: "경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분)\n",
+    carNamePrompt: "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)\n",
+    attemptPrompt: "시도할 횟수는 몇 회인가요?\n",
   };
 
   async askCarNames() {
@@ -10,6 +11,9 @@ class UserInput {
       UserInput.MESSAGES.carNamePrompt
     );
     return carNames.split(",");
+  }
+  async askAttempts() {
+    return await Console.readLineAsync(UserInput.MESSAGES.attemptPrompt);
   }
 }
 
