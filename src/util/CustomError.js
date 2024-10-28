c141950bbf3c1d44586166a4a356102112cd641e
ChanLee_KR <cl700120@gmail.com>
2024-10-28 18:37:55 +0900

    refactor: error prefix 상수 분리

--- src/util/CustomError.js
+++ src/util/CustomError.js
@@ -1,9 +1,11 @@
+const ERROR_PREFIX = '[ERROR]';
+
 class CustomError extends Error {
   /**
    * @param {string} message
    */
   constructor(message) {
-    super(['[ERROR]', message].join(' '));
+    super([ERROR_PREFIX, message].join(' '));
   }
 }
 
