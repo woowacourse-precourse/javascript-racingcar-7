eab51ba4d1c232b9263870661f67f46ab421dbe0
luminox93 <memoji1236@gmail.com>
2024-10-28 15:21:02 +0900

    test: Car 클래스 테스트 수정
    1. 랜덤 난수 생성을 mocking하여 테스트 효율 증대
    2. 반복 사용되는 코드를 반복문을 통해서 간결하게 수정
    3. updateDistance가 호출하지 않은 상태에서 위치값이 변화하는지 검증

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,10 +1,20 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
 import Car from '../src/models/Car';
 
 describe('Car 클래스', () => {
   let car;
+  let mockRandomNumberGenerator;
 
   beforeEach(() => {
     car = new Car('pobi');
+    mockRandomNumberGenerator = jest.spyOn(
+      MissionUtils.Random,
+      'pickNumberInRange',
+    );
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   test('올바른 이름과 위치로 초기화해야 합니다', () => {
@@ -14,26 +24,54 @@ describe('Car 클래스', () => {
 
   describe('updateDistance 메서드', () => {
     test('randomNumber가 4보다 클 때 위치를 증가시켜야 합니다', () => {
-      car.updateDistance(5);
+      mockRandomNumberGenerator.mockReturnValueOnce(5);
+      car.updateDistance();
       expect(car.getDistance()).toBe(1);
     });
 
     test('randomNumber가 4일 때 위치를 증가시켜야 합니다', () => {
-      car.updateDistance(4);
+      mockRandomNumberGenerator.mockReturnValueOnce(4);
+      car.updateDistance();
       expect(car.getDistance()).toBe(1);
     });
 
     test('randomNumber가 4보다 작을 때 위치를 증가시키지 않아야 합니다', () => {
-      car.updateDistance(3);
+      mockRandomNumberGenerator.mockReturnValueOnce(3);
+      car.updateDistance();
       expect(car.getDistance()).toBe(0);
     });
 
     test('여러 번의 이동을 올바르게 처리해야 합니다', () => {
-      car.updateDistance(5);
-      car.updateDistance(4);
-      car.updateDistance(3);
-      car.updateDistance(6);
+      const number_list = [5, 4, 3, 6];
+      number_list.forEach((number) =>
+        mockRandomNumberGenerator.mockReturnValueOnce(number),
+      );
+      number_list.forEach(() => car.updateDistance());
+
+      // 예상되는 거리: 3 (5, 4, 6에서 이동, 3에서는 이동하지 않음)
       expect(car.getDistance()).toBe(3);
+    });
+
+    test('updateDistance를 호출하지 않으면 위치가 증가하지 않아야 합니다', () => {
+      expect(car.getDistance()).toBe(0);
+    });
+
+    test('랜덤 숫자가 항상 4보다 작으면 위치가 증가하지 않아야 합니다', () => {
+      const number_list = [1, 2, 3, 2, 1];
+      number_list.forEach((number) =>
+        mockRandomNumberGenerator.mockReturnValueOnce(number),
+      );
+      number_list.forEach(() => car.updateDistance());
+      expect(car.getDistance()).toBe(0);
+    });
+
+    test('랜덤 숫자가 항상 4 이상이면 위치가 그에 따라 증가해야 합니다', () => {
+      const number_list = [3, 4, 5, 6, 7, 8, 9];
+      number_list.forEach((number) =>
+        mockRandomNumberGenerator.mockReturnValueOnce(number),
+      );
+      number_list.forEach(() => car.updateDistance());
+      expect(car.getDistance()).toBe(6);
     });
   });
 });
