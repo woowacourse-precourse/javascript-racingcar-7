022f5258660d8cc71270d32226e7a0a60ee16f48
keemsebeen <sebin0580@gmail.com>
2024-10-28 13:25:00 +0900

    feat : 자동차 이름 유효성 검사 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { EXECUTION_RESULT, FINAL_WINNER, INPUT } from "./constants/race.js";
+import { simulateRace } from "./race/race.js";
+import { parseCarNames } from "./car/parseCarNames.js";
+import { attemptValidator } from "./validation/attemptValidator.js";
 import { getInput } from "./view/input.js";
+import { getOutput, getResult, getWinner } from "./view/output.js";
 
 class App {
   async run() {
     const carNamesInput = await getInput(INPUT.RACE_CAR_NAME);
+    const carNames = parseCarNames(carNamesInput);
   }
 }
 
