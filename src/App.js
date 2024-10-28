744068c2b2cd90eec48ae5a76bb45d34bebc7829
김나연 <apinksky00@gmail.com>
2024-10-28 15:05:15 +0900

    feat(store) : 입력값에 따라 자동차 자료구조에 저장

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
+class Car {
+  constructor(carName) {
+    if (carName.length > 5) {
+      throw new Error("[ERROR]");
+    }
+    this.name = carName;
+    this.position = 0;
+  }
+}
+
+class RacingGame {
+  constructor(carsNames) {
+    this.cars = carsNames.split(",").map((name) => new Car(name.trim()));
+  }
+}
+
 class App {
   async run() {
     try {
@@ -7,6 +23,7 @@ class App {
         "경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분)\n"
       );
       const playsNumber = await Console.readLineAsync("시도할 횟수\n");
+      const game = new RacingGame(carsNames);
     } catch (error) {
       throw new Error(error.message);
     }
