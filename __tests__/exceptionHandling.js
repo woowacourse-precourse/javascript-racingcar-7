f696149b3e467ebc5cf6cf7623f4e7916928d45c
jicho <jIcho6412@gmail.com>
2024-10-28 13:03:05 +0900

    test.each()를 사용하여 App.js 실행되는 동안 발생할 수 있는 예외 처리에 대한 테스트 기능 추가

--- __tests__/exceptionHandling.js
+++ __tests__/exceptionHandling.js
@@ -1,4 +1,27 @@
 import App from "../src/App.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
 
-describe("사용자에게 입력받은 값에 대한 예외 처리", () => {});
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+describe("예외 처리 테스트", () => {
+  test.each([
+    ["자동차 이름이 5자를 초과했을 경우", ["minimanimu,mi"]],
+    ["자동차 이름에 중복되는 이름이 있을 경우", ["mi,ni,mi"]],
+    ["자동차 이름이 공백일 경우", ["mi,,ni"]],
+    ["실행 횟수가 숫자가 아닐 경우", ["mi,ni", "apple"]],
+    ["실행 횟수가 공백일 경우", ["mi,ni", ""]],
+    ["실행 횟수가 0일 경우", ["mi,ni", "0"]],
+    ["실행 횟수가 음수일 경우", ["mi,ni", "-12"]],
+  ])("%s", async (tests, inputs) => {
+    mockQuestions(inputs);
+    const app = new App();
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+});
