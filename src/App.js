2c53b544d3b1d981c0b8ae01b8b15202c327f2e6
Hyori <hyoring030@gmail.com>
2024-10-28 21:11:01 +0900

    docs : update README

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import { CountName } from "./CountNameFunction.js";
 import { CountNumber } from "./CountNumberFunction.js";
-import { RandomNumbers } from "./Random.js";
 import { DrawRound } from "./DrawRound.js";
 
 class App {
@@ -9,18 +8,18 @@ class App {
     const inputName = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
+    const { names, count } = CountName(inputName);
     const inputCount = await MissionUtils.Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?"
     );
 
     try {
-      const { names, count } = CountName(inputName);
       const number = CountNumber(inputCount);
-      const result = DrawRound(names, number);
+      DrawRound(names, number);
+      
     } catch (error) {
       throw error;
     }
   }
 }
-
 export default App;
