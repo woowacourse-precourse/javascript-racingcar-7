757417c7552df046ba451bfaa8a370439003471e
PangKiYeon <pdrkeamy@gmail.com>
2024-10-28 22:44:41 +0900

    feat: 차가 한 대 일 때 에러 발생하는 기능 구현

--- src/handleError.js
+++ src/handleError.js
@@ -11,10 +11,10 @@ export function isCarNameEmpty(carNames) {
 }
 
 export function hasDuplicateNames(carNames) {
-    const uniqueNames = new Set(
+    const UNIQUE_NAMES = new Set(
         carNames.map((carName) => carName.toLowerCase())
     );
-    if (carNames.length !== uniqueNames.size) {
+    if (carNames.length !== UNIQUE_NAMES.size) {
         throw new Error(
             '[ERROR] 자동차 이름은 대소문자를 구분하지 않고 중복될 수 없습니다.'
         );
@@ -32,5 +32,13 @@ export function isTrialCountZero(trialCount) {
 export function validateTrial(trialCount) {
     if (!Number.isInteger(trialCount)) {
         throw new Error('[ERROR] 횟수는 자연수로 입력해야합니다.');
+    }
+}
+
+export function isOneCar(carNames) {
+    if (carNames.length == 1) {
+        throw new Error(
+            '[ERROR] 자동차가 2대 이상이어야 경주를 진행할 수 있습니다.'
+        );
     }
 }
