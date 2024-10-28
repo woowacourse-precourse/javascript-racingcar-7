14f4397957870d74a611cd57ab412dde8b9f900c
spoyodevelop <kjy9505@gmail.com>
2024-10-28 16:52:27 +0900

    refactor(checkDuplicate): checkDuplicate를 some 함수를 쓰게 수정

--- src/Util/checkDuplicate.js
+++ src/Util/checkDuplicate.js
@@ -1,11 +1,23 @@
-// 성능을 위해 찾는 즉시 리턴
+/**
+ * 배열에서 중복된 항목이 있는지 확인합니다.
+ * 중복이 발견되면 성능을 위해 즉시 `true`를 반환합니다.
+ *
+ * @function checkDuplicate
+ * @param {Array} arr - 중복 여부를 검사할 배열입니다.
+ * @returns {boolean} 중복된 항목이 있으면 `true`, 없으면 `false`를 반환합니다.
+ *
+ *
+ * @example
+ * checkDuplicate(['apple', 'banana', 'apple']); // true
+ * checkDuplicate(['apple', 'banana', 'orange']); // false
+ */
 export default function checkDuplicate(arr) {
   const seen = new Set();
-  for (const name of arr) {
+  return arr.some((name) => {
     if (seen.has(name)) {
       return true;
     }
     seen.add(name);
-  }
-  return false;
+    return false;
+  });
 }
