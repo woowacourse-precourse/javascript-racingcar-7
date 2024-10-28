7a7ad2fa393d052a11be3807fb7fe269bc349997
Yerim1234 <kongsun0805@gmail.com>
2024-10-28 21:29:53 +0900

    feat: 시도 횟수 입력 받기 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
-  static async run() {
-    await App.getCarNames();
+  async run() {
+    const carNames = await this.getCarNames();
+    const attemptCount = await this.getAttemptCount();
+    Console.print(
+      `자동차 이름: ${carNames.join(", ")} | 시도 횟수: ${attemptCount}`
+    );
+    // 이후 로직 추가
   }
 
-  static getCarNames() {
+  getCarNames() {
     return new Promise((resolve, reject) => {
       Console.readLine(
         "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ",
         (input) => {
           try {
-            const carNames = App.validateCarNames(input);
+            const carNames = this.validateCarNames(input);
             Console.print(`입력된 자동차 이름: ${carNames.join(", ")}`);
             resolve(carNames);
           } catch (error) {
@@ -23,15 +28,36 @@ class App {
     });
   }
 
-  static validateCarNames(input) {
+  validateCarNames(input) {
     const carNames = input.split(",").map((name) => name.trim());
     if (carNames.some((name) => name.length === 0 || name.length > 5)) {
       throw new Error("자동차 이름은 1자 이상 5자 이하로 입력해야 합니다.");
     }
     return carNames;
   }
-}
 
-App.run();
+  getAttemptCount() {
+    return new Promise((resolve, reject) => {
+      Console.readLine("시도할 횟수는 몇 회인가요?: ", (input) => {
+        try {
+          const attemptCount = this.validateAttemptCount(input);
+          Console.print(`입력된 시도 횟수: ${attemptCount}`);
+          resolve(attemptCount);
+        } catch (error) {
+          Console.print(`[ERROR] ${error.message}`);
+          reject(error);
+        }
+      });
+    });
+  }
+
+  validateAttemptCount(input) {
+    const attemptCount = parseInt(input, 10);
+    if (isNaN(attemptCount) || attemptCount <= 0) {
+      throw new Error("시도 횟수는 1 이상의 숫자여야 합니다.");
+    }
+    return attemptCount;
+  }
+}
 
 export default App;
