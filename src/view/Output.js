02c809838843c1520a8847a2cf095f67f36e94d3
songhaeunsong <gkdms3469@naver.com>
2024-10-28 21:22:52 +0900

    feat: 출력 함수 생성

--- src/view/Output.js
+++ src/view/Output.js
@@ -1,1 +1,13 @@
-export class Output {}
+import { Console } from "@woowacourse/mission-utils";
+
+export class Output {
+  printRound(cars) {
+    cars.forEach((car) => {
+      Console.print(`${car.getName()} : ${"-".repeat(car.getDistance())}`);
+    });
+  }
+
+  printWinners(winners) {
+    Console.print(`최종 우승자 : ${winners.join(", ")}`);
+  }
+}
