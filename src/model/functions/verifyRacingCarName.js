82d6839ce31f13728be49989ff9caaa0b7a14d2a
Dobbymin <gmin990624@gmail.com>
2024-10-28 16:10:35 +0900

    fix: 반복문 수정 및 포맷팅 수정

--- src/model/functions/verifyRacingCarName.js
+++ src/model/functions/verifyRacingCarName.js
@@ -4,13 +4,12 @@ export const verifyRacingCarName = async (cars) => {
   const carsArray = [];
   const carNamesSet = new Set();
 
-  for (const car of cars) {
+  cars.forEach((car) => {
     validateCarName(car, carNamesSet);
 
     carNamesSet.add(car);
-
     carsArray.push({ name: car, go: 0 });
-  }
+  });
 
   return carsArray;
 };
