0e1e9f026319f5b0d857ccebcac2ea39b72760ef
choisuyeon <stellasue@naver.com>
2024-10-28 17:08:31 +0900

    feat: Print guideline and Get a string

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { Random, Console } from "@woowacourse/mission-utils";
-// Random.pickNumberInRange() : Random 값 추출
-// Console.readLineAsync() : 사용자 값 입력
-// Console.print() : 사용자 값 출력
-
 // Random.pickNumberInRange(0, 9);   : 0에서 9까지의 정수 중 한 개의 정수 반환
+import { Random, Console } from "@woowacourse/mission-utils";
 
 class App {
-  async run() {}
+  async run() {
+    try {
+      // 경주할 자동차 이름(이름은 쉼표 기준으로 구분)
+      const line = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    } catch(error) {
+      throw new Error(`[ERROR]`);
+    }
+  }
 }
 
 export default App;
