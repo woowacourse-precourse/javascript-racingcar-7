cc5f6969f3320f7b787bba0bfca09de6703e62b3
BoRamSon <onbrson1@gmail.com>
2024-10-28 23:06:36 +0900

    feat : [4] validate for tryNumber
    1. 빈값일 경우 ERROR
    2. 에러값(NaN) or 숫자가 아닌 값일 경우 ERROR
    3. 과도하게 큰숫자 방지 (숫자 20 초과 시 ERROR)

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,14 +1,11 @@
+import { Console } from "@woowacourse/mission-utils";
+
 const validateCommaInLastArray = (isComma) => {
   const extractArrayFromCarName = isComma.split(/,/).map((carName) => {
     return carName.length;
   });
 
   if (extractArrayFromCarName[extractArrayFromCarName.length - 1] === 0) {
-    console.log(
-      `xxxxx 확인용 - 배열의 마지막자리 확인 : ${
-        extractArrayFromCarName[extractArrayFromCarName.length - 1]
-      }`
-    );
     return true;
   } else {
     return false;
@@ -22,10 +19,6 @@ const validateLimitStringLength = (isOverLength) => {
   const hasGreaterThenFive = limitLength.some((carNameLength) => {
     return carNameLength > 5;
   });
-  console.log(
-    `xxxxx 확인용 - 5글자 이상의 문자가 존재하는가? : ${hasGreaterThenFive}`
-  );
-
   return hasGreaterThenFive;
 };
 
@@ -35,24 +28,24 @@ const validateInputCarNames = (inputCarNames) => {
 
   if (inputCarNames == "") {
     inputCarNames = "";
-    console.log("[ERROR] 입력값이 없습니다. 다시 입력해주세요.");
+    Console.print("[ERROR] 입력값이 없습니다. 다시 입력해주세요.");
   } else if (inputCarNames === null || inputCarNames === undefined) {
     inputCarNames = "";
-    console.log("[ERROR] 에러가 발생하였습니다. 정상값을 다시 입력해주세요.");
+    Console.print("[ERROR] 에러가 발생하였습니다. 정상값을 다시 입력해주세요.");
   } else if (/[^\da-zA-Z,]/.test(inputCarNames)) {
     inputCarNames = "";
-    console.log(
+    Console.print(
       "[ERROR] 자동차의 영어이름과 콤마(,)만 입력될 수 있습니다. 다시 입력해주세요."
     );
   } else if (inputCarNames.startsWith(",")) {
     inputCarNames = "";
-    console.log("[ERROR] 콤마(,)가 먼저 입력될 수 없습니다. ");
+    Console.print("[ERROR] 콤마(,)가 먼저 입력될 수 없습니다. ");
   } else if (isCommaLastPosition) {
     inputCarNames = "";
-    console.log("[ERROR] 콤마(,)가 마지막으로 끝날 수 없습니다.");
+    Console.print("[ERROR] 콤마(,)가 마지막으로 끝날 수 없습니다.");
   } else if (isLimitLength) {
     inputCarNames = "";
-    console.log(
+    Console.print(
       "[ERROR] 입력하신 자동차 이름 중 5글자가 넘어가는 이름이 존재합니다. 다시 입력해주세요."
     );
   } else {
@@ -60,4 +53,19 @@ const validateInputCarNames = (inputCarNames) => {
   }
 };
 
-export { validateInputCarNames };
+const validateInputTryNum = (inputTryCount) => {
+  if (inputTryCount == "") {
+    inputTryCount = "";
+    Console.print("[ERROR] 값이 없습니다. 다시 입력해주세요.");
+  } else if (isNaN(inputTryCount) || !/^\d+$/.test(inputTryCount)) {
+    inputTryCount = "";
+    Console.print("[ERROR] 숫자가 아닙니다. 다시 입력해주세요");
+  } else if (inputTryCount > 20) {
+    inputTryCount = "";
+    Console.print("[ERROR] 과도한 숫자를 입력하셨습니다. 다시 입력해주세요.");
+  } else {
+    return true;
+  }
+};
+
+export { validateInputCarNames, validateInputTryNum };
