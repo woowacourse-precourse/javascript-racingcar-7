ea1d0a265ce28f5c2ca6aecfc0a48944af869628
j0n0m2 <dbsk8223@naver.com>
2024-10-28 14:29:17 +0900

    사용자로부터 자동차 이름을 입력 받는다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from "@woowacourse/mission-utils";
+import { Random } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const carName = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+    const carNames = this.Separator(carName);
+  }
+}
+
+Separator(carName) {
+  const carNames = carName.split(",");
 }
 
 export default App;
