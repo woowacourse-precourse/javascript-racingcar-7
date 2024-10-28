dd52c9763622fefec61e33be16489bc1c96c87bc
BoRamSon <onbrson1@gmail.com>
2024-10-28 22:57:39 +0900

    feat: [1] carNames 입력 받기
    - await MissionUtils.Console.readLineAsync()

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 class App {
-  async run() {}
+  async run() {
+    try {
+      let inputCarNames = String(
+        await MissionUtils.Console.readLineAsync(
+          "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n => "
+        )
+      ).replace(/\s+/g, "");
+      
+    } catch (error) {
+      Console.print(error.message);
+      throw error;
+    }
+  }
 }
 
 export default App;
