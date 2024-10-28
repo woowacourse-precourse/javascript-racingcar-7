ba23f90a0f18528042bd093ed34150b0f40e49b2
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-10-28 13:17:50 +0900

    feat: "실행 결과" 출력

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
+import { MESSAGES } from "./constants.js";
+
 import { getAttempts, getCarNames } from "./inputHandlers.js";
 class App {
   async run() {
     const cars = await getCarNames();
     const attemps = await getAttempts();
+    Console.print(MESSAGES.EXECUTION_RESULT);
   }
 }
 
--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,7 @@ export const MESSAGES = {
   CAR_NAME_INPUT:
     "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
   ATTEMPTS_INPUT: "시도할 횟수는 몇 회인가요?\n",
+  EXECUTION_RESULT: "\n실행 결과",
 };
 
 export const ERROR_MESSAGES = {
