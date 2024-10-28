1b7938b4a0d87f3f8c392405f5fe7763a7880598
ncb6206 <ncb6206@gmail.com>
2024-10-28 18:28:27 +0900

    Feat : 자동차 이름 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 class App {
-  async run() {}
+  inputCars() {
+    return MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+  }
+
+  async run() {
+    try {
+      const carsName = await this.inputCars();
+    } catch (error) {
+      throw new Error("[ERROR]");
+    }
+  }
 }
 
 export default App;
