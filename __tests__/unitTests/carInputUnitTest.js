eeb4f1e7fafe1fe4b7f6768262be1a1079cffb21
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-10-28 14:33:45 +0900

    test : processCar 메서드 통과 테스트 추가

--- __tests__/unitTests/carInputUnitTest.js
+++ __tests__/unitTests/carInputUnitTest.js
@@ -10,7 +10,7 @@ const mockQuestions = (inputs) => {
   });
 };
 
-describe('자동차 입력 에러테스트', () => {
+describe('자동차 입력 유닛테스트', () => {
   const CAR_ERROR_CASES = [
     ['자동차 입력길이 5 초과', ['carrrrr,car'], CAR_ERROR.NOT_OVER_5],
     ['자동차의 개수 2 이하', ['car1'], CAR_ERROR.NOT_UNDER_2],
@@ -19,13 +19,26 @@ describe('자동차 입력 에러테스트', () => {
   ];
 
   test.each(CAR_ERROR_CASES)(
-    '(%s)',
+    'processCars 메서드 에러 테스트',
     async (testName, inputArray, errorMessage) => {
       mockQuestions(inputArray);
 
       const race = new Race();
 
       await expect(race.processCars()).rejects.toThrow(errorMessage);
+    }
+  );
+
+  const CAR_PASS_CASES = [[['car1,car2']], [['car1,car3, car4']]];
+
+  test.each(CAR_PASS_CASES)(
+    'processCars 메서드 통과 테스트',
+    async (inputArray) => {
+      mockQuestions(inputArray);
+
+      const race = new Race();
+
+      await expect(race.processCars()).resolves.not.toThrow();
     }
   );
 });
