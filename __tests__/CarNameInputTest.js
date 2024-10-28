54eebcb7e5eba82abb2f296fadbe4fcc5c616076
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-10-28 11:37:40 +0900

    feat: 쉼표로만 구분 되어 있는지 확인

--- __tests__/CarNameInputTest.js
+++ __tests__/CarNameInputTest.js
@@ -1,5 +1,6 @@
 import { getCarNames } from "../src/inputHandlers.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
+import { MESSAGES, ERROR_MESSAGES } from "../src/constants.js";
 
 const mockConsoleInput = (input) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -10,15 +11,18 @@ describe("자동차 이름 검사", () => {
   test("자동차 이름 길이가 5자 이하인지 검사", async () => {
     mockConsoleInput("pobi,woni,testerrorexample");
 
-    await expect(getCarNames()).rejects.toThrow(
-      "[ERROR] 자동차 이름은 1자 이상 5자 이하야 합니다."
-    );
+    await expect(getCarNames()).rejects.toThrow(ERROR_MESSAGES.CAR_NAME_LENGTH);
   });
   test("자동차 이름이 공백인 경우 검사", async () => {
     mockConsoleInput("pobi, ,woni");
 
+    await expect(getCarNames()).rejects.toThrow(ERROR_MESSAGES.CAR_NAME_LENGTH);
+  });
+  test("자동차 이름에 유효하지 않은 구분 기호가 포함된 경우 검사", async () => {
+    mockConsoleInput("철수;영희:짱구");
+
     await expect(getCarNames()).rejects.toThrow(
-      "[ERROR] 자동차 이름은 1자 이상 5자 이하야 합니다."
+      ERROR_MESSAGES.INVALID_SEPARATOR
     );
   });
 });
--- src/validator.js
+++ src/validator.js
@@ -5,15 +5,15 @@ export function validateCarNames(names) {
     throw new Error(ERROR_MESSAGES.CAR_NAME_LENGTH);
   }
 
-  names.forEach((name) => {
-    if (name.length > 5) {
-      throw new Error(ERROR_MESSAGES.CAR_NAME_LENGTH);
-    }
-  });
-
   const inputString = names.join(",");
   const invalidSeparator = /[^가-힣a-zA-Z0-9,]/.test(inputString);
   if (invalidSeparator) {
     throw new Error(ERROR_MESSAGES.INVALID_SEPARATOR);
   }
+
+  names.forEach((name) => {
+    if (name.length > 5) {
+      throw new Error(ERROR_MESSAGES.CAR_NAME_LENGTH);
+    }
+  });
 }
