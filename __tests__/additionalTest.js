4e0d1e37b4a9dbf7a718fcfdc4c85a4c2cb26669
kimyou1102 <yoosk5485@naver.com>
2024-10-28 18:58:54 +0900

    test: each로 예외 처리 테스트 케이스 묶기

--- __tests__/additionalTest.js
+++ __tests__/additionalTest.js
@@ -1,24 +1,11 @@
 import { validateName, validateTryCount } from '../src/validation';
 
 describe('자동차 경주', () => {
-  test('예외처리 테스트', async () => {
-    // given
-    const names = ['asdfgh', 'hjk'];
-
-    // then
-    await expect(() => validateName(names)).toThrow(
-      '[ERROR] 자동차 이름은 5자 이하만 가능합니다.'
-    );
-  });
-
-  test('함수 테스트', async () => {
-    // given
-    const names = ['asdf', ''];
-
-    // then
-    await expect(() => validateName(names)).toThrow(
-      '[ERROR] 자동차 이름은 1자 이상이어야 합니다.'
-    );
+  test.each([
+    [['asdfgh', 'hjk'], '[ERROR] 자동차 이름은 5자 이하만 가능합니다.'],
+    [['asdf', ''], '[ERROR] 자동차 이름은 1자 이상이어야 합니다.'],
+  ])('자동차 이름이  경우 예외처리', async (names, expectedError) => {
+    await expect(() => validateName(names)).toThrow(expectedError);
   });
 
   test.each([
