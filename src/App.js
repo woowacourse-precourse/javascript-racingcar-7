2ca13fbabb1ad6679a210599670fdc561dd3c813
tjdgus2308 <tjdgus2308@gmail.com>
2024-10-28 17:42:01 +0900

    feat: 사용자 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    // 사용자 입력 구현
+    const carNames = await this.getCarNames();
+    const attempts = await this.getAttempts();
+
+  }
+
+  async getCarNames() {
+    const input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)로 구분)\n");
+    const names = input.split(",").map((name) => name.trim());
+
+    return names;
+  }
+
+  async getAttempts() {
+    const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    const attempts = Number(input);
+
+    return attempts;
+  }
 }
 
 export default App;
