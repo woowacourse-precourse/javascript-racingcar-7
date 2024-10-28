111f9507f38837fc3d1309159bca3d0993bb0ab7
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-10-28 11:35:52 +0900

    test: getRoundCountInput 함수 테스트
    - 입력한 시도 횟수가 숫자로 반환되는지 테스트

--- __tests__/inputHandlerTest.js
+++ __tests__/inputHandlerTest.js
@@ -19,3 +19,16 @@ describe('getNameInput 함수 테스트', () => {
     );
   });
 });
+
+describe('getRoundCountInput 함수 테스트', () => {
+  test('사용자가 입력한 시도 횟수를 숫자로 반환하는지 확인', async () => {
+    Console.readLineAsync.mockResolvedValueOnce('5');
+
+    const result = await getRoundCountInput();
+
+    expect(result).toBe(5);
+    expect(Console.readLineAsync).toHaveBeenCalledWith(
+      '시도할 횟수는 몇 회인가요?\n'
+    );
+  });
+});
