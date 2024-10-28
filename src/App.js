93f761c989aa26d54aaf9083d4955fffde4f9ee3
inji0212 <in17100170@kakao.com>
2024-10-28 20:15:45 +0900

    feat 자동차,시도횟수 형식에 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-class App {
-  async run() {}
-}
+import { MissionUtils } from '@woowacourse/mission-utils';
+//[x]  1. 자동차,시도횟수 형식에 입력받기 (4.)
+//[]  2. 잘못된 값 입력시 ERROR 발생시키기 (8.)
+//[]  3. 이름이 5자 이하인 자동차 구분하기 (3.)
+//[]  4. 랜덤한 전진 조건 정하기 (5. 1.)
+//[]  5. 전진 조건에 맞게 출력하기 (2.)
+//[]  6. 시도횟수만큼 출력후 마지막 출력의 -을 비교하기 (6.)
+//[] 7. 최대 값의 자동차 이름들 출력하기 (6. 7.)
 
+class App {
+  async run() {
+    try {
+      const carList = await MissionUtils.Console.readLineAsync(
+        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+      );
+      const tyyNumber =
+        await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+
+      MissionUtils.Console.print(`차: ${carList}`);
+      MissionUtils.Console.print(`횟수: ${tyyNumber}`);
+    } catch (error) {
+      // reject 되는 경우
+      MissionUtils.Console.print('[ERROR]');
+    }
+  }
+}
 export default App;
