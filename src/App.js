61f2afd46760e137ab5d2a7d812aae5e9f9c8072
Lim-shweety <smile70200@naver.com>
2024-10-28 21:02:03 +0900

    feat(App.js) : 자동차 이름 예외 처리
    이름의 길이 5글자 초과일 때
    이름이 중복 될 때

--- src/App.js
+++ src/App.js
@@ -3,9 +3,23 @@ import { Console } from "@woowacourse/mission-utils"
 class App {
   async run() {
     let inputNames = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-    const NAMES = inputNames.split(',');
+    let NameTemp = inputNames.replace(/ /g, '');
+    const NAMES = NameTemp.split(',');
+    await this.checkName(NAMES);
     let times = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
     times = Number(times);
+  }
+
+  async checkName(NAMES) {
+    const SET_NAMES = new Set(NAMES);
+    if (SET_NAMES.size !== NAMES.length) {
+      throw new Error("[ERROR] 중복되는 자동차 이름이 존재합니다.");
+    }
+    for (let i = 0; i < NAMES.length; i++) {
+      if (NAMES[i].length > 5) {
+        throw new Error("[ERROR] 자동차 이름이 5글자 초과입니다.");
+      }
+    }
   }
 }
 
