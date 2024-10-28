cca9abc2c6f84811f6f9feead9546a400cf042b4
elect10 <wlgnsakswp@naver.com>
2024-10-28 16:49:18 +0900

    test(io): get name less than five

--- src/io/GetInput.js
+++ src/io/GetInput.js
@@ -3,6 +3,9 @@ import { Console } from '@woowacourse/mission-utils';
 class GetInput {
   static async getCarNames() {
     const carNames = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n');
+    if(carNames.length > 5) {
+      throw new Error('[ERROR] : 자동차 이름은 5자 이하로 입력해주세요.');
+    }
     return carNames.split(',');
   }
 
