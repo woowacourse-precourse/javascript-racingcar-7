5e8ed9556bdcca3f8955c237a0e8d575e7db066a
Dobbymin <gmin990624@gmail.com>
2024-10-28 16:10:17 +0900

    fix: 조건문 함수로 분리 및 코드 포맷팅 수정

--- src/model/functions/validateCarName.js
+++ src/model/functions/validateCarName.js
@@ -1,19 +1,32 @@
 import { ERROR } from '../../constants/constants.js';
 
-export const validateCarName = (car, carNamesSet) => {
-  if (!isNaN(Number(car))) {
+const validateCarNameIsString = (car) => {
+  if (!Number.isNaN(Number(car))) {
     throw new Error(ERROR.CAR_NAME_STRING);
   }
+};
 
+const validateCarNameIsNotEmpty = (car) => {
   if (car.length === 0) {
     throw new Error(ERROR.CAR_NAME_EMPTY);
   }
+};
 
+const validateCarNameLength = (car) => {
   if (car.length > 5) {
     throw new Error(ERROR.CAR_NAME_INPUT);
   }
+};
 
+const validateCarNameIsUnique = (car, carNamesSet) => {
   if (carNamesSet.has(car)) {
     throw new Error(ERROR.CAR_NAME_DUPLICATE);
   }
+};
+
+export const validateCarName = (car, carNamesSet) => {
+  validateCarNameIsString(car);
+  validateCarNameIsNotEmpty(car);
+  validateCarNameLength(car);
+  validateCarNameIsUnique(car, carNamesSet);
 };
