862e9c87d900974e64e483e29e3027c636a27277
Oliy <74090200+eunsoA@users.noreply.github.com>
2024-10-28 19:34:58 +0900

    [style] getCarNames() 들여쓰기, 출력시 \n 추가
    * 들여쓰기 잘못됨 수정
    * 입력요청문 출력 시 \n 추가

--- src/App.js
+++ src/App.js
@@ -20,16 +20,15 @@ class App {
     }
   }
 
-    // 사용자로부터 자동차 이름을 입력받는 함수
-    async getCarNames() {
-      const input = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ');
-      const carNames = input.split(',').map((name) => name.trim());
-      if (carNames.some((name) => name.length > 5 || name.length === 0)) {
-        throw new Error('자동차 이름은 1자 이상 5자 이하로 입력해주세요.');
-      }
-      return carNames;
+  // 사용자로부터 자동차 이름을 입력받는 함수
+  async getCarNames() {
+    const input = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const carNames = input.split(',').map((name) => name.trim());
+    if (carNames.some((name) => name.length > 5 || name.length === 0)) {
+      throw new Error('자동차 이름은 1자 이상 5자 이하로 입력해주세요.');
     }
-
+    return carNames;
+  }
 }
 
 export default App;
