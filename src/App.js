177c8356fac79a230974a42dd8c530d4f816998a
main <0ksuyeon0@gmail.com>
2024-10-28 22:11:43 +0900

    feat: 자동차 경주 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
+import getCarList from "./utils/getCarList.js";
+import getCount from "./utils/getCount.js";
+import setRandom from "./utils/setRandom.js";
+import { printFinalWinners, printRacing } from "./utils/printRacing.js";
+
 class App {
-  async run() {}
+  async run() {
+    const carList = await getCarList();
+    const count = await getCount();
+
+    setRandom(carList, count);
+  }
 }
 
 export default App;
