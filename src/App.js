800e09f3ffc4e0b63f8e43e3dffbd76ec1d434f3
LeeJinyoung218 <amy021899@gmail.com>
2024-10-28 17:45:09 +0900

    feat: 입력 받은 자동차 이름으로 Car 클래스 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { getCarsInput, getMoveCountInput } from "./handlers/IOHandler.js";
+import Car from "./models/Car.js";
 
 class App {
   async run() {
     try {
       const carNames = await getCarsInput();
       const moveCount = await getMoveCountInput();
+
+      const cars = carNames.map(name => new Car(name));
     } catch (err) {
       throw err;
     }
