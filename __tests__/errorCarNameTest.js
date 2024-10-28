be4798974ddd054cd68c530b86221f1f36ba3f54
YeWon Seo <syw1236@naver.com>
2024-10-28 22:12:43 +0900

    test(errorCarNameTest): 자동차 이름과 숫자에 대해 테스트 추가 진행

--- __tests__/errorCarNameTest.js
+++ __tests__/errorCarNameTest.js
@@ -2,12 +2,31 @@ import Validator from '../src/utils/Validator'
 import { errorMessage } from '../src/constants';
 
 describe('Validator', () => {
-    test('이름 에러 테스트', () => {
-        // 각 자동차의 progress를 업데이트하는 함수를 호출
+    test('빈 이름 에러 테스트', () => {
         const inputCarName = '';
         expect(() => {
             Validator.isFiveOrLess(inputCarName);
         }).toThrow(new Error(errorMessage.isFiveOrMoreErrorMessage));
+    
+    })
+})
+
+describe('Validator', () => {
+    test('5글자 이상 이름 에러 테스트', () => {
+        const inputCarName = 'himynameis';
+        expect(() => {
+            Validator.isFiveOrLess(inputCarName);
+        }).toThrow(new Error(errorMessage.isFiveOrMoreErrorMessage));
+    
+    })
+})
+
+describe('Validator', () => {
+    test('숫자가 0일 경우 에러 테스트', () => {
+        const inputTime = '0';
+        expect(() => {
+            Validator.checkInputTime(inputTime);
+        }).toThrow(new Error(errorMessage.isZeroErrorMessage));
     
     })
 })
