aa84ff41c87032c93a355b99dd539f1159e63323
MinGu Jeong <mingu830@gmail.com>
2024-10-28 09:06:00 +0900

    feat: 메서드 조립

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import * as run from './RacingCar.js';
 class App {
-  async run() {}
+  async run() {
+    const cars = await run.getCarName();
+    const tryCount = await run.getTryCount();
+    for (let i = 0; i < tryCount; i++) {
+      await run.moveCar(cars);
+      run.printCarPosition(cars);
+    }
+    run.printWinner(cars);
+  }
 }
 
 export default App;
