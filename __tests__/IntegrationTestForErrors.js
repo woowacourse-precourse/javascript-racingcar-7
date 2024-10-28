35247f2587018f227314f1216bb74d0ed553eb26
Jinny <ggab.challenge@gmail.com>
2024-10-28 19:32:27 +0900

    Test(integration): 자동차 이름 입력시 빈 값이면 에러를 던지는 기능 테스트 코드 생성
    - integrationTestForErros.js 파일 생성

---
+++ __tests__/IntegrationTestForErrors.js
@@ -0,0 +1,20 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import App from '../src/App.js';
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+describe('자동차 이름 입력 받기', () => {
+  test('자동차 이름 입력 값이 빈 값이면 에러를 던진다.', async () => {
+    const carNamesInput = [''];
+    mockQuestions(carNamesInput);
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow('[ERROR]');
+  });
+});
