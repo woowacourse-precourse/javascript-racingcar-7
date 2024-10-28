1f41967e6155df7c38ddd0ea94e29576b56dd90d
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-10-28 20:30:04 +0900

    test: 테스트 케이스 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -24,11 +24,12 @@ const getLogSpy = () => {
   return logSpy;
 };
 
-describe("자동차 경주", () => {
-  test("기능 테스트", async () => {
+describe("정상 케이스", () => {
+  const MOVING_FORWARD = 4;
+  const STOP = 3;
+
+  test("단독 우승 테스트", async () => {
     // given
-    const MOVING_FORWARD = 4;
-    const STOP = 3;
     const inputs = ["pobi,woni", "1"];
     const logs = ["pobi : -", "woni : ", "최종 우승자 : pobi"];
     const logSpy = getLogSpy();
@@ -46,9 +47,171 @@ describe("자동차 경주", () => {
     });
   });
 
-  test("예외 테스트", async () => {
+  test("공동 우승 테스트", async () => {
+    // given
+    const inputs = ["pobi,woni,jun", "2"];
+    const logs = [
+      "pobi : --",
+      "woni : --",
+      "jun : -",
+      "최종 우승자 : pobi, woni",
+    ];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      STOP,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+    ]);
+
+    // when
+    const app = new App();
+    await app.run();
+
+    // then
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test("자동차 이름에 공백 또는 숫자, 특수문자 포함 시 테스트", async () => {
+    // given
+    const inputs = ["po bi,woni!,123", "1"];
+    const logs = ["po bi : ", "woni! : -", "123 : ", "최종 우승자 : woni!"];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([STOP, MOVING_FORWARD, STOP]);
+
+    // when
+    const app = new App();
+    await app.run();
+
+    // then
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test("자동차 이름을 쉼표+공백(, )로 구분하여 입력 시 테스트", async () => {
+    // given
+    const inputs = ["pobi, woni", "3"];
+    const logs = ["pobi : ---", "woni : -", "최종 우승자 : pobi"];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      STOP,
+      MOVING_FORWARD,
+      STOP,
+    ]);
+
+    // when
+    const app = new App();
+    await app.run();
+
+    // then
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+});
+
+describe("에러 케이스", () => {
+  test("자동차 이름 5자 초과 시 테스트", async () => {
     // given
     const inputs = ["pobi,javaji"];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("자동차 이름 중복 시 테스트", async () => {
+    // given
+    const inputs = ["pobi,pobi"];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("자동차 이름 미입력 시 테스트", async () => {
+    // given
+    const inputs = [""];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("경주 횟수가 음수일 시 테스트", async () => {
+    // given
+    const inputs = ["pobi,woni", "-1"];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("경주 횟수가 0일 시 테스트", async () => {
+    // given
+    const inputs = ["pobi,woni", "0"];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("경주 횟수가 소수일 시 테스트", async () => {
+    // given
+    const inputs = ["pobi,woni", "1.5"];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("경주 횟수가 숫자가 아닐 시 테스트", async () => {
+    // given
+    const inputs = ["pobi,woni", "a"];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("경주 횟수 미입력 시 테스트", async () => {
+    // given
+    const inputs = ["pobi,woni", ""];
     mockQuestions(inputs);
 
     // when
