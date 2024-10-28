ce16e8c37528ddd49be252cab9912cf9f108e083
kochungwon <yud01063@gmail.com>
2024-10-28 10:06:44 +0900

    fix(유효성 검사): 입력 값 검증 로직 수정
    - 자동차 이름 및 시도 횟수 입력 시 비동기 함수라서  입력을 기다리지 않던 부분을 async await 구문추가했습니다.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import UserInput from './UserInput.js'
-import RacingSession from './RacingSession.js'
+import UserInput from './UserInput.js';
+import RacingSession from './RacingSession.js';
 
 class App {
   async run() {
     const userInput = new UserInput();
-    userInput.inputCarNames();
-    userInput.inputAttempts();
+    await userInput.inputCarNames();
+    await userInput.inputAttempts();
     userInput.validateInput();
   
     const racingSession = new RacingSession(userInput.carNames, userInput.attempts);
