4b96f792f0ee441084a50fcec96bc30e78222ea4
keemsebeen <sebin0580@gmail.com>
2024-10-28 13:18:56 +0900

    feat : 자동차 이름이 5글자가 넘어가는지 확인하는 함수 구현

--- src/validation/validators.js
+++ src/validation/validators.js
@@ -1,5 +1,6 @@
 import { CHAR_REGEX } from "../constants/charRegex.js";
 import { COMMA } from "../constants/delimiter.js";
+import { CAR_NAME } from "../constants/error.js";
 
 export const isEmptyCarName = (carNames) => {
   return !carNames;
@@ -20,4 +21,8 @@ export const hasSpecialCharacters = (carNames) => {
 export const hasDuplicateNames = (carNameArr) => {
   const uniqueCarSet = new Set(carNameArr);
   return uniqueCarSet.size !== carNameArr.length;
+};
+
+export const isInvalidNameLength = (carNameArr) => {
+  return carNameArr.some((name) => name.length > CAR_NAME.MAX);
 };
