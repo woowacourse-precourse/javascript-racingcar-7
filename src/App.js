56c2a7087c17ffe58a476ba23121f4022de47499
alicehrk <81912837+alicehrk@users.noreply.github.com>
2024-10-28 16:02:03 +0900

    feat: 자동차 이름 입력 및 입력 검증 - 사용자로부터 쉼표로 구분된 자동차 이름을 입력받습니다 - 이때 공백은 무시합니다(trim으로 제거) - 자동차를 1개 이상 입력하지 않거나, 6자 이상의 잘못된 입력이 들어오면 Error를 발생시키고 즉시 애플리케이션을 종료합니다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Console } from "@woowacourse/mission-utils";
+
+function validateCarsInput(carsInput) {
+  if (carsInput.length === 0)
+    throw new Error("[ERROR] 자동차를 1개 이상 입력하세요");
+  const clearedInput = carsInput.split(",").map((car) => car.trim());
+  for (let i = 0; i < clearedInput.length; i++) {
+    if (!(clearedInput[i].length > 0 && clearedInput[i].length <= 5)) {
+      throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다");
+    }
+  }
+}
+
 class App {
-  async run() {}
+  async run() {
+    const cars = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    validateCarsInput(cars);
+  }
 }
 
 export default App;
