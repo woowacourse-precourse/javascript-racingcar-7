6fd43c9c95af63842e137c72e9a20597508db595
yewonhihi <yewon1118@naver.com>
2024-10-28 23:17:18 +0900

    feat(App.js): 무작위 값으로 전진 여부 판별 & 게임 결과 출력

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Random, Console } from "@woowacourse/mission-utils";
+import { Random, Console, MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
@@ -6,21 +6,44 @@ class App {
     const inputCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
 
     let carsList = this.getCarList(inputCars);
+
+    console.log("\n실행 결과")
+    for (let i = 0; i < inputCount; i++) {
+      this.tryRacing(carsList);
+    }
   }
 
   getCarList(str) {
     const cars = str.split(",");
 
-    if (!cars.every(car => car.length <= 5)) {
-      this.throwError('자동차 이름은 5자 이하만 가능합니다.')
+    if (!cars.every((car) => car.length <= 5)) {
+      this.throwError("자동차 이름은 5자 이하만 가능합니다.");
     }
 
     let carList = {};
-    cars.forEach(car => {
-      carList[`${car}`] = 0
+    cars.forEach((car) => {
+      carList[`${car}`] = 0;
     });
 
     return carList;
+  }
+
+  tryRacing(cars) {
+    for (let key in cars) {
+      let randomInteger = Random.pickNumberInRange(0, 9);
+      if (randomInteger >= 4) {
+        cars[key] += 1;
+      }
+    }
+
+    this.printGameResult(cars);
+  }
+
+  printGameResult(cars) {
+    for (const [key, value] of Object.entries(cars)) {
+      console.log(`${key} : ${"-".repeat(value)}`);
+    }
+    console.log("/n");
   }
 
   throwError(message) {
