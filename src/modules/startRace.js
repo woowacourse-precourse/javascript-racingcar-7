9e9f28b80d88b0e3bc01a15aa532ed4d732808d7
Jin-Chanyong <cksdyd324@gmail.com>
2024-10-28 22:19:58 +0900

    feat: 경주 우승자를의 배열을 반환하는 함수 구현
    startRace.js 파일에 이하 내용 추가:
    - 경주 참가자들의 최대 distance 를 구하여, 이를 통해 우승자들의 이름 배열을 반환하는 getWinners 함수 구현

--- src/modules/startRace.js
+++ src/modules/startRace.js
@@ -15,3 +15,13 @@ function viewProgressBar(car) {
 
   Console.print(`${carName} : ${progressBar}`);
 }
+
+function getWinners(cars) {
+  const maxDistance = Math.max(...cars.map((car) => car.distance));
+
+  const winners = cars
+    .filter((car) => car.position === maxDistance)
+    .map((car) => car.name);
+
+  return winners;
+}
