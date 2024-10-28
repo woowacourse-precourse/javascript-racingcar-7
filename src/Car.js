07e14318f76243aa041ad0e25cb840e13255acce
YeWon Seo <syw1236@naver.com>
2024-10-28 18:16:27 +0900

    fix(Car): 진행도 증가 방법 수정

--- src/Car.js
+++ src/Car.js
@@ -9,7 +9,7 @@ export default class Car {
         return this.progress;
     }
 
-    updateProgress(progress) {
-        this.progress += progress;
+    updateProgress() {
+        this.progress ++;
     }
 }
