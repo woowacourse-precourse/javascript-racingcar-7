4fabec6a33993df4f3f117d7acda295e8ad6acbc
ljs831 <ljsljs2580@gmail.com>
2024-10-28 21:26:29 +0900

    refactor : Game 클래스 생성

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import { Console } from "@woowacourse/mission-utils";
 import IOHandler from "./IOHandler.js";
-import Utils from "./Utils.js";
+import Game from "./Game.js";
 
 class App {
   constructor() {
     this.cars = [];
     this.IOHandler = new IOHandler();
-    this.Utils = new Utils();
+    this.Game = new Game();
   }
   async run() {
     const inputNames = await this.IOHandler.getNameInput();
     const count = await this.IOHandler.getCountInput();
 
-    this.cars = this.Utils.runRacing(count, inputNames);
-
-    const winner = this.Utils.getWinner(this.cars);
+    const winner = this.Game.runRacing(count, inputNames);
 
     Console.print(`최종 우승자 : ${winner}`);
   }
--- src/Utils.js
+++ src/Utils.js
@@ -7,19 +7,6 @@ class Utils {
     return names.map((name) => new Car(name));
   }
 
-  runRacing(count, inputNames) {
-    let cars = this.makeCarObj(inputNames);
-    Console.print("\n실행결과");
-    for (let i = 0; i < count; i++) {
-      cars.forEach((car) => {
-        car.moveRandomDistance();
-        car.printStatus();
-      });
-      Console.print("\n");
-    }
-    return cars;
-  }
-
   getWinner(cars) {
     let winner = [cars[0]];
     for (let i = 1; i < cars.length; i++) {
