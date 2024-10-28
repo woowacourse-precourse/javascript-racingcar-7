8e49fa1c9735430ea1a3d02266ee6dabd312a26d
songhaeunsong <gkdms3469@naver.com>
2024-10-28 21:16:12 +0900

    feat: 자동차 이름 및 시도 횟수 입력 함수 호출

--- src/controller/GameController.js
+++ src/controller/GameController.js
@@ -1,1 +1,23 @@
-export class GameController {}
+import { Validator } from "../utils/Validator.js";
+import { ErrorView } from "../view/ErrorView.js";
+import { Input } from "../view/Input.js";
+import { Output } from "../view/Output.js";
+
+export class GameController {
+  async playGame() {
+    const input = new Input();
+    const output = new Output();
+    const validator = new Validator();
+    const errorView = new ErrorView();
+
+    try {
+      const carNames = await input.getCarNames();
+      validator.validateCarNames(carNames);
+
+      const round = await input.getRound();
+      validator.validateRound(round);
+    } catch (error) {
+      errorView.printError(error.message);
+    }
+  }
+}
