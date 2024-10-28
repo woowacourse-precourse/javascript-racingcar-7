b0f64df80d35d63c19ea24a6959d955fa6ada7bd
김지환 (Jihwan Kim) <kjhwan0802@naver.com>
2024-10-28 09:07:19 +0000

    feat: 출력 부분 구현
    - 입력 받기
    - 경주 단계별 실행 결과
    - 최종 우승자

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const carNameString = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    //TODO: validateName check
+    const playNum = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    //TODO: isNum or validateNum check
+
+    const racingCars = carNameString.split(",").map((carName) => {
+      return { name: carName, score: 0 };
+    });
+
+    Console.print("\n실행 결과");
+    for (let i = 0; i < playNum; i++) {
+      racingCars.map((car) => {
+        this.randomForward(car);
+        this.printRacingcarInfo(car);
+      });
+      Console.print("\n");
+    }
+
+    Console.print(
+      `최종 우승자 : ${this.whoiswinner(racingCars)
+        .map((car) => car.name)
+        .join(", ")}`
+    );
+  }
+
+
 }
 
 export default App;
