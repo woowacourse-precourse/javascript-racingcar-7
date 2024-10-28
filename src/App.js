ac5d7a8395ba4051bccb6436b61c38f9ced7fb03
choisuyeon <stellasue@naver.com>
2024-10-28 17:11:19 +0900

    feat: Occur Error if car name exceeds 5 characters

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ class App {
       // 경주할 자동차 이름(이름은 쉼표 기준으로 구분)
       const line = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
       const cars = line.split(',');
-      
+
       // 시도할 횟수
       let tryNum = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+
+      if (cars.some(car => car.length > 5))
+        throw new Error("[ERROR]");
     } catch(error) {
       throw new Error(`[ERROR]`);
     }
