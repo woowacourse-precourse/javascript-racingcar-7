0df428188724f56764e6a98ca16ebc6ecdb2b56f
조승우 <choseung97@gmail.com>
2024-10-28 15:33:59 +0900

    test(input): Input 클래스 테스트를 추가한다

---
+++ __tests__/inputTest.js
@@ -0,0 +1,22 @@
+import { Console } from '@woowacourse/mission-utils';
+import Input from '../src/components/Input.js';
+
+describe('Input 클래스 테스트', () => {
+  beforeAll(() => {
+    Console.readLineAsync = jest.fn();
+  });
+
+  test('자동차 이름과 반복 횟수를 올바르게 입력받아 반환한다.', async () => {
+    const mockNames = 'car1,car2,car3';
+    const mockRepetition = '5';
+
+    Console.readLineAsync
+      .mockResolvedValueOnce(mockNames)
+      .mockResolvedValueOnce(mockRepetition);
+
+    const result = await Input.get();
+
+    expect(result.names).toBe(mockNames);
+    expect(result.repetitionString).toBe(mockRepetition);
+  });
+});
