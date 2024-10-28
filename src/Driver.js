497e36c0a9011a0bfa39f3599bedb0fa5c95b9e5
Hyeok Lee <c235335@g.hongik.ac.kr>
2024-10-28 21:12:42 +0900

    feat: 랜덤 전진 구현, 진행도 출력

--- src/Driver.js
+++ src/Driver.js
@@ -2,11 +2,24 @@ import { Random, Console } from "@woowacourse/mission-utils";
 
 export function Driver(nameList, number) {
   let score = ArrObjConvertor(nameList);
+
+  Console.print(RESULT_TITLE);
+  score = EachTimePrinter(score, number);
 }
+
 function ArrObjConvertor(list) {
   let resultObj = {};
   list.forEach((element) => {
     resultObj[element] = 0;
   });
   return resultObj;
+}
+
+function EachTimePrinter(score, number) {
+  if (number === 0) return score;
+  for (let car of score) {
+    score[car] += Random.pickNumberInRange(0, 9);
+    Console.print(`${car} : ${PROGRESS_BAR * score[bar]}\n`);
+  }
+  Console.print("\n");
 }
