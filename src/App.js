8e3c15323248247577c4f205c0d8994f8fe7e546
Lim-shweety <smile70200@naver.com>
2024-10-28 21:07:43 +0900

    feat(App.js) : 입력 함수로 처리
    차이름 입력, 시도할 횟수 입력 함수로 분리

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import { Console } from "@woowacourse/mission-utils"
 
 class App {
   async run() {
+    const NAMES = await this.getNames();
+    const TIMES = await this.getTime();
+  }
+
+  async getNames() {
     let inputNames = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     let NameTemp = inputNames.replace(/ /g, '');
     const NAMES = NameTemp.split(',');
     await this.checkName(NAMES);
+    return NAMES;
+  }
+
+  async getTime() {
     let times = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
     times = Number(times);
+    return times;
   }
 
   async checkName(NAMES) {
