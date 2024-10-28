afceaf8af47537966d46b527223df0f287b7eda4
HW-K <rlaql99@gmail.com>
2024-10-28 14:27:25 +0900

    Implement method to input car list for race
    - Prompt user to input a comma-separated list of car names
    - Throw error if input is empty
    - Define Exception function to handle validation errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-class App {
-  async run() {}
-}
+import { Console, Random } from "@woowacourse/mission-utils";
 
-export default App;
+class App {
+  Exception(condition, message) {
+    if (condition) throw new Error(message);
+  }
+
+  async getNames() {
+    Console.print(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+    const namesInput = await Console.readLineAsync("");
+    this.Exception(namesInput === "", "[ERROR]");
+    return namesInput;
+  }
+}
