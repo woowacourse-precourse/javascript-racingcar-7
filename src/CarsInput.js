43367f7408bbe9e0c129db75b96617eed6856a1d
zzzzzuuuuu <agu0830@naver.com>
2024-10-28 15:03:42 +0900

    fix: 중복 이름 받았을 시 에러 처리 추가

--- src/CarsInput.js
+++ src/CarsInput.js
@@ -4,8 +4,9 @@ export class CarsInput {
   async getInput() {
     Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분")
     const input = await Console.readLineAsync("");
-    const cars = input.split(",").map(car => car.trim());
+    let cars = input.split(",").map(car => car.trim());
 
+    cars = [...new Set(cars)];
     this.validateCarNames(cars);
 
     return cars;
