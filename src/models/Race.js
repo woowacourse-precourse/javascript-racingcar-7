caa1d6cd833433ee60aab1dd49f0f8d8096a8174
JINWOOKIM <rlawlsdn9583@gmail.com>
2024-10-28 16:13:02 +0900

    feat: 레이스 중간 및 최종 결과 출력 기능 구현

--- src/models/Race.js
+++ src/models/Race.js
@@ -1,3 +1,5 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class Race {
   #cars;
   #attempts;
@@ -11,7 +13,27 @@ class Race {
   start() {
     for (let i = 0; i < this.#attempts; i++) {
       this.#cars.forEach((car) => car.move());
+      this.printRaceStatus();
     }
+    this.printWinners();
+  }
+
+  // 경주 중간 결과를 출력
+  printRaceStatus() {
+    this.#cars.forEach((car) => {
+      Console.print(`${car.getName()} : ${"-".repeat(car.getPosition())}`);
+    });
+    Console.print("");
+  }
+
+  // 경주 최종 결과를 출력
+  printWinners() {
+    const maxPosition = Math.max(...this.#cars.map((car) => car.getPosition()));
+    const winners = this.#cars.filter(
+      (car) => car.getPosition() === maxPosition
+    );
+    const winnerNames = winners.map((car) => car.getName()).join(", ");
+    Console.print(`최종 우승자 : ${winnerNames}`);
   }
 }
 
