d5a89bf0c013e717b4a0d1d4257f4d0d1faf5528
nerdchanii <nerdchanii@gmail.com>
2024-10-28 19:43:41 +0900

    feat(controller): implement controller for RaceGame

---
+++ src/controller/RaceGameController.js
@@ -0,0 +1,47 @@
+import parser from "../utils/parser.js";
+import { carNameValidatePipe } from '../utils/validationPipe.js'
+class RaceGameController {
+  constructor(view, service) {
+    this.view = view;
+    this.service = service;
+    this.view.setController(this);
+  }
+  
+  async run(){
+    const carnames = await this.view.getCarNames();
+    const iteration = await this.view.getIteration();
+    this.service.ready(carnames, iteration, this.onRoundEnd.bind(this), this.onBeforeEnd.bind(this));
+    this.service.start();
+    const { winners } = this.service.getResult();
+    this.view.showWinner(winners);
+  }
+
+  onGetRaceCarNames(carNamesString){
+    if(!carNamesString) {
+      throw new Error('[ERROR] 입력이 잘못되었습니다.')
+    }
+    carNameValidatePipe(carNamesString);
+    return parser.parseCarName(carNamesString);
+  }
+
+  onGetIteration(iterationString){
+  if(!iterationString)
+    throw new Error('[ERROR] 입력이 잘못 되었어요!');
+  if(isNaN(iterationString)){
+    throw new Error('[ERROR] 입력된 반복 횟수가 숫자가 아니에요!');
+  }
+  
+  return parser.parseIteration(iterationString);
+  }
+
+  onRoundEnd(raceGameStatus){
+    const { cars } = raceGameStatus;
+    this.view.showStatus(cars);
+  }
+
+  onBeforeEnd(){
+    this.view.out('\n실행 결과');
+  }
+}
+
+export default RaceGameController;
