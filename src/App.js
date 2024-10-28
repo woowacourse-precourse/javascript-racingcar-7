6004d4ea8e3ff21bef2bc44a5fd48c2dde3aa885
Doi Kim <154426332+d0ikim@users.noreply.github.com>
2024-10-28 23:25:02 +0900

    refactor: 가독성 개선 및 상수 추가

--- src/App.js
+++ src/App.js
@@ -2,75 +2,77 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 const SEPARATOR = ",";
 const MAXIMUM_CARNAME_THRESHOLD = 5;
 const MINIMUM_MOVE_THRESHOLD = 4;
-const ERROR_NO_SEPARATOR =
+
+const ERROR_MESSAGE_NO_SEPARATOR =
   "[ERROR] 쉼표(,) 구분자가 없습니다. 프로그램을 종료합니다.";
-const ERROR_LONG_CARNAME =
+const ERROR_MESSAGE_LONG_CARNAME =
   "[ERROR] 구분자는 있으나, 자동차 이름은 5자 이하만 가능합니다. 프로그램을 종료합니다.";
-const ERROR_INVALID_CARNAME =
+const ERROR_MESSAGE_INVALID_CARNAME =
   "[ERROR] 자동차 이름은 문자만 가능합니다. 프로그램을 종료합니다.";
-const ERROR_NOT_NUMBER =
+const ERROR_MESSAGE_NOT_NUMBER =
   "[ERROR] 숫자가 아닌 값을 입력하셨습니다. 프로그램을 종료합니다.";
-const ERROR_INVALID_ROUND_COUNT =
+const ERROR_MESSAGE_INVALID_ROUND_COUNT =
   "[ERROR] 시도 횟수는 1회 이상이어야 합니다. 프로그램을 종료합니다.";
+
+const GUIDE_MESSAGE_ASKING_CAR_NAMES =
+  "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로)\n";
+const GUIDE_MESSAGE_ASKING_ROUND_COUNT = "시도할 횟수는 몇 회인가요?\n";
+const GUIDE_MESSAGE_RESULT = "\n실행 결과";
 
 class App {
   async run() {
     let carNamesString = await MissionUtils.Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로)\n"
+      GUIDE_MESSAGE_ASKING_CAR_NAMES
     );
+
     if (!carNamesString.includes(SEPARATOR)) {
-      throw new Error(ERROR_NO_SEPARATOR);
+      throw new Error(ERROR_MESSAGE_NO_SEPARATOR);
     }
+
     let carNames = carNamesString.split(SEPARATOR);
-
-    if (!this.hasLongCarName(carNames)) {
-      throw new Error(ERROR_LONG_CARNAME);
+    if (this.hasLongCarName(carNames)) {
+      throw new Error(ERROR_MESSAGE_LONG_CARNAME);
     }
-
     if (this.hasCarNameStartingWithNumber(carNames)) {
-      throw new Error(ERROR_INVALID_CARNAME);
+      throw new Error(ERROR_MESSAGE_INVALID_CARNAME);
     }
 
     let roundCountString = await MissionUtils.Console.readLineAsync(
-      "시도할 횟수는 몇 회인가요?\n"
+      GUIDE_MESSAGE_ASKING_ROUND_COUNT
     );
-
     if (isNaN(roundCountString)) {
-      throw new Error(ERROR_NOT_NUMBER);
+      throw new Error(ERROR_MESSAGE_NOT_NUMBER);
     }
-
     let roundCount = Number(roundCountString);
     if (roundCount <= 0) {
-      throw new Error(ERROR_INVALID_ROUND_COUNT);
+      throw new Error(ERROR_MESSAGE_INVALID_ROUND_COUNT);
     }
 
-    const carsMap = this.createCarPositionMap(carNames);
+    const cars = this.createCarsWithPosition(carNames);
 
-    MissionUtils.Console.print("\n실행 결과");
-
+    MissionUtils.Console.print(GUIDE_MESSAGE_RESULT);
     for (let i = 0; i < roundCount; i++) {
-      this.moveOneStepRandomly(carsMap);
-
-      this.printOneRound(carsMap);
-
-      MissionUtils.Console.print("");
+      for (const car of cars) {
+        this.moveOneStepRandomly(
+          car,
+          MissionUtils.Random.pickNumberInRange(0, 9)
+        );
+      }
+      this.printOneRound(cars);
     }
-
-    this.printWinners(this.getWinners(carsMap)); // 우승자 가려내기
-    // 우승자 출력하기
+    this.printWinners(this.getWinners(cars));
   }
 
-  getWinners(carsMap) {
-    // 최종 우승자 계산 함수
-    const winners = []; // 공동우승자를 저장할 배열
-    let maxPosition = -1; // ?? 얘는 뭐지? 꼭 필요한가??? (보류)
+  getWinners(cars) {
+    const winners = [];
+    let maxPosition = -1;
 
-    for (const car of carsMap) {
+    for (const car of cars) {
       if (car.position > maxPosition) {
-        maxPosition = car.position; // 가장 고점인 애가 나타나면 maxPosition을 갱신함
-        winners.length = 0; // 새로운 우승자를 발견하면 기존 우승자 배열을 초기화
-        winners.push(car); // 새로운 우승자 정보를 차례대로 배열에 집어넣음
-      } else if (car.position === maxPosition) winners.push(car); // 공동우승자 추가
+        maxPosition = car.position;
+        winners.length = 0;
+        winners.push(car);
+      } else if (car.position === maxPosition) winners.push(car);
     }
     return winners;
   }
@@ -84,27 +86,25 @@ class App {
     }
   }
 
-  printOneRound(carsMap) {
-    for (const car of carsMap) {
+  printOneRound(cars) {
+    for (const car of cars) {
       const positionOutput = "-".repeat(car.position);
       MissionUtils.Console.print(`${car.name} : ${positionOutput}`);
     }
+    MissionUtils.Console.print("");
   }
 
-  moveOneStepRandomly(carsMap) {
-    for (const car of carsMap) {
-      const shouldMove =
-        MissionUtils.Random.pickNumberInRange(0, 9) >= MINIMUM_MOVE_THRESHOLD;
-      if (shouldMove) car.position++;
-    }
+  moveOneStepRandomly(car, random_number) {
+    const shouldMove = random_number >= MINIMUM_MOVE_THRESHOLD;
+    if (shouldMove) car.position++;
   }
 
-  createCarPositionMap(carNames) {
+  createCarsWithPosition(carNames) {
     return carNames.map((name) => ({ name, position: 0 }));
   }
 
   hasLongCarName(carNames) {
-    return carNames.every((name) => name.length <= MAXIMUM_CARNAME_THRESHOLD);
+    return carNames.some((name) => name.length > MAXIMUM_CARNAME_THRESHOLD);
   }
 
   hasCarNameStartingWithNumber(carNames) {
