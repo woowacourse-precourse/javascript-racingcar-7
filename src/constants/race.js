7cd58fe59683420af3d1cd861e2712b8e05bdfd0
keemsebeen <sebin0580@gmail.com>
2024-10-28 14:14:09 +0900

    feat : 자동차 경주 constants 분리

--- src/constants/race.js
+++ src/constants/race.js
@@ -3,3 +3,10 @@ export const INPUT = {
     "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
   ATTEMPT_COUNT_PROMPT: "시도할 횟수는 몇 회인가요?\n",
 };
+
+export const RACE_MOVE = {
+  MIN: 0,
+  MAX: 9,
+  PROGRESS: 4,
+  FORWARD: "-",
+};
