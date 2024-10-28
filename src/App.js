d6c75445d5ea2783e73a1669c27cb30a87018ce1
Glyph8 <newdy0808@gmail.com>
2024-10-28 15:30:40 +0900

    feat(carPrize) 이동종료 후 결과 출력
    최종 이동 후 이동성공 횟수 최대자동차(하나 이상) 우승자로 출력기능추가.

--- src/App.js
+++ src/App.js
@@ -18,6 +18,27 @@ function moveJudge(carNames, carRacing){
     })
 }
 
+function carPrize(carNames, carRacing){
+    let maxMove = 0;
+    let winnerList = "";
+
+    carRacing.map((race, index)=>{
+        const movement = race.length;
+        if(movement >= maxMove){
+            maxMove = movement;
+        }
+    });
+
+    carNames.map((car, index)=>{
+        const movement = carRacing[index].length;
+        if(movement === maxMove){
+            winnerList += carNames[index] + ", ";
+        }
+    });
+    const winner = winnerList.slice(0, winnerList.length-2);
+    Console.print(`최종 우승자 : ${winner}`);
+}
+
 function printCar(carNames, carRacing){
     carNames.map((car, index)=>{
         Console.print(`${car} : ${carRacing[index]}`);
@@ -28,15 +49,11 @@ function printCar(carNames, carRacing){
 class App {
   async run() {
     Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-
     const carNamesInput = await Console.readLineAsync("");
-
     const carNames = carNamesInput.split(",");
     checkCarName(carNames);
 
-    //let carRacing = [""];
     let carRacing = Array.from({length: carNames.length}, () => "");
-
     Console.print("시도할 횟수는 몇 회인가요?");
     const moveCount = await Console.readLineAsync("");
 
@@ -45,8 +62,7 @@ class App {
         moveJudge(carNames, carRacing);
         printCar(carNames, carRacing);
     }
-
-    Console.print("최종 우승자 : ");
+    carPrize(carNames,carRacing);
   }
 }
 
