84c5f512da601879d87e24c46536600c4cd5bbfd
Xen-alpha <hky7548@naver.com>
2024-10-28 21:06:32 +0900

    feat(ESLint): add linter settings
    * fix indent width to 2
    * fix some line break in formatOutput.js

--- src/formatOutput.js
+++ src/formatOutput.js
@@ -1,31 +1,26 @@
-import { MissionUtils } from '@woowacourse/mission-utils'
-const Console = MissionUtils.Console;
-const Random = MissionUtils.Random;
-
 export function formatOutput(resultArray) {
-  let result = '실행 결과\n';
+  let result = "실행 결과\n";
   // Get progress output
   resultArray.forEach((value) => {
     Object.keys(value).forEach((v) => {
-      const progress = new Array(value[v]).fill('-')
-        .join('');
+      const progress = new Array(value[v]).fill("-").join("");
       result += `${v} : ${progress}\n`;
     });
-    result += '\n';
+    result += "\n";
   });
   // Get winnner(s)
   const winner = Object.entries(resultArray.pop())
     .sort((a, b) => b[1] - a[1])
     .reduce((prev, curr) => {
       if (prev.length === 0) {
-          prev.push(curr);
+        prev.push(curr);
       } else if (prev.length > 0 && curr[1] === prev[0][1]) {
-          prev.push(curr);
+        prev.push(curr);
       }
       return prev;
     }, [])
     .map((value) => value[0])
-    .join(', ');
+    .join(", ");
   result += `최종 우승자 : ${winner}`;
   return result;
 }
--- src/parseInput.js
+++ src/parseInput.js
@@ -1,19 +1,15 @@
-import errorMessage from './ErrorMessage';
-
-import { MissionUtils } from '@woowacourse/mission-utils'
-const Console = MissionUtils.Console;
-const Random = MissionUtils.Random;
-
+import errorMessage from "./ErrorMessage";
 
 export function parsePlayers(inputstring) {
-    if (inputstring === '') throw new Error(errorMessage.NO_PLAYER)
-    const resultArray = inputstring.split(',');
-    if ( resultArray.some(( value ) => value.length > 5 )) throw new Error(errorMessage.NAME_IS_TOO_LONG);
-    return resultArray;
+  if (inputstring === "") throw new Error(errorMessage.NO_PLAYER);
+  const resultArray = inputstring.split(",");
+  if (resultArray.some((value) => value.length > 5))
+    throw new Error(errorMessage.NAME_IS_TOO_LONG);
+  return resultArray;
 }
 export function parseRound(inputstring) {
-    const inputNumber = parseInt(inputstring);
-    if (isNaN( inputNumber )) throw new Error(errorMessage.NOT_A_NUMBER);
-    else if (inputNumber < 0) throw new Error(errorMessage.NEGATIVE_NUMBER);
-    return inputNumber;
+  const inputNumber = parseInt(inputstring);
+  if (isNaN(inputNumber)) throw new Error(errorMessage.NOT_A_NUMBER);
+  else if (inputNumber < 0) throw new Error(errorMessage.NEGATIVE_NUMBER);
+  return inputNumber;
 }
