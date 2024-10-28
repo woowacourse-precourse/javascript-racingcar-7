88d9bfa5e8c4ef8af1d8d85ac49a4488054efb1c
dongmin <dongmin11566@gmail.com>
2024-10-28 20:30:58 +0900

    feat: 우승자 결정 및 출력
    가장 많이 이동한 자동차를 찾고 여러 명일 경우 쉼표로 구분하여 우승자를 출력하는 기능입니다.

--- src/Race.js
+++ src/Race.js
@@ -1,5 +1,5 @@
 import Car from "./Car.js";
-import { Random, Console } from "@woowacourse/mission-utils/src";
+import { Random, Console } from "@woowacourse/mission-utils";
 
 class Race {
     constructor(carNames, raceAttempts) {
@@ -22,9 +22,25 @@ class Race {
 
     printRoundResults() {
         this.cars.forEach(car => {
-            Console.print(`${car.getName} : ${'-'.repeat(car.getPosition())}`);
+            Console.print(`${car.getName()} : ${'-'.repeat(car.getPosition())}`);
         })
         Console.print('');
+    }
+
+    printWinners() {
+        const maxPosition = Math.max(...this.cars.map(car => car.getPosition()));
+        const winners = this.cars
+            .filter(car => car.getPosition() === maxPosition)
+            .map(car => car.getName());
+        Console.print(`최종 우승자 : ${winners.join(', ')}`);
+    }
+
+    startRace() {
+        Console.print('\n실행 결과');
+        for (let i = 0; i< this.raceAttempts; i++) {
+            this.runRound();
+        }
+        this.printWinners();
     }
 }
 
