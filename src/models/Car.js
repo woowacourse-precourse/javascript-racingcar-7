0b5ebaafb364481f6bfcfe54a929849bf1925b3d
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2024-10-28 20:51:07 +0900

    feat : 자동차 객체 생성

--- src/models/Car.js
+++ src/models/Car.js
@@ -1,5 +1,18 @@
 class Car {
+  constructor(name) {
+    this.name = name.trim;
+    this.position = 0;
+  }
 
+  move(randomNumber) {
+    if (randomNumber >= 4) {
+      this.position++;
+    }
+  }
+
+  getPosition() {
+    return  this.position;
+  }
 }
 
 export default Car;
