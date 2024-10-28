5df37770a396419b165d6a0975c7e98321b9bddb
CJY <cjy3458@naver.com>
2024-10-28 16:40:44 +0900

    feat: 자동차 이름 빈 문자열일 시 제거 기능 구현

--- src/utils/getCarNames.js
+++ src/utils/getCarNames.js
@@ -6,7 +6,10 @@ const getCarNames = async () => {
     '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
   );
 
-  const carNames = carInput.split(',').map((name) => name.trim());
+  const carNames = carInput
+    .split(',')
+    .map((name) => name.trim())
+    .filter((name) => name.length > 0);
 
   validateCarNames(carNames);
   return carNames;
