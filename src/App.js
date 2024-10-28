c694ccffc274f622b9d0fa766960123039a5ac5d
YSW2 <jrgyber@gmail.com>
2024-10-28 18:32:40 +0900

    feat(print_race): 경주 결과 출력 구현
    - 각 경주 진행마다 과정 출력
    - 경주 종료 후 우승자 탐색
    - 우승자 출력

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
 import { Console, Random } from '@woowacourse/mission-utils';
 
 class App {
+  constructor() {
+    this.name_list = [];
+    this.race_result = [];
+    this.try_count = 0;
+  }
+
   parse_input(input) {
     const result = input.split(',').map((str) => str.trim());
     return result;
   }
+
+  validate_name(names) {
+    names.forEach((name) => {
+      if (name.trim() === '' || name.trim().length > 5) {
+        throw new Error('[ERROR]');
+      }
+    });
+    return true;
+  }
+
   get_random_number() {
     return Random.pickNumberInRange(0, 9);
   }
-  start_race(race_result) {
-    for (let i = 0; i < race_result.length; i++) {
-      if (this.get_random_number() >= 4) {
-        race_result[i] += 1;
-      }
-    }
+
+  get_race_winner() {
+    const max_value = Math.max(...this.race_result);
+    return this.name_list.filter(
+      (_, index) => this.race_result[index] === max_value
+    );
   }
+
+  print_race_process() {
+    this.name_list.forEach((name, index) => {
+      Console.print(`${name} : ${'-'.repeat(this.race_result[index])}`);
+    });
+    Console.print('');
+  }
+
+  start_race() {
+    this.race_result.forEach((_, index) => {
+      if (this.get_random_number() >= 4) {
+        this.race_result[index] += 1;
+      }
+    });
+  }
+
   async run() {
     const input_names = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
@@ -23,13 +55,22 @@ class App {
       '시도할 횟수는 몇 회인가요?\n'
     );
 
-    const name_list = this.parse_input(input_names);
-    const race_result = new Array(name_list.length).fill(0);
+    const parsed_names = this.parse_input(input_names);
+    if (this.validate_name(parsed_names)) {
+      this.name_list = parsed_names;
+    }
+    this.race_result = new Array(this.name_list.length).fill(0);
+    this.try_count = Number(input_number);
+
+    Console.print('실행 결과');
 
     for (let repeat = 0; repeat < input_number; repeat++) {
-      this.start_race(race_result);
+      this.start_race();
+      this.print_race_process();
     }
-    Console.print(race_result);
+
+    const winner = this.get_race_winner();
+    Console.print(`최종 우승자 : ${winner.join(', ')}`);
   }
 }
 
