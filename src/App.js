052b258c28b666557c0af0a2ac3f626d11f04a65
Heesoo Jun <acesk123@gmail.com>
2024-10-28 21:22:37 +0900

    fix: 입력값 유효성 검사 및 에러 처리 추가

--- src/App.js
+++ src/App.js
@@ -18,6 +18,21 @@ class App {
     this.runRace();
   }
 
+  // 자동차 이름 입력 받기
+  async promptCarNames() {
+    return new Promise((resolve) => {
+      Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n').then(resolve);
+    });
+  }
+
+  // 시도할 횟수 입력 받기
+  async promptRounds() {
+    return new Promise((resolve) => {
+      Console.readLineAsync('시도할 횟수는 몇 회인가요?\n').then(resolve);
+    });
+  }
+
+  // 입력받은 자동차 이름 설정
   setCars(input) {
     const names = input.split(',');
     if (this.validateNames(names)) {
@@ -27,18 +42,12 @@ class App {
     }
   }
 
-  async promptCarNames() {
-    return new Promise((resolve) => {
-      Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n').then(resolve);
-    });
+  // 이름 유효성 검사
+  validateNames(names) {
+    return names.every((name) => name.length <= 5);
   }
 
-  async promptRounds() {
-    return new Promise((resolve) => {
-      Console.readLineAsync('시도할 횟수는 몇 회인가요?\n').then(resolve);
-    });
-  }
-
+  // 시도할 횟수 설정
   setRounds(input) {
     const rounds = parseInt(input, 10);
     if (isNaN(rounds) || rounds <= 0) {
@@ -47,6 +56,7 @@ class App {
     this.rounds = rounds;
   }
 
+  // 레이스 시작
   runRace() {
     const race = new Race(this.cars, this.rounds);
     race.start();
