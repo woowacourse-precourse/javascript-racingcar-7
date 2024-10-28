f9ee333663083d4d04203a6ae7ca00e2ef494253
keemsebeen <sebin0580@gmail.com>
2024-10-28 13:12:41 +0900

    feat : 자동차 이름 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { getInput } from "./view/input.js";
+
 class App {
-  async run() {}
+  async run() {
+    const carNamesInput = await getInput(INPUT.RACE_CAR_NAME);
+  }
 }
 
 export default App;
