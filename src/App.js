ea7285f05ecc681e21c538c22dddecd6ed375fd6
o2jsg <oji5656@naver.com>
2024-10-28 20:01:08 +0900

    feat: 예외처리 5자리 입력이상시 ERROR, 숫자 입력시 종료

--- src/App.js
+++ src/App.js
@@ -2,27 +2,40 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    async function getCarNames() {
-      try {
-        const CARS_NAME = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n");
-        const CARS_NAME_ARR = CARS_NAME.split(",");
-        return CARS_NAME_ARR
-      } catch (error) {
-        console.error('[ERROR]');
+    function vaildateCarNames(carNames) {
+      if(!carNames.trim()) {
+        throw new Error('자동차 이름을 입력해주세요.');
       }
+      const CAR_NAME_ARRAY = CAR_NAMES.split(",").map(name => name.trim());
+      CAR_NAME_ARRAY.forEach(name => {
+        if(name.length > 5) {
+          throw new Error('자동차 이름은 5자 이하만 가능합니다.');
+        }
+      })
+      return CAR_NAME_ARRAY;
     }
 
-    async function getTryCount() {
-      try {
-        const TRY_COUNT = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-        MissionUtils.Console.print(`시도할 횟수: ${TRY_COUNT}`);
-        return parseInt(TRY_COUNT);
-      } catch (error) {
-        console.error('[ERROR]');
+    function vaildateTryCount(tryCount) {
+      if(isNaN(tryCount) || tryCount.trim() === '') {
+        throw new Error('올바른 값을 입력해주새요');
       }
+      return parseInt(tryCount);
     }
-    await getCarNames()
-    await getTryCount()
+
+    async function getCarNames() {
+        const CAR_NAME = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n");
+        return vaildateCarNames(CAR_NAME);
+    }
+    async function getTryCount() {
+        const TRY_COUNT = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+        return vaildateTryCount(TRY_COUNT);
+    }
+
+    try {
+
+    } catch (error) {
+
+    }
   }
 }
 
