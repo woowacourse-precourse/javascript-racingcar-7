dd7cf7dc4d859ffe0bb80a38ff2d58b9d39e871d
JHLEE325 <ljntiger325@naver.com>
2024-10-28 12:35:04 +0900

    feat : n대의 자동차 이름을 입력받아 저장

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await MissionUtils.Console.readLineAsync('이름을 쉼표(,)로 구분하여 입력해 주세요: ');
+
+      const names = this.NameStorage(input);
+
+      MissionUtils.Console.print(names);
+
+    } catch (error) {
+      MissionUtils.Console.print(`${error.message}`);
+      throw error;
+    }
+  }
+
+  NameStorage(input) {
+    return input.split(",").map(name => name.trim());
+  }
 }
 
 export default App;
