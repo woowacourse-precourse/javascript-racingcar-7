93fd25625291664257f8aec96fcba1029dce4a45
inaemin <inaemin@gmail.com>
2024-10-28 21:04:54 +0900

    update: App 클래스에 메인 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { InputManager } from "./InputManager.js";
+import { Race } from "./Race.js";
 class App {
-  async run() {}
+  async run() {
+    try {
+      const carNames = await InputManager.readCarNames();
+      const tries = await InputManager.readTries();
+
+      const race = new Race(carNames);
+      await race.run(tries);
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
 export default App;
