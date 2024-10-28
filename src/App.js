8e0f3df4bd314b196fdff85f1441a4d96f7aac7f
SudoSzune <robin1415@kookmin.ac.kr>
2024-10-28 20:33:35 +0900

    feat(ATTEMPT): 시도할 횟수 입력 받기

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ class App {
     const LINE = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
     const CARS = LINE.split(',');
     console.log(CARS);
+    const ATTEMPT = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
   }
 }
 
