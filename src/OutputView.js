e5d32eb8788aab035c1be9c16948f6adb9b66b10
mywhzy <cmj2393a@naver.com>
2024-10-28 20:11:35 +0900

    feat: 경주 우승자 출력 기능 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,11 +1,16 @@
 import { Console } from '@woowacourse/mission-utils';
-import { FORWARD_SYMBOL } from '../src/constants.js';
+import { FORWARD_SYMBOL, OUTPUT_MESSAGE } from '../src/constants.js';
 
 const OutputView = {
   printCurrentCarInfo(name, forwardCount) {
     const forwardResult = FORWARD_SYMBOL.repeat(forwardCount);
 
     Console.print(`${name} : ${forwardResult}`);
+  },
+  printWinners(winners) {
+    Console.print(
+      `${OUTPUT_MESSAGE.winner} ${OUTPUT_MESSAGE.messageSeparator} ${winners.join(OUTPUT_MESSAGE.winnerSeparator)})}`
+    );
   },
 };
 
