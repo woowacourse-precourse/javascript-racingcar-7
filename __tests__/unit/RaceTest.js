95875bd264a8f78e342526a2c1d531c2e85c14df
Daeuning <happyjde3@gmail.com>
2024-10-28 09:22:59 +0900

    test(RaceTest): Car 객체 이동 관련 테스트 코드 tryMoveCar() 테스트 코드로 수정

--- __tests__/unit/RaceTest.js
+++ __tests__/unit/RaceTest.js
@@ -8,58 +8,59 @@ jest.mock('../../src/common/utils.js', () => ({
   printMessage: jest.fn(),
 }));
 
-describe("Race 클래스 테스트", () => {
-  let race;
+describe("tryMoveCar() 테스트", () => {
+  test("randomDistance가 4 미만이면 Car 객체가 이동하지 않아야 한다", () => {
+    const race = new Race();
+    const car = new Car("TestCar");
+    getRandomNumber.mockReturnValue(3);
+    const moveSpy = jest.spyOn(car, 'move');
 
-  beforeEach(() => {
-    race = new Race();
-    race.cars = [
-      new Car("Audi"),
-      new Car("BMW"),
-      new Car("Ford")
-    ];
-    jest.clearAllMocks();
+    race.tryMoveCar(car);
+
+    expect(moveSpy).not.toHaveBeenCalled();
   });
 
-  describe("generateRandomDistances() 테스트", () => {
-    test("무작위 값이 4 이상일 때만 Car 객체가 이동해야 한다", () => {
-      getRandomNumber.mockReturnValueOnce(3)
-                     .mockReturnValueOnce(5)
-                     .mockReturnValueOnce(7);
+  test("randomDistance가 4 이상이면 Car 객체가 이동해야 한다", () => {
+    const race = new Race();
+    const car = new Car("TestCar");
+    getRandomNumber.mockReturnValue(5);
+    const moveSpy = jest.spyOn(car, 'move');
 
-      race.generateRandomDistances();
+    race.tryMoveCar(car);
 
-      expect(race.cars[0].getDistance()).toBe(0); 
-      expect(race.cars[1].getDistance()).toBe(1); 
-      expect(race.cars[2].getDistance()).toBe(1); 
-    });
+    expect(moveSpy).toHaveBeenCalledTimes(1);
   });
+});
 
-  describe("getWinners() 테스트", () => {
-    const testCases = [
-      {
-        description: "최종 우승자를 올바르게 결정해야 한다",
-        distances: [6, 4, 2],
-        expectedWinners: ["Audi"]
-      },
-      {
-        description: "최종 우승자가 여러 명일 때도 올바르게 결정해야 한다",
-        distances: [5, 5, 3],
-        expectedWinners: ["Audi", "BMW"]
-      }
-    ];
-  
-    testCases.forEach(({ description, distances, expectedWinners }) => {
-      test(description, () => {
+describe("getWinners() 테스트", () => {
+  const testCases = [
+    {
+      description: "최종 우승자를 올바르게 결정해야 한다",
+      distances: [6, 4, 2],
+      expectedWinners: ["Audi"]
+    },
+    {
+      description: "최종 우승자가 여러 명일 때도 올바르게 결정해야 한다",
+      distances: [5, 5, 3],
+      expectedWinners: ["Audi", "BMW"]
+    }
+  ];
 
-        race.cars[0].move(distances[0]);
-        race.cars[1].move(distances[1]);
-        race.cars[2].move(distances[2]);
-  
-        const winners = race.getWinners();
-  
-        expect(winners).toEqual(expectedWinners);
-      });
+  testCases.forEach(({ description, distances, expectedWinners }) => {
+    test(description, () => {
+      const race = new Race();
+      race.cars = [
+        new Car("Audi"),
+        new Car("BMW"),
+        new Car("Ford")
+      ];
+      jest.clearAllMocks();
+
+      distances.forEach((distance, index) => race.cars[index].move(distance));
+
+      const winners = race.getWinners();
+
+      expect(winners).toEqual(expectedWinners);
     });
   });
 });
