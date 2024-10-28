a2a6af20363ec323d4b30274a71034926010be03
wrryu09 <rryu09@ewhain.net>
2024-10-28 17:18:13 +0900

    feat: 사용자 인풋 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    /** 자동차 이름 받기 */
+    const getCarNames = async () => {
+      const input = await Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+      );
+      const carNames = input.split(",").map((name) => name.trim());
+      // checkCarNames(carNames);
+      return carNames;
+    };
+
+    /** 시도 횟수 입력 */
+    const getTryCount = async () => {
+      const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+      const tryCount = parseInt(input, 10);
+      // checkTryCnt(tryCount);
+      return tryCount;
+    };
+
+    async function racingGame() {
+      try {
+        const carNames = await getCarNames();
+        const tryCount = await getTryCount();
+      } catch (error) {
+        Console.print(error.message);
+        throw error;
+      }
+    }
+
+    await racingGame();
+  }
 }
 
 export default App;
