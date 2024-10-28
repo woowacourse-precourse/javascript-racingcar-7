3a42d55207da3cb86b305972da8781890663240a
Glyph8 <newdy0808@gmail.com>
2024-10-28 11:56:52 +0900

    feat(moveJudge) 시도횟수만큼 차량이동 기능추가
    시도횟수만큼, 이동판정을하고, 이동을 수행하는 기능을 추가, 이동결과를 출력하는 함수 추가

--- src/App.js
+++ src/App.js
@@ -9,6 +9,21 @@ function checkCarName(carNames){
     return true;
 }
 
+function moveJudge(carNames, carRacing){
+    carNames.map((car, index)=>{
+        if(true){
+            carRacing[index] += "-";
+        }
+    })
+}
+
+function printCar(carNames, carRacing){
+    carNames.map((car, index)=>{
+        Console.print(`${car} : ${carRacing[index]}`);
+    })
+    Console.print("");
+}
+
 class App {
   async run() {
     Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -18,10 +33,17 @@ class App {
     const carNames = carNamesInput.split(",");
     checkCarName(carNames);
 
+    //let carRacing = [""];
+    let carRacing = Array.from({length: carNames.length}, () => "");
+
     Console.print("시도할 횟수는 몇 회인가요?");
     const moveCount = await Console.readLineAsync("");
 
     Console.print("실행 결과");
+    for(let i = 0; i < moveCount; i++){
+        moveJudge(carNames, carRacing);
+        printCar(carNames, carRacing);
+    }
 
     Console.print("최종 우승자 : ");
   }
