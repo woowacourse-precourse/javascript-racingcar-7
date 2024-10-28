188a1eb718cb21b8b70f42fd7fb412fc0ba39b82
nerdchanii <nerdchanii@gmail.com>
2024-10-28 19:47:05 +0900

    feat(app); implement App for entry points of application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import RaceGameController from "./controller/RaceGameController.js";
+import RaceGameService from "./service/RaceGameService.js";
+import RaceGameView from "./view/RaceGameView.js";
+import io from './utils/io.js'
+
 class App {
-  async run() {}
+  async run() {
+    const controller = new RaceGameController(new RaceGameView(io), new RaceGameService());
+    await controller.run();
+  }
 }
 
 export default App;
