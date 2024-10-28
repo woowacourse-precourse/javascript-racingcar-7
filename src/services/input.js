e1a861d634c3dbcbb5f7902cc1244ab7ef1eb4fd
OhSeChan <seb_s19@naver.com>
2024-10-28 19:36:19 +0900

    feat(input): 사용자 입력 검증 함수 추가
    - 자동차 이름과 시도 횟수 입력 함수에 각 입력에 대한 검증 함수 추가

--- src/services/input.js
+++ src/services/input.js
@@ -1,5 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
 import { INPUT_MESSAGES } from '../constant.js';
+import { validateCarNames, validateRoundCount } from './validation.js';
 
 const getInput = async (message) => {
   const input = await Console.readLineAsync(message);
@@ -9,12 +10,14 @@ const getInput = async (message) => {
 
 export const getCarNames = async () => {
   const input = await getInput(INPUT_MESSAGES.carNames);
+  validateCarNames(input);
 
   return input.split(',');
 };
 
 export const getRoundCount = async () => {
   const input = await getInput(INPUT_MESSAGES.roundCount);
+  validateRoundCount(input);
 
   return Number(input);
 };
