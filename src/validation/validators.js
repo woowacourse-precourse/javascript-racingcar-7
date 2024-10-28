e83f8d5fe566d4c040d21bac9654d36883dab657
keemsebeen <sebin0580@gmail.com>
2024-10-28 13:15:17 +0900

    feat : 자동차 이름 입력값이 `,` 로 끝나는 지 확인하는 함수 구현

--- src/validation/validators.js
+++ src/validation/validators.js
@@ -1,7 +1,9 @@
-import { CHAR_REGEX } from "../constants/charRegex.js";
 import { COMMA } from "../constants/delimiter.js";
-import { CAR_NAME } from "../constants/error.js";
 
 export const isEmptyCarName = (carNames) => {
   return !carNames;
+};
+
+export const isCarNameEndingWithComma = (carNames) => {
+  return carNames.endsWith(COMMA);
 };
