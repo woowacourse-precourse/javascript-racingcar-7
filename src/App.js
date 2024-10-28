54fc415e7e0af936e161105f9e85f5a109616371
yoojin <everywavyday@gmail.com>
2024-10-28 17:19:37 +0900

    feat: 가장 많이 전진한 자동차를 우승자로 선정하고 이름 출력

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App {
       this.carList = await this.getCarNames();
       const gameRound = await this.getGameRound();
       await this.runRacingGame(this.carList, gameRound);
+      this.printWinner();
     } catch (error) {
       Console.print(`[ERROR] ${error.message}`);
     }
@@ -22,6 +23,16 @@ class App {
     }
   }
 
+  isValidCarName(carName) {
+    if (!carName || carName.trim() === '') {
+      throw new Error('자동차 이름은 비어 있을 수 없습니다.');
+    }
+    if (carName.length > 5) {
+      throw new Error('자동차 이름은 5자 이하이어야 합니다.');
+    }
+    return carName.trim();
+  }
+
   async getGameRound() {
     try {
       const input = await Console.readLineAsync(
@@ -31,6 +42,17 @@ class App {
     } catch (error) {
       throw error;
     }
+  }
+
+  isValidGameRound(input) {
+    const gameRound = Number(input);
+    if (gameRound <= 0) {
+      throw new Error('게임 시행 횟수는 양의 정수로 입력해야 합니다.');
+    }
+    if (isNaN(gameRound)) {
+      throw new Error('게임 시행 횟수는 올바른 숫자로 입력해야 합니다.');
+    }
+    return gameRound;
   }
 
   async runRacingGame(carList, gameRound) {
@@ -66,25 +88,14 @@ class App {
     Console.print('');
   }
 
-  isValidCarName(carName) {
-    if (!carName || carName.trim() === '') {
-      throw new Error('자동차 이름은 비어 있을 수 없습니다.');
-    }
-    if (carName.length > 5) {
-      throw new Error('자동차 이름은 5자 이하이어야 합니다.');
-    }
-    return carName.trim();
+  getWinner() {
+    const gameWinner = Math.max(...this.gameRounds);
+    return this.carList.filter((car, index) => this.gameRounds[index] === gameWinner);
   }
 
-  isValidGameRound(input) {
-    const gameRound = Number(input);
-    if (gameRound <= 0) {
-      throw new Error('게임 시행 횟수는 양의 정수로 입력해야 합니다.');
-    }
-    if (isNaN(gameRound)) {
-      throw new Error('게임 시행 횟수는 올바른 숫자로 입력해야 합니다.');
-    }
-    return gameRound;
+  printWinner() {
+    const result = this.getWinner();
+    Console.print(`최종 우승자 : ${result.join(', ')}`);
   }
 }
 
