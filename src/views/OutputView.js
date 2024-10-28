98c2ce306b575539a47af04de5677d76e322c0e6
goodbyebada <goodbyebada@naver.com>
2024-10-28 22:17:27 +0900

    Refacotor: OutPutView에서 출력 목적에 맞지 않는 메소드 제거

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,16 +1,20 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class OutputView {
+  static printGameResult(winnerNameString) {
+    if (winnerNameString.length > 0) {
+      this.printWinner(winnerNameString);
+      return;
+    }
+    this.printNoWinner();
+  }
+
   static printWinner(names) {
     Console.print(`최종 우승자 : ${names}`);
   }
 
   static printNoWinner() {
     Console.print(`최종 우승자가 없습니다.`);
-  }
-
-  static makeError(message) {
-    throw new Error(`[ERROR]: ${message}`);
   }
 
   static print(message) {
