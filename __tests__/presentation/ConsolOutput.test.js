e0975eabcb805b7f634798002171f65a883b5080
rheefine <rheefine@gmail.com>
2024-10-28 20:08:06 +0900

    test: 출력 기능 테스트 코드 작성

---
+++ __tests__/presentation/ConsolOutput.test.js
@@ -0,0 +1,55 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import ConsoleOutput from '../../src/presentation/ConsoleOutput.js';
+import PROMPT from '../src/constants/Prompt.js';
+
+describe('ConsoleOutput 테스트', () => {
+  let printSpy;
+
+  beforeEach(() => {
+    printSpy = jest.spyOn(MissionUtils.Console, 'print');
+  });
+
+  afterEach(() => {
+    printSpy.mockClear();
+  });
+
+  test('displayRaceHeader가 레이스 헤더를 출력하는지 확인', () => {
+    ConsoleOutput.displayRaceHeader();
+    expect(printSpy).toHaveBeenCalledWith(PROMPT.RACE_HEADER);
+  });
+
+  test('displayCarState가 자동차 상태를 올바르게 출력하는지 확인', () => {
+    const testCases = [
+      { name: 'car1', moveCount: 3, expected: 'car1 : ---' },
+      { name: 'car2', moveCount: 0, expected: 'car2 : ' },
+      { name: 'car3', moveCount: 5, expected: 'car3 : -----' },
+    ];
+
+    testCases.forEach(({ name, moveCount, expected }) => {
+      ConsoleOutput.displayCarState(name, moveCount);
+      expect(printSpy).toHaveBeenCalledWith(expected);
+      printSpy.mockClear();
+    });
+  });
+
+  test('displayCarsState가 모든 자동차의 상태를 출력하는지 확인', () => {
+    const roundResult = [
+      { name: 'car1', moveCount: 2 },
+      { name: 'car2', moveCount: 3 },
+    ];
+
+    ConsoleOutput.displayCarsState(roundResult);
+
+    expect(printSpy).toHaveBeenCalledWith('car1 : --');
+    expect(printSpy).toHaveBeenCalledWith('car2 : ---');
+    expect(printSpy).toHaveBeenCalledWith('');
+  });
+
+  test('displayWinners가 우승자를 올바르게 출력하는지 확인', () => {
+    const winners = ['car1', 'car2'];
+    const expectedOutput = '최종 우승자 : car1, car2';
+
+    ConsoleOutput.displayWinners(winners);
+    expect(printSpy).toHaveBeenCalledWith(expectedOutput);
+  });
+});
--- src/constants/Prompt.js
+++ src/constants/Prompt.js
@@ -1,6 +1,11 @@
 const PROMPT = Object.freeze({
   INPUT_CAR_NAME: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
   INPUT_TARGET_ROUND: '시도할 횟수는 몇 회인가요?\n',
+  RACE_HEADER: '\n실행 결과',
+  RESULT_DELIMITER: ':',
+  POSITION_MARKER: '-',
+  WINNER_PROMPT: '최종 우승자 : ',
+  WINNER_DELIMITER: ',',
 });
 
 export default PROMPT;
