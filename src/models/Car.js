72ece24d7bfb73dd450b4bd85083b44bfbd17a60
luminox93 <memoji1236@gmail.com>
2024-10-28 15:26:32 +0900

    refactor: Car class 수정
    1. 기존 updateDistance가 외부에서 숫자를 입력받는 형식에서 직접 랜덤한 수를 생성하도록 수정

--- src/models/Car.js
+++ src/models/Car.js
@@ -1,10 +1,13 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 export default class Car {
   constructor(name) {
     this.name = name;
     this.distance = 0;
   }
 
-  updateDistance(randomNumber) {
+  updateDistance() {
+    const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
     if (randomNumber >= 4) {
       this.distance += 1;
     }
