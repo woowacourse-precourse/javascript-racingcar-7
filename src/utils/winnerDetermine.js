36a183f439ce4024ede41144b95fddc79e5330e8
leelsj2007 <leelsj2007@gmail.com>
2024-10-28 22:43:22 +0900

    refactor : winnerDetermine함수의 사용하지 않는 변수 삭제

--- src/utils/winnerDetermine.js
+++ src/utils/winnerDetermine.js
@@ -1,12 +1,9 @@
 function winnerDetermine(carArray) {
-  let winnerName = "";
-
   const maxPosition = carArray.reduce((max, car) => {
     return car.getPosition() > max ? car.getPosition() : max;
   }, 0);
 
   const winners = carArray.filter(car => car.getPosition() === maxPosition).map(car => car.getName());
-
   return winners.join(", ");
 }
 
