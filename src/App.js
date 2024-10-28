4db8e124b16869a5873b8a7b9ea1eb76b0bee663
youngsu <everdudtn@gmail.com>
2024-10-28 18:17:44 +0900

    feat(app): 경주 함수 구현

--- src/App.js
+++ src/App.js
@@ -11,11 +11,30 @@ class App {
       MissionUtils.Console.print("시도할 횟수를 입력해 주세요");
       return parseInt(await MissionUtils.Console.readLineAsync(""), 10);
     };
+    const tryRace = async (input, count) => {
+      MissionUtils.Console.print("실행 결과");
+      const result = {};
+      input.forEach((str) => {
+        result[str] = 0;
+      });
+
+      for (let i = 0; i < count; i++) {
+        input.forEach((str) => {
+          if (MissionUtils.Random.pickNumberInRange(0, 9) >= 4) {
+            result[str] += 1;
+          }
+          MissionUtils.Console.print(`${str} : ${"-".repeat(result[str])}`);
+        });
+        MissionUtils.Console.print("");
+      }
+
+      return result;
+    };
 
     const input = await getInput();
     const count = await getCount();
 
-    MissionUtils.Console.print(count);
+    await tryRace(input, count);
   }
 }
 
