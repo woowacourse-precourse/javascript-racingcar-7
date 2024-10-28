d7b008ead9b5c64a6b6b21854ee4baae1010da95
YEONSEO <yeonseochoi0717@gmail.com>
2024-10-28 13:50:29 +0900

    feat: 자동차 오브젝트 생성

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Console, Random } from "@woowacourse/mission-utils";
 
 // 주어진 횟수 동안 n대의 자동차는 전진 또는 멈출 수 있다.
 // 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.
-// 자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.
+// 이름은 5자 이하만 가능하다.
 // 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
 // 자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자는 한 명 이상일 수 있다.
 // 우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다.
@@ -13,6 +13,16 @@ class App {
   async run() {
     const CAR_NAME = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     const MAX_ATTEMPTS = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+
+    //, 기준으로 이름 분리와 함께 car(이름, 위치) 오브젝트 생성.
+    const CAR_NAME_ARRAY = CAR_NAME.split(',');
+    const CAR_POSITION_ARRAY = CAR_NAME_ARRAY.map(n => 0);
+    const car = {
+      name: CAR_NAME_ARRAY,
+      position: CAR_POSITION_ARRAY,
+    };
+    // 이름 5자 이하인지 유효성 체크 필요하다.
+
 
   }
 }
