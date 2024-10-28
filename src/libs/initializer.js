a5f5b2f3002849c7c40b3464d4c5d163bddb3c1b
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-28 16:35:02 +0900

    fix(parse): 자동차 이름을 파싱하는 로직 에러 수정

--- src/libs/initializer.js
+++ src/libs/initializer.js
@@ -2,10 +2,12 @@ import { validateRacerAmount, validateRounds } from "./validate.js";
 
 import Car from "../models/Car.js";
 import Race from "../models/Race.js";
+import { CARS_NAME_REGEXP } from "./constants.js";
 
 export function initialRace(carsName, rounds) {
-  validateRacerAmount(carsName.split(",").length);
+  const parseCarsName = carsName.split(CARS_NAME_REGEXP).filter((name) => name.trim());
+  validateRacerAmount(parseCarsName.length);
   validateRounds(rounds);
-  const cars = carsName.split(",").map((carName) => new Car(carName));
+  const cars = parseCarsName.map((carName) => new Car(carName));
   return new Race(cars, rounds);
 }
