3c656fb2394ca0313b85ded70329783d0e7730c6
“HYUNJIN105” <hyunjin.011005@gmail.com>
2024-10-28 20:16:35 +0900

    feat: 자동차 전진 기능 구현
    각 자동차에 대해 매 시도마다 0에서 9 사이의 무작위 값을
    생성하여 4 이상일 경우에 한 칸 전진한다.
    0에서 9사이의 무작위 값을 추출하기 위해서 @woowacourse/mission-utils의
    Random.pickNumberInRange() 메서드를 이용했다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
     try {
       const carNames = await App.getCarNames();
       const moveAttempts = await App.getMoveAttempts();
+      const raceResults = App.startRace(carNames, moveAttempts);
+
+      raceResults.forEach(result => {
+        Console.print(`${result.name} : ${'-'.repeat(result.position)}`);
+      });
     } catch (error) {
       Console.print(`[ERROR] ${error.message}`);
     }
@@ -12,11 +17,12 @@ class App {
 
   static async getCarNames() {
     const inputNames = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const MAX_CAR_NAME = 5;
+
     const carNames = inputNames.split(',').map(name => name.trim());
-    const MAX_CAR = 5;
 
     carNames.forEach(name => {
-      if (name.length > MAX_CAR) {
+      if (name.length > MAX_CAR_NAME) {
         throw new Error('자동차 이름은 5자 이하만 가능합니다.');
       }
     });
@@ -33,6 +39,31 @@ class App {
     }
 
     return moveAttempts;
+  }
+
+  static startRace(carNames, moveAttempts) {
+    const raceResults = carNames.map(name => ({ name, position: 0 }));
+
+    for (let i = 0; i < moveAttempts; i += 1) {
+      App.moveCars(raceResults);
+    }
+
+    return raceResults;
+  }
+
+  static moveCars(cars) {
+    cars.forEach(car => {
+      if (App.canMove()) {
+        car.position += 1;
+      }
+    });
+  }
+
+  static canMove() {
+    const MAX_RANDOM_RANGE = 9;
+    const MIN_MOVABLE_NUMBER = 4;
+
+    return Random.pickNumberInRange(0, MAX_RANDOM_RANGE) >= MIN_MOVABLE_NUMBER;
   }
 }
 
