6e0e1bb6c0e26e7d900f74199a365c4b1bd41396
SudoSzune <robin1415@kookmin.ac.kr>
2024-10-28 20:32:25 +0900

    feat(CARS): 경주할 자동차 이름 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console, Random } from '@woowacourse/mission-utils';
 class App {
-  async run() {}
+  async run() {
+    const LINE = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const CARS = LINE.split(',');
+    console.log(CARS);
+  }
 }
 
+const APP = new App();
+APP.run();
 export default App;
