1454dce9800c9fd5c92b4666a1cd26980a8568b6
kimzini <rlawlsdl11111@gmail.com>
2024-10-28 11:19:05 +0900

    feat: 경주 게임 로직 클래스 정의

--- src/Game.js
+++ src/Game.js
@@ -1,1 +1,51 @@
 // 경주 게임 로직 클래스
+
+import Car from "./Car.js";
+import { generateRandomNumber } from "./random.js";
+import { ERROR_MESSAGES } from "./constants.js";
+
+class Game {
+  constructor(carNames, raceCount) {
+    this.cars = this.createCars(carNames);
+    this.raceCount = this.validateRaceCount(raceCount);
+  }
+
+  createCars(carNames) {
+    const cars = carNames.split(",").map(name => new Car(name.trim()));
+    cars.forEach(car => {
+      if (car.name.length > 5) throw new Error(ERROR_MESSAGES.INVALID_CAR_NAME);
+    });
+    return cars;
+  }
+
+  validateRaceCount(raceCount) {
+    if (isNaN(raceCount)) {
+      throw new Error(ERROR_MESSAGES.INVALID_RACE_COUNT);
+    }
+    return Number(raceCount);
+  }
+
+  startRace() {
+    for (let i = 0; i < this.raceCount; i++) {
+      this.cars.forEach(car => car.move(generateRandomNumber()));
+      this.printRaceStatus();
+    }
+  }
+
+  printRaceStatus() {
+    this.cars.forEach(car => {
+      console.log(car.getDisplayPosition());
+    });
+    console.log("");
+  }
+
+  getWinners() {
+    const maxPosition = Math.max(...this.cars.map(car => car.getPosition()));
+    return this.cars
+      .filter(car => car.getPosition() === maxPosition)
+      .map(car => car.getName())
+      .join(", ");
+  }
+}
+
+export default Game;
