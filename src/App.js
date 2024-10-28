c798dc9a5702803ec19ab3388240d678ffe6a1fe
SeokHun <tommya98@naver.com>
2024-10-28 15:20:04 +0900

    feat(App): 사용자 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const carNames = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    const carNameArr = carNames.split(",").map((name) => name.trim());
+    for (const name of carNameArr) {
+      if (name.length > 5) {
+        throw new Error("[ERROR] 자동차 이름을 5자 이하로 작성해주세요.");
+      }
+    }
+
+    const tryNumber = parseInt(
+      await Console.readLineAsync("시도할 횟수는 몇회인가요?\n"),
+      10
+    );
+
+    Console.print(carNameArr);
+    Console.print(tryNumber);
+  }
 }
 
 export default App;
