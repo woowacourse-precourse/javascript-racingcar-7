8974a96c99bda9bb2ed97b0494ae4a734f296718
kimzini <rlawlsdl11111@gmail.com>
2024-10-28 11:19:43 +0900

    feat: 자동차 경주 게임 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { Console } from "@woowacourse/mission-utils";
+import Game from "./Game.js";
+import { ERROR_MESSAGES } from "./constants.js";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const carNames = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n");
+      this.handleCarNames(carNames);
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
+
+  async handleCarNames(carNames) {
+    try {
+      const raceCountInput = await Console.readLineAsync("시도할 횟수는 몇 회인가요? \n");
+      const raceCount = this.validateRaceCount(raceCountInput);
+      const game = new Game(carNames, raceCount);
+      game.startRace();
+      Console.print(`최종 우승자: ${game.getWinners()}`);
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
+
+  validateRaceCount(raceCount) {
+    if (isNaN(raceCount)) {
+      throw new Error(ERROR_MESSAGES.INVALID_RACE_COUNT);
+    }
+    return parseInt(raceCount, 10);
+  }
 }
 
 export default App;
