7a9d0d1ce4f6f3c35e04011d381c023db690bba5
Jinny <ggab.challenge@gmail.com>
2024-10-28 19:34:50 +0900

    test(integration): 자동차 이름 입력시 중복되면 에러를 던지는 기능 테스트 코드 생성

--- __tests__/IntegrationTestForErrors.js
+++ __tests__/IntegrationTestForErrors.js
@@ -17,4 +17,12 @@ describe('자동차 이름 입력 받기', () => {
 
     await expect(app.run()).rejects.toThrow('[ERROR]');
   });
+
+  test('자동차 이름 입력 값이 중복되면 에러를 던진다.', async () => {
+    const carNamesInput = ['pobi, pobi, jinny'];
+    mockQuestions(carNamesInput);
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow('[ERROR]');
+  });
 });
