7be9dc12b715890a4a0fa81c668c34efcfca6289
kimyou1102 <yoosk5485@naver.com>
2024-10-28 19:20:50 +0900

    test: 입력에 대한 예외처리 테스트 추가 작성

--- __tests__/additionalTest.js
+++ __tests__/additionalTest.js
@@ -4,6 +4,9 @@ describe('자동차 경주', () => {
   test.each([
     ['asdfgh,hjk', '[ERROR] 자동차 이름은 5자 이하만 가능합니다.'],
     ['asdf,', '[ERROR] 자동차 이름은 1자 이상이어야 합니다.'],
+    ['', '[ERROR] 자동차 이름은 빈값이 올 수 없습니다.'],
+    ['asdf.', '[ERROR] 자동차 이름의 특수문자는 쉼표(,)만 허용됩니다.'],
+    ['asdf#$', '[ERROR] 자동차 이름의 특수문자는 쉼표(,)만 허용됩니다.'],
   ])('자동차 이름이  경우 예외처리', async (names, expectedError) => {
     await expect(() => validateName(names)).toThrow(expectedError);
   });
@@ -11,6 +14,7 @@ describe('자동차 경주', () => {
   test.each([
     [0, '[ERROR] 시도 횟수는 1이상이어야 합니다.'],
     ['asdf', '[ERROR] 시도 횟수는 숫자여야 합니다.'],
+    ['', '[ERROR] 시도 횟수는 빈값이 올 수 없습니다.'],
   ])('시도 횟수에 대한 예외처리', async (count, ErrorMessage) => {
     await expect(() => validateTryCount(count)).toThrow(ErrorMessage);
   });
