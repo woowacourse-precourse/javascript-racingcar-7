57b217451207a6b8b10ec66eb918d6762e97f787
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-28 18:12:10 +0900

    ♻️ refactor(simulate): state를 private 필드로 변경
    - `Simulator` 클래스의 `state`를 private 필드 `#state` 로 변경했습니다.
    - 외부에서 직접 `state`에 접근할 수 없도록 하여 캡슐화를 강화했습니다.

--- src/Simulator.js
+++ src/Simulator.js
@@ -4,9 +4,11 @@ import pickRandomNumberInRange from './util/pickRandomNumberInRange';
 import printSimulate from './util/printSimulate';
 
 class Simulator {
+  #state;
+
   constructor(names) {
     printOutput(MESSAGE.EXECUTE_OUTPUT);
-    this.state = names.reduce((arr, name) => {
+    this.#state = names.reduce((arr, name) => {
       arr.push({ name, count: 0 });
       return arr;
     }, []);
@@ -18,7 +20,7 @@ class Simulator {
   }
 
   simulate() {
-    this.state = this.state.map(({ name, count }) => {
+    this.#state = this.#state.map(({ name, count }) => {
       if (this.#canMoveForward()) count++;
       printSimulate(name, count);
       return { name, count };
