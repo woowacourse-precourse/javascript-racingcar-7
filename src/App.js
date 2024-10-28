5f4fc6faa84bdd5a6677c3404f5523fd98747ba8
choisuyeon <stellasue@naver.com>
2024-10-28 12:44:48 +0900

    style: Create a new branch

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+// Random.pickNumberInRange() : Random 값 추출
+// Console.readLineAsync() : 사용자 값 입력
+// Console.print() : 사용자 값 출력
+
+// Random.pickNumberInRange(0, 9);   : 0에서 9까지의 정수 중 한 개의 정수 반환
+
 class App {
   async run() {}
 }
