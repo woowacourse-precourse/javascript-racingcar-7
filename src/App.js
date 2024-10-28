4397625fb0f211932b6e1869440cf609d77c9c10
YSW2 <jrgyber@gmail.com>
2024-10-28 17:18:35 +0900

    feat(get_random_number): 랜덤 숫자 생성 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 
 class App {
   parse_input(input) {
     const result = input.split(',').map((str) => str.trim());
     return result;
   }
-
+  get_random_number() {
+    return Random.pickNumberInRange(0, 9);
+  }
   async run() {
     const input_names = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
     );
     const input_number = await Console.readLineAsync(
-      '시도할 횟수는 몇 회인가요?'
+      '시도할 횟수는 몇 회인가요?\n'
     );
     const name_list = this.parse_input(input_names);
     Console.print(name_list);
+    Console.print(this.get_random_number());
   }
 }
 
