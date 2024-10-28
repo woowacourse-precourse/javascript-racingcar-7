eb4d6b72b6e181dba132bf5e51be3ebb651e4f0d
0Jaemin0 <jaemin2167@naver.com>
2024-10-28 16:38:25 +0900

    feat: 자동차 경주 게임의 전체적인 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import getUserInput from './utils/getUserInput.js';
-import INPUT_OUTPUT_MESSAGES from './constants/inputOutputMessages.js';
 import CarValidator from './CarValidator.js';
 import CountValidator from './CountValidator.js';
+import CarRacing from './CarRacing.js';
+import { getUserInput } from './utils/inputOutput.js';
+import INPUT_OUTPUT_MESSAGES from './constants/inputOutputMessages.js';
 
 class App {
   async run() {
     const inputCars = await getUserInput(INPUT_OUTPUT_MESSAGES.INPUT_CAR);
-    const carValidator = new CarValidator(inputCars);
-    const cars = carValidator.validateCars();
+    const cars = CarValidator.validateCars(inputCars);
 
     const inputCount = await getUserInput(INPUT_OUTPUT_MESSAGES.INPUT_COUNT);
-    const countValidator = new CountValidator(inputCount);
-    const count = countValidator.validateCount();
+    const count = CountValidator.validateCount(inputCount);
+
+    const carRacing = new CarRacing(cars, count);
+    carRacing.start();
+    carRacing.printWinner();
   }
 }
 
