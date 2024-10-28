16aa176d41c62ecc3875d06f6912df9050a3821c
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2024-10-28 19:48:00 +0900

    test: 시도 횟수에 대한 테스트 코드 추가

---
+++ __tests__/UserAttemptsValidationTest.js
@@ -0,0 +1,59 @@
+import App from "../src/App.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+describe("예외 테스트", () => {
+  test("시도 횟수가 입력되지 않았거나 공백인 경우", async () => {
+    // given
+    const inputs = ["pobi,woni", ""];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("시도 횟수가 숫자가 아닌 값인 경우", async () => {
+    // given
+    const inputs = ["pobi,woni", "2번"];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("시도 횟수가 음수인 경우", async () => {
+    // given
+    const inputs = ["pobi,woni", "-5"];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+  test("시도 횟수가 0인 경우", async () => {
+    // given
+    const inputs = ["pobi,woni", "0"];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+});
