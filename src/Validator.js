2fe10540c0135c5c613ab2499c59dbdadf5d1073
YeWon Seo <syw1236@naver.com>
2024-10-28 19:31:04 +0900

    feat(Validator): 입력한 숫자가 숫자인지 0인지 체크 예외 처리 구현

--- src/Validator.js
+++ src/Validator.js
@@ -2,9 +2,19 @@ import CauseError from './CauseError.js';
 import { errorMessage } from './constants.js'
 
 export default class Validator {
-    // 사용자가 입력한 것이 비어있는지 체크
+
     static isEmpty(input) {
         if(!input)
             CauseError.causeError(errorMessage.inputEmptyErrorMessage);
+    }
+
+    static isNumber(inputTime) {
+        if(isNaN(inputTime))
+            CauseError.causeError(errorMessage.isNotNuberErrorMessage);
+    }
+
+    static isZero(inputTime) {
+        if(inputTime === 0)
+            CauseError.causeError(errorMessage.isZeroErrorMessage);
     }
 }
