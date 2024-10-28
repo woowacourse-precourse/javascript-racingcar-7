97e8cfe108e7a66c4b6ad33ff9f9b69b86db5e1e
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-10-28 14:48:40 +0900

    feat: 자동차 리스트 생성 기능 추가
    - App 클래스에서 createCars 메서드를 통해 자동차 객체 리스트 초기화

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import Car from './model/Car.js';
+import InputView from './view/InputView.js';
+
 class App {
-  async run() {}
+  constructor() {
+    this.carList = [];
+    this.position = 0;
+  }
+
+  async run() {
+    const carNames = await InputView.readCarName();
+    this.createCars(carNames);
+  }
+
+  createCars(carNames) {
+    this.carList = carNames.map((name) => new Car(name));
+  }
 }
 
 export default App;
