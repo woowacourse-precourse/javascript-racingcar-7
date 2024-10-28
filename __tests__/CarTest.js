fff58b2f4a771f91d144e9a7c4a21d8b0ef47d76
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-10-28 12:43:00 +0900

    test: Car 클래스의 메서드 테스트
    - 이동 조건을 만족할 때만 이동하는지 확인

--- __tests__/CarTest.js
+++ __tests__/CarTest.js
@@ -1,4 +1,11 @@
+import { Random } from '@woowacourse/mission-utils';
 import Car from '../src/models/Car';
+
+jest.mock('@woowacourse/mission-utils', () => ({
+  Random: {
+    pickNumberInRange: jest.fn(),
+  },
+}));
 
 describe('Car 클래스 테스트', () => {
   test('getName 메서드가 올바른 이름을 반환해야 함', () => {
@@ -9,5 +16,17 @@ describe('Car 클래스 테스트', () => {
   test('getDistance 메서드가 초기 거리 0을 반환해야 함', () => {
     const car = new Car('pobi');
     expect(car.getDistance()).toBe(0);
+  });
+
+  test('tryMove 메서드가 이동 조건을 만족할 때만 거리 증가', () => {
+    const car = new Car('pobi');
+
+    Random.pickNumberInRange.mockReturnValueOnce(5);
+    car.tryMove();
+    expect(car.getDistance()).toBe(1);
+
+    Random.pickNumberInRange.mockReturnValueOnce(1);
+    car.tryMove();
+    expect(car.getDistance()).toBe(1);
   });
 });
