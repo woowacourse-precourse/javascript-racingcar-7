fa8473d011bc058e83cee4a093d85e6c9b7cdf01
@u_limit_ <yurim@ajou.ac.kr>
2024-10-28 16:20:58 +0900

    test: 정상적인 이동 및 우승자 출력 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,9 +1,8 @@
-import App from "../src/App.js";
-import { MissionUtils } from "@woowacourse/mission-utils";
+import App from '../src/App.js';
+import { MissionUtils } from '@woowacourse/mission-utils';
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
-
   MissionUtils.Console.readLineAsync.mockImplementation(() => {
     const input = inputs.shift();
     return Promise.resolve(input);
@@ -12,29 +11,62 @@ const mockQuestions = (inputs) => {
 
 const mockRandoms = (numbers) => {
   MissionUtils.Random.pickNumberInRange = jest.fn();
-
   numbers.reduce((acc, number) => {
     return acc.mockReturnValueOnce(number);
   }, MissionUtils.Random.pickNumberInRange);
 };
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
   return logSpy;
 };
 
-describe("자동차 경주", () => {
-  test("기능 테스트", async () => {
+describe('자동차 경주 기능 테스트', () => {
+  test('정상적인 이동 및 우승자 출력 테스트', async () => {
     // given
     const MOVING_FORWARD = 4;
     const STOP = 3;
-    const inputs = ["pobi,woni", "1"];
-    const logs = ["pobi : -", "woni : ", "최종 우승자 : pobi"];
+    const inputs = ['pobi,woni,jun', '5'];
+    const logs = [
+      'pobi : -',
+      'woni : ',
+      'jun : -',
+      'pobi : --',
+      'woni : -',
+      'jun : --',
+      'pobi : ---',
+      'woni : --',
+      'jun : ---',
+      'pobi : ----',
+      'woni : ---',
+      'jun : ----',
+      'pobi : -----',
+      'woni : ----',
+      'jun : -----',
+      '최종 우승자 : pobi, jun',
+    ];
     const logSpy = getLogSpy();
 
     mockQuestions(inputs);
-    mockRandoms([MOVING_FORWARD, STOP]);
+    // 예제의 요구에 맞춰 각 라운드에서 전진하거나 멈추는 값을 설정
+    mockRandoms([
+      MOVING_FORWARD,
+      STOP,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+    ]);
 
     // when
     const app = new App();
