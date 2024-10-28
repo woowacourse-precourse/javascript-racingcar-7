39ee38949b095c1b8b8eba2446cc2e8801d9b07c
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-10-28 20:35:48 +0900

    refactor(test): 테스트 코드 파라미터화

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -28,96 +28,54 @@ describe("정상 케이스", () => {
   const MOVING_FORWARD = 4;
   const STOP = 3;
 
-  test("단독 우승 테스트", async () => {
-    // given
-    const inputs = ["pobi,woni", "1"];
-    const logs = ["pobi : -", "woni : ", "최종 우승자 : pobi"];
+  test.each([
+    {
+      name: "단독 우승 테스트",
+      inputs: ["pobi,woni", "1"],
+      logs: ["pobi : -", "woni : ", "최종 우승자 : pobi"],
+      randoms: [MOVING_FORWARD, STOP],
+    },
+    {
+      name: "공동 우승 테스트",
+      inputs: ["pobi,woni,jun", "2"],
+      logs: ["pobi : --", "woni : --", "jun : -", "최종 우승자 : pobi, woni"],
+      randoms: [
+        MOVING_FORWARD,
+        MOVING_FORWARD,
+        STOP,
+        MOVING_FORWARD,
+        MOVING_FORWARD,
+        MOVING_FORWARD,
+      ],
+    },
+    {
+      name: "자동차 이름에 공백 또는 숫자, 특수문자 포함 시 테스트",
+      inputs: ["po bi,woni!,123", "1"],
+      logs: ["po bi : ", "woni! : -", "123 : ", "최종 우승자 : woni!"],
+      randoms: [STOP, MOVING_FORWARD, STOP],
+    },
+    {
+      name: "자동차 이름을 쉼표+공백(, )로 구분하여 입력 시 테스트",
+      inputs: ["pobi, woni", "3"],
+      logs: ["pobi : ---", "woni : -", "최종 우승자 : pobi"],
+      randoms: [
+        MOVING_FORWARD,
+        MOVING_FORWARD,
+        MOVING_FORWARD,
+        STOP,
+        MOVING_FORWARD,
+        STOP,
+      ],
+    },
+  ])("$name", async ({ inputs, logs, randoms }) => {
     const logSpy = getLogSpy();
 
     mockQuestions(inputs);
-    mockRandoms([MOVING_FORWARD, STOP]);
+    mockRandoms(randoms);
 
-    // when
     const app = new App();
     await app.run();
 
-    // then
-    logs.forEach((log) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-    });
-  });
-
-  test("공동 우승 테스트", async () => {
-    // given
-    const inputs = ["pobi,woni,jun", "2"];
-    const logs = [
-      "pobi : --",
-      "woni : --",
-      "jun : -",
-      "최종 우승자 : pobi, woni",
-    ];
-    const logSpy = getLogSpy();
-
-    mockQuestions(inputs);
-    mockRandoms([
-      MOVING_FORWARD,
-      MOVING_FORWARD,
-      STOP,
-      MOVING_FORWARD,
-      MOVING_FORWARD,
-      MOVING_FORWARD,
-    ]);
-
-    // when
-    const app = new App();
-    await app.run();
-
-    // then
-    logs.forEach((log) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-    });
-  });
-
-  test("자동차 이름에 공백 또는 숫자, 특수문자 포함 시 테스트", async () => {
-    // given
-    const inputs = ["po bi,woni!,123", "1"];
-    const logs = ["po bi : ", "woni! : -", "123 : ", "최종 우승자 : woni!"];
-    const logSpy = getLogSpy();
-
-    mockQuestions(inputs);
-    mockRandoms([STOP, MOVING_FORWARD, STOP]);
-
-    // when
-    const app = new App();
-    await app.run();
-
-    // then
-    logs.forEach((log) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-    });
-  });
-
-  test("자동차 이름을 쉼표+공백(, )로 구분하여 입력 시 테스트", async () => {
-    // given
-    const inputs = ["pobi, woni", "3"];
-    const logs = ["pobi : ---", "woni : -", "최종 우승자 : pobi"];
-    const logSpy = getLogSpy();
-
-    mockQuestions(inputs);
-    mockRandoms([
-      MOVING_FORWARD,
-      MOVING_FORWARD,
-      MOVING_FORWARD,
-      STOP,
-      MOVING_FORWARD,
-      STOP,
-    ]);
-
-    // when
-    const app = new App();
-    await app.run();
-
-    // then
     logs.forEach((log) => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
     });
@@ -125,99 +83,19 @@ describe("정상 케이스", () => {
 });
 
 describe("에러 케이스", () => {
-  test("자동차 이름 5자 초과 시 테스트", async () => {
-    // given
-    const inputs = ["pobi,javaji"];
+  test.each([
+    { name: "자동차 이름 5자 초과 시 테스트", inputs: ["pobi,javaji"] },
+    { name: "자동차 이름 중복 시 테스트", inputs: ["pobi,pobi"] },
+    { name: "자동차 이름 미입력 시 테스트", inputs: [""] },
+    { name: "경주 횟수가 음수일 시 테스트", inputs: ["pobi,woni", "-1"] },
+    { name: "경주 횟수가 0일 시 테스트", inputs: ["pobi,woni", "0"] },
+    { name: "경주 횟수가 소수일 시 테스트", inputs: ["pobi,woni", "1.5"] },
+    { name: "경주 횟수가 숫자가 아닐 시 테스트", inputs: ["pobi,woni", "a"] },
+    { name: "경주 횟수 미입력 시 테스트", inputs: ["pobi,woni", ""] },
+  ])("$name", async ({ inputs }) => {
     mockQuestions(inputs);
 
-    // when
     const app = new App();
-
-    // then
-    await expect(app.run()).rejects.toThrow("[ERROR]");
-  });
-
-  test("자동차 이름 중복 시 테스트", async () => {
-    // given
-    const inputs = ["pobi,pobi"];
-    mockQuestions(inputs);
-
-    // when
-    const app = new App();
-
-    // then
-    await expect(app.run()).rejects.toThrow("[ERROR]");
-  });
-
-  test("자동차 이름 미입력 시 테스트", async () => {
-    // given
-    const inputs = [""];
-    mockQuestions(inputs);
-
-    // when
-    const app = new App();
-
-    // then
-    await expect(app.run()).rejects.toThrow("[ERROR]");
-  });
-
-  test("경주 횟수가 음수일 시 테스트", async () => {
-    // given
-    const inputs = ["pobi,woni", "-1"];
-    mockQuestions(inputs);
-
-    // when
-    const app = new App();
-
-    // then
-    await expect(app.run()).rejects.toThrow("[ERROR]");
-  });
-
-  test("경주 횟수가 0일 시 테스트", async () => {
-    // given
-    const inputs = ["pobi,woni", "0"];
-    mockQuestions(inputs);
-
-    // when
-    const app = new App();
-
-    // then
-    await expect(app.run()).rejects.toThrow("[ERROR]");
-  });
-
-  test("경주 횟수가 소수일 시 테스트", async () => {
-    // given
-    const inputs = ["pobi,woni", "1.5"];
-    mockQuestions(inputs);
-
-    // when
-    const app = new App();
-
-    // then
-    await expect(app.run()).rejects.toThrow("[ERROR]");
-  });
-
-  test("경주 횟수가 숫자가 아닐 시 테스트", async () => {
-    // given
-    const inputs = ["pobi,woni", "a"];
-    mockQuestions(inputs);
-
-    // when
-    const app = new App();
-
-    // then
-    await expect(app.run()).rejects.toThrow("[ERROR]");
-  });
-
-  test("경주 횟수 미입력 시 테스트", async () => {
-    // given
-    const inputs = ["pobi,woni", ""];
-    mockQuestions(inputs);
-
-    // when
-    const app = new App();
-
-    // then
     await expect(app.run()).rejects.toThrow("[ERROR]");
   });
 });
