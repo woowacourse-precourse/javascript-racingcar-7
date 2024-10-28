6683475d7cc67f5393e14ee611a598ae35c04e39
jiHeeFlee <esder1310@naver.com>
2024-10-28 20:53:32 +0900

    refactor(App.js) : receive the number of attempts for the game (expressed as rounds)

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ class App {
     const car_input = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
+    
+    // 2. 게임을 몇번 시도할 건지 횟수 입력 받기(횟수를 라운드로 표현)
+    const number_input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    const rounds = Number(number_input);
   }
 }
 
