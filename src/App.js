624a289e90cc57c57f724e28fea1d3ecef75c2b4
Baek <woonn97@gmail.com>
2024-10-28 14:31:51 +0900

    refactor: Validator 클래스 객체로 나누기

--- src/App.js
+++ src/App.js
@@ -1,48 +1,18 @@
 import { Console } from "@woowacourse/mission-utils";
 import { ERROR_MESSAGE, MESSAGE } from "./constants.js";
 import Car from "./Car.js";
+import Validator from "./Validator.js";
 
 class App {
   async run() {
     const nameInput = await Console.readLineAsync(MESSAGE.CARNAME_INPUT);
-    this.validateNameInput(nameInput);
+    Validator.validateNameInput(nameInput);
 
     const carNames = nameInput.split(",").map((name) => new Car(name));
 
     const tryCount = await Console.readLineAsync(MESSAGE.TRYCOUNT_INPUT);
-    this.validateTryCountInput(tryCount);
-  }
-
-  validateNameInput(nameInput) {
-    if (nameInput === "" || nameInput.split(",").some((name) => name === "")) {
-      throw new Error(ERROR_MESSAGE.EMPTY_NAME); // 차 이름에 빈 값이 있는지 확인
-    }
-    if (nameInput.split(",").length !== new Set(nameInput.split(",")).size) {
-      throw new Error(ERROR_MESSAGE.DUPLICATE_NAME); // 차 이름에 중복된 값이 있는지 확인
-    }
-    if (nameInput.split(",").some((name) => name.length > 5)) {
-      throw new Error(ERROR_MESSAGE.OVER_LENGTH_NAME); // 차 이름이 5글자를 초과하는지 확인
-    }
-    if (nameInput.includes(" ")) {
-      throw new Error(ERROR_MESSAGE.SPACE_NAME); // 입력할 때 공백이 있는지 확인
-    }
-
-    const nameFormat = /^[A-Za-z]+$/; // 영문자만 허용하는 정규 표현식
-    if (nameInput.split(",").some((name) => !nameFormat.test(name))) {
-      throw new Error(ERROR_MESSAGE.INVALID_CHAR_NAME); // 숫자나 특수 문자가 포함된 경우 에러 발생
-    }
-  }
-
-  validateTryCountInput(tryCount) {
-    if (tryCount === "") {
-      throw new Error(ERROR_MESSAGE.EMPTY_TRYCOUNT); // 시도 횟수에 빈 값이 있는지 확인
-    }
-    if (isNaN(tryCount)) {
-      throw new Error(ERROR_MESSAGE.NOT_NUMBER_TRYCOUNT); // 시도 횟수가 숫자인지 확인
-    }
-    if (Number(tryCount) <= 0) {
-      throw new Error(ERROR_MESSAGE.INVALID_TRYCOUNT); // 시도 횟수가 0보다 큰지 확인
-    }
+    Validator.validateTryCountInput(tryCount);
+    const tryNumber = Number(tryCount);
   }
 }
 
---
+++ src/Validator.js
@@ -0,0 +1,37 @@
+import { ERROR_MESSAGE } from "./constants";
+
+class Validator {
+  static validateTryCountInput(tryCount) {
+    if (tryCount === "") {
+      throw new Error(ERROR_MESSAGE.EMPTY_TRYCOUNT);
+    }
+    if (tryCount <= 0) {
+      throw new Error(ERROR_MESSAGE.INVALID_TRYCOUNT);
+    }
+    if (isNaN(tryCount)) {
+      throw new Error(ERROR_MESSAGE.NOT_NUMBER_TRYCOUNT);
+    }
+  }
+
+  static validateNameInput(nameInput) {
+    if (nameInput === "" || nameInput.split(",").some((name) => name === "")) {
+      throw new Error(ERROR_MESSAGE.EMPTY_NAME);
+    }
+    if (nameInput.split(",").length !== new Set(nameInput.split(",")).size) {
+      throw new Error(ERROR_MESSAGE.DUPLICATE_NAME);
+    }
+    if (nameInput.split(",").some((name) => name.length > 5)) {
+      throw new Error(ERROR_MESSAGE.OVER_LENGTH_NAME);
+    }
+    if (nameInput.includes(" ")) {
+      throw new Error(ERROR_MESSAGE.SPACE_NAME);
+    }
+
+    const nameFormat = /^[A-Za-z]+$/; // 영문자만 허용하는 정규 표현식
+    if (nameInput.split(",").some((name) => !nameFormat.test(name))) {
+      throw new Error(ERROR_MESSAGE.INVALID_CHAR_NAME); // 숫자나 특수 문자가 포함된 경우 에러 발생
+    }
+  }
+}
+
+export default Validator;
