46e5a1248df8196f2f5cf084179bbe855a8dcc50
Daeuning <happyjde3@gmail.com>
2024-10-28 10:30:19 +0900

    test(validatorsTest): 테스트 코드 리팩토링

--- __tests__/unit/validatorsTest.js
+++ __tests__/unit/validatorsTest.js
@@ -1,66 +1,52 @@
 import { validateCarName, validateAttemptCount } from '../../src/common/validators.js';
 
-describe("validators 테스트", () => {
+const ERROR_MESSAGE = "[ERROR]";
 
-  describe("validateCarName() 테스트", () => {
+describe("validateCarName() 테스트", () => {
+  test("자동차 이름은 5자 이하만 가능하다", () => {
+    const input = "Ferrari,Mercedes,Audi,Porsche,BMW,LongNameCar";
 
-    test("자동차 이름은 5자 이하만 가능하다", () => {
-      expect(() => {
-        validateCarName("Ferrari,Mercedes,Audi,Porsche,BMW,LongNameCar"); 
-      }).toThrow("[ERROR]");
-    });
-
-    test("자동차 이름이 존재해야 한다 (빈 문자열일 경우)", () => {
-      expect(() => {
-        validateCarName(""); 
-      }).toThrow("[ERROR]");
-    });
-
-    test("자동차 이름은 중복되어서는 안된다", () => {
-      expect(() => {
-        validateCarName("BMW,Audi,BMW"); 
-      }).toThrow("[ERROR]");
-    });
-
-    test("유효한 자동차 이름을 입력했을 때는 예외가 발생하지 않아야 한다", () => {
-      expect(() => {
-        validateCarName("Audi,BMW,Kia"); 
-      }).not.toThrow();
-    });
+    expect(() => validateCarName(input)).toThrow(ERROR_MESSAGE);
   });
 
-  describe("validateAttemptCount() 테스트", () => {
+  test("자동차 이름이 존재해야 한다 (빈 문자열일 경우)", () => {
+    const input = "";
 
-    const INVALID_ATTEMPTS_COUNT_CASES = [
-      // 숫자가 아닌 값을 넣었을 때
-      "1e",
-      "1234q",
-      
-      // 0일 때
-      "0",
-      
-      // 0보다 작은 값일 때
-      "-1",
-      "-100",
-      
-      // 정수가 아닌 값일 때
-      "1.5",
-      "3.14",
-    ];
-    
-    test("시도할 횟수를 입력해야 한다 (빈 문자열일 경우)", () => {
-      expect(() => {
-        validateAttemptCount(""); 
-      }).toThrow("[ERROR]");
-    });
+    expect(() => validateCarName(input)).toThrow(ERROR_MESSAGE);
+  });
 
-    test.each(INVALID_ATTEMPTS_COUNT_CASES)(
-      "시도 횟수에는 양의 정수값만 입력해야 한다",
-      (input) => {
-        expect(() => {
-          validateAttemptCount(input);
-        }).toThrow("[ERROR]");
-      }
-    );
+  test("자동차 이름은 중복되어서는 안된다", () => {
+    const input = "BMW,Audi,BMW";
+
+    expect(() => validateCarName(input)).toThrow(ERROR_MESSAGE);
+  });
+
+  test("유효한 자동차 이름을 입력했을 때는 예외가 발생하지 않아야 한다", () => {
+    const input = "Audi,BMW,Kia";
+
+    expect(() => validateCarName(input)).not.toThrow();
+  });
+});
+
+describe("validateAttemptCount() 테스트", () => {
+  const INVALID_ATTEMPTS_COUNT_CASES = [
+    "1e",
+    "1234q",
+    "0",
+    "-1",
+    "-100",
+    "1.5",
+    "3.14",
+  ];
+
+  test("시도할 횟수를 입력해야 한다 (빈 문자열일 경우)", () => {
+    const input = "";
+
+    expect(() => validateAttemptCount(input)).toThrow(ERROR_MESSAGE);
+  });
+
+  test.each(INVALID_ATTEMPTS_COUNT_CASES)("시도 횟수에는 양의 정수값만 입력해야 한다", (input) => {
+
+    expect(() => validateAttemptCount(input)).toThrow(ERROR_MESSAGE);
   });
 });
