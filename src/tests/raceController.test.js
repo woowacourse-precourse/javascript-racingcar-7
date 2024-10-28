e673052608acc074b2530c898764a8c20cceeeac
seonae-j <wkdtjsdo135@naver.com>
2024-10-28 15:41:27 +0900

    test(raceController): 사용자가 입력한 이름의 cars 배열이 올바르게 생성되는 지 테스트하는 코드 추가

---
+++ src/tests/raceController.test.js
@@ -0,0 +1,16 @@
+import { raceController } from '../Controller.js';
+
+describe('raceController Test', () => {
+  let controller;
+
+  beforeEach(() => {
+    controller = new raceController('pobi, jun, woni', 5);
+  });
+
+  test('inputNames 입력값이 cars 배열로 제대로 생성된다.', () => {
+    expect(controller.cars.length).toBe(3);
+    expect(controller.cars[0].getName()).toBe('pobi');
+    expect(controller.cars[1].getName()).toBe('jun');
+    expect(controller.cars[2].getName()).toBe('woni');
+  });
+});
