7d25db836291050176058e8e564bd545d7374872
developer-kanghyun <dev.kanghyun@gmail.com>
2024-10-28 23:25:09 +0900

    feat: 자동차의 이름들을 "," 단위로 쪼개어 배열로 만든다.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ class App {
       "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n"
     );
     const tryCount = await Console.readLineAsync("시도할 횟수는 몇회인가요?\n");
+
+    const splittedCarNames = carNames.split(",");
   }
 }
 
