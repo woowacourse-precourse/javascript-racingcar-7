21e154380f290fc0082b4e1396c20ee4b9947273
tjdgus2308 <tjdgus2308@gmail.com>
2024-10-28 21:03:28 +0900

    refactor: 경기 진행 함수 기능 분리

--- src/App.js
+++ src/App.js
@@ -52,14 +52,22 @@ class App {
   startRace(cars, attempts) {
     Console.print("\n실행결과");
     for (let i = 0; i < attempts; i++) {
-      cars.forEach((car) => {
-        const randomNumber = Random.pickNumberInRange(0, 9);
-        if (randomNumber >= 4) {
-          car.move();
-        }
-        Console.print(`${car.name} : ${"-".repeat(car.position)}`);
-      });
+      this.runSingleRound(cars);
       Console.print(""); // 회차 구분을 위해 빈 줄 출력
+    }
+  }
+  
+  runSingleRound(cars) {
+    cars.forEach((car) => {
+      this.attemptToMove(car);
+      Console.print(`${car.name} : ${"-".repeat(car.position)}`);
+    });
+  }
+  
+  attemptToMove(car) {
+    const randomNumber = Random.pickNumberInRange(0, 9);
+    if (randomNumber >= 4) {
+      car.move();
     }
   }
 
