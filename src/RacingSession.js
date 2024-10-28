3f5ef367d5b5e19e2daaa485e52ab41c05ca5873
kochungwon <yud01063@gmail.com>
2024-10-28 10:07:42 +0900

    fix(출력메시지수정): \n이 출력되던 출력메시지를 수정했습니다. - \\n 으로 되어있어 \n 이 같이 출력되던 내용을 수정했습니다.

--- src/RacingSession.js
+++ src/RacingSession.js
@@ -10,7 +10,7 @@ class RacingSession {
     // 시도 횟수만큼 반복하여 경주를 진행, 각 시도에서 자동차의 이동 상태를 출력합니다.
     startRace() {
         for (let i = 0; i < this.attempts; i++) {
-            console.log(`\\n${i + 1}차 시도 결과:`);
+            console.log(`\n${i + 1}차 시도 결과:`);
             this.cars.forEach(car => {
                 car.move();
                 let positionDisplay = '-'.repeat(car.position); // 현재 위치 표시
@@ -24,7 +24,7 @@ class RacingSession {
     announceWinner() {
         const maxPosition = Math.max(...this.cars.map(car => car.position));
         const winners = this.cars.filter(car => car.position === maxPosition).map(car => car.name);
-        console.log(`\\n최종 우승자 : ${winners.join(', ')}`);
+        console.log(`\n최종 우승자 : ${winners.join(', ')}`);
     }
 }
 export default RacingSession;
