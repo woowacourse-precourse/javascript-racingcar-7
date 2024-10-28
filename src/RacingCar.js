8483ba52262ce6027311518646495f990b450283
rkdcodus <rcd7275@naver.com>
2024-10-28 19:53:41 +0900

    feat(RacingCar.js): handle input exception by creating exception function

--- src/RacingCar.js
+++ src/RacingCar.js
@@ -12,17 +12,31 @@ class RacingCar {
     this.#status = [];
   }
 
+  static handleCarsInputException(input) {
+    const regex = /^[^,]{1,5}(,[^,]{1,5})*$/;
+    if (!regex.test(input))
+      throw new Error(
+        '[ERROR] 경주할 자동차를 올바른 형식으로 입력해주세요. 이름은 쉼표(,)를 기준으로 구분하며 자동차 이름은 최대 5자입니다.',
+      );
+  }
+
+  static handleCountInputException(input) {
+    const regex = /^[1-9]\d*$/;
+    if (!regex.test(input)) throw new Error('[ERROR] 시도할 횟수는 양수만 가능합니다.');
+  }
+
   async receiveInput() {
     const cars = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
     );
-    const count = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
-
+    RacingCar.handleCarsInputException(cars);
     cars.split(',').forEach((car) => {
       this.#cars.push(car.trim());
       this.#status.push('');
     });
 
+    const count = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    RacingCar.handleCountInputException(count);
     this.#count = count;
   }
 
