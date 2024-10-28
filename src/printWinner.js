e50709b7213cf7e9ef80fcc499cc2ea73bd40ffa
H0ngJu <hongju4385@gmail.com>
2024-10-28 18:44:34 +0900

    feat: 우승자가 없는 경우 error 반환

--- src/printWinner.js
+++ src/printWinner.js
@@ -1,4 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
+import { ERROR_MESSAGES } from "./errorMessages.js";
 
 export function printWinner(carDistances, carNames) {
   const maxValue = Math.max(...carDistances);
@@ -6,5 +7,7 @@ export function printWinner(carDistances, carNames) {
     (_, index) => carDistances[index] === maxValue
   );
 
-  Console.print(`최종 우승자 : ${winners.join(", ")}`);
+  if (winners.length === 0) {
+    throw new Error(ERROR_MESSAGES.NO_WINNER_EXIST_ERROR);
+  } else Console.print(`최종 우승자 : ${winners.join(", ")}`);
 }
