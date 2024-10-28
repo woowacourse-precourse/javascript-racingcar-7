28a0312a6d51e231b978450c4e8fdde9f91f22f0
0Jaemin0 <jaemin2167@naver.com>
2024-10-28 16:27:54 +0900

    refactor: CarRacing 클래스 내부 접근 방지를 통한 안정성 강화

--- src/CarRacing.js
+++ src/CarRacing.js
@@ -1,40 +1,41 @@
 import Car from './Car.js';
-import { Console } from '@woowacourse/mission-utils';
 import getMostMovedCar from './utils/getMostMovedCar.js';
+import { printMessage } from './utils/inputOutput.js';
 import INPUT_OUTPUT_MESSAGES from './constants/inputOutputMessages.js';
 
 class CarRacing {
+  #cars;
+  #count;
+
   constructor(cars, count) {
-    this.cars = cars.map((car) => new Car(car));
-    this.count = count;
+    this.#cars = cars.map((car) => new Car(car));
+    this.#count = count;
   }
 
-  startRace() {
-    Console.print(`\n${INPUT_OUTPUT_MESSAGES.OUTPUT_RESULT}`);
+  start() {
+    printMessage(`${INPUT_OUTPUT_MESSAGES.OUTPUT_RESULT}`);
 
-    for (let i = 0; i < this.count; i++) {
-      this.race();
-      this.printResult();
+    for (let i = 0; i < this.#count; i++) {
+      this.#race();
+      this.#printResult();
     }
-
-    this.printWinners();
   }
 
-  race() {
-    this.cars.forEach((car) => car.moveForward());
+  #race() {
+    this.#cars.forEach((car) => car.moveForward());
   }
 
-  printResult() {
-    this.cars.forEach((car) =>
-      Console.print(`${car.getName()} : ${'-'.repeat(car.getPosition())}`)
+  #printResult() {
+    this.#cars.forEach((car) =>
+      printMessage(`${car.getName()} : ${'-'.repeat(car.getPosition())}`)
     );
 
-    Console.print('');
+    printMessage('');
   }
 
-  printWinners() {
-    const winner = getMostMovedCar(this.cars);
-    Console.print(`${INPUT_OUTPUT_MESSAGES.OUTPUT_WINNER}${winner.join(', ')}`);
+  printWinner() {
+    const winner = getMostMovedCar(this.#cars);
+    printMessage(`${INPUT_OUTPUT_MESSAGES.OUTPUT_WINNER}${winner.join(', ')}`);
   }
 }
 
