1d3cb968b5596c7b13a77fb0c9089ebe4901649a
o2jsg <oji5656@naver.com>
2024-10-28 12:38:33 +0900

    feat: 시도 횟수 입력 받기 기능

--- src/App.js
+++ src/App.js
@@ -5,14 +5,24 @@ class App {
     async function getCarNames() {
       try {
         const CARS_NAME = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n");
-        MissionUtils.Console.print(`${CARS_NAME}`);
         const CARS_NAME_ARR = CARS_NAME.split(",");
         return CARS_NAME_ARR
       } catch (error) {
         console.error('[ERROR]');
       }
     }
-    getCarNames()
+
+    async function getTryCount() {
+      try {
+        const TRY_COUNT = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+        MissionUtils.Console.print(`시도할 횟수: ${TRY_COUNT}`);
+        return parseInt(TRY_COUNT);
+      } catch (error) {
+        console.error('[ERROR]');
+      }
+    }
+    await getCarNames()
+    await getTryCount()
   }
 }
 
