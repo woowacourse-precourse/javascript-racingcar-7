d1ffee0da23d9df21f4f082e8aab4252d963ce1b
심우석 <dntjr5741@naver.com>
2024-10-28 22:17:12 +0900

    feat : 자동차 이름 유효성 확인 및 이동 횟수 유효성 확인

--- src/App.js
+++ src/App.js
@@ -8,14 +8,29 @@ class App {
       );
 
       const nameList = carsNames.split(",").map((car) => car.trim());
+      this.validCarNames(nameList);
 
-      console.log(nameList);
       const playNum = await Console.readLineAsync(
         "시도할 횟수는 몇 회인가요?\n"
       );
+      this.validateMoveCount(playNum);
     } catch (error) {
-      Console.readLineAsync(error.message);
+      Console.print(`[ERROR] ${error.message}`);
       throw error;
+    }
+  }
+
+  validCarNames(nameList) {
+    nameList.forEach((name) => {
+      if (name.length === 0 || name.length > 5) {
+        throw new Error("자동차 이름은 1자 이상 5자 이하만 가능합니다.");
+      }
+    });
+  }
+
+  validateMoveCount(playNum) {
+    if (isNaN(playNum) || playNum <= 0) {
+      throw new Error("[ERROR] 이동 횟수는 1 이상의 정수여야 합니다.");
     }
   }
 }
