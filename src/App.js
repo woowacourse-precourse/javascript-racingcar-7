f9c9f10a8cc4290206933fe4db0b60c2fcd21475
심우석 <dntjr5741@naver.com>
2024-10-28 23:43:16 +0900

    fix : 불필요한 코드 제거 및 함수사용하여 자동차들 분리

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ class App {
         "경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분)\n"
       );
 
-      const nameList = carsNames.split(",").map((car) => car.trim());
-      this.validCarNames(nameList);
+      const nameList = this.validCarNames(
+        carsNames.split(",").map((car) => car.trim())
+      );
 
       const playNum = Number(
         await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n")
@@ -23,27 +24,24 @@ class App {
   }
 
   validCarNames(nameList) {
-    nameList.forEach((name) => {
-      if (name.length === 0 || name.length > 5) {
-        throw new Error(
-          "[ERROR] 자동차 이름은 1자 이상 5자 이하만 가능합니다."
-        );
-      }
-    });
+    if (nameList.some((name) => name.length === 0 || name.length > 5)) {
+      throw new Error("[ERROR] 자동차 이름은 1자 이상 5자 이하만 가능합니다.");
+    }
+    return nameList;
   }
 
   validMoveCount(playNum) {
-    if (isNaN(playNum) || playNum <= 0) {
+    if (!Number.isInteger(playNum) || playNum <= 0) {
       throw new Error("[ERROR] 이동 횟수는 1 이상의 정수여야 합니다.");
     }
   }
 
   goToCars(nameList, playNum) {
-    let movingCars = new Array(nameList.length).fill(0);
+    const movingCars = new Array(nameList.length).fill(0);
 
+    Console.print("\n경기 시작!");
     for (let i = 0; i < playNum; i++) {
       this.moveCars(nameList, movingCars);
-      this.nowCar(nameList, movingCars);
       Console.print("\n");
     }
 
@@ -53,18 +51,14 @@ class App {
   moveCars(nameList, movingCars) {
     nameList.forEach((car, idx) => {
       const randomNum = Random.pickNumberInRange(0, 9);
-      if (randomNum >= 4) {
-        movingCars[idx]++;
-      }
+      if (randomNum >= 4) movingCars[idx]++;
+      this.displayProgress(car, movingCars[idx]);
     });
   }
 
-  nowCar(nameList, movingCars) {
-    movingCars.forEach((distance, idx) => {
-      const car = nameList[idx];
-      const carPosition = "-".repeat(distance);
-      Console.print(`${car} : ${carPosition}`);
-    });
+  displayProgress(car, position) {
+    const progress = "-".repeat(position);
+    Console.print(`${car} : ${progress}`);
   }
 
   winnerCar(nameList, movingCars) {
@@ -72,7 +66,6 @@ class App {
     const winners = nameList.filter(
       (_, idx) => movingCars[idx] === maxDistance
     );
-
     Console.print(`최종 우승자 : ${winners.join(", ")}`);
   }
 }
