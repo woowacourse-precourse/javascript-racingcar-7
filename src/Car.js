b61b0bd237677a8d6e3bea52251228635cfa4269
Seoyeon Kim <kseoy0607@gmail.com>
2024-10-28 15:27:34 +0900

    feat: 이름 검증 후 인스턴스  변수 초기화

--- src/Car.js
+++ src/Car.js
@@ -1,10 +1,23 @@
+import Validator from './Validator.js';
+
 class Car {
   #name;
   #distance;
 
+  constructor(name) {
+    this.#name = this.#validateCarName(name);
+    this.#distance = 0;
+  }
+
   #setNameIfEmpty(name) {
     if (name === '') return (this.#name = 'null');
     return name;
+  }
+
+  #validateCarName(name) {
+    const validator = new Validator();
+    const validNames = validator.validateCarName(name);
+    return this.#setNameIfEmpty(validNames);
   }
 }
 
