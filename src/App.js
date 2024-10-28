3811c073432214efa65b16af101e344ef69266df
YSW2 <jrgyber@gmail.com>
2024-10-28 17:47:49 +0900

    feat(start_race): 자동차 경주 기능 구현
    - 배열을 순회하며 경주 실행
    - 각 자동차의 경주 실행 결과를 배열로 저장

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ class App {
   get_random_number() {
     return Random.pickNumberInRange(0, 9);
   }
+  start_race(race_result) {
+    for (let i = 0; i < race_result.length; i++) {
+      if (this.get_random_number() >= 4) {
+        race_result[i] += 1;
+      }
+    }
+  }
   async run() {
     const input_names = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
@@ -15,9 +22,14 @@ class App {
     const input_number = await Console.readLineAsync(
       '시도할 횟수는 몇 회인가요?\n'
     );
+
     const name_list = this.parse_input(input_names);
-    Console.print(name_list);
-    Console.print(this.get_random_number());
+    const race_result = new Array(name_list.length).fill(0);
+
+    for (let repeat = 0; repeat < input_number; repeat++) {
+      this.start_race(race_result);
+    }
+    Console.print(race_result);
   }
 }
 
