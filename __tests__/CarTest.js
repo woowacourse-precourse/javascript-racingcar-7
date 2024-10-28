3c2a5dfea0cde3c4109b003b81f735e9936602f4
silver93hq <gkagjsrb@gmail.com>
2024-10-28 10:29:00 +0900

    test: Car클래스와 Game클래스의 변경사항에 맞게 테스트 수정 및 작성

--- __tests__/CarTest.js
+++ __tests__/CarTest.js
@@ -3,59 +3,33 @@ import { VALUES } from '../src/constants/values.js';
 import { mockRandoms } from '../src/utils/testUtils.js';
 
 describe('Car 클래스 테스트', () => {
-  beforeEach(() => {
-    Car.clearInstances();
-  });
   test.each([
     {
-      carNames: ['a', 'b'],
+      carName: 'a',
       randomValues: [VALUES.moveIfOrMore, VALUES.moveIfOrMore - 1],
-      result: [
-        {
-          name: 'a',
-          progress: 1,
-        },
-        {
-          name: 'b',
-          progress: 0,
-        },
-      ],
+      expectedResult: {
+        name: 'a',
+        progress: 1,
+      },
     },
     {
-      carNames: ['a', 'b'],
+      carName: 'a',
       randomValues: [VALUES.moveIfOrMore, VALUES.moveIfOrMore],
-      result: [
-        {
-          name: 'a',
-          progress: 1,
-        },
-        {
-          name: 'b',
-          progress: 1,
-        },
-      ],
+      expectedResult: {
+        name: 'a',
+        progress: 2,
+      },
     },
-  ])('자동차 이동 결과를 반환하는 기능 테스트', ({ carNames, randomValues, result }) => {
+  ])('자동차 이동 결과를 반환하는 기능 테스트', ({ carName, randomValues, expectedResult }) => {
+    // when
     mockRandoms(randomValues);
-    Car.addCarInstances(carNames);
-    const executeResult = Car.executeAllCars();
-    expect(executeResult).toEqual(result);
-  });
-  test.each([
-    {
-      carNames: ['a', 'b'],
-      randomValues: [VALUES.moveIfOrMore, VALUES.moveIfOrMore - 1],
-      winner: ['a'],
-    },
-    {
-      carNames: ['a', 'b'],
-      randomValues: [VALUES.moveIfOrMore, VALUES.moveIfOrMore],
-      winner: ['a', 'b'],
-    },
-  ])('우승한 자동차를 구하는 기능 테스트', ({ carNames, randomValues, winner }) => {
-    Car.addCarInstances(carNames);
-    mockRandoms(randomValues);
-    Car.executeAllCars();
-    expect(Car.getWinner()).toEqual(winner);
+    const car = new Car(carName);
+    for (let i = 0; i < randomValues.length; i += 1) {
+      car.move();
+    }
+    const result = car.getState();
+
+    // then
+    expect(result).toEqual(expectedResult);
   });
 });
