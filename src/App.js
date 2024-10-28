342d38cdf2c857b5fe37e903d1908b4ff73f17d7
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-10-28 13:44:44 +0900

    feat: 4이상이면 전진

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Console } from "@woowacourse/mission-utils";
 import { MESSAGES } from "./constants.js";
 
 import { getAttempts, getCarNames } from "./inputHandlers.js";
+import { race } from "./raceHandler.js";
 class App {
   async run() {
     const cars = await getCarNames();
     const attemps = await getAttempts();
     Console.print(MESSAGES.EXECUTION_RESULT);
+    const result = race(cars, attemps);
   }
 }
 
--- src/raceHandler.js
+++ src/raceHandler.js
@@ -1,4 +1,27 @@
 import { Random } from "@woowacourse/mission-utils";
-function move(cars) {
+
+function canMove() {
   const randomValue = Random.pickNumberInRange(0, 9);
+  return randomValue >= 4;
+}
+
+function updateCarPosition(results, index) {
+  if (canMove()) {
+    results[index] += "-";
+  }
+}
+
+function move(results, cars) {
+  cars.forEach((_, index) => {
+    updateCarPosition(results, index);
+  });
+}
+
+export function race(cars, moveAttempts) {
+  const results = cars.map(() => "");
+
+  for (let i = 0; i < moveAttempts; i++) {
+    move(results, cars);
+  }
+  return results;
 }
