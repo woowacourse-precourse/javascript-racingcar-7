3625d852ff638c118f510d4688da4a353ca61e4e
luminox93 <memoji1236@gmail.com>
2024-10-28 15:30:36 +0900

    feat: Controller 클래스 생성
    1. 모든 게임 로직을 관장하는 Controller 클래스 생성
    2. 사용자로부터 참여자 이름, 게임 횟수를 입력받음
    3. 입력값들을 검증한 후 해당 값들을 기반으로 게임 생성
    4. 게임 실행 후 결과값 추출

---
+++ src/controllers/Controller.js
@@ -0,0 +1,75 @@
+// src/controllers/Controller.js
+
+import { InputView, OutputView } from '../views/index.js';
+import {
+  isEmptyStringIncluded,
+  isPositiveInteger,
+  isValidCarName,
+  isValidGameAttempts,
+  isValidParticipantAmount,
+} from '../validations/InputValidation.js';
+import Car from '../models/Car.js';
+import RaceGame from '../models/RaceGame.js';
+
+export default class Controller {
+  constructor() {
+    this.cars = [];
+    this.gameAttempts = 0;
+    this.raceGame = null;
+  }
+
+  async run() {
+    try {
+      await this.inputParticipants();
+      await this.inputGameAttempts();
+      this.startRace();
+      this.showResult();
+    } catch (error) {
+      OutputView.printError(`[ERROR] ${error.message}`);
+    }
+  }
+
+  async inputParticipants() {
+    const input = await InputView.readInput(
+      '참가자 이름을 입력해주세요 (이름은 쉼표(,) 기준으로 구분): ',
+    );
+    isEmptyStringIncluded(input);
+    isValidParticipantAmount(input);
+
+    const names = input.split(',');
+    names.forEach((name) => {
+      isValidCarName(name);
+    });
+
+    this.cars = names.map((name) => new Car(name));
+  }
+
+  async inputGameAttempts() {
+    const input = await InputView.readInput('시도할 횟수를 입력해주세요: ');
+    isEmptyStringIncluded(input);
+    isPositiveInteger(input);
+    isValidGameAttempts(Number(input));
+
+    this.gameAttempts = Number(input);
+  }
+
+  startRace() {
+    this.raceGame = new RaceGame(this.cars, this.gameAttempts);
+    OutputView.printResult('\n실행 결과');
+
+    for (let round = 0; round < this.gameAttempts; round++) {
+      this.raceGame.participants.forEach((car) => {
+        car.updateDistance();
+        const progress = '-'.repeat(car.getDistance());
+        OutputView.printResult(`${car.name} : ${progress}`);
+      });
+      OutputView.printResult('');
+    }
+  }
+
+  showResult() {
+    const winners = this.raceGame.getWinners();
+    const winnerNames = winners.map((car) => car.name).join(', ');
+    OutputView.printResult(`최종 우승자 : ${winnerNames}`);
+  }
+}
