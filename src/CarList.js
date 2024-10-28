74bf7e9631d41147d6e9a21dfdbf65deab93c182
govl0407 <govl0407@gmail.com>
2024-10-28 18:00:48 +0900

    fix : 전진 로직 수정
    랜덤값을 %4 하는 부분을 /4 하도록 수정

--- src/CarList.js
+++ src/CarList.js
@@ -1,50 +1,83 @@
-import {Console, Random} from '@woowacourse/mission-utils'
-
+import {Console, Random} from '@woowacourse/mission-utils';
 
 class CarList {
     constructor() {
         this.carnames = [];
         this.iter = 0;
-        this.sco = [];
+        this.scores = [];
     }
 
     async initialize() {
-        this.carnames = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"); // 사용자 입력
-        this.carnames = this.carnames.split(',');
+        await this.getCarNames();
+        await this.getIterations();
+        this.scores = new Array(this.carnames.length).fill(0);
+        this.validateCarNames();
+        this.validateIterations();
+    }
+
+    async getCarNames() {
+        let namesInput = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        this.carnames = namesInput.split(',');
+    }
+
+    async getIterations() {
         this.iter = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-        this.sco = new Array(this.carnames.length).fill(0);
+    }
+
+    validateCarNames() {
+        const invalidName = this.carnames.find(name => name.length > 6);
+        if (invalidName) {
+            this.throwError("잘못된 입력입니다");
+        }
+    }
+
+    validateIterations() {
+        if (isNaN(this.iter)) {
+            this.throwError("잘못된 입력입니다");
+        }
+    }
+
+    throwError(message) {
+        throw new Error(`[ERROR] : ${message}`);
     }
 
     simulate() {
         for (let i = 0; i < this.iter; i++) {
-            this.sco.forEach((_, j) => (this.sco[j] += Random.pickNumberInRange(0, 9) % 4));
-            this.sco.forEach((score, j) => Console.print(this.carnames[j] + " : " + "-".repeat(score)));
-            Console.print("");
+            this.runIteration();
+            this.displayScores();
         }
     }
 
-    updateWinners(score, max, car, winner) {
-        if (score > max) {//최대 점수가 업데이트 되면 기존 우승 후보자는 목록에서 제외
-            winner.length = 0;
-        }
-        if (score >= max) {
-            winner.push(car);
-        }
+    runIteration() {
+        this.scores = this.scores.map(score => score + this.generateScore());
+    }
+
+    generateScore() {
+        return Random.pickNumberInRange(0, 9) / 4;
+    }
+
+    displayScores() {
+        this.scores.forEach((score, index) => {
+            Console.print(`${this.carnames[index]} : ${'-'.repeat(score)}`);
+        });
+        Console.print("");
     }
 
     prizing() {
-        let max = 0;
-        let winner = [];
-
-        this.carnames.forEach((car, i) => {
-            this.updateWinners(this.sco[i], max, car, winner);
-            max = Math.max(max, this.sco[i]);
-        });
-        return winner;
+        let max = Math.max(...this.scores);
+        let winners = this.findWinners(max);
+        return winners;
     }
 
-
+    findWinners(max) {
+        let winners = [];
+        this.scores.forEach((score, index) => {
+            if (score === max) {
+                winners.push(this.carnames[index]);
+            }
+        });
+        return winners;
+    }
 }
-
 
 export default CarList;
