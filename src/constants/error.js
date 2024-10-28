f824ae68309c6f09b3341011ba1ff3f8e423ffe1
keemsebeen <sebin0580@gmail.com>
2024-10-28 14:20:10 +0900

    feat : 자동차 이름 유효성 검사에 사용되는 constants 추가

--- src/constants/error.js
+++ src/constants/error.js
@@ -3,4 +3,14 @@ export const ATTEMPT = {
   EMPTY_TRY: "시도할 횟수가 입력되지 않았습니다.",
 };
 
+export const CAR_NAME = {
+  MAX: 5,
+  EMPTY: "자동차 이름이 누락되었습니다.",
+  MAX_LENGTH: "이름은 5글자 이내로 작성해주세요.",
+  DUPLICATE: "이름이 겹칩니다. 고유한 이름을 사용해주세요.",
+  SPECIAL_CHAR: "쉼표를 제외한 다른 특수문자는 사용할 수 없습니다.",
+  DOUBLE_COMMA:
+    "잘못된 구분이 사용되었습니다. 두 개 이상의 쉼표는 허용되지 않습니다.",
+};
+
 export const ERROR_MESSAGE = "[ERROR] ";
