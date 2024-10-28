f474ad5d53773c60d8b15df65d69c8a0064a6736
geunhwa37 <jacika37@naver.com>
2024-10-28 20:45:19 +0900

    feat: 자동차 이름과 위치 출력 메서드

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ class Car{
     this.name = name;
     this.position = 0;
   }
-  //자동차의 이동 메서드 구현
+  // 자동차의 이동 메서드 구현
   move(){
     const randomNum = Random.pickNumberInRange(0, 9);
       if (randomNum >= 4) {
         this.position += 1;
       }
   }
-  //현재 위치 메서드 구현
+  // 현재 위치 메서드 구현
   getPosition(){
     return '-'.repeat(this.position);
   }
@@ -27,12 +27,20 @@ class Race{
     this.attempts = attempts;
   }
 
-  //경주 시작 메서드
+  // 경주 시작 메서드
   run(){
     for (let i = 0; i < this.attempts; i++) {
       this.cars.forEach(car => car.move()); // 각 자동차 이동
       this.printPositions();
     }
+  
+  }
+  // 자동차 이름과 위치 출력 메서드
+  printPositions(){
+    this.cars.forEach(car => {
+      Console.print(`${car.name} : ${car.getPosition()}`); // 자동차 이름과 현재 위치 출력
+    });
+    Console.print('');
   }
 
 class App {
