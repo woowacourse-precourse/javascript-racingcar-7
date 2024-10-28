4bd0b267aafa351e494821afbcad9136b2c463bc
sanghyeonLee <hyeon012366@gmail.com>
2024-10-28 20:28:18 +0900

    refactor: 실행결과 메세지 출력하도록 단일 책임 원칙에 따라 메소드 추가 후 코드 이동

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -1,39 +1,38 @@
-import {Console, MissionUtils} from "@woowacourse/mission-utils";
+import {Console} from "@woowacourse/mission-utils";
 import {OUTPUT_MESSAGE, PROMPT_MESSAGE} from "./constants/message.js";
 import {Car} from "./Car.js";
-import {getFirstPromptError, getSecondPromptError} from "./utils/errorMessageHandler.js";
 import {inputHandler} from "./utils/inputHandler.js";
 import {setWinner, winCounter} from "./utils/raceHandler.js";
 
 class RacingGame {
-    promptSequence = 1 //이걸 명확하게? 스트링?
-    carArr = [] // race 클래스 생성자로 만들고
-    //코드를 갈자 걍 ㅋㅋ
-    async handleRaceSequence(promptMessage) {
+    promptSequence = 1
+    carArr = []
+
+    async handleRaceSequence() {
         const fistPrompt = await this.getInput(PROMPT_MESSAGE.FIRST)
         this.promptSequence++
         this.createCarList(fistPrompt)
         const secondPrompt = await this.getInput(PROMPT_MESSAGE.SECOND)
-        const result = this.playRace(secondPrompt)
-        this.getOutput(result)
+        this.displayResult(secondPrompt)
     }
-
-    //입력, 출력, 에러 처리 제외한 게임 로직을 클래스로 분리하자
 
     createCarList(prompt) {
         this.carArr = new Car().setCar(prompt)
     }
 
-    playRace(tryNum) {
-        // this.output("실행결과")
-        for (let i = 0; i < tryNum; i++) {
-            this.aggregationResult()
-        }
-        return winCounter(this.carArr)
+    displayResult(tryNum) {
+        Console.print(OUTPUT_MESSAGE.RESULT)
+        const winner = this.playRace(tryNum)
+        this.promptSequence++
+        this.getOutput(winner)
     }
 
-    aggregationResult() {
-        return this.getOutput()
+    playRace(tryNum) {
+        for (let i = 0; i < tryNum; i++) {
+            winCounter(this.carArr)
+            this.getOutput(this.carArr)
+        }
+        return setWinner(this.carArr)
     }
 
     async getInput(message) {
@@ -41,15 +40,14 @@ class RacingGame {
         return inputHandler(input)
     }
 
-    getOutput(data) { //최소 단위니까 이것도 나눠야?
+    getOutput(data) {
         if (this.promptSequence === 2) {
-            this.carArr.map((car) => Console.print(`${car.carName} : ${"-".repeat(car.winCnt)}`))
-            return Console.print("\n")
+            return data.map((data) => Console.print(`${data.carName} : ${"-".repeat(data.winCnt)}`)) && Console.print("\n")
         }
         if (this.promptSequence === 3) {
             return Console.print(OUTPUT_MESSAGE.WINNER + data)
         }
-        this.handleError()
+        return this.handleError()
     }
 
     handleError() {
