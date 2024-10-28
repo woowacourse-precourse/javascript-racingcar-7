867ccb1515607554b0e35edc7fe48758cbeac8d6
10012 <nas7062@naver.com>
2024-10-28 12:23:36 +0900

    feat(App) : App 클래스 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { Console, Random } from "@woowacourse/mission-utils"
+import InputView from "./InputView.js"
+import Car from "./Car.js";
+import { RESULT_STATUS } from "./constant.js";
 class App {
-  async run() {}
+  constructor() {
+    this.cars = [];
+    this.countInput = 0;
+  }
+  async run() {
+    const carInput = await InputView.CarNameInput();
+    this.countInput = await InputView.CountInput();
+    this.cars = carInput.map((name) => new Car(name));
+    this.getForwardPrint();
+    this.WinnerCheck();
+  }
+  getForwardPrint() {
+    Console.print(RESULT_STATUS.PROGRESS);
+    for (let i = 0; i < this.countInput; i++) {
+      this.cars.forEach((car, _) => {
+        const randomNumber = Random.pickNumberInRange(0, 9);
+        car.MoveForward(randomNumber);
+        Console.print(car.getPosition());
+      })
+      Console.print('');
+    }
+  }
+  WinnerCheck() {
+    const MAX = Math.max(...this.cars.map((car) => car.getForwardCount()));
+    const winners = this.cars
+      .filter((car) => car.getForwardCount() === MAX)
+      .map((car) => car.name)
+      .join(', ');
+    Console.print(`최종 우승자 : ${winners}`);
+  }
 }
 
 export default App;
