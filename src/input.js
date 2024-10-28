025812c1f739ebf87369374b3b20c6de6c994ebd
yoojin <everywavyday@gmail.com>
2024-10-28 20:14:37 +0900

    feat: 자동차 이름 입력 예외 추가

--- src/input.js
+++ src/input.js
@@ -5,7 +5,13 @@ class Input {
         const input = await Console.readLineAsync(
             '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
         );
-        return input.split(',').map((carName) => this.isValidCarName(carName));
+        const carNames = input.split(',').map((carName) => this.isValidCarName(carName));
+
+        if (carNames.length < 2) {
+            throw new Error('[ERROR] 자동차 이름은 2개 이상 입력해야 합니다.');
+        }
+
+        return carNames;
     }
 
     async getGameRound() {
@@ -21,6 +27,9 @@ class Input {
         }
         if (carName.length > 5) {
             throw new Error('[ERROR] 자동차 이름은 5자 이하이어야 합니다.');
+        }
+        if (/[^a-zA-Z0-9]/.test(carName)) {
+            throw new Error('[ERROR] 자동차 이름에는 특수 문자가 포함될 수 없습니다.');
         }
         return carName.trim();
     }
