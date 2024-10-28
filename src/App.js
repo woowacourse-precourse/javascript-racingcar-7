32541f7a35b6cd861a980f7e4a4ae8c7aa890949
Juyudang <ssuw223@gmail.com>
2024-10-28 18:13:20 +0900

    Feat: Exception hanling when name over 5 add "throw new Error" to handle name over 5 letters.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const INPUT = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-    const { PLAYERS, GOING } = setPlayer(INPUT);
-    const INPUT2 = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
-    printRace(INPUT2, PLAYERS, GOING);
-    setWinner(PLAYERS, GOING);
+    try {
+      const INPUT = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+      const { PLAYERS, GOING } = setPlayer(INPUT);
+      const INPUT2 = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+      printRace(INPUT2, PLAYERS, GOING);
+      setWinner(PLAYERS, GOING);
+    } catch (error) {
+      throw new Error("[ERROR]" + error.message);
+    }
   }
 }
 
 function setPlayer(INPUT) {
   const PLAYERS = INPUT.split(',');
+  PLAYERS.forEach(element => {
+    if (element.length > 5) {
+      throw new Error("이름의 길이가 5 보다 큽니다.");
+    }
+  });
   const GOING = Array.from({ length: PLAYERS.length }, () => "");
   return { PLAYERS, GOING }
 }
