53274c073b5dea7e8cb0726e905f53281d018375
YeWon Seo <syw1236@naver.com>
2024-10-28 20:27:19 +0900

    test(errorCarNameTest): 사용자가 입력한 자동차 이름 테스트

---
+++ __tests__/errorCarNameTest.js
@@ -0,0 +1,13 @@
+import Validator from '../src/Validator'
+import { errorMessage } from '../src/constants';
+
+describe('Validator', () => {
+    test('이름 에러 테스트', () => {
+        // 각 자동차의 progress를 업데이트하는 함수를 호출
+        const inputCarName = ['one','','two'];
+        expect(() => {
+            Validator.isFiveOrLess(inputCarName);
+        }).toThrow(new Error(errorMessage.isFiveOrMoreErrorMessage));
+    
+    })
+})
