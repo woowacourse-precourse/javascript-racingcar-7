3eebc54f73200d4ca5eb880c1c9fcf65b261d752
mywhzy <cmj2393a@naver.com>
2024-10-28 20:05:23 +0900

    feat: 실행 결과 출력 관련 상수 추가

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,12 @@ export const INPUT_PROMPT = {
   readCarNames: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
   readRoundCount: '시도할 횟수는 몇 회인가요?',
 };
+
+export const FORWARD_SYMBOL = '-';
+
+export const OUTPUT_MESSAGE = {
+  messageSeparator: ':',
+  winnerSeparator: ',',
+  executionResult: '실행 결과',
+  winner: '최종 우승자',
+};
