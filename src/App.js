3f70abc9e2da27b0e1d89fb7db7845f56a78c68d
BaeJia <qowldk1030@gmail.com>
2024-10-28 20:21:35 +0900

    feat: 자동차 이름 길이 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Console } from "@woowacourse/mission-utils";
-import { getCarNames, checkForDuplicates } from "./utils/index.js";
+import {
+  getCarNames,
+  checkForDuplicates,
+  hasInvalidCarNameLength,
+} from "./utils/index.js";
 
 class App {
   async run() {
     try {
       const carNames = await getCarNames();
       checkForDuplicates(carNames);
+      hasInvalidCarNameLength(carNames);
     } catch (error) {
       console.error(error.message);
     }
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,3 +1,6 @@
+const CARNAME_MIN_LENGTH = 1;
+const CARNAME_MAX_LENGTH = 5;
+
 export function checkForDuplicates(carNames) {
   const uniqueCarNames = new Set(carNames);
   if (uniqueCarNames.size !== carNames.length) {
@@ -8,3 +11,20 @@ export function checkForDuplicates(carNames) {
 
   return true;
 }
+
+// 자동차 이름 길이 수 체크
+export const isValidLength = (target, min, max) => {
+  return min <= target.length && target.length <= max;
+};
+
+// 자동차 이름 길이 유효성 검사
+export const hasInvalidCarNameLength = (carNames) => {
+  if (
+    carNames.some(
+      (carName) =>
+        !isValidLength(carName, CARNAME_MIN_LENGTH, CARNAME_MAX_LENGTH)
+    )
+  ) {
+    throw new Error("[ERROR] 자동차 이름의 길이가 유효하지 않습니다.");
+  }
+};
