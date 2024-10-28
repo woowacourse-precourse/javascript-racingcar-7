09097a507a820f909a5d1f6289e8b4876ded51a9
ShinjungOh <tlswjd9260@gmail.com>
2024-10-28 20:03:04 +0900

    refactor: RacingGameValidator 클래스 생성 및 예외 처리 메소드 분리

---
+++ src/RacingGameValidator.js
@@ -0,0 +1,17 @@
+class RacingGameValidator {
+  static validateCarNameLengthUnderSix(carNames) {
+    carNames.forEach((car) => {
+      if (car.length > 5) {
+        throw Error('[ERROR] 자동차 이름은 5자 이하만 가능합니다.');
+      }
+    });
+  }
+
+  static validateTryCountIsNumber (tryCount) {
+    if (isNaN(tryCount)) {
+      throw Error('[ERROR] 시도할 횟수는 숫자만 입력할 수 있습니다.');
+    }
+  }
+}
+
+export default RacingGameValidator;
--- src/View.js
+++ src/View.js
@@ -1,4 +1,5 @@
 import {MissionUtils} from "@woowacourse/mission-utils";
+import RacingGameValidator from "./RacingGameValidator.js";
 import Utils from "./Utils.js";
 
 class View {
@@ -6,11 +7,7 @@ class View {
     const input = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
     const carNames = input.toString().split(',');
 
-    carNames.forEach((car) => {
-      if (car.length > 5) {
-        throw Error('[ERROR] 자동차 이름은 5자 이하만 가능합니다.');
-      }
-    });
+    RacingGameValidator.validateCarNameLengthUnderSix(carNames);
 
     return carNames;
   }
@@ -19,9 +16,7 @@ class View {
     const input = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
     const tryCount = parseInt(input, 10);
 
-    if (isNaN(tryCount)) {
-      throw Error('[ERROR] 시도할 횟수는 숫자만 입력할 수 있습니다.');
-    }
+    RacingGameValidator.validateTryCountIsNumber(tryCount);
 
     return tryCount;
   }
