4a636d3df8572ed7cb23ffe41498cfa71e2b5307
Hyeok Lee <c235335@g.hongik.ac.kr>
2024-10-28 21:13:34 +0900

    feat: 우승자 탐색 구현, 출력 구현

--- src/Driver.js
+++ src/Driver.js
@@ -3,8 +3,9 @@ import { Random, Console } from "@woowacourse/mission-utils";
 export function Driver(nameList, number) {
   let score = ArrObjConvertor(nameList);
 
-  Console.print(RESULT_TITLE);
+  Console.print("\n실행 결과\n");
   score = EachTimePrinter(score, number);
+  WinnerPrinter(score);
 }
 
 function ArrObjConvertor(list) {
@@ -19,7 +20,14 @@ function EachTimePrinter(score, number) {
   if (number === 0) return score;
   for (let car of score) {
     score[car] += Random.pickNumberInRange(0, 9);
-    Console.print(`${car} : ${PROGRESS_BAR * score[bar]}\n`);
+    Console.print(`${car} : ${"-" * score[bar]}\n`);
   }
   Console.print("\n");
+}
+
+function WinnerPrinter(score) {
+  let max_score = 0;
+  for (let car of score) max_score = Math.max(score[car], max_score);
+  let winner = score.map((car) => score[car] == max_score);
+  Console.print("최종 우승자 : " + winner.join(", "));
 }
