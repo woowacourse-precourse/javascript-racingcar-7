83eea8114092a5c5b8911e7554c7c9185d54d707
soyeooooo <alththdus1@naver.com>
2024-10-28 18:05:59 +0900

    feat: 유효성 검증

---
+++ src/Validation.js
@@ -0,0 +1,28 @@
+import { ERROR } from './Message.js';
+
+export function validateName(names) {
+  for (const name of names) {
+    if (name === '') {
+      throw new Error(ERROR.nameNull);
+    }
+
+    if (name.length > 5) {
+      throw new Error(ERROR.nameLength);
+    }
+  }
+  if (new Set(names).size !== names.length) {
+    throw new Error(ERROR.nameDuplicate);
+  }
+}
+
+export function validateTryCount(tryCount) {
+  if (tryCount === '') {
+    throw new Error(ERROR.tryCountNull);
+  }
+  if (isNaN(tryCount)) {
+    throw new Error(ERROR.tryCountNan);
+  }
+  if (tryCount < 0) {
+    throw new Error(ERROR.tryCountMin);
+  }
+}
