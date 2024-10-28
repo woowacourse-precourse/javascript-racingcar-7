e83cea798fb952928c96b737ab67ff09d7c5830a
chickenisamazing <dieswing@naver.com>
2024-10-28 11:12:57 +0900

    feat: 경주할 자동차 목록 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 쉼표로 구분하여 입력해주세요. \n"
+    );
+
+    try {
+      const result = this.racing(input);
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+
+      throw error;
+    }
+  }
+
+  racing(input) {}
 }
 
 export default App;
