f5fdb940f5c8935bfa061acfa492a3fb626fc9b3
ryanbae94 <ryanbae94@gmail.com>
2024-10-28 15:05:58 +0900

    refactor (validation): freeze 적용
    - ERROR_MESSAGES에 Object.freeze를 적용하여 캡슐화

--- src/constants/validation.js
+++ src/constants/validation.js
@@ -1,4 +1,4 @@
-export const ERROR_MESSAGES = {
+export const ERROR_MESSAGES = Object.freeze({
 	hasWhiteSpace: '[ERROR] 공백을 포함할 수 없어요.',
 	hasNothing: '[ERROR] 값을 입력해 주세요.',
 	invalidNameLength: '[ERROR] 이름은 1~5자 사이로 입력해 주세요.',
@@ -7,4 +7,4 @@ export const ERROR_MESSAGES = {
 	isNaN: '[ERROR] 정수만 입력할 수 있어요.',
 	isNegative: '[ERROR] 1이상의 정수만 입력할 수 있어요.',
 	isRational: '[ERROR] 정수만 입력할 수 있어요.',
-};
+});
