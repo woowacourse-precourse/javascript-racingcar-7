fcac98b6d76a9f2d61f3633befa38bd4ac098e29
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2024-10-28 20:08:42 +0900

    fix: 입력문장 수정

--- src/UserInput.js
+++ src/UserInput.js
@@ -2,7 +2,7 @@ import { Console } from "@woowacourse/mission-utils";
 
 class UserInput {
   static MESSAGES = {
-    carNamePrompt: "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)\n",
+    carNamePrompt: "경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분)\n",
     attemptPrompt: "시도할 횟수는 몇 회인가요?\n",
   };
 
