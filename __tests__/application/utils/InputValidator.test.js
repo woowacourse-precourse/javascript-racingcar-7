dc58bbaeb904372b0494cc2bb02cbcbd91d14a7c
rheefine <rheefine@gmail.com>
2024-10-28 15:42:21 +0900

    test: Input 값이 null 혹은 빈칸이 아닐 경우에 대한 테스트 추가

--- __tests__/application/utils/InputValidator.test.js
+++ __tests__/application/utils/InputValidator.test.js
@@ -1,12 +1,24 @@
 import Validator from '../../../src/application/utils/InputValidator.js';
 
 describe('InputValidator class 테스트', () => {
-
-  describe('validateInputIsNull 메소트 테스트', () => {
-    test('Input 값이 null 혹은 빈칸일 경우에 대한 예외 빌생 확인', () => {
+  describe('validateInputIsNull 메소드 테스트', () => {
+    test('Input 값이 null 혹은 빈칸일 경우에 대한 예외 발생 여부 확인', () => {
+      // given
       const inputs = [null, ''];
+
       inputs.forEach((input) => {
+        // when & then
         expect(() => Validator.validateInputIsNull(input)).toThrow('[ERROR]');
+      });
+    });
+
+    test('Input 값이 null 혹은 빈칸이 아닐 경우에 대한 예외 발생 여부 확인', () => {
+      // given
+      const inputs = ['12', 'pobi,crong,jun', 'asd'];
+
+      inputs.forEach((input) => {
+        // when & then
+        expect(() => Validator.validateInputIsNull(input)).not.toThrow();
       });
     });
   });
