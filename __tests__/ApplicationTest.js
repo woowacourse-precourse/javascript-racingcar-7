a257555c9dd608b9161343521347fedfb55323b2
sanghee01 <sang6299@naver.com>
2024-10-28 20:29:03 +0900

    refactor(ApplicationTest): 테스트케이스 추가 및 테스트 처리 부분을 함수로 분리
    입력 테스트 케이스를 추가함
    또한 테스트 처리하는 부분을 함수로 분리하여 가독성과 생산성을 높임

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -24,37 +24,88 @@ const getLogSpy = () => {
   return logSpy;
 };
 
+// 기능 테스트 함수
+const functionalTestCase = async (inputs, randoms, logs) => {
+  mockQuestions(inputs);
+  mockRandoms(randoms);
+  const logSpy = getLogSpy();
+
+  const app = new App();
+  await app.run();
+
+  logs.forEach((log) => {
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+  });
+};
+
+// 예외 테스트 함수
+const exceptionTestCase = async (inputs, expectedError) => {
+  mockQuestions(inputs);
+
+  const app = new App();
+
+  await expect(app.run()).rejects.toThrow(expectedError);
+};
+
 describe('자동차 경주', () => {
-  test('기능 테스트', async () => {
-    // given
-    const MOVING_FORWARD = 4;
-    const STOP = 3;
-    const inputs = ['pobi,woni', '1'];
-    const logs = ['pobi : -', 'woni : ', '최종 우승자 : pobi'];
-    const logSpy = getLogSpy();
+  test('기능 테스트 - 우승자가 한 명인 경우', async () => {
+    await functionalTestCase(
+      ['pobi,woni', '1'],
+      [3, 4],
+      ['pobi : ', 'woni : -', '최종 우승자 : woni'],
+    );
+  });
 
-    mockQuestions(inputs);
-    mockRandoms([MOVING_FORWARD, STOP]);
+  test('기능 테스트 - 우승자가 여러 명일 경우', async () => {
+    await functionalTestCase(
+      ['pobi,woni', '2'],
+      [3, 2, 4, 4],
+      [
+        'pobi : ',
+        'woni : ',
+        '',
+        'pobi : -',
+        'woni : -',
+        '',
+        '최종 우승자 : pobi, woni',
+      ],
+    );
+  });
 
-    // when
-    const app = new App();
-    await app.run();
-
-    // then
-    logs.forEach((log) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-    });
+  test('기능 테스트 - 우승자가 여러 명일 경우 2', async () => {
+    await functionalTestCase(
+      ['pobi,woni,jun', '5'],
+      [4, 2, 4, 4, 4, 4, 5, 5, 5, 6, 4, 6, 4, 4, 4],
+      [
+        'pobi : -',
+        'woni : ',
+        'jun : -',
+        '',
+        'pobi : --',
+        'woni : -',
+        'jun : --',
+        '',
+        'pobi : ---',
+        'woni : --',
+        'jun : ---',
+        '',
+        'pobi : ----',
+        'woni : ---',
+        'jun : ----',
+        '',
+        'pobi : -----',
+        'woni : ----',
+        'jun : -----',
+        '',
+        '최종 우승자 : pobi, jun',
+      ],
+    );
   });
 
   test('예외 테스트', async () => {
-    // given
-    const inputs = ['pobi,javaji'];
-    mockQuestions(inputs);
-
-    // when
-    const app = new App();
-
-    // then
-    await expect(app.run()).rejects.toThrow('[ERROR]');
+    await exceptionTestCase(
+      ['pobi,javaji'],
+      '[ERROR] 자동차 이름은 5자 이하이어야 합니다.',
+    );
   });
 });
