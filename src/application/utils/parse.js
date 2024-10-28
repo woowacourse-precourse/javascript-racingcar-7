c7500ff80b664d3ba0e68b49f631af4ec6226250
rheefine <rheefine@gmail.com>
2024-10-28 16:22:14 +0900

    feat: 쉼표를 기준으로 자동차 이름을 분리하는 기능 구현

---
+++ src/application/utils/parse.js
@@ -0,0 +1,8 @@
+import CAR_NAME_SPLITER from '../../constants/Rule.js';
+
+const parse = function (rawCarNames) {
+  const carNames = rawCarNames.split(CAR_NAME_SPLITER);
+  return carNames.map((carName) => carName.trim());
+};
+
+export default parse;
---
+++ src/constants/Rule.js
@@ -0,0 +1,3 @@
+const CAR_NAME_SPLITER = Object.freeze(',');
+
+export default CAR_NAME_SPLITER ;
