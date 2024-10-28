e0501dd34ae2b14997bb4f9729df53611452b61a
jiucchu <jiucchu@gmail.com>
2024-10-28 09:43:05 +0900

    refactor: 출력 수정

--- src/controller/RacingGame.js
+++ src/controller/RacingGame.js
@@ -1,6 +1,6 @@
 import Car from "./Car.js";
-import RandomNumberGenerator from "./RandomNumberGenerator.js";
 import { Console } from "@woowacourse/mission-utils";
+import { Random } from "@woowacourse/mission-utils";
 
 class RacingGame {
   constructor(carNames, tryNumber) {
@@ -13,7 +13,7 @@ class RacingGame {
     Console.print('실행 결과');
     for (let i = 0; i < this.tryNumber; i++) {
       this.cars.forEach(car => {
-        if (RandomNumberGenerator.moveForward()) {
+        if (Random.pickNumberInRange(0, 9) >= 4) {
           car.move();
         }
       });
@@ -23,16 +23,15 @@ class RacingGame {
   }
 
   printNowRacing() {
-    Console.print('');
     this.cars.forEach(car => {
       Console.print(`${car.getName()} : ${'-'.repeat(car.getPosition())}`);
     });
+    Console.print('');
   }
 
   printWinners() {
     const maxPosition = Math.max(...this.cars.map(car => car.getPosition()));
     const winners = this.cars.filter(car => car.getPosition() === maxPosition).map(car => car.getName());
-    Console.print('');
     Console.print("최종 우승자 : " + winners.join(", "));
   }
 }
--- src/controller/RandomNumberGenerator.js
+++
@@ -1,9 +0,0 @@
-import { Random } from "@woowacourse/mission-utils";
-
-class RandomNumberGenerator {
-  static moveForward() {
-    return Random.pickNumberInRange(0, 9) >= 4;
-  }
-}
-
-export default RandomNumberGenerator;
