6eb640c2c3af1264c08374fa403e4b3729d254f5
Seoyeon Kim <kseoy0607@gmail.com>
2024-10-28 14:43:13 +0900

    feat: 사용자로부터 자동차 이름을 입력 받는 기능 구현

---
+++ src/views/InputView.js
@@ -0,0 +1,11 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+class InputView {
+  static async inputNames() {
+    return await MissionUtils.Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+    );
+  }
+}
+
+export default InputView;
