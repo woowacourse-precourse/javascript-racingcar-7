51997264273159749b8fb73b10fe4a3f3ada54d6
jakejun98 <111010564+jakejun98@users.noreply.github.com>
2024-10-28 18:31:53 +0900

    feat : 시도횟수 입력기능 구현
    자연수 1이상이 아닌 모든 경우 예외처리

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   constructor() {
     this.carNames = [];
+    this.attempts = 0;
   }
 
   async run() {
     try {
       await this.getCarNamesFromUser();
+      await this.getAttemptsFromUser();
     } catch (error) {
       console.error(error.message);
     }
@@ -15,7 +17,7 @@ class App {
 
   async getCarNamesFromUser() {
     const userInput = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)'
+      '경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분): '
     );
 
     const carNames = userInput.split(',').map(name => name.trim());
@@ -27,16 +29,31 @@ class App {
   validateCarNames(carNames) {
     carNames.forEach(name => {
       if (name === '') {
-        throw new Error('[ERROR] 공백입니다');
+        throw new Error('[ERROR] 공백입니다.');
       }
 
       if (name.length > 5) {
-        throw new Error(`[ERROR] 자동차 이름은 5자를 초과할 수 없습니다.`);
+        throw new Error('[ERROR] 자동차 이름은 5자를 초과할 수 없습니다.');
       }
     });
 
     if (new Set(carNames).size !== carNames.length) {
       throw new Error('[ERROR] 중복된 자동차 이름이 있습니다.');
+    }
+  }
+
+  async getAttemptsFromUser() {
+    const attemptsInput = await Console.readLineAsync('시도할 횟수를 입력하세요: ');
+
+    const attempts = Number(attemptsInput);
+    this.validateAttempts(attempts);
+
+    this.attempts = attempts;
+  }
+
+  validateAttempts(attempts) {
+    if (!Number.isInteger(attempts) || attempts < 1) {
+      throw new Error('[ERROR] 시도 횟수는 1 이상의 자연수여야 합니다.');
     }
   }
 }
