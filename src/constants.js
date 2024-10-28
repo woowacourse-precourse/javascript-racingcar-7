b4d83954e04ad6c48660d114b38f9d341503073f
inaemin <inaemin@gmail.com>
2024-10-28 17:49:53 +0900

    fix: MIN_CARS 값을 1에서 2로 변경

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const MAX_NAME_LENGTH = 5; // 자동차 이름의 최대 허용 길이
 export const MOVE_FORWARD_MIN_VALUE = 4; // 전진하기 위한 최소값
-export const MIN_CARS = 1; // 경주에 필요한 최소 자동차의 개수
+export const MIN_CARS = 2; // 경주에 필요한 최소 자동차의 개수
