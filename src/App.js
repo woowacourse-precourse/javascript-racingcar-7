0a39c24ab36df6d201cc42371fb879fc5098a2c6
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-10-28 15:31:41 +0900

    fix: console 문자열

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App {
 
   async getRaceCars() {
     const inputCarNames = await Console.readLineAsync(
-      "경주할 자동차 이름(이름은 쉽표(,) 기준으로 구분)\n"
+      "경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분)\n"
     );
     this.validateCarNames(inputCarNames);
 
