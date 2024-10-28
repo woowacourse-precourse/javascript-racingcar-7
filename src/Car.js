a5277b31215c83bfeac7f64dc0af3ab240f6f7e6
OhSeChan <seb_s19@naver.com>
2024-10-28 17:26:25 +0900

    feat(Car): 자동차 이동 기능 추가
    - 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우 자동차
      위치를 이동시키는 기능 구현

--- src/Car.js
+++ src/Car.js
@@ -1,3 +1,5 @@
+import { getRandomNumber } from './utils/getRandomNumber.js';
+
 class Car {
   #name;
   #position;
@@ -5,6 +7,13 @@ class Car {
   constructor(name) {
     this.#name = name;
     this.#position = 0;
+  }
+
+  move() {
+    const randomNumber = getRandomNumber();
+    if (randomNumber < 4) return;
+
+    this.#position += 1;
   }
 
   get name() {
