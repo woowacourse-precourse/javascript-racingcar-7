7da357076a9865899256ef0cc54e7dfb0b2703aa
alicehrk <81912837+alicehrk@users.noreply.github.com>
2024-10-28 16:20:29 +0900

    feat: 입력 형식 통일

--- src/App.js
+++ src/App.js
@@ -13,10 +13,11 @@ function validateCarsInput(carsInput) {
 
 class App {
   async run() {
-    const cars = await Console.readLineAsync(
+    let cars = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
     validateCarsInput(cars);
+    cars = cars.split(",").map((car) => car.trim());
   }
 }
 
