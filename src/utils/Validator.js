18f37eabd3cba9ecd5d6d23b91ea6dea969ee433
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2024-10-28 21:32:10 +0900

    feat : 유효성 검사

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -1,5 +1,25 @@
-class Validator { 
+import Constants from './constants.js';
+class Validator {
+  static validateCarNames(carNames) {
+    if (!carNames || carNames.trim() === "") {
+      throw new Error(Constants.ERROR_MESSAGES.EMPTY_CAR_NAME);
+    }
+    const names = carNames.split(",");
+    names.forEach((name) => {
+      if (name.length > 5) {
+        throw new Error(Constants.ERROR_MESSAGES.CAR_NAME_LENGTH);
+      }
+      if (name.trim() === "") {
+        throw new Error(Constants.ERROR_MESSAGES.EMPTY_CAR_NAME);
+      }
+    });
+  }
 
+  static validateAttemptCount(attemptCount) {
+    if (isNaN(attemptCount) || attemptCount <= 0) {
+      throw new Error(Constants.ERROR_MESSAGES.INVALID_ATTEMPT_COUNT);
+    }
+  }
 }
 
 export default Validator;
