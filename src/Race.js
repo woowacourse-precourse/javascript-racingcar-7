5bab0fde7726022da7880256c36099eef9d1b66c
OhSeChan <seb_s19@naver.com>
2024-10-28 17:44:55 +0900

    feat(Race): 차수별 경주 결과 출력 기능 추가
    - printCarPosition 메서드로 개별 자동차의 이름과 위치 출력
    - printRoundResult 메서드로 라운드가 끝날 때마다 모든 자동차의 위치 출력
    - start 메서드에서 경주 실행 시 라운드 결과가 출력되도록 변경

--- src/Race.js
+++ src/Race.js
@@ -1,3 +1,4 @@
+import { Console } from '@woowacourse/mission-utils';
 import Car from './Car.js';
 
 class Race {
@@ -13,13 +14,24 @@ class Race {
     this.#cars.forEach((car) => car.move());
   }
 
+  #printCarPosition(car) {
+    Console.print(`${car.name} : ${'-'.repeat(car.position)}`);
+  }
+
+  #printRoundResult() {
+    this.#cars.forEach((car) => this.#printCarPosition(car));
+    Console.print('');
+  }
+
   #runRounds() {
     for (let i = 0; i < this.#roundCount; i++) {
       this.#moveCars();
+      this.#printRoundResult();
     }
   }
 
   start() {
+    Console.print('\n실행 결과');
     this.#runRounds();
   }
 }
