ec696724f98b81f735bc82963ef9135b61aaf3fa
dudcns02 <duddl2640@naver.com>
2024-10-28 17:20:59 +0900

    feat: Add log message
    Run results, add final winner message

--- src/content.js
+++ src/content.js
@@ -2,6 +2,8 @@ const LOG_MESSAGE = Object.freeze({
   START_MESSAGE:
     "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
   TRY_NUM_MESSAGE: "시도할 횟수는 몇 회인가요?\n",
+  EXECUTION_RESULT: "\n실행 결과",
+  FINAL_WINNER: "최종 우승자 : ",
 });
 const ERROR_MESSAGE = Object.freeze({
   INCORRECT_VALUE: "[ERROR] 공백은 입력할 수 없습니다.",
