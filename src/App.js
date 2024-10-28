a70a609115630082281db2a1f936381884080bf3
yewonhihi <yewon1118@naver.com>
2024-10-28 20:05:00 +0900

    style(App.js): 문구 오탈자 해결

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Random, Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const inputCars = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n");
+    const inputCars = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     const inputCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
 
     let carsList = this.getCarList(inputCars);
