74336fc2a86a0775a4aea283a302b7751316d070
COMPOSEDKIM <composedkim@naver.com>
2024-10-28 16:31:40 +0900

    feat: 우승자 출력

--- src/App.js
+++ src/App.js
@@ -10,6 +10,20 @@ class App {
       this.carMove(userCarObj);
       this.showRace(userCarObj);
     }
+    const winCar = this.findWinner(userCarObj);
+
+    let winner = "";
+    winCar.forEach((car, index) => {
+      winner = winner + car.carname;
+      if (index < winCar.length - 1) {
+        winner = winner + ", ";
+      }
+    });
+    if (winCar.length === 0) {
+      MissionUtils.Console.print("자동차가 움직이지 않아 우승자가 없습니다.");
+    } else {
+      MissionUtils.Console.print("최종 우승자 : " + winner);
+    }
   }
 
   async getCarName() {
@@ -51,6 +65,17 @@ class App {
       MissionUtils.Console.print(`${car.carname} : ${"-".repeat(car.movecar)}`);
     });
     MissionUtils.Console.print("\n");
+  }
+
+  findWinner(car) {
+    const moveCarValue = car.map((car) => car.movecar);
+    const maxValue = Math.max(...moveCarValue);
+    const winner = car.filter((car) => car.movecar === maxValue);
+
+    if (maxValue === 0) {
+    }
+
+    return winner;
   }
 }
 
