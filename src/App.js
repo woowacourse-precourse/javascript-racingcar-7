aab9f7db7ae4b4f6f174a093e079055206b403c9
kimzini <rlawlsdl11111@gmail.com>
2024-10-28 12:26:50 +0900

    feat: 오류 처리 일관성 개선

--- src/App.js
+++ src/App.js
@@ -6,21 +6,34 @@ class App {
   async run() {
     try {
       const carNames = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n");
-      this.handleCarNames(carNames);
+      this.validateCarNames(carNames);
+      await this.handleCarNames(carNames);
     } catch (error) {
       Console.print(error.message);
+      throw error;
     }
+  }
+
+  validateCarNames(carNames) {
+    const names = carNames.split(",").map((name) => name.trim());
+    names.forEach((name) => {
+      if (name.length > 5) {
+        throw new Error(ERROR_MESSAGES.INVALID_CAR_NAME);
+      }
+    });
   }
 
   async handleCarNames(carNames) {
     try {
       const raceCountInput = await Console.readLineAsync("시도할 횟수는 몇 회인가요? \n");
       const raceCount = this.validateRaceCount(raceCountInput);
+      Console.print("\n실행 결과");
       const game = new Game(carNames, raceCount);
       game.startRace();
-      Console.print(`최종 우승자: ${game.getWinners()}`);
+      Console.print(`최종 우승자 : ${game.getWinners()}`);
     } catch (error) {
       Console.print(error.message);
+      throw error;
     }
   }
 
@@ -28,7 +41,14 @@ class App {
     if (isNaN(raceCount)) {
       throw new Error(ERROR_MESSAGES.INVALID_RACE_COUNT);
     }
-    return parseInt(raceCount, 10);
+
+    const raceCountNumber = Number(raceCount);
+
+    if (raceCountNumber === 0) {
+      throw new Error(ERROR_MESSAGES.INVALID_RACE_COUNT_ZERO);
+    }
+
+    return raceCountNumber;
   }
 }
 
