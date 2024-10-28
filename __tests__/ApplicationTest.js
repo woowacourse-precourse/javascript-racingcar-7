cc98f8dfa62fac7f718e7f23dc5fb2ac70f29ed9
정해성 <gotjd0230@naver.com>
2024-10-28 22:55:09 +0900

    test: 테스트 코드 중복 제거

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -24,40 +24,69 @@ const getLogSpy = () => {
   return logSpy;
 };
 
-describe('자동차 경주', () => {
-  test('기능 테스트', async () => {
-    const MOVING_FORWARD = 4;
-    const STOP = 3;
-    const inputs = ['pobi,woni', '1'];
-    const logs = ['pobi : -', 'woni : ', '최종 우승자 : pobi'];
-    const logSpy = getLogSpy();
+const testHappyPathTemplate = async (inputs, randomStates, logs) => {
+  const logSpy = getLogSpy();
 
+  mockQuestions(inputs);
+  mockRandoms(randomStates);
+
+  const app = new App();
+  await app.run();
+
+  logs.forEach((log) => {
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+  });
+};
+
+const testExeptionTamplate = (query, inputs) => {
+  test(query, async () => {
     mockQuestions(inputs);
-    mockRandoms([MOVING_FORWARD, STOP]);
 
     const app = new App();
-    await app.run();
 
-    logs.forEach((log) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    await expect(app.run()).rejects.toThrow('[ERROR]');
+  });
+};
+
+describe('자동차 경주 게임 테스트', () => {
+  describe('자동차 경주', () => {
+    const GO = 4;
+    const STOP = 3;
+
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+
+    test('기능 테스트', async () => {
+      const inputs = ['pobi,woni', '1'];
+      const randomStates = [GO, STOP];
+      const logs = ['pobi : -', 'woni : ', '최종 우승자 : pobi'];
+
+      testHappyPathTemplate(inputs, randomStates, logs);
+    });
+
+    test('복수 우승자 테스트', async () => {
+      const inputs = ['pobi,woni', '1'];
+      const randomStates = [GO, GO];
+      const logs = ['pobi : -', 'woni : -', '최종 우승자 : pobi, woni'];
+
+      testHappyPathTemplate(inputs, randomStates, logs);
     });
   });
 
-  test('복수 우승자 테스트', async () => {
-    const MOVING_FORWARD = 4;
-    const STOP = 3;
-    const inputs = ['pobi,woni,jun', '1'];
-    const logs = ['pobi : -', 'woni : -', 'jun : -', '최종 우승자 : pobi, woni, jun'];
-    const logSpy = getLogSpy();
-
-    mockQuestions(inputs);
-    mockRandoms([MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD]);
-
-    const app = new App();
-    await app.run();
-
-    logs.forEach((log) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-    });
+  describe('예외 테스트', () => {
+    testExeptionTamplate('이름 없음 예외', ['']);
+    testExeptionTamplate('빈 이름 예외', ['pobi,,wone']);
+    testExeptionTamplate('한 명 예외', ['pobi']);
+    testExeptionTamplate('5자 초과 이름 예외', ['pobidia,woni']);
+    testExeptionTamplate('이름 중복 예외', ['pobi,pobi,woni']);
+    testExeptionTamplate('0회 시도 예외', ['pobi,woni', 0]);
+    testExeptionTamplate('미입력 시도 예외', ['pobi,woni', '']);
+    testExeptionTamplate('최대 시도 횟수 초과 예외', ['pobi,woni', 1234567890123456]);
+    testExeptionTamplate('문자 시도 입력 예외', ['pobi,woni', 'x']);
+    testExeptionTamplate('소수 시도 입력 예외', ['pobi,woni', '3.5']);
+    testExeptionTamplate('수식 시도 입력 예외', ['pobi,woni', '3+3']);
+    testExeptionTamplate('공백 시도 입력 예외', ['pobi,woni', ' ']);
+    testExeptionTamplate('빈 문자열 시도 입력 예외', ['pobi,woni', '']);
   });
 });
