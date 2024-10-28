c53f026b3868781a013721984f7004f41a5c095f
Dobbymin <gmin990624@gmail.com>
2024-10-28 14:55:16 +0900

    refactor(view): view 함수 모듈화 구현

---
+++ src/view/view.js
@@ -0,0 +1,13 @@
+import { inputRacingCarName } from '../view/functions/inputRacingCarName.js';
+import { inputTryNumber } from '../view/functions/inputTryNumber.js';
+import { printWinner } from '../view/functions/printWinner.js';
+import { progressRacingCar } from '../view/functions/progressRacingCar.js';
+import { printRunRacingCar } from '../view/functions/printRunRacingCar.js';
+
+export {
+  inputRacingCarName,
+  inputTryNumber,
+  printWinner,
+  progressRacingCar,
+  printRunRacingCar,
+};
