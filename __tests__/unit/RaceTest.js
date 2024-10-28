ad2cf111674485bde72a97ca05fcf491a4920010
Daeuning <happyjde3@gmail.com>
2024-10-28 09:28:27 +0900

    test(RaceTest): getWinners 메소드 테스트 코드 수정

--- __tests__/unit/RaceTest.js
+++ __tests__/unit/RaceTest.js
@@ -1,6 +1,6 @@
 import Race from '../../src/Race.js';
 import Car from '../../src/Car.js';
-import { getRandomNumber, printMessage } from '../../src/common/utils.js';
+import { getRandomNumber } from '../../src/common/utils.js';
 
 jest.mock('../../src/common/utils.js', () => ({
   ...jest.requireActual('../../src/common/utils.js'),
@@ -33,34 +33,32 @@ describe("tryMoveCar() 테스트", () => {
 });
 
 describe("getWinners() 테스트", () => {
-  const testCases = [
-    {
-      description: "최종 우승자를 올바르게 결정해야 한다",
-      distances: [6, 4, 2],
-      expectedWinners: ["Audi"]
-    },
-    {
-      description: "최종 우승자가 여러 명일 때도 올바르게 결정해야 한다",
-      distances: [5, 5, 3],
-      expectedWinners: ["Audi", "BMW"]
-    }
-  ];
+  const CARS = ["Audi", "BMW", "Ford"];
+  
+  const initializeRaceWithDistances = (distances) => {
+    const race = new Race();
+    race.cars = CARS.map((name) => new Car(name));
+    distances.forEach((distance, index) => race.cars[index].move(distance));
+    return race;
+  };
 
-  testCases.forEach(({ description, distances, expectedWinners }) => {
-    test(description, () => {
-      const race = new Race();
-      race.cars = [
-        new Car("Audi"),
-        new Car("BMW"),
-        new Car("Ford")
-      ];
-      jest.clearAllMocks();
+  test("최종 우승자를 올바르게 결정해야 한다", () => {
+    const distances = [6, 4, 2];
+    const expectedWinners = ["Audi"];
+    const race = initializeRaceWithDistances(distances);
 
-      distances.forEach((distance, index) => race.cars[index].move(distance));
+    const winners = race.getWinners();
 
-      const winners = race.getWinners();
+    expect(winners).toEqual(expectedWinners);
+  });
 
-      expect(winners).toEqual(expectedWinners);
-    });
+  test("최종 우승자가 여러 명일 때도 올바르게 결정해야 한다", () => {
+    const distances = [5, 5, 3];
+    const expectedWinners = ["Audi", "BMW"];
+    const race = initializeRaceWithDistances(distances);
+
+    const winners = race.getWinners();
+
+    expect(winners).toEqual(expectedWinners);
   });
 });
