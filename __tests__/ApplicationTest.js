c9620e65cd0b01f84576b73bf3b5b35c16dd685c
hanheel <hanhee630630@gmail.com>
2024-10-28 14:48:30 +0900

    feat(ApplicationTest) : Add test cases for input validation logic
    횟수 및 이름 입력 유효성 검사 함수 테스트 코드 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,7 @@
 import App from "../src/App.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
+import SetCarMovementModel from "../src/model/SetCarMovementModel.js";
+import InputView from "../src/view/InputView.js";
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -25,7 +27,30 @@ const getLogSpy = () => {
 };
 
 describe("자동차 경주", () => {
-  test("기능 테스트", async () => {
+  test("이름과 횟수를 입력받아 랜덤한 2차원 배열로 반환할 수 있다", async () => {
+    //given
+    const TRY_COUNT = 2;
+    const carNames = ["pobi", "woni"];
+    const expectResult = [
+      [4, 2],
+      [1, 3],
+    ];
+    mockRandoms([4, 2, 1, 3]);
+
+    //when
+    const carMovementModel = new SetCarMovementModel();
+    const carMovement = carMovementModel.setCarMovementValues(
+      carNames,
+      TRY_COUNT
+    );
+
+    //then
+    expect(carMovement).toEqual(expectResult);
+  });
+
+  test("자동차 별로 매 순간의 이동 거리를 계산할 수 있다", async () => {});
+
+  test("전체 기능 테스트", async () => {
     // given
     const MOVING_FORWARD = 4;
     const STOP = 3;
@@ -46,15 +71,26 @@ describe("자동차 경주", () => {
     });
   });
 
-  test("예외 테스트", async () => {
-    // given
-    const inputs = ["pobi,javaji"];
+  test("예외처리1 : 비어있는 사용자 이름", async () => {
+    const inputs = ["pobi,javaji,,jake"];
     mockQuestions(inputs);
 
+    //when
+    const inputView = new InputView();
+
+    //then
+    await expect(inputView.getCarNames()).rejects.toThrow("[ERROR]");
+  });
+
+  test("예외처리2 : 시도할 횟수가 0이하인 경우", async () => {
+    // given
+    const TRY_COUNT = ["-1"];
+    mockQuestions(TRY_COUNT);
+
     // when
-    const app = new App();
+    const inputView = new InputView();
 
     // then
-    await expect(app.run()).rejects.toThrow("[ERROR]");
+    await expect(inputView.getTryCount()).rejects.toThrow("[ERROR]");
   });
 });
