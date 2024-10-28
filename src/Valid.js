0d89547f522992d9f1a4a3aa65db8ac4f16b70c7
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2024-10-28 21:54:28 +0900

    fix: [ERROR] 추가

--- src/Valid.js
+++ src/Valid.js
@@ -3,15 +3,15 @@ export const nameStrValid = (NAMESTR) => {
     const nameStr = NAMESTR.toString()
 
     if (NAMESTR === '') {
-        throw new Error("경주할 자동차 이름을 입력해주세요.")
+        throw new Error("[ERROR] 경주할 자동차 이름을 입력해주세요.")
     }
 
     if (nameStr.match(/,/) === null) {
-        throw new Error("쉼표를 사용해주세요.")
+        throw new Error("[ERROR] 쉼표를 사용해주세요.")
     }
 
     if (nameStr.match(/,,/)) {
-        throw new Error("쉼표를 한 번만 사용해주세요.")
+        throw new Error("[ERROR] 쉼표를 한 번만 사용해주세요.")
     }
 
 }
@@ -19,11 +19,12 @@ export const nameStrValid = (NAMESTR) => {
 export const runTimesValid = (RUNTIMES) => {
 
     if (RUNTIMES === '') {
-        throw new Error("횟수를 입력해주세요")
+        throw new Error("[ERROR] 횟수를 입력해주세요")
     }
 
+
     if (isNaN(RUNTIMES)) {
-        throw new Error("숫자를 입력해주세요")
+        throw new Error("[ERROR] 숫자를 입력해주세요")
     }
 
 }
