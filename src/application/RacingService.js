78a70f276f8b7df5214d8e9b37f5bcd463f9582c
rheefine <rheefine@gmail.com>
2024-10-28 20:30:19 +0900

    feat: Application Layer에서 Domain Layer의 결과를 Presentation Layer에 전달

--- src/application/RacingService.js
+++ src/application/RacingService.js
@@ -1,15 +1,23 @@
 import InputPort from '../ports/InputPort.js';
+import OutputPort from '../ports/OutputPort.js';
 import RacingInputProcessor from './RacingInputProcessor.js';
+import RacingProcessor from './RacingProcessor.js';
 
 export default class RacingService {
+  #outputPort;
   #racingInputProcessor;
+  #raceProcessor;
   #racing;
 
-  constructor (inputPort = InputPort) {
+  constructor (inputPort = InputPort, outputPort = OutputPort) {
+    this.#outputPort = outputPort;
     this.#racingInputProcessor = new RacingInputProcessor(inputPort);
+    this.#raceProcessor = new RacingProcessor(this.#outputPort);
   }
 
   async execute () {
     this.#racing = await this.#racingInputProcessor.createRacing();
+    this.#raceProcessor.processRacing(this.#racing);
+    this.#outputPort.displayWinners(this.#racing.getWinners());
   }
 }
