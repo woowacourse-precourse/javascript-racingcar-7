a85848752e1a3da3f3d1f9e75519f34eebe00c73
Daeuning <happyjde3@gmail.com>
2024-10-28 12:56:30 +0900

    refactor(validatorsTest): 코드 간 공백 조절

--- __tests__/unit/validatorsTest.js
+++ __tests__/unit/validatorsTest.js
@@ -5,25 +5,21 @@ const ERROR_MESSAGE = "[ERROR]";
 describe("validateCarName() 테스트", () => {
   test("자동차 이름은 5자 이하만 가능하다", () => {
     const input = "Ferrari,Mercedes,Audi,Porsche,BMW,LongNameCar";
-
     expect(() => validateCarName(input)).toThrow(ERROR_MESSAGE);
   });
 
   test("자동차 이름이 존재해야 한다 (빈 문자열일 경우)", () => {
     const input = "";
-
     expect(() => validateCarName(input)).toThrow(ERROR_MESSAGE);
   });
 
   test("자동차 이름은 중복되어서는 안된다", () => {
     const input = "BMW,Audi,BMW";
-
     expect(() => validateCarName(input)).toThrow(ERROR_MESSAGE);
   });
 
   test("유효한 자동차 이름을 입력했을 때는 예외가 발생하지 않아야 한다", () => {
     const input = "Audi,BMW,Kia";
-
     expect(() => validateCarName(input)).not.toThrow();
   });
 });
@@ -41,12 +37,10 @@ describe("validateAttemptCount() 테스트", () => {
 
   test("시도할 횟수를 입력해야 한다 (빈 문자열일 경우)", () => {
     const input = "";
-
     expect(() => validateAttemptCount(input)).toThrow(ERROR_MESSAGE);
   });
 
   test.each(INVALID_ATTEMPTS_COUNT_CASES)("시도 횟수에는 양의 정수값만 입력해야 한다", (input) => {
-
     expect(() => validateAttemptCount(input)).toThrow(ERROR_MESSAGE);
   });
 });
