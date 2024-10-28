8b3d5938b2c445e1e0b6c55010112b4163293d09
Hyeok Lee <c235335@g.hongik.ac.kr>
2024-10-28 21:08:26 +0900

    feat: 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    let nameInput = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    let numberInput = await Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    );
+  }
 }
 
 export default App;
