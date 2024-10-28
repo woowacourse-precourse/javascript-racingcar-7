74bf48e7b212a81d31c6be99ce423b5964dfc467
ahhyun <wndkgus0225@naver.com>
2024-10-28 21:40:05 +0900

    feat(error) : 경로 수정

--- src/error.js
+++ src/error.js
@@ -1,4 +1,4 @@
-import { ERROR_MESSAGE } from "./message.js";
+import { ERROR_MESSAGE } from "../src/message.js";
 
 export function validateCarNames(names) {
   names.forEach(name => {
@@ -8,10 +8,9 @@ export function validateCarNames(names) {
   });
 }
 
-
 export function validateCount(count) {
   if (isNaN(count)) throw new Error(ERROR_MESSAGE.NOT_NUMBER);
   if (!Number.isInteger(Number(count))) throw new Error(ERROR_MESSAGE.NOT_INTEGER);
-  if (count = 0) throw new Error(ERROR_MESSAGE.NUM_IS_ZERO);
+  if (count === 0) throw new Error(ERROR_MESSAGE.NUM_IS_ZERO);
   if (count < 0) throw new Error(ERROR_MESSAGE.NEGATIVE_NUMBER);
 }
