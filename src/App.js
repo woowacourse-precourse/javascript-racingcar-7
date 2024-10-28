7d427a638f1f036647b4e93cff74f18d927d8d1d
thisisyejin <yejink321@gmail.com>
2024-10-28 12:38:00 +0900

    refactor: 사용자 입력 함수 분리
    사용자로부터 입력을 받고 유효성을 검사하는 부분을
    별도의 함수로 분리하여, app.run이 함수를 실행하는
    역할만 담당하도록 하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { inputCarName, inputTryTime } from "./inputHandler.js";
 import Car from "./Car.js"
-import { isValidNumber } from "./validation.js";
 
 class App {
   async run() {
     try {
-      const carName = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-      const carList = Car.makeCarList(carName);
-
-      const tryTime = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
-      isValidNumber(tryTime);
-
+      const carList = await inputCarName();
+      const tryTime = await inputTryTime();
       Car.race(carList, tryTime);
-
       Car.whoIsWinner(carList);
 
     } catch (err) {
