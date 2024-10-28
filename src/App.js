de2658550a15464d476a1b3c447220f79ad24f74
kim-museong <antjd0419@gmail.com>
2024-10-28 15:22:23 +0900

    feat(input) : 사용자 입력 기능 구현
    자동차를 입력했을 때 해당 값을 변경하여 관리하는 기능을 구현했습니다.
    1. 사용자가 입력한 값을 각 이름별로 배열화 합니다.
    2. 해당 값들이 요구사항에 맞는 값인지 확인을 합니다.
    3. 해당 배열을 객체화하여 cars라는 배열에 저장합니다. -> 각 자동차별로 현채위치를 관리하기 위해서 position이라는 값을 추가하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  constructor() {
+    this.cars = [];
+    this.moveCount = 0;
+  }
+
+  async run() {
+    Console.print('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+
+    const carNames = await this.getCarNames();
+    this.createCars(carNames);
+  }
+
+  async getCarNames() {
+    const namesInput = await Console.readLineAsync('');
+
+    const nameList = namesInput.split(',').map((name) => name.trim());
+
+    if (nameList.some((name) => name.length > 5 || name === '')) {
+      this.onError();
+    }
+
+    return nameList;
+  }
+
+  createCarObejct(names) {
+    this.cars = names.map((name) => ({ name, position: 0 }));
+  }
+
+  onError() {
+    throw new Error('[ERROR]');
+  }
 }
 
 export default App;
