2969ecc28f1325be67bb99642a43f718c3c94efc
Whatdoyumin <lym4963@naver.com>
2024-10-28 22:42:12 +0900

    feat: 자동차 이동 표시 및 차수별 실행 결과 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { getCarList, getRound } from './utils/userInput.js';
+import { StartRacingGame } from './services/startRacingGame';
 class App {
   async run() {
     const carList = await getCarList();
     const round = await getRound();
+    const score = new Array(carList.length).fill('');
+
+    StartRacingGame(carList, round, score);
   }
 }
 
