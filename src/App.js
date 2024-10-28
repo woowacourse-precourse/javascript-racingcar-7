95ff53c7051d9b8853426d58a40c95a4860a5584
giseon <pakox123@gmail.com>
2024-10-28 22:12:09 +0900

    feat: App에서 자동차 이름과 시도 횟수 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const carNames = await this.getCarNames();
+      const maxAttempts = await this.getAttempts();
+
+    } catch (error) {
+      Console.print(error.message);
+      return Promise.reject(error);
+    }
+  }
+
+  async getCarNames() {
+    const input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요 (쉼표로 구분): ");
+    return Validate.validateCarNames(input);
+  }
+
+  async getAttempts() {
+    const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?: ");
+    return Validate.validateAttempts(input); 
+  }
 }
 
 export default App;
