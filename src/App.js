b223878bd0987bdc91205e1e1d6766e553c54329
chickenisamazing <dieswing@naver.com>
2024-10-28 23:36:13 +0900

    refactor: 불필요한 주석 제거와 최종 우승자 메서드 코드 수정

--- src/App.js
+++ src/App.js
@@ -6,15 +6,11 @@ class App {
       const input = await MissionUtils.Console.readLineAsync(
         "경주할 자동차 이름을 쉼표로 구분하여 입력해주세요. \n"
       );
-
-      //자동차 목록 입력 유효성 검증
       this.validateInput(input);
 
       const count = await MissionUtils.Console.readLineAsync(
         "시도할 횟수는 몇 회인가요? \n"
       );
-
-      //횟수 입력 유효성 검증
       this.validateCount(count);
 
       const { resultList, parsed } = this.racing(input, count);
@@ -43,7 +39,6 @@ class App {
     this.validateEmptyCarNames(cars);
     this.validateCarNameLength(cars);
     this.validateDuplicateCarNames(cars);
-
     return;
   }
 
@@ -72,6 +67,7 @@ class App {
       throw new Error("[ERROR]");
     }
   }
+
   validateInput(input) {
     const cars = input.split(",").map((item) => item.trim());
     this.validateEmptyCarNames(cars);
@@ -121,7 +117,6 @@ class App {
     this.validateCountIsInteger(count);
   }
 
-  // count가 숫자가 아닐 경우
   validateCountIsNumber(count) {
     if (isNaN(count)) {
       MissionUtils.Console.print("[ERROR] 경주 횟수는 숫자여야 합니다.");
@@ -129,7 +124,6 @@ class App {
     }
   }
 
-  // count가 0또는 음수일 경우
   validateCountIsPositive(count) {
     if (count <= 0) {
       MissionUtils.Console.print("[ERROR] 경주 횟수는 1회 이상이어야 합니다.");
@@ -137,7 +131,6 @@ class App {
     }
   }
 
-  //count가 양수이지만 정수가 아닌 소수일 경우
   validateCountIsInteger(count) {
     if (!Number.isInteger(Number(count))) {
       MissionUtils.Console.print("[ERROR] 경주 횟수는 정수여야 합니다.");
@@ -147,17 +140,12 @@ class App {
 
   racing(input, count) {
     const arr = input.split(",").map((item) => item.trim());
-
-    //scoreArray는 각 자동차의 득점을 저장할 배열이다.
     const scoreArray = arr.map(() => 0);
-
-    //중간 결과를 저장
     const raceResult = [];
 
     console.log("");
     console.log("실행결과");
 
-    //경주가 한 번씩 실행되는 턴
     for (let i = 0; i < count; i++) {
       const currentRace = this.updateCurrentRace(arr, scoreArray);
       raceResult.push(currentRace);
@@ -166,10 +154,8 @@ class App {
     return { resultList: raceResult, parsed };
   }
 
-  //매번 경기를 업데이트하는 메서드
   updateCurrentRace(arr, scoreArray) {
     const currentRace = {};
-
     arr.forEach((car, index) => {
       const num = MissionUtils.Random.pickNumberInRange(0, 9);
       if (num >= 4) {
@@ -177,31 +163,26 @@ class App {
       }
       currentRace[car] = "-".repeat(scoreArray[index]);
     });
-
     return currentRace;
   }
 
-  //우승자 판별 로직 메서드
   determineWinner(arr, scoreArray) {
-    //scoreArr에서 최댓값을 찾고 그와 index가 같은 우승자를 arr에서 가져오자
     const max = Math.max(...scoreArray);
     console.log("");
-    // //최종 우승자를 담을 배열 winners
     const winners = [];
-
     scoreArray.forEach((value, index) => {
-      //최댓값을 가진 우승자들을 arr에서 찾아내서 winners로 push한다.
       if (value === max) {
         winners.push(arr[index]);
       }
     });
+
+    let parsed;
     if (max === 0) {
-      const parsed = "아무도 득점하지 못했습니다.";
-      return parsed;
+      parsed = "아무도 득점하지 못했습니다.";
     } else {
-      const parsed = "최종 우승자 : " + winners.join(", ");
-      return parsed;
+      parsed = "최종 우승자 : " + winners.join(", ");
     }
+    return parsed;
   }
 }
 
