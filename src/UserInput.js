6b606f80ded80e7b126603faeb07a0c97e4a8dd2
kochungwon <yud01063@gmail.com>
2024-10-28 09:22:14 +0900

    hotfix(UserInput): 사용자 입력 방식 수정
    - MissionUtils.Console.readLineAsync 사용하여 자동차 이름과 시도 횟수 입력받도록 변경

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,3 +1,5 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class UserInput {
     // 자동차 이름 배열과 시도 횟수를 저장
     constructor() {
@@ -7,13 +9,13 @@ class UserInput {
 
     // 자동차 이름을 사용자에게 입력받아 설정
     inputCarNames() {
-        const input = prompt("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        const input = MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         this.carNames = input.split(',').map(name => name.trim());
     }
 
     // 경주 시도 횟수를 사용자에게 입력받아 설정
     inputAttempts() {
-        const input = prompt("시도할 횟수는 몇 회인가요?");
+        const input = MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?");
         this.attempts = parseInt(input, 10);
     }
 
