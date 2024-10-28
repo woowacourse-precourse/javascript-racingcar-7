14039fa927925d6f8bd167557e355b3cb7f36118
syg0629 <syg0629@hanmail.net>
2024-10-28 22:28:43 +0900

    feat: 입력된 자동차 이름 쉼표를 기준으로 구분하여 저장

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
+    const map = new Map();
+
     const carNames = await MissionUtils.Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
     );
+    const trimmedCarNames = carNames.split(',').map((name) => name.trim());
+
+    for (const carName of trimmedCarNames) {
+      if (carName) map.set(carName, '');
+    }
 
     const raceRounds = await MissionUtils.Console.readLineAsync(
       '시도할 횟수는 몇 회인가요?\n'
