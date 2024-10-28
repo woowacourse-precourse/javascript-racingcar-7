6b61243fd01f3f3f9a8e667601c170950d387bc4
luminox93 <memoji1236@gmail.com>
2024-10-28 15:24:57 +0900

    feat: views 생성
    1. 입력을 담당하는 InputView 클래스 생성
    2. 출력을 담당하는 OutputView 클래스 생성

---
+++ src/views/OutputView.js
@@ -0,0 +1,11 @@
+import { Console, MissionUtils } from '@woowacourse/mission-utils';
+
+export class OutputView {
+  static printResult(result) {
+    Console.print(result);
+  }
+
+  static printError(ERROR_MESSAGE) {
+    Console.print(ERROR_MESSAGE);
+  }
+}
