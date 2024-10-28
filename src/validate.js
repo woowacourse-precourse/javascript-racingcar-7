e15e5691e8dc39d50ac6b386449ffc5dce60658b
mywhzy <cmj2393a@naver.com>
2024-10-28 20:47:21 +0900

    feat: 경주 참여 자동차 이름 글자수 검증 기능 구현

---
+++ src/validate.js
@@ -0,0 +1,15 @@
+import { ERROR_MESSAGE, NAME_SEPARATOR, REGEX } from './constants';
+
+const splitCarNames = (value) => {
+  return value.split(NAME_SEPARATOR);
+};
+
+export const validateCarName = (value) => {
+  const carNames = splitCarNames(value);
+
+  carNames.map((carName) => {
+    if (!REGEX.carName.test(carName)) throw new Error(ERROR_MESSAGE.carNameCharacterCountError);
+  });
+};
+
+export const validateRoundCount = () => {};
