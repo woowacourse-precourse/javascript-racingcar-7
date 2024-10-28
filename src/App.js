10f1119b440f14a963fb83ae85626f856b9b0f61
giseon <pakox123@gmail.com>
2024-10-28 22:14:15 +0900

    feat: App에서 게임 로직과 우승자 연결

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { Console } from "@woowacourse/mission-utils";
+import Validate from "./Validate.js";
+import Race from "./Race.js";
+import Winner from "./Winner.js";
 
 class App {
   async run() {
@@ -6,6 +9,13 @@ class App {
       const carNames = await this.getCarNames();
       const maxAttempts = await this.getAttempts();
 
+      const cars = carNames.map(name => ({ name, position: 0 }));
+
+      Console.print("\n경기 시작!");
+      Race.startRace(maxAttempts, cars);
+
+      const winners = Winner.determineWinners(cars);
+      Winner.announceWinners(winners);
     } catch (error) {
       Console.print(error.message);
       return Promise.reject(error);
@@ -19,7 +29,7 @@ class App {
 
   async getAttempts() {
     const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?: ");
-    return Validate.validateAttempts(input); 
+    return Validate.validateAttempts(input);
   }
 }
 
