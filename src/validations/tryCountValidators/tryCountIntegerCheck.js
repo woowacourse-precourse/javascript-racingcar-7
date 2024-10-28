b0451ec916b13eeff23db428c4f5d5f15234fcf0
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-10-28 15:38:09 +0900

    feat: 소수점 포함 검사 기능
     - Number.isInteger는 2.0도 정수로 판단하므로 소수점이 존재하는지 한 번 더 검사

--- src/validations/tryCountValidators/tryCountIntegerCheck.js
+++ src/validations/tryCountValidators/tryCountIntegerCheck.js
@@ -1,8 +1,12 @@
 import ERROR_MESSAGES from '../../constraints/errorMessages.js';
 
 export function tryCountIntegerCheck(tryCount) {
-  const PARSED_TO_FLOAT = parseFloat(tryCount);
-  if (!Number.isInteger(PARSED_TO_FLOAT)) {
+  const parseToFloat = parseFloat(tryCount);
+  if (!Number.isInteger(parseToFloat)) {
+    throw new Error(ERROR_MESSAGES.NOT_INTEGER);
+  }
+
+  if (tryCount.includes('.')) {
     throw new Error(ERROR_MESSAGES.NOT_INTEGER);
   }
 }
