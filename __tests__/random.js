9cbf847e796c03c47b44e8fd2bbb0678955ff414
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-28 16:43:52 +0900

    🧪 test(random): `pickRandomNumberInRange` 함수 테스트 추가
    - `pickRandomNumberInRange` 함수의 기능을 검증하는 테스트 코드를 작성했습니다.
    - `Random.pickNumberInRange` 메서드를 `jest.spyOn`으로 모의 처리하여 테스트의 일관성을 유지하였습니다.
    - 0-9 사이의 범위에서 반환되는 값이 지정된 범위 내에 있는지 테스트하였습니다
    - mock을 사용하여 설정한 중간값이 반환되는지 확인하는 테스트를 추가하였습니다.
    - 테스트 전후로 mock을 설정 및 복원하여 다른 테스트에 영향을 주지 않도록 처리하였습니다.

---
+++ __tests__/random.js
@@ -0,0 +1,32 @@
+import { Random } from '@woowacourse/mission-utils';
+import pickRandomNumberInRange from '../src/util/random';
+
+describe('random function', () => {
+  beforeEach(() => {
+    jest
+      .spyOn(Random, 'pickNumberInRange')
+      .mockImplementation((startRange, endRange) => {
+        // 항상 중간값을 반환하도록 모의 처리
+        return Math.floor((startRange + endRange) / 2);
+      });
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  test('0-9 사이의 랜덤한 숫자 추출하기', () => {
+    const startRange = 0;
+    const endRange = 9;
+    const result = pickRandomNumberInRange(startRange, endRange);
+    expect(result).toBeGreaterThanOrEqual(startRange);
+    expect(result).toBeLessThanOrEqual(endRange);
+  });
+
+  test('mock에서 설정한 중간값 반환되는지 확인하기', () => {
+    const startRange = 2;
+    const endRange = 8;
+    const result = pickRandomNumberInRange(startRange, endRange);
+    expect(result).toBe(5);
+  });
+});
