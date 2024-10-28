7077d9809eb1893977c8d4577c44909ceaa8e943
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-10-28 12:40:36 +0900

    test: Car 클래스 메서드 테스트
    - 거리를 잘 반환하는지 확인

--- __tests__/CarTest.js
+++ __tests__/CarTest.js
@@ -5,4 +5,9 @@ describe('Car 클래스 테스트', () => {
     const car = new Car('pobi');
     expect(car.getName()).toBe('pobi');
   });
+
+  test('getDistance 메서드가 초기 거리 0을 반환해야 함', () => {
+    const car = new Car('pobi');
+    expect(car.getDistance()).toBe(0);
+  });
 });
