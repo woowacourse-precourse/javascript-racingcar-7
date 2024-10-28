144c4978acae0b8cf1eaa009b4c2f32d690a8492
choisuyeon <stellasue@naver.com>
2024-10-28 17:09:46 +0900

    feat: Print guideline and Get a trial number

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ class App {
     try {
       // 경주할 자동차 이름(이름은 쉼표 기준으로 구분)
       const line = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+      
+      // 시도할 횟수
+      let tryNum = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
     } catch(error) {
       throw new Error(`[ERROR]`);
     }
