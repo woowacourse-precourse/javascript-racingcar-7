8f43175f658d81593fa941c83dbf8147988f3b45
Apoliasm <syjsir@gmail.com>
2024-10-28 18:04:50 +0900

    TEST  : Add test code for each function
    - Test case when length is over 5
    - Test case when attempt number is not number
    - Execute Boundary test for random value
    - Test case when has common winner

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -46,7 +46,8 @@ describe("자동차 경주", () => {
     });
   });
 
-  test("예외 테스트", async () => {
+
+  test("1. 차 이름 입력 - 예외 테스트", async () => {
     // given
     const inputs = ["pobi,javaji"];
     mockQuestions(inputs);
@@ -57,4 +58,63 @@ describe("자동차 경주", () => {
     // then
     await expect(app.run()).rejects.toThrow("[ERROR]");
   });
+
+  test("2. 시도 횟수 입력 - 예외 테스트", async ()=>{
+    //given
+    const inputs = ["pobi,woni", "a"];
+    mockQuestions(inputs);
+
+    //when
+    const app = new App();
+
+    //then
+    await expect(app.run()).rejects.toThrow("[ERROR]")
+
+  });
+
+  test("3. 경주 - 경곗값 테스트",async () => {
+    //given
+    const inputs = ["pobi,woni,kona","2"];
+    const FORWARD = 5
+    const BOUNDARY = 4 
+    const STOP = 3
+    const logs = ["pobi : -","woni : -","kona : ","","pobi : --","woni : -","kona : ","","최종 우승자 : pobi"]
+    const logSpy = getLogSpy();
+    mockQuestions(inputs);
+    mockRandoms([FORWARD,BOUNDARY,STOP,FORWARD,STOP,STOP])
+
+    //when
+    const app = new App();
+    await app.run();
+
+    // then
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+
+  })
+
+  test("4. 우승자 출력 테스트 - 공통으로 있을 때", async () => {
+    
+    //given
+    const inputs = ["pobi,woni,kona", "2"];
+    const FORWARD = 4 
+    const STOP = 3
+    const logs = ["pobi : -","woni : ","kona : -","","pobi : --","woni : ","kona : --","최종 우승자 : pobi, kona"]
+    const logSpy = getLogSpy();
+    mockQuestions(inputs);
+    mockRandoms([FORWARD,STOP,FORWARD,FORWARD,STOP,FORWARD])
+
+    //when
+    const app = new App();
+    await app.run();
+
+    // then
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+
+  })
+
+
 });
