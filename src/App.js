868a7d26f4497d8e04d3299b7ad6b601e2101aa7
이창준 <2018110021@dongguk.edu>
2024-10-28 21:41:40 +0900

    feat(App): 사용자 입출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      let carNameInput = await Console.readLineAsync(`경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n`);
+      let carNames = carNameInput.split(",");
+      let raceCountInput = await Console.readLineAsync(`시도할 횟수는 몇 회인가요?\n`);
+      let raceCount = parseInt(raceCountInput, 10);
+
+      console.log("names: ",carNames);
+      console.log("count: ",raceCount);
+    } catch (error) {
+        Console.print(error.message);
+        throw error;
+    }
+  }
 }
 
 export default App;
