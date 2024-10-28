75459fbf19fd9f1ab9cd074894394197d652ae2b
two678 <two6788@gmail.com>
2024-10-28 17:23:56 +0900

    feat: 이동 횟수 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -14,10 +14,21 @@ class App {
     return items;
   }
 
+  validateAttempt(input) {
+    const attempt = parseInt(input, 10);
+    if (isNaN(attempt) || attempt <= 0) {
+      throw new Error("[ERROR] 입력 오류: 시도 횟수는 양의 정수로 입력해야 합니다.");
+    }
+    return attempt;
+  }
+
   async run() {
     try {
       const input = await this.console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
       const validInput = this.validateInput(input);
+
+      const attemptInput = await this.console.readLineAsync("시도할 횟수를 입력하세요.\n");
+      const attemptCount = this.validateAttempt(attemptInput);
 
     } catch (error) {
       this.console.print(error.message);
