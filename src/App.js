c075bf94349dbbaae928550e4cd485a6fda49d37
Jin-Chanyong <cksdyd324@gmail.com>
2024-10-28 22:31:55 +0900

    feat: App.js 파일 작성
    App.js 파일에 이하 내용 추가:
    - 사용자의 입력값을 받아 경주를 실행하는 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { getValidatedCars, getValidatedCount } from "./modules/userInput.js";
+import startRace from "./modules/startRace.js";
+
 class App {
-  async run() {}
+  async run() {
+    const cars = await getValidatedCars();
+    const count = await getValidatedCount();
+
+    startRace(cars, count);
+  }
 }
 
 export default App;
