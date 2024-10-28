7214c81ec1448f433e94a3e15b66954f0634d2e5
luminox93 <memoji1236@gmail.com>
2024-10-28 20:06:14 +0900

    refactor: InputValidation 코드 수정
    1. composeValidators 함수를 통해 여러개의 검증 함수들을 하나의 검증 로직으로 묶음
    2. 입력된 자동차 이름의 유효성(이름 길이, 사용 가능한 문자열)을 검사하는 validateCarName 로직 생성
    3. 입력된 게임 시도 횟수의 유효성 (최대/최소값, 양의 정수 여부) 검사하는 validateGameAttepmts 로직 생성
    4. 게임 참가자 유효성 (중복 닉네임 여부, 참여자 최대/최소) 검증하는 validateParticipants 로직 생성
    5. 입력값이 유효한 문자열인지(빈 문자열, null, undefined 등 여부) 검증하는 validateInputStrings 로직 생성

--- src/validations/InputValidation.js
+++ src/validations/InputValidation.js
@@ -1,58 +1,56 @@
 import {
+  isNotEmpty,
+  hasNoWhitespace,
+  matchesRegex,
+  isWithinRange,
+  isPositiveInteger,
+  hasNoDuplicates,
+  composeValidators,
+} from './ValidationUtils.js';
+
+import {
   GAME_ATTEMPT_RANGE,
   NAME_LENGTH_RANGE,
   PARTICIPANT_COUNT_RANGE,
   VALID_USERNAME_REGEX,
-  REMOVE_ALL_SPACES_REGEX,
   VALIDATION_ERROR_MESSAGE,
-} from '../constants/constants';
+} from '../constants/constants.js';
 
-export const isEmptyStringIncluded = (input) => {
-  if (input === null || input == undefined || input === '') {
-    throw new Error(VALIDATION_ERROR_MESSAGE.INCLUDE_EMPTY_SPACE);
-  }
+const validateCarName = composeValidators(
+  (input) => matchesRegex(VALID_USERNAME_REGEX, input),
+  (input) =>
+    isWithinRange(
+      NAME_LENGTH_RANGE.MIN,
+      NAME_LENGTH_RANGE.MAX,
+      input.length,
+      VALIDATION_ERROR_MESSAGE.NAME_LENGTH_OUT_OF_RANGE,
+    ),
+);
 
-  const removed_all_spaced = input.replace(REMOVE_ALL_SPACES_REGEX, '');
-  if (input.length !== removed_all_spaced.length) {
-    throw new Error(VALIDATION_ERROR_MESSAGE.INCLUDE_EMPTY_SPACE);
-  }
-  return true;
-};
-export const isPositiveInteger = (input) => {
-  const number = Number(input);
-  if (Number.isNaN(number) || !Number.isInteger(number) || number <= 0) {
-    throw new Error(VALIDATION_ERROR_MESSAGE.IS_NOT_POSITIVE_INTEGER);
-  }
-  return true;
-};
+const validateGameAttempts = composeValidators(isPositiveInteger, (number) =>
+  isWithinRange(
+    GAME_ATTEMPT_RANGE.MIN,
+    GAME_ATTEMPT_RANGE.MAX,
+    number,
+    VALIDATION_ERROR_MESSAGE.GAME_ATTEMPT_OUT_OF_RANGE,
+  ),
+);
 
-export const isValidCarName = (input) => {
-  if (!input.match(VALID_USERNAME_REGEX)) {
-    throw new Error(VALIDATION_ERROR_MESSAGE.INVALID_NAME_FORMAT);
-  }
-  if (
-    NAME_LENGTH_RANGE.MIN > input.length ||
-    input.length > NAME_LENGTH_RANGE.MAX
-  ) {
-    throw new Error(VALIDATION_ERROR_MESSAGE.NAME_LENGTH_OUT_OF_RANGE);
-  }
-  return true;
-};
+const validateParticipants = composeValidators(hasNoDuplicates, (names) => {
+  const participantsNumber = names.length;
+  return isWithinRange(
+    PARTICIPANT_COUNT_RANGE.MIN,
+    PARTICIPANT_COUNT_RANGE.MAX,
+    participantsNumber,
+    VALIDATION_ERROR_MESSAGE.PARTICIPANT_COUNT_OUT_OF_RANGE,
+  );
+});
 
-export const isValidGameAttempts = (input) => {
-  if (GAME_ATTEMPT_RANGE.MIN > input || GAME_ATTEMPT_RANGE.MAX < input) {
-    throw new Error(VALIDATION_ERROR_MESSAGE.GAME_ATTEMPT_OUT_OF_RAGNE);
-  }
-  return true;
-};
+const validateInputStrings = composeValidators(isNotEmpty, hasNoWhitespace);
 
-export const isValidParticipantAmount = (input) => {
-  const participantsNumber = input.split(',').length;
-  if (
-    PARTICIPANT_COUNT_RANGE.MIN > participantsNumber ||
-    PARTICIPANT_COUNT_RANGE.MAX < participantsNumber
-  ) {
-    throw new Error(VALIDATION_ERROR_MESSAGE.PARTICIPANT_COUNT_OUT_OF_RANGE);
-  }
-  return true;
+export {
+  validateCarName,
+  validateGameAttempts,
+  validateParticipants,
+  validateInputStrings,
 };
