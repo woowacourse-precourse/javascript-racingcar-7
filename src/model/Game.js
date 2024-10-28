cea6eef21d84cc284370ac286fc4f91b6bf57353
songhaeunsong <gkdms3469@naver.com>
2024-10-28 21:18:33 +0900

    feat: 자동차 이동함수 호출

--- src/model/Game.js
+++ src/model/Game.js
@@ -1,1 +1,14 @@
-export class Game {}
+import { CarService } from "./service/CarService.js";
+
+export class Game {
+  constructor(cars) {
+    this.cars = cars;
+  }
+
+  playRound() {
+    const carService = new CarService();
+    this.cars.forEach((car) => (car = carService.move(car)));
+  }
+
+  getWinners() {}
+}
