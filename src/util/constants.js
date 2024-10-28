557e5542ce4ac3bbe577a6c758eac58c8fd2508f
taein0926 <kimtaein0926@gmail.com>
2024-10-28 22:53:15 +0900

    feat(constants): add error message constant

---
+++ src/util/constants.js
@@ -0,0 +1,11 @@
+export const NUMBER_ERROR = Object.freeze({
+  NUMBER_BLANK: "[ERROR] 입력하신 시도 횟수가 공백입니다.",
+  NUMBER_NONNUM: "[ERROR] 입력하신 시도 횟수가 숫자가 아닙니다.",
+});
+
+export const CAR_ERROR = Object.freeze({
+  CAR_UNDER_5: "[ERROR] 입력하신 자동차의 이름이 5자를 초과합니다.",
+  CAR_OVER_2: "[ERROR] 입력하신 자동차의 개수가 2개 미만입니다.",
+  CAR_BLANK: "[ERROR] 입력하신 자동차의 이름이 공백입니다.",
+  CAR_DUPLICATE: "[ERROR] 입력하신 자동차의 이름이 중복입니다.",
+});
