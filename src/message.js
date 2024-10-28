5a31e1cc101c0974e1d8821009d238624896e398
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2024-10-28 19:34:56 +0900

    fear: 자동차 유효성 검사 에러 메시지 추가

--- src/message.js
+++ src/message.js
@@ -1,6 +1,11 @@
 export const CAR_NAME_ERROR_MESSAGE = Object.freeze({
   carNameTooLong: "[ERROR] 자동차 이름은 5자 이하만 가능합니다.",
   carNameEmpty: "[ERROR] 자동차 이름은 공백일 수 없습니다.",
+  carNameContainsWhitespace: "[ERROR] 자동차 이름에 공백이 포함될 수 없습니다.",
+});
+
+export const CAR_VALIDATION_ERROR_MESSAGE = Object.freeze({
+  carCountTooLow: "[ERROR] 자동차는 최소 두 대 이상이어야 합니다.",
 });
 
 export const ATTEMPT_COUNT_ERROR_MESSAGE = Object.freeze({
