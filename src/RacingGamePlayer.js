22c07f9143a3635767294128cb0558357b433ecf
hienjoy <wisdom16no@naver.com>
2024-10-28 13:21:02 +0900

    feat: 자동차 이동 출력 추가

--- src/RacingGamePlayer.js
+++ src/RacingGamePlayer.js
@@ -1,4 +1,6 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
 import Car from './Car.js';
+import messages from './printMessages.js';
 
 class RacingGamePlayer {
   constructor(carNames, tryNumber) {
@@ -13,10 +15,19 @@ class RacingGamePlayer {
   moveCars() {
     for (let i = 0; i < this.roundNumber; i++) {
       this.cars.forEach((car) => car.move());
+      this.printRoundResults();
     }
   }
 
+  printRoundResults() {
+    this.cars.forEach((car) => {
+      MissionUtils.Console.print(`${car.name}: ${'-'.repeat(car.dist)}`);
+    });
+    MissionUtils.Console.print(''); //라운드 간 개행
+  }
+
   play() {
+    MissionUtils.Console.print(messages.newLine + messages.showRoundResults);
     this.moveCars();
   }
 }
