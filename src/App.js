9c19ca0cbe555e3a6c61901d550e02adee691b0e
jakejun98 <111010564+jakejun98@users.noreply.github.com>
2024-10-28 18:26:30 +0900

    feat : 이름 입력 기능 구현
    n대의 자동차 이름 입력
    쉼표로 구분
    5자 초과일 경우 예외처리
    공백일 경우 예외처리
    중복일 경우 예외처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  constructor() {
+    this.carNames = [];
+  }
+
+  async run() {
+    try {
+      await this.getCarNamesFromUser();
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  async getCarNamesFromUser() {
+    const userInput = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)'
+    );
+
+    const carNames = userInput.split(',').map(name => name.trim());
+    this.validateCarNames(carNames);
+
+    this.carNames = carNames;
+  }
+
+  validateCarNames(carNames) {
+    carNames.forEach(name => {
+      if (name === '') {
+        throw new Error('[ERROR] 공백입니다');
+      }
+
+      if (name.length > 5) {
+        throw new Error(`[ERROR] 자동차 이름은 5자를 초과할 수 없습니다.`);
+      }
+    });
+
+    if (new Set(carNames).size !== carNames.length) {
+      throw new Error('[ERROR] 중복된 자동차 이름이 있습니다.');
+    }
+  }
 }
 
 export default App;
