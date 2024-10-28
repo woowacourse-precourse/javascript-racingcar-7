783c56c00abda6db63865077c650277a68ec4203
kochungwon <yud01063@gmail.com>
2024-10-28 14:30:32 +0900

    fix(출력형식 수정) :임시 consol 로직 수정 - 출력형식이  console로 되어있어 인식하지못해 틀리던 test를 통과하지 못해 MissionUtils라이브러리를 수정해 출력하도록 바꿧습니다.

--- src/RaceCar.js
+++ src/RaceCar.js
@@ -3,7 +3,6 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class RaceCar {
     // 자동차 이름과 초기 위치를 설정
-
     constructor(name) {
         this.name = name;
         this.position = 0;
@@ -11,7 +10,6 @@ class RaceCar {
 
     move() {
         // 자동차가 전진할 조건을 정의, 0~9 사이의 숫자를 생성하여, 4 이상일 경우 위치를 1 증가
-
         const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
         if (randomNumber >= 4) {
             this.position += 1;
--- src/RacingSession.js
+++ src/RacingSession.js
@@ -1,3 +1,4 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
 import RaceCar from './RaceCar.js'
 
 class RacingSession {
@@ -9,13 +10,14 @@ class RacingSession {
 
     // 시도 횟수만큼 반복하여 경주를 진행, 각 시도에서 자동차의 이동 상태를 출력합니다.
     startRace() {
+        MissionUtils.Console.print('\n실행 결과');
         for (let i = 0; i < this.attempts; i++) {
-            console.log(`\n${i + 1}차 시도 결과:`);
             this.cars.forEach(car => {
                 car.move();
                 let positionDisplay = '-'.repeat(car.position); // 현재 위치 표시
-                console.log(`${car.name} : ${positionDisplay}`);
+                MissionUtils.Console.print(`${car.name} : ${positionDisplay}`);
             });
+            MissionUtils.Console.print('')
         }
         this.announceWinner();
     }
@@ -24,7 +26,7 @@ class RacingSession {
     announceWinner() {
         const maxPosition = Math.max(...this.cars.map(car => car.position));
         const winners = this.cars.filter(car => car.position === maxPosition).map(car => car.name);
-        console.log(`\n최종 우승자 : ${winners.join(', ')}`);
+        MissionUtils.Console.print(`\n최종 우승자 : ${winners.join(', ')}`);
     }
 }
 export default RacingSession;
