6a047da0ef323c28090159107fbee2450a9a0e6d
이창준 <2018110021@dongguk.edu>
2024-10-28 22:07:15 +0900

    feat(App): 자동차 이동 조건 구현

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ class App {
       let raceCountInput = await Console.readLineAsync(`시도할 횟수는 몇 회인가요?\n`);
       let raceCount = parseInt(raceCountInput, 10);
 
+      carNames.forEach(car => {
+        Console.print(`${car}의 이동 결과: `);
+        for(let i = 0; i < raceCount; i++) {
+          const isMoving = this.shouldMove();
+          this.carMovingCheck(car, isMoving);
+        }
+      })
+
     } catch (error) {
       Console.print(`[ERROR] ${error.message}`);
       throw error;
@@ -16,9 +24,9 @@ class App {
 
   /**
    * @author CWDll
-   * @describe 입력받은 문자열을 계산하는 함수
+   * @describe 입력받은 문자열을 검증하는 함수
    * @parameter {carNameInput: string[]}
-   * @returnValue {carNames}
+   * @returnValue {carNames} - 검증된 자동차 이름 배열
    */
   validateCarNames(carNameInput) {
     let carNames = carNameInput.split(",").map(name => name.trim());
@@ -33,6 +41,39 @@ class App {
 
     return carNames;
   }
+
+  /**
+   * @author CWDll
+   * @describe 난수를 생성하는 함수
+   * @returnValue {number} - 0 ~ 9 사이의 난수
+   */
+  generateRandomNumber() {
+    return Random.pickNumberInRange(0, 9);
+  }
+
+  /**
+   * @author CWDll
+   * @describe 난수를 생성하고, 4 이상일 경우 자동차가 전진하도록 설정하는 함수
+   * @returnValue {boolean} - 전진 여부를 반환 (true: 전진, false: 멈춤)
+   */
+  shouldMove() {
+    const randomNumber = this.generateRandomNumber();
+    return randomNumber >= 4;
+  }
+
+  /**
+   * @author CWDll
+   * @describe 선택한 자동차가 움직이는지 확인하는 함수
+   */
+  carMovingCheck(car, isMoving) {
+    if(isMoving) {
+      Console.print(`${car} 전진`);
+    } else {
+      Console.print(`${car} 멈춤`);
+    }
+  }
+
+
 }
 
 
