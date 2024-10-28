3b5e19cbaf2cec4c3d2db606375b52970a5fd7b1
chickenisamazing <dieswing@naver.com>
2024-10-28 19:23:25 +0900

    style: 불필요한 주석 코드 삭제

--- src/App.js
+++ src/App.js
@@ -10,13 +10,7 @@ class App {
     );
 
     try {
-      // const result = this.racing(input, count);
-
       const { resultList, parsed } = this.racing(input, count);
-
-      // MissionUtils.Console.print(` 경기 결과 : ${JSON.stringify(resultList)} `);
-      // MissionUtils.Console.print(resultList);
-      // MissionUtils.Console.print(resultList);
 
       resultList.forEach((item) => {
         for (const key in item) {
@@ -43,10 +37,10 @@ class App {
     // scoreArr = [1,2,3]
     // 치킨이 1회 승리, 닭이 2회 승리, 다리가 3회 승리했다는 것
 
-    //scoreArray는 arr과 길이가 같으면서 모든 값을 0으로 갖는 배열이다.
+    //scoreArray는 arr과 길이가 같으면서 모든 값을 0으로 초기화된 배열이다.
     const scoreArray = arr.map(() => 0);
 
-    //중간 결과를 저장할 배열? 객체?
+    //중간 결과를 저장
     const raceResult = [];
 
     //depth가 깊어진다. 후에 메서드를 분리해줘야 한다.
@@ -55,29 +49,14 @@ class App {
 
     //경주가 한 번씩 실행되는 턴
     for (let i = 0; i < count; i++) {
-      console.log("");
-      //현재의 경주를 객체로 저장해야 한다.
       const currentRace = {};
-      // console.log(i + 1 + "번째 경기");
+
       for (let j = 0; j < arr.length; j++) {
         const num = MissionUtils.Random.pickNumberInRange(0, 9);
 
         if (num >= 4) {
           scoreArray[j] = scoreArray[j] + 1;
-
-          // process.stdout.write(arr[j] + " : ");
-          for (let k = 0; k < scoreArray[j]; k++) {
-            // process.stdout.write("-");
-          }
-          // console.log("");
-        } else {
-          // process.stdout.write(arr[j] + " : ");
-          for (let k = 0; k < scoreArray[j]; k++) {
-            // process.stdout.write("-");
-          }
-          console.log("");
         }
-
         currentRace[arr[j]] = "-".repeat(scoreArray[j]);
       }
 
@@ -89,26 +68,11 @@ class App {
     return { resultList: raceResult, parsed };
   }
 
-  // //출력해야 하는뎅
-  // printRaceResults(arr, scoreArray) {
-  //   const raceResult = arr.map((car, index) => ({
-  //     car: car,
-  //     score: "-".repeat(scoreArray[index]),
-  //   }));
-  //   return raceResult;
-  // }
-
   getRaceResult(arr, scoreArray) {
     return arr.map((car, index) => ({
       car,
       score: "-".repeat(scoreArray[index]),
     }));
-  }
-
-  printRaceResults(arr, raceResult) {
-    return arr
-      .map((car, index) => `${car} : ${"-".repeat(raceResult[index])}`)
-      .join("\n");
   }
 
   //우승자 판별 로직 메서드 분리
@@ -126,8 +90,6 @@ class App {
       }
     });
     // //join은 배열 요소를 쉼표와 띄어쓰기로 연결해 하나의 문자열로 만들어준다.
-    // console.log("최종 우승자 : " + winners.join(", "));
-
     const parsed = "최종 우승자 : " + winners.join(", ");
 
     return parsed;
