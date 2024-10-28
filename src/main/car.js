3b80374556b159cb99f18f2a59897ce11dcb0024
Joohyun Kim <corinthionia@gmail.com>
2024-10-28 23:21:13 +0900

    feat(car): add car class with movement functions

---
+++ src/main/car.js
@@ -0,0 +1,26 @@
+import { Random } from "@woowacourse/mission-utils";
+
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.forwardCount = 0;
+  }
+
+  getCarName() {
+    return this.name;
+  }
+
+  getForwardCount() {
+    return this.forwardCount;
+  }
+
+  moveForward() {
+    const randomNumber = Random.pickNumberInRange(0, 9);
+
+    if (4 <= randomNumber) {
+      this.forwardCount += 1;
+    }
+  }
+}
+
+export default Car;
