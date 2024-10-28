a708f32ce0a7afd8a5df8b45766bb6c101d1eac9
Saebom <spring180@naver.com>
2024-10-28 13:49:12 +0900

    feat(App): 자동차 전진 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
+    // 1. 자동차 이름 입력
     const carName = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분) \n"
     );
 
-    const cars = carName.split(",");
+    const cars = carName.split(",").map((car) => car.trim()); // 자동차 이름 공백 제거
 
+    // 2. 이동 횟수 입력
     const count = Number(
-      await Console.readLineAsync("시도할 횟수는 몇 회인가요? \n")
+      await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n")
     );
+
+    // 3. 자동차 전진
+    let movingCars = new Array(cars.length).fill(0); // 자동차 이동 거리 초기화
+
+    Console.print("\n");
+    Console.print("실행 결과 \n");
+    for (let i = 0; i < count; i++) {
+      cars.forEach((car, idx) => {
+        const randomNum = Random.pickNumberInRange(0, 9);
+        if (randomNum >= 4) {
+          movingCars[idx]++; // 4 이상일 경우 전진
+        }
+      });
+    }
   }
 }
 
