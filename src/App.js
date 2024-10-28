04f96e18036af8c9f355b0985b8b1dd637529d98
j0n0m2 <dbsk8223@naver.com>
2024-10-28 14:30:40 +0900

    사용자로부터 자동차 이동 횟수 값을 받는다

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ class App {
   async run() {
     const carName = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     const carNames = this.Separator(carName);
+    
+    const forwardTime = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
   }
 }
 
