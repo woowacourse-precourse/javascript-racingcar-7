201253b50403aaad1671bff786e0768e705d8521
thisisyejin <yejink321@gmail.com>
2024-10-28 15:19:18 +0900

    refactor: 에러 메시지 상수 선언
    에러 메시지를 상수로 선언하여, 유효성 테스트 시
    상황에 부합하는 에러 메시지가 출력하는지 확인하도록
    하였습니다.

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const ERROR_MESSAGE = Object.freeze({
+  NAME_IS_REPEATED: '[ERROR] 자동차 이름을 중복으로 사용할 수 없습니다.',
+  NAME_IS_BLANK: '[ERROR] 자동차 이름은 공백으로 할 수 없습니다.',
+  NAME_IS_OVER_5: '[ERROR] 자동차 이름은 5글자 이내로 입력해주세요.',
+
+  NUMBER_IS_UNDER_1: '[ERROR] 1 미만의 수는 입력할 수 없습니다. 양수를 입력해주세요.',
+  NUMBER_IS_NOT_ENTERED: '[ERROR] 숫자를 입력해주세요.',
+  NUMBER_IS_DECIMAL: '[ERROR] 소수는 입력할 수 없습니다. 정수를 입력해주세요.',
+});
--- src/validation.js
+++ src/validation.js
@@ -1,22 +1,24 @@
+import { ERROR_MESSAGE } from "./constants.js";
+
 function isRepeat(arr) {
   const isError = arr.some((element) => {
     return arr.indexOf(element) !== arr.lastIndexOf(element)
   });
-  if (isError) throw Error('[ERROR] 자동차 이름을 중복으로 사용할 수 없습니다.');
+  if (isError) throw Error(ERROR_MESSAGE.NAME_IS_REPEATED);
 }
 
 function isBlank(arr) {
   const isError = arr.some((element) => {
     return element.length == 0
   });
-  if (isError) throw Error('[ERROR] 자동차 이름은 공백으로 할 수 없습니다.');
+  if (isError) throw Error(ERROR_MESSAGE.NAME_IS_BLANK);
 }
 
 function isOver5(arr) {
   const isError = arr.some((element) => {
     return element.length > 5
   });
-  if (isError) throw Error('[ERROR] 자동차 이름은 5글자 이내로 입력해주세요.');
+  if (isError) throw Error(ERROR_MESSAGE.NAME_IS_OVER_5);
 }
 
 export function isValidName(nameList) {
@@ -27,9 +29,9 @@ export function isValidName(nameList) {
 
 export function isValidNumber(tryTime) {
   if (parseInt(tryTime) < 1)
-    throw Error(`[ERROR] 1 미만의 수는 입력할 수 없습니다. 양수를 입력해주세요.`);
+    throw Error(ERROR_MESSAGE.NUMBER_IS_UNDER_1);
   if (Number.isNaN(parseInt(tryTime)))
-    throw Error(`[ERROR] 숫자를 입력해주세요.`);
+    throw Error(ERROR_MESSAGE.NUMBER_IS_NOT_ENTERED);
   if (tryTime.includes('.'))
-    throw Error(`[ERROR] 소수는 입력할 수 없습니다. 정수를 입력해주세요.`);
+    throw Error(ERROR_MESSAGE.NUMBER_IS_DECIMAL);
 }
