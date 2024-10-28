dc548ae398c8e86ddd5fb457c986f680c2871129
Joohyun Kim <corinthionia@gmail.com>
2024-10-28 23:23:55 +0900

    feat(output): add print each step function

--- src/utils/output.js
+++ src/utils/output.js
@@ -1,6 +1,12 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class Output {
+  static async printEachStep(cars) {
+    cars.forEach((car) => {
+      Console.print(`${car.name} : ${"-".repeat(car.forwardCount)}`);
+    });
+  }
+
   static printWinners(winners) {
     Console.print(`최종 우승자 : ${winners.join(", ")}`);
   }
