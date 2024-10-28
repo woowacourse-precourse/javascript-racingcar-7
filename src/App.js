1cea9e2d6fe5bc4eb36db750ad258aef770f5bdd
Lim-shweety <smile70200@naver.com>
2024-10-28 20:44:38 +0900

    feat(App.js) : 시도할 횟수 입력 받고 저장
    시도할 횟수를 입력 받고, 숫자로 변경

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { Console } from "@woowacourse/mission-utils"
 class App {
   async run() {
     let inputNames = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-    const NAMES=inputNames.split(',');
-
+    const NAMES = inputNames.split(',');
+    let times = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    times = Number(times);
   }
 }
 
