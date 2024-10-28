ab39b11a3fb3efe13abd682d68cec1777dc6f86a
hyeongdong <khd941018@gmail.com>
2024-10-28 20:17:49 +0900

    refactor(App): 메인 실행 로직 구현 및 필드 속성 제거
    유저입력값인 자동차이름과 실행 횟수는 게임에만 필요한 데이터이므로,
    App 클래스의 필드 속성으로 선언하지 않고, 메인 로직을 실행하는 run메서드에 선언하여
    racingGame객체의 초기화 값으로 사용할 수 있도록 코드를 수정

--- src/App.js
+++ src/App.js
@@ -1,82 +1,25 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 import InputValidator from "./InputValidator.js";
-import Car from "./Car.js";
+import RacingGame from "./RacingGame.js";
+import Printer from "./Printer.js";
 
 class App {
-  #tryCount;
-  #carNames;
-  #cars = [];
-
-  async init() {
-    await this.setCarNames();
-    await this.setTryCount();
-    this.makeCar();
-  }
-
   async run() {
-    await this.init();
-    this.play();
-    this.printResult();
-    this.printWinners(this.findWinner());
+    const carNames = [...(await this.getCarNames())];
+    const tryCount = await this.setTryCount();
+    const racingGame = new RacingGame(tryCount, carNames);
+    racingGame.makeCar();
+    racingGame.play();
+    Printer.printResult(racingGame);
+    Printer.printFinal(racingGame.findWinner());
   }
 
-  play() {
-    for (let gameRound = 0; gameRound < this.#tryCount; gameRound++) {
-      this.#cars.forEach((car) => {
-        const randomNumber = Random.pickNumberInRange(0, 9);
-        car.move(randomNumber);
-        car.saveResult();
-      });
-    }
-  }
-
-  printWinners(winners) {
-    Console.print(`결과: ${winners.join(", ")}`);
-  }
-
-  findWinner(maxDistance = 0, winners = []) {
-    this.#cars.forEach((car) => {
-      const carDistance = car.getDistance();
-
-      if (maxDistance < carDistance) {
-        maxDistance = carDistance;
-        winners.length = 0;
-        winners.push(car.getName());
-      } else if (maxDistance === carDistance) {
-        winners.push(car.getName());
-      }
-    });
-
-    return winners;
-  }
-
-  printResult() {
-    Console.print("실행 결과");
-
-    for (let gameRound = 0; gameRound < this.#tryCount; gameRound++) {
-      this.#cars.forEach((car) => {
-        const distanceByRound = car.getResultBy(gameRound);
-        Console.print(`${car.getName()}: ${"-".repeat(distanceByRound)}`);
-      });
-
-      Console.print("");
-    }
-  }
-
-  makeCar() {
-    this.#carNames.forEach((carName) => {
-      const car = new Car(carName);
-      this.#cars.push(car);
-    });
-  }
-
-  async setCarNames() {
+  async getCarNames() {
     try {
       const userInputCarName = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
       const carNames = userInputCarName.split(",");
-
       this.validateCarName(carNames);
-      this.#carNames = [...carNames];
+      return carNames;
     } catch (error) {
       throw new Error(error.message);
     }
@@ -86,9 +29,8 @@ class App {
     try {
       const userInputTryCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
       const tryCount = Number(userInputTryCount);
-
       this.validateTryCount(tryCount);
-      this.#tryCount = tryCount;
+      return tryCount;
     } catch (error) {
       throw new Error(error.message);
     }
