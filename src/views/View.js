7bea6d42d645c2471ec4f0242b088f954ed70845
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-10-28 14:01:51 +0900

    refector: 입출력 안내 문구 추가

--- src/views/View.js
+++ src/views/View.js
@@ -1,9 +1,13 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-export function getUserInput() {
-  return MissionUtils.Console.readLineAsync(''); // promist 반환
+export function getUserCarName() {
+  return MissionUtils.Console.readLineAsync(
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  ); // promist 반환
 }
-
+export function getUserRacingNum() {
+  return MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n'); // promist 반환
+}
 export function printRacingState(carData) {
   carData.forEach((line) => {
     MissionUtils.Console.print(line);
