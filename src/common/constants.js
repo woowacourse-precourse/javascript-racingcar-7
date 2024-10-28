9ed0cfdade27f101f3480cd02d419164c8fa5622
Daeuning <happyjde3@gmail.com>
2024-10-28 13:07:21 +0900

    refactor(constants): 에러메세지 수정

--- src/common/constants.js
+++ src/common/constants.js
@@ -4,10 +4,10 @@ const LOG_MESSAGE = Object.freeze({
 })
 
 const ERROR_MESSAGE = Object.freeze({
-  INVALID_INPUT: "자동차의 이름은 5글자 이하여야 합니다.",
-  EMPTY_INPUT: "빈 값이 입력되었습니다. 입력값을 넣어주세요!",
-  DUPLICATE_INPUT: "자동차 이름이 중복되었습니다. 서로 다른 이름으로 지어주세요!",
-  INVALID_ATTEMPTS_COUNT: "올바르지 못한 시도 횟수를 입력하셨습니다. 시도 횟수에는 1 이상의 양의 정수값만 입력해 주세요!",
+  INVALID_INPUT: "자동차 이름은 최대 5글자까지 가능합니다. 다시 입력해 주세요.",
+  EMPTY_INPUT: "입력값이 비어 있습니다. 값을 입력해 주세요!",
+  DUPLICATE_INPUT: "중복된 자동차 이름이 있습니다. 서로 다른 이름으로 입력해 주세요!",
+  INVALID_ATTEMPTS_COUNT: "시도 횟수는 1 이상의 정수여야 합니다. 다시 입력해 주세요!",
 });
 
 const TAG = Object.freeze({
