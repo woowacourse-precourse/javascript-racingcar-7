d1cdf3bb24ebfbcff316d04d9546670940a8611b
thisisyejin <yejink321@gmail.com>
2024-10-28 18:17:19 +0900

    refactor: 사용자 입력 함수 이름 변경
    사용자로부터 입력을 받는 inputCarName, inputTryTime 함수의
    기능적인 의미를 직관적으로 보여주기 위하여, input 대신
    ask를 사용하는 이름으로 변경하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { inputCarName, inputTryTime } from "./utils/inputHandler.js";
+import { askCarName, askTryTime } from "./utils/inputHandler.js";
 import Car from "./models/Car.js"
 
 class App {
   async run() {
     try {
-      const carName = await inputCarName();
+      const carName = await askCarName();
       const carList = Car.makeCarList(carName);
-      const tryTime = await inputTryTime();
+      const tryTime = await askTryTime();
       Car.race(carList, tryTime);
       Car.whoIsWinner(carList);
 
--- src/utils/inputHandler.js
+++ src/utils/inputHandler.js
@@ -1,12 +1,12 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import { isValidNumber } from "./validation.js";
 
-export async function inputCarName() {
+export async function askCarName() {
   const carName = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
   return carName;
 }
 
-export async function inputTryTime() {
+export async function askTryTime() {
   const tryTime = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
   isValidNumber(tryTime);
   return parseInt(tryTime, 10);
