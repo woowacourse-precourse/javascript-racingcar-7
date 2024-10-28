39aa18a21fa0bb8efd09d8f7dd5cb78aee4e20a3
이서현 <mellifluous3380@gmail.com>
2024-10-28 14:25:10 +0900

    feat: 사용자 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+    async run() {
+        try {
+            // 입력
+            Console.print('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+            const racingCars = await Console.readLineAsync('');
+            const carArray = racingCars.split(',');
+            if (!chkCarName(carArray)) {
+                this.throwError('자동차 이름이 5글자를 초과');
+            }
+
+            Console.print('시도할 횟수는 몇 회인가요?');
+            const tryNum = await Console.readLineAsync('');
+        } catch (error) {
+            Console.print(error.message);
+            throw error;
+        }
+    }
+
+    throwError(message) {
+        throw new Error(`[ERROR] ${message}`);
+    }
 }
 
 export default App;
+
+function chkCarName(cars) {
+    for (let i = 0; i < cars.length; i++) {
+        if (cars[i].length > 5) return false;
+    }
+    return true;
+}
