0e9ee2a210a6993bf6332732b7070b3449453e4d
ShinjungOh <tlswjd9260@gmail.com>
2024-10-28 22:13:58 +0900

    refactor: 에러 메시지 상수로 분리

--- src/RacingGameValidator.js
+++ src/RacingGameValidator.js
@@ -1,24 +1,26 @@
+import { ERROR_MESSAGES } from "./constants/errorMessages.js";
+
 class RacingGameValidator {
   static validateCarNameLengthUnderSix(carNames) {
     carNames.forEach((car) => {
-      if (car.length > 5) throw Error('[ERROR] 자동차 이름은 5자 이하만 가능합니다.');
+      if (car.length > 5) throw Error(ERROR_MESSAGES.CAR_NAME_LENGTH_OVER_FIVE);
     });
   }
 
   static validateCarNumberOverOne(carNames) {
-    if (carNames.length < 2) throw Error('[ERROR] 자동차 갯수는 2대 이상 가능합니다.');
+    if (carNames.length < 2) throw Error(ERROR_MESSAGES.CAR_NUMBER_UNDER_TWO);
   }
 
   static validateTryCountIsNumber(tryCount) {
-    if (isNaN(tryCount)) throw Error('[ERROR] 시도할 횟수는 숫자만 입력할 수 있습니다.');
+    if (isNaN(tryCount)) throw Error(ERROR_MESSAGES.TRY_COUNT_IS_NOT_NUMBER);
   }
 
   static validateTryCountOverZero(tryCount) {
-    if (tryCount === 0) throw Error('[ERROR] 0회 이상의 시도 횟수를 입력해 주세요.');
+    if (tryCount === 0) throw Error(ERROR_MESSAGES.TRY_COUNT_UNDER_ZERO);
   }
 
   static validateTryCountIsPositiveNumber(tryCount) {
-    if (tryCount < 0) throw Error('[ERROR] 음수는 입력할 수 없습니다.');
+    if (tryCount < 0) throw Error(ERROR_MESSAGES.TRY_COUNT_IS_NEGATIVE_NUMBER);
   }
 }
 
