89e4b740df3d1543fb7ed171c0fd4435db6defe3
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2024-10-28 17:12:54 +0900

    feat: 시도할 횟수 유효성 검사 메시지 구현

--- src/message.js
+++ src/message.js
@@ -2,3 +2,10 @@ export const CAR_NAME_ERROR_MESSAGE = Object.freeze({
   carNameTooLong: "[ERROR] 자동차 이름은 5자 이하만 가능합니다.",
   carNameEmpty: "[ERROR] 자동차 이름은 공백일 수 없습니다.",
 });
+
+export const ATTEMPT_COUNT_ERROR_MESSAGE = Object.freeze({
+  attemptCountEmpty: "[ERROR] 시도횟수를 공백으로 입력하실 수 없습니다.",
+  attemptCountNotNumber:
+    "[ERROR] 시도횟수에 숫자가 아닌 값을 입력하실 수 없습니다.",
+  attemptCountNegative: "[ERROR] 시도횟수가 0보다 작으면 안됩니다.",
+});
