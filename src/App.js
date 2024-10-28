5d9f94add787fa49289b7a3375d038af4ead88d1
wrryu09 <rryu09@ewhain.net>
2024-10-28 17:19:04 +0900

    feat: 인풋 검증 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
@@ -8,16 +8,30 @@ class App {
         "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
       );
       const carNames = input.split(",").map((name) => name.trim());
-      // checkCarNames(carNames);
+      checkCarNames(carNames);
       return carNames;
+    };
+
+    /** 자동차 이름 검증 */
+    const checkCarNames = (names) => {
+      if (names.some((name) => name.length > 5 || !name)) {
+        throw new Error("[ERROR] 자동차 이름은 1~5자 사이여야 합니다.");
+      }
     };
 
     /** 시도 횟수 입력 */
     const getTryCount = async () => {
       const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
       const tryCount = parseInt(input, 10);
-      // checkTryCnt(tryCount);
+      checkTryCnt(tryCount);
       return tryCount;
+    };
+
+    /** 시도 횟수 검증 */
+    const checkTryCnt = (cnt) => {
+      if (isNaN(cnt) || cnt <= 0) {
+        throw new Error("[ERROR] 시도 횟수는 1 이상의 숫자여야 합니다.");
+      }
     };
 
     async function racingGame() {
