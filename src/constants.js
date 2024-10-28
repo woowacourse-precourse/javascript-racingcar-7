941c9c600fb019fd0b50cb056740ab8a8b215a86
xxxjinn <tlsguswls319@naver.com>
2024-10-28 15:11:53 +0900

    refactor(constants.js): attempt_count_message에 불필요한 온점 삭제

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ const RESULT_MESSAGE="최종 우승자";
 
 const INPUT_MESSAGES={
     CAR_NAME_MESSAGE:"경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
-    ATTEMPT_COUNT_MESSAGE:"시도할 횟수는 몇 회인가요?.\n"
+    ATTEMPT_COUNT_MESSAGE:"시도할 횟수는 몇 회인가요?\n"
 }
 
 const ERROR_MESSAGES={
