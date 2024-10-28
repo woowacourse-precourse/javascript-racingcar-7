b3dfb4c81a52533bc6f923b62b9ce59164fc01bb
potalzz <tprhkd1607@naver.com>
2024-10-28 20:08:02 +0900

    test: 자동차 이름 길이 테스트 추가

--- __tests__/InputValidationTest.js
+++ __tests__/InputValidationTest.js
@@ -11,11 +11,19 @@ const mockQuestions = (inputs) => {
 };
 
 describe("자동차 경주 입력 관련 예외 테스트", () => {
-  test("자동차 이름의 길이가 1 ~ 5 범위를 벗어나는 경우", async () => {
-    const inputs = ["멋진 자동차,asdf"];
+  test("자동차 이름의 길이가 1미만인 경우", async () => {
+    const inputs = [",james"];
     mockQuestions(inputs);
 
     const app = new App();
-    await expect(app.run().rejects.toThrow("[ERROR]"));
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("자동차 이름의 길이가 공백 포함 5자 초과인 경우", async () => {
+    const inputs = ["멋진 자동차"];
+    mockQuestions(inputs);
+
+    const app = new App();
+    await expect(app.run()).rejects.toThrow("[ERROR]");
   });
 });
