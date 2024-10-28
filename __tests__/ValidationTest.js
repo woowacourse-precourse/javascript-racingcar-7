249ed88fe5b8c5f2fe60d9d5ccb37be2990deced
thisisyejin <yejink321@gmail.com>
2024-10-28 12:49:17 +0900

    test: 유효성 테스트 단위 수정
    애플리케이션 전체에 대해서 테스트하던 기존 스크립트를
    특정 메서드에 대해서 테스트하도록 수정하였습니다.
    또한, 코드 중복을 개선하였습니다.

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,68 +1,24 @@
-import App from "../src/App.js";
-import { MissionUtils } from "@woowacourse/mission-utils";
+import Car from "../src/Car.js"
+import { isValidNumber } from "../src/validation.js";
 
-const mockQuestions = (inputs) => {
-  MissionUtils.Console.readLineAsync = jest.fn();
-  MissionUtils.Console.readLineAsync.mockImplementation(() => {
-    const input = inputs.shift();
-    return Promise.resolve(input);
+describe("자동차 이름 유효성 테스트", () => {
+  test.each([
+    ["5자 초과", "pobi,pororo"],
+    ["공백", "pobi, "],
+    ["중복", "pobi,crong,pobi"],
+  ])("%s", (_, nameList) => {
+    expect(() => Car.makeCarList(nameList)).toThrow("[ERROR]");
   });
-};
+});
 
-describe("입력 오류 테스트", () => {
-  test("자동차 이름: 5자 초과", async () => {
-    const inputs = ["pobi,pororo"];
-    mockQuestions(inputs);
-    const app = new App();
-    await expect(app.run()).rejects.toThrow("[ERROR] 자동차 이름은 5글자")
-  });
-
-  test("자동차 이름: 공백", async () => {
-    const inputs = ["pobi, "];
-    mockQuestions(inputs);
-    const app = new App();
-    await expect(app.run()).rejects.toThrow("[ERROR] 자동차 이름은 공백")
-  });
-
-  test("자동차 이름: 중복", async () => {
-    const inputs = ["pobi, crong, pobi"];
-    mockQuestions(inputs);
-    const app = new App();
-    await expect(app.run()).rejects.toThrow("[ERROR] 자동차 이름을 중복")
-  });
-
-  test("이동 횟수: 0", async () => {
-    const inputs = ["pobi,crong", "0"];
-    mockQuestions(inputs);
-    const app = new App();
-    await expect(app.run()).rejects.toThrow("[ERROR] 1")
-  });
-
-  test("이동 횟수: 음수", async () => {
-    const inputs = ["pobi,crong", "-3"];
-    mockQuestions(inputs);
-    const app = new App();
-    await expect(app.run()).rejects.toThrow("[ERROR] 1")
-  });
-
-  test("이동 횟수: 문자", async () => {
-    const inputs = ["pobi,crong", "abc"];
-    mockQuestions(inputs);
-    const app = new App();
-    await expect(app.run()).rejects.toThrow("[ERROR] 숫자")
-  });
-
-  test("이동 횟수: 빈 값", async () => {
-    const inputs = ["pobi,crong", ""];
-    mockQuestions(inputs);
-    const app = new App();
-    await expect(app.run()).rejects.toThrow("[ERROR] 숫자")
-  });
-
-  test("이동 횟수: 소수", async () => {
-    const inputs = ["pobi,crong", "1.9999999999999999"];
-    mockQuestions(inputs);
-    const app = new App();
-    await expect(app.run()).rejects.toThrow("[ERROR] 소수")
+describe("이동 횟수 유효성 테스트", () => {
+  test.each([
+    ["0", "0"],
+    ["음수", "-3"],
+    ["문자", "abc"],
+    ["빈 값", ""],
+    ["소수", "1.9999999999999999"],
+  ])("%s", (_, number) => {
+    expect(() => isValidNumber(number)).toThrow("[ERROR]");
   });
 });
