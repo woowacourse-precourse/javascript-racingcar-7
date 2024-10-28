45514862e4816037977735c91cf379047e6bce1c
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2024-10-28 22:24:00 +0900

    refactor: 파일구조 추상화
    carRun의 하는 일이 잘 이해되도록 내부 함수들을 밖으로 분리하여 추상화했다.

--- src/carRun.js
+++ src/carRun.js
@@ -4,40 +4,41 @@ import { getWinner } from "./getWinner.js";
 
 
 export const carRun = (carArr, RUN_TIMES) => {
-    const racing = () => {
-
-        for (let i = 1; i <= carArr.length; i += 2) {
-            let isRun = MissionUtils.Random.pickNumberInRange(0, 9) >= 4
-
-            if (isRun) {
-                //i-1번째에 저장된 car이동
-                carArr[i] += 1
-            }
-        }
-
-    }
-
-    const printRacing = () => {
-        let result = ''
-
-        for (let i = 0; i < carArr.length; i += 2) {
-            const CAR_NAME = carArr[i]
-            const CAR_MOVE_NODE = '-'.repeat(carArr[i + 1])
-
-            result += `${CAR_NAME} : ${CAR_MOVE_NODE}\n`
-        }
-
-        Console.print(result)
-    }
 
     Console.print("실행 결과")
 
     for (let i = 0; i < RUN_TIMES; i++) {
-        racing()
-        printRacing()
+        racing(carArr)
+        printRacing(carArr)
     }
 
     Console.print(getWinner(carArr))
 
 }
 
+const racing = (carArr) => {
+
+    for (let i = 1; i <= carArr.length; i += 2) {
+        let isRun = MissionUtils.Random.pickNumberInRange(0, 9) >= 4
+
+        if (isRun) {
+            //i-1번째에 저장된 car이동
+            carArr[i] += 1
+        }
+    }
+
+}
+
+
+const printRacing = (carArr) => {
+    let result = ''
+
+    for (let i = 0; i < carArr.length; i += 2) {
+        const CAR_NAME = carArr[i]
+        const CAR_MOVE_NODE = '-'.repeat(carArr[i + 1])
+
+        result += `${CAR_NAME} : ${CAR_MOVE_NODE}\n`
+    }
+
+    Console.print(result)
+}
