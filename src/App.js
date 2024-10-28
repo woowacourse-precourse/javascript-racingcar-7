073040f82640aacd73ffb1a8fd161416438a7ddc
Doyeon <duseh729@naver.com>
2024-10-28 17:00:55 +0900

    feat: 자동차 이름 input 후 분리 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const names = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const count = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n")
+    
+    const nameArray = this.separateNameHandler(names);
+
+    Console.print(nameArray);
+  }
+
+  separateNameHandler(names){
+    return names.split(',');
+  }
 }
 
 export default App;
