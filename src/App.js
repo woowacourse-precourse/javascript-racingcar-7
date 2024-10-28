2266b6cc6c2c9451cd55da826ca3afb6943e792a
xxxjinn <tlsguswls319@naver.com>
2024-10-28 11:36:13 +0900

    refactor(App.js): 불필요한 console.log 삭제

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import { simulateRace, displayWinners } from "./raceManager.js";
 class App {
   async run() {
     const CARS = await getValidatedCarNames();
-    console.log(CARS);
 
     const ATTEMPT_COUNT = await getValidatedAttemptCount();
-      console.log("Attempt Count:", ATTEMPT_COUNT);
 
     const RACE_RESULT = simulateRace(CARS, ATTEMPT_COUNT);
     displayWinners(RACE_RESULT);
