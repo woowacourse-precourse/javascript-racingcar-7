77f66affe4cf439c9b63fe40a7a1c17707e9291e
L2HYUNN <donghyuni98@gmail.com>
2024-10-28 11:04:48 +0900

    chore(app): 불필요한 주석 제거

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import RacingModule from './racing/racing.module.js';
 
 class App {
-  // eslint-disable-next-line class-methods-use-this, no-empty-function
   async run() {
-    // new RacingController({
-    //   models: { RacingModel: new RacingModel() },
-    //   views: { RacingView: new RacingView() },
-    // }).init();
     await RacingModule.init();
   }
 }
