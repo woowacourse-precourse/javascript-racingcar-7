9d1d1ec6ec28cef8be6aae91f1b246d50fd7d6ac
Seoyeon Kim <kseoy0607@gmail.com>
2024-10-28 14:52:15 +0900

    feat: 공백 제거 후 자동차 이름 검증 및 반환 메서드 구현

--- src/Validator.js
+++ src/Validator.js
@@ -1,6 +1,14 @@
 import { trimSpaces } from './utils/trimSpaces.js';
 
 class Validator {
+  // 이름 검증 메서드
+  validateCarName(carName) {
+    const trimmedInput = trimSpaces(carName);
+    this.#checkLength(trimmedInput);
+    this.#checkName(trimmedInput);
+    return trimmedInput;
+  }
+
   #checkLength(carName) {
     if (carName.length > 5) {
       throw new Error('[ERROR] 이름은 5자 이하만 가능합니다.');
