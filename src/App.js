d25c7ca396a366505678a39c89f3b3e0cc96ab56
kochungwon <yud01063@gmail.com>
2024-10-28 14:28:47 +0900

    fix(오류수정):  오류를 내지않던 오류 수정 - InputVaildator 클래서에서 catch 한 오류룰 throw 하지못해 app 에서 오류가 발생하지않던 로직을 수정했습니다.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import UserInput from './UserInput.js';
 import RacingSession from './RacingSession.js';
+import InputValidator from './InputValidator.js';
 
 class App {
   async run() {
     const userInput = new UserInput();
     await userInput.inputCarNames();
     await userInput.inputAttempts();
-    userInput.validateInput();
+    
+    const validator = new InputValidator();
+    validator.validateCarNames(userInput.carNames); 
+    validator.validateAttempts(userInput.attempts);
   
     const racingSession = new RacingSession(userInput.carNames, userInput.attempts);
     racingSession.startRace();
   }
 }
+
 
 export default App;
--- src/InputValidator.js
+++ src/InputValidator.js
@@ -1,29 +1,21 @@
-    class InputValidator {
-        // 자동차 이름 배열을 확인하여 각 이름이 5자 이하인지 검사
-        validateCarNames(carNames) {
-            try {
-                carNames.forEach(name => {
-                    if (name.length > 5) {
-                        throw new Error("[ERROR] 자동차 이름은 5자 이하로 입력해야 합니다.");
-                    }
-                });
-            } catch (error) {
-                console.error(error.message);
-                return
+class InputValidator {
+    // 자동차 이름 배열을 확인하여 각 이름이 5자 이하인지 검사
+    validateCarNames(carNames) {
+        for (const name of carNames) {
+            if (name.length > 5) {
+                throw new Error("[ERROR] 자동차 이름은 5자 이하로 입력해야 합니다.");
             }
         }
-    
-        // 시도 횟수가 양의 정수인지 확인
-        validateAttempts(attempts) {
-            try {
-                if (attempts <= 0) {
-                    throw new Error("[ERROR] 경주 시도 횟수는 양수여야 합니다.");
-                }
-            } catch (error) {
-                console.error(error.message);
-                return
-            }
-        }
+        return true; 
     }
 
-    export default InputValidator;
+    // 시도 횟수가 양의 정수인지 확인
+    validateAttempts(attempts) {
+        if (attempts <= 0) {
+            throw new Error("[ERROR] 경주 시도 횟수는 양수여야 합니다.");
+        }
+        return true; 
+    }
+}
+
+export default  InputValidator
