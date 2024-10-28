6f53367d7e5fc2949b68a58f7663710489790296
sanghyeonLee <hyeon012366@gmail.com>
2024-10-28 20:22:06 +0900

    refactor: 에러 메세지 세부화로 인한 세부 조건 불필요에 의해 조건 유틸함수 삭제

--- src/utils/condition.js
+++
@@ -1,7 +0,0 @@
-export const firstErrorCondition =(string)=>{
-    return string.includes(" ") || string.length > 5
-}
-
-export const secondErrorCondition =(string)=>{
-    return string.includes(" ") || isNaN(string)
-}
