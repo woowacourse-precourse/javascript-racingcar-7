a1e832849d7f011623c0b5893595af2e33543a59
dudcns02 <duddl2640@naver.com>
2024-10-28 18:47:29 +0900

    chore: Correct typo

--- src/content.js
+++ src/content.js
@@ -9,7 +9,7 @@ const ERROR_MESSAGE = Object.freeze({
   INCORRECT_VALUE: "[ERROR] 공백은 입력할 수 없습니다.",
   LIMIT_COUNT_INCORRECT_VALUE: "[ERROR] 자동차 입력은 최대 5대까지 입니다.",
   DUPLICATE_VALUE: "[ERROR] 중복된 자동차가 포함되어 있습니다.",
-  NAME_LIMIT: "[ERROR] 이름은 최소 5자 이하만 가능합니다.",
+  NAME_LIMIT: "[ERROR] 이름은 5자 이하만 가능합니다.",
   TRY_NUMBER_LIMIT: "[ERROR] 최대 시도 횟수는 10회입니다.",
   ONLY_INTGER: "[ERROR] 시도 횟수는 숫자만 입력 가능합니다.",
 });
