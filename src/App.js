bc43e176fe11ac038b0d3cb92e8d929bed1dfd04
JeongWon-CHO <ahin0312@naver.com>
2024-10-28 20:35:34 +0900

    refactor: 카멜 표기법으로 수정

--- src/App.js
+++ src/App.js
@@ -1,88 +1,88 @@
 import { Console, MissionUtils } from '@woowacourse/mission-utils';
 
 async function InputValue() {
-  let input_name = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-  let car_name = input_name.split(',');
+  let inputName = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+  let carName = inputName.split(',');
 
-  car_name.forEach(index => {
+  carName.forEach(index => {
     if (index.length > 5) throw new Error("[ERROR] 잘못된 입력입니다.");
   })
-  // 되냐 이거 ??
-  if (car_name[car_name.length - 1] == '') throw new Error("[ERROR] 잘못된 입력입니다.");
 
-  let input_times = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-  let times = Number(input_times);  // 숫자가 아닌 무언가가 들어가면 NaN 반환
+  if (carName[carName.length - 1] == '') throw new Error("[ERROR] 잘못된 입력입니다.");
+
+  let inputTimes = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+  let times = Number(inputTimes);  // 숫자가 아닌 무언가가 들어가면 NaN 반환
 
   if (Number.isNaN(times)) throw new Error("[ERROR] 잘못된 입력입니다.");
 
-  return [car_name, input_times];
+  return [carName, inputTimes];
 }
 
 function Forward_Judgment() {  // 랜덤 돌리고 결과값 boolean 값으로 반환
-  let randNum = MissionUtils.Random.pickNumberInRange(0, 9);
+  let randomNum = MissionUtils.Random.pickNumberInRange(0, 9);
   
-  if (randNum >= 4)
+  if (randomNum >= 4)
     return true;
   else
     return false;
 }
 
-function IncreaseCount(decision, cnt) {  // boolean 값으로 cnt 증가시킴
+function IncreaseCount(decision, count) {  // boolean 값으로 cnt 증가시킴
   if (decision === true)
-    cnt += 1
-  return cnt
+    count += 1
+  return count
 }
 
-function DrawForward(car, cnt) {  // 중간 출력 형식 맞추기
-  Console.print(car + ' : ' + '-'.repeat(cnt));
+function DrawForward(car, count) {  // 중간 출력 형식 맞추기
+  Console.print(car + ' : ' + '-'.repeat(count));
   return 0;
 }
 
-function WinnerDecision(cnt_array) {
-  let max_value = 0;
-  max_value = Math.max.apply(null, cnt_array);  // 최대값 구하기
+function WinnerDecision(cntArray) {
+  let maxValue = 0;
+  maxValue = Math.max.apply(null, cntArray);  // 최대값 구하기
 
   let result = []
   let i = 0;
-  cnt_array.forEach(value => {  // 최대값 인덱스 구하기
-    if (value === max_value) result.push(i);
+  cntArray.forEach(value => {  // 최대값 인덱스 구하기
+    if (value === maxValue) result.push(i);
     i++;
   });
   
   return result;
 }
 
-function WinnerPrint(carArr, win) {  // 우승자 출력
+function WinnerPrint(carArr, winArr) {  // 우승자 출력
   let result = []
-  win.forEach(i => {
+  winArr.forEach(i => {
     result.push(carArr[i]);
   })
 
-  const tmp = result.join(', ');
-  return tmp;
+  const arrToStr = result.join(', ');
+  return arrToStr;
 }
 
 class App {
   async run() {
 
-    let [car_name, input_times] = await InputValue();
-    let forward_cnt = Array.from({ length: car_name.length }, () => 0);
-    Console.print('forward : '+ forward_cnt);
+    let [carName, inputTimes] = await InputValue();
+    let forwardCount = Array.from({ length: carName.length }, () => 0);
+    Console.print('forward : '+ forwardCount);
     Console.print("\n실행 결과");
-    for (let i=0; i<input_times; i++) {
-      for (let j=0; j<car_name.length; j++) {
-        let forward_decision = Forward_Judgment();
-        forward_cnt[j] = await IncreaseCount(forward_decision, forward_cnt[j]);
-        let tmp = DrawForward(car_name[j], forward_cnt[j]);
+    for (let i=0; i<inputTimes; i++) {
+      for (let j=0; j<carName.length; j++) {
+        let forwardDecision = Forward_Judgment();
+        forwardCount[j] = await IncreaseCount(forwardDecision, forwardCount[j]);
+        let tmp = DrawForward(carName[j], forwardCount[j]);
       }
       Console.print('');
     }
 
-    const winner = WinnerDecision(forward_cnt);
+    const winner = WinnerDecision(forwardCount);
 
-    let win_car = WinnerPrint(car_name, winner);
+    let winCar = WinnerPrint(carName, winner);
     
-    Console.print('최종 우승자 : ' + win_car);
+    Console.print('최종 우승자 : ' + winCar);
   }
 }
 
