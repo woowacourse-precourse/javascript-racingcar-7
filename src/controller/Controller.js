74adf3435a68681aa87ee2ae477bd98352638f0d
leelsj2007 <leelsj2007@gmail.com>
2024-10-28 21:45:04 +0900

    feat :Controller에 settingCar 함수 추가

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,19 +1,31 @@
 import InputView from "../view/InputView.js";
 import OutputView from "../view/OutputView.js";
 import carNameSplit from "../utils/carNameSplit.js";
+import Car from "../model/Car.js";
 
 class Controller {
     #inputView;
     #outputView;
+    #car
 
     constructor() {
         this.#inputView = new InputView();
         this.#outputView = new OutputView();
+        this.#car = [];
+    }
+
+    settingCar(nameArray) {
+      for (const carName of nameArray) {
+        this.#car.push(new Car(carName));
+      }
     }
 
     async playRacingCar() {
       const [carNameData, numberOfAttemptsData] = await this.#inputView.readInput();
       const carNameArray = carNameSplit(carNameData);
+
+      this.settingCar(carNameArray);
+
       this.#outputView.printResult();
     }
 }
