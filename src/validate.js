b43fd0cef21c0b218fde2157420dded8e3750a36
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2024-10-28 19:35:42 +0900

    feat: 자동차 최소 대수에 대한 예외처리 추가

--- src/validate.js
+++ src/validate.js
@@ -1,18 +1,28 @@
 import {
   CAR_NAME_ERROR_MESSAGE,
+  CAR_VALIDATION_ERROR_MESSAGE,
   ATTEMPT_COUNT_ERROR_MESSAGE,
 } from "./Message.js";
 
 const MAX_NAME_LENGTH = 5;
 
-export const validateName = (name) => {
-  if (name.length > MAX_NAME_LENGTH) {
-    throw new Error(CAR_NAME_ERROR_MESSAGE.carNameTooLong);
+export const validateName = (carNames) => {
+  if (carNames.length < 2) {
+    throw new Error(CAR_VALIDATION_ERROR_MESSAGE.carCountTooLow);
   }
-  if (name.trim() === "") {
-    throw new Error(CAR_NAME_ERROR_MESSAGE.carNameEmpty);
-  }
-  return name;
+  carNames.forEach((name) => {
+    if (name.length > MAX_NAME_LENGTH) {
+      throw new Error(CAR_NAME_ERROR_MESSAGE.carNameTooLong);
+    }
+    if (name.trim() === "") {
+      throw new Error(CAR_NAME_ERROR_MESSAGE.carNameEmpty);
+    }
+    if (name.includes(" ")) {
+      throw new Error(CAR_NAME_ERROR_MESSAGE.carNameContainsWhitespace);
+    }
+  });
+
+  return carNames;
 };
 
 export const validateUserAttempts = (attempts) => {
