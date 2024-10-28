800b55b1c022319a90c3764fda2de8ab3647df24
egg-silver <v6984@naver.com>
2024-10-28 16:53:20 +0900

    feat: 시도 횟수 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Console, MissionUtils } from '@woowacourse/mission-utils';
-import { checkCarNum } from './checkInput.js';
+import { checkCarNum, checkAttemptCount } from './checkInput.js';
 
 class App {
   async run() {
@@ -7,11 +7,19 @@ class App {
       Console.print(
         '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
       );
-
       const input = await Console.readLineAsync('');
-      const carNames = input.split(',');
+      const carNames = input
+        .split(',')
+        .map((name) => name.trim())
+        .filter((name) => name !== '');
 
       checkCarNum(carNames);
+
+      // 시도 횟수 입력받기
+      Console.print('시도할 횟수는 몇 회인가요?');
+      const attemptInput = await Console.readLineAsync('');
+      const attemptCount = Number(attemptInput);
+      checkAttemptCount(attemptCount);
 
       let random = MissionUtils.Random.pickNumberInRange(0, 9);
       console.log(random);
