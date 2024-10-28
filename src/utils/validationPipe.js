37b75be761ffa526b18d2e2a4d430597f814e383
nerdchanii <nerdchanii@gmail.com>
2024-10-28 21:33:01 +0900

    fix(validator): fix validate function

--- src/utils/validationPipe.js
+++ src/utils/validationPipe.js
@@ -7,7 +7,9 @@ const someConditionPipe = function (data, ...fn) {
 export const carNameValidatePipe = function (carNameString) {
   const carNames = carNameString.split(',');
   for (const carName of carNames) {
-    if(someConditionPipe(carName, isEmptyString, isLongerThan(5))) throw new Error('[ERROR] 차 이름이 잘못 되었어요');
+    if(someConditionPipe(carName, isEmptyString, isLongerThan(5))) {
+      throw new Error('[ERROR] 차 이름이 잘못 되었어요');
+    }
   }
   const carSet = new Set(carNames.map((carname) => carname.trim()));
   if (carSet.size !== carNames.length) {
@@ -21,7 +23,6 @@ export const iterationValidatePipe = function (iterationString) {
     throw new Error('[ERROR] 입력된 반복 횟수가 숫자가 아니에요!');
   }
   const parsedNumber = parseInt(iterationString, 10);
-
   if (parsedNumber !== Number(iterationString)) {
     throw new Error('[ERROR] 정수로 입력해주세요!:)');
   }
--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,19 +1,7 @@
-export const isNumber = function (string) {
-  return !isNaN(string);
-};
-
 export const isEmptyString = function (string) {
   return string.trim() === '';
 };
 
-export const isNotEmptyString = function (string) {
-  return string.length !== 0;
-};
-
-export const isShorterThan = function (maxLength) {
-  return (str) => str.length <= maxLength;
-};
-
 export const isLongerThan = function (len) {
-  return (str) => str.length >= len;
+  return (str) => str.length > len;
 };
