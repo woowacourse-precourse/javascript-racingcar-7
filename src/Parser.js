291cceca24197bdbaf3a4660bf0f3aacf6c83ccb
YeWon Seo <syw1236@naver.com>
2024-10-28 20:46:31 +0900

    refactor(Parser): , 기준으로 나누는 기능만 하도록 수정

--- src/Parser.js
+++ src/Parser.js
@@ -2,9 +2,7 @@ import Validator from './Validator.js';
 
 export default class Parser {
     static separateCarName(inputCarName) {
-        Validator.isEmpty(inputCarName);
         const carNameList = inputCarName.split(',').map(name => name.trim());
         return carNameList
     }
-
 }
