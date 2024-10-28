2e5ee43aa04642261babdec4c86200f4089c86c5
jeonhyeonseon <goodpretty0325@gmail.com>
2024-10-28 22:43:16 +0900

    style: 코드 일관성을 위해 Prettier로 포매팅

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import Race from './Race.js';
+import Race from "./Race.js";
 
 class App {
   async run() {
-
     try {
-    const carNames = await this.getInput("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ");
-    const position = await this.getInput("시도할 횟수를 입력하세요: ");
-    
+      const carNames = await this.getInput(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): "
+      );
+      const position = await this.getInput("시도할 횟수를 입력하세요: ");
+
       const race = new Race(carNames, parseInt(position, 10));
       race.showResult();
     } catch (error) {
@@ -19,7 +20,6 @@ class App {
   async getInput(promptText) {
     return MissionUtils.Console.readLineAsync(promptText);
   }
-
 }
 
 export default App;
