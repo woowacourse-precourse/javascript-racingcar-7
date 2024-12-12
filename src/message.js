c8cb19f019a96a8534c2d8940c267feee5901cdb
ahhyun <wndkgus0225@naver.com>
2024-12-12 22:27:20 +0900

    4

--- src/message.js
+++ src/message.js
@@ -1,13 +1,14 @@
 export const MESSAGE = {
   ASK_CARNAMES: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
   ASK_COUNT: "시도할 횟수는 몇 회인가요?",
-  EXECUTION_RESULT: "실행 결과"
+  RESULT: "실행 결과",
+  WINNER: "최종 우승자 : "
 }
 
 export const ERROR_MESSAGE = {
   NAME_LENGTH_EXCEED: "[ERROR] 이름은 5자 이하만 가능합니다.",
-  NOT_NUMBER: "[ERROR] 숫자 이외의 입력은 사용할 수 없습니다.",
-  NOT_INTEGER: "[ERROR] 정수 이외의 입력은 사용할 수 없습니다.",
-  NUM_IS_ZERO : "[ERROR] 0은 입력할 수 없습니다.",
-  NEGATIVE_NUMBER: "[ERROR] 음수는 입력할 수 없습니다."
+  NOT_NUMBER: "[ERROR] 숫자 외에는 입력할 수 없습니다.",
+  NUM_IS_ZERO: "[ERROR] 0은 입력할 수 없습니다.",
+  NOT_INTEGER: "[ERROR] 정수만 입력이 가능합니다.",
+  NUM_IS_NEGATIVE: "[ERROR] 음수는 입력할 수 없습니다."
 }
